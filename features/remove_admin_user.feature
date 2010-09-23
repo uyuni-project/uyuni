42d60a2cf28c26df2d4824194d027864a1c22ac0
Klaus Kämpf <kkaempf@suse.de>
2010-09-23 11:12:28 +0200

    Feature: remove admin user
    This uses DBI (rubygem-dbi) low-level database access to remove the
    admin user.

---
+++ features/remove_admin_user.feature
@@ -0,0 +1,13 @@
+# feature/remove_admin_user.feature
+#  see also: init_user_create.feature
+@cleanup @database
+Feature: Remove initial users
+  In Order to revert to a pristine state
+  As a testing users
+  I need to remove the admin user
+
+  Scenario: Remove Admin users
+    Given I have low-level access to the database
+    When I remove the admin user
+# hack to make it DRY
+    Given I access the host the first time
--- features/step_definitions/database_lowlevel.rb
+++ features/step_definitions/database_lowlevel.rb
@@ -1,6 +1,11 @@
 # low-level access to the database
 
 class Database
+  # Set locale to 'C'. Without it, ruby-oci8 hangs in nsgetcinfo() (deep inside the oracle client lib)
+  #   Found http://www.nntp.perl.org/group/perl.dbi.users/2010/09/msg35317.html and
+  #   http://www.nntp.perl.org/group/perl.dbi.users/2010/09/msg35319.html
+  #   hinting towards a locale problem. And setting LC_ALL to C fixed it.
+  ENV["LC_ALL"] = "C"
   require File.join(File.dirname(__FILE__), "..", "..", "lib", "spacewalk_testsuite_base", "database")
   include DatabaseLowLevelAccess
 end
@@ -17,11 +22,38 @@ When /^I remove the admin user$/ do
   @dbh = db.get_dbi_connection()
   raise "Can't connect to database" unless @dbh
   admin = nil
-  @dbh.execute("SELECT user_id FROM rhnusergroupmembers WHERE user_group_id = ?", "6") do |sth|
-    row = sth.fetch
-    admin = row[0]
-    $stderr.puts "Admin at id #{admin}"
+  begin
+    # get the 'spacewalk superuser'
+    @dbh.execute("SELECT user_id FROM rhnusergroupmembers WHERE user_group_id = ?", "6") do |sth|
+      row = sth.fetch
+      admin = row[0] if row
+      $stderr.puts "Admin at id #{admin}"
+    end
+    if admin
+      rows = @dbh.do("DELETE from RHNUSERINFO where user_id = ?", admin)
+      $stderr.puts "#{rows} removed from RHNUSERINFO"
+      
+      rows = @dbh.do("DELETE from RHNUSERGROUPMEMBERS where user_id = ?", admin)
+      $stderr.puts "#{rows} removed from RHNUSERGROUPMEMBERS"
+      
+      rows = @dbh.do("UPDATE RHNUSERGROUP set CURRENT_MEMBERS = ? where CURRENT_MEMBERS = ?", 0, 1)
+      $stderr.puts "#{rows} updated in RHNUSERGROUP"
+      
+      rows = @dbh.do("TRUNCATE TABLE WEB_CONTACT")
+      $stderr.puts "#{rows} removed from WEB_CONTACT"
+
+      @dbh.do("COMMIT")
+    else
+      $stderr.puts "Admin user already removed"
+    end
+
+  rescue DBI::DatabaseError => e
+    $stderr.puts "An error occurred"
+    $stderr.puts "Error code: #{e.err}"
+    $stderr.puts "Error message: #{e.errstr}"
+    $stderr.puts "Error SQLSTATE: #{e.state}"
+    raise "Datbase operation failed"
+  ensure
+    @dbh.disconnect
   end
-  raise "Can't read from RHNUSERGROUPMEMBERS" unless admin
-  @dbh.disconnect
 end
