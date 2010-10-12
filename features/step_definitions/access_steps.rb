bedc2e0d352c4443a95a54b1294d6ab9f45ab1a7
Michael Calmer <mc@suse.de>
2010-10-12 13:57:01 +0000

    raise an exception if failed pages are found, otherwise it is not visible in the cucumber log

--- features/step_definitions/access_steps.rb
+++ features/step_definitions/access_steps.rb
@@ -33,29 +33,34 @@ Then /^no link should be broken$/ do
       $stderr.puts "-- ** failed"
     else
       collect_all_hrefs.each do |href|
-	next if href[0,1] == "#" # relative link
-	next if hrefs.include?(href)
-	hbase = href.split("?")[0]
-	next if visited[hbase]
-	visited[hbase] = true
+        next if href[0,1] == "#" # relative link
+        next if hrefs.include?(href)
+        hbase = href.split("?")[0]
+        next if visited[hbase]
+        visited[hbase] = true
         unless href[0,1] == "/"
-#	  $stderr.puts "From #{href} (#{base})"
-	  hsplit = base.split("/")
-	  hsplit.pop
-	  hsplit << href
-	  href = hsplit.join("/")
-#	  $stderr.puts "\t to #{href}"
-	end
-#	$stderr.puts "Adding #{href}"
-	hrefs << href 
+#	      $stderr.puts "From #{href} (#{base})"
+	      hsplit = base.split("/")
+	      hsplit.pop
+	      hsplit << href
+	      href = hsplit.join("/")
+#	      $stderr.puts "\t to #{href}"
+	    end
+#	    $stderr.puts "Adding #{href}"
+	    hrefs << href 
       end
     end
     break if hrefs.empty?
   end
   $stderr.puts "\nFinished. Visited #{visited.size} pages. Failed pages:"
+  failed_pages = ""
   visited.each_value do |f|
     next if f.is_a? TrueClass
+    failed_pages << "\t#{f}\n"
     $stderr.puts "\t#{f}"
   end
   $stderr.puts "End of failed pages"
+  if ! failed_pages.empty?
+    raise "Failed pages:\n#{failed_pages}"
+  end
 end
