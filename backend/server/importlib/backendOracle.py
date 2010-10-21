c7f5f3f1868da20a12c0053d28a7a481961dd7fb
Michael Calmer <mc@suse.de>
2010-10-21 11:01:53 +0200

    DB column 'href' is part of the ehnErrataBuglist Table

--- backend/server/importlib/backendOracle.py
+++ backend/server/importlib/backendOracle.py
@@ -205,7 +205,6 @@ class OracleBackend(Backend):
 		'locally_modified' : DBstring(1),
                 'severity_id'   : DBint(),
                 'errata_from'   : DBstring(127),
-                'href'          : DBstring(255),
                 # We will treat issue_date and update_date as regular dates
                 # with times instead of DBdate types, otherwise we'd have 
                 # issues with timezones
@@ -221,6 +220,7 @@ class OracleBackend(Backend):
                 'errata_id'     : DBint(),
                 'bug_id'        : DBint(),
                 'summary'       : DBstring(4000),
+                'href'          : DBstring(255),
             },
             pk          = ['errata_id', 'bug_id'],
             attribute   = 'bugs',
