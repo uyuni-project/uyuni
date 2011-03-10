4d7b7e67b8f89f241b42ab034d0776b8518138bb
Uwe Gansert <ug@suse.de>
2011-03-10 17:03:46 +0100

    Registration of REHL4 clients fails due to incomplete database table (bnc#678110)

--- schema/spacewalk/common/data/suseOSTarget.sql
+++ schema/spacewalk/common/data/suseOSTarget.sql
@@ -25,6 +25,12 @@ insert into suseOSTarget (id, os, target, channel_arch_id) values
 insert into suseOSTarget (id, os, target, channel_arch_id) values
 (sequence_nextval('suse_ostarget_id_seq'), 'sle-11-x86_64', 'sle-11-x86_64', LOOKUP_CHANNEL_ARCH('channel-x86_64'));
 
+insert into suseOSTarget (id, os, target, channel_arch_id) values
+(sequence_nextval('suse_ostarget_id_seq'), 'i386', 'i386', LOOKUP_CHANNEL_ARCH('channel-ia32'));
+
+insert into suseOSTarget (id, os, target, channel_arch_id) values
+(sequence_nextval('suse_ostarget_id_seq'), 'x86_64', 'x86_64', LOOKUP_CHANNEL_ARCH('channel-x86_64'));
+
 -- insert into suseOSTarget (id, os, target, channel_arch_id) values
 -- (sequence_nextval('suse_ostarget_id_seq'), '', '', LOOKUP_CHANNEL_ARCH())
 
