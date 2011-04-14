343525b8ffdf0ad19a31db9eefdef492bb3909c9
Johannes Renner <jrenner@suse.de>
2011-04-14 15:15:50 +0200

    CVE-2010-1171 - Remove the channel APP handler (bnc#685078)

--- backend/server/handlers/app/__init__.py
+++ backend/server/handlers/app/__init__.py
@@ -18,12 +18,10 @@
 __all__ = []
 
 import packages
-import channel
 import comps
 
 rpcClasses = {
     "packages"          : packages.Packages,
-    "channel"           : channel.Channel,
     "comps"             : comps.Comps,
     }
 
