2c0efd084a5ef07c3d18393e98e3ec23cfd8dee4
Ionuț Arțăriși <mapleoin@lavabit.com>
2011-04-21 10:59:58 +0200

    channel families labels are unique, so no need to branch

--- susemanager/ncc-sync/mgr_ncc_sync_lib.py
+++ susemanager/ncc-sync/mgr_ncc_sync_lib.py
@@ -209,11 +209,8 @@ class NCCSync(object):
         for family in etree.parse(CHANNEL_FAMILIES).getroot():
             if family.get('default_nodecount') == '-1':
                 label = family.get('label')
-                if label not in subscription_count:
-                    subscription_count[label] = {'consumed': 0,
-                                                 'nodecount': INFINITE}
-                else:
-                    subscription_count[label]['nodecount'] = INFINITE
+                subscription_count[label] = {'consumed': 0,
+                                             'nodecount': INFINITE}
 
         # delete subscriptions that are no longer available in NCC
         q = rhnSQL.prepare(
