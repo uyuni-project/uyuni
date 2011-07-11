cae945e4972b72337f8a5f216398ba3a46cc658d
Michael Calmer <mc@suse.de>
2011-07-11 14:31:29 +0200

    add suseImages table

--- schema/spacewalk/upgrade/susemanager-schema-1.2.71-to-susemanager-schema-1.2.72/002-create-suseImages-table.sql
+++ schema/spacewalk/upgrade/susemanager-schema-1.2.71-to-susemanager-schema-1.2.72/002-create-suseImages-table.sql
@@ -21,27 +21,18 @@ CREATE TABLE suseImages
 			    ON DELETE CASCADE,
     name               VARCHAR2(254) NOT NULL,
     version            VARCHAR2(64) NOT NULL,
-    arch               VARCHAR2(10) NOT NULL,
-    image_type         VARCHAR2(10) NOT NULL,
-    download_url       VARCHAR2(256) NOT NULL,
+    imgtype            VARCHAR2(10) NOT NULL,
     path               VARCHAR2(1000) NOT NULL,
-    file_name          VARCHAR2(128) NOT NULL,
     checksum           VARCHAR2(128) NOT NULL,
-    status             VARCHAR2(10) DEFAULT('NEW') NOT NULL
-                                CHECK (status in ('NEW', 'PICKUP', 'RUNNING', 'DONE', 'ERROR')),
     created            DATE
                            DEFAULT (sysdate) NOT NULL,
     modified           DATE
-                           DEFAULT (sysdate) NOT NULL
+                           DEFAULT (sysdate) NOT NULL,
 )
 ENABLE ROW MOVEMENT
 ;
 
 CREATE SEQUENCE suse_images_id_seq;
-
-CREATE UNIQUE INDEX suse_img_file_name_checksum_uq
-    ON suseImages (file_name, checksum)
-            TABLESPACE [[2m_tbs]];
 
 create or replace trigger
 suse_images_mod_trig
