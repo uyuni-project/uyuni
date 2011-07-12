5b0963c7f023edaaf61812f4a1ea6ca522c51e4d
Michael Calmer <mc@suse.de>
2011-07-12 17:57:25 +0200

    implement new java action image.deploy

--- java/code/src/com/redhat/rhn/domain/action/image/DeployImageAction.java
+++ java/code/src/com/redhat/rhn/domain/action/image/DeployImageAction.java
@@ -1,17 +1,65 @@
+/**
+ * Copyright (c) 2011 Novell
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ */
 package com.redhat.rhn.domain.action.image;
 
 import com.redhat.rhn.domain.action.Action;
+import com.redhat.rhn.domain.action.ActionFormatter;
+import com.redhat.rhn.domain.image.Images;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * DeployImageAction - Class representation of the table rhnAction.
+ * @version $Rev$
+ */
 public class DeployImageAction extends Action {
 
-	private static final long serialVersionUID = 1438261396065921002L;
-	private DeployImageActionDetails details;
+    private Set images;
 
-	public DeployImageActionDetails getDetails() {
-		return details;
-	}
+    /**
+     * @return Returns the images.
+     */
+    public Set getImages() {
+        return images;
+    }
 
-	public void setDetails(DeployImageActionDetails details) {
-		this.details = details;
-	}
+    /**
+     * @param imagesIn The images to set.
+     */
+    public void setImages(Set imagesIn) {
+        this.images = imagesIn;
+    }
+
+    /**
+     * Add an images to this action.
+     * @param i Image to add
+     */
+    public void addImage(Images i) {
+        if (images == null) {
+            images = new HashSet();
+        }
+        images.add(i);
+    }
+
+    /**
+     * Get the Formatter for this class but in this case we use
+     * ErrataActionFormatter.
+     *
+     * {@inheritDoc}
+     */
+    public ActionFormatter getFormatter() {
+        if (formatter == null) {
+            formatter = new DeployImageActionFormatter(this);
+        }
+        return formatter;
+    }
 }
--- java/code/src/com/redhat/rhn/manager/action/ActionManager.java
+++ java/code/src/com/redhat/rhn/manager/action/ActionManager.java
@@ -28,7 +28,6 @@ import com.redhat.rhn.domain.action.config.ConfigAction;
 import com.redhat.rhn.domain.action.config.ConfigUploadAction;
 import com.redhat.rhn.domain.action.errata.ErrataAction;
 import com.redhat.rhn.domain.action.image.DeployImageAction;
-import com.redhat.rhn.domain.action.image.DeployImageActionDetails;
 import com.redhat.rhn.domain.action.kickstart.KickstartAction;
 import com.redhat.rhn.domain.action.kickstart.KickstartActionDetails;
 import com.redhat.rhn.domain.action.kickstart.KickstartGuestAction;
@@ -46,6 +45,7 @@ import com.redhat.rhn.domain.config.ConfigFileName;
 import com.redhat.rhn.domain.config.ConfigRevision;
 import com.redhat.rhn.domain.config.ConfigurationFactory;
 import com.redhat.rhn.domain.errata.Errata;
+import com.redhat.rhn.domain.image.Images;
 import com.redhat.rhn.domain.errata.impl.PublishedErrata;
 import com.redhat.rhn.domain.image.Image;
 import com.redhat.rhn.domain.kickstart.KickstartData;
@@ -621,26 +621,20 @@ public class ActionManager extends BaseManager {
      * @param user The user scheduling image deployment
      * @param image The image pertaining to this action
      */
-    public static Action createDeployImageAction(User user, Image image, Long vcpus, Long memKb) {
+    public static Action createDeployImageAction(User user, Images image) {
         DeployImageAction a = (DeployImageAction)ActionFactory
                              .createAction(ActionFactory.TYPE_DEPLOY_IMAGE);
         if (user != null) {
             a.setSchedulerUser(user);
             a.setOrg(user.getOrg());
         }
-        
-        DeployImageActionDetails details = new DeployImageActionDetails();
-        details.setParentAction(a);
-        details.setImageId(image.getId());
-        details.setVcpus(vcpus);
-        details.setMemKb(memKb);
-        a.setDetails(details);
+        a.addImage(image);
 
         //Object[] args = new Object[2];
         //args[0] = errata.getAdvisory();
         //args[1] = errata.getSynopsis();
         //a.setName(LocalizationService.getInstance().getMessage("action.name", args));
-        a.setName("Image deployment: " + image.getName() + "-" + image.getVersion());
+        a.setName("Image deployment" + image.getName() + "-" + image.getVersion());
         return a;
     }
 
