9c15c8afdce27dee313455b4cde19821d9c1bf4a
Johannes Renner <jrenner@suse.de>
2011-07-13 11:20:10 +0200

    Add new page for loading images from SUSE Studio and listing these

---
+++ java/code/src/com/redhat/rhn/frontend/action/rhnpackage/profile/StudioImagesListAction.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2009--2010 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.frontend.action.rhnpackage.profile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
+
+import com.redhat.rhn.frontend.struts.RequestContext;
+import com.redhat.rhn.frontend.struts.RhnListAction;
+import com.redhat.rhn.frontend.taglibs.list.helper.ListHelper;
+import com.redhat.rhn.frontend.taglibs.list.helper.Listable;
+import com.suse.studio.client.SUSEStudioClient;
+import com.suse.studio.client.data.Appliance;
+
+/**
+ * This action will present the user with a list of all studio images
+ * and allow one to be selected.
+ *
+ * @version $Revision$
+ */
+public class StudioImagesListAction extends RhnListAction implements Listable {
+
+    private static final String DATA_SET = "pageList";
+
+    // The credentials
+    private String studioUser;
+    private String studioAPIKey;
+    
+    /** {@inheritDoc} */
+    public ActionForward execute(ActionMapping actionMapping,
+                                 ActionForm actionForm,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+        throws Exception {
+    	
+    	// Determine credentials from the form
+        if (actionForm instanceof DynaActionForm) {
+        	DynaActionForm form = (DynaActionForm) actionForm;
+        	studioUser = form.getString("studio_user");
+        	studioAPIKey = form.getString("studio_api_key");
+        }
+    	
+        ListHelper helper = new ListHelper(this, request);
+        helper.setDataSetName(DATA_SET);
+        helper.execute();
+        
+//        ActionForward forward;
+//        if (helper.isDispatched()) {
+//            // Nothing to do when dispatched, there is a confirmation page displayed next
+//            // that will do the actual work
+//            forward = actionMapping.findForward("continue");
+//        }
+//        else {
+//        	forward = actionMapping.findForward("default");
+//        }
+        
+        ActionForward forward = actionMapping.findForward("default");
+        return forward;
+    }
+
+    /** {@inheritDoc} */
+    public List getResult(RequestContext context) {
+        List<Appliance> ret = new ArrayList<Appliance>();
+    	if (weHaveCredentials()) {
+        	SUSEStudioClient client = new SUSEStudioClient(studioUser, studioAPIKey);
+    		try {
+    			ret = client.getAppliances();
+    		} catch (IOException e) {
+    			throw new RuntimeException(e);
+    		}	
+    	}
+        return ret;
+    }
+    
+    /**
+     * Check if we currently have valid credentials.
+     * @return true or false
+     */
+    private boolean weHaveCredentials() {
+    	return studioUser != null && !studioUser.isEmpty() && 
+		studioAPIKey != null && !studioAPIKey.isEmpty();
+    }
+}
---
+++ java/code/webapp/WEB-INF/pages/studio/list.jsp
@@ -0,0 +1,51 @@
+<%@ taglib uri="http://rhn.redhat.com/rhn" prefix="rhn" %>
+<%@ taglib uri="http://rhn.redhat.com/tags/list" prefix="rl" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://jakarta.apache.org/struts/tags-html" prefix="html" %>
+<%@ taglib uri="http://jakarta.apache.org/struts/tags-bean" prefix="bean" %>
+
+<html:html xhtml="true">
+    <body>
+	<rhn:toolbar base="h1" img="/img/rhn-icon-errata.gif">
+        SUSE Studio Images
+    </rhn:toolbar>
+
+	<div class="page-summary">
+      <p>Please enter SUSE Studio credentials to list your images from SUSE Studio.</p>
+	</div>
+
+    <rl:listset name="groupSet">
+        <rhn:csrf />
+        <p>
+            <input type="text" name="studio_user" />
+            <input type="text" name="studio_api_key" />
+            <html:submit>
+                List Images
+            </html:submit>
+            <br/>
+        </p>
+
+        <rl:list dataset="pageList" 
+                 emptykey="studio.images.list.noimages">
+            <rl:column headerkey="studio.images.list.name">
+                ${current.name}
+            </rl:column>
+            <rl:column headerkey="studio.images.list.arch">
+                ${current.arch}
+            </rl:column>
+            <rl:column headerkey="studio.images.list.type">
+                ${current.type}
+            </rl:column>
+        </rl:list>
+
+        <div align="right">
+            <rhn:submitted/>
+            <hr/>
+            <input type="submit"
+                   name="dispatch"
+                   value="Schedule Download" />
+        </div>
+    </rl:listset>
+
+    </body>
+</html:html>
