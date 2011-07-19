5ee2a237d4eb8b168514e470cc4b1b7dd429bcc8
Michael Calmer <mc@suse.de>
2011-07-19 16:35:05 +0200

    new mgr-bootstrap feature
    * create a bootstrap script using mgr-bootstrap
      with activation key for package installation
    * download and execute the bootstrap script
    * test if the package is installed.

--- features/step_definitions/ssh_steps.rb
+++ features/step_definitions/ssh_steps.rb
@@ -9,6 +9,33 @@ When /^I execute ncc\-sync "([^"]*)"$/ do |arg1|
     end
 end
 
+When /^I execute mgr\-bootstrap "([^"]*)"$/ do |arg1|
+    arch=`uname -m`
+    arch.chomp!
+    if arch != "x86_64"
+        arch = "i586"
+    end
+    $sshout = ""
+    $sshout = `echo | ssh -l root -o StrictHostKeyChecking=no $TESTHOST mgr-bootstrap --activation-keys=1-SUSE-PKG-#{arch} #{arg1} 2>&1`
+    if ! $?.success?
+        raise "Execute command failed: #{$!}: #{$sshout}"
+    end
+end
+
+When /^I fetch "([^"]*)" from server$/ do |arg1|
+    output = `curl -SkO http://$TESTHOST/#{arg1}`
+    if ! $?.success?
+	raise "Execute command failed: #{$!}: #{output}"
+    end
+end
+
+When /^I execute "([^"]*)"$/ do |arg1|
+    output = `sh ./#{arg1}`
+    if ! $?.success?
+	raise "Execute command (#{arg1}) failed(#{$?}): #{$!}: #{output}"
+    end
+end
+
 Then /^I want to get "([^"]*)"$/ do |arg1|
     found = false
     $sshout.each_line() do |line|
