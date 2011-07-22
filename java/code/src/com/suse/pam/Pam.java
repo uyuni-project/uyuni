d35f21512ff3fdc3e34bf378859aa85d7215cf71
Johannes Renner <jrenner@suse.de>
2011-07-22 14:59:27 +0200

    Wrap around unix2_chkpwd instead of using jpam library (bnc#705758)

---
+++ java/code/src/com/suse/pam/Pam.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2011 Novell
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.suse.pam;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+/**
+ * Wrap around 'unix2_chkpwd' for authenticating a user against PAM.
+ */
+public class Pam {
+
+    /* Path to the command, can be made configurable as well */
+    private static String COMMAND = "/sbin/unix2_chkpwd";
+    /* The specific authentication service to use for this instance */
+    private final String authService;
+
+    /**
+     * Public constructor.
+     * 
+     * @param authService
+     *            The auth service to use for this instance
+     */
+    public Pam(String authService) {
+        this.authService = authService;
+    }
+
+    /**
+     * Perform the actual authentication by calling the 'unix2_chkpwd' binary.
+     * 
+     * @param user
+     *            The user to authenticate
+     * @param passwd
+     *            The password
+     * @return A {@link PamReturnValue} representing the result.
+     */
+    public PamReturnValue authenticate(String user, String passwd) {
+        PamReturnValue ret = PamReturnValue.PAM_FAILURE;
+        try {
+            // Init the command execution
+            Runtime rt = Runtime.getRuntime();
+            Process pr = rt.exec(COMMAND + " " + this.authService + " " + user);
+
+            // Write the password to the stdin of the program
+            BufferedWriter output = new BufferedWriter(new OutputStreamWriter(
+                    pr.getOutputStream()));
+            output.write(passwd);
+            output.flush();
+
+            // Determine the exit value
+            int exitVal = pr.waitFor();
+            if (exitVal == 0) {
+                ret = PamReturnValue.PAM_SUCCESS;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return ret;
+    }
+}
