e58193558252fae75f817d7840a5243e736b0c6c
Bo Maryniuk <bo@suse.de>
2011-07-29 16:04:58 +0000

    Added XML-RPC checks for "user: name space.

---
+++ features/step_definitions/xmlrpc_user_steps.rb
@@ -0,0 +1,93 @@
+rpctest = XMLRPCUserTest.new(ENV["TESTHOST"])
+users = nil
+roles = nil
+password = "die gurke"
+
+
+Given /^I am logged in via XML\-RPC\/user as user "([^"]*)" and password "([^"]*)"$/ do |luser, password|
+  rpctest.login(luser, password)
+end
+
+When /^I call user\.listUsers\(\)$/ do
+  users = rpctest.getUserIds()
+end
+
+Then /^I should get at least user "([^"]*)"$/ do |luser|
+  seen = false
+  for user in users
+    if luser == user['login']
+      seen = true
+      break
+    end
+  end
+
+  fail if not seen
+end
+
+When /^I call user\.getDetails\(\) on user "([^"]*)"$/ do |luser|
+  roles = rpctest.getUserRoles(luser)
+end
+
+Then /^I should see at least one role that matches "([^"]*)" suffix$/ do |sfx|
+  fail if roles.find_all{|el| el =~ /#{sfx}/}.length < 1
+end
+
+When /^I call user\.create\(sid, login, pwd, name, lastname, email\) with login "([^"]*)"$/ do |luser|
+  fail if rpctest.createUser(luser, password) != 1
+end
+
+Then /^when I call user\.listUsers\(\), I should see a user "([^"]*)"$/ do |luser|
+  users = rpctest.getUserIds()
+  seen = false
+  for user in users
+    if luser == user['login']
+      seen = true
+      break
+    end
+  end
+
+  fail if not seen
+end
+
+When /^I call user\.addRole\(\) on "([^"]*)" with the role "([^"]*)"$/ do |luser, role|
+  fail if rpctest.addRole(luser, role) != 1
+end
+
+Then /^I should see "([^"]*)" when I call user\.listRoles\(\) with "([^"]*)"$/ do |rolename, luser|
+  passed = false
+  roles = rpctest.getUserRoles(luser)
+  for role in roles
+    if role == rolename
+      passed = true
+    end
+  end
+  fail if not passed
+end
+
+
+Then /^I logout from XML\-RPC\/user namespace\.$/ do
+  fail if not rpctest.logout()
+end
+
+When /^I delete user "([^"]*)"$/ do |luser|
+  rpctest.deleteUser(luser)
+end
+
+Given /^I make sure "([^"]*)" is not present$/ do |luser|
+  users = rpctest.getUserIds()
+  for user in users
+    if luser == user['login']
+      rpctest.deleteUser(luser)
+      break
+    end
+  end
+end
+
+When /^I call user\.removeRole\(\) against uid "([^"]*)" with the role "([^"]*)"$/ do |luser, rolename|
+  fail if rpctest.delRole(luser, rolename) != 1
+end
+
+Then /^I shall not see "([^"]*)" when I call user\.listRoles\(\) with "([^"]*)" uid$/ do |rolename, luser|
+  roles = rpctest.getUserRoles(luser)
+  fail if roles != nil ? roles.length != 0 : false
+end
---
+++ features/support/xmlrpc_user.rb
@@ -0,0 +1,40 @@
+File.expand_path(__FILE__)           # For Ruby 1.9.2+
+$LOAD_PATH << File.dirname(__FILE__) # For Ruby 1.8
+
+require 'xmlrpctest'
+
+
+class XMLRPCUserTest < XMLRPCBaseTest
+  def getUserIds()
+    users = @connection.call("user.listUsers", @sid)
+    ids = []
+    for user in users
+      ids.push(user['login'])
+    end
+  end
+
+
+  def getUserRoles(uid)
+    return @connection.call("user.listRoles", @sid, uid)
+  end
+
+
+  def createUser(uid, password)
+    return @connection.call("user.create", @sid, uid, password, "Thiel", "Testerschmidt", "thiel@test.suse.de")
+  end
+
+
+  def deleteUser(uid)
+    return @connection.call("user.delete", @sid, uid)
+  end
+
+
+  def addRole(user, role)
+    return @connection.call("user.addRole", @sid, user, role)
+  end
+
+
+  def delRole(user, role)
+    return @connection.call("user.removeRole", @sid, user, role)
+  end
+end
