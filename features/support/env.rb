1723cc5841d23723ca1d5270ea064279ee5d2007
Duncan Mac-Vicar P <dmacvicar@suse.de>
2011-08-31 14:20:55 +0200

    Capybara::Driver::Selenium renamed to Capybara::Selenium::Driver
    Conflicts:
    
    	features/support/env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -73,19 +73,11 @@ Capybara.default_wait_time = 30
 
 # Register different browsers
 Capybara.register_driver :selenium_chrome do |app|
-  # Selenium Driver was renamed in newer version
-  #Capybara::Selenium::Driver.new(app, :browser => :chrome)
-
-  # old driver name
-  Capybara::Driver::Selenium.new(app, :browser => :chrome)
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
 Capybara.register_driver :selenium_firefox do |app|
-  # Selenium Driver was renamed in newer version
-  #Capybara::Selenium::Driver.new(app, :browser => :firefox)
-
-  # old driver name
-  Capybara::Driver::Selenium.new(app, :browser => :firefox)
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
 end
 
 case browser
