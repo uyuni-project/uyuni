f6176cc98490e8df8f2107e7abfa1ede67de9937
Johannes Renner <jrenner@suse.de>
2011-09-01 17:46:06 +0200

    Updated audit logging for requests to the Web UI
    - using a yaml configuration now containing what will be logged
    - configure log4j programmatically instead of log4j.properties
    - rename LogUtil.java to AuditLogUtil.java
    - updated build-props.xml, we use snakeyaml

--- java/code/src/com/redhat/rhn/frontend/servlets/AuditLogFilter.java
+++ java/code/src/com/redhat/rhn/frontend/servlets/AuditLogFilter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2011 SUSE Linux Products GmbH
+ * Copyright (c) 2011 Novell
  *
  * This software is licensed to you under the GNU General Public License,
  * version 2 (GPLv2). There is NO WARRANTY for this software, express or
@@ -15,7 +15,12 @@
 
 package com.redhat.rhn.frontend.servlets;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -26,9 +31,11 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts.Globals;
+import org.yaml.snakeyaml.Yaml;
 
 import com.redhat.rhn.common.conf.Config;
 import com.redhat.rhn.common.conf.ConfigDefaults;
+import com.redhat.rhn.common.localization.LocalizationService;
 import com.redhat.rhn.common.logging.AuditLog;
 
 /**
@@ -38,31 +45,171 @@ import com.redhat.rhn.common.logging.AuditLog;
  */
 public class AuditLogFilter implements Filter {
 
+    // Path to the config file
+    private final String configFile = "/usr/share/spacewalk/audit/auditlog-config.yaml";
+
+    // Config keys
+    private final String KEY_TYPE = "type";
+    private final String KEY_REQUIRED = "required";
+    private final String KEY_DISPATCH = "dispatch";
+    private final String KEY_LOG_BEFORE = "log_before";
+    private final String KEY_LOG_FAILURES = "log_failures";
+
+    // Local boolean to check if logging is enabled
+    private Boolean enabled = null;
+
+    // Configuration objects
+    private HashMap auditConfig;
+
     /** {@inheritDoc} */
     public void init(FilterConfig filterConfig) {
-        // nop
+        // Put enabled into a local boolean
+        if (enabled == null) {
+            enabled = Config.get().getBoolean(ConfigDefaults.AUDIT_ENABLED);
+        }
+
+        // Load configuration from YAML file
+        if (auditConfig == null) {
+            Object obj = null;
+            try {
+                Yaml yaml = new Yaml();
+                obj = yaml.load(new FileReader(configFile));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            if (obj instanceof HashMap) {
+                auditConfig = (HashMap) obj;
+            }
+        }
     }
 
     /** {@inheritDoc} */
     public void destroy() {
-        // nop
+        auditConfig = null;
     }
 
     /** {@inheritDoc} */
     public void doFilter(ServletRequest req, ServletResponse resp,
             FilterChain chain) throws IOException, ServletException {
+        // Indicate if this request should be logged
+        boolean log = false;
+        // The current request and URI configuration
+        HttpServletRequest request = null;
+        Map uriConfig = null;
+
+        if (enabled) {
+            request = (HttpServletRequest) req;
+            if (auditConfig.containsKey(request.getServletPath())) {
+                // Get the configuration for this URI
+                uriConfig = (HashMap) auditConfig.get(request.getServletPath());
+                if (uriConfig != null) {
+                    if (!hasRequiredParams(uriConfig, request)
+                            || !dispatch(uriConfig, request)) {
+                        // Do not log this request
+                    } else if (logBefore(uriConfig)) {
+                        // Log this request now
+                        AuditLog.getInstance().log(false,
+                                (String) uriConfig.get(KEY_TYPE), request);
+                    } else {
+                        // Log this request later
+                        log = true;
+                    }
+                }
+            }
+        }
 
         // Finished for now
         chain.doFilter(req, resp);
 
-        // Check if audit logging is enabled
-        if (Config.get().getBoolean(ConfigDefaults.AUDIT)) {
-            HttpServletRequest request = (HttpServletRequest) req;
-            // Log POST requests that do not have errors
-            if (request.getMethod().equals("POST")
-                    && request.getAttribute(Globals.ERROR_KEY) == null) {
-                AuditLog.getInstance().log(null, request, null);
+        // Do the actual logging
+        if (log) {
+            // Check for errors
+            if (request.getAttribute(Globals.ERROR_KEY) != null) {
+                if (logFailures(uriConfig)) {
+                    // Log a failure
+                    AuditLog.getInstance().log(true,
+                            (String) uriConfig.get(KEY_TYPE), request);
+                }
+            } else {
+                AuditLog.getInstance().log(false,
+                        (String) uriConfig.get(KEY_TYPE), request);
             }
         }
+    }
+
+    /**
+     * Check if this request should be logged before it goes to the server.
+     *
+     * @param uriConfig
+     * @return true or false
+     */
+    private boolean logBefore(Map uriConfig) {
+        boolean ret = false;
+        if (uriConfig.containsKey(KEY_LOG_BEFORE)) {
+            Object bool = uriConfig.get(KEY_LOG_BEFORE);
+            if (bool instanceof Boolean) {
+                ret = (Boolean) bool;
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * Check if failures should be logged for this URL.
+     *
+     * @param uriConfig
+     * @return true or false
+     */
+    private boolean logFailures(Map uriConfig) {
+        boolean ret = false;
+        if (uriConfig.containsKey(KEY_LOG_FAILURES)) {
+            Object bool = uriConfig.get(KEY_LOG_FAILURES);
+            if (bool instanceof Boolean) {
+                ret = (Boolean) bool;
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * Check if all of the required parameters are contained in the request.
+     *
+     * @param uriConfig
+     * @param request
+     * @return true if required parameters are there, else false.
+     */
+    private boolean hasRequiredParams(Map uriConfig, HttpServletRequest request) {
+        boolean ret = true;
+        if (uriConfig.containsKey(KEY_REQUIRED)) {
+            List requiredParams = (List) uriConfig.get(KEY_REQUIRED);
+            for (Object key : requiredParams) {
+                if (!request.getParameterMap().containsKey(key)) {
+                    // Required parameter is not there
+                    ret = false;
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * Check for a specific (internationalized!) value of 'dispatch'.
+     *
+     * @param uriConfig
+     * @param request
+     * @return true if dispatch has the expected value, else false
+     */
+    private boolean dispatch(Map uriConfig, HttpServletRequest request) {
+        boolean ret = true;
+        if (uriConfig.containsKey(KEY_DISPATCH)) {
+            String value = LocalizationService.getInstance().getMessage(
+                    (String) uriConfig.get(KEY_DISPATCH));
+            String dispatch = request.getParameter("dispatch");
+            if (dispatch == null || !value.equals(dispatch)) {
+                ret = false;
+            }
+        }
+        return ret;
     }
 }
