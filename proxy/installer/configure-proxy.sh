cc8d8deda40b6e15c89973bf94425f2630badb68
Michael Calmer <mc@suse.de>
2011-09-12 11:29:49 +0200

    fix example answer file and configure-proxy.sh

--- proxy/installer/configure-proxy.sh
+++ proxy/installer/configure-proxy.sh
@@ -105,7 +105,7 @@ while [ $# -ge 1 ]; do
 			--ssl-password=*) SSL_PASSWORD=$(echo $1 | cut -d= -f2-);;
 			--install-monitoring=*) INSTALL_MONITORING=$(echo $1 | cut -d= -f2-);;
 			--enable-scout=*) ENABLE_SCOUT=$(echo $1 | cut -d= -f2-);;
-			--monitoring-parent=*) MONITORING_PARENT_IP=$(echo $1 | cut -d= -f2-);;
+			--monitoring-parent=*) MONITORING_PARENT=$(echo $1 | cut -d= -f2-);;
 			--monitoring-parent-ip=*) MONITORING_PARENT_IP=$(echo $1 | cut -d= -f2-);;
 			--populate-config-channel=*) POPULATE_CONFIG_CHANNEL=$(echo $1 | cut -d= -f2-);;
 			--start-services=*) START_SERVICES=$(echo $1 | cut -d= -f2-);;
