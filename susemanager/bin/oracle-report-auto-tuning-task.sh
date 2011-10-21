47d0261485f9da22f61ad154e9fb6b6ab0619c85
Michael Calmer <mc@suse.de>
2011-10-21 15:52:11 +0200

    add some diagnostics DB tools

---
+++ susemanager/bin/oracle-report-auto-tuning-task.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+user=`grep db_user /etc/rhn/rhn.conf | awk '{print $3}'`
+pass=`grep db_password /etc/rhn/rhn.conf | awk '{print $3}'`
+sid=`grep db_name /etc/rhn/rhn.conf | awk '{print $3}'`
+
+file=`mktemp`
+
+echo "
+--
+-- return a report for the last auto tuning task
+--
+set long 80000
+col recs format a90
+select dbms_sqltune.report_auto_tuning_task() as recs from dual;
+quit" > $file
+sqlplus -S $user/$pass@$sid @$file
+rm -f $file
