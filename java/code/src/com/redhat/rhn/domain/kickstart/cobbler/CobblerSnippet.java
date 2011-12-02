5be02bb215bcf059055ee8bf66592b5c354c46e9
Uwe Gansert <ug@suse.de>
2011-12-02 11:17:26 +0100

    new fix display of XML snippets in the web ui (bnc#731304)

--- java/code/src/com/redhat/rhn/domain/kickstart/cobbler/CobblerSnippet.java
+++ java/code/src/com/redhat/rhn/domain/kickstart/cobbler/CobblerSnippet.java
@@ -196,22 +196,10 @@ public class CobblerSnippet implements Comparable {
      * @return String to get
     */
     public String getContents() {
-    	String result = null;
-
         if (path.exists()) {
-        	result = FileUtils.readStringFromFile(path.getAbsolutePath());
-        	if (!isEditable()) {
-        		/* 
-        		 * HTML escape Snippets
-        		 * only needed for the read-only display in the web ui
-        		 */
-        		result = result.replace("&", "&amp;");
-        		result = result.replace("<", "&lt;");
-        		result = result.replace(">", "&gt;");
-
-        	}
+        	return FileUtils.readStringFromFile(path.getAbsolutePath());
         }
-        return result;
+        return null;
     }
 
     /**
