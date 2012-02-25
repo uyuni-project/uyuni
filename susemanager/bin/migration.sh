24b888eff8a82d102d6d26617debbd8fe07a4802
Michael Calmer <mc@suse.de>
2012-02-25 18:16:24 +0100

    try out pgtune if available

--- susemanager/bin/migration.sh
+++ susemanager/bin/migration.sh
@@ -211,13 +211,12 @@ compute_pg_mem() {
     ecsmin=128
     wmmin=1
     TM=`cat /proc/meminfo | grep '^MemTotal' | awk '{print $2}'`
-    TMMB=`echo $TM / 1024 | bc`
-    TMMB=`echo $TMMB-1024 | bc`
-    SBUF=`echo 0.25 \* $TM | bc | sed "s/\..*//"`
-    ECS=`echo $TM \* 0.5 | bc`
-    sbuf=`echo $SBUF / 1024  | bc | sed "s/\..*//"`
-    ecs=`echo $ECS / 1024  | bc | sed "s/\..*//"`
-    wm=`echo $TMMB / 1000 | bc`
+    TMMB=`echo $TM / 1024 | bc | sed "s/\..*//"`
+    sbuf=`echo 0.25 \* $TMMB | bc | sed "s/\..*//"` # 25% of Total
+
+    FREEMB=`echo $TMMB-1024 | bc | sed "s/\..*//"`  # leave 1G for System
+    ecs=`echo $FREEMB \* 0.5 | bc | sed "s/\..*//"` # 50% of free
+    wm=`echo $FREEMB / 1000 | bc | sed "s/\..*//"`  # /(10*100) connections
     check=`echo $sbuf \< $sbufmin | bc`
     if test $check != 0
     then
@@ -245,7 +244,6 @@ compute_pg_mem() {
 setup_db_postgres() {
     insserv postgresql
     rcpostgresql start
-    compute_pg_mem
     su - postgres -c "createdb $MANAGER_DB_NAME ; createlang plpgsql $MANAGER_DB_NAME ; echo \"CREATE ROLE $MANAGER_USER PASSWORD '$MANAGER_PASS' SUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;\" | psql"
 
     echo "local $MANAGER_DB_NAME $MANAGER_USER md5
@@ -255,15 +253,20 @@ host $MANAGER_DB_NAME $MANAGER_USER ::1/128 md5
     cat /var/lib/pgsql/data/pg_hba.conf >> /tmp/pg_hba.conf
     mv /var/lib/pgsql/data/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf.bak
     mv /tmp/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf
-    echo -n "shared_buffers = $sbuf" >> /var/lib/pgsql/data/postgresql.conf
-    echo "MB" >> /var/lib/pgsql/data/postgresql.conf
-    echo -n "effective_cache_size = $ecs" >> /var/lib/pgsql/data/postgresql.conf
-    echo "MB" >> /var/lib/pgsql/data/postgresql.conf
-    echo "checkpoint_segments = 16" >> /var/lib/pgsql/data/postgresql.conf
-    echo "checkpoint_completion_target = 0.9" >> /var/lib/pgsql/data/postgresql.conf
-    echo -n "work_mem = $wm" >> /var/lib/pgsql/data/postgresql.conf
-    echo "MB" >> /var/lib/pgsql/data/postgresql.conf
-
+    if [ ! -e "/root/pgtune-0.9.3/pgtune" ]; then
+      compute_pg_mem
+      echo -n "shared_buffers = $sbuf" >> /var/lib/pgsql/data/postgresql.conf
+      echo "MB" >> /var/lib/pgsql/data/postgresql.conf
+      echo -n "effective_cache_size = $ecs" >> /var/lib/pgsql/data/postgresql.conf
+      echo "MB" >> /var/lib/pgsql/data/postgresql.conf
+      echo "checkpoint_segments = 16" >> /var/lib/pgsql/data/postgresql.conf
+      echo "checkpoint_completion_target = 0.9" >> /var/lib/pgsql/data/postgresql.conf
+      echo -n "work_mem = $wm" >> /var/lib/pgsql/data/postgresql.conf
+      echo "MB" >> /var/lib/pgsql/data/postgresql.conf
+    else
+      mv /var/lib/pgsql/data/postgresql.conf /var/lib/pgsql/data/postgresql.conf.orig
+      /root/pgtune-0.9.3/pgtune -T Mixed -i /var/lib/pgsql/data/postgresql.conf.orig -o /var/lib/pgsql/data/postgresql.conf
+    fi
     rcpostgresql restart
 }
 
