3ee92172b83dab563983d399d70173ab7914b7bc
Johannes Renner <jrenner@suse.de>
2012-03-15 15:17:49 +0100

    Add support for studio image deployments (database migrations)

---
+++ schema/spacewalk/common/tables/rhnActionImageDeploy.sql
@@ -0,0 +1,33 @@
+--
+-- Copyright (c) 2011 Novell
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+--
+
+CREATE TABLE rhnActionImageDeploy
+(
+    id            NUMBER NOT NULL PRIMARY KEY,
+    action_id     NUMBER NOT NULL
+                   CONSTRAINT rhn_act_idp_act_fk
+                       REFERENCES rhnAction (id)
+                       ON DELETE CASCADE,
+    vcpus         NUMBER DEFAULT(1) NOT NULL,
+    mem_kb        NUMBER DEFAULT(524288) NOT NULL,
+    bridge_device VARCHAR2(32)  DEFAULT('br0') NOT NULL,
+    image_type    VARCHAR2(32)  NOT NULL,
+    download_url  VARCHAR2(256) NOT NULL,
+    proxy_server  VARCHAR2(64),
+    proxy_user    VARCHAR2(32),
+    proxy_pass    VARCHAR2(64)
+)
+ENABLE ROW MOVEMENT
+;
+
+CREATE SEQUENCE rhn_action_image_deploy_id_seq;
+
---
+++ schema/spacewalk/common/tables/suseCredentials.sql
@@ -0,0 +1,32 @@
+--
+-- Copyright (c) 2012 Novell
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+--
+
+CREATE TABLE suseCredentials
+(
+    id       NUMBER NOT NULL
+                 CONSTRAINT suse_credentials_pk PRIMARY KEY,
+    user_id  NUMBER NOT NULL UNIQUE
+                 CONSTRAINT suse_credentials_oid_fk
+                 REFERENCES web_contact (id)
+                 ON DELETE CASCADE,
+    type     VARCHAR2(32) NOT NULL,
+    url      VARCHAR2(256),
+    username VARCHAR2(64) NOT NULL,
+    password VARCHAR2(64) NOT NULL,
+    created  DATE DEFAULT (sysdate) NOT NULL,
+    modified DATE DEFAULT (sysdate) NOT NULL
+)
+ENABLE ROW MOVEMENT
+;
+
+CREATE SEQUENCE suse_credentials_id_seq START WITH 100;
+
