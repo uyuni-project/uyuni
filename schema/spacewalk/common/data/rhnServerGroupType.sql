c128a2aa6a92bc3935b0ec6153a6b02a789a1198
Uwe Gansert <ug@suse.de>
2012-04-03 13:05:09 +0200

    bootstrap image patches

--- schema/spacewalk/common/data/rhnServerGroupType.sql
+++ schema/spacewalk/common/data/rhnServerGroupType.sql
@@ -73,6 +73,14 @@ insert into rhnServerGroupType ( id, label, name, permanent, is_base)
    );      
 
 
+--  bootstrap_entitled type ----------------------------------------------------
+
+insert into rhnServerGroupType ( id, label, name, permanent, is_base)
+   values ( sequence_nextval('rhn_servergroup_type_seq'),
+      'bootstrap_entitled', 'Bootstrap Entitled Servers',
+      'N', 'Y'
+   );
+
 commit;
 
 --
