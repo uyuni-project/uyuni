40c842fa152fdac4c2dfab0a4b38a314eecc5313
Michael Calmer <mc@suse.de>
2012-04-13 12:41:24 +0200

    helper to get real values in case they change

--- features/step_definitions/xmlrpctest_steps.rb
+++ features/step_definitions/xmlrpctest_steps.rb
@@ -8,6 +8,7 @@ Given /^I am logged in via XML\-RPC\/api as user "([^"]*)" and password "([^"]*)
 end
 
 When /^I call getApiNamespaces, I should get (\d+) namespaces$/ do |apiCount|
+  #$stderr.puts "namespaces #{rpcApiTester.getCountOfApiNamespaces()}"
   fail if apiCount.to_i != rpcApiTester.getCountOfApiNamespaces()
 end
 
@@ -20,9 +21,11 @@ When /^I call systemVersion, I should get "([^"]*)" as result$/ do |version|
 end
 
 When /^I call getApiNamespaceCallList, I should get (\d+) available api calls$/ do |apiCount|
+  #$stderr.puts "API count #{rpcApiTester.getCountOfApiNamespaceCallList()}"
   fail if apiCount.to_i != rpcApiTester.getCountOfApiNamespaceCallList()
 end
 
 When /^I call getApiCallList, I should get (\d+) available groups$/ do |groupCount|
+  #$stderr.puts "available groups: #{rpcApiTester.getCountOfApiCallListGroups()}"
   fail if groupCount.to_i != rpcApiTester.getCountOfApiCallListGroups()
 end
