53883937fe56bdf672e2bf5334169b5b6d5f4dc6
Johannes Renner <jrenner@suse.de>
2012-05-10 10:40:09 +0200

    Escape image name to allow quotes and prevent XSS (bnc#761165)

--- web/html/javascript/images.js
+++ web/html/javascript/images.js
@@ -1,17 +1,22 @@
 // These functions make use of prototype.js:
-// Show form fields and hide table
 function showForm(id, name, version, arch, type, editUrl) {
-    $('edit-link').update(name);
+    // Setup link for editing image
+    var nameEsc = name.escapeHTML();
+    $('edit-link').update(nameEsc);
     $('edit-link').setAttribute('href', editUrl);
-    $('image-string').update("\"" + name + "\", Version " + version + " (" + arch + ", " + type + ")");
+    // Create string representation
+    var imgString = "\"" + nameEsc + "\", Version " + version + " (" + arch + ", " + type + ")";
+    $('image-string').update(imgString);
     $('image-id').setValue(id);
+    // Show form fields and hide table
     $('deployment-form').show();
     $('images-table').hide();
 }
-// Show images table and hide form fields
+// Show table and hide form fields
 function showImages() {
     $('images-table').show();
     $('deployment-form').hide();
+    // Reset elements
     $('image-id').setValue('');
     $('image-string').update();
     $('edit-link').setAttribute('href', '');
