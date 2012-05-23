d1f2367cd60c6246305f555bacecf1914a2cda12
Michael Calmer <mc@suse.de>
2012-05-23 13:25:08 +0200

    added missing "end"

--- features/step_definitions/weak_deps_steps.rb
+++ features/step_definitions/weak_deps_steps.rb
@@ -26,4 +26,4 @@ Then /^"([^"]*)" should exists in the metadata$/ do |file|
         arch = "i586"
     end
     fail if not File.exists?("#{client_raw_repodata_dir("sles11-sp1-updates-#{arch}-channel")}/#{file}")
-
+end
