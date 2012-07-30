5ac2c2142e8ade4b1fcad427e399ba03ac10f5e4
Michael Calmer <mc@suse.de>
2012-07-30 18:17:20 +0200

    check if cleanup-data-bunch exists and create it if not (bnc#772620)

--- schema/spacewalk/upgrade/susemanager-schema-1.2.75-to-susemanager-schema-1.7/102-rhnTaskoTemplate_data.sql
+++ schema/spacewalk/upgrade/susemanager-schema-1.2.75-to-susemanager-schema-1.7/102-rhnTaskoTemplate_data.sql
@@ -1,6 +1,23 @@
-INSERT INTO rhnTaskoTemplate (id, bunch_id, task_id, ordering, start_if)
+declare
+  data_bunch_id number := 0;
+begin
+  begin
+      SELECT id INTO data_bunch_id FROM rhnTaskoBunch WHERE name='cleanup-data-bunch';
+  exception
+    WHEN no_data_found THEN
+
+      INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+      VALUES (sequence_nextval('rhn_tasko_bunch_id_seq'), 'cleanup-data-bunch', 'Cleans up orphaned and outdated data', null);
+
+      commit;
+  end;
+  INSERT INTO rhnTaskoTemplate (id, bunch_id, task_id, ordering, start_if)
     VALUES (sequence_nextval('rhn_tasko_template_id_seq'),
         (SELECT id FROM rhnTaskoBunch WHERE name='cleanup-data-bunch'),
         (SELECT id FROM rhnTaskoTask WHERE name='cleanup-packagechangelog-data'),
         1,
         null);
+
+end;
+/
+
