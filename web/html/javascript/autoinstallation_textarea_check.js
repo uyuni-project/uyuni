d90d5645a62009ad51d68fb58059f99970bfcfec
Eugene Trounev <eugene.trounev@gmail.com>
2012-08-22 19:53:05 -0400

    This should finally cix the problem and work as expected. As usual I underestimated the ingenuity of the original developers :) Had to rework the whole concept from ground up to have it working in the end. In the process found there was no provision for disabled widget states in general css, so that was added as well.

--- web/html/javascript/autoinstallation_textarea_check.js
+++ web/html/javascript/autoinstallation_textarea_check.js
@@ -1,13 +1,28 @@
-setTheTextareaChecking();
-function setTheTextareaChecking(){
-	var formSubmitButton = document.getElementById("autoinstallationDetailsUpdateButton");
-	if(formSubmitButton){ //we do not want this to happen if the element does not exist
-		formSubmitButton.disabled = false; //First disable the submit button
-		var textArea = document.getElementById("contents"); //Get the text area
-		var timer = function(){
-            if(textArea.value!=""&&formSubmitButton.disabled) formSubmitButton.disabled = false;
-            	else formSubmitButton.disabled = true;
-        }
-        setTimeout(timer, 1200); //Set up the timed checks
-	}
+var checkTheTextareaTimer;
+
+function theTextareaIsNotEmpty(t){return t.replace(/[^a-zA-Z.]/g,"")!="";}
+
+function checkTheTextarea(){
+  var textAreaText = false;
+  //Check textarea
+  var textArea = document.getElementById("contents"); //Get the text area
+  if(textArea) textAreaText = theTextareaIsNotEmpty(textArea.value);
+  //END Check textarea
+  //Check iFrame
+  var iFrame = document.getElementById("frame_contents");
+  if(iFrame&&iFrame.style.display=="inline"){
+    var innerDoc = iFrame.contentDocuments || iFrame.contentWindow.document;
+    if(innerDoc){
+      var frameTextArea = innerDoc.getElementById("selection_field");
+      textAreaText = theTextareaIsNotEmpty(frameTextArea.textContent||frameTextArea.innerText);
+    }
+  }
+  //END Check iFrame
+  //Toggle Submit button status
+  var formSubmitButton = document.getElementById("autoinstallationDetailsUpdateButton");
+  if(formSubmitButton){
+    if(textAreaText && formSubmitButton.disabled) formSubmitButton.disabled = false;
+    if(!textAreaText && !formSubmitButton.disabled) formSubmitButton.disabled = true;
+  }
+  checkTheTextareaTimer = setTimeout(function(){checkTheTextarea()}, 1200);
 }
