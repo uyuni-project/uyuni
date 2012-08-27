7d5d1d86ab92d4a1cfc57c50aade87e4d5eba299
Michael Calmer <mc@suse.de>
2012-08-27 15:38:42 +0200

    Read system proxy values during setup and use them as default.

--- spacewalk/setup/lib/Spacewalk/Setup.pm
+++ spacewalk/setup/lib/Spacewalk/Setup.pm
@@ -48,6 +48,12 @@ use constant DEFAULT_ANSWER_FILE_GLOB =>
 use constant DEFAULT_RHN_CONF_LOCATION =>
   '/etc/rhn/rhn.conf';
 
+use constant DEFAULT_PROXY_CONF_LOCATION =>
+  '/etc/sysconfig/proxy';
+
+use constant DEFAULT_PROXYAUTH_CONF_LOCATION =>
+  '/root/.curlrc';
+
 use constant DEFAULT_RHN_ETC_DIR =>
   '/etc/sysconfig/rhn';
 
