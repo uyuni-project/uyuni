108d4074a58ec6ec9d1fc03d972f9d7d19f4731a
Michael Calmer <mc@suse.de>
2013-02-01 13:36:56 +0100

    systemd should also use rhnmd subdir in var/run

--- monitoring/rhnmd/rhnmd_create_key.sh
+++ monitoring/rhnmd/rhnmd_create_key.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
+mkdir /var/run/rhnmd
+chown nocpulse.nocpulse /var/run/rhnmd
+
 IDENTITY="/var/lib/nocpulse/.ssh/nocpulse-identity"
 if [ ! -f "$IDENTITY" ]
 then
         /bin/su -s /bin/bash -c "/usr/bin/ssh-keygen -q -t dsa -N '' -f $IDENTITY" - nocpulse
         if [ ! -e "/var/lib/nocpulse/.bashrc" ]; then
                 echo 'LANG="C"' > /var/lib/nocpulse/.bashrc
+                chown nocpulse.nocpulse /var/lib/nocpulse/.bashrc
         fi
 fi
