1be652628015427a956b3743ba18669209a89c05
Michael Calmer <mc@suse.de>
2013-02-19 18:35:31 +0100

    fix import path for unit test

--- susemanager/src/test/test_sync.py
+++ susemanager/src/test/test_sync.py
@@ -20,7 +20,7 @@ from StringIO import StringIO
 
 from mock import Mock, patch
 
-from susemanager import mgr_ncc_sync_lib as ncc_sync
+from spacewalk.susemanager import mgr_ncc_sync_lib as ncc_sync
 
 class SyncTest(unittest.TestCase):
     def setUp(self):
