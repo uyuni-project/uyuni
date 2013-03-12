a8ad18a99c2fa21ebbcb402c72b37a00beff3e7b
Michael Calmer <mc@suse.de>
2013-03-12 14:01:21 +0100

    fix is_connected_to in case of unix domain socket use

--- backend/server/rhnSQL/driver_postgresql.py
+++ backend/server/rhnSQL/driver_postgresql.py
@@ -183,6 +183,9 @@ class Database(sql_base.Database):
         adjusted_port = -1
         if port:
             adjusted_port = port
+        if host is None or host == '' or host == 'localhost':
+            host = None
+            adjusted_port = None
         return (backend == POSTGRESQL) and (self.host == host) and \
                (self.port == adjusted_port) and (self.username == username) \
                and (self.password == password) and (self.database == database)
