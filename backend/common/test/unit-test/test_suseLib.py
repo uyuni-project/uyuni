099abed04126bf242cbf62923de74d36929b7b8f
Flavio Castelli <fcastelli@suse.com>
2013-07-24 13:11:46 +0200

    backend: fix unit test when using python 2.4
    Backport a unit test to _not_ use the `with` statement.

--- backend/common/test/unit-test/test_suseLib.py
+++ backend/common/test/unit-test/test_suseLib.py
@@ -10,8 +10,6 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 #
 
-from io import BytesIO
-from contextlib import contextmanager
 import unittest2 as unittest
 
 import mock
@@ -36,100 +34,86 @@ class SuseLibTest(unittest.TestCase):
     def tearDownClass(self):
         self.initCFG.stop()
 
-    def test_get_proxy_from_yast(self):
-        with mock.patch('spacewalk.common.suseLib.CFG', http_proxy=None,
-                        http_proxy_username=None, http_proxy_password=None):
-            with mock_open(suseLib.YAST_PROXY,
-                           ' --proxy "%s"\n --proxy-user "%s:%s"'
-                           % (HTTPS_PROXY, PROXY_USER, PROXY_PASS)):
-                self.assertEqual((HTTPS_PROXY, PROXY_USER, PROXY_PASS),
-                                 suseLib.get_proxy())
+    @mock.patch('spacewalk.common.suseLib.CFG')
+    @mock.patch('__builtin__.open')
+    def test_get_proxy_from_yast(self, mocked_open, mocked_CFG):
+        mocked_CFG.http_proxy          = None
+        mocked_CFG.http_proxy_username = None
+        mocked_CFG.http_proxy_password = None
 
-    def test_get_proxy_from_yast_no_creds(self):
-        with mock.patch('spacewalk.common.suseLib.CFG', http_proxy=None,
-                        http_proxy_username=None, http_proxy_password=None):
-            with mock_open(suseLib.YAST_PROXY,
-                           ' --proxy "%s"' % HTTPS_PROXY):
-                self.assertEqual((HTTPS_PROXY, None, None), suseLib.get_proxy())
+        mocked_file                    = mock.MagicMock()
+        mocked_file.read.return_value  = ' --proxy "%s"\n --proxy-user "%s:%s"' % (HTTPS_PROXY, PROXY_USER, PROXY_PASS)
+        mocked_file.close.return_value = True
 
-    def test_get_proxy_none(self):
-        with mock.patch('spacewalk.common.suseLib.CFG', http_proxy=None,
-                        http_proxy_username=None, http_proxy_password=None):
-            with mock.patch('spacewalk.common.suseLib.log_debug') as log_debug:
-                with mock_open(suseLib.YAST_PROXY):
-                    self.assertEqual((None, None, None), suseLib.get_proxy())
-                    self.assertIn('Could not read proxy URL from rhn config',
-                                  log_debug.call_args_list[0][0][1])
-                    self.assertEqual(
-                        'Could not read proxy URL from /root/.curlrc',
-                        log_debug.call_args_list[1][0][1])
-                    self.assertEqual(2, log_debug.call_count)
+        mocked_open.return_value = mocked_file
 
-    def test_get_proxy_rhn_conf_no_creds(self):
-        with mock.patch('spacewalk.common.suseLib.CFG', http_proxy=PROXY_ADDR,
-                        http_proxy_username=None, http_proxy_password=None):
-            self.assertEqual((HTTP_PROXY, None, None), suseLib.get_proxy())
+        self.assertEqual(
+            (HTTPS_PROXY, PROXY_USER, PROXY_PASS),
+            suseLib.get_proxy()
+        )
 
-    def test_get_proxy_rhn_conf_creds(self):
-        with mock.patch('spacewalk.common.suseLib.CFG', http_proxy=PROXY_ADDR,
-                        http_proxy_username=PROXY_USER,
-                        http_proxy_password=PROXY_PASS):
-            self.assertEqual((HTTP_PROXY, PROXY_USER, PROXY_PASS),
-                             suseLib.get_proxy())
+    @mock.patch('spacewalk.common.suseLib.CFG')
+    @mock.patch('__builtin__.open')
+    def test_get_proxy_from_yast_no_creds(self, mocked_open, mocked_CFG):
+        mocked_CFG.http_proxy          = None
+        mocked_CFG.http_proxy_username = None
+        mocked_CFG.http_proxy_password = None
 
-    def test_get_proxy_rhn_only_username(self):
-        with mock.patch('spacewalk.common.suseLib.CFG', http_proxy=PROXY_ADDR,
-                        http_proxy_username='user', http_proxy_password=None):
-            self.assertEqual((HTTP_PROXY, 'user', None), suseLib.get_proxy())
+        mocked_file                    = mock.MagicMock()
+        mocked_file.read.return_value  = ' --proxy "%s"' % HTTPS_PROXY
+        mocked_file.close.return_value = True
 
+        mocked_open.return_value = mocked_file
 
-@contextmanager
-def mock_open(filename, contents=None, complain=True):
-    """Mock __builtin__.open() on a specific filename
+        self.assertEqual((HTTPS_PROXY, None, None), suseLib.get_proxy())
 
-    Let execution pass through to __builtin__.open() on other
-    files. Return a BytesIO with :contents: if the file was matched. If
-    the :contents: parameter is not given or if it None, a BytesIO
-    instance simulating an empty file is returned.
+    @mock.patch('spacewalk.common.suseLib.CFG')
+    @mock.patch('spacewalk.common.suseLib.log_debug')
+    @mock.patch('__builtin__.open')
+    def test_get_proxy_none(self, mocked_open, mocked_log_debug, mocked_CFG):
+        mocked_CFG.http_proxy          = None
+        mocked_CFG.http_proxy_username = None
+        mocked_CFG.http_proxy_password = None
 
-    If :complain: is True (default). Will raise an error if a
-    __builtin__.open was called with a file that was not mocked.
+        mocked_file                    = mock.MagicMock()
+        mocked_file.read.return_value  = ""
+        mocked_file.close.return_value = True
 
-    """
-    open_files = []  # simulate non-local scope with mutable lists
-    def mock_file(*args):
-        if args[0] == filename:
-            r = BytesIO(contents)
-            r.name = filename
-        else:
-            mocked_file.stop()
-            r = open(*args)
-            mocked_file.start()
+        mocked_open.return_value = mocked_file
 
-        open_files.append(r)
-        return r
+        self.assertEqual((None, None, None), suseLib.get_proxy())
+        self.assertIn('Could not read proxy URL from rhn config',
+                      mocked_log_debug.call_args_list[0][0][1])
+        self.assertEqual(
+            'Could not read proxy URL from /root/.curlrc',
+            mocked_log_debug.call_args_list[1][0][1]
+        )
+        self.assertEqual(2, mocked_log_debug.call_count)
 
-    mocked_file = mock.patch('__builtin__.open', mock_file)
-    mocked_file.start()
-    try:
-        yield
-    except NotMocked as e:
-        if e.filename != filename:
-            raise
-    mocked_file.stop()
+    @mock.patch('spacewalk.common.suseLib.CFG')
+    def test_get_proxy_rhn_conf_no_creds(self, mocked_CFG):
+        mocked_CFG.http_proxy          = PROXY_ADDR
+        mocked_CFG.http_proxy_username = None
+        mocked_CFG.http_proxy_password = None
 
-    found = False
-    for f in open_files:
-        if f.name == filename:
-            found = f
-        elif complain:
-            raise NotMocked(f.name)
+        self.assertEqual((HTTP_PROXY, None, None), suseLib.get_proxy())
 
-    if not found:
-        raise AssertionError("The file %s was not opened." % filename)
+    @mock.patch('spacewalk.common.suseLib.CFG')
+    def test_get_proxy_rhn_conf_creds(self, mocked_CFG):
+        mocked_CFG.http_proxy          = PROXY_ADDR
+        mocked_CFG.http_proxy_username = PROXY_USER
+        mocked_CFG.http_proxy_password = PROXY_PASS
 
-class NotMocked(Exception):
-    def __init__(self, filename):
-        super(NotMocked, self).__init__(
-            "The file %s was opened, but not mocked." % filename)
-        self.filename = filename
+        self.assertEqual(
+            (HTTP_PROXY, PROXY_USER, PROXY_PASS),
+            suseLib.get_proxy()
+        )
+
+    @mock.patch('spacewalk.common.suseLib.CFG')
+    def test_get_proxy_rhn_only_username(self, mocked_CFG):
+        mocked_CFG.http_proxy          = PROXY_ADDR
+        mocked_CFG.http_proxy_username = 'user'
+        mocked_CFG.http_proxy_password = None
+
+        self.assertEqual((HTTP_PROXY, 'user', None), suseLib.get_proxy())
+
