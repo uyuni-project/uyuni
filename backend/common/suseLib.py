e3a6794c2a9f9e72f160a332f12abcad2616f1ad
Flavio Castelli <fcastelli@suse.com>
2013-08-27 17:14:30 +0200

    Fix bnc#837035
    Ensure the file object is closed only when no IOError took place.
    
    This fixes bnc#837035.

--- backend/common/suseLib.py
+++ backend/common/suseLib.py
@@ -500,6 +500,7 @@ def _get_proxy_from_yast():
     pass can be None.
 
     """
+    f = None
     try:
         try:
             f = open(YAST_PROXY)
@@ -508,7 +509,8 @@ def _get_proxy_from_yast():
             log_debug(1, "Couldn't open " + YAST_PROXY)
             return None
     finally:
-        f.close()
+        if f:
+            f.close()
 
     proxy_url = _parse_curl_proxy_url(contents)
     if not proxy_url:
