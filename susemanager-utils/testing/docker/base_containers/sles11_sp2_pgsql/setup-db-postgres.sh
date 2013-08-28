7f4475dcbfd59c9bf748724c638106e746ec8478
Johannes Renner <jrenner@suse.de>
2013-08-28 11:59:00 +0200

    Add smdba and call it for SP2 and SP3 pgsql based containers

--- susemanager-utils/testing/docker/base_containers/sles11_sp2_pgsql/setup-db-postgres.sh
+++ susemanager-utils/testing/docker/base_containers/sles11_sp2_pgsql/setup-db-postgres.sh
@@ -11,7 +11,6 @@ rcpostgresql start
 su - postgres -c "createdb susemanager ; echo \"CREATE ROLE spacewalk PASSWORD 'spacewalk' SUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;\" | psql"
 # "createlang plpgsql $MANAGER_DB_NAME" not needed on SUSE. plpgsql is already enabled
 
-
 # Update psql configuration
 cp /var/lib/pgsql/data/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf.bak
 echo "local susemanager spacewalk md5
@@ -25,4 +24,5 @@ if [ -x "/usr/bin/pgtune" ]; then
   /usr/bin/pgtune -T Mixed -i /var/lib/pgsql/data/postgresql.conf.orig -o /var/lib/pgsql/data/postgresql.conf
 fi
 
+smdba system-check autotuning
 rcpostgresql stop
