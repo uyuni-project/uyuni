b0be606cbae432872af16d0a2234e52835b9acd5
Johannes Renner <jrenner@suse.de>
2013-08-28 16:25:01 +0200

    Refer to the Head container and set shmmax before starting the DB

--- java/scripts/docker-testing.sh
+++ java/scripts/docker-testing.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -v "${PWD}/../:/manager" manager_java_testing_pgsql /bin/sh -c "rcpostgresql start; rhn-satellite-activate --rhn-cert /manager/branding/setup/spacewalk-public.cert --disconnected; mgr-ncc-sync --from-dir=/root/UC5; cd /manager/java; ant resolve-ivy; ant -f manager-build.xml run-tests-locally"
+docker run -v "${PWD}/../:/manager" manager_java_testing_pgsql_Head /bin/sh -c "sysctl -w kernel.shmmax=18446744073709551615; rcpostgresql start; rhn-satellite-activate --rhn-cert /manager/branding/setup/spacewalk-public.cert --disconnected; mgr-ncc-sync --from-dir=/root/UC5; cd /manager/java; ant resolve-ivy; ant -f manager-build.xml run-tests-locally"
 
