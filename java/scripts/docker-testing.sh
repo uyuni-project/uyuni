f3c4bce9b138751007c058dc4397c037d233710c
Johannes Renner <jrenner@suse.de>
2013-08-28 14:46:29 +0200

    Fix docker script after container has been renamed

--- java/scripts/docker-testing.sh
+++ java/scripts/docker-testing.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -v "${PWD}/../:/manager" manager_java_testing_pgsql /bin/sh -c "rcpostgresql start; rhn-satellite-activate --rhn-cert /manager/branding/setup/spacewalk-public.cert --disconnected; mgr-ncc-sync --from-dir=/root/UC5; cd /manager/java; ant resolve-ivy; ant -f manager-build.xml run-tests-locally"
+docker run -v "${PWD}/../:/manager" manager_java_testing_pgsql_1_7 /bin/sh -c "rcpostgresql start; rhn-satellite-activate --rhn-cert /manager/branding/setup/spacewalk-public.cert --disconnected; mgr-ncc-sync --from-dir=/root/UC5; cd /manager/java; ant resolve-ivy; ant -f manager-build.xml run-tests-locally"
 
