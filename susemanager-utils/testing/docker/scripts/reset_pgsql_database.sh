0d62a5ca5df990595ffe75f8b42615fa1dbf6f35
Johannes Renner <jrenner@suse.de>
2013-08-29 17:48:20 +0200

    This script is needed to init the pgsql database

---
+++ susemanager-utils/testing/docker/scripts/reset_pgsql_database.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+export PERLLIB=/manager/spacewalk/setup/lib/:/manager/web/modules/rhn/:/manager/web/modules/pxt/
+export PATH=/manager/schema/spacewalk/:/manager/spacewalk/setup/bin/:$PATH
+
+echo Going to reset pgsql database
+
+echo $PATH
+echo $PERLLIB
+
+./build-schema.sh
+
+rcpostgresql restart
+
+spacewalk-setup --skip-system-version-test --skip-selinux-test --skip-fqdn-test --skip-gpg-key-import --skip-ssl-cert-generation --skip-ssl-vhost-setup --skip-services-check --clear-db --answer-file=clear-db-answers.txt --non-interactive
