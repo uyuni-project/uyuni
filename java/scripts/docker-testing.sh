675ea1c6831974b2e60ace9135c87700d09798be
Johannes Renner <jrenner@suse.de>
2013-08-29 11:17:25 +0200

    Make this a script that we can call with docker run <script>

--- java/scripts/docker-testing.sh
+++ java/scripts/docker-testing.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-docker run -v "${PWD}/../:/manager" manager_java_testing_pgsql_Head /bin/sh -c "sysctl -w kernel.shmmax=18446744073709551615; rcpostgresql start; rhn-satellite-activate --rhn-cert /manager/branding/setup/spacewalk-public.cert --disconnected; mgr-ncc-sync --from-dir=/root/UC5; cd /manager/java; ant resolve-ivy; ant -f manager-build.xml run-tests-locally"
+# Database preparations and startup
+sysctl -w kernel.shmmax=18446744073709551615
+smdba system-check autotuning
+rcpostgresql start
+
+# SUSE Manager initialization
+rhn-satellite-activate --rhn-cert /manager/branding/setup/spacewalk-public.cert --disconnected
+mgr-ncc-sync --from-dir=/root/UC5
+
+# Resolve libs and run tests
+cd /manager/java
+ant resolve-ivy
+ant -f manager-build.xml run-tests-locally
 
