d3649b463f20d1abc4d3c6d307a19dc02e339695
Hubert Mantel <mantel@suse.de>
2013-09-04 16:04:52 +0200

    add tests for nagios plugin

---
+++ features/step_definitions/nagios_steps.rb
@@ -0,0 +1,42 @@
+# Copyright (c) 2010-2011 Novell, Inc.
+# Licensed under the terms of the MIT license.
+
+When /^I perform a nagios check patches$/ do
+    command = "ssh #{ENV['TESTHOST']} /usr/lib/nagios/plugins/check_suma_patches #{$myhostname}"
+    output = `#{command} > /tmp/nagios.out`
+end
+
+When /^I perform a nagios check last event$/ do
+    command = "ssh #{ENV['TESTHOST']} /usr/lib/nagios/plugins/check_suma_lastevent #{$myhostname}"
+    output = `#{command} > /tmp/nagios.out`
+end
+
+When /^I perform an invalid nagios check patches$/ do
+    command = "ssh #{ENV['TESTHOST']} /usr/lib/nagios/plugins/check_suma_patches does.not.exist"
+    output = `#{command} > /tmp/nagios.out`
+end
+
+Then /^I should see WARNING: 1 patch pending$/ do
+  command = "grep \"WARNING: 1 patch(es) pending\" /tmp/nagios.out"
+  output = `#{command} 2>&1`
+  if ! $?.success?
+    raise "Nagios check patches failed '#{command}' #{$!}: #{output}"
+  end
+end
+
+Then /^I should see Completed: OpenSCAP xccdf scanning scheduled by testing$/ do
+  command = "grep \"Completed: OpenSCAP xccdf scanning scheduled by testing\" /tmp/nagios.out"
+  output = `#{command} 2>&1`
+  if ! $?.success?
+    raise "Nagios check last event failed '#{command}' #{$!}: #{output}"
+  end
+end
+
+Then /^I should see an unknown system message$/ do
+  command = "grep -i \"^Unknown system:.*does.not.exist\" /tmp/nagios.out"
+  output = `#{command} 2>&1`
+  if ! $?.success?
+    raise "Nagios check patches for nonexisting system failed '#{command}' #{$!}: #{output}"
+  end
+end
+
