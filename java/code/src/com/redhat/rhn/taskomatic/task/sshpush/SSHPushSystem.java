83a448a06d8203b4c3d3962639a42c8033b8fe7f
Johannes Renner <jrenner@suse.de>
2013-09-09 09:49:41 +0200

    Remember systems we are currently talking to via SSH push (bnc#838188)

--- java/code/src/com/redhat/rhn/taskomatic/task/sshpush/SSHPushSystem.java
+++ java/code/src/com/redhat/rhn/taskomatic/task/sshpush/SSHPushSystem.java
@@ -123,4 +123,45 @@ public class SSHPushSystem {
     public void setName(String nameIn) {
         this.name = nameIn;
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        SSHPushSystem other = (SSHPushSystem) obj;
+        if (id != other.id) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        }
+        else if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
 }
