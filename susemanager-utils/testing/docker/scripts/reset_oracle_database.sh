ea6e2c82616319766a6b7089cd352aba4e5f9beb
Johannes Renner <jrenner@suse.de>
2013-09-11 17:00:56 +0200

    Create oracle version of db reset script + make it more explicit

---
+++ susemanager-utils/testing/docker/scripts/reset_oracle_database.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+export PERLLIB=/manager/spacewalk/setup/lib/:/manager/web/modules/rhn/:/manager/web/modules/pxt/
+export PATH=/manager/schema/spacewalk/:/manager/spacewalk/setup/bin/:$PATH
+
+echo Going to reset ORACLE database
+
+echo $PATH
+echo $PERLLIB
+
+./build-schema.sh
+
+smdba db-stop
+smdba db-start
+
+spacewalk-setup --skip-system-version-test --skip-selinux-test --skip-fqdn-test --skip-gpg-key-import --skip-ssl-cert-generation --skip-ssl-vhost-setup --skip-services-check --clear-db --answer-file=clear-db-answers.txt --external-oracle --non-interactive
--- susemanager-utils/testing/docker/scripts/reset_pgsql_database.sh
+++ susemanager-utils/testing/docker/scripts/reset_pgsql_database.sh
@@ -5,13 +5,14 @@ set -e
 export PERLLIB=/manager/spacewalk/setup/lib/:/manager/web/modules/rhn/:/manager/web/modules/pxt/
 export PATH=/manager/schema/spacewalk/:/manager/spacewalk/setup/bin/:$PATH
 
-echo Going to reset pgsql database
+echo Going to reset PGSQL database
 
 echo $PATH
 echo $PERLLIB
 
 ./build-schema.sh
 
-rcpostgresql restart
+smdba db-stop
+smdba db-start
 
 spacewalk-setup --skip-system-version-test --skip-selinux-test --skip-fqdn-test --skip-gpg-key-import --skip-ssl-cert-generation --skip-ssl-vhost-setup --skip-services-check --clear-db --answer-file=clear-db-answers.txt --external-postgresql --non-interactive
