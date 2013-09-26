dd5728e7acd2030d90290c3571cb921159475a2f
Silvio Moioli <smoioli@suse.de>
2013-09-26 15:23:08 +0200

    Ant file refactoring, removed unused code

--- java/scripts/docker-testing-oracle.sh
+++ java/scripts/docker-testing-oracle.sh
@@ -15,5 +15,5 @@ rhn-satellite-activate --rhn-cert /manager/branding/setup/spacewalk-public.cert 
 # Resolve libs and run tests
 cd /manager/java
 ant resolve-ivy
-ant -f manager-build.xml run-tests-locally
+ant -f manager-build.xml run-tests
 
