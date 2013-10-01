ed9e291023d6f9d8dc80e813a097cae25b146ff2
Silvio Moioli <smoioli@suse.de>
2013-10-01 08:22:17 +0200

    manager-build.xml: rename Ant test task

--- java/scripts/docker-testing-pgsql.sh
+++ java/scripts/docker-testing-pgsql.sh
@@ -12,5 +12,5 @@ rhn-satellite-activate --rhn-cert /manager/branding/setup/spacewalk-public.cert 
 # Resolve libs and run tests
 cd /manager/java
 ant resolve-ivy
-ant -f manager-build.xml run-tests
+ant -f manager-build.xml test
 
