0cb24e0c39a912a2b3e16f4ab8ea4911d600c274
Silvio Moioli <smoioli@suse.de>
2013-11-07 18:26:45 +0100

    Page added to configure and power on/off/reboot a single system

---
+++ java/code/src/com/redhat/rhn/frontend/action/kickstart/PowerManagementAction.java
@@ -0,0 +1,201 @@
+package com.redhat.rhn.frontend.action.kickstart;
+
+import com.redhat.rhn.common.conf.ConfigDefaults;
+import com.redhat.rhn.common.validator.ValidatorError;
+import com.redhat.rhn.domain.server.Server;
+import com.redhat.rhn.domain.user.User;
+import com.redhat.rhn.frontend.struts.RequestContext;
+import com.redhat.rhn.frontend.struts.RhnAction;
+import com.redhat.rhn.frontend.struts.RhnHelper;
+import com.redhat.rhn.manager.kickstart.cobbler.CobblerPowerOffCommand;
+import com.redhat.rhn.manager.kickstart.cobbler.CobblerPowerOnCommand;
+import com.redhat.rhn.manager.kickstart.cobbler.CobblerPowerSettingsUpdateCommand;
+import com.redhat.rhn.manager.kickstart.cobbler.CobblerRebootCommand;
+import com.redhat.rhn.manager.kickstart.cobbler.CobblerXMLRPCHelper;
+import com.redhat.rhn.manager.system.SystemManager;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
+import org.cobbler.CobblerConnection;
+import org.cobbler.SystemRecord;
+import org.cobbler.XmlRpcException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Saves power settings and boots machines.
+ *
+ * @version $Rev$
+ */
+public class PowerManagementAction extends RhnAction {
+
+    /** The log. */
+    private static Logger log = Logger.getLogger(PowerManagementAction.class);
+
+    /** Attribute name. */
+    public static final String TYPES = "types";
+
+    /** Attribute name. */
+    public static final String POWER_TYPE = "powerType";
+
+    /** Attribute name. */
+    public static final String POWER_ADDRESS = "powerAddress";
+
+    /** Attribute name. */
+    public static final String POWER_USERNAME = "powerUsername";
+
+    /** Attribute name. */
+    public static final String POWER_PASSWORD = "powerPassword";
+
+    /** Attribute name. */
+    public static final String POWER_ID = "powerId";
+
+    /** Attribute name. */
+    public static final String POWER_STATUS = "powerStatus";
+
+    /** Attribute name. */
+    public static final String POWER_ADDITIONAL_ACTION = "powerAdditionalAction";
+
+    /** Possible attribute value for POWER_ADDITIONAL_ACTION. */
+    public static final String POWER_ON = "powerOn";
+
+    /** Possible attribute value for POWER_ADDITIONAL_ACTION. */
+    public static final String POWER_OFF = "powerOff";
+
+    /** Possible attribute value for POWER_ADDITIONAL_ACTION. */
+    public static final String REBOOT = "reboot";
+
+    /**
+     * Runs this action.
+     *
+     * @param mapping action mapping
+     * @param formIn form submitted values
+     * @param request http request object
+     * @param response http response object
+     * @return an action forward object
+     */
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm formIn,
+        HttpServletRequest request, HttpServletResponse response) {
+        RequestContext context = new RequestContext(request);
+        DynaActionForm form = (DynaActionForm) formIn;
+        User user = context.getCurrentUser();
+        Long sid = context.getRequiredParam(RequestContext.SID);
+        Server server = SystemManager.lookupByIdAndUser(sid, user);
+
+        if (context.isSubmitted()) {
+            CobblerPowerSettingsUpdateCommand command =
+                new CobblerPowerSettingsUpdateCommand(
+                user, server, form.getString(POWER_TYPE), form.getString(POWER_ADDRESS),
+                form.getString(POWER_USERNAME), form.getString(POWER_PASSWORD),
+                form.getString(POWER_ID));
+
+            ValidatorError error = command.store();
+            if (error == null) {
+                addMessage(request, "kickstart.powermanagement.saved");
+                log.debug("Power management settings saved for system " + sid);
+                if (POWER_ON.equals(form.getString(POWER_ADDITIONAL_ACTION))) {
+                    error = new CobblerPowerOnCommand(user, server).store();
+                    if (error == null) {
+                        log.debug("Power on succeded for system " + sid);
+                        addMessage(request, "kickstart.powermanagement.powered_on");
+                    }
+                }
+                if (POWER_OFF.equals(form.getString(POWER_ADDITIONAL_ACTION))) {
+                    error = new CobblerPowerOffCommand(user, server).store();
+                    if (error == null) {
+                        log.debug("Power off succeded for system " + sid);
+                        addMessage(request, "kickstart.powermanagement.powered_off");
+                    }
+                }
+                if (REBOOT.equals(form.getString(POWER_ADDITIONAL_ACTION))) {
+                    error = new CobblerRebootCommand(user, server).store();
+                    if (error == null) {
+                        log.debug("Reboot succeded for system " + sid);
+                        addMessage(request, "kickstart.powermanagement.rebooted");
+                    }
+                }
+            }
+
+            if (error != null) {
+                ActionErrors errors = new ActionErrors();
+                getStrutsDelegate().addError(errors, error.getKey(), error.getValues());
+                getStrutsDelegate().saveMessages(request, errors);
+            }
+        }
+
+        setAttributes(request, context, server, user);
+
+        return getStrutsDelegate().forwardParams(
+            mapping.findForward(RhnHelper.DEFAULT_FORWARD), request.getParameterMap());
+    }
+
+    /**
+     * Sets the page attributes.
+     *
+     * @param request the request
+     * @param context the context
+     * @param server the server
+     * @param user the user
+     */
+    private void setAttributes(HttpServletRequest request, RequestContext context,
+        Server server, User user) {
+        request.setAttribute(RequestContext.SID, server.getId());
+        request.setAttribute(RequestContext.SYSTEM, server);
+
+        List<String> types = new ArrayList<String>();
+        String typeString = ConfigDefaults.get().getCobblerPowerTypes();
+        if (typeString != null) {
+            types.addAll(Arrays.asList(typeString.split(" *, *")));
+        }
+        request.setAttribute(TYPES, types);
+
+        if (types.size() == 0) {
+            ActionErrors errors = new ActionErrors();
+            getStrutsDelegate().addError(errors, "kickstart.powermanagement.notypes",
+                ConfigDefaults.POWER_MANAGEMENT_TYPES);
+            getStrutsDelegate().saveMessages(request, errors);
+        }
+        else {
+            CobblerConnection connection = CobblerXMLRPCHelper.getConnection(user);
+            SystemRecord record = SystemRecord
+                .lookupById(connection, server.getCobblerId());
+
+            if (record == null) {
+                request.setAttribute(POWER_TYPE, types.get(0));
+            }
+            else {
+                request.setAttribute(POWER_TYPE, record.getPowerType());
+                request.setAttribute(POWER_ADDRESS, record.getPowerAddress());
+                request.setAttribute(POWER_USERNAME, record.getPowerUsername());
+                request.setAttribute(POWER_PASSWORD, record.getPowerPassword());
+                request.setAttribute(POWER_ID, record.getPowerId());
+
+                Boolean powerStatus = null;
+
+                try {
+                    if (StringUtils.isNotBlank(record.getPowerUsername()) &&
+                        StringUtils.isNotBlank(record.getPowerAddress())) {
+                        powerStatus = record.getPowerStatus();
+                    }
+                }
+                catch (XmlRpcException e) {
+                    log.warn("Could not get power status from Cobbler for system " +
+                        server.getId());
+                }
+
+                request.setAttribute(POWER_STATUS, powerStatus);
+            }
+        }
+    }
+}
---
+++ java/code/webapp/WEB-INF/pages/kickstart/powermanagement.jsp
@@ -0,0 +1,140 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+<%@ taglib uri="http://rhn.redhat.com/rhn" prefix="rhn" %>
+<%@ taglib uri="http://struts.apache.org/tags-bean" prefix="bean" %>
+<%@ taglib uri="http://struts.apache.org/tags-html" prefix="html" %>
+<html:xhtml />
+<html>
+<head>
+  <meta http-equiv="Pragma" content="no-cache" />
+</head>
+
+<body>
+  <%@ include file="/WEB-INF/pages/common/fragments/systems/system-header.jspf" %>
+  <br />
+
+  <h2>
+    <img src="/img/icon_kickstart_session-medium.gif"
+      alt="<bean:message key='system.common.kickstartAlt' />"
+    >
+    <bean:message key="kickstart.powermanagement.jsp.heading" />
+  </h2>
+
+  <c:choose>
+    <c:when test="${powerStatus eq true}">
+      <bean:message key="kickstart.powermanagement.jsp.currentPowerStatusOn" />
+    </c:when>
+    <c:when test="${powerStatus eq false}">
+      <bean:message key="kickstart.powermanagement.jsp.currentPowerStatusOff" />
+    </c:when>
+    <c:otherwise>
+      <bean:message key="kickstart.powermanagement.jsp.currentPowerStatusUnknown" />
+    </c:otherwise>
+  </c:choose>
+
+  <c:if test="${fn:length(types) >= 1}">
+    <html:form action="/systems/details/kickstart/PowerManagement.do?sid=${sid}">
+
+      <rhn:csrf />
+      <rhn:submitted />
+      <div class="search-choices">
+        <div class="search-choices-group">
+          <table class="details">
+
+          <tr>
+            <th>
+              <label for="powerType">
+                <bean:message key="kickstart.powermanagement.jsp.powertype" />
+                <rhn:required-field />
+              </label>
+            </th>
+            <td>
+              <c:choose>
+                <c:when test="${fn:length(types) == 1}">
+                  <bean:message key="kickstart.powermanagement.${types[0]}" />
+                  <input type="hidden" name="powerType" value="${types[0]}">
+                </c:when>
+                <c:otherwise>
+                  <select name="powerType">
+                    <c:forEach items="${types}" var="type">
+                      <option value="${type}" <c:if test="${type eq powerType}">selected</c:if>>
+                        <bean:message key="kickstart.powermanagement.${type}" />
+                      </option>
+                    </c:forEach>
+                  </select>
+                </c:otherwise>
+              </c:choose>
+            </td>
+          </tr>
+
+            <tr>
+              <th>
+                <label for="powerAddress">
+                  <bean:message key="kickstart.powermanagement.jsp.poweraddress" />
+                  <rhn:required-field />
+                </label>
+              </th>
+              <td>
+                <html:text property="powerAddress" name="powerAddress"
+                  value="${powerAddress}"
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label for="powerUsername">
+                  <bean:message key="kickstart.powermanagement.jsp.powerusername" />
+                  <rhn:required-field />
+                </label>
+              </th>
+              <td>
+                <html:text property="powerUsername" name="powerUsername"
+                  value="${powerUsername}"
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label for="powerPassword">
+                  <bean:message key="kickstart.powermanagement.jsp.powerpassword" />
+                  <rhn:required-field />
+                </label>
+              </th>
+              <td>
+                <html:password property="powerPassword" name="powerPassword"
+                  value="${powerPassword}"
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label for="powerId">
+                  <bean:message key="kickstart.powermanagement.jsp.powerid" />
+                </label>
+              </th>
+              <td>
+                <html:text property="powerId" name="powerId" value="${powerId}" />
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+
+      <hr>
+
+      <button type="submit" name="powerAdditionalAction" value="none">
+        <bean:message key="kickstart.powermanagement.jsp.save" />
+      </button>
+      <button type="submit" name="powerAdditionalAction" value="powerOn">
+        <bean:message key="kickstart.powermanagement.jsp.savepoweron" />
+      </button>
+      <button type="submit" name="powerAdditionalAction" value="powerOff">
+        <bean:message key="kickstart.powermanagement.jsp.savepoweroff" />
+      </button>
+      <button type="submit" name="powerAdditionalAction" value="reboot">
+        <bean:message key="kickstart.powermanagement.jsp.savereboot" />
+      </button>
+    </html:form>
+  </c:if>
+</body>
+</html>
