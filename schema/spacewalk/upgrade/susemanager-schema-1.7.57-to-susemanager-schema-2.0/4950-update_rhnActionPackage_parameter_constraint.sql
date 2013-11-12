0336239c0b168ed35ef52ec25a067353d512e319
Flavio Castelli <fcastelli@suse.com>
2013-11-12 16:01:03 +0100

    DB: updated rhnActionPackage parameter constraint
    Updated the constraint checking rhnActionPackage.parameter values to
    support also the 'lock' value.

---
+++ schema/spacewalk/upgrade/susemanager-schema-1.7.57-to-susemanager-schema-2.0/4950-update_rhnActionPackage_parameter_constraint.sql
@@ -0,0 +1,19 @@
+--
+-- Copyright (c) 2013 Novell
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+--
+
+ALTER TABLE rhnActionPackage DROP CONSTRAINT rhn_act_p_param_ck;
+
+-- add the contraints
+ALTER TABLE rhnActionPackage
+    ADD CONSTRAINT rhn_act_p_param_ck
+    CHECK (parameter IN ('upgrade', 'install', 'remove', 'downgrade', 'lock'));
+
