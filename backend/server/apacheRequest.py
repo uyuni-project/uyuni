717742fd0580e31d4bfda2eb5e1a1072eb5d31c1
Michael Calmer <mc@suse.de>
2013-11-14 18:04:43 +0100

    log errors on bad request (bnc#849856)

--- backend/server/apacheRequest.py
+++ backend/server/apacheRequest.py
@@ -481,6 +481,7 @@ class apachePOST(apacheRequest):
         try:
             fd = self.input.decode(self.req)
         except IOError: # client timed out
+            log_error("IOError: client timed out?")
             return apache.HTTP_BAD_REQUEST
 
         # Read the data from the request
@@ -491,6 +492,7 @@ class apachePOST(apacheRequest):
         # In this case, we talk to a client (maybe through a proxy)
         # make sure we have something to decode
         if _body is None or len(_body) == 0:
+            log_error("Empty body in post request")
             return apache.HTTP_BAD_REQUEST
 
         # Decode the request; avoid logging crappy responses
