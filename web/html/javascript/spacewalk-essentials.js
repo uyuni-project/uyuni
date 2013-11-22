2eb08568c84e601ec7b441d3d33e3ea4435ed8d1
Cynthia Sanchez <csanchez@suse.com>
2013-11-22 13:38:09 +0100

    comments changed

--- web/html/javascript/spacewalk-essentials.js
+++ web/html/javascript/spacewalk-essentials.js
@@ -28,7 +28,7 @@ $(document).on("ready", function(){
   // This is a function from spacewalk-checkall.js
   create_checkall_checkbox();
 
-  // Adding div wrapping the tables in a div which will make them responsive    
+  // Wrapping the tables in a div which will make them responsive    
   $(".table").wrap("<div class='table-responsive'>");
 
 });
@@ -56,7 +56,7 @@ function columnHeight() {
     var newHeight = heightDoc - 145 - sectionHeight + "px";
     $(".spacewalk-main-column-layout section").css("padding-bottom", newHeight);
   };
-  //function to hide or show the Collapsable menues. 768 is the size described by Bootstrap for @screen-desktop
+  //function to hide or show the Collapsable menues.
   if ($(document).width()>1051) {
     $(".collapse").addClass('in').css({
       "height": 'auto'
