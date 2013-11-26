2eaf06275e4b0186184dc16fc8031abccfa0f7c1
Michael Calmer <mc@suse.de>
2013-11-26 16:19:32 +0100

    find table

--- features/step_definitions/add_sys_of_group_to_SSM_steps.rb
+++ features/step_definitions/add_sys_of_group_to_SSM_steps.rb
@@ -2,8 +2,10 @@
 # Licensed under the terms of the MIT license.
 
 When /^I click on "([^"]+)" for "([^"]+)"$/ do |arg1, arg2|
-  within(:xpath, "//form/table/tbody/tr[.//a[contains(.,'#{arg2}')]]") do
-    find_link(arg1).click
+  within(:xpath, "//section") do
+    within(:xpath, "//table/tbody/tr[.//a[contains(.,'#{arg2}')]]") do
+      find_link(arg1).click
+    end
   end
 end
 
