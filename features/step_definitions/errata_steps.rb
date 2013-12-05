24ccc4cc4dc156bd161a395f7bf1800275d526e4
Michael Calmer <mc@suse.de>
2013-12-05 15:09:56 +0100

    remove unused step definition

--- features/step_definitions/errata_steps.rb
+++ features/step_definitions/errata_steps.rb
@@ -14,21 +14,6 @@ Given /^I am on the "([^"]*)" errata Details page$/ do |arg1|
     step "I follow \"#{arg1}\""
 end
 
-Then /^I should see three links to the errata in the list$/ do
-  arch=`uname -m`
-  arch.chomp!
-  linknames = [ "slessp2-nfs-client-6222",
-		"slessp2-aaa_base-6544"]
-  if arch != "x86_64"
-      linknames << "slessp2-kernel-6641"
-  else
-      linknames << "slessp2-kernel-6648"
-  end
-  linknames.each() do |link|
-    fail if not has_xpath?("//form/table/tbody/tr/td/a[contains(.,'#{link}')]")
-  end
-end
-
 Then /^I should see an update in the list$/ do
   fail if not has_xpath?("//form/div/div/div/table/tbody/tr/td/a")
 end
