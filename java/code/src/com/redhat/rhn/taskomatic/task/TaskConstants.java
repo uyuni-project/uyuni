7adfbc7b21de6057c5055a8b5225097844e47cb8
Michael Calmer <mc@suse.de>
2014-01-08 14:06:26 +0100

    Implement task to invalidate reboot actions
    If a client do not checkin 6 hours after the reboot was picked up
    set the action to failed and all dependent actions too.

--- java/code/src/com/redhat/rhn/taskomatic/task/TaskConstants.java
+++ java/code/src/com/redhat/rhn/taskomatic/task/TaskConstants.java
@@ -180,5 +180,14 @@ public class TaskConstants {
     public static final String TASK_QUERY_SKIP_RUNNING_AND_READY_JOBS_BY_LABEL =
             "skip_running_and_ready_jobs_by_label";
 
+    public static final String TASK_QUERY_LOOKUP_REBOOT_ACTION_CLEANUP =
+        "lookup_reboot_action_cleanup";
+
+    public static final String TASK_QUERY_LOOKUP_CHILD_ACTION =
+        "lookup_child_action";
+
+    public static final String TASK_QUERY_LOOKUP_KICKSTART_SESSION_ID =
+        "lookup_kickstart_session_id";
+
     private TaskConstants() { }
 }
