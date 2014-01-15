0e6ede34243de49acf7c38aa9dcb57569cf5c6f0
Silvio Moioli <smoioli@suse.de>
2014-01-15 10:00:47 +0100

    Java unit tests: update docker script to new Oracle host

--- java/scripts/docker-testing-oracle.sh
+++ java/scripts/docker-testing-oracle.sh
@@ -8,7 +8,7 @@ cd /manager/susemanager-utils/testing/docker/scripts/
 cd /manager/java
 ant resolve-ivy
 
-ssh root@1c059.qa.suse.de rhn-satellite-activate --rhn-cert /usr/share/spacewalk/setup/spacewalk-public.cert --disconnected
+ssh root@sumaoracletest.suse.de rhn-satellite-activate --rhn-cert /usr/share/spacewalk/setup/spacewalk-public.cert --disconnected
 
 cp buildconf/test/rhn.conf.oracle-example buildconf/test/rhn.conf
 ant -f manager-build.xml test
