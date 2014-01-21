27d7e67e0874fdaad62c7da19ba6e37f94e6b62a
Michael Calmer <mc@suse.de>
2014-01-21 10:58:16 +0100

    Test new CVE ID syntax

--- features/step_definitions/weak_deps_steps.rb
+++ features/step_definitions/weak_deps_steps.rb
@@ -37,3 +37,14 @@ Then /^"([^"]*)" should exists in the metadata$/ do |file|
     end
     fail if not File.exists?("#{client_raw_repodata_dir("sles11-sp3-updates-#{arch}-channel")}/#{file}")
 end
+
+Then /^I should have '([^']*)' in the patch metadata$/ do |text|
+   arch=`uname -m`
+   arch.chomp!
+   if arch != "x86_64"
+     arch = "i586"
+   end
+   `zgrep '#{text}' #{client_raw_repodata_dir("sles11-sp3-updates-#{arch}-channel")}/updateinfo.xml.gz`
+   fail if ! $?.success?
+end
+
