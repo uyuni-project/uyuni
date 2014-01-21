5178693e415b89d1a668371578164c1f93b3310c
Johannes Renner <jrenner@suse.de>
2014-01-21 13:53:06 +0100

    Created SetupWizardManager class as well as *Test

---
+++ java/code/src/com/redhat/rhn/manager/setup/SetupWizardManager.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2014 SUSE
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.redhat.rhn.manager.setup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.redhat.rhn.common.conf.Config;
+import com.redhat.rhn.domain.credentials.Credentials;
+import com.redhat.rhn.manager.BaseManager;
+
+public class SetupWizardManager extends BaseManager {
+
+    // Logger for this class
+    private static Logger logger = Logger.getLogger(SetupWizardManager.class);
+
+    // Config keys
+    public final static String KEY_MIRROR_CREDENTIALS_USER = "server.susemanager.mirrcred_user";
+    public final static String KEY_MIRROR_CREDENTIALS_PASS = "server.susemanager.mirrcred_pass";
+    public final static String KEY_MIRROR_CREDENTIALS_EMAIL = "server.susemanager.mirrcred_email";
+
+    /**
+     * Find all valid mirror credentials and return them.
+     * @return List of all available mirror credentials
+     */
+    public static List<Credentials> getMirrorCredentials() {
+        List<Credentials> credsList = new ArrayList<Credentials>();
+
+        // Get the main pair of credentials
+        String user = Config.get().getString(KEY_MIRROR_CREDENTIALS_USER);
+        String password = Config.get().getString(KEY_MIRROR_CREDENTIALS_PASS);
+        String email = Config.get().getString(KEY_MIRROR_CREDENTIALS_EMAIL);
+
+        // Add credentials as long as they have user and password
+        Credentials creds;
+        int index = 1;
+        while (user != null && password != null) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Mirror Credentials: " + user + ":" + password + ", " + email);
+            }
+
+            // Create credentials object
+            creds = new Credentials();
+            creds.setUsername(user);
+            creds.setPassword(password);
+            creds.setEmail(email);
+            credsList.add(creds);
+
+            // Search additional credentials with continuous enumeration
+            user = Config.get().getString(KEY_MIRROR_CREDENTIALS_USER + "." + index);
+            password = Config.get().getString(KEY_MIRROR_CREDENTIALS_PASS + "." + index);
+            email = Config.get().getString(KEY_MIRROR_CREDENTIALS_EMAIL + "." + index);
+            index++;
+        }
+
+        return credsList;
+    }
+}
---
+++ java/code/src/com/redhat/rhn/manager/setup/test/SetupWizardManagerTest.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2014 SUSE
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.redhat.rhn.manager.setup.test;
+
+import java.util.List;
+
+import com.redhat.rhn.common.conf.Config;
+import com.redhat.rhn.domain.credentials.Credentials;
+import com.redhat.rhn.manager.setup.SetupWizardManager;
+import com.redhat.rhn.testing.RhnBaseTestCase;
+
+/**
+ * Tests for {@link SetupWizardManager}.
+ */
+public class SetupWizardManagerTest extends RhnBaseTestCase {
+
+    public void testGetMirrorCredsEmpty() throws Exception {
+        List<Credentials> credentials = SetupWizardManager.getMirrorCredentials();
+        assertEquals(0, credentials.size());
+    }
+
+    public void testGetMirrorCreds() throws Exception {
+        writeTestCredentials("testuser", "testpass", "testemail", 0);
+        writeTestCredentials("testuser1", "testpass1", "testemail1", 1);
+        writeTestCredentials("testuser2", "testpass2", "testemail2", 2);
+        List<Credentials> credentials = SetupWizardManager.getMirrorCredentials();
+        assertEquals(3, credentials.size());
+    }
+
+    public void testGetMirrorCredsMissing() throws Exception {
+        writeTestCredentials("testuser", "testpass", "testemail", 0);
+        writeTestCredentials("testuser2", "testpass2", "testemail2", 2);
+        List<Credentials> credentials = SetupWizardManager.getMirrorCredentials();
+        assertEquals(1, credentials.size());
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        // Clear credentials from config
+        super.tearDown();
+        for (int i=0; i<=10; i++) {
+            writeTestCredentials("", "", "", i);
+        }
+    }
+
+    private void writeTestCredentials(String user, String pass, String email, int index) {
+        String keyUser = SetupWizardManager.KEY_MIRROR_CREDENTIALS_USER;
+        String keyPass = SetupWizardManager.KEY_MIRROR_CREDENTIALS_PASS;
+        String keyEmail = SetupWizardManager.KEY_MIRROR_CREDENTIALS_EMAIL;
+        if (index >= 1) {
+            keyUser += "." + index;
+            keyPass += "." + index;
+            keyEmail += "." + index;
+        }
+        Config.get().setString(keyUser, user);
+        Config.get().setString(keyPass, pass);
+        Config.get().setString(keyEmail, email);
+    }
+}
