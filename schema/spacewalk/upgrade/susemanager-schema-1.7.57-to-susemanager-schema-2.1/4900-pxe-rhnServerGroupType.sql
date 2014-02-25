60056e1aeece1b888a61859093f95d1deade1292
Michael Calmer <mc@suse.de>
2014-02-25 13:44:08 +0100

    write insert of bootstrap entitlement in a way, that it can be called multiple times

--- schema/spacewalk/upgrade/susemanager-schema-1.7.57-to-susemanager-schema-2.1/4900-pxe-rhnServerGroupType.sql
+++ schema/spacewalk/upgrade/susemanager-schema-1.7.57-to-susemanager-schema-2.1/4900-pxe-rhnServerGroupType.sql
@@ -1,8 +1,15 @@
 --  bootstrap_entitled type ----------------------------------------------------
 
-insert into rhnServerGroupType ( id, label, name, permanent, is_base)
-   values ( sequence_nextval('rhn_servergroup_type_seq'),
-      'bootstrap_entitled', 'Bootstrap Entitled Servers',
-      'N', 'Y'
-   );
+insert into rhnServerGroupType ( id, label, name, permanent, is_base) (
+    select sequence_nextval('rhn_servergroup_type_seq'),
+                            'bootstrap_entitled',
+                            'Bootstrap Entitled Servers',
+                            'N', 'Y'
+      from dual
+     where not exists (
+           select 1
+             from rhnServerGroupType
+            where label = 'bootstrap_entitled'
+     )
+);
 
