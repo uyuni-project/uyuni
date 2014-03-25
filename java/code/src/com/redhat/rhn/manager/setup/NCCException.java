1b48703dbb560139b3b4bba307a5d9256bae139c
Silvio Moioli <smoioli@suse.de>
2014-03-25 16:04:53 +0100

    NCCException: code style fixed

--- java/code/src/com/redhat/rhn/manager/setup/NCCException.java
+++ java/code/src/com/redhat/rhn/manager/setup/NCCException.java
@@ -1,19 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (c) 2014 SUSE
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
  */
-
 package com.redhat.rhn.manager.setup;
 
 /**
- *
+ * Exception from NCCClient.
  * @author duncan
  */
 public class NCCException extends Exception {
-
+    /**
+     * Standard constructor with custom cause
+     * @param cause the cause
+     */
     public NCCException(Throwable cause) {
         super(cause);
     }
-    
 }
