b4991e095842b0cdb9af61e3a030ab2644925b54
Johannes Renner <jrenner@suse.de>
2014-03-28 14:07:20 +0100

    Add tests for SetupWizardSessionCache

--- java/code/src/com/redhat/rhn/manager/setup/SubscriptionDto.java
+++ java/code/src/com/redhat/rhn/manager/setup/SubscriptionDto.java
@@ -62,4 +62,59 @@ public class SubscriptionDto {
     public void setEndDate(Date endDate) {
         this.endDate = endDate;
     }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((endDate == null) ? 0 : endDate.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        SubscriptionDto other = (SubscriptionDto) obj;
+        if (endDate == null) {
+            if (other.endDate != null) {
+                return false;
+            }
+        }
+        else if (!endDate.equals(other.endDate)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        }
+        else if (!name.equals(other.name)) {
+            return false;
+        }
+        if (startDate == null) {
+            if (other.startDate != null) {
+                return false;
+            }
+        }
+        else if (!startDate.equals(other.startDate)) {
+            return false;
+        }
+        return true;
+    }
 }
---
+++ java/code/src/com/redhat/rhn/manager/setup/test/SetupWizardSessionCacheTest.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2014 SUSE
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.redhat.rhn.manager.setup.test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.redhat.rhn.manager.setup.MirrorCredentialsDto;
+import com.redhat.rhn.manager.setup.SetupWizardSessionCache;
+import com.redhat.rhn.manager.setup.SubscriptionDto;
+import com.redhat.rhn.testing.RhnMockStrutsTestCase;
+
+/**
+ * Tests for {@link SetupWizardSessionCache}.
+ */
+public class SetupWizardSessionCacheTest extends RhnMockStrutsTestCase {
+
+    // Internally used credentials and subscriptions
+    private MirrorCredentialsDto creds = getTestCredentials();
+    private MirrorCredentialsDto creds2 = getTestCredentials2();
+    private List<SubscriptionDto> subs = getTestSubscriptions();
+
+    public void testProxyStatus() {
+        SetupWizardSessionCache.storeProxyStatus(true, request);
+        assertTrue(SetupWizardSessionCache.getProxyStatus(false, request));
+        SetupWizardSessionCache.storeProxyStatus(false, request);
+        assertFalse(SetupWizardSessionCache.getProxyStatus(false, request));
+    }
+
+    public void testGetSubscriptionsNull() {
+        assertNull(SetupWizardSessionCache.getSubscriptions(creds, request));
+    }
+
+    public void testCredentialsStatusTrue() {
+        assertTrue(SetupWizardSessionCache.credentialsStatusUnknown(creds, request));
+    }
+
+    public void testCredentialsStatusFalse() {
+        SetupWizardSessionCache.storeSubscriptions(subs, creds, request);
+        assertFalse(SetupWizardSessionCache.credentialsStatusUnknown(creds, request));
+    }
+
+    public void testGetSubscriptions() {
+        SubscriptionDto subscription = subs.get(0);
+        SetupWizardSessionCache.storeSubscriptions(subs, creds, request);
+        List<SubscriptionDto> cachedSubs = SetupWizardSessionCache.getSubscriptions(creds, request);
+        assertEquals(1, cachedSubs.size());
+        assertEquals(cachedSubs.get(0), subscription);
+    }
+
+    public void testClearSubscriptions() {
+        // Store subscriptions for different credentials
+        SetupWizardSessionCache.storeSubscriptions(subs, creds, request);
+        SetupWizardSessionCache.storeSubscriptions(subs, creds2, request);
+
+        // Retrieve subscriptions
+        List<SubscriptionDto> subs = SetupWizardSessionCache.getSubscriptions(creds, request);
+        assertNotNull(subs);
+        subs = SetupWizardSessionCache.getSubscriptions(creds2, request);
+        assertNotNull(subs);
+
+        // Clear only for creds
+        SetupWizardSessionCache.clearSubscriptions(creds, request);
+        assertNull(SetupWizardSessionCache.getSubscriptions(creds, request));
+        assertNotNull(SetupWizardSessionCache.getSubscriptions(creds2, request));
+    }
+
+    public void testClearAllSubscriptions() {
+        // Store subscriptions for different credentials
+        SetupWizardSessionCache.storeSubscriptions(subs, creds, request);
+        SetupWizardSessionCache.storeSubscriptions(subs, creds2, request);
+
+        // Retrieve subscriptions
+        List<SubscriptionDto> subs = SetupWizardSessionCache.getSubscriptions(creds, request);
+        assertNotNull(subs);
+        subs = SetupWizardSessionCache.getSubscriptions(creds2, request);
+        assertNotNull(subs);
+
+        // Clear all
+        SetupWizardSessionCache.clearAllSubscriptions(request);
+        assertNull(SetupWizardSessionCache.getSubscriptions(creds, request));
+        assertNull(SetupWizardSessionCache.getSubscriptions(creds2, request));
+    }
+
+    private MirrorCredentialsDto getTestCredentials() {
+        return new MirrorCredentialsDto("foo", "foo", "foo");
+    }
+
+    private MirrorCredentialsDto getTestCredentials2() {
+        return new MirrorCredentialsDto("bar", "bar", "bar");
+    }
+
+    private List<SubscriptionDto> getTestSubscriptions() {
+        List<SubscriptionDto> ret = new ArrayList<SubscriptionDto>();
+        ret.add(getTestSubscriptionDto());
+        return ret;
+    }
+
+    private SubscriptionDto getTestSubscriptionDto() {
+        SubscriptionDto ret = new SubscriptionDto();
+        ret.setName("foobar-subscription");
+        ret.setStartDate(new Date());
+        ret.setEndDate(new Date());
+        return ret;
+    }
+}
