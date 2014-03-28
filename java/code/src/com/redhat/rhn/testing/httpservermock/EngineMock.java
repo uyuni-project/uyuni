d73a2bf933ba4e452ca4dd322f0f83c1bfb517d3
Silvio Moioli <smoioli@suse.de>
2014-03-28 15:43:00 +0100

    Setup Wizard: file headers fixed

--- java/code/src/com/redhat/rhn/testing/httpservermock/EngineMock.java
+++ java/code/src/com/redhat/rhn/testing/httpservermock/EngineMock.java
@@ -1,9 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (c) 2014 SUSE
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
  */
-
 package com.redhat.rhn.testing.httpservermock;
 
 import simple.http.serve.Resource;
