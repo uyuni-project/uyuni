ff8af0ace888ca78587db19d30a2e124c07bf846
Johannes Renner <jrenner@suse.de>
2014-04-02 10:21:52 +0200

    Refactor javascript and remove obsolete initChannelSelect()

--- web/html/javascript/susemanager-sp-migration.js
+++ web/html/javascript/susemanager-sp-migration.js
@@ -1,40 +1,39 @@
-// Event handler methods
-function initChannelSelect() {
-  $('#base-channel-select').val(0);
-}
-// Show child channels of selected base channel
-function showChannelTree(channelID) {
-  $('.channels-tree#channels-tree-' + channelID).show();
-  $('.channels-tree[id != channels-tree-' + channelID + ']').hide();
-}
-// Create hidden inputs to submit channel IDs
-function prepareSubmitChannels() {
-  $('#migrationForm input[type="hidden", name="childChannels[]"]').remove();
-  // Submit all checked child channel's IDs
-  $('.channels-tree:visible input:checked').each(function() {
-    $('#migrationForm').append(
-      createHiddenInput('childChannels[]', $(this).val())
-    );
-  });
-  return true;
-}
-// Create a hidden input
-function createHiddenInput(name, value) {
-  var el = document.createElement("input");
-  el.type = "hidden";
-  el.name = name;
-  el.value = value;
-  return el;
-}
-
 $(function() {
-  initChannelSelect();
+  // Show child channels of selected base channel
+  function showChannelTree(channelID) {
+    $('.channels-tree#channels-tree-' + channelID).show();
+    $('.channels-tree[id != channels-tree-' + channelID + ']').hide();
+  }
 
+  // Create hidden inputs to submit channel IDs
+  function prepareSubmitChannels() {
+    $('#migrationForm input[type="hidden", name="childChannels[]"]').remove();
+    // Submit all checked child channel's IDs
+    $('.channels-tree:visible input:checked').each(function() {
+      $('#migrationForm').append(
+        createHiddenInput('childChannels[]', $(this).val())
+      );
+    });
+    return true;
+  }
+
+  // Create a hidden input
+  function createHiddenInput(name, value) {
+    var el = document.createElement("input");
+    el.type = "hidden";
+    el.name = name;
+    el.value = value;
+    return el;
+  }
+
+  // Prepare submitting the form
   $('#migrationForm').submit(function() {
     prepareSubmitChannels();
   });
 
+  // Switch displayed channel tree
   $('#base-channel-select').change(function() {
     showChannelTree($(this).val());
   });
 });
+
