3165fff78f17efdf12c5058e281a6df2a2652f8c
Martin Seidl <mseidl@suse.de>
2014-04-08 11:26:34 +0200

    added tests for products page in setupwizard

--- features/setup_wizard.feature
+++ features/setup_wizard.feature
@@ -31,3 +31,17 @@ Feature: I want to verify the Setup Wizard
         And I click on "Close"
         And I should not see a "asdf" text
         And I should not see a "test@suse.com" text
+
+   Scenario: I want to test the products page
+     Given I am on the Admin page
+       And I follow "Admin" in the tab bar
+       And I follow "SUSE Products" in the content area
+       And I should see a "Available Products Below" text
+       And I should see a "Architecture" text
+       And I should see a "1-click" text
+       And I select SLES SP3 VMWare as a product
+       And I verify the product was added
+       And I select the product Webyast 1.3
+       And I sync the repos
+      Then the product should appear in the output of mgr-ncc-sync
+
--- features/step_definitions/setup_wizard.rb
+++ features/step_definitions/setup_wizard.rb
@@ -26,4 +26,30 @@ When /^I click on "([^"]*)" link in the setup wizard$/ do |arg1|
   end
 end
 
+When /^I select SLES SP3 VMWare as a product$/ do
+   within(:xpath, "//tr[td[position()=2 and contains(text(), 'SUSE Linux Enterprise Server 11 SP3 VMWare')] and td[position()=3 and contains(text(), 'x86_64')]][1]") do
+      fail if not find('i.start-sync-icon').click
+   end
+end
+
+When /^I verify the product was added$/ do 
+   $sshout = `echo | ssh -l root -o StrictHostKeyChecking=no $TESTHOST mgr-ncc-sync -l` 
+   fail if not $sshout.include? '[P] sles11-sp3-vmware-pool-x86_64'
+end  
+
+When /^I select the product Webyast 1.3$/ do
+   within(:xpath, "//tr[td[position()=2 and contains(text(), 'SUSE Linux Enterprise Server 11 SP3 VMWare')] and td[position()=3 and contains(text(), 'x86_64')]][1]/following-sibling::tr[1]") do
+      fail if not find("input#select-single").click
+   end
+end
+
+When /^I sync the repos$/ do
+   fail if not find("button#synchronize").click
+end
+
+When /^the product should appear in the output of mgr-ncc-sync$/ do
+   $sshout = `echo | ssh -l root -o StrictHostKeyChecking=no $TESTHOST mgr-ncc-sync -l`
+   fail if not $sshout.include? '[P] sle11-sp2-webyast-1.3-pool-x86_64-vmware-sp3'
+   fail if not $sshout.include? 'sle11-sp2-webyast-1.3-updates-x86_64-vmware-sp3'
+end
 
