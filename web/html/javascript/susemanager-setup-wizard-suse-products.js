c83e3d197d74adfe2e573643b59b1bde5f7a5ddf
Silvio Moioli <smoioli@suse.de>
2014-04-15 16:29:00 +0200

    Products: change checkboxes behavior according to thallman feedback

--- web/html/javascript/susemanager-setup-wizard-suse-products.js
+++ web/html/javascript/susemanager-setup-wizard-suse-products.js
@@ -20,7 +20,7 @@ $(function(){
 
   // handles synchronize bottom button
   $("#synchronize").click(function() {
-    var checkboxes = $("#table-content input[type='checkbox']:checked");
+    var checkboxes = $("#table-content input[type='checkbox']:checked:enabled");
     var idents = checkboxes.closest("tr").map(function() {
       return $(this).data("ident");
     }).toArray();
@@ -34,6 +34,26 @@ $(function(){
         showSyncStarted(this);
       });
     }));
+  });
+
+  // handles clicks on product checkboxes
+  $("#table-content").on("change", "input", function(event) {
+    var checked = $(this).prop("checked");
+    var row = $(this).closest("tr");
+    var baseProductIdent = row.data("baseproductident");
+    var base = baseProductIdent == "";
+
+    if (!checked && base) {
+      var productIdent = row.data("ident");
+      $("tr[data-baseproductident='" + productIdent + "']")
+        .find("input")
+        .prop("checked", false);
+    }
+    if (checked && !base) {
+      $("tr[data-ident='" + baseProductIdent + "']")
+        .find("input")
+        .prop("checked", true);
+    }
   });
 
   // Get the products and show message in case of errors
@@ -71,7 +91,7 @@ $(function(){
     row.find(".sync-started-icon").show();
 
     var checkbox = row.find("input[type='checkbox']");
-    checkbox.prop("checked", false);
+    checkbox.prop("checked", true);
     checkbox.prop("disabled", true);
 
     var addonRows = $("tr[data-baseproductident='" + ident + "']");
