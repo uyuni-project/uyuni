378773ba89c51b1133ff54348cb57ee904b1d33a
Silvio Moioli <smoioli@suse.de>
2014-04-16 10:33:56 +0200

    Product: remove 1-click column functionality

--- web/html/javascript/susemanager-setup-wizard-suse-products.js
+++ web/html/javascript/susemanager-setup-wizard-suse-products.js
@@ -1,18 +1,6 @@
 $(function(){
   showProducts();
 
-  // click on an add product icon. Handler is attached to table because rows
-  // can be reloaded via AJAX
-  $("#table-content").on("click", ".start-sync-icon", function(event) {
-    var ident = $(this).closest("tr").data("ident");
-
-    showSyncStarting(ident);
-
-    ProductSyncAction.synchronizeSingle(ident, makeAjaxHandler(function() {
-      showSyncStarted(ident);
-    }));
-  });
-
   // handles select all top/bottom checkboxes
   $("input.select-all").change(function() {
     $("#suse-products input[type='checkbox']:enabled").prop("checked", $(this).is(":checked"));
@@ -25,13 +13,10 @@ $(function(){
       return $(this).data("ident");
     }).toArray();
 
-    $.each(idents, function(){
-      showSyncStarting(this);
-    });
-
-    ProductSyncAction.synchronizeMultiple(idents, makeAjaxHandler(function() {
-      $.each(idents, function(){
-        showSyncStarted(this);
+    ProductSyncAction.synchronize(idents, makeAjaxHandler(function() {
+      $.each(checkboxes, function() {
+        $(this).prop("checked", true);
+        $(this).prop("disabled", true);
       });
     }));
   });
@@ -73,32 +58,5 @@ $(function(){
         }
       })
     );
-  }
-
-  function getRow(ident) {
-    return $("#table-content tr[data-ident='" + ident + "']");
-  }
-
-  function showSyncStarting(ident) {
-    var row = getRow(ident);
-    row.find(".start-sync-icon").hide();
-    row.find(".starting-sync-icon").show();
-  }
-
-  function showSyncStarted(ident) {
-    var row = getRow(ident);
-    row.find(".starting-sync-icon").hide();
-    row.find(".sync-started-icon").show();
-
-    var checkbox = row.find("input[type='checkbox']");
-    checkbox.prop("checked", true);
-    checkbox.prop("disabled", true);
-
-    var addonRows = $("tr[data-baseproductident='" + ident + "']");
-    var addonStartSyncIcons =  addonRows.find(".start-sync-icon");
-    var addonCheckboxes = addonRows.find("input[type='checkbox']");
-
-    addonStartSyncIcons.show();
-    addonCheckboxes.removeAttr("disabled");
   }
 });
