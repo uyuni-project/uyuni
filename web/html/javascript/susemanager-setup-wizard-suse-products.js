23d8ef531a33ff91bee8a9f2d2b54c9725d02726
Silvio Moioli <smoioli@suse.de>
2014-04-17 15:52:57 +0200

    Products: make texts clickable for expanding/collapsing

--- web/html/javascript/susemanager-setup-wizard-suse-products.js
+++ web/html/javascript/susemanager-setup-wizard-suse-products.js
@@ -6,24 +6,17 @@ $(function(){
     $("#suse-products input[type='checkbox']:enabled").prop("checked", $(this).is(":checked"));
   });
 
-  // handles expand/collapse buttons
-  $("#table-content").on("click", ".expand-button", function(event) {
-    $(this).hide();
-    $(this).siblings(".collapse-button").show();
-    $("tr[data-baseproductident='" + $(this).closest("tr").data("ident") + "']").show();
-    return false;
-  });
-
-  $("#table-content").on("click", ".collapse-button", function(event) {
-    $(this).hide();
-    $(this).siblings(".expand-button").show();
-    $("tr[data-baseproductident='" + $(this).closest("tr").data("ident") + "']").hide();
-    return false;
+  // handles expand/collapse buttons and text
+  $(".table-content").on("click", ".collapsible", function() {
+    var row = $(this).closest("tr");
+    row.find(".expand-button").toggle();
+    row.find(".collapse-button").toggle();
+    $("tr[data-baseproductident='" + row.data("ident") + "']").toggle();
   });
 
   // handles synchronize bottom button
   $("#synchronize").click(function() {
-    var checkboxes = $("#table-content input[type='checkbox']:checked:enabled");
+    var checkboxes = $(".table-content input[type='checkbox']:checked:enabled");
     var idents = checkboxes.closest("tr").map(function() {
       return $(this).data("ident");
     }).toArray();
@@ -54,7 +47,7 @@ $(function(){
   });
 
   // handles clicks on product checkboxes
-  $("#table-content").on("change", "input", function(event) {
+  $(".table-content").on("change", "input", function(event) {
     var checked = $(this).prop("checked");
     var row = $(this).closest("tr");
     var baseProductIdent = row.data("baseproductident");
@@ -78,7 +71,7 @@ $(function(){
     ProductsRenderer.renderAsync(makeAjaxHandler(
       function(content) {
         $("#loading-placeholder").hide();
-        $("#table-content").append(content);
+        $(".table-content").append(content);
         $('.product-add-btn').tooltip();
         $('.product-channels-btn').tooltip();
       },
