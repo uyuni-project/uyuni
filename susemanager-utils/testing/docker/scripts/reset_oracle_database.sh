deffe73fef34d9d745c1495defe614d9f6544f76
Flavio Castelli <fcastelli@suse.com>
2014-06-10 09:53:41 +0200

    docker: update Java testing files to use latest Oracle container
    Take advantage of our new docker container running Oracle inside of it.

--- susemanager-utils/testing/docker/scripts/reset_oracle_database.sh
+++ susemanager-utils/testing/docker/scripts/reset_oracle_database.sh
@@ -12,4 +12,6 @@ echo $PERLLIB
 
 ./build-schema.sh
 
+touch /var/lib/rhn/rhn-satellite-prep/etc/rhn/rhn.conf
+
 spacewalk-setup --skip-system-version-test --skip-selinux-test --skip-fqdn-test --skip-gpg-key-import --skip-ssl-cert-generation --skip-ssl-vhost-setup --skip-services-check --clear-db --answer-file=clear-db-answers-oracle.txt --external-oracle --non-interactive
