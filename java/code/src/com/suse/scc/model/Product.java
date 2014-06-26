bb268dadf1412cfa44da50f0adb5b16fe1d80625
Johannes Renner <jrenner@suse.de>
2014-06-26 16:01:18 +0200

    List products using all available credentials

--- java/code/src/com/suse/scc/model/Product.java
+++ java/code/src/com/suse/scc/model/Product.java
@@ -148,4 +148,36 @@ public class Product {
     public List<Repository> getRepositories() {
         return repositories;
     }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Product other = (Product) obj;
+        if (id != other.id) {
+            return false;
+        }
+        return true;
+    }
 }
