2d6649da93feafb35236dac503d72c1100da4efe
Johannes Renner <jrenner@suse.de>
2014-06-26 16:01:18 +0200

    List products using all available credentials

---
+++ java/code/src/com/redhat/rhn/manager/content/ContentSyncManager.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2014 SUSE
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.manager.content;
+
+import com.redhat.rhn.manager.setup.MirrorCredentialsDto;
+import com.redhat.rhn.manager.setup.MirrorCredentialsManager;
+
+import com.suse.scc.SCCClient;
+import com.suse.scc.SCCClientException;
+import com.suse.scc.model.Product;
+
+import org.apache.log4j.Logger;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Content synchronization logic.
+ */
+public class ContentSyncManager {
+
+    // Logger instance
+    private static Logger log = Logger.getLogger(ContentSyncManager.class);
+
+    /**
+     * Default constructor.
+     */
+    public ContentSyncManager() {
+    }
+
+    /**
+     * Returns all products available with all the configured credentials.
+     * @return list of all available products
+     */
+    public Collection<Product> getProducts() {
+        Set<Product> productList = new HashSet<Product>();
+        List<MirrorCredentialsDto> credentials =
+                new MirrorCredentialsManager().findMirrorCredentials();
+        // Query products for all mirror credentials
+        for (MirrorCredentialsDto c : credentials) {
+            SCCClient scc = new SCCClient(c.getUser(), c.getPassword());
+            try {
+                List<Product> products = scc.listProducts();
+                productList.addAll(products);
+            } catch (SCCClientException e) {
+                log.error(e.getMessage(), e);
+            }
+        }
+        if (log.isDebugEnabled()) {
+            log.debug("Found " + productList.size() + " available products.");
+        }
+        return productList;
+    }
+}
