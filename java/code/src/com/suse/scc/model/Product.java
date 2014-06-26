7d9c7ec59a01c8c78c423e9fa1d352e7e8ced02c
Johannes Renner <jrenner@suse.de>
2014-06-26 11:25:34 +0200

    Deserialize products and repositories with all their attributes

--- java/code/src/com/suse/scc/model/Product.java
+++ java/code/src/com/suse/scc/model/Product.java
@@ -14,23 +14,138 @@
  */
 package com.suse.scc.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * This is a SUSE product as parsed from JSON coming in from SCC.
  */
-@SuppressWarnings("unused")
 public class Product {
 
     private int id;
     private String name;
     private String identifier;
     private String version;
-    private String release_type;
+    @SerializedName("release_type")
+    private String releaseType;
     private String arch;
-    private String friendly_name;
+    @SerializedName("friendly_name")
+    private String friendlyName;
+    @SerializedName("enabled_repositories")
+    private List<Integer> enabledRepositories;
+    @SerializedName("product_class")
+    private String productClass;
+    private String cpe;
+    private boolean free;
+    private String description;
+    @SerializedName("eula_url")
+    private String eulaUrl;
+    private List<Product> extensions;
+    private List<Repository> repositories;
 
     /**
-     * No-args constructor
+     * @return the id
      */
-    public Product() {
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the identifier
+     */
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    /**
+     * @return the version
+     */
+    public String getVersion() {
+        return version;
+    }
+
+    /**
+     * @return the release_type
+     */
+    public String getReleaseType() {
+        return releaseType;
+    }
+
+    /**
+     * @return the arch
+     */
+    public String getArch() {
+        return arch;
+    }
+
+    /**
+     * @return the friendly_name
+     */
+    public String getFriendlyName() {
+        return friendlyName;
+    }
+
+    /**
+     * @return the enabled_repositories
+     */
+    public List<Integer> getEnabledRepositories() {
+        return enabledRepositories;
+    }
+
+    /**
+     * @return the product_class
+     */
+    public String getProductClass() {
+        return productClass;
+    }
+
+    /**
+     * @return the cpe
+     */
+    public String getCpe() {
+        return cpe;
+    }
+
+    /**
+     * @return the free
+     */
+    public boolean isFree() {
+        return free;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @return the eula_url
+     */
+    public String getEulaUrl() {
+        return eulaUrl;
+    }
+
+    /**
+     * @return the extensions
+     */
+    public List<Product> getExtensions() {
+        return extensions;
+    }
+
+    /**
+     * @return the repositories
+     */
+    public List<Repository> getRepositories() {
+        return repositories;
     }
 }
