c6347ee47bd3a5569ca5e284f782b62ee17eb44d
Johannes Renner <jrenner@suse.de>
2014-06-27 10:08:17 +0200

    Implement getRepositories() listing repos from SCC
    Needs refactoring since lots of code is duplicated

--- java/code/src/com/suse/scc/model/Repository.java
+++ java/code/src/com/suse/scc/model/Repository.java
@@ -62,4 +62,36 @@ public class Repository {
     public boolean isAutorefresh() {
         return autorefresh;
     }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Repository other = (Repository) obj;
+        if (id != other.id) {
+            return false;
+        }
+        return true;
+    }
 }
