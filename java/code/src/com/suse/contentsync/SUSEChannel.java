7aebe560ecc0c641f7f7c9acfe1dc8b9d84e5fd4
Bo Maryniuk <bo@suse.de>
2014-07-01 16:53:21 +0200

    Added channels.xml, channels_families.xml and upgrade_paths.xml parsing

---
+++ java/code/src/com/suse/contentsync/SUSEChannel.java
@@ -0,0 +1,169 @@
+/**
+ * Copyright (c) 2014 SUSE
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.suse.contentsync;
+
+import java.util.Collection;
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.ElementList;
+import org.simpleframework.xml.Root;
+
+/**
+ * SUSE Channels are available from NCC and SCC.
+ */
+@Root(name = "channel")
+public class SUSEChannel {
+    @Attribute
+    private String arch;
+
+    @Attribute
+    private String description;
+
+    @Attribute
+    private String family;
+
+    @Attribute(name = "is_signed")
+    private String isSigned;
+
+    @Attribute
+    private String label;
+
+    @Attribute
+    private String name;
+
+    @Attribute
+    private String optional;
+
+    @Attribute
+    private String parent;
+
+    @Attribute(name = "product_name")
+    private String productName;
+
+    @Attribute(name = "product_version")
+    private String productVersion;
+
+    @Attribute(name = "source_url")
+    private String sourceUrl;
+
+    @Attribute
+    private String summary;
+
+    @Attribute(name = "update_tag")
+    private String updateTag;
+
+    private Collection<SUSEProduct> products;
+
+    @ElementList(name = "products")
+    public Collection<SUSEProduct> getProducts() {
+        return products;
+    }
+
+    @ElementList(name = "products")
+    public void setProducts(Collection<SUSEProduct> products) {
+        this.products = products;
+    }
+
+    /**
+     * @return the arch
+     */
+    public String getArch() {
+        return arch;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @return the family
+     */
+    public String getFamily() {
+        return family;
+    }
+
+    /**
+     * @return the isSigned
+     */
+    public Boolean isSigned() {
+        return (this.isSigned + "").toUpperCase().equals("Y");
+    }
+
+    /**
+     * @return the label
+     */
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the optional
+     */
+    public String getOptional() {
+        return optional;
+    }
+
+    /**
+     * @return the parent
+     */
+    public String getParent() {
+        return parent;
+    }
+
+    /**
+     * @return the productName
+     */
+    public String getProductName() {
+        return productName;
+    }
+
+    /**
+     * @return the productVersion
+     */
+    public String getProductVersion() {
+        return productVersion;
+    }
+
+    /**
+     * @return the sourceUrl
+     */
+    public String getSourceUrl() {
+        return sourceUrl;
+    }
+
+    /**
+     * @return the summary
+     */
+    public String getSummary() {
+        return summary;
+    }
+
+    /**
+     * @return the updateTag
+     */
+    public String getUpdateTag() {
+        return updateTag;
+    }
+}
---
+++ java/code/src/com/suse/contentsync/SUSEUpgradePaths.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2014 SUSE
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.suse.contentsync;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.simpleframework.xml.ElementList;
+
+/**
+ *
+ * @author bo
+ */
+public class SUSEUpgradePaths {
+    @ElementList(name = "upgradepathes", inline = true, required = false)
+                        //          ^
+                        //          |
+                        //          +-- Can we change "pathEs" to "paths"?
+
+    private List<SUSEUpgradePath> paths;
+
+    /**
+     * Return the list of {@link SUSEFamily} objects.
+     * @return subscriptions
+     */
+    public List<SUSEUpgradePath> getPaths() {
+        if (this.paths == null) {
+            this.paths = new ArrayList<SUSEUpgradePath>();
+        }
+        return this.paths;
+    }
+}
