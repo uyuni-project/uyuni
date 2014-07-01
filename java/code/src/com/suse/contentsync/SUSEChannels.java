01ebbc2c1b1f5d32da65bd05b6cf0cbe01318528
Bo Maryniuk <bo@suse.de>
2014-07-01 16:53:21 +0200

    Added channels.xml, channels_families.xml and upgrade_paths.xml parsing

---
+++ java/code/src/com/suse/contentsync/SUSEChannels.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2014 SUSE
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.suse.contentsync;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.simpleframework.xml.ElementList;
+import org.simpleframework.xml.Root;
+
+@Root(name = "channels")
+public class SUSEChannels {
+    @ElementList(name = "channel", inline = true, required = false)
+    private List<SUSEChannel> channels;
+
+    /**
+     * Return the list of {@link Subscription} objects.
+     * @return subscriptions
+     */
+    public List<SUSEChannel> getChannels() {
+        if (this.channels == null) {
+            this.channels = new ArrayList<SUSEChannel>();
+        }
+
+        return this.channels;
+    }
+}
---
+++ java/code/src/com/suse/contentsync/SUSEProduct.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2014 SUSE
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.suse.contentsync;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Root;
+import org.simpleframework.xml.Text;
+
+/**
+ *
+ * @author bo
+ */
+@Root(name = "product_id")
+public class SUSEProduct {
+    @Attribute
+    private String name;
+    private Integer id;
+
+    @Text
+    public Integer getId() {
+        return id;
+    }
+
+    @Text
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
