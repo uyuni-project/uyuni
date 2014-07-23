d8192de985833530b4162df35bae63ecaf85730b
Silvio Moioli <smoioli@suse.de>
2014-07-23 18:33:48 +0200

    Add support to ppc64le arch

--- schema/spacewalk/common/data/rhnServerServerGroupArchCompat.sql
+++ schema/spacewalk/common/data/rhnServerServerGroupArchCompat.sql
@@ -125,6 +125,10 @@ insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
             lookup_sg_type('sw_mgr_entitled'));
 
 insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
+    values (lookup_server_arch('ppc64le-redhat-linux'),
+            lookup_sg_type('sw_mgr_entitled'));
+
+insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
 	values (lookup_server_arch('pSeries-redhat-linux'), 
             lookup_sg_type('sw_mgr_entitled'));
 
@@ -270,6 +274,10 @@ insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
 
 insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
 	values (lookup_server_arch('ppc64-redhat-linux'), 
+            lookup_sg_type('enterprise_entitled'));
+
+insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
+    values (lookup_server_arch('ppc64le-redhat-linux'),
             lookup_sg_type('enterprise_entitled'));
 
 insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
@@ -434,6 +442,10 @@ insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
 
 insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
 	values (lookup_server_arch('ppc64-redhat-linux'), 
+            lookup_sg_type('provisioning_entitled'));
+
+insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
+    values (lookup_server_arch('ppc64le-redhat-linux'),
             lookup_sg_type('provisioning_entitled'));
 
 insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
@@ -582,6 +594,10 @@ insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
 
 insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
 	values (lookup_server_arch('ppc64-redhat-linux'), 
+            lookup_sg_type('monitoring_entitled'));
+
+insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
+    values (lookup_server_arch('ppc64le-redhat-linux'),
             lookup_sg_type('monitoring_entitled'));
 
 insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
@@ -716,6 +732,10 @@ insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type)
 
 insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type)
     values (lookup_server_arch('ppc64-redhat-linux'),
+            lookup_sg_type('virtualization_host'));
+
+insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type)
+    values (lookup_server_arch('ppc64le-redhat-linux'),
             lookup_sg_type('virtualization_host'));
 
 insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type)
@@ -882,6 +902,10 @@ insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
 
 insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
 	values (lookup_server_arch('ppc64-redhat-linux'), 
+            lookup_sg_type('bootstrap_entitled'));
+
+insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
+    values (lookup_server_arch('ppc64le-redhat-linux'),
             lookup_sg_type('bootstrap_entitled'));
 
 insert into rhnServerServerGroupArchCompat ( server_arch_id, server_group_type )
