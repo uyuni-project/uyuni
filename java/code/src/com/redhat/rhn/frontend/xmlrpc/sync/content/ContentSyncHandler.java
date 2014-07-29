ec152137329b337f2af9d5fa9b6b71937c4e1fbe
Bo Maryniuk <bo@suse.de>
2014-07-29 10:42:29 +0200

    Removed stub methods from the XML-RPC handler for the Mgr Sync.

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/sync/content/ContentSyncHandler.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/sync/content/ContentSyncHandler.java
@@ -19,56 +19,12 @@ import com.redhat.rhn.frontend.xmlrpc.BaseHandler;
 import com.redhat.rhn.manager.content.ContentSyncException;
 import com.redhat.rhn.manager.content.ContentSyncManager;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @xmlrpc.namespace sync.content
  * @xmlrpc.doc Provides the namespace for the Content synchronization methods.
  */
 public class ContentSyncHandler extends BaseHandler {
-    /**
-     * Add product by name.
-     *
-     * @param sessionKey Session token.
-     * @param name Name of the product.
-     * @return State of the action result.
-     *
-     * @xmlrpc.doc Add product by name.
-     * @xmlrpc.param #param_desc("string", "sessionKey", "Session token, issued at login")
-     * @xmlrpc.param #param_desc("string", "name", "Name of the product")
-     * @xmlrpc.returntype #return_int_success()
-     */
-    public Integer addProduct(String sessionKey, String name) {
-        BaseHandler.getLoggedInUser(sessionKey);
-        System.err.println("Added product: " + name);
-
-        return BaseHandler.VALID;
-    }
-
-
-    /**
-     * Add channel by label.
-     *
-     * @param sessionKey Session token.
-     * @param label Name of the product.
-     * @return State of the action result.
-     *
-     * @xmlrpc.doc Add product by name.
-     * @xmlrpc.param #param_desc("string", "sessionKey", "Session token, issued at login")
-     * @xmlrpc.param #param_desc("string", "label", "Label of the channel")
-     * @xmlrpc.returntype #return_int_success()
-     */
-    public Integer addChannel(String sessionKey, String label) {
-        BaseHandler.getLoggedInUser(sessionKey);
-        System.err.println("Added channel: " + label);
-
-        return BaseHandler.VALID;
-    }
-
-
     /**
      * List all products that are accessible to the organization.
      *
@@ -104,51 +60,6 @@ public class ContentSyncHandler extends BaseHandler {
         BaseHandler.getLoggedInUser(sessionKey);
         ContentSyncManager csm = new ContentSyncManager();
         return csm.listChannels(csm.getRepositories()).toArray();
-    }
-
-
-    /**
-     * List channel extensions (add-ons).
-     *
-     * @param sessionKey Session token.
-     * @param label The label of the channel.
-     * @return List of channels.
-     *
-     * @xmlrpc.doc List all channels that are accessible to the organization.
-     * @xmlrpc.param #param_desc("string", "sessionKey", "Session token, issued at login")
-     * @xmlrpc.param #param_desc("string", "label", "Channel label")
-     * @xmlrpc.returntype #array()
-     *                      #struct("addon")
-     *                        #prop_desc("string", "name", "Name of the product")
-     *                        #prop_desc("string", "label",
-     *                                             "Label of the product (identifier)")
-     *                        #prop_desc("string", "version", "Version")
-     *                        #prop_desc("string", "release", "Release type")
-     *                        #prop_desc("string", "arch", "Architecture")
-     *                        #prop_desc("string", "title", "Title (friendly name)")
-     *                        #prop_desc("string", "description", "Description")
-     *                      #struct_end()
-     *                    #array_end()
-     */
-    public List<Map<String, Object>> listChannelExtensions(String sessionKey, String label) {
-        BaseHandler.getLoggedInUser(sessionKey);
-        List<Map<String, Object>> extensions = new ArrayList<Map<String, Object>>();
-
-        // Some bogus data that looks like real.
-        for (int i = 0; i < 10; i++) {
-            Map<String, Object> ext = new HashMap<String, Object>();
-            ext.put("name", "SUSE Linux Enterprise Server");
-            ext.put("label", "sle-sdk");
-            ext.put("version", "12");
-            ext.put("release", "GA");
-            ext.put("arch", "ppc64le");
-            ext.put("title", "SUSE Linux Enterprise Software Development Cat 12 ppc64le");
-            ext.put("description", "Something to entertain you for long.");
-            ext.put("status", "available");
-            extensions.add(ext);
-        }
-
-        return extensions;
     }
 
 
