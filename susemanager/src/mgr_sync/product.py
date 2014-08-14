ff9966776e4017dd9054efaee2412c9ddd51943a
Flavio Castelli <fcastelli@suse.com>
2014-08-14 16:20:10 +0200

    mgr-sync: handle products with a dedicated class

---
+++ susemanager/src/mgr_sync/product.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2014 SUSE
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+# SUSE trademarks are not licensed under GPLv2. No permission is
+# granted to use or replicate SUSE trademarks that are incorporated
+# in this software or its documentation.
+
+from enum import Enum
+
+
+class Product(object):
+
+    class Status(str, Enum):
+        INSTALLED = "INSTALLED"
+        AVAILABLE = "AVAILABLE"
+
+    def __init__(self, data):
+        self.arch = data["arch"]
+        self.friendly_name = data["friendly_name"]
+        self.status = Product.Status(data["status"].upper())
+        self.extensions = []
+        self._parse_extensions(data["extensions"])
+
+    @property
+    def short_status(self):
+        return "[%s]" % str(self.status.value)[0]
+
+    def to_ascii_row(self):
+        return "{0} {1} ({2})".format(self.short_status, self.friendly_name,
+                                      self.arch)
+
+    def to_stdout(self, indentation_level=0):
+        prefix = indentation_level * "  "
+        print(prefix + self.to_ascii_row())
+        indentation_level += 1
+        for ext in self.extensions:
+            ext.to_stdout(indentation_level=indentation_level)
+
+    def _parse_extensions(self, data):
+        for extension in data:
+            self.extensions.append(Product(extension))
+
+
+def parse_products(data):
+    """
+    Parses the data returned by SUSE Manager list products API.
+    Returns a list of the Products.
+    """
+
+    products = []
+
+    for p in data:
+        products.append(Product(p))
+
+    return products
+
---
+++ susemanager/src/test/test_mgr_sync/test_product.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014 Novell, Inc.
+#   This library is free software; you can redistribute it and/or modify
+# it only under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import os
+import sys
+import unittest2 as unittest
+
+from spacewalk.susemanager.mgr_sync.product import parse_products, Product
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from helper import read_data_from_fixture, CaptureStdout
+
+
+class ProductTest(unittest.TestCase):
+
+    def setUp(self):
+        self.products = parse_products(read_data_from_fixture("list_products.data"))
+
+
+    def test_parse_products(self):
+
+        res4 = next(p for p in self.products if p.friendly_name == 'RES 4' and p.arch == 'i386')
+        self.assertEqual([], res4.extensions)
+        self.assertEqual(Product.Status.AVAILABLE, res4.status)
+
+        sles11sp3_s390x = next(p for p in self.products if p.friendly_name == 'SUSE Linux Enterprise Server 11 SP3' and p.arch == 's390x')
+        self.assertEqual(2, len(sles11sp3_s390x.extensions))
+        self.assertEqual(Product.Status.AVAILABLE, sles11sp3_s390x.status)
+
+        sle_ha = next(p for p in sles11sp3_s390x.extensions if p.friendly_name == 'SUSE Linux Enterprise High Availability Extension 11 SP3')
+        self.assertEqual([], sle_ha.extensions)
+        self.assertEqual(Product.Status.AVAILABLE, sle_ha.status)
+        self.assertEqual(sle_ha.arch, sles11sp3_s390x.arch)
+
+        sles_sdk = next(p for p in sles11sp3_s390x.extensions if p.friendly_name == 'SUSE Linux Enterprise Software Development Kit 11 SP3')
+        self.assertEqual([], sles_sdk.extensions)
+        self.assertEqual(Product.Status.INSTALLED, sles_sdk.status)
+        self.assertEqual(sles_sdk.arch, sles11sp3_s390x.arch)
+
+    def test_to_stdout(self):
+        sles11sp3_s390x = next(p for p in self.products if p.friendly_name == 'SUSE Linux Enterprise Server 11 SP3' and p.arch == 's390x')
+        with CaptureStdout() as output:
+            sles11sp3_s390x.to_stdout()
+
+        expected_output = """[A] SUSE Linux Enterprise Server 11 SP3 (s390x)
+  [A] SUSE Linux Enterprise High Availability Extension 11 SP3 (s390x)
+  [I] SUSE Linux Enterprise Software Development Kit 11 SP3 (s390x)"""
+
+        self.assertEqual(expected_output.split("\n"), output)
+
+        res4 = next(p for p in self.products if p.friendly_name == 'RES 4' and p.arch == 'i386')
+        with CaptureStdout() as output:
+            res4.to_stdout()
+
+        expected_output = ["[A] RES 4 (i386)"]
+        self.assertEqual(expected_output, output)
+
