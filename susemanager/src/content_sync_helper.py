559eece34ddfd90114cbc68085b20192eada7ee9
Flavio Castelli <fcastelli@suse.com>
2014-08-26 12:08:52 +0200

    Added mgr-contenent-sync tool
    This tool is used to change/inspect the content sync backend used by
    SUSE Manager.

---
+++ susemanager/src/content_sync_helper.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2014 SUSE
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+# SUSE trademarks are not licensed under GPLv2. No permission is
+# granted to use or replicate SUSE trademarks that are incorporated
+# in this software or its documentation.
+
+import os
+from enum import Enum
+
+from spacewalk.susemanager.authenticator import Authenticator
+
+
+MASTER_SWITCH_FILE = "/var/lib/spacewalk/scc/migrated"
+
+
+class BackendType(str, Enum):
+    NCC = "NCC"
+    SCC = "SCC"
+
+
+def current_backend():
+    """ Returns an instance of `BackendType` """
+
+    if os.path.isfile(MASTER_SWITCH_FILE):
+        return BackendType.SCC
+    else:
+        return BackendType.NCC
+
+
+def switch_to_scc(connection):
+    auth = Authenticator(connection=connection,
+                         user=None,
+                         password=None,
+                         token=None)
+
+    connection.sync.content.performMigration(auth.token())
