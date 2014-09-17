516fe084b2eed5d879ee97f355149452134bd3aa
Silvio Moioli <smoioli@suse.de>
2014-09-17 11:15:36 +0200

    SCCClient: checkstyle fixes

--- java/code/src/com/suse/scc/client/SCCClient.java
+++ java/code/src/com/suse/scc/client/SCCClient.java
@@ -26,13 +26,17 @@ import java.util.List;
 
 /**
  * SCC API Client.
- * TODO: Support proxied connections.
  */
 public class SCCClient {
 
     private SCCConfig config = new SCCConfig();
+
     /**
      * Constructor expecting a hostname and credentials.
+     *
+     * @param hostname the hostname
+     * @param username the username
+     * @param password the password
      */
     public SCCClient(String hostname, String username, String password) {
         // Put a given hostname to config
@@ -48,15 +52,18 @@ public class SCCClient {
 
     /**
      * Constructor for connecting to scc.suse.com.
+     *
+     * @param username the username
+     * @param password the password
      */
     public SCCClient(String username, String password) {
         this(null, username, password);
     }
 
-     /**
-      * Directly access the configuration
-      * @return the configuration
-      */
+    /**
+     * Directly access the configuration
+     * @return the configuration
+     */
     public SCCConfig getConfig() {
         return config;
     }
@@ -72,17 +79,18 @@ public class SCCClient {
         }
         if (settings.getUsername() != null) {
             config.put(SCCConfig.PROXY_USERNAME, settings.getUsername());
-                if (settings.getPassword() != null) {
-            config.put(SCCConfig.PROXY_PASSWORD, settings.getPassword());
+            if (settings.getPassword() != null) {
+                config.put(SCCConfig.PROXY_PASSWORD, settings.getPassword());
             }
         }
     }
 
     /**
-     * Configure this client's UUID that will be sent to SCC for debugging purposes.
+     * Configure this client's UUID that will be sent to SCC for debugging
+     * purposes.
      * @param uuid the UUID to send for debugging
      */
-    public void setUUID (String uuid) {
+    public void setUUID(String uuid) {
         if (uuid != null) {
             config.put(SCCConfig.UUID, uuid);
         }
@@ -94,11 +102,12 @@ public class SCCClient {
      * GET /connect/organizations/products
      *
      * @return list of products available to organization
+     * @throws SCCClientException if anything goes wrong SCC side
      */
     public List<SCCProduct> listProducts() throws SCCClientException {
-        SCCConnection scc = new SCCConnection("/connect/organizations/products", config);
-        Type returnType = new TypeToken<List<SCCProduct>>(){}.getType();
-        return scc.get(returnType);
+        Type returnType = new TypeToken<List<SCCProduct>>() { } .getType();
+        return new SCCConnection("/connect/organizations/products/unscoped", config)
+                .get(returnType);
     }
 
     /**
@@ -107,11 +116,12 @@ public class SCCClient {
      * GET /connect/organizations/repositories
      *
      * @return list of repositories available to organization
+     * @throws SCCClientException if anything goes wrong SCC side
      */
     public List<SCCRepository> listRepositories() throws SCCClientException {
-        SCCConnection scc = new SCCConnection("/connect/organizations/repositories", config);
-        Type returnType = new TypeToken<List<SCCRepository>>(){}.getType();
-        return scc.get(returnType);
+        Type returnType = new TypeToken<List<SCCRepository>>() { } .getType();
+        return new SCCConnection("/connect/organizations/repositories", config)
+                .get(returnType);
     }
 
     /**
@@ -120,10 +130,11 @@ public class SCCClient {
      * GET /connect/organizations/subscriptions
      *
      * @return list of subscriptions available to organization
+     * @throws SCCClientException if anything goes wrong SCC side
      */
     public List<SCCSubscription> listSubscriptions() throws SCCClientException {
-        SCCConnection scc = new SCCConnection("/connect/organizations/subscriptions", config);
-        Type returnType = new TypeToken<List<SCCSubscription>>(){}.getType();
-        return scc.get(returnType);
+        Type returnType = new TypeToken<List<SCCSubscription>>() { } .getType();
+        return new SCCConnection("/connect/organizations/subscriptions", config)
+                .get(returnType);
     }
 }
