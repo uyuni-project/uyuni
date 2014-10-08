0bb3b43f5ae015853669129b413bdc1bd7b0971d
Johannes Renner <jrenner@suse.de>
2014-10-08 11:22:10 +0200

    Remove unused import

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/serializer/MgrSyncChannelSerializer.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/serializer/MgrSyncChannelSerializer.java
@@ -17,7 +17,6 @@ package com.redhat.rhn.frontend.xmlrpc.serializer;
 import com.redhat.rhn.frontend.xmlrpc.serializer.util.SerializerHelper;
 
 import com.suse.mgrsync.MgrSyncChannel;
-import com.suse.mgrsync.MgrSyncStatus;
 
 import java.io.IOException;
 import java.io.Writer;
