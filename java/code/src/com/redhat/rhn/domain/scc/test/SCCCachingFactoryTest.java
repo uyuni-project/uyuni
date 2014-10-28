3569cf92bc70f47d55970f994f820a69bde0970f
Johannes Renner <jrenner@suse.de>
2014-10-28 17:14:28 +0100

    Add SCCCachingFactory to operate on caching tables + test

---
+++ java/code/src/com/redhat/rhn/domain/scc/test/SCCCachingFactoryTest.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2014 SUSE
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.domain.scc.test;
+
+import com.redhat.rhn.domain.scc.SCCCachingFactory;
+import com.redhat.rhn.domain.scc.SCCRepository;
+import com.redhat.rhn.testing.TestUtils;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests for {@link SCCCachingFactory}.
+ */
+public class SCCCachingFactoryTest extends TestCase {
+
+    /**
+     * Test if initially an empty list is returned.
+     */
+    public void testRepositoriesEmpty() {
+        List<SCCRepository> repos = SCCCachingFactory.lookupRepositories();
+        assertTrue(repos.isEmpty());
+    }
+
+    /**
+     * Test repository insertion and lookup.
+     */
+    public void testRepositoriesInsertAndLookup() {
+        SCCRepository repo0 = createTestRepo(0L);
+        SCCRepository repo1 = createTestRepo(1L);
+        SCCRepository repo2 = createTestRepo(2L);
+
+        SCCCachingFactory.storeRepository(repo0);
+        SCCCachingFactory.storeRepository(repo1);
+        SCCCachingFactory.storeRepository(repo2);
+
+        List<SCCRepository> repos = SCCCachingFactory.lookupRepositories();
+        assertFalse(repos.isEmpty());
+        assertEquals(repo0, repos.get(0));
+        assertEquals(repo1, repos.get(1));
+        assertEquals(repo2, repos.get(2));
+    }
+
+    /**
+     * Repo for testing setting random strings and a given ID.
+     * @return repository
+     */
+    private SCCRepository createTestRepo(Long id) {
+        SCCRepository repo = new SCCRepository();
+        repo.setId(id);
+        repo.setDescription(TestUtils.randomString());
+        repo.setDistroTarget(TestUtils.randomString());
+        repo.setName(TestUtils.randomString());
+        repo.setUrl(TestUtils.randomString());
+        repo.setAutorefresh(true);
+        // TODO: repo.setCredentials();
+        return repo;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        SCCCachingFactory.clearRepositories();
+    }
+}
