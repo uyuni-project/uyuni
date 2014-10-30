019b0e8f6f4a593610e4e4a76e55044f1b2b317c
Bo Maryniuk <bo@suse.de>
2014-10-30 13:13:06 +0100

    Move root path to the global var

--- features/step_definitions/scc_migration.rb
+++ features/step_definitions/scc_migration.rb
@@ -1,11 +1,12 @@
 # Copyright 2011-2014 SUSE
 $migrated = false
 $default_scc = false
+$SCC_PATH = "/var/lib/spacewalk/scc"
 
 When(/^I save migration state$/) do
-  $migrated = sshcmd("test -e /var/lib/spacewalk/scc/migrated && echo 'E'", ignore_err: true)[:stdout].include? 'E'
-  $default_scc = sshcmd("test -e /var/lib/spacewalk/scc/default_scc && echo 'E'", ignore_err: true)[:stdout].include? 'E'
-  sshcmd("rm /var/lib/spacewalk/scc/*", ignore_err: true)
+  $migrated = sshcmd("test -e #{$SCC_PATH}/migrated && echo 'E'", ignore_err: true)[:stdout].include? 'E'
+  $default_scc = sshcmd("test -e #{$SCC_PATH}/default_scc && echo 'E'", ignore_err: true)[:stdout].include? 'E'
+  sshcmd("rm #{$SCC_PATH}/scc/*", ignore_err: true)
 end
 
 Given(/^there is no "(.*?)" file on the server$/) do |target_file|
@@ -18,8 +19,8 @@ end
 
 Then(/^I restore migration state$/) do
   action = $migrated ? "touch" : "rm"
-  sshcmd("#{action} /var/lib/spacewalk/scc/migrated", ignore_err: true)    
+  sshcmd("#{action} #{$SCC_PATH}/migrated", ignore_err: true)    
 
   action = $default_scc ? "touch" : "rm"
-  sshcmd("#{action} /var/lib/spacewalk/scc/default_scc", ignore_err: true)    
+  sshcmd("#{action} #{$SCC_PATH}/default_scc", ignore_err: true)    
 end
