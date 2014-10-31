8e60cf0e0d4104c6783ac4d2aef5dd76d16cc034
Silvio Moioli <smoioli@suse.de>
2014-10-31 16:21:06 +0100

    Moved taskomatic job to midnight, 23 is already crowded

--- schema/spacewalk/common/data/rhnTaskoSchedule.sql
+++ schema/spacewalk/common/data/rhnTaskoSchedule.sql
@@ -122,4 +122,4 @@ INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES (sequence_nextval('rhn_tasko_schedule_id_seq'), 'mgr-sync-refresh-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='mgr-sync-refresh-bunch'),
-        current_timestamp, '0 0 23 ? * *');
+        current_timestamp, '0 0 0 ? * *');
