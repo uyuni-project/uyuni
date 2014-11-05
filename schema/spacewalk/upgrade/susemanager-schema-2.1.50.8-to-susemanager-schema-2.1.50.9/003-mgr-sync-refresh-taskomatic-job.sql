e2bbdaaabf215ddb0566680b1795e2bb5a71f049
Johannes Renner <jrenner@suse.de>
2014-11-05 11:32:08 +0100

    Schedule at 0:00 as the initial setup does it

--- schema/spacewalk/upgrade/susemanager-schema-2.1.50.8-to-susemanager-schema-2.1.50.9/003-mgr-sync-refresh-taskomatic-job.sql
+++ schema/spacewalk/upgrade/susemanager-schema-2.1.50.8-to-susemanager-schema-2.1.50.9/003-mgr-sync-refresh-taskomatic-job.sql
@@ -30,4 +30,4 @@ INSERT INTO rhnTaskoTemplate (id, bunch_id, task_id, ordering, start_if)
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES (sequence_nextval('rhn_tasko_schedule_id_seq'), 'mgr-sync-refresh-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='mgr-sync-refresh-bunch'),
-        current_timestamp, '0 0 23 ? * *');
+        current_timestamp, '0 0 0 ? * *');
