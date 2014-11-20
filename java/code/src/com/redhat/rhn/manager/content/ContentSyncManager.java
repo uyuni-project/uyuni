c1c5616cd338e1f0a23662ad0e805e3fb31cee34
Johannes Renner <jrenner@suse.de>
2014-11-20 14:59:52 +0100

    Update channel family membership when channel is updated (bsc#901193)

--- java/code/src/com/redhat/rhn/manager/content/ContentSyncManager.java
+++ java/code/src/com/redhat/rhn/manager/content/ContentSyncManager.java
@@ -719,6 +719,8 @@ public class ContentSyncManager {
                     c.setName(channel.getName());
                     c.setSummary(channel.getSummary());
                     c.setUpdateTag(channel.getUpdateTag());
+                    c.setChannelFamily(ChannelFamilyFactory.lookupByLabel(
+                            channel.getFamily(), null));
                     ChannelFactory.save(c);
                 }
             }
