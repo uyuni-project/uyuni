dee71643ebb333bc4b9cdc76920d79cd07395035
Paul Wayper <paulway@redhat.com>
2014-11-21 14:45:59 +1100

    Documentation changes - fix name and refer to RFC.

--- java/code/src/com/redhat/rhn/domain/server/Server.java
+++ java/code/src/com/redhat/rhn/domain/server/Server.java
@@ -1022,7 +1022,7 @@ public class Server extends BaseDomainHelper implements Identifiable {
 
     /**
      * Get the primary hostname for this server
-     * If hostname is IDN, it is decoded from Pune encoding
+     * If hostname is IDN, it is decoded from Puny encoding
      * @return Returns the primary hostname for this server
      */
     public String getDecodedHostname() {
@@ -1032,7 +1032,7 @@ public class Server extends BaseDomainHelper implements Identifiable {
 
     /**
      * Get the hostname aliases (cname records) for this server
-     * If hostname is IDN, it is decoded from Pune encoding
+     * If hostname is IDN, it is decoded from Puny encoding
      * @return Returns the primary hostname for this server
      */
     public List<String> getDecodedCnames() {
