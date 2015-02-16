dbb693d786b4abf416b0adb45969b0767c063a0e
Michael Calmer <mc@suse.de>
2015-02-16 11:19:06 +0100

    init the second DB connection only when needed (inspired by bsc#915581)

--- backend/server/rhnSQL/__init__.py
+++ backend/server/rhnSQL/__init__.py
@@ -102,7 +102,7 @@ def __init__DB2(backend, host, port, username, password, database, sslmode, sslr
     return 0
 
 def initDB(backend=None, host=None, port=None, username=None,
-           password=None, database=None, sslmode=None, sslrootcert=None):
+           password=None, database=None, sslmode=None, sslrootcert=None, initsecond=False):
     """
     Initialize the database.
 
@@ -135,18 +135,17 @@ def initDB(backend=None, host=None, port=None, username=None,
     # Hide the password
     add_to_seclist(password)
     try:
-        __init__DB(backend, host, port, username, password, database, sslmode, sslrootcert)
-        __init__DB2(backend, host, port, username, password, database, sslmode, sslrootcert)
+        if initsecond == False:
+            __init__DB(backend, host, port, username, password, database, sslmode, sslrootcert)
+        else:
+            __init__DB2(backend, host, port, username, password, database, sslmode, sslrootcert)
 #    except (rhnException, SQLError):
 #        raise  # pass on, we know those ones
 #    except (KeyboardInterrupt, SystemExit):
 #        raise
     except SQLConnectError, e:
         try:
-            global __DB
-            global __DB2
-            del __DB
-            del __DB2
+            closeDB()
         except NameError:
             pass
         raise e
@@ -204,7 +203,12 @@ def __test_DB2():
     try:
         return __DB2
     except NameError:
-        raise SystemError("Not connected to secondary database!"), None, sys.exc_info()[2]
+        # try open the connection
+        try:
+            initDB(initsecond=True)
+            return __DB2
+        except NameError:
+            raise SystemError("Not connected to secondary database!"), None, sys.exc_info()[2]
 
 # wrapper for a Procedure callable class
 def Procedure(name):
