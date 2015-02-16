aa665583ddc097835338a95cb06c6847da1204e8
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-02-16 18:01:53 +0100

    File.expand_path(__FILE__) is an expression

--- features/support/xmlrpc_system.rb
+++ features/support/xmlrpc_system.rb
@@ -1,7 +1,4 @@
-File.expand_path(__FILE__)           # For Ruby 1.9.2+
-$LOAD_PATH << File.dirname(__FILE__) # For Ruby 1.8
-
-require 'xmlrpctest'
+require_relative 'xmlrpctest'
 
 class XMLRPCSystemTest < XMLRPCBaseTest
   def listSystems
