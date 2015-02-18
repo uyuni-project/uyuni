aa8f26b037d2a96e442522ac2347daecfd720066
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-02-18 13:04:46 +0100

    improvements to xmlrpc cve audit steps
    - do not use globals but member variables
    - use minitest assertions
    - add a step that recognizes "this client"

--- features/step_definitions/xmlrpc_cve_audit_steps.rb
+++ features/step_definitions/xmlrpc_cve_audit_steps.rb
@@ -1,26 +1,26 @@
-rpctest = nil 
-result_list = []
-result = nil
 
 Given /^I am logged in via XML\-RPC\/cve audit as user "([^"]*)" and password "([^"]*)"$/ do |luser, password|
-  rpctest = XMLRPCCVEAuditTest.new(ENV["TESTHOST"])
-  rpctest.login(luser, password)
+  @rpctest = XMLRPCCVEAuditTest.new(ENV["TESTHOST"])
+  @rpctest.login(luser, password)
 end
 
 Given /^channel data has already been updated$/ do
-  fail if rpctest.populateCVEServerChannels() != 1
+  fail if @rpctest.populateCVEServerChannels() != 1
 end
 
 When /^I call audit.listSystemsByPatchStatus with CVE identifier "([^\"]*)"$/ do |cve_identifier|
-  result_list = rpctest.listSystemsByPatchStatus(cve_identifier)
+  @result_list = @rpctest.listSystemsByPatchStatus(cve_identifier) || []
 end
 
 Then /^I should get status "([^\"]+)" for system "([0-9]+)"$/ do |status, system|
-  result = result_list.select {|result| result["system_id"] == system.to_i}
+  @result = @result_list.select {|item| item['system_id'] == system.to_i}
+  refute_empty(@result)
+  @result = @result[0]
+  assert_equal(status, @result['patch_status'])
+end
 
-  fail if result.empty?
-  result = result[0]
-  fail if result["patch_status"] != status
+Then /^I should get status "([^\"]+)" for this client$/ do |status|
+  step "I should get status \"#{status}\" for system \"#{client_system_id_to_i}\""
 end
 
 Then /^I should get the sles11-sp3-updates channel$/ do
@@ -31,15 +31,15 @@ Then /^I should get the sles11-sp3-updates channel$/ do
   else
     channel = "sles11-sp3-updates-x86_64-channel"
   end
-  $stderr.puts "result: #{result}"
-  fail if result["channel_labels"].include?(channel) == false
+  $stderr.puts "result: #{@result}"
+  fail if @result["channel_labels"].include?(channel) == false
 end
 
 Then /^I should get the "([^"]*)" patch$/ do |patch|
-  $stderr.puts "result: #{result}"
-  fail if result["errata_advisories"].include?(patch) == false
+  $stderr.puts "result: #{@result}"
+  fail if @result["errata_advisories"].include?(patch) == false
 end
 
 Then /^I logout from XML\-RPC\/cve audit namespace\.$/ do
-  fail if not rpctest.logout()
+  fail if not @rpctest.logout()
 end
