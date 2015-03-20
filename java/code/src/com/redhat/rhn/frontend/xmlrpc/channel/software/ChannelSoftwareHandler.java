257e273278ec2cbf53c2358ca9738961f77fc193
Johannes Renner <jrenner@suse.de>
2015-03-20 11:59:57 +0100

    Fix parameter names for "insert_into_task_queue"

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/channel/software/ChannelSoftwareHandler.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/channel/software/ChannelSoftwareHandler.java
@@ -1794,8 +1794,8 @@ public class ChannelSoftwareHandler extends BaseHandler {
 
             inParams = new HashMap();
             inParams.put("org_id", org.getId());
-            inParams.put("cid", channel.getId());
-            inParams.put("task_data", "update_errata_cache_by_channel");
+            inParams.put("task_name", "update_errata_cache_by_channel");
+            inParams.put("task_data", channel.getId());
             inParams.put("earliest", new Timestamp(System.currentTimeMillis() + delay));
 
             w.executeUpdate(inParams);
