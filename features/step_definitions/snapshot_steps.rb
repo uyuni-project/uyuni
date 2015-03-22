8abe84ef8c70259fc634e3bf2277b2ddbe438fd9
Martin Seidl <mseidl@suse.de>
2015-03-22 02:15:28 +0100

    Style Updates: tabs

--- features/step_definitions/snapshot_steps.rb
+++ features/step_definitions/snapshot_steps.rb
@@ -1,7 +1,7 @@
 #snapshots
 
 When /^I take a snapshot "([^"]*)"$/ do |name|
-    $sshout = ""
-    $sshout = `echo | ssh -o StrictHostKeyChecking=no root@$VHOST qemu-img create -f qcow -b $IMGDIR/$VMDISK.qcow2 $IMGDIR/#{name}.qcow2` 
-    raise "Creating snapsnot failed..." unless $?.success? 
+  $sshout = ""
+  $sshout = `echo | ssh -o StrictHostKeyChecking=no root@$VHOST qemu-img create -f qcow -b $IMGDIR/$VMDISK.qcow2 $IMGDIR/#{name}.qcow2`
+  raise "Creating snapsnot failed..." unless $?.success?
 end
