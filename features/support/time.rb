a1b411fa1e16c4615dc6a0dd9f8ea324bacb873c
Martin Seidl <mseidl@suse.de>
2015-03-22 11:10:15 +0100

    Style Updates: tabs, copyright

--- features/support/time.rb
+++ features/support/time.rb
@@ -1,4 +1,6 @@
+# Copyright 2015 SUSE LLC
+
 Before do |scenario|
-    current_time = Time.new
-    puts "This scenario ran at: #{current_time}"
+  current_time = Time.new
+  puts "This scenario ran at: #{current_time}"
 end
