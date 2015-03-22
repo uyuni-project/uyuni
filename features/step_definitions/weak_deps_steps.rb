b5c025055a0d87424b28141c6d3015a24b7fe7a7
Martin Seidl <mseidl@suse.de>
2015-03-22 02:17:25 +0100

    Style Updates: tabs

--- features/step_definitions/weak_deps_steps.rb
+++ features/step_definitions/weak_deps_steps.rb
@@ -2,49 +2,48 @@
 # Licensed under the terms of the MIT license.
 
 When /^I refresh the metadata$/ do
-   output = `rhn_check -vvv 2>&1`
-   if ! $?.success?
-       raise "rhn_check failed: #{$!}: #{output}"
-   end
-   client_refresh_metadata
+  output = `rhn_check -vvv 2>&1`
+    if ! $?.success?
+      raise "rhn_check failed: #{$!}: #{output}"
+    end
+  client_refresh_metadata
 end
 
 Then /^I should have '([^']*)' in the metadata$/ do |text|
-   arch=`uname -m`
-   arch.chomp!
-   if arch != "x86_64"
-     arch = "i586"
-   end
-   `zgrep '#{text}' #{client_raw_repodata_dir("sles11-sp3-updates-#{arch}-channel")}/primary.xml.gz`
-   fail if ! $?.success?
+  arch=`uname -m`
+  arch.chomp!
+  if arch != "x86_64"
+    arch = "i586"
+  end
+  `zgrep '#{text}' #{client_raw_repodata_dir("sles11-sp3-updates-#{arch}-channel")}/primary.xml.gz`
+  fail if ! $?.success?
 end
 
 Then /^I should not have '([^']*)' in the metadata$/ do |text|
-   arch=`uname -m`
-   arch.chomp!
-   if arch != "x86_64"
-     arch = "i586"
-   end
-   `zgrep '#{text}' #{client_raw_repodata_dir("sles11-sp3-updates-#{arch}-channel")}/primary.xml.gz`
-   fail if $?.success?
+  arch=`uname -m`
+  arch.chomp!
+  if arch != "x86_64"
+    arch = "i586"
+  end
+  `zgrep '#{text}' #{client_raw_repodata_dir("sles11-sp3-updates-#{arch}-channel")}/primary.xml.gz`
+  fail if $?.success?
 end
 
 Then /^"([^"]*)" should exists in the metadata$/ do |file|
-    arch=`uname -m`
-    arch.chomp!
-    if arch != "x86_64"
-        arch = "i586"
-    end
-    fail if not File.exists?("#{client_raw_repodata_dir("sles11-sp3-updates-#{arch}-channel")}/#{file}")
+  arch=`uname -m`
+  arch.chomp!
+  if arch != "x86_64"
+    arch = "i586"
+  end
+  fail if not File.exists?("#{client_raw_repodata_dir("sles11-sp3-updates-#{arch}-channel")}/#{file}")
 end
 
 Then /^I should have '([^']*)' in the patch metadata$/ do |text|
-   arch=`uname -m`
-   arch.chomp!
-   if arch != "x86_64"
-     arch = "i586"
-   end
-   `zgrep '#{text}' #{client_raw_repodata_dir("sles11-sp3-updates-#{arch}-channel")}/updateinfo.xml.gz`
-   fail if ! $?.success?
+  arch=`uname -m`
+  arch.chomp!
+  if arch != "x86_64"
+    arch = "i586"
+  end
+  `zgrep '#{text}' #{client_raw_repodata_dir("sles11-sp3-updates-#{arch}-channel")}/updateinfo.xml.gz`
+  fail if ! $?.success?
 end
-
