0e4f9799ff0d76e790575814882a44c0440bde33
Martin Seidl <mseidl@suse.de>
2015-03-22 02:11:53 +0100

    Style Updates: tabs, copyright

--- features/step_definitions/ipmi.rb
+++ features/step_definitions/ipmi.rb
@@ -1,12 +1,14 @@
+# COPYRIGHT 2015 SUSE LLC
+
 When /^I setup the ipmi network card$/ do
-    $sshout = `echo | ssh -l root -o StrictHostKeyChecking=no $TESTHOST yast lan add name=eth1 ethdevice=eth1 bootproto=dhcp && ifup eth1 2>&1`
-       if ! $?.success?
-           raise "Unable to change setup network card"
-       end
+  $sshout = `echo | ssh -l root -o StrictHostKeyChecking=no $TESTHOST yast lan add name=eth1 ethdevice=eth1 bootproto=dhcp && ifup eth1 2>&1`
+  if ! $?.success?
+    raise "Unable to change setup network card"
+  end
 end
 
 When /^I should see the power is "([^"]*)"$/ do |arg1|
-    within(:xpath, "//*[@for='powerStatus']/..") do
- 	fail if not has_content?(arg1)
-    end
+  within(:xpath, "//*[@for='powerStatus']/..") do
+    fail if not has_content?(arg1)
+  end
 end
