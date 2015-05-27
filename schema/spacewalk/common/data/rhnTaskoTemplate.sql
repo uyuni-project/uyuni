72ee69c631ac2f37850f38a10befea467d803eef
Silvio Moioli <smoioli@suse.de>
2015-05-27 16:31:35 +0200

    Merge error fixed

--- schema/spacewalk/common/data/rhnTaskoTemplate.sql
+++ schema/spacewalk/common/data/rhnTaskoTemplate.sql
@@ -185,6 +185,7 @@ INSERT INTO rhnTaskoTemplate (id, bunch_id, task_id, ordering, start_if)
 INSERT INTO rhnTaskoTemplate (id, bunch_id, task_id, ordering, start_if)
              VALUES (sequence_nextval('rhn_tasko_template_id_seq'),
                         (SELECT id FROM rhnTaskoBunch WHERE name='auto-errata-bunch'),
+                        (SELECT id FROM rhnTaskoTask WHERE name='auto-errata'),
                         0,
                         null);
 
