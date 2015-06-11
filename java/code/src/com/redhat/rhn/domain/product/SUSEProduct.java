7f2d85e311e1316fffd85b4b44f0d011d686332c
Silvio Moioli <smoioli@suse.de>
2015-06-11 11:44:54 +0200

    SUSEProduct: add proper equals() and hashCode() to prevent Hibernate problems

--- java/code/src/com/redhat/rhn/domain/product/SUSEProduct.java
+++ java/code/src/com/redhat/rhn/domain/product/SUSEProduct.java
@@ -14,10 +14,13 @@
  */
 package com.redhat.rhn.domain.product;
 
-import java.io.Serializable;
-
 import com.redhat.rhn.domain.BaseDomainHelper;
 import com.redhat.rhn.domain.rhnpackage.PackageArch;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.io.Serializable;
 
 /**
  * Class representation of a SUSE product.
@@ -130,5 +133,35 @@ public class SUSEProduct extends BaseDomainHelper implements Serializable {
      */
     public void setProductId(long productIdIn) {
         this.productId = productIdIn;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object otherObject) {
+        if (!(otherObject instanceof SUSEProduct)) {
+            return false;
+        }
+        SUSEProduct other = (SUSEProduct) otherObject;
+        return new EqualsBuilder()
+            .append(getName(), other.getName())
+            .append(getVersion(), other.getVersion())
+            .append(getRelease(), other.getRelease())
+            .append(getArch(), other.getArch())
+            .isEquals();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+            .append(getName())
+            .append(getVersion())
+            .append(getRelease())
+            .append(getArch())
+            .toHashCode();
     }
 }
