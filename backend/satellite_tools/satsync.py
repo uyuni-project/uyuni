ae07a8445a1be0b96a3236971ae62925db593c9f
Michael Calmer <mc@suse.de>
2015-06-24 09:48:27 +0200

    backend: remove local handling of channel family members from satsync

--- backend/satellite_tools/satsync.py
+++ backend/satellite_tools/satsync.py
@@ -632,12 +632,6 @@ Please contact your administrator""") % (generation, sat_cert.generation))
 
         # possible place for bug 146395
 
-        # Populate channel family permissions
-        sync_handlers.populate_channel_family_permissions(sat_cert)
-
-        # Get rid of the extra channel families
-        sync_handlers.purge_extra_channel_families()
-
         if store_cert:
             # store it! (does a commit)
             # XXX bug 146395
