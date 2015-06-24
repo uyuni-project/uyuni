b30beb595a1e157e1ef2df7db5df0a5c24f33882
Frantisek Kobzik <fkobzik@suse.de>
2015-06-24 15:45:42 +0200

    refactor: inline updateCobblerFields() method (used only once)

--- java/code/src/com/redhat/rhn/manager/kickstart/cobbler/CobblerDistroEditCommand.java
+++ java/code/src/com/redhat/rhn/manager/kickstart/cobbler/CobblerDistroEditCommand.java
@@ -17,6 +17,10 @@ package com.redhat.rhn.manager.kickstart.cobbler;
 import com.redhat.rhn.common.validator.ValidatorError;
 import com.redhat.rhn.domain.kickstart.KickstartableTree;
 import com.redhat.rhn.domain.user.User;
+import org.cobbler.CobblerConnection;
+import org.cobbler.Distro;
+
+import java.util.Map;
 
 /**
  * KickstartCobblerCommand - class to contain logic to communicate with cobbler
@@ -47,7 +51,61 @@ public class CobblerDistroEditCommand extends CobblerDistroCommand {
      */
     @Override
     public ValidatorError store() {
-        updateCobblerFields();
+        CobblerConnection con = getCobblerConnection();
+        Distro nonXen = Distro.lookupById(con, tree.getCobblerId());
+        Distro xen = Distro.lookupById(con, tree.getCobblerXenId());
+
+        Map<String, String> ksmeta = createKsMetadataFromTree(this.tree);
+
+        // set architecture (fix 32bit vm's on a 64bit system)
+        // especially for SUSE where the kernel+initrd is under a path that contains
+        // the $arch
+        String archName = tree.getChannel().getChannelArch().getName();
+        if (archName.equals("IA-32")) {
+            archName = "i386";
+        }
+
+        //if the newly edited tree does para virt....
+        if (tree.doesParaVirt()) {
+            //IT does paravirt so we need to either update the xen distro or create one
+            if (xen == null) {
+                xen = Distro.create(con, tree.getCobblerXenDistroName(), tree
+                        .getKernelXenPath(), tree.getInitrdXenPath(), ksmeta, tree
+                        .getInstallType().getCobblerBreed(), tree.getInstallType()
+                        .getCobblerOsVersion(), tree.getChannel().getChannelArch()
+                        .cobblerArch());
+                tree.setCobblerXenId(xen.getId());
+            }
+            else {
+                xen.setArch(archName);
+                xen.setKernel(tree.getKernelXenPath());
+                xen.setInitrd(tree.getInitrdXenPath());
+                xen.setBreed(tree.getInstallType().getCobblerBreed());
+                xen.setOsVersion(tree.getInstallType().getCobblerOsVersion());
+                xen.setKsMeta(ksmeta);
+                xen.setBreed(tree.getInstallType().getCobblerBreed());
+                xen.setArch(tree.getChannel().getChannelArch().cobblerArch());
+                xen.save();
+            }
+        }
+        else {
+            //it doesn't do paravirt, so we need to delete the xen distro
+            if (xen != null) {
+                xen.remove();
+                tree.setCobblerXenId(null);
+            }
+        }
+
+        if (nonXen != null) {
+            nonXen.setArch(archName);
+            nonXen.setInitrd(tree.getInitrdPath());
+            nonXen.setKernel(tree.getKernelPath());
+            nonXen.setBreed(tree.getInstallType().getCobblerBreed());
+            nonXen.setOsVersion(tree.getInstallType().getCobblerOsVersion());
+            nonXen.setKsMeta(ksmeta);
+            nonXen.setArch(tree.getChannel().getChannelArch().cobblerArch());
+            nonXen.save();
+        }
         return null;
     }
 
