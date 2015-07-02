dcda14a72d20aee3726b5e4d4e4682297fae9c5c
Michael Calmer <mc@suse.de>
2015-07-02 12:00:57 +0200

    detect new and old SUSE patch style (bsc#936869)

--- backend/satellite_tools/reposync.py
+++ backend/satellite_tools/reposync.py
@@ -1426,8 +1426,9 @@ def _fix_notice(notice):
     return notice
 
 def _is_old_suse_style(notice):
-    if (notice['from'] and "suse" in notice['from'].lower() and
-        (int(notice['version']) >= 1000 or notice['update_id'].startswith('res'))):
+    if((notice['from'] and "suse" in notice['from'].lower() and
+        int(notice['version']) >= 1000) or
+        (notice['update_id'][:4] in ('res5', 'res6') and int(notice['version']) > 6 )):
         # old style suse updateinfo starts with version >= 1000 or
         # have the res update_tag
         return True
