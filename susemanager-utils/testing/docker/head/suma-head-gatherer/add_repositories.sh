40e159449b3d3cfc2894d3f16007b460982ea5df
Michael Calmer <mc@suse.de>
2015-07-07 18:50:56 +0200

    Docker files for the gatherer

---
+++ susemanager-utils/testing/docker/head/suma-head-gatherer/add_repositories.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12:/GA/standard "SLE12 GA"
+zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12:/Update/standard/ "SLE12 Updates"
+zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12-SP1:/GA/standard "SLE12 SP1 GA"
+zypper ar -f http://download.suse.de/ibs/Devel:/Galaxy:/Manager:/Head/SLE_12_SP1/ "Devel:Galaxy:Manager:Head"
+zypper ar -f http://download.suse.de/ibs/Devel:/Galaxy:/Manager:/Head:/SLE12_Products_Test/SLE_12_SP1/ "Devel:Galaxy:Manager:Head:SLE12_Products_Test"
