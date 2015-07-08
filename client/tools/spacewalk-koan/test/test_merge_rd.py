87975e64d6ba6a3e49b59ccd5225438dba30bc16
Michael Calmer <mc@suse.de>
2015-07-08 09:11:00 +0200

    find correct unittest module

--- client/tools/spacewalk-koan/test/test_merge_rd.py
+++ client/tools/spacewalk-koan/test/test_merge_rd.py
@@ -15,7 +15,10 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-import unittest2 as unittest
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
 import shutil
 import os
 import tempfile
