68582c9f09dead0696d9240b66e135ba93d1f6f6
Johannes Renner <jrenner@suse.de>
2015-07-28 12:25:52 -0600

    Initial version of a UI listing the minions
    This commit also includes initial implementations of:
    
    - localization support
    - handling of CSRF tokens

---
+++ java/code/src/com/suse/manager/webui/Router.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2015 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.suse.manager.webui;
+
+import static com.suse.manager.webui.Spark.get;
+import static spark.Spark.exception;
+
+import com.suse.manager.controllers.MinionsController;
+import com.suse.saltstack.netapi.datatypes.Keys;
+
+import org.apache.log4j.Logger;
+
+import spark.ModelAndView;
+import spark.servlet.SparkApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Router class defining the web UI routes.
+ */
+public class Router implements SparkApplication {
+
+    private static Logger logger = Logger.getLogger(Router.class);
+
+    @Override
+    public void init() {
+
+        // List all minions
+        get("/manager/minions", (request, response) -> {
+            MinionsController controller = new MinionsController();
+            Keys keys = controller.getKeys();
+            Map<String, Object> data = new HashMap<String, Object>() {{
+                put("minions", keys.getMinions());
+                put("unaccepted_minions", keys.getUnacceptedMinions());
+                put("rejected_minions", keys.getRejectedMinions());
+                put("localize", new Localizer());
+            }};
+            return new ModelAndView(data, "minions.jade");
+        });
+
+        exception(RuntimeException.class, (e, request, response) -> {
+            logger.error(e.getMessage(), e);
+            response.status(500);
+            response.body("<h1>Internal Server Error</h1>");
+        });
+    }
+}
---
+++ java/code/src/com/suse/manager/webui/Spark.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2015 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.suse.manager.webui;
+
+import com.redhat.rhn.common.security.CSRFTokenValidator;
+
+import javax.servlet.http.HttpSession;
+
+import spark.TemplateViewRoute;
+import spark.template.jade.JadeTemplateEngine;
+
+public final class Spark {
+
+    /**
+     * Responds to a GET with a jade template
+     *
+     * @param path the path
+     * @param route the route
+     */
+    public static void get(String path, TemplateViewRoute route) {
+        spark.Spark.get(path, (request, response) -> {
+            HttpSession session = request.raw().getSession(true);
+            request.attribute("csrf_token", CSRFTokenValidator.getToken(session));
+            response.type("text/html");
+            return route.handle(request, response);
+        }, new JadeTemplateEngine("com/suse/templates"));
+    }
+}
