6639cc95eccebd9d56182d9ff64f3c00169e8e03
Hubert Mantel <mantel@suse.de>
2015-07-29 13:41:18 +0200

    Timeout when user does not enter credentials for 60 seconds for mgr-sync

--- susemanager/src/helpers.py
+++ susemanager/src/helpers.py
@@ -15,6 +15,10 @@
 
 import getpass
 import re
+from functools import wraps
+import errno
+import os
+import signal
 
 
 def cli_ask(msg, password=False, validator=None):
@@ -45,3 +49,24 @@ def cli_ask(msg, password=False, validator=None):
                 break
 
     return value
+
+class TimeoutError(Exception):
+    pass
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
