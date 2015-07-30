5449e95f045df15c5e35005dc23acb2628f38c3e
Martin Seidl <mseidl@suse.de>
2015-07-30 08:27:39 +0000

    Switched to minitest assertions

--- features/step_definitions/xmlrpctest_steps.rb
+++ features/step_definitions/xmlrpctest_steps.rb
@@ -6,30 +6,25 @@ rpcApiTester = XMLRPCApiTest.new(ENV["TESTHOST"])
 # Steps
 #
 Given /^I am logged in via XML\-RPC\/api as user "([^"]*)" and password "([^"]*)"$/ do |luser, password|
-  fail if not rpcApiTester.login(luser, password)
+  assert(rpcApiTester.login(luser, password))
 end
 
 When /^I call getApiNamespaces, I should get (\d+) namespaces$/ do |apiCount|
-  $stderr.puts "namespaces #{rpcApiTester.getCountOfApiNamespaces()}"
-  fail if apiCount.to_i != rpcApiTester.getCountOfApiNamespaces()
+  assert_equal(apiCount.to_i, rpcApiTester.getCountOfApiNamespaces())
 end
 
 When /^I call getVersion, I should get "([^"]*)" as result$/ do |version|
-  $stderr.puts "version is #{rpcApiTester.getVersion()}"
-  fail if not rpcApiTester.getVersion() == version
+  fail if not rpcApiTester.getVersion().include? version
 end
 
 When /^I call systemVersion, I should get "([^"]*)" as result$/ do |version|
-  $stderr.puts "systemVersion is #{rpcApiTester.systemVersion()}"
-  fail if not rpcApiTester.systemVersion().include? version
+  assert(rpcApiTester.systemVersion().include? version)
 end
 
 When /^I call getApiNamespaceCallList, I should get (\d+) available api calls$/ do |apiCount|
-  $stderr.puts "API count #{rpcApiTester.getCountOfApiNamespaceCallList()}"
-  fail if apiCount.to_i != rpcApiTester.getCountOfApiNamespaceCallList()
+  assert_equal(apiCount.to_i, rpcApiTester.getCountOfApiNamespaceCallList())
 end
 
 When /^I call getApiCallList, I should get (\d+) available groups$/ do |groupCount|
-  $stderr.puts "available groups: #{rpcApiTester.getCountOfApiCallListGroups()}"
-  fail if groupCount.to_i != rpcApiTester.getCountOfApiCallListGroups()
+  assert_equal(groupCount.to_i, rpcApiTester.getCountOfApiCallListGroups())
 end
