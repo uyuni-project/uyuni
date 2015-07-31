5de58ad71a74bdd2ef0a7c0fc68441626f3398a1
Martin Seidl <mseidl@suse.de>
2015-07-31 11:36:16 +0000

    Updated calls to use minitest assertions

--- features/step_definitions/xmlrpc_channel_steps.rb
+++ features/step_definitions/xmlrpc_channel_steps.rb
@@ -1,60 +1,53 @@
 rpctest = XMLRPCChannelTest.new(ENV["TESTHOST"])
 
 Given /^I am logged in via XML\-RPC\/channel as user "([^"]*)" and password "([^"]*)"$/ do |luser, password|
-  fail if not rpctest.login(luser, password)
+  assert(rpctest.login(luser, password))
 end
 
 When /^I create a repo with label "([^"]*)" and url "([^"]*)"$/ do |label, url|
-  ret = rpctest.createRepo(label, url)
-  fail if not ret
+  assert(rpctest.createRepo(label, url))
 end
 
 When /^I associate repo "([^"]*)" with channel "([^"]*)"$/ do |repoLabel, channelLabel|
-  ret = rpctest.associateRepo(channelLabel, repoLabel)
-  fail if not ret
+  assert(rpctest.associateRepo(channelLabel, repoLabel))
 end
 
 When /^I create a channel with label "([^"]*)", name "([^"]*)", summary "([^"]*)", arch "([^"]*)" and parent "([^"]*)"$/ do |label, name, summary, arch, parent|
-  ret = rpctest.create(label, name, summary, arch, parent)
-  fail if ret != 1
+  assert_equal(rpctest.create(label, name, summary, arch, parent), 1)
 end
 
 When /^I delete the software channel with label "([^"]*)"$/ do |label|
-  ret = rpctest.delete(label)
-  fail if ret != 1
+  assert_equal(rpctest.delete(label), 1)
 end
 
 When /^I delete the repo with label "([^"]*)"$/ do |label|
-  ret = rpctest.deleteRepo(label)
-  fail if ret != 1
+  assert_equal(rpctest.deleteRepo(label), 1)
 end
 
 Then /^something should get listed with a call of listSoftwareChannels$/ do
-  fail if rpctest.getSoftwareChannelsCount() < 1
+  assert_equal(rpctest.getSoftwareChannelsCount() < 1, false)
 end
 
 Then /^"([^"]*)" should get listed with a call of listSoftwareChannels$/ do |label|
-  fail if not rpctest.verifyChannel(label)
+  assert(rpctest.verifyChannel(label))
 end
 
 Then /^"([^"]*)" should not get listed with a call of listSoftwareChannels$/ do |label|
-  fail if rpctest.verifyChannel(label)
+  assert_equal(rpctest.verifyChannel(label), false)
 end
 
 Then /^"([^"]*)" should be the parent channel of "([^"]*)"$/ do |parent, child|
-  fail if not rpctest.isParentChannel(child, parent)
+  assert(rpctest.isParentChannel(child, parent))
 end
 
 Then /^channel "([^"]*)" should have attribute "([^"]*)" from type "([^"]*)"$/ do |label, attr, type|
   ret = rpctest.getChannelDetails(label)
-  fail if not ret
-  c = ret[attr].class.to_s
-  #print "CLASS: #{c}\n"
-  fail if c != type
+  assert(ret)
+  assert_equal(ret[attr].class.to_s, type)
 end 
 
 Then /^channel "([^"]*)" should not have attribute "([^"]*)"$/ do |label, attr|
   ret = rpctest.getChannelDetails(label)
-  fail if not ret
-  fail if ret.has_key?(attr)
+  assert(ret)
+  assert_equal(ret.has_key?(attr), false)
 end 
