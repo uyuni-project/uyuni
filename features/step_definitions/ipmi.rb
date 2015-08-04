a27b31115e2ff71f683ae1e85d5ac3f78a174a46
Michael Calmer <mc@suse.de>
2015-08-04 09:56:36 +0200

    Do network interface configuration in two steps

--- features/step_definitions/ipmi.rb
+++ features/step_definitions/ipmi.rb
@@ -1,9 +1,13 @@
 # COPYRIGHT 2015 SUSE LLC
 
 When /^I setup the ipmi network card$/ do
-  $sshout = `echo | ssh -l root -o StrictHostKeyChecking=no $TESTHOST TERM=xterm yast lan add name=eth1 ethdevice=eth1 bootproto=dhcp && ifup eth1 2>&1`
+  $sshout = `echo | ssh -l root -o StrictHostKeyChecking=no $TESTHOST TERM=xterm yast lan add name=eth1 ethdevice=eth1 bootproto=dhcp 2>&1`
   if ! $?.success?
-    raise "Unable to change setup network card"
+    raise "Unable to change setup network card: #{$sshout}"
+  end
+  $sshout = `echo | ssh -l root -o StrictHostKeyChecking=no $TESTHOST TERM=xterm ifup eth1 2>&1`
+  if ! $?.success?
+    raise "Unable to bring up the network interface: #{$sshout}"
   end
 end
 
