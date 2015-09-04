009bed37916c2317cfec08990ab6437cb3454109
Johannes Renner <jrenner@suse.de>
2015-09-04 12:24:14 +0200

    Configure message queue from the SaltReactor.start() method

--- java/code/src/com/redhat/rhn/common/messaging/MessageQueue.java
+++ java/code/src/com/redhat/rhn/common/messaging/MessageQueue.java
@@ -49,10 +49,6 @@ import com.redhat.rhn.frontend.events.TraceBackAction;
 import com.redhat.rhn.frontend.events.TraceBackEvent;
 import com.redhat.rhn.frontend.events.UpdateErrataCacheAction;
 import com.redhat.rhn.frontend.events.UpdateErrataCacheEvent;
-
-import com.suse.manager.reactor.RegisterMinionAction;
-import com.suse.manager.reactor.RegisterMinionEvent;
-
 import com.redhat.rhn.frontend.events.SsmInstallPackagesAction;
 import com.redhat.rhn.frontend.events.SsmInstallPackagesEvent;
 import com.redhat.rhn.frontend.events.SsmSystemRebootAction;
@@ -319,10 +315,6 @@ public class MessageQueue {
         // Deploy configuration files
         MessageQueue.registerAction(new SsmConfigFilesAction(),
                                     SsmConfigFilesEvent.class);
-
-        // Create system records for salt minions
-        MessageQueue.registerAction(new RegisterMinionAction(),
-                RegisterMinionEvent.class);
     }
 }
 
