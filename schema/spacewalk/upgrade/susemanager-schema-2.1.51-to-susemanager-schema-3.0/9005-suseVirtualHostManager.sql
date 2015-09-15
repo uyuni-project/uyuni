2246f0f6878352ded79636eb138032986d605812
Frantisek Kobzik <fkobzik@suse.de>
2015-09-15 13:58:36 +0200

    db: Add migrations for Virtual Host Manager

---
+++ schema/spacewalk/upgrade/susemanager-schema-2.1.51-to-susemanager-schema-3.0/9005-suseVirtualHostManager.sql
@@ -0,0 +1,34 @@
+--
+-- Copyright (c) 2015 SUSE LLC
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+
+CREATE TABLE suseVirtualHostManager
+(
+    id          NUMBER NOT NULL
+                    CONSTRAINT suse_vhms_id_pk PRIMARY KEY,
+    org_id      NUMBER NOT NULL
+                    CONSTRAINT suse_vhms_oid_fk
+                    REFERENCES web_customer (id)
+                    ON DELETE CASCADE,
+    gatherer_module     VARCHAR2(50) NOT NULL,
+    cred_id     NUMBER
+                    CONSTRAINT suse_vhms_creds_fk
+                    REFERENCES suseCredentials (id)
+                    ON DELETE SET NULL,
+    created     timestamp with local time zone
+                    DEFAULT (current_timestamp) NOT NULL,
+    modified    timestamp with local time zone
+                    DEFAULT (current_timestamp) NOT NULL
+)
+ENABLE ROW MOVEMENT
+;
+
+CREATE SEQUENCE suse_vhms_id_seq;
+
