3921fc6ba523fd48b5821dbcb76fdca8a52efe6f
Michael Calmer <mc@suse.de>
2015-09-17 17:12:00 +0200

    checkstyle

--- java/code/src/com/suse/manager/gatherer/GathererCache.java
+++ java/code/src/com/suse/manager/gatherer/GathererCache.java
@@ -1,7 +1,21 @@
+/**
+ * Copyright (c) 2015 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
 package com.suse.manager.gatherer;
 
 import com.suse.manager.model.gatherer.GathererModule;
-
 import java.util.Map;
 import java.util.Set;
 
@@ -16,7 +30,7 @@ public enum GathererCache {
     private Map<String, GathererModule> gathererModules =
             new GathererCommand().listModules();
     private Long cacheCreated = System.currentTimeMillis();
-    private long CACHE_LIFETIME = 5*60*1000;
+    private long CACHE_LIFETIME = 5 * 60 * 1000;
 
     /**
      * List the currently available modules of the gatherer
--- java/code/src/com/suse/manager/gatherer/GathererCommand.java
+++ java/code/src/com/suse/manager/gatherer/GathererCommand.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2015 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
 package com.suse.manager.gatherer;
 
 import com.redhat.rhn.manager.satellite.Executor;
@@ -17,7 +32,7 @@ import java.util.Map;
  */
 public class GathererCommand {
 
-    private static String GATHERER_CMD = "/usr/bin/gatherer";
+    private static final String GATHERER_CMD = "/usr/bin/gatherer";
 
     /**
      * Logger for this class
