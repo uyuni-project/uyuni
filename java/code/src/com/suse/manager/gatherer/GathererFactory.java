27f1c319fc2fb1455e7f72547152f93d59a1e0c8
Michael Calmer <mc@suse.de>
2015-09-17 15:00:21 +0200

    implement cache refreshing

--- java/code/src/com/suse/manager/gatherer/GathererFactory.java
+++ java/code/src/com/suse/manager/gatherer/GathererFactory.java
@@ -11,13 +11,26 @@ public enum GathererFactory {
     /** Singleton instance. */
     INSTANCE;
 
-    private final Map<String, GathererModule> gathererModules = new GathererCommand().listModules();
+    private final Map<String, GathererModule> gathererModules =
+            new GathererCommand().listModules();
+    private static Long created = System.currentTimeMillis();
 
     public Set<String> listModules() {
+        updateCacheIfNeeded(false);
         return gathererModules.keySet();
     }
 
     public GathererModule getDetails(String moduleName) {
+        updateCacheIfNeeded(false);
         return gathererModules.get(moduleName);
+    }
+
+    private void updateCacheIfNeeded(boolean force) {
+        if(force || System.currentTimeMillis() - created > 5*60*60*1000) {
+            // cache is more than 5 minutes old => refresh
+            gathererModules.clear();
+            gathererModules.putAll(new GathererCommand().listModules());
+            created = System.currentTimeMillis();
+        }
     }
 }
