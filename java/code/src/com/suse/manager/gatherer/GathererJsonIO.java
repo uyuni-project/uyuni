d22b3b142e0659bd4f6fcd158921d4101f7792b6
Michael Calmer <mc@suse.de>
2015-09-22 16:54:29 +0200

    Serializer for VirtualHostManager and deserialize generic objects

--- java/code/src/com/suse/manager/gatherer/GathererJsonIO.java
+++ java/code/src/com/suse/manager/gatherer/GathererJsonIO.java
@@ -15,6 +15,9 @@
 
 package com.suse.manager.gatherer;
 
+import com.redhat.rhn.domain.server.virtualhostmanager.VirtualHostManager;
+import com.redhat.rhn.domain.server.virtualhostmanager.VirtualHostManagerConfig;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
@@ -25,6 +28,7 @@ import com.google.gson.stream.JsonWriter;
 import com.suse.manager.model.gatherer.GathererModule;
 
 import java.io.IOException;
+import java.io.Reader;
 import java.util.Map;
 
 /**
@@ -40,6 +44,7 @@ public class GathererJsonIO {
         gson = new GsonBuilder()
             .setPrettyPrinting()
             .registerTypeAdapter(GathererModule.class, new GathererModuleAdapter())
+            .registerTypeAdapter(VirtualHostManager.class, new VHMAdapter())
             .create();
     }
 
@@ -54,6 +59,28 @@ public class GathererJsonIO {
             throws JsonSyntaxException {
         return  gson.fromJson(reader,
                 new TypeToken<Map<String, GathererModule>>() { }.getType());
+    }
+
+    /**
+     * Read Hosts from JSON.
+     *
+     * @param reader the reader object
+     * @return the list
+     * @throws JsonSyntaxException in case JSON does not have correct syntax
+     */
+    public Map<String, Map<String, JsonHost>> readHosts(Reader reader)
+            throws JsonSyntaxException {
+        return  gson.fromJson(reader,
+                new TypeToken<Map<String, Map<String, JsonHost>>>() { }.getType());
+    }
+
+    /**
+     * Write Object to Json
+     * @param o an object
+     * @return the corresponding JSON string
+     */
+    public String toJson(Object o) {
+        return gson.toJson(o);
     }
 
     /**
@@ -85,6 +112,32 @@ public class GathererJsonIO {
             writer.name("module").value(value.getName());
             for (Map.Entry<String, String> e : value.getParameters().entrySet()) {
                 writer.name(e.getKey()).value(e.getValue());
+            }
+            writer.endObject();
+        }
+    }
+
+    /**
+     * (De)serializer for GathererModule class.
+     * Breaks the incoming arguments into module and parameter list
+     * and fills a new GathererModule instance.
+     */
+    public class VHMAdapter extends TypeAdapter<VirtualHostManager> {
+        @Override
+        public VirtualHostManager read(JsonReader reader) throws IOException {
+            return null;
+        }
+
+        @Override
+        public void write(JsonWriter writer, VirtualHostManager value) throws IOException {
+            writer.beginObject();
+            writer.name("module").value(value.getGathererModule());
+            if (value.getCredentials() != null) {
+                writer.name("user").value(value.getCredentials().getUsername());
+                writer.name("pass").value(value.getCredentials().getPassword());
+            }
+            for (VirtualHostManagerConfig c : value.getConfigs()) {
+                writer.name(c.getParameter()).value(c.getValue());
             }
             writer.endObject();
         }
