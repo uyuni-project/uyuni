a207b72383663ae8ea3978a227e30064a39a5adf
Johannes Renner <jrenner@suse.com>
2015-09-23 17:27:21 +0200

    New version of the test based on RhnJmockBaseTestCase

--- java/code/src/com/suse/manager/reactor/test/RegisterMinionActionTest.java
+++ java/code/src/com/suse/manager/reactor/test/RegisterMinionActionTest.java
@@ -16,32 +16,38 @@ package com.suse.manager.reactor.test;
 
 import com.redhat.rhn.domain.server.Server;
 import com.redhat.rhn.domain.server.ServerFactory;
-import com.redhat.rhn.testing.RhnBaseTestCase;
+import com.redhat.rhn.testing.RhnJmockBaseTestCase;
 import com.redhat.rhn.testing.TestUtils;
 
 import com.suse.manager.reactor.RegisterMinionAction;
 import com.suse.manager.reactor.RegisterMinionEvent;
 import com.suse.manager.webui.services.SaltService;
-import com.suse.saltstack.netapi.calls.wheel.Key.Names;
-import com.suse.saltstack.netapi.event.EventStream;
+
+import org.jmock.Mock;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
  * Tests for {@link RegisterMinionAction}.
  */
-public class RegisterMinionActionTest extends RhnBaseTestCase {
+public class RegisterMinionActionTest extends RhnJmockBaseTestCase {
+
+    private Map<String, String> machineIds = new HashMap<>();
 
     /**
      * Test the minion registration.
      */
     public void testDoExecute() {
-        // Register a minion
-        SaltService saltService = new SaltServiceStub();
-        RegisterMinionAction action = new RegisterMinionAction(saltService) {};
+        // Register a minion via RegisterMinionAction and mocked SaltService
+        Mock saltServiceMock = mock(SaltService.class);
         String minionId = TestUtils.randomString();
+        saltServiceMock.stubs().method("getMachineId").with(eq(minionId)).will(
+                returnValue(getMachineId(minionId)));
+        saltServiceMock.stubs().method("getGrains").with(eq(minionId)).will(
+                returnValue(getGrains(minionId)));
+        SaltService saltService = (SaltService) saltServiceMock.proxy();
+        RegisterMinionAction action = new RegisterMinionAction(saltService) {};
         action.doExecute(new RegisterMinionEvent(minionId));
 
         // Verify the resulting system entry
@@ -61,61 +67,21 @@ public class RegisterMinionActionTest extends RhnBaseTestCase {
                 minion.getContactMethod());
     }
 
-    /**
-     * Stubbed out version of {@link SaltService} for testing.
-     */
-    private class SaltServiceStub implements SaltService {
+    private String getMachineId(String minionId) {
+        if (!machineIds.containsKey(minionId)) {
+            machineIds.put(minionId, TestUtils.randomString());
+        }
+        return machineIds.get(minionId);
+    }
 
-        private Map<String, String> machineIds = new HashMap<>();
-
-        @Override
-        public Map<String, Object> getGrains(String minionId) {
-            Map<String, Object> grains = new HashMap<>();
-            grains.put("kernelrelease", "3.12.47-2-default");
-            grains.put("machine_id", getMachineId(minionId));
-            grains.put("mem_total", 477.0);
-            grains.put("os", "SLES");
-            grains.put("osfullname", "SLES");
-            grains.put("osrelease", "12");
-            return grains;
-        }
-
-        @Override
-        public String getMachineId(String minionId) {
-            if (!machineIds.containsKey(minionId)) {
-                machineIds.put(minionId, TestUtils.randomString());
-            }
-            return machineIds.get(minionId);
-        }
-
-        // Unsupported operations
-        @Override
-        public Names getKeys() {
-            throw new UnsupportedOperationException();
-        }
-        @Override
-        public List<String> present() {
-            throw new UnsupportedOperationException();
-        }
-        @Override
-        public Map<String, List<String>> getPackages(String minionId) {
-            throw new UnsupportedOperationException();
-        }
-        @Override
-        public void acceptKey(String minionId) {
-            throw new UnsupportedOperationException();
-        }
-        @Override
-        public void deleteKey(String minionId) {
-            throw new UnsupportedOperationException();
-        }
-        @Override
-        public void rejectKey(String minionId) {
-            throw new UnsupportedOperationException();
-        }
-        @Override
-        public EventStream getEventStream() {
-            throw new UnsupportedOperationException();
-        }
+    private Map<String, Object> getGrains(String minionId) {
+        Map<String, Object> grains = new HashMap<>();
+        grains.put("kernelrelease", "3.12.47-2-default");
+        grains.put("machine_id", getMachineId(minionId));
+        grains.put("mem_total", 477.0);
+        grains.put("os", "SLES");
+        grains.put("osfullname", "SLES");
+        grains.put("osrelease", "12");
+        return grains;
     }
 }
