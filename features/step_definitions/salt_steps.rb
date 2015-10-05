a29465e9d720dea0945b53706da5ab84aa317d43
Johannes Renner <jrenner@suse.com>
2015-10-05 17:51:54 +0200

    Revisit salt.feature and step definitions
    This patch is supposed to:
    
    - make the feature actually succeed
    - give better names to scenarios and steps
    - get rid of obsolete step definitions
    - get rid of unnecessary sleep() calls

--- features/step_definitions/salt_steps.rb
+++ features/step_definitions/salt_steps.rb
@@ -1,24 +1,18 @@
 # Copyright 2015 SUSE LLC
 
-Given(/^this client hostname$/) do
-  @this_client_hostname = `hostname -f`.chomp
-end
-Given(/^the salt-minion is configured$/) do
+Given(/^the Salt Minion is configured$/) do
   File.rename("/etc/salt/minion", "/etc/salt/minion.orig")
   File.open("/etc/salt/minion", 'w') { |file| file.write("master: #{ENV['TESTHOST']}") }
-  step %[I restart Salt Minion]
+  step %[I restart salt-minion]
 end
 
-When(/^I get a content of a file "(.*?)"$/) do |filename|
+When(/^I get the contents of the remote file "(.*?)"$/) do |filename|
   @output = sshcmd("cat #{filename}")
 end
 
-When(/^I delete key of this client$/) do
+When(/^I delete the key of this client$/) do
+  @this_client_hostname = `hostname -f`.chomp
   sshcmd("yes | salt-key -d #{@this_client_hostname}")
-  system("rcsalt-minion restart")
-  puts "Waiting for the longest RSA key re-issue (10 secons)"
-  # Longest key re-issue is 10 seconds. We sleep here 15.
-  sleep(15)
 end
 
 When(/^I remove possible Salt Master key "(.*?)"$/) do |filename|
@@ -28,14 +22,12 @@ When(/^I remove possible Salt Master key "(.*?)"$/) do |filename|
   end
 end
 
-When(/^I restart Salt Minion$/) do
-  system("rcsalt-minion stop")
-  system("rcsalt-minion start")
-  sleep(2)
+When(/^I restart salt-minion$/) do
+  system("systemctl restart salt-minion")
 end
 
 Then(/^the Salt Minion should be running$/) do
-  out = `rcsalt-minion status`
+  out = `systemctl status salt-minion`
   unless $?.success?
     raise "salt-minion status: #{out}"
   end
@@ -49,7 +41,8 @@ When(/^I list accepted keys at Salt Master$/) do
   @output = sshcmd("salt-key --list accepted")
 end
 
-Then(/^the list of the keys should contain this client hostname$/) do
+Then(/^the list of the keys should contain this client's hostname$/) do
+  @this_client_hostname = `hostname -f`.chomp
   fail if not @output[:stdout].include? @this_client_hostname
 end
 
@@ -57,27 +50,19 @@ When(/^I accept all Salt unaccepted keys$/) do
   sshcmd("yes | salt-key -A")
 end
 
-When(/^I ping client machine from the Master$/) do
-  @output = sshcmd("salt #{@this_client_hostname} test.ping")
-end
-
-When(/^I get OS information of the client machine from the Master$/) do
-  sleep(15)
+When(/^I get OS information of the Minion from the Master$/) do
+  @this_client_hostname = `hostname -f`.chomp
   @output = sshcmd("salt #{@this_client_hostname} grains.get os")
 end
 
-Then(/^it should contain "(.*?)" text$/) do |content|
+Then(/^it should contain a "(.*?)" text$/) do |content|
   fail if not @output[:stdout].include? content
 end
 
-Then(/^it should contain testsuite hostname$/) do
-  fail if not @output[:stdout].include? @this_client_hostname
-end
-
-Then(/^the Salt rest\-api should be listening on local port (\d+)$/) do |port|
+Then(/^salt\-api should be listening on local port (\d+)$/) do |port|
   fail if not sshcmd("ss -nta | grep #{port}")[:stdout].include? "127.0.0.1:#{port}"
 end
 
-Then(/^the salt\-master should be listening on public port (\d+)$/) do |port|
+Then(/^salt\-master should be listening on public port (\d+)$/) do |port|
   fail if not sshcmd("ss -nta | grep #{port}")[:stdout].include? "*:#{port}"
 end
