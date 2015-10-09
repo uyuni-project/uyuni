8035c8885edb5156c2afc200371f6f5b9164f59b
Frantisek Kobzik <fkobzik@suse.de>
2015-10-09 08:36:47 +0200

    Taskomatic job for running gatherer

---
+++ java/code/src/com/redhat/rhn/taskomatic/task/gatherer/GathererJob.java
@@ -0,0 +1,54 @@
+package com.redhat.rhn.taskomatic.task.gatherer;
+
+import com.redhat.rhn.common.hibernate.HibernateFactory;
+import com.redhat.rhn.domain.server.virtualhostmanager.VirtualHostManager;
+import com.redhat.rhn.domain.server.virtualhostmanager.VirtualHostManagerFactory;
+import com.redhat.rhn.taskomatic.task.RhnJavaJob;
+import com.suse.manager.gatherer.GathererCommand;
+import com.suse.manager.gatherer.JsonHost;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Taskomatic job for running gatherer on all Virtual Host Managers and processing its
+ * results.
+ */
+public class GathererJob extends RhnJavaJob {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext)
+            throws JobExecutionException {
+        List<VirtualHostManager> managers
+                = VirtualHostManagerFactory.getInstance().listVirtualHostManagers();
+        if (managers == null || managers.isEmpty()) {
+            log.debug("No Virtual Host Managers to run the gatherer job");
+            return;
+        }
+
+        log.debug(String.format("Running gatherer for %d Virtual Host Managers",
+                managers.size()));
+
+        try {
+            Map<String, Map<String, JsonHost>> results = new GathererCommand().run(managers);
+            log.debug(String.format("Got %d Virtual Host Managers from gatherer",
+                    results.size()));
+
+            managers.stream()
+                    .filter(manager -> results.containsKey(manager))
+                    .forEach(manager -> {
+                        String label = manager.getLabel();
+                        log.debug("Processing " + label);
+                        new VirtualHostManagerProcessor(manager, results.get(label))
+                                .processMapping();
+                    });
+        } catch (Throwable t) {
+            log.error(t.getMessage(), t);
+            HibernateFactory.rollbackTransaction();
+        } finally {
+            HibernateFactory.closeSession();
+        }
+    }
+}
