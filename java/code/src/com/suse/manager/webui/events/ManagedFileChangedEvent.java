04f7d2423c519c581b017805410c0d4ea16268cb
Johannes Renner <jrenner@suse.com>
2015-10-12 10:25:52 +0200

    Add documentation in event classes

--- java/code/src/com/suse/manager/webui/events/ManagedFileChangedEvent.java
+++ java/code/src/com/suse/manager/webui/events/ManagedFileChangedEvent.java
@@ -1,7 +1,24 @@
+/**
+ * Copyright (c) 2015 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package com.suse.manager.webui.events;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Event to signalize that a managed file has been changed on a minion.
+ */
 public class ManagedFileChangedEvent implements Event {
 
     @SerializedName("$type")
@@ -25,6 +42,8 @@ public class ManagedFileChangedEvent implements Event {
     }
 
     /**
+     * Get the minion id.
+     *
      * @return the minion id
      */
     public String getMinionId() {
@@ -32,6 +51,8 @@ public class ManagedFileChangedEvent implements Event {
     }
     
     /**
+     * Get the path of the managed file that has been changed.
+     *
      * @return the path
      */
     public String getPath() {
@@ -39,6 +60,8 @@ public class ManagedFileChangedEvent implements Event {
     }
 
     /**
+     * Get the diff of the change.
+     *
      * @return the diff
      */
     public String getDiff() {
