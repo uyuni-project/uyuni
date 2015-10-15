930976f20266f878e9a157841eaee17e9d0ca78c
Frantisek Kobzik <fkobzik@suse.de>
2015-10-15 09:45:55 +0200

    Fix license header

--- schema/spacewalk/upgrade/susemanager-schema-2.1.51-to-susemanager-schema-3.0/9009-add-gatherer-cred-type.sql
+++ schema/spacewalk/upgrade/susemanager-schema-2.1.51-to-susemanager-schema-3.0/9009-add-gatherer-cred-type.sql
@@ -1,5 +1,5 @@
 --
--- Copyright (c) 2014 Novell
+-- Copyright (c) 2015 SUSE LLC
 --
 -- This software is licensed to you under the GNU General Public License,
 -- version 2 (GPLv2). There is NO WARRANTY for this software, express or
@@ -7,10 +7,6 @@
 -- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
 -- along with this software; if not, see
 -- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
---
--- Red Hat trademarks are not licensed under GPLv2. No permission is
--- granted to use or replicate Red Hat trademarks that are incorporated
--- in this software or its documentation.
 --
 
 insert into suseCredentialsType (id, label, name) (
