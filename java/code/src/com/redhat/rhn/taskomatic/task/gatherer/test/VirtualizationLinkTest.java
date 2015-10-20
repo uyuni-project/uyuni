324a2ebbcb39024cee97e2a3b2c06e1f2d5d21eb
Silvio Moioli <smoioli@suse.de>
2015-10-20 14:22:58 +0200

    VirtualizationLinkTest: style fixes

--- java/code/src/com/redhat/rhn/taskomatic/task/gatherer/test/VirtualizationLinkTest.java
+++ java/code/src/com/redhat/rhn/taskomatic/task/gatherer/test/VirtualizationLinkTest.java
@@ -27,7 +27,8 @@ public class VirtualizationLinkTest extends BaseTestCaseWithUser {
      * @throws Exception - if anything goes wrong
      */
     public void testLinking() throws Exception {
-        FileReader fr = new FileReader(TestUtils.findTestData("gatherer.out.json").getPath());
+        FileReader fr =
+                new FileReader(TestUtils.findTestData("gatherer.out.json").getPath());
         Map<String, Map<String, JSONHost>> hosts = new GathererJsonIO().readHosts(fr);
 
         VirtualHostManager manager = new VirtualHostManager();
@@ -53,8 +54,10 @@ public class VirtualizationLinkTest extends BaseTestCaseWithUser {
         assertEquals(5, server2.getGuests().size());
 
         Map<String, String> vms = new HashMap<>();
-        vms.put("49737e0a-c9e6-4ceb-aef8-6a9452f67cb5", "4230c60f-3f98-2a65-f7c3-600b26b79c22");
-        vms.put("5a2e4e63-a957-426b-bfa8-4169302e4fdb", "42307b15-1618-0595-01f2-427ffcddd88e");
+        vms.put("49737e0a-c9e6-4ceb-aef8-6a9452f67cb5",
+                "4230c60f-3f98-2a65-f7c3-600b26b79c22");
+        vms.put("5a2e4e63-a957-426b-bfa8-4169302e4fdb",
+                "42307b15-1618-0595-01f2-427ffcddd88e");
         vms.put("NSX-gateway", "4230d43e-aafe-38ba-5a9e-3cb67c03a16a");
         vms.put("NSX-l3gateway", "4230b00f-0b21-0e9d-dfde-6c7b06909d5f");
         vms.put("NSX-service", "4230e924-b714-198b-348b-25de01482fd9");
