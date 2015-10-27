3daac2220dd12fee2968c96669a0c0a1c9e64df4
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-10-27 23:18:49 +0100

    implement token verification

--- java/code/src/com/suse/manager/webui/controllers/DownloadController.java
+++ java/code/src/com/suse/manager/webui/controllers/DownloadController.java
@@ -14,10 +14,19 @@
  */
 package com.suse.manager.webui.controllers;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.redhat.rhn.common.conf.Config;
 import com.redhat.rhn.domain.rhnpackage.*;
 import com.redhat.rhn.domain.rhnpackage.Package;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.MalformedClaimException;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
+import org.jose4j.keys.AesKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Request;
@@ -29,12 +38,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.security.Key;
+import java.util.List;
+import java.util.Set;
 
 
 public class DownloadController {
 
     private static final int BUF_SIZE = 4096;
     private static final Logger LOG = LoggerFactory.getLogger(Request.class);
+    private static final Gson GSON = new GsonBuilder().create();
 
     private DownloadController() {
     }
@@ -51,6 +64,43 @@ public class DownloadController {
         rest = StringUtils.substringBeforeLast(rest, "-");
         String version = StringUtils.substringAfterLast(rest, "-");
         String name = StringUtils.substringBeforeLast(rest, "-");
+
+        Set<String> queryParams = request.queryParams();
+        if (queryParams.size() < 1) {
+            halt(403, String.format("You need a token to access %s in %s", filename, channel));
+            return null;
+        }
+
+        if (queryParams.size() > 1) {
+            halt(400, "Only one token is accepted");
+            return null;
+        }
+
+        String token = queryParams.iterator().next();
+        Key key = new AesKey(Config.get().getString("server.secret_key").getBytes());
+
+        JwtConsumer jwtConsumer = new JwtConsumerBuilder()
+                //.setRequireExpirationTime()
+                //.setAllowedClockSkewInSeconds(30)
+                //.setRequireSubject()
+                //.setExpectedIssuer("Issuer")
+                //.setExpectedAudience("Audience")
+                .setVerificationKey(key)
+                .build();
+
+        try {
+            JwtClaims jwtClaims = jwtConsumer.processToClaims(token);
+
+            List<String> channels = jwtClaims.getStringListClaimValue("channels");
+            if (!channels.contains(channel)) {
+                halt(403, String.format("Token is not provide access to channel %s", channel));
+                return null;
+            }
+        }
+        catch (InvalidJwtException|MalformedClaimException e) {
+            halt(403, String.format("Token is not valid to access %s in %s: %s", filename, channel, e.getMessage()));
+            return null;
+        }
 
         Package pkg = PackageFactory.lookupByChannelLabelNevra(channel, name, version, release, null, arch);
         if (pkg == null) {
