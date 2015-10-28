b8427157b182335baf54ead66a61aca7a77e4ab8
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-10-29 00:09:08 +0100

    implements getting the token via a web page

--- web/html/javascript/manager/tokens.js
+++ web/html/javascript/manager/tokens.js
@@ -1,11 +1,37 @@
 'use strict';
 
 $(function() {
-  $('#channel-list').select2()
+  new Clipboard('button#copy-token');
+  $('#error').hide();
   $('#orgaccess').prop('checked', true);
+  $('#channel-list').attr('disabled', true);
 
   $('#orgaccess').change(function() {
-    $('#channels-group').prop('disabled', $('#orgaccess').is(':checked'));
+    $('#channel-list').attr('disabled', $('#orgaccess').is(':checked'));
   });
 
+  $('#channel-list').select2()
+
+  $('button#create').click(function() {
+     $('#error').hide();
+     var params = {};
+     if ($('#orgaccess').is(':checked')) {
+       var orgid = $('#orgid').val();
+       params['orgid'] = orgid;
+     } else {
+       var channels = $('#channel-list').val() || [];
+       if (channels.length > 0) {
+         params['channels'] = encodeURIComponent(channels.join(','));
+       }
+     }
+
+     var xhr = $.get('/rhn/manager/api/tokens/create', params)
+     xhr.done(function(data) {
+       $('input#token').val(data);
+     })
+     xhr.fail(function() {
+       $('#error').show();
+       $('input#token').val('');
+     });
+  });
 });
