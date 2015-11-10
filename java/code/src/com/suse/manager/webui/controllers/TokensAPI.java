4b6bf5bbf3fe7683d19ce858ce07626d18ed1fec
Johannes Renner <jrenner@suse.com>
2015-11-10 13:38:32 +0100

    Remove the tokens API to re-introduce it in a separate branch

--- java/code/src/com/suse/manager/webui/controllers/TokensAPI.java
+++ java/code/src/com/suse/manager/webui/controllers/TokensAPI.java
@@ -14,40 +14,24 @@
  */
 package com.suse.manager.webui.controllers;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.redhat.rhn.common.conf.Config;
-import com.redhat.rhn.common.localization.LocalizationService;
-import com.redhat.rhn.domain.channel.Channel;
-import com.redhat.rhn.domain.channel.ChannelFactory;
-import com.redhat.rhn.domain.user.User;
 import com.suse.manager.webui.utils.TokenUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.jose4j.jwe.ContentEncryptionAlgorithmIdentifiers;
 import org.jose4j.jwe.JsonWebEncryption;
 import org.jose4j.jwe.KeyManagementAlgorithmIdentifiers;
 import org.jose4j.jwt.JwtClaims;
 import org.jose4j.lang.JoseException;
-import spark.Request;
-import spark.Response;
 
 import java.security.Key;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
-
-import static spark.Spark.halt;
 
 /**
- * Provides a programmatically way to generate channel access tokens and
- * also exposes it as an http endpoint.
+ * Provides a programmatic way to generate channel access tokens.
  */
 public class TokensAPI {
 
-    private static final Gson GSON = new GsonBuilder().create();
     private final static int YEAR_IN_MINUTES = 525600;
     private final static int NOT_BEFORE_MINUTES = 2;
 
@@ -97,51 +81,5 @@ public class TokensAPI {
             throw new RuntimeException("Server has no secret key");
         }
         return createTokenWithKey(TokenUtils.getServerKey(), orgId, channels);
-    }
-
-    /**
-     * API endpoint to create a token for a given org or channel set
-     * @param request the request object
-     * @param response the response object
-     * @return json result of the API call
-     */
-    public static String create(Request request, Response response, User user) {
-        JwtClaims claims = new JwtClaims();
-        claims.setExpirationTimeMinutesInTheFuture(YEAR_IN_MINUTES);
-        claims.setIssuedAtToNow();
-        claims.setNotBeforeMinutesInThePast(NOT_BEFORE_MINUTES);
-
-        try {
-            Optional<Long> orgId =
-                    Optional.ofNullable(request.queryParams("orgid"))
-                            .filter(StringUtils::isNotBlank)
-                            .map(Long::parseLong);
-
-            Set<String> channels = Optional.ofNullable(request.queryParams("channels"))
-                            .map(str -> StringUtils.split(str, ","))
-                            .map(arr -> new HashSet<String>(Arrays.asList(arr)))
-                            .orElse(new HashSet<String>());
-
-            // check that the user has access to those channels
-            Set<String> orgChannels =
-                    ChannelFactory.getAccessibleChannelsByOrg(user.getOrg().getId())
-                            .stream()
-                            .map(Channel::getLabel)
-                            .collect(Collectors.toCollection(HashSet::new));
-
-            // remove all channels not in the current user accessible
-            // channels list
-            boolean anyNotAuthorized = channels.retainAll(orgChannels);
-            if (anyNotAuthorized) {
-                halt(403, LocalizationService.getInstance()
-                        .getMessage("tokens.usernotallchannelsaccess", user.getOrg().getName()));
-            }
-
-            response.type("application/json");
-            return GSON.toJson(createTokenWithServerKey(orgId, channels));
-        } catch (NumberFormatException | JoseException e) {
-            response.status(500);
-            return e.getMessage();
-        }
     }
 }
