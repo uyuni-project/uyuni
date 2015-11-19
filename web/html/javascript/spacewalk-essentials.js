d4f622f77d9b7bc1a51dd2ee54be8599b3685a0e
Dario Leidi <dleidi@suse.com>
2015-11-19 15:55:57 +0100

    Bug Fix: add custom rule to scroll to the url hash for pages in 'rhn/systems/ssm/misc' path (bsc#934865)

--- web/html/javascript/spacewalk-essentials.js
+++ web/html/javascript/spacewalk-essentials.js
@@ -298,7 +298,8 @@ function disableEnterKey() {
 
 // fix anchor scrolling in inline help
 $(function() {
-  if (window.location.pathname.indexOf("rhn/help/user") < 0) {
+  if (window.location.pathname.indexOf("rhn/help/user") < 0 &&
+	window.location.pathname.indexOf("rhn/systems/ssm/misc") < 0) {
     return;
   }
   if (window.location.hash) {
