559cf534ebbb60ee9e0ca75911431f05a03a780f
Silvio Moioli <smoioli@suse.de>
2015-11-20 11:40:26 +0100

    VHM UI: simplify forms by pre-selecting the gatherer module

--- java/code/src/com/suse/manager/webui/controllers/VirtualHostManagerController.java
+++ java/code/src/com/suse/manager/webui/controllers/VirtualHostManagerController.java
@@ -50,15 +50,12 @@ public class VirtualHostManagerController {
 
     private VirtualHostManagerController() { }
 
-    // to separate gatherer modules config values in the form:
-    // (VMWare<separator>host, SUSECloud<separator>port)
-    public static final String SEPARATOR = "---";
-
     public static ModelAndView list(Request request, Response response, User user) {
         Map<String, Object> data = new HashMap<>();
         data.put("csrf_token", CSRFTokenValidator.getToken(request.session().raw()));
         data.put("virtualHostManagers", getFactory()
                 .listVirtualHostManagers(user.getOrg()));
+        data.put("modules", new GathererRunner().listModules().keySet());
         data.put("info", FlashScopeHelper.flash(request));
         return new ModelAndView(data, "virtualhostmanager/list.jade");
     }
@@ -75,7 +72,8 @@ public class VirtualHostManagerController {
     }
 
     public static ModelAndView add(Request request, Response response) {
-        Map<String, Object> data = makeModuleFormData("", null, null);
+        String module = request.queryParams("module");
+        Map<String, Object> data = makeModuleFormData("", module, null);
         data.put("csrf_token", CSRFTokenValidator.getToken(request.session().raw()));
         return new ModelAndView(data, "virtualhostmanager/add.jade");
     }
@@ -90,30 +88,24 @@ public class VirtualHostManagerController {
      *
      * todo this boilerplate should be definitely handled by some kind of framework!
      *
-     * @param filledGathererModule
+     * @param module
      * @param filledGMParams
      * @return
      */
     private static HashMap<String, Object> makeModuleFormData(String vhmLabel,
-            String filledGathererModule, Map<String, String> filledGMParams) {
+            String module, Map<String, String> filledGMParams) {
         HashMap<String, Object> data = new HashMap<>();
-        Map<String, GathererModule> gathererModules = new GathererRunner().listModules();
-
-        // the provided one or the first valid one
-        String preselectedModule = (filledGathererModule == null)
-                ? gathererModules.entrySet().iterator().next().getKey()
-                : filledGathererModule;
+        GathererModule gathererModule = new GathererRunner().listModules().get(module);
 
         // pre-filled gatherer config values, if provided
-        if (filledGathererModule != null && filledGMParams != null) {
+        if (filledGMParams != null) {
             // add (possibly partly) already filled data
-            gathererModules.get(filledGathererModule).getParameters().putAll(
+            gathererModule.getParameters().putAll(
                     filledGMParams);
         }
 
         data.put("label", vhmLabel);
-        data.put("selectedGathererModule", preselectedModule);
-        data.put("gathererModules", gathererModules);
+        data.put("module", gathererModule);
         return data;
     }
 
@@ -122,7 +114,7 @@ public class VirtualHostManagerController {
         List<String> errors = new LinkedList<>();
 
         String label = request.queryParams("label");
-        String gathererModule = request.queryParams("gathererModule");
+        String gathererModule = request.queryParams("module");
         Map<String, String> gathererModuleParams =
                 createGathererModuleParams(gathererModule, request.queryMap().toMap());
 
@@ -165,9 +157,9 @@ public class VirtualHostManagerController {
             Map<String, String[]> queryMap) {
         return queryMap.entrySet().stream()
                 .filter(keyVal -> StringUtils.isNotEmpty(keyVal.getValue()[0]))
-                .filter(keyVal -> keyVal.getKey().startsWith(withSeparator(gathererModule)))
+                .filter(keyVal -> keyVal.getKey().startsWith("module_"))
                 .collect(Collectors.toMap(
-                        keyVal -> stripSeparator(keyVal.getKey()),
+                        keyVal -> keyVal.getKey().replaceFirst("module_", ""),
                         keyVal -> keyVal.getValue()[0]));
     }
 
@@ -210,13 +202,5 @@ public class VirtualHostManagerController {
 
     private static VirtualHostManagerFactory getFactory() {
         return VirtualHostManagerFactory.getInstance();
-    }
-
-    private static String withSeparator(String gathererModule) {
-        return gathererModule + SEPARATOR;
-    }
-
-    private static String stripSeparator(String enhanced) {
-        return enhanced.replaceFirst(".+" + SEPARATOR, "");
     }
 }
