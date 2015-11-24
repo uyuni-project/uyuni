4249a9fb71c1507fe6ca1b8f7a9f8604b5858b9c
Frantisek Kobzik <fkobzik@suse.de>
2015-11-24 15:04:21 +0100

    Break the giant test in DownloadControllerTest into smaller methods

--- java/code/src/com/suse/manager/webui/controllers/test/DownloadControllerTest.java
+++ java/code/src/com/suse/manager/webui/controllers/test/DownloadControllerTest.java
@@ -12,6 +12,7 @@
  * granted to use or replicate Red Hat trademarks that are incorporated
  * in this software or its documentation.
  */
+
 package com.suse.manager.webui.controllers.test;
 
 import com.mockobjects.servlet.MockHttpServletResponse;
@@ -19,12 +20,14 @@ import com.redhat.rhn.common.conf.Config;
 import com.redhat.rhn.common.conf.ConfigDefaults;
 import com.redhat.rhn.domain.channel.Channel;
 import com.redhat.rhn.domain.rhnpackage.Package;
-import com.redhat.rhn.domain.user.User;
-import com.redhat.rhn.testing.RhnBaseTestCase;
+import com.redhat.rhn.testing.BaseTestCaseWithUser;
 import com.redhat.rhn.testing.TestUtils;
 import com.suse.manager.webui.controllers.DownloadController;
 import com.suse.manager.webui.utils.SparkTestUtils;
 import com.suse.manager.webui.utils.TokenUtils;
+import junit.extensions.TestSetup;
+import junit.framework.Test;
+import junit.framework.TestSuite;
 import org.apache.commons.io.FilenameUtils;
 import spark.Request;
 import spark.RequestResponseFactory;
@@ -40,104 +43,132 @@ import java.util.Optional;
 
 import static com.redhat.rhn.testing.ErrataTestUtils.createTestChannel;
 import static com.redhat.rhn.testing.ErrataTestUtils.createTestPackage;
-import static com.redhat.rhn.testing.ErrataTestUtils.createTestUser;
 
 /**
  * Tests for the {@link DownloadController} endpoint.
  */
-public class DownloadControllerTest extends RhnBaseTestCase {
+public class DownloadControllerTest extends BaseTestCaseWithUser {
 
-    private String originalMountPoint;
+    private Channel channel;
+    private String uriFile;
+    private File packageFile;
+    private Response response;
+    private Package pkg;
 
-    public void setUp() throws Exception {
-        super.setUp();
+    private static String originalMountPoint;
 
-        // Config class keeps the config files sorted by a TreeSet with a File
-        // comparator, which makes it sometimes override the test rhn.conf with
-        // the defaults, nullifying server.secret_key.
-        // Until this is fixed, set it manually
-        Config.get().setString("server.secret_key", TestUtils.randomString());
+    /**
+     * One-time setup.
+     * @return Test
+     */
+    public static Test suite() {
+        TestSuite suite = new TestSuite(DownloadControllerTest.class);
+        TestSetup wrapper = new TestSetup(suite) {
+            protected void setUp() throws Exception {
+                // Config class keeps the config files sorted by a TreeSet with a File
+                // comparator, which makes it sometimes override the test rhn.conf with
+                // the defaults, nullifying server.secret_key.
+                // Until this is fixed, set it manually
+                Config.get().setString("server.secret_key", TestUtils.randomString());
+                originalMountPoint = Config.get().getString(ConfigDefaults.MOUNT_POINT);
+            }
+        };
 
-        originalMountPoint = Config.get().getString(ConfigDefaults.MOUNT_POINT);
-    }
-
-    public void tearDown() throws Exception {
-        Config.get().setString(ConfigDefaults.MOUNT_POINT, originalMountPoint);
-        super.tearDown();
+        return wrapper;
     }
 
     /**
-     * This is just one method verifying various scenarios, it should rather be split up
-     * into separate methods for each of the tested scenarios.
-     *
-     * @throws Exception in case of an error
+     * {@inheritDoc}
      */
-    public void testEndpoint() throws Exception {
-        User user = createTestUser();
-        Channel channel = createTestChannel(user);
-        Package pkg = createTestPackage(user, channel, "noarch");
+    public void setUp() throws Exception {
+        super.setUp();
 
-        // Write a fake file for the package
+        this.channel = createTestChannel(user);
+        this.response = RequestResponseFactory.create(new MockHttpServletResponse());
+
+        this.pkg = createTestPackage(user, channel, "noarch");
         final String nvra = String.format("%s-%s-%s.%s",
                 pkg.getPackageName().getName(), pkg.getPackageEvr().getVersion(),
                 pkg.getPackageEvr().getRelease(), pkg.getPackageArch().getLabel());
-        File packageFile = File.createTempFile(nvra, ".rpm");
+        this.uriFile = String.format("%s.rpm", nvra);
+        this.packageFile = File.createTempFile(nvra, ".rpm");
+        // Write a fake file for the package
         Files.write(packageFile.getAbsoluteFile().toPath(),
                 TestUtils.randomString().getBytes());
 
-        // Change mount point to the parent of the temp file
-        Config.get().setString(ConfigDefaults.MOUNT_POINT, packageFile.getParent());
         pkg.setPath(FilenameUtils.getName(packageFile.getAbsolutePath()));
         TestUtils.saveAndFlush(pkg);
 
-        Map<String, String> params = new HashMap<>();
-        final String uriFile = String.format("%s.rpm", nvra);
-        Request request = SparkTestUtils.createMockRequestWithParams(
+        // Change mount point to the parent of the temp file
+        Config.get().setString(ConfigDefaults.MOUNT_POINT, packageFile.getParent());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void tearDown() throws Exception {
+        super.tearDown();
+        Config.get().setString(ConfigDefaults.MOUNT_POINT, originalMountPoint);
+        Files.deleteIfExists(packageFile.toPath());
+    }
+
+    /**
+     * Helper method for creating a Spark Request with parameters.
+     *
+     * @param params - parameters
+     * @return - Spark Request
+     */
+    private Request getMockRequestWithParams(Map<String, String> params) {
+        return SparkTestUtils.createMockRequestWithParams(
                 "http://localhost:8080/rhn/manager/download/:channel/getPackage/:file",
                 params,
                 channel.getLabel(), uriFile);
+    }
 
-        Response response = RequestResponseFactory.create(new MockHttpServletResponse());
+    /**
+     * Tests download without a token.
+     */
+    public void testDownloadWithoutToken() {
+        Request request = getMockRequestWithParams(new HashMap<>());
 
-        // Try to download package without a token
         try {
             DownloadController.downloadPackage(request, response);
             fail("Controller should fail if no token was given");
         } catch (spark.HaltException e) {
             assertEquals(403, e.getStatusCode());
         }
+    }
 
-        // Add an invalid token parameter
+    /**
+     * Test download with an invalid token parameter.
+     */
+    public void testDownloadWithInvalidToken() {
+        Map<String, String> params = new HashMap<>();
         params.put("invalid-token-should-return-403", "");
+        Request request = getMockRequestWithParams(params);
+
         try {
             DownloadController.downloadPackage(request, response);
             fail("Controller should fail if wrong token was given");
         } catch (spark.HaltException e) {
             assertEquals(403, e.getStatusCode());
         }
+    }
 
-        // Create a token for a different organization
-        String tokenOtherOrg = TokenUtils.createTokenWithServerKey(
-                user.getOrg().getId() + 1,
-                Optional.of(new HashSet<String>(Arrays.asList(channel.getLabel()))));
-
-        // Create a token for a WRONG channel only
-        String tokenOtherChannel = TokenUtils.createTokenWithServerKey(
-                user.getOrg().getId(),
-                Optional.of(new HashSet<String>(Arrays.asList(channel.getLabel() + "WRONG"))));
-
-        // Create a token for the channel only
-        String tokenChannel = TokenUtils.createTokenWithServerKey(
-                user.getOrg().getId(),
-                Optional.of(new HashSet<String>(Arrays.asList(channel.getLabel()))));
-
-        // Create a token for the organization only
-        String token = TokenUtils.createTokenWithServerKey(user.getOrg().getId(), Optional.empty());
-
-        params.clear();
+    /**
+     * Test that the download is rejected when two tokens are present (even though the 1st
+     * one is valid).
+     *
+     * @throws Exception if anything goes wrong
+     */
+    public void testTwoTokens() throws Exception {
+        String token = TokenUtils.createTokenWithServerKey(user.getOrg().getId(),
+                Optional.empty());
+        Map<String, String> params = new HashMap<>();
         params.put(token, "");
-        // Add a second param: the controller should reject it
         params.put("2ndtoken", "");
+        Request request = getMockRequestWithParams(params);
+
         try {
             DownloadController.downloadPackage(request, response);
             fail(String.format("%s should halt 400 if 2 tokens given",
@@ -145,21 +176,22 @@ public class DownloadControllerTest extends RhnBaseTestCase {
         } catch (spark.HaltException e) {
             assertEquals(400, e.getStatusCode());
         }
+    }
 
-        // The added token is for a different organization
-        params.clear();
-        params.put(tokenOtherOrg, "");
-        try {
-            DownloadController.downloadPackage(request, response);
-            fail(String.format("%s should halt 403 if a different org token is given",
-                    DownloadController.class.getSimpleName()));
-        } catch (spark.HaltException e) {
-            assertEquals(403, e.getStatusCode());
-        }
-
+    /**
+     * Tests a download with a wrong channel in the token.
+     *
+     * @throws Exception if anything goes wrong
+     */
+    public void testTokenDifferentChannel() throws Exception {
         // The added token is for a different channel
-        params.clear();
+        String tokenOtherChannel = TokenUtils.createTokenWithServerKey(
+                user.getOrg().getId(),
+                Optional.of(new HashSet<>(Arrays.asList(channel.getLabel() + "WRONG"))));
+        Map<String, String> params = new HashMap<>();
         params.put(tokenOtherChannel, "");
+        Request request = getMockRequestWithParams(params);
+
         try {
             DownloadController.downloadPackage(request, response);
             fail(String.format("%s should halt 403 if a different channel token is given",
@@ -167,25 +199,65 @@ public class DownloadControllerTest extends RhnBaseTestCase {
         } catch (spark.HaltException e) {
             assertEquals(403, e.getStatusCode());
         }
+    }
 
-        // The token is valid for the right channel
-        params.clear();
+    /**
+     * Tests a download with a wrong organization in the token.
+     *
+     * @throws Exception if anything goes wrong
+     */
+    public void testTokenWrongOrg() throws Exception {
+        String tokenOtherOrg = TokenUtils.createTokenWithServerKey(
+                user.getOrg().getId() + 1,
+                Optional.of(new HashSet<>(Arrays.asList(channel.getLabel()))));
+        Map<String, String> params = new HashMap<>();
+        params.put(tokenOtherOrg, "");
+        Request request = getMockRequestWithParams(params);
+
+        try {
+            DownloadController.downloadPackage(request, response);
+            fail(String.format("%s should halt 403 if a different org token is given",
+                    DownloadController.class.getSimpleName()));
+        } catch (spark.HaltException e) {
+            assertEquals(403, e.getStatusCode());
+        }
+    }
+
+    /**
+     * Test a download with a correct channel in the token.
+     *
+     * @throws Exception if anything goes wrong
+     */
+    public void testCorrectChannel() throws Exception {
+        String tokenChannel = TokenUtils.createTokenWithServerKey(user.getOrg().getId(),
+                Optional.of(new HashSet<>(Arrays.asList(channel.getLabel()))));
+        Map<String, String> params = new HashMap<>();
         params.put(tokenChannel, "");
+        Request request = getMockRequestWithParams(params);
+
         try {
             assertNull(DownloadController.downloadPackage(request, response));
         } catch (spark.HaltException e) {
             fail("No HaltException should be thrown with a valid token!");
         }
+    }
 
-        // The token is valid for the right org
-        params.clear();
+    /**
+     * Test a download with a correct organization in the token.
+     *
+     * @throws Exception if anything goes wrong
+     */
+    public void testCorrectOrg() throws Exception {
+        String token = TokenUtils.createTokenWithServerKey(user.getOrg().getId(),
+                Optional.empty());
+        Map<String, String> params = new HashMap<>();
         params.put(token, "");
+        Request request = getMockRequestWithParams(params);
+
         try {
             assertNull(DownloadController.downloadPackage(request, response));
         } catch (spark.HaltException e) {
             fail("No HaltException should be thrown with a valid token!");
         }
-
-        Files.deleteIfExists(packageFile.toPath());
     }
 }
