075331e50d0127faa8781c75490809becb9ef36b
Johannes Renner <jrenner@suse.com>
2015-11-30 01:16:22 +0100

    Add StateRevision and reference it from PackageState

---
+++ java/code/src/com/redhat/rhn/domain/state/ServerStateRevision.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2015 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.domain.state;
+
+import com.redhat.rhn.domain.server.Server;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+/**
+ * A state revision that is assigned to a server.
+ */
+public class ServerStateRevision extends StateRevision {
+
+    private Server server;
+
+    /**
+     * @return the server
+     */
+    public Server getServer() {
+        return server;
+    }
+
+    /**
+     * @param server the server to set
+     */
+    public void setServer(Server server) {
+        this.server = server;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof ServerStateRevision)) {
+            return false;
+        }
+        ServerStateRevision otherRevision = (ServerStateRevision) other;
+        return new EqualsBuilder()
+                .append(getRevision(), otherRevision.getRevision())
+                .append(getPackageStates(), otherRevision.getPackageStates())
+                .append(getServer(), otherRevision.getServer())
+                .isEquals();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .append(getRevision())
+                .append(getPackageStates())
+                .append(getServer())
+                .toHashCode();
+    }
+}
