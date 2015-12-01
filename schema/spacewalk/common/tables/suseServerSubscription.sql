7b2f291472263d7375f639c8ed2e73c9c49a9b9b
Michael Calmer <mc@suse.de>
2015-12-01 16:34:12 +0100

    create table suseServerSubscription

---
+++ schema/spacewalk/common/tables/suseServerSubscription.sql
@@ -0,0 +1,34 @@
+--
+-- Copyright (c) 2015 SUSE LLC
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+-- Red Hat trademarks are not licensed under GPLv2. No permission is
+-- granted to use or replicate Red Hat trademarks that are incorporated
+-- in this software or its documentation.
+--
+
+
+CREATE TABLE suseServerSubscription
+(
+    id               NUMBER NOT NULL
+                       CONSTRAINT suse_srvsub_id_pk PRIMARY KEY
+                       USING INDEX TABLESPACE [[64k_tbs]],
+    server_id        NUMBER NOT NULL
+                       CONSTRAINT suse_srvsub_sid_fk
+                         REFERENCES rhnServer (id),
+    subscription_id  NUMBER NOT NULL
+)
+ENABLE ROW MOVEMENT
+;
+
+CREATE UNIQUE INDEX suse_srcsub_sid_subid_uq
+    ON suseServerSubscription (server_id, subscription_id)
+    TABLESPACE [[8m_tbs]];
+
+CREATE SEQUENCE suse_srvsub_id_seq;
