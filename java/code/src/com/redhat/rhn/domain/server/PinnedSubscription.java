2deadc79dca5b108bb829aa2e36355e4c60cea79
Michael Calmer <mc@suse.de>
2015-12-01 16:35:51 +0100

    add hibernate mapping for pinned subscriptions

---
+++ java/code/src/com/redhat/rhn/domain/server/PinnedSubscription.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2015 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.redhat.rhn.domain.server;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+
+public class PinnedSubscription {
+
+    private Long id;
+    private Server server;
+    private Long subscriptionId;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Getter for server
+     * @return the server associated with the subscription id
+     */
+    public Server getServer() {
+        return server;
+    }
+
+    /**
+     * @return the subscriptionId
+     */
+    public Long getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * associate a server with a subscription id
+     * @param serverIn the Server to associate
+     */
+    public void setServer(Server serverIn) {
+        this.server = serverIn;
+    }
+
+    /**
+     * @param subscriptionIdIn the subscriptionId to set
+     */
+    public void setSubscriptionId(Long subscriptionIdIn) {
+        this.subscriptionId = subscriptionIdIn;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean equals(final Object other) {
+        if (!(other instanceof PinnedSubscription)) {
+            return false;
+        }
+        PinnedSubscription castOther = (PinnedSubscription) other;
+        return new EqualsBuilder().append(getServer(), castOther.getServer())
+                .append(getSubscriptionId(), castOther.getSubscriptionId())
+                .isEquals();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int hashCode() {
+        return new HashCodeBuilder().append(getServer())
+                .append(getSubscriptionId())
+                .toHashCode();
+    }
+}
