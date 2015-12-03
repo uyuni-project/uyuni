ce54dc15c4aca6e1009500d79149d066f7d9449c
Johannes Renner <jrenner@suse.com>
2015-12-03 17:42:14 +0100

    Fix checkstyle and allow null for state and constraint

--- java/code/src/com/suse/manager/webui/utils/gson/PackageStateDto.java
+++ java/code/src/com/suse/manager/webui/utils/gson/PackageStateDto.java
@@ -1,5 +1,22 @@
+/**
+ * Copyright (c) 2015 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package com.suse.manager.webui.utils.gson;
 
+/**
+ * Transfer object for serializing package states as JSON.
+ */
 public class PackageStateDto {
 
     private final String name;
@@ -24,14 +41,23 @@ public class PackageStateDto {
         this.versionConstraintId = versionConstraintIdIn;
     }
 
+    /**
+     * @return the package name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return the package evr
+     */
     public String getEvr() {
         return evr;
     }
 
+    /**
+     * @return the package arch
+     */
     public String getArch() {
         return arch;
     }
@@ -43,6 +69,9 @@ public class PackageStateDto {
         return packageStateId;
     }
 
+    /**
+     * @return the version constraint id
+     */
     public int getVersionConstraintId() {
         return versionConstraintId;
     }
