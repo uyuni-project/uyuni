3d8ed6c7f2fa2d66637f574d6924bf341089551b
Michael Calmer <mc@suse.de>
2015-12-03 17:50:02 +0100

    unittest: testSubscriptionsToJson

--- java/code/src/com/suse/manager/matcher/test/MatcherJsonIOTest.java
+++ java/code/src/com/suse/manager/matcher/test/MatcherJsonIOTest.java
@@ -1,5 +1,7 @@
 package com.suse.manager.matcher.test;
 
+import com.redhat.rhn.common.conf.Config;
+import com.redhat.rhn.common.hibernate.HibernateFactory;
 import com.redhat.rhn.domain.common.LoggingFactory;
 import com.redhat.rhn.domain.product.SUSEProductFactory;
 import com.redhat.rhn.domain.product.test.SUSEProductTestUtils;
@@ -9,17 +11,23 @@ import com.redhat.rhn.domain.server.InstalledProduct;
 import com.redhat.rhn.domain.server.Server;
 import com.redhat.rhn.domain.server.ServerFactory;
 import com.redhat.rhn.domain.server.VirtualInstance;
+import com.redhat.rhn.manager.content.ContentSyncManager;
 import com.redhat.rhn.testing.ServerTestUtils;
 import com.redhat.rhn.testing.TestUtils;
 
 import com.suse.manager.matcher.MatcherJsonIO;
+import com.suse.scc.model.SCCSubscription;
 
+import java.io.File;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 import junit.framework.TestCase;
 
 public class MatcherJsonIOTest extends TestCase {
+    private static final String JARPATH = "/com/redhat/rhn/manager/content/test/";
+    private static final String SUBSCRIPTIONS_JSON = JARPATH + "sccdata/organizations_subscriptions.json";
 
     public void testSystemsToJson() throws Exception {
         SUSEProductFactory.clearAllProducts();
@@ -77,6 +85,33 @@ public class MatcherJsonIOTest extends TestCase {
         assertTrue(jsonString.contains("\"1324\": \"SUSE Linux Enterprise High Availability Extension 12 SP1\""));
         assertTrue(jsonString.contains("\"1076\": \"SUSE Manager Mgmt Single 1.2\""));
         assertTrue(jsonString.contains("\"1097\": \"SUSE Manager Prov Single 1.2\""));
+    }
+
+    public void testSubscriptionsToJson() throws Exception {
+        File subJson = new File(TestUtils.findTestData(SUBSCRIPTIONS_JSON).getPath());
+        String fromdir = subJson.getParent();
+        try {
+            Config.get().setString(ContentSyncManager.RESOURCE_PATH, fromdir);
+
+            ContentSyncManager cm = new ContentSyncManager();
+            Collection<SCCSubscription> s = cm.getSubscriptions();
+            HibernateFactory.getSession().flush();
+            assertNotNull(s);
+            String jsonString = new MatcherJsonIO().getJsonSubscriptions();
+            assertNotNull(jsonString);
+            assertTrue(jsonString.contains("\"part_number\": \"662644474670\""));
+            assertTrue(jsonString.contains("\"system_limit\": 10,"));
+            assertTrue(jsonString.contains("\"regcode\": \"55REGCODE180\""));
+            assertTrue(jsonString.contains("706,"));
+            assertTrue(jsonString.contains("\"scc_org_id\": \"extFile\""));
+            assertTrue(jsonString.contains("\"id\": 9998"));
+            assertTrue(jsonString.contains("\"id\": 9999"));
+            assertTrue(jsonString.contains("\"system_limit\": 100,"));
+        }
+        finally {
+            Config.get().remove(ContentSyncManager.RESOURCE_PATH);
+            SUSEProductTestUtils.deleteIfTempFile(subJson);
+        }
     }
 
     private VirtualInstance createVirtualInstance(Server host, Server guest, String uuid) {
