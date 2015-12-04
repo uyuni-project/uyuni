72b3408a36d2294b8595bc75ef7f53a8b0aada4f
Frantisek Kobzik <fkobzik@suse.de>
2015-12-04 13:51:40 +0100

    Rename gatherer-bunch to gatherer-matcher-bunch
    ...since this bunch will be re-used to run the matcher task too.

--- schema/spacewalk/common/data/rhnTaskoTemplate.sql
+++ schema/spacewalk/common/data/rhnTaskoTemplate.sql
@@ -177,7 +177,7 @@ INSERT INTO rhnTaskoTemplate (id, bunch_id, task_id, ordering, start_if)
 
 INSERT INTO rhnTaskoTemplate (id, bunch_id, task_id, ordering, start_if)
              VALUES (sequence_nextval('rhn_tasko_template_id_seq'),
-                        (SELECT id FROM rhnTaskoBunch WHERE name='gatherer-bunch'),
+                        (SELECT id FROM rhnTaskoBunch WHERE name='gatherer-matcher-bunch'),
                         (SELECT id FROM rhnTaskoTask WHERE name='gatherer'),
                         0,
                         null);
