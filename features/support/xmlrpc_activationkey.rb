a82fad5ace65066e9768e212a04300959583c415
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-12-15 08:47:15 +0100

    remove exception paranoia, port to ruby

--- features/support/xmlrpc_activationkey.rb
+++ features/support/xmlrpc_activationkey.rb
@@ -1,32 +1,14 @@
-File.expand_path(__FILE__)           # For Ruby 1.9.2+
-$LOAD_PATH << File.dirname(__FILE__) # For Ruby 1.8
-
-require 'xmlrpctest'
-
+require_relative 'xmlrpctest'
 
 class XMLRPCActivationKeyTest < XMLRPCBaseTest
   def createKey(id, descr, limit)
-    key = nil
-    begin
-      key = @connection.call("activationkey.create", @sid, id, descr, "", limit.to_i, [], false)
-    rescue Exception => ex
-      puts "Something went wrong: " + ex
-    end
-
-    return key
+    @connection.call("activationkey.create", @sid, id, descr, "", limit.to_i, [], false)
   end
 
 
   def deleteKey(id)
-    begin
-      @connection.call("activationkey.delete", @sid, id)
-      @keys = @connection.call("activationkey.listActivationKeys", @sid)
-    rescue Exception => ex
-      puts "Something went wrong during key deletion: " + ex
-      return false
-    end
-
-    return true
+    @connection.call("activationkey.delete", @sid, id)
+    @keys = @connection.call("activationkey.listActivationKeys", @sid)
   end
 
 
@@ -41,68 +23,40 @@ class XMLRPCActivationKeyTest < XMLRPCBaseTest
     return @keys == nil ? 0 : @keys.length
   end
 
-
   def verifyKey(kid)
-    keys = @connection.call("activationkey.listActivationKeys", @sid)
-    for key in keys
-      if kid == key['key']
-          return true
-      end
-    end
-    return false
+    @connection.call("activationkey.listActivationKeys", @sid)
+      .map {|key| key['key'] }
+      .include?(kid)
   end
-
 
   def getConfigChannelsCount(key)
     channels = @connection.call("activationkey.listConfigChannels", @sid, key)
     return channels == nil ? 0 : channels.length
   end
 
-
   def addConfigChannel(key, name)
-    res = 0
-    begin
-      res = @connection.call("activationkey.addConfigChannels", @sid, [key], [name], false)
-    rescue Exception => ex
-      puts "Cannot add config channel: " + ex
-    end
-
-    return res
+    @connection.call("activationkey.addConfigChannels", @sid, [key], [name], false)
   end
-
 
   def setDetails(key)
-    res = false
-    begin
-      details = {
-        'description' => 'Test description of the key ' + key,
-#        'base_channel_label' => "", # <---- Insert here a valid channel
-        'usage_limit' => 10,
-        'universal_default' => false
-      }
-      res = @connection.call("activationkey.setDetails", @sid, key, details).to_i == 1
-    rescue Exception => ex
-      puts "Cannot set details to the key: " + ex
-    end
-
-    return res
+    details = {
+      'description' => 'Test description of the key ' + key,
+      #        'base_channel_label' => "", # <---- Insert here a valid channel
+      'usage_limit' => 10,
+      'universal_default' => false
+    }
+    @connection.call("activationkey.setDetails", @sid, key, details).to_i == 1
   end
 
-
   def getDetails(key)
-    res = false
-    begin
-      keyinfo = @connection.call("activationkey.getDetails", @sid, key)
-      puts "      Key info for the key " + keyinfo['key']
+    keyinfo = @connection.call("activationkey.getDetails", @sid, key)
+    puts "      Key info for the key " + keyinfo['key']
 
-      keyinfo.each_pair do |k,v|
-        puts "        " + k.to_s + ": " + v.to_s
-      end
-
-      res = ('Test description of the key ' + key) == keyinfo['description']
-    rescue Exception => ex
+    keyinfo.each_pair do |k,v|
+      puts "        " + k.to_s + ": " + v.to_s
     end
 
+    res = ('Test description of the key ' + key) == keyinfo['description']
     return res
   end
 end
