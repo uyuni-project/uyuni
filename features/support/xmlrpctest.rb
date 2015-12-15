7099041615e89c0f564ed4a470803ddeaf13f388
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-12-15 08:40:34 +0100

    remove exception paranoia

--- features/support/xmlrpctest.rb
+++ features/support/xmlrpctest.rb
@@ -1,16 +1,9 @@
-# COPYRIGHT 2015 SUSE LLC
-
 require 'xmlrpc/client'
 
 class XMLRPCBaseTest
-  #
-  # Constructor.
-  #
+
   def initialize(host)
     @host = host
-    if !@host
-      raise Exception, "Hostname is missing!"
-    end
     @connection = XMLRPC::Client.new2("http://" + @host + "/rpc/api")
   end
 
@@ -19,24 +12,12 @@ class XMLRPCBaseTest
   # Authenticate against the $HOST
   #
   def login(luser, password)
-    begin
-      @sid = @connection.call("auth.login", luser, password)
-      return true
-    rescue Exception => exception
-      puts "Login failed. Try harder. :)"
-      return false
-    end
+    @sid = @connection.call("auth.login", luser, password)
   end
 
 
   def logout()
-    begin
       @connection.call("auth.logout", @sid)
-      return true
-    rescue Exception => exception
-      puts "Well, you finished anyways..."
-      return false
-    end
   end
 end
 
