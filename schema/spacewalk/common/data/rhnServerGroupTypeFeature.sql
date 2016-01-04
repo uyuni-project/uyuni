76efc94d9607a60c959f755e2bd264092a73285a
Johannes Renner <jrenner@suse.com>
2016-01-04 15:14:04 +0100

    Add 'ftr_errata_updates' to saltstack entitled systems

--- schema/spacewalk/common/data/rhnServerGroupTypeFeature.sql
+++ schema/spacewalk/common/data/rhnServerGroupTypeFeature.sql
@@ -121,6 +121,11 @@ values (lookup_sg_type('bootstrap_entitled'), lookup_feature_type('ftr_system_gr
 
 insert into rhnServerGroupTypeFeature (server_group_type_id, feature_id,
                                        created, modified)
+values (lookup_sg_type('saltstack_entitled'), lookup_feature_type('ftr_errata_updates'),
+        current_timestamp,current_timestamp);
+
+insert into rhnServerGroupTypeFeature (server_group_type_id, feature_id,
+                                       created, modified)
 values (lookup_sg_type('saltstack_entitled'), lookup_feature_type('ftr_daily_summary'),
         current_timestamp,current_timestamp);
 
