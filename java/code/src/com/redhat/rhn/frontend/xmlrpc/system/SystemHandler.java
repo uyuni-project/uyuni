957dfb324b8534524a8d61f2ed058b1cfcafdc79
Silvio Moioli <smoioli@suse.de>
2016-01-11 11:28:46 +0100

    SystemHandler: fix JDK7 compatibility

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/system/SystemHandler.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/system/SystemHandler.java
@@ -264,7 +264,7 @@ public class SystemHandler extends BaseHandler {
         //Get the logged in user and server
         Server server = lookupServer(loggedInUser, sid);
 
-        Entitlement entitlement = EntitlementManager.getByName(entitlementLevel);
+        final Entitlement entitlement = EntitlementManager.getByName(entitlementLevel);
 
         // Make sure we got a valid entitlement and the server can be entitled to it
         validateEntitlements(new ArrayList() { { add(entitlement); } });
