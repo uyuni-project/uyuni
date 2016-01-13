e300d94549b6b2c6c39f50d3eca0839a601868c0
Johannes Renner <jrenner@suse.com>
2016-01-13 16:24:04 +0100

    Unit tests for MinionServerFactory

---
+++ java/code/src/com/redhat/rhn/domain/server/test/MinionServerFactoryTest.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2016 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.domain.server.test;
+
+import com.redhat.rhn.domain.server.MinionServer;
+import com.redhat.rhn.domain.server.MinionServerFactory;
+import com.redhat.rhn.domain.server.ServerConstants;
+import com.redhat.rhn.testing.BaseTestCaseWithUser;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * MinionServerFactoryTest
+ */
+public class MinionServerFactoryTest extends BaseTestCaseWithUser {
+
+    /**
+     * Test for {@link MinionServerFactory#findByMachineId(String)}.
+     */
+    public void testFindByMachineId() throws Exception {
+        MinionServer minionServer = createTestMinionServer();
+        Optional<MinionServer> minion = MinionServerFactory
+                .findByMachineId(minionServer.getMachineId());
+        assertTrue(minion.isPresent());
+        assertEquals(minionServer, minion.get());
+    }
+
+    /**
+     * Test for {@link MinionServerFactory#findByMinionId(String)}.
+     */
+    public void testFindByMinionId() throws Exception {
+        MinionServer minionServer = createTestMinionServer();
+        Optional<MinionServer> minion = MinionServerFactory
+                .findByMinionId(minionServer.getMinionId());
+        assertTrue(minion.isPresent());
+        assertEquals(minionServer, minion.get());
+    }
+
+    /**
+     * Test for {@link MinionServerFactory#listMinions()}.
+     */
+    public void testListMinions() throws Exception {
+        MinionServer minionServer = createTestMinionServer();
+        List<MinionServer> minions = MinionServerFactory.listMinions();
+        assertTrue(minions.contains(minionServer));
+    }
+
+    /**
+     * Test for {@link MinionServerFactory#lookupById(Long)}.
+     */
+    public void testLookupById() throws Exception {
+        MinionServer minionServer = createTestMinionServer();
+        Optional<MinionServer> minion = MinionServerFactory
+                .lookupById(minionServer.getId());
+        assertTrue(minion.isPresent());
+        assertEquals(minionServer, minion.get());
+    }
+
+    /**
+     * Create a {@link MinionServer} for testing.
+     *
+     * @return the MinionServer object
+     * @throws Exception in case of an error
+     */
+    private MinionServer createTestMinionServer() throws Exception {
+        return (MinionServer) ServerFactoryTest.createTestServer(user, true,
+                ServerConstants.getServerGroupTypeSaltStackEntitled(),
+                ServerFactoryTest.TYPE_SERVER_MINION);
+    }
+}
