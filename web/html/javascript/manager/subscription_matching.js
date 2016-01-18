ff84a43ddddadb9e355758b52b298688bf2f3ab7
Silvio Moioli <smoioli@suse.de>
2016-01-18 13:55:53 +0100

    Subscription Matching UI: add a table for messages

--- web/html/javascript/manager/subscription_matching.js
+++ web/html/javascript/manager/subscription_matching.js
@@ -2,7 +2,7 @@
 
 var SubscriptionMatching = React.createClass({
    getInitialState: function() {
-     return null;
+     return { "messages" : [] };
    },
 
   componentWillMount: function() {
@@ -12,12 +12,97 @@ var SubscriptionMatching = React.createClass({
   },
 
   render: function() {
-    if (this.state == null) {
-      return <h1>{t("Loading...")}</h1>;
+    return (
+      <div>
+        <div className="spacewalk-toolbar-h1">
+          <h1><i className="fa spacewalk-icon-subscription-counting"></i>{t("Subscription Matching")}</h1>
+        </div>
+        <div>
+          <div className="row col-md-12">
+            <p>{t("Please review warning and information messages from last matching below.")}</p>
+            <Table headers={[t("Message"), t("Additional information")]} data={humanReadable(this.state.messages)} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+function humanReadable(raw_messages){
+  var result= raw_messages.map(function(raw_message){
+    var data = raw_message["data"];
+    var message;
+    var additionalInformation;
+    switch(raw_message["type"]) {
+      case "unknown_part_number" :
+        message = t("Unsupported part number detected");
+        additionalInformation = data["part_number"];
+        break;
+      case "physical_guest" :
+        message = t("Physical system is reported as virtual guest, please check hardware data");
+        additionalInformation = data["name"];
+        break;
+      case "guest_with_unknown_host" :
+        message = t("Virtual guest has unknown host, assuming it is a physical system");
+        additionalInformation = data["name"];
+        break;
+      case "unknown_cpu_count" :
+        message = t("System has an unknown number of sockets, assuming 16");
+        additionalInformation = data["name"];
+        break;
+      case "unsatisfied_pinned_match" :
+        message = t("Matcher was not able to satisfy a pinned subscription");
+        additionalInformation = t("{0} to system {1}", data["subscription_name"], data["system_name"]);
+        break;
+      default:
+        message = raw_message["type"];
+        additionalInformation = data;
     }
-    else {
-      return <h1>{t("Matched on {0}", this.state.timestamp)}!</h1>;
-    }
+    return [message, additionalInformation];
+  });
+  return result;
+}
+
+var Table = React.createClass({
+  render: function() {
+    return (
+      <div className="panel panel-default">
+        <table className="table table-striped">
+          <TableHeader headers={this.props.headers} />
+          {this.props.data.map(function(columns){
+            return (<TableRow columns={columns}/>);
+          })}
+        </table>
+      </div>
+    );
+  }
+});
+
+var TableHeader = React.createClass({
+  render: function() {
+    return (
+      <thead>
+        <tr>
+          {this.props.headers.map(function(header){
+            return (<th>{header}</th>);
+          })}
+        </tr>
+      </thead>
+    );
+  }
+});
+
+var TableRow = React.createClass({
+  render: function() {
+    return (
+      <tbody className="table-content">
+        <tr>
+          {this.props.columns.map(function(column){
+            return (<td>{column}</td>);
+          })}
+        </tr>
+      </tbody>
+    );
   }
 });
 
