3701abb4448c1754f6e4375a242e67ce46fda721
Duncan Mac-Vicar P <dmacvicar@suse.de>
2016-01-20 17:19:02 +0100

    checksytle: unused imports

--- java/code/src/com/suse/manager/webui/controllers/DownloadController.java
+++ java/code/src/com/suse/manager/webui/controllers/DownloadController.java
@@ -32,9 +32,7 @@ import spark.Request;
 import spark.Response;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.security.Key;
