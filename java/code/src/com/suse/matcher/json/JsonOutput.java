7f09d56b2c3dae58f12610b88ec0b0b70412164f
Silvio Moioli <smoioli@suse.de>
2016-01-25 17:47:39 +0100

    Checkstyle fixes

--- java/code/src/com/suse/matcher/json/JsonOutput.java
+++ java/code/src/com/suse/matcher/json/JsonOutput.java
@@ -1,31 +1,16 @@
 /**
  * Copyright (c) 2016 SUSE LLC
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
  *
- *    * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *    * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *    * Neither the name of SUSE LLC nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
  */
 package com.suse.matcher.json;
 
@@ -58,6 +43,7 @@ public class JsonOutput {
      * @param timestampIn the timestamp
      * @param confirmedMatchesIn the confirmed matches
      * @param messagesIn the messages
+     * @param subscriptionPoliciesIn mapping from subscription id to its policy
      */
     public JsonOutput(Date timestampIn, List<JsonMatch> confirmedMatchesIn,
             List<JsonMessage> messagesIn, Map<Long, String> subscriptionPoliciesIn) {
@@ -113,10 +99,10 @@ public class JsonOutput {
 
     /**
      * Sets the subscription to policy mapping.
-     * @param subscriptionPolicies subscription to policy map
+     * @param subscriptionPoliciesIn subscription to policy map
      */
-    public void setSubscriptionPolicies(Map<Long, String> subscriptionPolicies) {
-        this.subscriptionPolicies = subscriptionPolicies;
+    public void setSubscriptionPolicies(Map<Long, String> subscriptionPoliciesIn) {
+        this.subscriptionPolicies = subscriptionPoliciesIn;
     }
 
     /**
