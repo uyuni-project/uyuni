4bafceb1a5787b9ce4d13ae67ebf8ab0a9048b41
Michael Calmer <mc@suse.de>
2016-01-29 13:57:51 +0100

    build docker image for salt testing

---
+++ susemanager-utils/testing/docker/head/suma-head-salt/add_packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# make sure the package repository is up to date
+zypper --non-interactive --gpg-auto-import-keys ref
+
+# Packages required to run salt-minion
+zypper -n in  --no-recommends util-linux-systemd \
+                              iproute2 \
+                              python \
+                              cronie \
+                              wicked \
+                              sysconfig \
+                              python-pyOpenSSL \
+                              postfix \
+                              dmidecode \
+                              psmisc
+
+zypper -n in vim less
