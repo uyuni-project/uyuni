5b32a1db5dee115962590777ce84d7daee0015ed
Kevin Walter <kwalter@suse.com>
2016-01-29 12:41:24 +0100

    use netapi library for Grains target

--- java/code/src/com/suse/manager/webui/controllers/StatesAPI.java
+++ java/code/src/com/suse/manager/webui/controllers/StatesAPI.java
@@ -29,6 +29,7 @@ import com.redhat.rhn.manager.rhnpackage.PackageManager;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import com.suse.saltstack.netapi.datatypes.target.Grains;
 import org.apache.log4j.Logger;
 
 import com.suse.manager.webui.services.SaltService;
@@ -41,7 +42,6 @@ import com.suse.manager.webui.utils.SaltStateGenerator;
 import com.suse.manager.webui.utils.gson.JSONPackageState;
 import com.suse.manager.webui.utils.gson.JSONServerApplyStates;
 import com.suse.manager.webui.utils.gson.JSONServerPackageStates;
-import com.suse.manager.webui.utils.salt.Grains;
 import com.suse.saltstack.netapi.calls.LocalAsyncResult;
 
 import java.io.IOException;
