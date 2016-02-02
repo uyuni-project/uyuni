e3de7a3872138428083ec306c49b08e9026121d1
Duncan Mac-Vicar P <dmacvicar@suse.de>
2016-02-02 15:47:37 +0100

    implement reboot action
    Still a schema change is needed, run with spacewalk-sql:
    
    insert into rhnServerGroupTypeFeature (server_group_type_id, feature_id,
                                           created, modified)
                                           values (lookup_sg_type('salt_entitled'), lookup_feature_type('ftr_reboot'),
                                           current_timestamp, current_timestamp);

---
+++ java/code/src/com/suse/manager/webui/utils/salt/System.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2016 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.suse.manager.webui.utils.salt;
+
+import com.google.gson.reflect.TypeToken;
+import com.suse.saltstack.netapi.calls.LocalCall;
+
+import java.util.LinkedHashMap;
+import java.util.Optional;
+
+/**
+ * salt.modules.system
+ *
+ * https://docs.saltstack.com/en/latest/ref/modules/all/salt.modules.system.html
+ */
+public class System {
+
+    /**
+     * salt.modules.system.reboot
+     * @return the result
+     */
+    public static LocalCall<String> reboot(Optional<Integer> at_time) {
+        LinkedHashMap<String, Object> args = new LinkedHashMap<>();
+        at_time.ifPresent(t -> {
+            args.put("at_time", t);
+        });
+        return new LocalCall<>("system.reboot", Optional.empty(), Optional.of(args),
+                new TypeToken<String>() { });
+    }
+}
