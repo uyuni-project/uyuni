90813ff11b690479e49e3bc8674ebad7b88ab582
Matei Albu <malbu@suse.de>
2016-02-09 12:52:20 +0100

    build cleanup

--- web/html/src/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-export PATH=$PATH:$(readlink -f ./node_modules/.bin/)
-
-#browserify -r react > ../javascript/manager/react-bundle.js
-browserify -x react -t [ babelify --presets [ es2015 react ] ] manager/org-state-catalog.js > ../javascript/manager/org-state-catalog-app.js
