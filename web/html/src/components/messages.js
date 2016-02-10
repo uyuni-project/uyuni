d0f13289b3ccc9aff329cf1bfce80d23e53442f7
Matei Albu <malbu@suse.de>
2016-02-10 14:38:15 +0100

    cleanup

--- web/html/src/components/messages.js
+++ web/html/src/components/messages.js
@@ -3,25 +3,25 @@
 var React = require("react");
 
 var Messages = React.createClass({
-    _classNames : {
-        "error": "danger",
-        "success": "success",
-        "info": "info",
-        "warning": "warning",
-    },
+  _classNames : {
+    "error": "danger",
+    "success": "success",
+    "info": "info",
+    "warning": "warning",
+  },
 
   getInitialState: function() {
     return {};
   },
 
-    render: function() {
-        var msgs = this.props.items.map(function(item) {
-                    return (<div className={'alert alert-' + this._classNames[item.severity]}>{item.text}</div>);
-                }.bind(this));
-        return (<div>{msgs}</div>);
-    }
+  render: function() {
+    var msgs = this.props.items.map(function(item) {
+                return (<div className={'alert alert-' + this._classNames[item.severity]}>{item.text}</div>);
+            }.bind(this));
+    return (<div>{msgs}</div>);
+  }
 
-})
+});
 
 module.exports = {
     Messages : Messages
