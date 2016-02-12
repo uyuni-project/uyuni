6038571f5842d1b14bc9a1c12d75352e53bc036d
Matei Albu <malbu@suse.de>
2016-02-12 13:57:44 +0100

    tidy up imports

--- java/code/src/com/suse/manager/webui/utils/SparkApplicationHelper.java
+++ java/code/src/com/suse/manager/webui/utils/SparkApplicationHelper.java
@@ -14,10 +14,23 @@
  */
 package com.suse.manager.webui.utils;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.redhat.rhn.common.conf.Config;
+import com.redhat.rhn.common.security.CSRFTokenValidator;
+import com.redhat.rhn.domain.role.RoleFactory;
+import com.redhat.rhn.domain.user.User;
+import com.redhat.rhn.frontend.struts.RequestContext;
+import com.redhat.rhn.common.security.PermissionException;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.suse.manager.webui.Languages;
+
+import de.neuland.jade4j.JadeConfiguration;
 
 import org.apache.http.HttpStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import spark.ModelAndView;
 import spark.Response;
@@ -25,17 +38,6 @@ import spark.Route;
 import spark.Spark;
 import spark.TemplateViewRoute;
 import spark.template.jade.JadeTemplateEngine;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.redhat.rhn.common.conf.Config;
-import com.redhat.rhn.common.security.CSRFTokenValidator;
-import com.redhat.rhn.common.security.PermissionException;
-import com.redhat.rhn.domain.role.RoleFactory;
-import com.redhat.rhn.domain.user.User;
-import com.redhat.rhn.frontend.struts.RequestContext;
-import com.suse.manager.webui.Languages;
-import de.neuland.jade4j.JadeConfiguration;
 
 /**
  * Utility methods to integrate Spark with SUSE Manager's infrastructure.
