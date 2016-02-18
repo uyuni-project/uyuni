6f26ec30404413054edc3fb51e7714cf76f2b8b2
Dario Leidi <dleidi@suse.com>
2016-02-18 17:54:56 +0100

    Refactoring: set className and title to optional for a popup

--- web/html/src/components/popup.js
+++ web/html/src/components/popup.js
@@ -6,8 +6,8 @@ var React = require("react");
       propTypes: {
         id: React.PropTypes.string.isRequired, // the id of the html div tag
         onClosePopUp: React.PropTypes.func.isRequired, // a callback function with no parameters
-        className: React.PropTypes.string.isRequired, // the css className for the 'modal-dialog' div
-        title: React.PropTypes.string.isRequired,
+        className: React.PropTypes.string, // the css className for the 'modal-dialog' div
+        title: React.PropTypes.string,
         content: React.PropTypes.node.isRequired, // the body of the popup
         footer: React.PropTypes.node,
       },
@@ -19,13 +19,13 @@ var React = require("react");
     render: function() {
       return (
         <div className="modal fade" tabindex="-1" role="dialog" aria-labelledby="addPinPopUpLabel" id={this.props.id} >
-          <div className={"modal-dialog " + this.props.className}>
+          <div className={"modal-dialog " + (this.props.className ? this.props.className : "")}>
             <div className="modal-content">
               <div className="modal-header">
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
-                <h4 className="modal-title">{this.props.title}</h4>
+                {this.props.title ? <h4 className="modal-title">{this.props.title}</h4> : null}
               </div>
               <div className="modal-body">{this.props.content}</div>
               {this.props.footer ? <div className="modal-footer">{this.props.footer}</div> : null}
