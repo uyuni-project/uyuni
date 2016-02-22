10afec6663dd4ed376c310c6f8aecf9bd8063b7b
Silvio Moioli <smoioli@suse.de>
2016-02-22 11:08:37 +0100

    Refactoring: split in two components

--- web/html/src/manager/subscription-matching-unmatched-products.js
+++ web/html/src/manager/subscription-matching-unmatched-products.js
@@ -12,6 +12,10 @@ var PopUp = require("../components/popup").PopUp;
 var UnmatchedProducts = React.createClass({
   mixins: [StatePersistedMixin],
 
+  getInitialState: function() {
+    return {selectedProductId: null};
+  },
+
   rowComparator: function(a, b, columnIndex, ascending) {
     var columnKeyInRawData = ["productName", "systemCount"];
     var columnKey = columnKeyInRawData[columnIndex];
@@ -35,19 +39,34 @@ var UnmatchedProducts = React.createClass({
     return result * orderCondition;
   },
 
-  systemsComparator: function(a, b, columnIndex, ascending) {
-    var columnKeyInRawData = ["systemName"];
-    var columnKey = columnKeyInRawData[columnIndex];
-    var orderCondition = ascending ? 1 : -1;
-    var result = 0;
-    var aValue = a.props["rawData"][columnKey];
-    var bValue = b.props["rawData"][columnKey];
-    result = aValue.toLowerCase().localeCompare(bValue.toLowerCase());
-    return result * orderCondition;
-  },
+  buildRows: function() {
+    var products = this.props.products;
+    return this.props.unmatchedProductIds.map((pid) => {
+      var productName = products[pid].productName;
+      var systemCount = products[pid].unmatchedSystemCount;
+      var listButton =
+          <button
+            className="btn btn-default btn-cell"
+            onClick={() => {this.showPopUp(pid);}}
+            data-toggle="modal"
+            data-target="#unmatchedProductsPopUp">
+            {t("Show system list")}
+          </button>;
 
-  getInitialState: function() {
-    return {selectedProductId: null};
+      var columns = [
+        <TableCell content={productName} />,
+        <TableCell content={systemCount} />,
+        <TableCell content={listButton} />,
+      ];
+
+      var rawData = {
+        id: pid,
+        productName: productName,
+        systemCount: systemCount
+      };
+
+      return <TableRow columns={columns} rawData={rawData} />
+    });
   },
 
   showPopUp: function(id) {
@@ -61,22 +80,10 @@ var UnmatchedProducts = React.createClass({
   render: function() {
     var body;
     if (this.props.unmatchedProductIds.length > 0) {
-      var popUpContent = this.state.selectedProductId == null ?
-        null :
-        <Table
-          headers={[t("System name")]}
-          rows={unmatchedSystemsToRows(this.props.products[this.state.selectedProductId], this.props.systems)}
-          rowComparator={this.systemsComparator}
-          sortableColumnIndexes={[0]}
-          rowFilter={(tableRow, searchValue) => tableRow.props["rawData"]["systemName"].toLowerCase().indexOf(searchValue.toLowerCase()) > -1}
-          filterPlaceholder={t("Filter by name")}
-        />
-      ;
-
       body = (
         <div>
           <Table headers={[t("Product name"), t("Unmatched system count"), ""]}
-            rows={unmatchedProductsToRows(this)}
+            rows={this.buildRows()}
             loadState={this.props.loadState}
             saveState={this.props.saveState}
             rowComparator={this.rowComparator}
@@ -84,10 +91,11 @@ var UnmatchedProducts = React.createClass({
           />
           <CsvLink name="unmatched_product_report.csv" />
 
-          <PopUp title={t("Unmatched systems")}
-            id="unmatchedProductsPopUp"
-            content={popUpContent}
-            onClosePopUp={this.closePopUp}
+          <UnmatchedSystemPopUp
+            systems={this.props.systems}
+            products={this.props.products}
+            selectedProductId={this.state.selectedProductId}
+            onClosePopUp={this.onClosePopUp}
           />
         </div>
       );
@@ -105,45 +113,50 @@ var UnmatchedProducts = React.createClass({
   }
 });
 
-function unmatchedSystemsToRows(product, systems) {
-  return product.unmatchedSystemIds.map((sid) => {
-    var systemName = systems[sid].name;
-    var column = <TableCell content={systemName}/>;
-    var rawData = {systemName: systemName};
+var UnmatchedSystemPopUp = React.createClass({
+  rowComparator: function(a, b, columnIndex, ascending) {
+    var columnKeyInRawData = ["systemName"];
+    var columnKey = columnKeyInRawData[columnIndex];
+    var orderCondition = ascending ? 1 : -1;
+    var result = 0;
+    var aValue = a.props["rawData"][columnKey];
+    var bValue = b.props["rawData"][columnKey];
+    result = aValue.toLowerCase().localeCompare(bValue.toLowerCase());
+    return result * orderCondition;
+  },
 
-    return <TableRow columns={[column]} rawData={rawData} />
-  });
-}
+  buildRows: function(product, systems) {
+    return product.unmatchedSystemIds.map((sid) => {
+      var systemName = systems[sid].name;
+      var column = <TableCell content={systemName}/>;
+      var rawData = {systemName: systemName};
 
-function unmatchedProductsToRows(myParent) {
-  var products = myParent.props.products;
-  return myParent.props.unmatchedProductIds.map((pid) => {
-    var productName = products[pid].productName;
-    var systemCount = products[pid].unmatchedSystemCount;
-    var listButton =
-        <button
-          className="btn btn-default btn-cell"
-          onClick={function() {myParent.showPopUp(pid);}}
-          data-toggle="modal"
-          data-target="#unmatchedProductsPopUp">
-          {t("Show system list")}
-        </button>;
+      return <TableRow columns={[column]} rawData={rawData} />
+    });
+  },
 
-    var columns = [
-      <TableCell content={productName} />,
-      <TableCell content={systemCount} />,
-      <TableCell content={listButton} />,
-    ];
+  render: function() {
+    var popUpContent = this.props.selectedProductId == null ?
+      null :
+      <Table
+        headers={[t("System name")]}
+        rows={this.buildRows(this.props.products[this.props.selectedProductId], this.props.systems)}
+        rowComparator={this.rowComparator}
+        sortableColumnIndexes={[0]}
+        rowFilter={(tableRow, searchValue) => tableRow.props["rawData"]["systemName"].toLowerCase().indexOf(searchValue.toLowerCase()) > -1}
+        filterPlaceholder={t("Filter by name")}
+      />
+    ;
 
-    var rawData = {
-      id: pid,
-      productName: productName,
-      systemCount: systemCount
-    };
-
-    return <TableRow columns={columns} rawData={rawData} />
-  });
-}
+    return (
+      <PopUp title={t("Unmatched systems")}
+        id="unmatchedProductsPopUp"
+        content={popUpContent}
+        onClosePopUp={this.props.closePopUp}
+      />
+    );
+  }
+});
 
 module.exports = {
   UnmatchedProducts: UnmatchedProducts,
