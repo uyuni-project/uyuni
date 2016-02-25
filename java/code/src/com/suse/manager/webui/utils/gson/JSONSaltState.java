7e8545d3e02a727f4b34b4c7a70d8afd7e7a022d
Matei Albu <malbu@suse.de>
2016-02-25 10:31:42 +0100

    checkstyle cleanup

--- java/code/src/com/suse/manager/webui/utils/gson/JSONSaltState.java
+++ java/code/src/com/suse/manager/webui/utils/gson/JSONSaltState.java
@@ -1,7 +1,21 @@
+/**
+ * Copyright (c) 2016 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package com.suse.manager.webui.utils.gson;
 
 /**
- * Created by matei on 2/16/16.
+ * JSON representation of a custom Salt state.
  */
 public class JSONSaltState {
 
@@ -9,31 +23,51 @@ public class JSONSaltState {
 
     private boolean assigned;
 
-    public JSONSaltState(String name, boolean assigned) {
-        this.name = name;
-        this.assigned = assigned;
+    /**
+     * @param nameIn the name of the state
+     * @param assignedIn if it's assigned or not
+     */
+    public JSONSaltState(String nameIn, boolean assignedIn) {
+        this.name = nameIn;
+        this.assigned = assignedIn;
     }
 
+    /**
+     * @return the name of the custom state
+     */
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    /**
+     * @param nameIn the name of the custom state
+     */
+    public void setName(String nameIn) {
+        this.name = nameIn;
     }
 
+    /**
+     * @return true if the state is assigned
+     */
     public boolean isAssigned() {
         return assigned;
     }
 
-    public void setAssigned(boolean assigned) {
-        this.assigned = assigned;
+    /**
+     * @param assignedIn true if state is assigned
+     */
+    public void setAssigned(boolean assignedIn) {
+        this.assigned = assignedIn;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         JSONSaltState that = (JSONSaltState) o;
 
