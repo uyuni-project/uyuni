81774208d6f09aa29195b1c7d1f240fe3e38806a
Johannes Renner <jrenner@suse.com>
2016-03-02 18:21:27 +0100

    Allow to apply the highstate from the UI

--- web/html/src/manager/highstate.js
+++ web/html/src/manager/highstate.js
@@ -1,13 +1,19 @@
 'use strict';
 
-var React = require("react");
-var Panel = require("../components/panel").Panel;
+const React = require("react");
+const Messages = require("../components/messages").Messages;
+const AsyncButton = require("../components/buttons").AsyncButton;
+
+function msg(severityIn, textIn) {
+    return [{severity: severityIn, text: textIn}];
+}
 
 var Highstate = React.createClass({
 
     getInitialState: function() {
         var state = {
-            "highstate": []
+            "highstate": [],
+            "messages": null
         };
         return state;
     },
@@ -23,12 +29,55 @@ var Highstate = React.createClass({
         this.refreshHighstate();
     },
 
+    applyHighstate: function() {
+      const request = $.ajax({
+          type: "POST",
+          url: "/rhn/manager/api/states/apply",
+          data: JSON.stringify({
+              sid: serverId,
+              states: []
+          }),
+          contentType: "application/json",
+          dataType: "json"
+      })
+      .done( data => {
+          console.log("state.apply action scheduled:" + data)
+          this.setState({
+              messages: msg('info', <span>{t("Applying the highstate has been ")}
+                  <a href={"/rhn/systems/details/history/Event.do?sid=" + serverId + "&aid=" + data}>{t("scheduled")}</a>
+                  {t(".")}
+              </span>)
+          });
+      });
+      return Promise.resolve(request);
+    },
+
     render: function() {
+        const messages = this.state.messages ? <Messages items={this.state.messages}/> : null;
         return (
-            <Panel title="Highstate" icon="spacewalk-icon-virtual-host-manager">
-                <textarea className="form-control" rows="20" name="content"
-                        value={JSON.stringify(this.state.highstate, undefined, 4)} readOnly="true"/>
-            </Panel>
+            <span>
+            {messages}
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4>Show and apply the highstate for {serverName}</h4>
+                </div>
+                <div className="panel-body">
+                    <div className="form-horizontal">
+                        <div className="form-group">
+                            <div className="col-md-offset-3 col-md-6">
+                                <textarea className="form-control" rows="20"
+                                    value={this.state.highstate} readOnly="true" />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-md-offset-3 col-md-6">
+                                <AsyncButton action={this.applyHighstate} name={t("Apply")} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </span>
         );
     }
 });
