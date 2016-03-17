96e90f2cbc5268e4dad61afb7496c3ff1ce3f1b7
Johannes Renner <jrenner@suse.com>
2016-03-17 17:15:17 +0100

    Support package removals in the same way as installs/updates

--- java/code/src/com/suse/manager/webui/utils/salt/Pkg.java
+++ java/code/src/com/suse/manager/webui/utils/salt/Pkg.java
@@ -40,4 +40,18 @@ public class Pkg {
         return new LocalCall<>("pkg.install", Optional.empty(), Optional.of(kwargs),
                 new TypeToken<Map<String, Object>>() { });
     }
+
+    public static LocalCall<Map<String, Object>> remove(Map<String, String> pkgs) {
+        LinkedHashMap<String, Object> kwargs = new LinkedHashMap<>();
+
+        // Convert map into a list of maps with a single entry each as expected by Salt
+        List<Map<String, String>> pkgsList = pkgs.entrySet().stream()
+                .map(entry -> Collections.unmodifiableMap(Stream.of(entry).collect(
+                        Collectors.toMap(e -> e.getKey(), e -> e.getValue()))))
+                .collect(Collectors.toList());
+        kwargs.put("pkgs", pkgsList);
+
+        return new LocalCall<>("pkg.remove", Optional.empty(), Optional.of(kwargs),
+                new TypeToken<Map<String, Object>>() { });
+    }
 }
