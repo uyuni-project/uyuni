e164ca469c76a2316823cc16346a4f42fdf98570
Michael Calmer <mc@suse.de>
2016-03-31 15:21:57 +0200

    update repos in container

--- susemanager-utils/testing/docker/head/suma-head-base/add_repositories.sh
+++ susemanager-utils/testing/docker/head/suma-head-base/add_repositories.sh
@@ -4,6 +4,7 @@ set -e
 zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12:/GA/standard "SLE12 GA"
 zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12:/Update/standard/ "SLE12 Updates"
 zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12-SP1:/GA/standard "SLE12 SP1 GA"
-zypper ar -f http://download.suse.de/ibs/Devel:/Galaxy:/Manager:/Head:/SLE12_Products_Test/SLE_12_SP1/ "Devel:Galaxy:Manager:Head:SLE12_Products_Test"
+zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12-SP1:/Update/standard "SLE12 SP1 Update"
+zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12-SP1:/Update:/Products/standard "SLE12 SP1 Update Products"
 zypper ar -f http://download.suse.de/ibs/Devel:/Galaxy:/Manager:/Head/SLE_12_SP1/ "Devel:Galaxy:Manager:Head"
 zypper ar -f http://download.suse.de/ibs/Devel:/Galaxy:/pylint/SLE_12_SP1/ "pylint"
