023c7aed854e8c7b68bb3a68c99135aef15774b7
Michael Calmer <mc@suse.de>
2016-03-31 15:22:22 +0200

    drop pltcl in postgresql docker images

--- susemanager-utils/testing/docker/head/suma-head-pgsql/setup-db-postgres.sh
+++ susemanager-utils/testing/docker/head/suma-head-pgsql/setup-db-postgres.sh
@@ -8,7 +8,6 @@ MANAGER_DB_NAME="susemanager"
 rcpostgresql start
 
 su - postgres -c "createdb $MANAGER_DB_NAME ; echo \"CREATE ROLE $MANAGER_USER PASSWORD '$MANAGER_PASS' SUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;\" | psql"
-su - postgres -c "createlang pltclu '$MANAGER_DB_NAME'"
 echo "listen_addresses = '*'" >> /var/lib/pgsql/data/postgresql.conf
 echo "local $MANAGER_DB_NAME $MANAGER_USER md5
 host $MANAGER_DB_NAME $MANAGER_USER 127.0.0.1/8 md5
