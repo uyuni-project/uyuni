d48264cd2e1843164e7b1a8837a836ea14cbd720
Martin Seidl <mseidl@suse.de>
2016-03-31 11:38:34 +0200

    Updated finger detection due to ui truncation

--- features/step_definitions/salt_minions_page_steps.rb
+++ features/step_definitions/salt_minions_page_steps.rb
@@ -23,7 +23,10 @@ end
 When(/^I see my fingerprint$/) do
   output = `salt-call --local key.finger`
   fing = output.split("\n")[1].strip!
-  fail if not page.has_content?(fing)
+  partial_fing = find("strong[title=\"#{fing}\"]").text
+  partial_fing.gsub!('.','')
+  fail if not find("strong[title=\"#{fing}\"]")
+  fail if not fing.include?(partial_fing)
 end
 
 When(/^I accept this client's minion key$/) do
