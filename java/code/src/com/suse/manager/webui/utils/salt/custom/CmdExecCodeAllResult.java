334b0e5b53b9bdf376a340cc62fc3e48c9a915ae
Kevin Walter <kwalter@suse.com>
2016-04-04 15:00:53 +0200

    add datatypes mapping contents of JobReturnEvent data

---
+++ java/code/src/com/suse/manager/webui/utils/salt/custom/CmdExecCodeAllResult.java
@@ -0,0 +1,25 @@
+package com.suse.manager.webui.utils.salt.custom;
+
+public class CmdExecCodeAllResult {
+
+    private long pid;
+    private int retcode;
+    private String stderr;
+    private String stdout;
+
+    public long getPid() {
+        return pid;
+    }
+
+    public int getRetcode() {
+        return retcode;
+    }
+
+    public String getStderr() {
+        return stderr;
+    }
+
+    public String getStdout() {
+        return stdout;
+    }
+}
---
+++ java/code/src/com/suse/manager/webui/utils/salt/custom/Ret.java
@@ -0,0 +1,11 @@
+package com.suse.manager.webui.utils.salt.custom;
+
+
+public class Ret<T> {
+
+    private T ret;
+
+    public T getRet() {
+        return ret;
+    }
+}
