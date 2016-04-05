75bb7a4ce4480cda05a25122db496d3fde891eb3
Matei Albu <malbu@suse.de>
2016-04-05 19:02:53 +0200

    create pillar and salt temp dirs for all tests

--- java/code/src/com/suse/manager/webui/services/impl/test/SaltAPIServiceTest.java
+++ java/code/src/com/suse/manager/webui/services/impl/test/SaltAPIServiceTest.java
@@ -8,15 +8,11 @@ import com.redhat.rhn.domain.state.ServerStateRevision;
 import com.redhat.rhn.domain.state.StateFactory;
 import com.redhat.rhn.testing.BaseTestCaseWithUser;
 import com.redhat.rhn.testing.TestUtils;
-import com.suse.manager.webui.services.SaltCustomStateStorageManager;
 import com.suse.manager.webui.services.SaltStateGeneratorService;
 import com.suse.manager.webui.services.StateRevisionService;
 import com.suse.manager.webui.services.impl.SaltAPIService;
 import org.apache.commons.io.FileUtils;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
 
@@ -29,32 +25,6 @@ public class SaltAPIServiceTest extends BaseTestCaseWithUser {
 
     private static final String THE_STATE_CONTENT = "#the state content";
     public static final long ORG_ID = 1;
-
-    private Path slsDir;
-    private Path pillarDir;
-
-    /**
-     * {@inheritDoc}
-     */
-    public void setUp() throws Exception {
-        super.setUp();
-
-        slsDir = Files.createTempDirectory("salt-" + TestUtils.randomString());
-        pillarDir = Files.createTempDirectory("pillar-" + TestUtils.randomString());
-
-        SaltCustomStateStorageManager.INSTANCE.setBaseDirPath(slsDir.toString());
-        SaltStateGeneratorService.INSTANCE.setGeneratedSlsRoot(slsDir.toString());
-        SaltStateGeneratorService.INSTANCE.setGeneratedPillarRoot(pillarDir.toString());
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public void tearDown() throws Exception {
-        super.tearDown();
-        FileUtils.deleteDirectory(slsDir.toFile());
-        FileUtils.deleteDirectory(pillarDir.toFile());
-    }
 
     public void testDelete() throws Exception {
         SaltAPIService service = SaltAPIService.INSTANCE;
@@ -80,7 +50,7 @@ public class SaltAPIServiceTest extends BaseTestCaseWithUser {
 
         String stateFullName = "- manager_org_" + user.getOrg().getId() + "." + name;
 
-        Path generatedSlsPath = slsDir.resolve("custom").resolve("custom_" + server.getDigitalServerId() + ".sls");
+        Path generatedSlsPath = tmpSaltRoot.resolve("custom").resolve("custom_" + server.getDigitalServerId() + ".sls");
         String slsContent = FileUtils.readFileToString(generatedSlsPath.toFile());
 
         assertTrue(contains(slsContent, stateFullName));
