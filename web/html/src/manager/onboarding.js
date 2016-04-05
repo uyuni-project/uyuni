c2357d97c53c51dd853c4fc911ec739cb2167fcb
Dario Leidi <dleidi@suse.com>
2016-04-05 16:38:01 +0200

    Fix: show fingerprint and scroll if it overflows

--- web/html/src/manager/onboarding.js
+++ web/html/src/manager/onboarding.js
@@ -106,14 +106,6 @@ class Onboarding extends React.Component {
     });
   }
 
-  fingerprintRenderer (entity) {
-    var text = entity;
-    if (text.length > 42) {
-      text = "..." + entity.substring(entity.length-42);
-    }
-    return <strong title={entity}>{text}</strong>;
-  }
-
   render() {
     const minions = this.state.keys;
     const panelButtons = <div className="pull-right btn-group">
@@ -144,10 +136,11 @@ class Onboarding extends React.Component {
                   },{
                       "header": t("Fingerprint"),
                       "entryToCell": (entry) => entry.fingerprint,
-                      "renderCell": this.fingerprintRenderer,
+                      "renderCell": Renderer.highlightSubstring,
                       "sort": Comparators.locale,
                       "filter": Filters.substring,
-                      "ratio": 0.5
+                      "ratio": 0.5,
+                      "className": "fingerprintCell"
                   },{
                       "header": t("State"),
                       "entryToCell": (entry) => entry.state,
