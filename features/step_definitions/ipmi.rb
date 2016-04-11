465e569dc8ce34055f4d282ad9291b0dadd798f8
Martin Seidl <mseidl@suse.de>
2016-04-11 11:32:18 +0200

    Add timeouts for/rechecks for ipmi test

--- features/step_definitions/ipmi.rb
+++ features/step_definitions/ipmi.rb
@@ -13,6 +13,14 @@ end
 
 When /^I should see the power is "([^"]*)"$/ do |arg1|
   within(:xpath, "//*[@for='powerStatus']/..") do
+    10.times do
+      if not has_content?(arg1)
+        find(:xpath, '//button[.="Get Status"]').click
+      else
+        break
+      end
+      sleep 3
+    end
     fail if not has_content?(arg1)
   end
 end
