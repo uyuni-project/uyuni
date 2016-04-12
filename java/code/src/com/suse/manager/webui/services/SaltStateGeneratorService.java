ea9d42bc39c3e756236b57a0b4c214a9cb2227a3
Matei Albu <malbu@suse.de>
2016-04-12 10:33:38 +0200

    remove prefix and extension fields

--- java/code/src/com/suse/manager/webui/services/SaltStateGeneratorService.java
+++ java/code/src/com/suse/manager/webui/services/SaltStateGeneratorService.java
@@ -73,15 +73,9 @@ public enum SaltStateGeneratorService {
 
     private String generatedPillarRoot;
 
-    private String generatedPillarPrefix;
-
-    private String generatedPillarExtension;
-
     SaltStateGeneratorService() {
         generatedSlsRoot = SaltCustomStateStorageManager.GENERATED_SLS_ROOT;
         generatedPillarRoot = PILLAR_DATA_PATH;
-        generatedPillarPrefix = PILLAR_DATA_FILE_PREFIX;
-        generatedPillarExtension = PILLAR_DATA_FILE_EXT;
     }
 
     /**
@@ -148,9 +142,9 @@ public enum SaltStateGeneratorService {
     }
 
     private String getServerPillarFileName(Server server) {
-        return generatedPillarPrefix + "_" +
+        return PILLAR_DATA_FILE_PREFIX + "_" +
             server.asMinionServer().get().getMinionId() + "." +
-                generatedPillarExtension;
+                PILLAR_DATA_FILE_EXT;
     }
 
     /**
