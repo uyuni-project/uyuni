11dd86aa2457903c0a89405c6dca44b8e4540081
Bo Maryniuk <bo@suse.de>
2016-04-12 10:26:33 +0200

    Update docstring (cherry picked from commit 51d570c)

--- susemanager-utils/susemanager-sls/modules/pillar/suma_minion.py
+++ susemanager-utils/susemanager-sls/modules/pillar/suma_minion.py
@@ -3,35 +3,12 @@
 Retrieve SUSE Manager pillar data by the ID. File format is: <prefix>_<minion_id>.sls
 
 Parameters:
-    prefix - Any string, used as a prefix for the SLS filenames to match.
-             Default "minion".
-
-    file_extension - Extension to the pillar file.
-                     Default "sls"
-
-    environment - Used nvironment for pillar_roots
-                  Default first met from the configuration.
-
-    path - List of paths where to look for SLS pillar data.
-           Default is a list of "pillar_roots".
-
-    overwrite - Files will be processed in the order of the directories.
-                if there are few pillar files where inside values are interfering,
-                "overwrite: true" will replace an existing key with the new,
-                otherwise skip it.
-                Default: False
+    path - Path where SUSE Manager stores pillar data
 
 .. code-block:: yaml
 
     ext_pillar:
-      - suma_minion:
-          prefix: server
-          file_extension: sls
-          overwrite: true
-          environment: base
-          pillar_roots:
-            - /path/with/the/pillar/files
-            - /another/path/with/the/pillar/files
+      - suma_minion: /another/path/with/the/pillar/files
 
 '''
 
