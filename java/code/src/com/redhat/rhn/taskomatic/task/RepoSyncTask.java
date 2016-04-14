b498a0d9bdcf37960ab9f33684b9ed84539530ef
Silvio Moioli <smoioli@suse.de>
2016-04-14 11:33:20 +0200

    RepoSyncTask: expect bulk as well as single channel runs

--- java/code/src/com/redhat/rhn/taskomatic/task/RepoSyncTask.java
+++ java/code/src/com/redhat/rhn/taskomatic/task/RepoSyncTask.java
@@ -25,7 +25,9 @@ import org.quartz.JobExecutionException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Used for syncing repos (like yum repos) to a channel.
@@ -38,11 +40,9 @@ public class RepoSyncTask extends RhnJavaJob {
      */
     @Override
     @SuppressWarnings("unchecked")
-    public void execute(JobExecutionContext context)
-        throws JobExecutionException {
+    public void execute(JobExecutionContext context) {
+        List<Long> channelIds = getChannelIds(context.getJobDetail().getJobDataMap());
 
-        String channelIdString =
-            (String) context.getJobDetail().getJobDataMap().get("channel_id");
         String[] lparams = {"no-errata", "latest", "sync-kickstart", "fail"};
         List<String> ltrue = Arrays.asList("true", "1");
         List<String> params = new ArrayList<String>();
@@ -54,21 +54,59 @@ public class RepoSyncTask extends RhnJavaJob {
                 }
             }
         }
-        Long channelId;
-        try {
-            channelId = Long.parseLong(channelIdString);
-        }
-        catch (Exception e) {
-            throw new JobExecutionException("No valid channel_id given.");
-        }
 
-        Channel c = ChannelFactory.lookupById(channelId);
-        if (c == null) {
-            throw new JobExecutionException("No such channel with channel_id " + channelId);
+        for (Long channelId : channelIds) {
+            Channel channel = ChannelFactory.lookupById(channelId);
+            if (channel != null) {
+                log.info("Syncing repos for channel: " + channel.getName());
+
+                try {
+                    executeExtCmd(getSyncCommand(channel, params).toArray(new String[0]));
+                    channel.setLastSynced(new Date());
+                }
+                catch (JobExecutionException e) {
+                    log.error(e.getMessage());
+                }
+            }
+            else {
+                log.error("No such channel with channel_id " + channelId);
+            }
         }
-        log.info("Syncing repos for channel: " + c.getName());
-        executeExtCmd(getSyncCommand(c, params).toArray(new String[0]));
-        c.setLastSynced(new Date());
+    }
+
+    /**
+     * Gets the ids of channel(s) in a schedule/job data map.
+     *
+     * @param dataMap the data map
+     * @return the channel ids
+     */
+    @SuppressWarnings("unchecked")
+    public static List<Long> getChannelIds(final Map<String, Object> dataMap) {
+        List<Long> result = new LinkedList<>();
+        if (dataMap != null) {
+            // bulk reposync case
+            List<String> channelIds = (List<String>) dataMap.get("channel_ids");
+
+            // single reposync case
+            if (channelIds == null) {
+                channelIds = new LinkedList<>();
+                String channelId = (String) dataMap.get("channel_id");
+                if (channelId != null) {
+                    channelIds.add(channelId);
+                }
+            }
+
+            // String -> Long
+            for (String channelId : channelIds) {
+                try {
+                  result.add(Long.parseLong(channelId));
+                }
+                catch (NumberFormatException nfe) {
+                    // there is a channel id but it is not valid
+                }
+            }
+        }
+        return result;
     }
 
     private static List<String> getSyncCommand(Channel c, List<String> params) {
