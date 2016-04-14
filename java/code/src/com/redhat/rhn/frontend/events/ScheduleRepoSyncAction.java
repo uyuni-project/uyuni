1c2e9369b83b8be90dabcdcb698d3c77ac3abf34
Silvio Moioli <smoioli@suse.de>
2016-04-14 10:36:15 +0200

    ProductSyncManager: when scheduling reposyncs, use bulk mode through TaskomaticApi

--- java/code/src/com/redhat/rhn/frontend/events/ScheduleRepoSyncAction.java
+++ java/code/src/com/redhat/rhn/frontend/events/ScheduleRepoSyncAction.java
@@ -14,21 +14,19 @@
  */
 package com.redhat.rhn.frontend.events;
 
-import com.redhat.rhn.common.conf.ConfigDefaults;
 import com.redhat.rhn.common.messaging.EventMessage;
 import com.redhat.rhn.common.messaging.MessageAction;
-import com.redhat.rhn.domain.channel.ChannelFactory;
-import com.redhat.rhn.taskomatic.TaskomaticApiException;
+import com.redhat.rhn.domain.channel.Channel;
+import com.redhat.rhn.domain.org.Org;
+import com.redhat.rhn.domain.user.User;
+import com.redhat.rhn.domain.user.UserFactory;
+import com.redhat.rhn.manager.channel.ChannelManager;
+import com.redhat.rhn.taskomatic.TaskomaticApi;
 
 import org.apache.log4j.Logger;
 
-import java.net.MalformedURLException;
-import java.util.ArrayList;
 import java.util.List;
-
-import redstone.xmlrpc.XmlRpcClient;
-import redstone.xmlrpc.XmlRpcException;
-import redstone.xmlrpc.XmlRpcFault;
+import java.util.stream.Collectors;
 
 /**
  * ScheduleRepoSyncAction
@@ -45,47 +43,27 @@ public class ScheduleRepoSyncAction implements MessageAction {
     public void execute(EventMessage msg) {
         ScheduleRepoSyncEvent event = (ScheduleRepoSyncEvent) msg;
         if (logger.isDebugEnabled()) {
-            logger.debug("Scheduling repo sync for: " + event.getChannelLabel());
+            logger.debug("Scheduling repo sync for channels: " + event.getChannelLabels());
         }
-        scheduleRepoSync(event.getChannelLabel());
+        scheduleRepoSync(event.getChannelLabels(), event.getUserId());
     }
 
     /**
      * Schedule an immediate reposync via the Taskomatic API.
      *
-     * @param channelLabel label of the channel to sync
+     * @param channelLabels labels of the channel to sync
+     * @param userId id of user requesting the sync
      */
-    private void scheduleRepoSync(String channelLabel) {
-        List<String> labels = new ArrayList<String>();
-        labels.add(channelLabel);
-        @SuppressWarnings("unchecked")
-        List<Long> channelIds = ChannelFactory.getChannelIds(labels);
-        if (!channelIds.isEmpty()) {
-            this.rpcInvoke("tasko.scheduleSingleSatRepoSync", channelIds.get(0));
-        }
-    }
+    private void scheduleRepoSync(List<String> channelLabels, Long userId) {
+        User user = UserFactory.lookupById(userId);
+        if (user != null && !channelLabels.isEmpty()) {
+            Org org = user.getOrg();
 
-    /**
-     * Invoke an XMLRPC method from the client.
-     *
-     * @param name
-     * @param args
-     * @return
-     */
-    private Object rpcInvoke(String name, Object...args) {
-        try {
-            return new XmlRpcClient(ConfigDefaults.get()
-                    .getTaskoServerUrl(), false)
-                    .invoke(name, args);
-        }
-        catch (MalformedURLException e) {
-            throw new TaskomaticApiException(e);
-        }
-        catch (XmlRpcException e) {
-            throw new TaskomaticApiException(e);
-        }
-        catch (XmlRpcFault e) {
-            throw new TaskomaticApiException(e);
+            List<Channel> channels = channelLabels.stream()
+                .map(label -> ChannelManager.lookupByLabel(org, label))
+                .collect(Collectors.toList());
+
+            new TaskomaticApi().scheduleSingleRepoSync(channels);
         }
     }
 }
