1d7df6fec5909a6b193e6fee8f695cc92201d58e
Johannes Renner <jrenner@suse.com>
2016-04-26 18:50:31 +0200

    Initial support for multithreaded message handling

---
+++ java/code/src/com/suse/manager/reactor/messaging/MessageHandlerThreadPool.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2016 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.suse.manager.reactor.messaging;
+
+import org.apache.log4j.Logger;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Thread pool for parallel
+ */
+public enum MessageHandlerThreadPool {
+
+    /* Singleton instance of this class */
+    INSTANCE;
+
+    /* Logger for this class */
+    private static final Logger LOG = Logger.getLogger(MessageHandlerThreadPool.class);
+
+    /* The executor service to be used */
+    private ExecutorService executor = Executors.newFixedThreadPool(5);
+
+    /**
+     * Submit a {@link Runnable} for execution.
+     *
+     * @param runnable the runnable to submit
+     */
+    public Future<?> submit(Runnable runnable) {
+        LOG.debug("Submitting runnable");
+        return executor.submit(runnable);
+    }
+}
