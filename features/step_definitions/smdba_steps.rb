d96b715531bd296d37eb78aa92bf2de37bec2d46
Duncan Mac-Vicar P <dmacvicar@suse.de>
2016-04-27 22:48:08 +0200

    smdba: skip tests if no postgresql is there instead of marking them green

--- features/step_definitions/smdba_steps.rb
+++ features/step_definitions/smdba_steps.rb
@@ -1,8 +1,12 @@
 # Copyright 2011-2014 SUSE
-$is_postgres = true
-$msg = "This test has been disabled"
 
-Given(/^database is running$/) do
+Given(/^a postgresql database is running$/) do
+  $output = sshcmd("file /var/lib/pgsql/data/postgresql.conf", ignore_err: true)
+  if !$output[:stdout].include? "ASCII text"
+    puts "Tests require Postgresql database, skipping..."
+    pending
+  end
+
   if not sshcmd("smdba db-status")[:stdout].include? "online"
     sshcmd("smdba db-start")
     assert_includes(sshcmd("smdba db-status")[:stdout], "online")
@@ -15,101 +19,51 @@ Given(/^there is no such "(.*?)" directory$/) do |bkp_dir|
   sshcmd("test -d #{bkp_dir} && rm -rf #{bkp_dir}")
 end
 
-When(/^I cannot find file "(.*?)"$/) do |target_file|
-  $output = sshcmd("file #{target_file}", ignore_err: true)
-end
-
 When(/^I start database with the command "(.*?)"$/) do |start_command|
-  if $is_postgres
-    $output = sshcmd(start_command)
-  else
-    puts $msg
-  end
+  $output = sshcmd(start_command)
 end
 
 When(/^when I stop the database with the command "(.*?)"$/) do |stop_command|
-  if $is_postgres
-    $output = sshcmd(stop_command)
-  else
-    puts $msg
-  end
+  $output = sshcmd(stop_command)
 end
 
 When(/^when I check the database status with the command "(.*?)"$/) do |check_command|
-  if $is_postgres
-    $output = sshcmd(check_command)
-  else
-    puts $msg
-  end
+  $output = sshcmd(check_command)
 end
 
 When(/^I stop the database with the command "(.*?)"$/) do |stop_command|
-  if $is_postgres
-    $output = sshcmd(stop_command)
-  else
-    puts $msg
-  end
+  $output = sshcmd(stop_command)
 end
 
 Then(/^when I issue command "(.*?)"$/) do |command|
-  if $is_postgres
-    $output = sshcmd(command, ignore_err: true)
-  end
-end
-
-Then(/^I disable all the tests below$/) do
-  $is_postgres = $output[:stdout].include? "ASCII text"
+  $output = sshcmd(command, ignore_err: true)
 end
 
 When(/^when I see that the database is "(.*?)" or "(.*?)" as it might already running$/) do |scs_status, fl_status|
-  if $is_postgres
-    assert_match(/#{scs_status}|#{fl_status}/, $output[:stdout])
-  else
-    puts $msg
-  end
+  assert_match(/#{scs_status}|#{fl_status}/, $output[:stdout])
 end
 
 Then(/^I want to see if the database is "(.*?)"$/) do |status|
-  if $is_postgres
-    assert_includes($output[:stdout], status)
-  else
-    puts $msg
-  end
+  assert_includes($output[:stdout], status)
 end
 
 Then(/^I want to see if "(.*?)" is in the output$/) do |status|
-  if $is_postgres
-    assert_includes($output[:stdout], status)
-  else
-    puts $msg
-  end
+  assert_includes($output[:stdout], status)
 end
 
 When(/^when I configure "(.*?)" parameter "(.*?)" to "(.*?)"$/) do |config_file, param, value|
-  if $is_postgres
-    sshcmd("sed -i '/wal_level/d' #{config_file}", ignore_err: true)
-    sshcmd("echo \"#{param} = #{value}\" >> #{config_file}", ignore_err: true)
-    local_output = sshcmd("cat #{config_file} | grep #{param}", ignore_err: true)
-    assert_includes(local_output[:stdout], value)
-  else
-    puts $msg
-  end
+  sshcmd("sed -i '/wal_level/d' #{config_file}", ignore_err: true)
+  sshcmd("echo \"#{param} = #{value}\" >> #{config_file}", ignore_err: true)
+  local_output = sshcmd("cat #{config_file} | grep #{param}", ignore_err: true)
+  assert_includes(local_output[:stdout], value)
 end
 
 Then(/^when I check internally configuration for "(.*?)" option$/) do |config_key|
-  if $is_postgres
-    $current_checked_config_value = sshcmd("cd /;sudo -u postgres psql -c 'show wal_level;'")[:stdout]
-  else
-    puts $msg
-  end
+  $current_checked_config_value = sshcmd("cd /;sudo -u postgres psql -c 'show wal_level;'")[:stdout]
 end
 
 Then(/^I expect to see the configuration is set to "(.*?)"$/) do |value|
-  if $is_postgres
-    assert_includes($current_checked_config_value, value)
-  else
-    puts $msg
-  end
+  assert_includes($current_checked_config_value, value)
 end
 
 Then(/^I issue command "(.*?)"$/) do |cmd|
@@ -117,181 +71,108 @@ Then(/^I issue command "(.*?)"$/) do |cmd|
 end
 
 Then(/^I find tablespaces "(.*?)" and "(.*?)"$/) do |suma_ts, pg_ts|
-  if $is_postgres
-    assert_includes($output[:stdout], suma_ts)
-    assert_includes($output[:stdout], pg_ts)
-  else
-    puts $msg
-  end
+  assert_includes($output[:stdout], suma_ts)
+  assert_includes($output[:stdout], pg_ts)
 end
 
 Then(/^I find core examination is "(.*?)", database analysis is "(.*?)" and space reclamation is "(.*?)"$/) do |arg1, arg2, arg3|
-  if $is_postgres
-    refute_includes($output[:stdout], "failed")
-  else
-    puts $msg
-  end
+  refute_includes($output[:stdout], "failed")
 end
 
 Then(/^I find "(.*?)", "(.*?)" and "(.*?)" are in the list\.$/) do |rhn_tbl, rhn_tbl1, suse_tbl|
-  if $is_postgres
-    [rhn_tbl, rhn_tbl1, suse_tbl].each do |tbl|
-      assert_includes($output[:stdout], tbl)
-    end
-  else
-    puts $msg
+  [rhn_tbl, rhn_tbl1, suse_tbl].each do |tbl|
+    assert_includes($output[:stdout], tbl)
   end
 end
-
 
 #
 # Backup-related tests
 #
 Given(/^database "(.*?)" has no table "(.*?)"$/) do |dbname, tbl|
   $db = dbname
-  if $is_postgres
-    out = sshcmd("sudo -u postgres psql -d #{$db} -c 'drop table dummy'", ignore_err: true)
-    refute_includes(out[:stdout], "DROP TABLE")
-    assert_includes(out[:stderr], "table \"#{tbl}\" does not exist")
-  else
-    puts $msg
-  end
+  out = sshcmd("sudo -u postgres psql -d #{$db} -c 'drop table dummy'", ignore_err: true)
+  refute_includes(out[:stdout], "DROP TABLE")
+  assert_includes(out[:stderr], "table \"#{tbl}\" does not exist")
 end
 
 When(/^I create backup directory "(.*?)" with UID "(.*?)" and GID "(.*?)"$/) do |bkp_dir, uid, gid|
-  if $is_postgres
-    sshcmd("mkdir /#{bkp_dir};chown #{uid}:#{gid} /#{bkp_dir}")
-    bkp_dir.sub!("/", "")
-    puts "Backup directory:"
-    puts sshcmd("ls -la / | /usr/bin/grep #{bkp_dir}")[:stdout]
-  else
-    puts $msg
-  end
+  sshcmd("mkdir /#{bkp_dir};chown #{uid}:#{gid} /#{bkp_dir}")
+  bkp_dir.sub!("/", "")
+  puts "Backup directory:"
+  puts sshcmd("ls -la / | /usr/bin/grep #{bkp_dir}")[:stdout]
 end
 
 Then(/^I should see error message that asks "(.*?)" belong to the same UID\/GID as "(.*?)" directory$/) do |bkp_dir, data_dir|
-  if $is_postgres
-    assert_includes($output[:stderr],
-                    "The \"#{bkp_dir}\" directory must belong to the same user and group as \"#{data_dir}\" directory.")
-  else
-    puts $msg
-  end
+  assert_includes($output[:stderr],
+                  "The \"#{bkp_dir}\" directory must belong to the same user and group as \"#{data_dir}\" directory.")
 end
 
 Then(/^I should see error message that asks "(.*?)" has same permissions as "(.*?)" directory$/) do |bkp_dir, data_dir|
-  if $is_postgres
-    assert_includes($output[:stderr],
-                    "The \"#{bkp_dir}\" directory must have the same permissions as \"#{data_dir}\" directory.")
-  else
-    puts $msg
-  end
+  assert_includes($output[:stderr],
+                  "The \"#{bkp_dir}\" directory must have the same permissions as \"#{data_dir}\" directory.")
 end
 
 Then(/^I remove backup directory "(.*?)"$/) do |bkp_dir|
-  if $is_postgres
-    sshcmd("test -d #{bkp_dir} && rm -rf #{bkp_dir}")
-  else
-    puts "This step can be done only with PostgreSQL database."
-  end
+  sshcmd("test -d #{bkp_dir} && rm -rf #{bkp_dir}")
 end
 
 When(/^when I change Access Control List on "(.*?)" directory to "(.*?)"$/) do |bkp_dir, acl_octal|
-  if $is_postgres
-    bkp_dir.sub!("/", "")
-    sshcmd("test -d /#{bkp_dir} && chmod #{acl_octal} /#{bkp_dir}")
-    puts "Backup directory, ACL to \"#{acl_octal}\":"
-    puts sshcmd("ls -la / | /usr/bin/grep #{bkp_dir}")[:stdout]
-    puts "\n*** Taking backup, this might take a while ***\n"
-  else
-    puts $msg
-  end
+  bkp_dir.sub!("/", "")
+  sshcmd("test -d /#{bkp_dir} && chmod #{acl_octal} /#{bkp_dir}")
+  puts "Backup directory, ACL to \"#{acl_octal}\":"
+  puts sshcmd("ls -la / | /usr/bin/grep #{bkp_dir}")[:stdout]
+  puts "\n*** Taking backup, this might take a while ***\n"
 end
 
 Then(/^base backup is taken$/) do
-  if $is_postgres
-    assert_includes($output[:stdout], "Finished")
-  else
-    puts $msg
-  end
+  assert_includes($output[:stdout], "Finished")
 end
 
 Then(/^in "(.*?)" directory there is "(.*?)" file and at least one backup checkpoint file$/) do |bkp_dir, archive_file|
-  if $is_postgres
-    refute_includes(
-      sshcmd("test -f #{bkp_dir}/#{archive_file} && echo \"exists\" || echo \"missing\"")[:stdout],
-      "missing")
-    refute_includes(
-      sshcmd("ls #{bkp_dir}/*.backup 1>/dev/null 2>/dev/null && echo \"exists\" || echo \"missing\"")[:stdout],
-      "missing")
-  else
-    puts $msg
-  end
+  refute_includes(
+    sshcmd("test -f #{bkp_dir}/#{archive_file} && echo \"exists\" || echo \"missing\"")[:stdout],
+    "missing")
+  refute_includes(
+    sshcmd("ls #{bkp_dir}/*.backup 1>/dev/null 2>/dev/null && echo \"exists\" || echo \"missing\"")[:stdout],
+    "missing")
 end
 
 Then(/^parameter "(.*?)" in the configuration file "(.*?)" is "(.*?)"$/) do |param, cfg_file, fuzzy_value|
-  if $is_postgres
-    $output = sshcmd("cat #{cfg_file} | grep #{param}")
-    assert_includes($output[:stdout], fuzzy_value)
-  else
-    puts $msg
-  end
+  $output = sshcmd("cat #{cfg_file} | grep #{param}")
+  assert_includes($output[:stdout], fuzzy_value)
 end
 
 Then(/^"(.*?)" destination should be set to "(.*?)" in configuration file$/) do |arch_cmd, dest_dir|
-  if $is_postgres
-    assert_includes($output[:stdout], dest_dir)
-  else
-    puts $msg
-  end
+  assert_includes($output[:stdout], dest_dir)
 end
 
 When(/^I set a checkpoint$/) do
-  if $is_postgres
-    sshcmd("sudo -u postgres psql -d #{$db} -c 'checkpoint' 2>/dev/null", ignore_err: true)
-  else
-    puts "This step is required only for PostgreSQL database"
-  end
+  sshcmd("sudo -u postgres psql -d #{$db} -c 'checkpoint' 2>/dev/null", ignore_err: true)
 end
 
 When(/^when in the database I create dummy table "(.*?)" with column "(.*?)" and value "(.*?)"$/) do |tbl, clm, val|
-  if $is_postgres
-    fn = "/tmp/smdba-data-test.sql"
-    sshcmd("echo \"create table #{tbl} (#{clm} varchar);insert into #{tbl} (#{clm}) values (\'#{val}\');\" > #{fn}", ignore_err: false)
-    sshcmd("sudo -u postgres psql -d #{$db} -c 'drop table dummy' 2>/dev/null", ignore_err: true)
-    sshcmd("sudo -u postgres psql -d #{$db} -af #{fn}", ignore_err: true)
-    sshcmd("file -f #{fn} && rm #{fn}")
-    assert_includes(
-      sshcmd("sudo -u postgres psql -d #{$db} -c 'select * from dummy' 2>/dev/null", ignore_err: true)[:stdout],
-      val)
-    puts "Table \"#{tbl}\" has been created with some dummy data inside"
-  else
-    puts $msg
-  end
+  fn = "/tmp/smdba-data-test.sql"
+  sshcmd("echo \"create table #{tbl} (#{clm} varchar);insert into #{tbl} (#{clm}) values (\'#{val}\');\" > #{fn}", ignore_err: false)
+  sshcmd("sudo -u postgres psql -d #{$db} -c 'drop table dummy' 2>/dev/null", ignore_err: true)
+  sshcmd("sudo -u postgres psql -d #{$db} -af #{fn}", ignore_err: true)
+  sshcmd("file -f #{fn} && rm #{fn}")
+  assert_includes(
+    sshcmd("sudo -u postgres psql -d #{$db} -c 'select * from dummy' 2>/dev/null", ignore_err: true)[:stdout],
+    val)
+  puts "Table \"#{tbl}\" has been created with some dummy data inside"
 end
 
 When(/^when I restore database from the backup$/) do
-  if $is_postgres
-    puts "\n*** Restoring database from the backup. This will may take a while. ***\n\n"
-    sshcmd("smdba backup-restore")
-  else
-    puts $msg
-  end
+  puts "\n*** Restoring database from the backup. This will may take a while. ***\n\n"
+  sshcmd("smdba backup-restore")
 end
 
 Then(/^I disable backup in the directory "(.*?)"$/) do |arg1|
-  if $is_postgres
-    assert_includes(
-      sshcmd("smdba backup-hot --enable=off")[:stdout], "Finished")
-  else
-    puts $msg
-  end
+  assert_includes(
+    sshcmd("smdba backup-hot --enable=off")[:stdout], "Finished")
 end
 
 When(/^when I destroy "(.*?)" directory$/) do |pg_xlog|
-  if $is_postgres
-    sshcmd("rm -rf #{pg_xlog}")
-  else
-    puts "This step is required only for PostgreSQL database"
-  end
+  sshcmd("rm -rf #{pg_xlog}")
 end
 
