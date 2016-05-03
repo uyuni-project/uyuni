fee27fa8886b3864bc89617722e4088ecf8f0475
Matei Albu <malbu@suse.de>
2016-05-03 18:10:21 +0200

    use pillar and static states to install/remove packages (bsc#975424)

--- java/code/src/com/suse/manager/webui/utils/salt/State.java
+++ java/code/src/com/suse/manager/webui/utils/salt/State.java
@@ -7,6 +7,10 @@ import com.suse.salt.netapi.calls.LocalCall;
 
 import com.google.gson.reflect.TypeToken;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -20,4 +24,13 @@ public class State {
         return new LocalCall<>("state.show_highstate", Optional.empty(), Optional.empty(),
                 new TypeToken<Object>() { });
     }
+
+    public static LocalCall<Map<String, Object>> apply(String mod, Map<String, Object> pillar) {
+        LinkedHashMap args = new LinkedHashMap();
+        args.put("mods", Collections.singletonList(mod));
+        args.put("pillar", pillar);
+        return new LocalCall("state.apply", Optional.empty(), Optional.of(args),
+                new TypeToken<Map<String, Object>>() { });
+    }
+
 }
