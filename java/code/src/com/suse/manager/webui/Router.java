85729497681c450f005162d98da42be642197e53
Dario Leidi <dleidi@suse.com>
2016-05-16 16:50:43 +0200

    BugFix: prevent non org-admin user accept/reject/delete a minion (bsc#979686)

--- java/code/src/com/suse/manager/webui/Router.java
+++ java/code/src/com/suse/manager/webui/Router.java
@@ -79,9 +79,9 @@ public class Router implements SparkApplication {
         post("/manager/api/minions/cmd", withUser(MinionsAPI::run));
         get("/manager/api/minions/match", withUser(MinionsAPI::match));
         get("/manager/api/minions/keys", MinionsAPI::listKeys);
-        post("/manager/api/minions/keys/:target/accept", MinionsAPI::accept);
-        post("/manager/api/minions/keys/:target/reject", MinionsAPI::reject);
-        post("/manager/api/minions/keys/:target/delete", MinionsAPI::delete);
+        post("/manager/api/minions/keys/:target/accept", withOrgAdmin(MinionsAPI::accept));
+        post("/manager/api/minions/keys/:target/reject", withOrgAdmin(MinionsAPI::reject));
+        post("/manager/api/minions/keys/:target/delete", withOrgAdmin(MinionsAPI::delete));
 
         // States API
         post("/manager/api/states/apply", withUser(StatesAPI::apply));
