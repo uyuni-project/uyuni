e2fbcb3605860f2137a22ed4c768d6610b6343f2
Johannes Renner <jrenner@suse.com>
2016-05-24 13:24:19 +0200

    Use queue=true for all calls to state.apply (bsc#980556)

--- java/code/src/com/suse/manager/webui/utils/salt/State.java
+++ java/code/src/com/suse/manager/webui/utils/salt/State.java
@@ -7,6 +7,7 @@ import com.suse.salt.netapi.calls.LocalCall;
 
 import com.google.gson.reflect.TypeToken;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,8 +31,16 @@ public class State {
         if (pillar.isPresent()) {
             args.put("pillar", pillar.get());
         }
+        args.put("queue", true);
         return new LocalCall("state.apply", Optional.empty(), Optional.of(args),
                 new TypeToken<Map<String, Object>>() { });
     }
 
+    public static LocalCall<Map<String, Object>> apply(List<String> mods) {
+        return apply(mods, Optional.empty());
+    }
+
+    public static LocalCall<Map<String, Object>> apply(String... mods) {
+        return apply(Arrays.asList(mods), Optional.empty());
+    }
 }
