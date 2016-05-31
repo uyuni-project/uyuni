5a452427e67a0b25a15475a65b4e3a72dec1c237
Bo Maryniuk <bo@suse.de>
2016-05-31 17:09:28 +0200

    Add lscpu unit test

--- susemanager-utils/susemanager-sls/src/tests/test_grains_cpuinfo.py
+++ susemanager-utils/susemanager-sls/src/tests/test_grains_cpuinfo.py
@@ -26,3 +26,37 @@ def test_total_num_cpus():
             assert 'total_num_cpus' in cpus
             assert cpus['total_num_cpus'] == 4
 
+
+def test_cpusockets_parse_cpuinfo():
+    '''
+    Test parse_cpuinfo sub in cpusockets function.
+
+    :return:
+    '''
+
+
+def test_cpusockets_lscpu():
+    '''
+    Test lscpu sub in cpusockets function.
+
+    :return:
+    '''
+    lscpu_out = """
+# The following is the parsable format, which can be fed to other
+# programs. Each different item in every column has an unique ID
+# starting from zero.
+# CPU,Core,Socket,Node,,L1d,L1i,L2,L3
+0,0,0,0,,0,0,0,0
+1,0,0,0,,0,0,0,0
+2,1,0,0,,1,1,1,0
+3,1,0,0,,1,1,1,0
+    """
+
+    cpuinfo.log = MagicMock()
+    with patch('salt.utils.which_bin', MagicMock(return_value="/bogus/path")):
+        with patch.dict(cpuinfo.__salt__, {'cmd.run_all': MagicMock(return_value={'retcode': 0, 'stdout': lscpu_out})}):
+            out = cpuinfo._lscpu()
+            assert type(out) == dict
+            assert 'cpusockets' in out
+            assert out['cpusockets'] == 1
+
