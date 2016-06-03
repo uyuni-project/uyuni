84181cbbc63a18e7af76e2861788b47adcbb62ce
Michael Calmer <mc@suse.de>
2016-06-03 16:26:45 +0200

    fix comments

--- susemanager-utils/testing/docker/head/suma-head-gatherer/add_repositories.sh
+++ susemanager-utils/testing/docker/head/suma-head-gatherer/add_repositories.sh
@@ -6,5 +6,6 @@ zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12:/Update/standard/ "SLE12 U
 zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12-SP1:/GA/standard "SLE12 SP1 GA"
 zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12-SP1:/Update/standard "SLE12 SP1 Update"
 zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12-SP1:/Update:/Products/standard "SLE12 SP1 Update Products"
-zypper ar -f http://download.suse.de/ibs/Devel:/Galaxy:/pylint/SLE_12_SP1/ "pylint"
 zypper ar -f http://download.suse.de/ibs/Devel:/Galaxy:/Manager:/Head/SLE_12_SP1/ "Devel:Galaxy:Manager:Head"
+zypper ar -f http://download.suse.de/ibs/Devel:/Galaxy:/pylint/SLE_12_SP1/ "pylint"
+
