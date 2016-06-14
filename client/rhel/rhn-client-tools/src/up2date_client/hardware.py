855ca46d38be2e0dc7ce47dfdd90836b7e57abd0
Matei Albu <malbu@suse.de>
2016-06-14 13:03:21 +0200

    machine_info capability and check for client tool compatibility with Suma 2.1

--- client/rhel/rhn-client-tools/src/up2date_client/hardware.py
+++ client/rhel/rhn-client-tools/src/up2date_client/hardware.py
@@ -854,6 +854,9 @@ def get_sysinfo():
 
 
 def read_machineinfo():
+    s = rhnserver.RhnServer()
+    if not s.capabilities.hasCapability('machine_info'):
+        return
     machineinfo = {}
     machineinfo["class"] = "MACHINEINFO"
     machine_id = getMachineId()
