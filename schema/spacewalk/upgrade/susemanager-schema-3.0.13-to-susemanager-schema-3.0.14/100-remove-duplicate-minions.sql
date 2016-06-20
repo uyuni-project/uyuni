d00c64ff3cef189b02de1c1e170d81e65a6925f8
Michele Bologna <michele.bologna@suse.com>
2016-06-20 16:51:45 +0200

    Fix indentation, camelCase for tables
    Signed-off-by: Michele Bologna <michele.bologna@suse.com>

--- schema/spacewalk/upgrade/susemanager-schema-3.0.13-to-susemanager-schema-3.0.14/100-remove-duplicate-minions.sql
+++ schema/spacewalk/upgrade/susemanager-schema-3.0.13-to-susemanager-schema-3.0.14/100-remove-duplicate-minions.sql
@@ -1,13 +1,12 @@
 DELETE FROM 
-	rhnservernetwork 
-	WHERE id IN 
-	(
-		SELECT DISTINCT(a.id)
-		FROM rhnservernetwork a, rhnservernetwork b, suseminioninfo c
-		WHERE
-			1=1
-			AND a.server_id = b.server_id
-			AND a.id < b.id
-			AND a.server_id = c.server_id
-	)
+  rhnServerNetwork
+    WHERE id IN
+    (
+        SELECT DISTINCT(a.id)
+        FROM rhnServerNetwork a, rhnServerNetwork b, suseMinionInfo c
+        WHERE
+            a.server_id = b.server_id
+            AND a.id < b.id
+            AND a.server_id = c.server_id
+    )
 ;
