9e1ce43e4b9c0d60a63cf876ec76d40ad39b089b
Matei Albu <malbu@suse.de>
2016-06-21 17:56:33 +0200

    use bootstrap mark for search highlight

--- web/html/src/components/tableng.js
+++ web/html/src/components/tableng.js
@@ -262,7 +262,7 @@ function Highlight(props) {
   let chunk3 = text.substring(pos + high.length, text.length);
 
   chunk1 = chunk1 ? <span key="m1">{chunk1}</span> : null;
-  chunk2 = chunk2 ? <span key="m2" style={{backgroundColor: "#f0ad4e", borderRadius: "2px"}}>{ chunk2 }</span> : null;
+  chunk2 = chunk2 ? <span key="m2" style={{borderRadius: "2px"}}><mark>{ chunk2 }</mark></span> : null;
   chunk3 = chunk3 ? <span key="m3">{chunk3}</span> : null;
 
   return <span key="hl">{chunk1}{chunk2}{chunk3}</span>;
