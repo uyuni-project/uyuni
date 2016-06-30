e2a55af16ce714ef43d14f056797d736e4a0af18
Dario Leidi <dleidi@suse.com>
2016-06-30 18:05:05 +0200

    Formatting code

--- web/html/src/manager/taskotop.js
+++ web/html/src/manager/taskotop.js
@@ -62,15 +62,8 @@ const TaskoTop = React.createClass({
         <TableCell key="runId" content={rawJob["id"]} />,
         <TableCell key="name" content={rawJob["name"]} />,
         <TableCell key="startTime" content={moment(rawJob["startTime"]).format("HH:mm:ss")} />,
-        <TableCell key="endTime" content={
-            rawJob["endTime"] == null ?
-            "" :
-            moment(rawJob["endTime"]).format("HH:mm:ss")} />,
-        <TableCell key="elapsedTime" content={
-            rawJob["elapsedTime"] == null ?
-            "" :
-            rawJob["elapsedTime"] + ' seconds'
-          } />,
+        <TableCell key="endTime" content={rawJob["endTime"] == null ? "" : moment(rawJob["endTime"]).format("HH:mm:ss")} />,
+        <TableCell key="elapsedTime" content={rawJob["elapsedTime"] == null ? "" : rawJob["elapsedTime"] + ' seconds'} />,
         <TableCell key="status" content={thisObject.decodeStatus(rawJob["status"])} />,
         <TableCell key="data" content={rawJob["data"].map(c => <div>{c}</div>)} />
       ];
