46fdd46506d8a38775d8a5a7dbecda5cb4328de0
Dario Leidi <dleidi@suse.com>
2016-07-01 15:38:06 +0200

    Add missing power_management feature for bootstrap_entitlement systems

--- schema/spacewalk/upgrade/susemanager-schema-3.1.0-to-susemanager-schema-3.5.0/9002-enable-entitlements-on-new-features.sql
+++ schema/spacewalk/upgrade/susemanager-schema-3.1.0-to-susemanager-schema-3.5.0/9002-enable-entitlements-on-new-features.sql
@@ -94,3 +94,11 @@ select lookup_sg_type('salt_entitled'), lookup_feature_type('ftr_system_custom_v
 where not exists ( select 1 from rhnServerGroupTypeFeature
     where server_group_type_id = lookup_sg_type('salt_entitled')
       and feature_id = lookup_feature_type('ftr_system_custom_values') );
+
+insert into rhnServerGroupTypeFeature (server_group_type_id, feature_id,
+                                       created, modified)
+select lookup_sg_type('bootstrap_entitled'), lookup_feature_type('ftr_power_management'),
+        current_timestamp,current_timestamp from dual
+where not exists ( select 1 from rhnServerGroupTypeFeature
+    where server_group_type_id = lookup_sg_type('bootstrap_entitled')
+      and feature_id = lookup_feature_type('ftr_power_management') );
