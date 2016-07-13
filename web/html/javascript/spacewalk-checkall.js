ef138c87a7b7d09217408cc9302cb93da29f301f
Dario Leidi <dleidi@suse.com>
2016-07-13 11:03:45 +0200

    Add javascript function that allows only numeric values

--- web/html/javascript/spacewalk-checkall.js
+++ web/html/javascript/spacewalk-checkall.js
@@ -227,3 +227,17 @@ function disableAutoComplete(){
 			document.getElementsByTagName('input').item(i).setAttribute('autocomplete', 'off');
 	};
 }
+
+/*
+* Allow only numeric values
+*/
+function numericValidate(evt) {
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode( key );
+  var regex = /[0-9]/;
+  if( !regex.test(key) ) {
+    theEvent.returnValue = false;
+    if(theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
