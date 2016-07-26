a012df8f4dc8572ba2a689055b64ae4f840c2fd8
Duncan Mac-Vicar P <dmacvicar@suse.de>
2016-07-26 11:30:48 +0200

    add tests for static content as well

--- features/security.feature
+++ features/security.feature
@@ -8,12 +8,20 @@ Feature: Basic web security measures and recommendations
     Then the response header "Cache-Control" should be "no-cache,no-store,must-revalidate,private"
     And the response header "Pragma" should be "no-cache"
     And the response header "Expires" should be "0"
-    And the response header "Set-Cookie" should include ";HttpOnly;Secure"
-    And the response header "X-Frame-Options" should include "SAMEORIGIN"
+    And the response header "Set-Cookie" should contain ";HttpOnly;Secure"
+    And the response header "X-Frame-Options" should contain "SAMEORIGIN"
     And the response header "X-XSS-Protection" should be "1; mode=block"
     And the response header "X-Content-Type-Options" should be "nosniff"
     And the response header "X-Permitted-Cross-Domain-Policies" should be "master-only"
 
-
-
+  Scenario: Caching should be enabled for static content
+    Given I retrieve any static resource
+    Then the response header "Cache-Control" should be "max-age=86400, public"
+    And the response header "Pragma" should not be present
+    And the response header "Expires" should not be "0"
+    And the response header "Set-Cookie" should not be present
+    And the response header "X-Frame-Options" should contain "SAMEORIGIN"
+    And the response header "X-XSS-Protection" should be "1; mode=block"
+    And the response header "X-Content-Type-Options" should be "nosniff"
+    And the response header "X-Permitted-Cross-Domain-Policies" should be "master-only"
 
--- features/step_definitions/security_steps.rb
+++ features/step_definitions/security_steps.rb
@@ -19,15 +19,39 @@ end
 
 And(/^the response header "(.*?)" should be "(.*?)"$/) do |arg1, arg2|
   begin
-    assert_equal(Capybara.current_session.response_headers[arg1], arg2)
+    assert_includes(Capybara.current_session.response_headers.keys, arg1,
+                    "Header '#{arg1}' not present in #{Capybara.current_session.current_url}'")
+    assert_equal(arg2, Capybara.current_session.response_headers[arg1],
+                 "Header '#{arg1}' in #{Capybara.current_session.current_url}' is not '#{arg2}'")
   rescue Capybara::NotSupportedByDriverError
     pending('Current driver does not support checking response headers')
   end
 end
 
-Then(/^the response header "(.*?)" should include "(.*?)"$/) do |arg1, arg2|
+And(/^the response header "(.*?)" should not be "(.*?)"$/) do |arg1, arg2|
   begin
-    assert_includes(Capybara.current_session.response_headers[arg1], arg2)
+    refute_equal(arg2, Capybara.current_session.response_headers[arg1],
+                 "Header '#{arg1}' in #{Capybara.current_session.current_url}' is '#{arg2}'")
+  rescue Capybara::NotSupportedByDriverError
+    pending('Current driver does not support checking response headers')
+  end
+end
+
+Then(/^the response header "(.*?)" should contain "(.*?)"$/) do |arg1, arg2|
+  begin
+    assert_includes(Capybara.current_session.response_headers.keys, arg1,
+                    "Header '#{arg1}' not present in #{Capybara.current_session.current_url}'")
+    assert_includes(Capybara.current_session.response_headers[arg1], arg2,
+                    "Header '#{arg1}' in #{Capybara.current_session.current_url}' does not contain '#{arg2}'")
+  rescue Capybara::NotSupportedByDriverError
+    pending('Current driver does not support checking response headers')
+  end
+end
+
+Then(/^the response header "(.*?)" should not be present$/) do |arg1|
+  begin
+    refute_includes(Capybara.current_session.response_headers.keys, arg1,
+                    "Header '#{arg1}' present in #{Capybara.current_session.current_url}'")
   rescue Capybara::NotSupportedByDriverError
     pending('Current driver does not support checking response headers')
   end
