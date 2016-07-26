7ac228d333c9c44d384b964b932b5428c30023e4
Duncan Mac-Vicar P <dmacvicar@suse.de>
2016-07-26 14:42:17 +0200

    trailing space

--- features/step_definitions/snapshot_steps.rb
+++ features/step_definitions/snapshot_steps.rb
@@ -2,6 +2,6 @@
 
 When(/^I take a snapshot "([^"]*)"$/) do |name|
   $sshout = ""
-  $sshout = `echo | ssh -o StrictHostKeyChecking=no root@$VHOST qemu-img snapshot -c #{name} $IMGDIR/$VMDISK.qcow2 `
+  $sshout = `echo | ssh -o StrictHostKeyChecking=no root@$VHOST qemu-img snapshot -c #{name} $IMGDIR/$VMDISK.qcow2`
   puts "Creating snapsnot failed..." unless $?.success?
 end
