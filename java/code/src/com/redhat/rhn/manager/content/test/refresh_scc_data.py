b8b2952b55f4992d2afa4b7e3e2cdeeb05d3b58e
Silvio Moioli <smoioli@suse.de>
2016-07-27 10:48:15 +0200

    refresh_scc_data.py: use httplib instead of httplib2

--- java/code/src/com/redhat/rhn/manager/content/test/refresh_scc_data.py
+++ java/code/src/com/redhat/rhn/manager/content/test/refresh_scc_data.py
@@ -1,50 +1,55 @@
 #!/usr/bin/env python
 
-import httplib2
-import re
+import base64
+import httplib
 import json
+import re
 import sys
 
-def find_next_url(resp):
-  if "link" in resp:
-    regex = re.compile(r'<([^>]+)>; rel="next"')
-    match = regex.search(resp["link"])
+def find_next_path(resp):
+  link = resp.getheader("Link")
+  if link is not None:
+    regex = re.compile(r'<https://scc.suse.com([^>]+)>; rel="next"')
+    match = regex.search(link)
     if match is not None:
         return match.group(1)
 
-def get_paginated(http, url):
+def get_paginated(connection, headers, path):
   result = []
-  current_url = url
-  while current_url is not None:
-    (resp, content) = http.request(current_url, "GET")
+  current_path = path
+  while current_path is not None:
+    connection.request("GET", current_path, headers=headers)
+    resp = connection.getresponse()
+    content = resp.read()
 
     if resp.status != 200:
-        print("Unexpected HTTP status received on %s: %d" % (current_url, resp.status))
+        print("Unexpected HTTP status received on %s: %d" % (current_path, resp.status))
         sys.exit(1)
 
     result.extend(json.loads(content))
-    current_url = find_next_url(resp)
+    current_path = find_next_path(resp)
   return result
 
 def save_json(content, path):
   with open(path, 'w') as out_file:
-    json.dump(content, out_file, sort_keys=True, indent=2)
+    json.dump(content, out_file, sort_keys=True, indent=4)
 
-http = httplib2.Http(".cache", disable_ssl_certificate_validation=True)
-http.add_credentials("UC7", "a48210ea39")
+connection = httplib.HTTPSConnection("scc.suse.com")
+token = base64.b64encode(b"UCUSER:UCPASSWORD").decode("ascii")
+headers = { 'Authorization' : 'Basic %s' %  token }
 
-products = get_paginated(http, "https://scc.suse.com/connect/organizations/products/unscoped")
+products = get_paginated(connection, headers, "/connect/organizations/products/unscoped")
 save_json(products, "products.json")
 print("products.json refreshed")
 
-repositories = get_paginated(http, "https://scc.suse.com/connect/organizations/repositories")
+repositories = get_paginated(connection, headers, "/connect/organizations/repositories")
 save_json(repositories, "repositories.json")
 print("repositories.json refreshed")
 
-subscriptions = get_paginated(http, "https://scc.suse.com/connect/organizations/subscriptions")
+subscriptions = get_paginated(connection, headers, "/connect/organizations/subscriptions")
 save_json(subscriptions, "organizations_subscriptions.json")
 print("subscriptions.json refreshed")
 
-orders = get_paginated(http, "https://scc.suse.com/connect/organizations/orders")
+orders = get_paginated(connection, headers, "/connect/organizations/orders")
 save_json(orders, "organizations_orders.json")
 print("organizations_orders.json refreshed")
