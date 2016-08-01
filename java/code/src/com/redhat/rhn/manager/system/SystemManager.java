5b05e7dfd25a8bf8ac2fed6b4cfca3a19b91c28c
Silvio Moioli <smoioli@suse.de>
2016-08-01 15:02:27 +0200

    Get rid of the SaltService interface

--- java/code/src/com/redhat/rhn/manager/system/SystemManager.java
+++ java/code/src/com/redhat/rhn/manager/system/SystemManager.java
@@ -83,7 +83,7 @@ import com.redhat.rhn.manager.kickstart.cobbler.CobblerSystemRemoveCommand;
 import com.redhat.rhn.manager.rhnset.RhnSetDecl;
 import com.redhat.rhn.manager.user.UserManager;
 import com.suse.manager.webui.services.SaltStateGeneratorService;
-import com.suse.manager.webui.services.impl.SaltAPIService;
+import com.suse.manager.webui.services.impl.SaltService;
 import org.apache.commons.lang.BooleanUtils;
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
@@ -394,7 +394,7 @@ public class SystemManager extends BaseManager {
         // remove server itself
         ServerFactory.delete(server);
         server.asMinionServer().ifPresent(minion -> {
-            SaltAPIService.INSTANCE.deleteKey(minion.getMinionId());
+            SaltService.INSTANCE.deleteKey(minion.getMinionId());
             SaltStateGeneratorService.INSTANCE.removeServer(minion);
         });
     }
