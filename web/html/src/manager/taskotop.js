2c401d6947fea94fcfaa6c072271689c15ebb5ad
Dario Leidi <dleidi@suse.com>
2016-08-03 13:22:47 +0200

    Move TaskoTop into an inner tab of TaskStatus

--- web/html/src/manager/taskotop.js
+++ web/html/src/manager/taskotop.js
@@ -94,15 +94,31 @@ const TaskoTop = React.createClass({
 
   render: function() {
     const data = this.state.serverData;
+    const headerTabs =
+      <div className="spacewalk-content-nav">
+        <ul className="nav nav-tabs">
+          <li><a href="/rhn/admin/TaskStatus.do">{t('Last Execution Times')}</a></li>
+          <li className="active"><a href="">{t('RunTime Execution')}</a></li>
+        </ul>
+      </div>
+    ;
+
     if (data != null && Object.keys(data).length > 0) {
       return  (
         <div key="taskotop-content">
           <div className="spacewalk-toolbar-h1">
-            <h1><i className="fa fa-gears"></i>{t("Task Engine Runtime")}</h1>
-            <ErrorMessage error={this.state.error} />
-            <p>{t('The server is running or has finished executing the following tasks during the latest 5 minutes.')}</p>
-            <p>{t('Data are refreshed every ')}{this.props.refreshInterval/1000}{t(' seconds')}</p>
+            <h1>
+              <i className="fa fa-tachometer"></i>
+              {t('Task Engine Status')}
+              <a href="/rhn/help/reference/en-US/s2-sattools-task-engine.jsp"
+                  target="_blank"><i className="fa fa-question-circle spacewalk-help-link"></i>
+              </a>
+            </h1>
           </div>
+          {headerTabs}
+          <ErrorMessage error={this.state.error} />
+          <p>{t('The server is running or has finished executing the following tasks during the latest 5 minutes.')}</p>
+          <p>{t('Data are refreshed every ')}{this.props.refreshInterval/1000}{t(' seconds')}</p>
           <Table
             data={this.buildRows(data)}
             identifier={(row) => row["id"]}
@@ -162,10 +178,8 @@ const TaskoTop = React.createClass({
     else {
       return (
         <div key="taskotop-no-content">
-          <div className="spacewalk-toolbar-h1">
-            <h1><i className="fa fa-tasks"></i>{t("Task Engine Runtime")}</h1>
-            <p>{t('There are no tasks running on the server at the moment.')}</p>
-          </div>
+          {headerTabs}
+          <p>{t('There are no tasks running on the server at the moment.')}</p>
         </div>
       );
     }
