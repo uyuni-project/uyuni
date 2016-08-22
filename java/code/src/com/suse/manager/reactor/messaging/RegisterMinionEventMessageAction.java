54dffbf45cdc115308219cd8040df07cf968b63e
Bo Maryniuk <bo@suse.de>
2016-08-22 15:42:07 +0200

    Setup Salt Minion before packages are taken

--- java/code/src/com/suse/manager/reactor/messaging/RegisterMinionEventMessageAction.java
+++ java/code/src/com/suse/manager/reactor/messaging/RegisterMinionEventMessageAction.java
@@ -286,6 +286,7 @@ public class RegisterMinionEventMessageAction extends AbstractDatabaseAction {
                     ApplyStatesEventMessage.CERTIFICATE,
                     ApplyStatesEventMessage.CHANNELS,
                     ApplyStatesEventMessage.CHANNELS_DISABLE_LOCAL_REPOS,
+                    ApplyStatesEventMessage.SETUP,
                     ApplyStatesEventMessage.PACKAGES
             ));
         }
