80cdd0295f11d0f82b21e79730ce209d5e83e291
benediktwerner <1benediktwerner@gmail.com>
2016-08-22 13:35:46 +0200

    Added first xmlrpc formula handler

---
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/formulas/FormulaHandler.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2009--2014 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.frontend.xmlrpc.formulas;
+
+import java.util.List;
+
+import com.redhat.rhn.domain.user.User;
+import com.redhat.rhn.frontend.xmlrpc.BaseHandler;
+import com.suse.manager.webui.controllers.FormulaCatalogController;
+
+
+/**
+ * FormulaHandler
+ * @version $Rev$
+ * @xmlrpc.namespace formula
+ * @xmlrpc.doc Provides methods to access and modify formulas.
+ */
+public class FormulaHandler extends BaseHandler {
+
+    /**
+     * List all installed formulas.
+     * @param loggedInUser The current user
+     * @return the list of formulas currently installed.
+     *
+     * @xmlrpc.doc Return the list of formulas currently installed.
+
+     * @xmlrpc.param #session_key()
+     * @xmlrpc.returntype
+     *      #array()
+     *          string
+     *      #array_end()
+     */
+    public List<String> listFormulas(User loggedInUser) {
+        return FormulaCatalogController.listFormulas();
+    }
+}
