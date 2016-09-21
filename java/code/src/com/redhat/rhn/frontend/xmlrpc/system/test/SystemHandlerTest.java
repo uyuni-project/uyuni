dd8224072323eba0097cc6b4897e616b13100ad1
Michael Calmer <mc@suse.de>
2016-09-21 17:18:50 +0200

    function rename

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/system/test/SystemHandlerTest.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/system/test/SystemHandlerTest.java
@@ -2273,7 +2273,7 @@ public class SystemHandlerTest extends BaseHandlerTestCase {
     }
 
     public void testListMigrationTargetBaseOnly() throws Exception {
-        SUSEProductTestUtils.createVendorSUSEProducts();
+        SUSEProductTestUtils.createVendorSUSEProductEnvironment();
 
         InstalledProduct installedPrd = new InstalledProduct();
         installedPrd.setName("SLES");
@@ -2305,7 +2305,7 @@ public class SystemHandlerTest extends BaseHandlerTestCase {
     }
 
     public void testListMigrationTargetExtension() throws Exception {
-        SUSEProductTestUtils.createVendorSUSEProducts();
+        SUSEProductTestUtils.createVendorSUSEProductEnvironment();
 
         InstalledProduct installedPrd = new InstalledProduct();
         installedPrd.setName("SLES");
