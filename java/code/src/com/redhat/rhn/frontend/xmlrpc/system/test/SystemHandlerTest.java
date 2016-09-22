091617cc6eb384b787be8569523775330cea66ae
Michael Calmer <mc@suse.de>
2016-09-22 13:30:15 +0200

    create channels for products

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/system/test/SystemHandlerTest.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/system/test/SystemHandlerTest.java
@@ -2234,7 +2234,7 @@ public class SystemHandlerTest extends BaseHandlerTestCase {
     }
 
     public void testListMigrationTargetBaseOnly() throws Exception {
-        SUSEProductTestUtils.createVendorSUSEProductEnvironment();
+        SUSEProductTestUtils.createVendorSUSEProductEnvironment(admin);
 
         InstalledProduct installedPrd = new InstalledProduct();
         installedPrd.setName("SLES");
@@ -2266,7 +2266,7 @@ public class SystemHandlerTest extends BaseHandlerTestCase {
     }
 
     public void testListMigrationTargetExtension() throws Exception {
-        SUSEProductTestUtils.createVendorSUSEProductEnvironment();
+        SUSEProductTestUtils.createVendorSUSEProductEnvironment(admin);
 
         InstalledProduct installedPrd = new InstalledProduct();
         installedPrd.setName("SLES");
