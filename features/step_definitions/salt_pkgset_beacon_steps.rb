0a93db53ac0552960061b85571854c91f239ae7e
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2016-09-26 12:02:19 +0100

    Fixes 'pkgset' beacon tests for manager30 branch

--- features/step_definitions/salt_pkgset_beacon_steps.rb
+++ features/step_definitions/salt_pkgset_beacon_steps.rb
@@ -2,23 +2,21 @@
 # Licensed under the terms of the MIT license.
 
 Then(/^I manually install the "([^"]*)" package in the minion$/) do |package|
-  if file_exist($minion, "/usr/bin/zypper").zero?
-    cmd = "zypper --non-interactive install -y #{package}"
-  elsif file_exist($minion, "/usr/bin/yum").zero?
-    cmd = "yum -y install #{package}"
+  if File.exist?("/usr/bin/zypper")
+    `zypper --non-interactive install -y #{package}`
+  elsif File.exist?("/usr/bin/yum")
+    `yum -y install #{package}`
   else
     fail "not found: zypper or yum"
   end
-  $minion.run(cmd, false)
 end
 
 Then(/^I manually remove the "([^"]*)" package in the minion$/) do |package|
-  if file_exist($minion, "/usr/bin/zypper").zero?
-    cmd = "zypper --non-interactive remove -y #{package}"
-  elsif file_exist($minion, "/usr/bin/yum").zero?
-    cmd = "yum -y remove #{package}"
+  if File.exist?("/usr/bin/zypper")
+    `zypper --non-interactive remove -y #{package}`
+  elsif File.exist?("/usr/bin/yum")
+    `yum -y remove #{package}`
   else
     fail "not found: zypper or yum"
   end
-  $minion.run(cmd, false)
 end
