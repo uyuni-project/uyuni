cc662da225a7551b891b7e9e93f0bbe201a3f5f8
Bo Maryniuk <bo@suse.de>
2016-09-29 16:24:01 +0200

    Fix PEP8 issues

--- susemanager-utils/susemanager-sls/salt/channels/yum-susemanager-plugin/susemanagerplugin.py
+++ susemanager-utils/susemanager-sls/salt/channels/yum-susemanager-plugin/susemanagerplugin.py
@@ -11,19 +11,22 @@ plugin_type = TYPE_CORE
 
 
 def _get_mtime():
-    '''
+    """
     Get the modified time of the RPM Database.
+
     Returns:
         Unix ticks
-    '''
+    """
     return os.path.exists(RPM_PATH) and int(os.path.getmtime(RPM_PATH)) or 0
 
+
 def _get_checksum():
-    '''
+    """
     Get the checksum of the RPM Database.
+
     Returns:
         hexdigest
-    '''
+    """
     digest = hashlib.md5()
     with open(RPM_PATH, "rb") as rpm_db_fh:
         while True:
@@ -33,14 +36,17 @@ def _get_checksum():
             digest.update(buff)
     return digest.hexdigest()
 
+
 def config_hook(conduit):
     config.RepoConf.susemanager_token = config.Option()
+
 
 def prereposetup_hook(conduit):
     for repo in conduit.getRepos().listEnabled():
        susemanager_token = getattr(repo, 'susemanager_token', None)
        if susemanager_token:
           repo.http_headers['X-Mgr-Auth'] = susemanager_token
+
 
 def posttrans_hook(conduit):
     if 'SALT_RUNNING' not in os.environ:
