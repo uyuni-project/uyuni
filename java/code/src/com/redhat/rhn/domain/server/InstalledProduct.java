0f802cc9e1f2698d129430f9e60cb82806a12620
Dario Leidi <dleidi@suse.com>
2016-10-10 14:41:26 +0200

    arch_type of a SUSEProduct can be null (bsc#1001738 bsc#1001784 bsc#1001923 bsc#1002678)

--- java/code/src/com/redhat/rhn/domain/server/InstalledProduct.java
+++ java/code/src/com/redhat/rhn/domain/server/InstalledProduct.java
@@ -124,8 +124,9 @@ public class InstalledProduct extends BaseDomainHelper {
      * @return a SUSE Product if one could be found or null otherwise
      */
     public SUSEProduct getSUSEProduct() {
+        PackageArch arch = getArch();
         return SUSEProductFactory.findSUSEProduct(getName(), getVersion(), getRelease(),
-            getArch().getLabel(), true);
+                arch == null ? null : arch.getLabel(), true);
     }
 
     /**
