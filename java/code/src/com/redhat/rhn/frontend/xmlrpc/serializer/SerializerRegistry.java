7f6239d40692b9ffe29f9f30b2ff2c6daa70a5b3
Kevin Walter <kwalter@suse.com>
2016-10-14 11:47:51 +0200

    add channelinfo serializer

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/serializer/SerializerRegistry.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/serializer/SerializerRegistry.java
@@ -40,6 +40,7 @@ public class SerializerRegistry {
         SERIALIZER_CLASSES.add(ActivationKeySerializer.class);
         SERIALIZER_CLASSES.add(TokenSerializer.class);
         SERIALIZER_CLASSES.add(ChannelArchSerializer.class);
+        SERIALIZER_CLASSES.add(ChannelInfoSerializer.class);
         SERIALIZER_CLASSES.add(ChannelSerializer.class);
         SERIALIZER_CLASSES.add(CpuSerializer.class);
         SERIALIZER_CLASSES.add(DeviceSerializer.class);
