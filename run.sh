b22400329a14d32ba4ba81607d848c82a9849ff7
Dario Maiocchi <MalloZup@users.noreply.github.com>
2016-10-21 14:10:18 +0200

    fix space

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export MINION= minion12sp1.tf.local
+export MINION=minion12sp1.tf.local
 export TESTHOST=suma3pg.tf.local
 export CLIENT=clisles12sp1.tf.local
 export BROWSER=phantomjs
