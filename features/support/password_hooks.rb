1c02f2683cea3fbcc86412ab7db12d7a946d5287
Michael Calmer <mc@suse.de>
2016-10-21 10:22:00 +0200

    travis fixes

--- features/support/password_hooks.rb
+++ features/support/password_hooks.rb
@@ -14,32 +14,32 @@ end
 
 def changepass(scenario, password)
   # only change the password if the wrong one worked
-  if has_xpath?("//a[@href='/rhn/Logout.do']")
-    signout = find(:xpath, "//a[@href='/rhn/Logout.do']")
-    if signout
-      signout.click
-    end
-    fill_in "username", :with => "admin"
-    fill_in "password", :with => password
-    click_button "Sign In"
+  return unless has_xpath?("//a[@href='/rhn/Logout.do']")
 
-    find_link("Your Account").click
-    sleep(2)
-
-    begin
-        fill_in "desiredpassword", :with => "admin"
-    rescue
-        $stderr.puts "ERROR - try again"
-        sleep(5)
-        fill_in "desiredpassword", :with => "admin"
-    end
-    begin
-        fill_in "desiredpasswordConfirm", :with => "admin"
-    rescue
-        $stderr.puts "ERROR - try again"
-        sleep(5)
-        fill_in "desiredpasswordConfirm", :with => "admin"
-    end
-    click_button "Update"
+  signout = find(:xpath, "//a[@href='/rhn/Logout.do']")
+  if signout
+    signout.click
   end
+  fill_in "username", :with => "admin"
+  fill_in "password", :with => password
+  click_button "Sign In"
+
+  find_link("Your Account").click
+  sleep(2)
+
+  begin
+      fill_in "desiredpassword", :with => "admin"
+  rescue
+      $stderr.puts "ERROR - try again"
+      sleep(5)
+      fill_in "desiredpassword", :with => "admin"
+  end
+  begin
+      fill_in "desiredpasswordConfirm", :with => "admin"
+  rescue
+      $stderr.puts "ERROR - try again"
+      sleep(5)
+      fill_in "desiredpasswordConfirm", :with => "admin"
+  end
+  click_button "Update"
 end
