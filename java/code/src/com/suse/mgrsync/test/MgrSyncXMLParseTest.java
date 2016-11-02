d9243f5ecfa19cfebf1762c3b8d68f8377adeacd
Michael Calmer <mc@suse.de>
2016-11-02 14:00:14 +0100

    unittests: three new channel families added

--- java/code/src/com/suse/mgrsync/test/MgrSyncXMLParseTest.java
+++ java/code/src/com/suse/mgrsync/test/MgrSyncXMLParseTest.java
@@ -132,7 +132,7 @@ public class MgrSyncXMLParseTest extends RhnBaseTestCase {
      */
     public void testReadFamilies() throws Exception {
         List<MgrSyncChannelFamily> families = readFamilies();
-        assertEquals(54, families.size());
+        assertEquals(57, families.size());
     }
 
     /**
