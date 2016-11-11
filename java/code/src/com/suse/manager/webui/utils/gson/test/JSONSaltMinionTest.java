084687a42e0b56801bd3de626b713a0a829b54ee
Can Bulut Bayburt <cbbayburt@suse.com>
2016-11-11 16:20:03 +0100

    Adds a JUnit test for JSONSaltMinion class
    (cherry picked from commit 03ba10a6c1acb95fccf080d98d17ca91c1b29294)

---
+++ java/code/src/com/suse/manager/webui/utils/gson/test/JSONSaltMinionTest.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2016 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.suse.manager.webui.utils.gson.test;
+
+import com.suse.manager.webui.utils.gson.JSONSaltMinion;
+import com.suse.salt.netapi.calls.wheel.Key;
+
+import org.jmock.Expectations;
+import org.jmock.integration.junit3.MockObjectTestCase;
+import org.jmock.lib.legacy.ClassImposteriser;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class JSONSaltMinionTest extends MockObjectTestCase {
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        setImposteriser(ClassImposteriser.INSTANCE);
+    }
+
+    public void testFromFingerprints() {
+        Map<String, String> accepted = new HashMap<>();
+        accepted.put("m1", "fingerprint1");
+        accepted.put("m2", "fingerprint2");
+
+        Map<String, String> pending = new HashMap<>();
+        pending.put("m3", "fingerprint3");
+
+        Map<String, String> denied = new HashMap<>();
+        denied.put("m4", "fingerprint4");
+
+        Key.Fingerprints fingerprints = context().mock(Key.Fingerprints.class);
+        context().checking(new Expectations() {{
+            oneOf(fingerprints).getMinions(); will(returnValue(accepted));
+            oneOf(fingerprints).getUnacceptedMinions(); will(returnValue(pending));
+            oneOf(fingerprints).getDeniedMinions(); will(returnValue(denied));
+            oneOf(fingerprints).getRejectedMinions(); will(returnValue(new HashMap<>()));
+        }});
+
+        Map<String, Long> sids = new HashMap<>();
+        sids.put("m1", 1L);
+        sids.put("m2", 2L);
+        sids.put("m4", 4L);
+
+        List<JSONSaltMinion> minions = JSONSaltMinion.fromFingerprints(fingerprints, sids);
+
+        assertNotNull(minions);
+        assertEquals(4, minions.size());
+
+        Map<String, JSONSaltMinion> minionMap = minions.stream()
+                .collect(Collectors.toMap(JSONSaltMinion::getId, Function.identity()));
+
+        JSONSaltMinion minion = minionMap.get("m1");
+        assertEquals("fingerprint1", minion.getFingerprint());
+        assertEquals(1L, (long)minion.getSid());
+        assertEquals("accepted", minion.getState());
+
+        minion = minionMap.get("m2");
+        assertEquals("fingerprint2", minion.getFingerprint());
+        assertEquals(2L, (long)minion.getSid());
+        assertEquals("accepted", minion.getState());
+
+        minion = minionMap.get("m3");
+        assertEquals("fingerprint3", minion.getFingerprint());
+        assertNull(minion.getSid());
+        assertEquals("pending", minion.getState());
+
+        minion = minionMap.get("m4");
+        assertEquals("fingerprint4", minion.getFingerprint());
+        assertEquals(4L, (long)minion.getSid());
+        assertEquals("denied", minion.getState());
+    }
+}
