3484857d967a79f51e4092fe62c738f5ebe034b2
Frantisek Kobzik <fkobzik@suse.de>
2016-11-28 14:38:43 +0100

    Revert "Temporarily hide the checkbox for bootstrapping salt-ssh minion"
    This reverts commit e2cfb390bae0ed77b2a0e9d5de219acfa64922ba.

--- web/html/src/manager/bootstrap-minions.js
+++ web/html/src/manager/bootstrap-minions.js
@@ -200,7 +200,7 @@ class BootstrapMinions extends React.Component {
                         </div>
                     </div>
                 </div>
-                <div className="form-group" style={{display: 'none'}}>
+                <div className="form-group">
                     <div className="col-md-3"></div>
                     <div className="col-md-6">
                         <div className="checkbox">
