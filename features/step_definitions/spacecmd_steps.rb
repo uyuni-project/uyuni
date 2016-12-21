024cdb6723becf200cbcd56b2a8a852c14b8d8ed
Dario Maiocchi <dmaiocchi@suse.com>
2016-12-21 16:35:50 +0100

    Develop centos ssh-salt feature

--- features/step_definitions/spacecmd_steps.rb
+++ features/step_definitions/spacecmd_steps.rb
@@ -2,14 +2,13 @@
 # Licensed under the terms of the MIT license.
 
 $space = "spacecmd -u admin -p admin "
-
 And(/I check status "([^"]*)" with spacecmd on "([^"]*)"$/) do |status, target|
-  if target == "ssh-minion"
-    cmd = "#{$space} system_listevents #{$ssh_minion_fullhostname} | head -n5"
-    $server.run("#{$space} clear_caches")
-    out, _code = $server.run(cmd)
-    unless out.include? status
-      raise "#{out} should contain #{status}"
-    end
+  host = $ssh_minion_fullhostname if target == "ssh-minion"
+  host = $ceos_minion_fullhostname if target == "ceos-minion"
+  cmd = "#{$space} system_listevents #{host} | head -n5"
+  $server.run("#{$space} clear_caches")
+  out, _code = $server.run(cmd)
+  unless out.include? status
+    raise "#{out} should contain #{status}"
   end
 end
