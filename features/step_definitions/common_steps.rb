9632422b67e782f398a72d3df694e515b95e4c80
Dario Maiocchi <dmaiocchi@suse.com>
2016-12-23 14:23:00 +0100

    Move mgr_create_boostrap to common_steps

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -31,3 +31,14 @@ Then(/^I should not see the "([^"]*)" as text$/) do |target|
   step %(I should not see a "#{$ssh_minion_fullhostname}" text) if target == "ssh-minion"
   step %(I should not see a "#{$ceos_minion_fullhostname}" text) if target == "ceos-minion"
 end
+
+Given(/^the list of distributions$/) do |table|
+  @distros = table.raw.flatten
+end
+
+Then(/^calling mgr\-create\-bootstrap\-repo \-c should show no error$/) do
+  @distros.each do |distro|
+    command_output = sshcmd("mgr-create-bootstrap-repo -c #{distro}")[:stdout]
+    refute_includes(command_output, "ERROR")
+  end
+end
