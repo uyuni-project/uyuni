6bb493d9143ba070bffd4f231e92dfcaf13ee878
Dario Maiocchi <dmaiocchi@suse.com>
2016-12-23 13:00:59 +0100

    Add additional test for testing system reboot gui

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -10,9 +10,24 @@ When(/^I run rhn_check on this client$/) do
 end
 
 Then(/^I download the SSL certificate$/) do
-  # download certicate on the client from the server via ssh protocol
   cert_path = "/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT"
   wget = "wget --no-check-certificate -O"
   $client.run("#{wget} #{cert_path} http://#{$server_ip}/pub/RHN-ORG-TRUSTED-SSL-CERT", true, 500, 'root')
   $client.run("ls #{cert_path}")
+end
+
+Then(/^I should see the "([^"]*)" as link$/) do |target|
+  # $client_hostname here is correct, don't use the full for trad. client
+  step %(I should see a "#{$client_hostname}" link) if target == "sle-client"
+  step %(I should see a "#{$minion_fullhostname}" link) if target == "sle-minion"
+  step %(I should see a "#{$ssh_minion_fullhostname}" link) if target == "ssh-minion"
+  step %(I should see a "#{$ceos_minion_fullhostname}" link) if target == "ceos-minion"
+end
+
+Then(/^I should not see the "([^"]*)" as text$/) do |target|
+  # $client_hostname here is correct, don't use the full for trad. client
+  step %(I should not see a "#{$client_hostname}" text) if target == "sle-client"
+  step %(I should not see a "#{$minion_fullhostname}" text) if target == "sle-minion"
+  step %(I should not see a "#{$ssh_minion_fullhostname}" text) if target == "ssh-minion"
+  step %(I should not see a "#{$ceos_minion_fullhostname}" text) if target == "ceos-minion"
 end
