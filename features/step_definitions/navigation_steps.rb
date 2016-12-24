8a0216d11ae6c4702aed608b882392a4a84016a8
Dario Maiocchi <dmaiocchi@suse.com>
2016-12-24 10:01:59 +0100

    Refractoring: Move steps in navigation.rb

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -233,3 +233,69 @@ Then(/^Table row for "([^"]*)" should contain "([^"]*)"$/) do |arg1, arg2|
     fail unless has_content?(arg2)
   end
 end
+
+# login, logout steps
+
+Given(/^I am authorized as "([^"]*)" with password "([^"]*)"$/) do |arg1, arg2|
+  visit Capybara.app_host
+  fill_in "username", :with => arg1
+  fill_in "password", :with => arg2
+  click_button "Sign In"
+  step %(I should be logged in)
+end
+
+Given(/^I am authorized$/) do
+  step %(I am authorized as "testing" with password "testing")
+end
+
+When(/^I sign out$/) do
+  page.find(:xpath, "//a[@href='/rhn/Logout.do']").click
+end
+
+Then(/^I should not be authorized$/) do
+  fail unless page.has_no_xpath?("//a[@href='/rhn/Logout.do']")
+end
+
+Then(/^I should be logged in$/) do
+  fail unless page.has_xpath?("//a[@href='/rhn/Logout.do']")
+end
+
+Then(/^I am logged-in$/) do
+  fail unless page.find(:xpath, "//a[@href='/rhn/Logout.do']").visible?
+  fail unless page.has_content?("You have just created your first SUSE Manager user. To finalize your installation please use the Setup Wizard")
+end
+
+When(/^I go to the admin configuration page$/) do
+  find_link("Admin").click
+  find_link("SUSE Manager Configuration").click
+end
+
+When(/^I go to the users page$/) do
+  find_link("Users").click
+end
+
+When(/^I go to the configuration page$/) do
+  find_link("Configuration").click
+end
+
+Given(/^I am on the Errata page$/) do
+  step %(I am authorized)
+  within(:xpath, "//header") do
+    find_link(debrand_string("Errata")).click
+  end
+end
+
+When(/^I check test channel$/) do
+  step %(I check "Test Base Channel" in the list)
+end
+
+When(/^I check "([^"]*)" erratum$/) do |arg1|
+  step %(I check "#{arg1}" in the list)
+end
+
+When(/^I search for "([^"]*)"$/) do |arg1|
+  within(:xpath, "//div[@class=\"table-responsive\"]/table/tbody/tr[contains(.,'Search For')]") do
+    fill_in "search_string", :with => arg1
+    click_button "Search"
+  end
+end
