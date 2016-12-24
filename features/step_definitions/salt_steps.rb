b5bb94f795d00e1e87321c7da9cd40329bfe1d99
Dario Maiocchi <dmaiocchi@suse.com>
2016-12-24 10:15:16 +0100

    Move pkgset_beacon to salt steps

--- features/step_definitions/salt_steps.rb
+++ features/step_definitions/salt_steps.rb
@@ -330,3 +330,44 @@ When(/^"(.*)" exists on the filesystem$/) do |file|
   end
   fail unless file_exist($minion, file)
 end
+
+# salt_pkgset_beacon_steps.rb
+Then(/^I manually install the "([^"]*)" package in the minion$/) do |package|
+  if file_exist($minion, "/usr/bin/zypper").zero?
+    cmd = "zypper --non-interactive install -y #{package}"
+  elsif file_exist($minion, "/usr/bin/yum").zero?
+    cmd = "yum -y install #{package}"
+  else
+    fail "not found: zypper or yum"
+  end
+  $minion.run(cmd, false)
+end
+
+Then(/^I manually remove the "([^"]*)" package in the minion$/) do |package|
+  if file_exist($minion, "/usr/bin/zypper").zero?
+    cmd = "zypper --non-interactive remove -y #{package}"
+  elsif file_exist($minion, "/usr/bin/yum").zero?
+    cmd = "yum -y remove #{package}"
+  else
+    fail "not found: zypper or yum"
+  end
+  $minion.run(cmd, false)
+end
+
+Then(/^I click on the css "(.*)" until page does not contain "([^"]*)" text$/) do |css, arg1|
+  not_found = false
+  begin
+    Timeout.timeout(30) do
+      loop do
+        unless page.has_content?(debrand_string(arg1))
+          not_found = true
+          break
+        end
+        find(css).click
+      end
+    end
+  rescue Timeout::Error
+    raise "'#{arg1}' cannot be found after several tries"
+  end
+  fail unless not_found
+end
