fcb680f50669810e83ef01df341bd71732784cb2
Dario Maiocchi <dmaiocchi@suse.com>
2017-01-04 11:20:52 +0100

    Refractor: remove useless steps

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -55,3 +55,74 @@ Then(/^I check the rhn logs for ERROR entries$/) do
   out, code = $server.run(cmd)
   raise "RHN_LOGS: ERROR FOUNDS ! #{out} " if code.nonzero?
 end
+# action chains
+When(/^I check radio button "(.*?)"$/) do |arg1|
+   fail unless choose(arg1)
+end
+
+When(/^I open the action chain box$/) do
+   fail unless find('#s2id_action-chain span.select2-arrow').click
+end
+
+When(/^I enter "(.*?)" in action-chain$/) do |arg1|
+   find('#select2-drop input.select2-input').set(arg1)
+end
+
+When(/^I enter as remote command this script in$/) do |multiline|
+   within(:xpath, "//section") do
+      x = find('textarea#fSptInput')
+      x.set(multiline) # find("#{arg1}") #.set(lines)
+   end
+end
+
+# bare metal
+When(/^I check the ram value$/) do
+   get_ram_value = "grep MemTotal /proc/meminfo |awk '{print $2}'"
+   ram_value, _local, _remote, _code = $client.test_and_store_results_together(get_ram_value, "root", 600)
+   ram_value = ram_value.gsub(/\s+/, "")
+   ram_mb = ram_value.to_i / 1024
+   step %(I should see a "#{ram_mb}" text)
+end
+
+When(/^I check the MAC address value$/) do
+   get_mac_address = "cat /sys/class/net/eth0/address"
+   mac_address, _local, _remote, _code = $client.test_and_store_results_together(get_mac_address, "root", 600)
+   mac_address = mac_address.gsub(/\s+/, "")
+   mac_address.downcase!
+   step %(I should see a "#{mac_address}" text)
+end
+
+Then(/^I should see the CPU frequency of the client$/) do
+   get_cpu_freq = "lscpu  | grep 'CPU MHz'" # | awk '{print $4}'"
+   cpu_freq, _local, _remote, _code = $client.test_and_store_results_together(get_cpu_freq, "root", 600)
+   get_cpu = cpu_freq.gsub(/\s+/, "")
+   cpu = get_cpu.split(".")
+   cpu = cpu[0].gsub(/[^\d]/, '')
+   step %(I should see a "#{cpu.to_i / 1000} GHz" text)
+end
+
+# ipmi 
+When(/^I setup the ipmi network card$/) do
+  $sshout = `echo | ssh -l root -o StrictHostKeyChecking=no $TESTHOST TERM=xterm yast lan add name=eth1 ethdevice=eth1 bootproto=dhcp 2>&1`
+  unless $?.success?
+    raise "Unable to change setup network card: #{$sshout}"
+  end
+  $sshout = `echo | ssh -l root -o StrictHostKeyChecking=no $TESTHOST TERM=xterm ifup eth1 2>&1`
+  unless $?.success?
+    raise "Unable to bring up the network interface: #{$sshout}"
+  end
+end
+
+When(/^I should see the power is "([^"]*)"$/) do |arg1|
+  within(:xpath, "//*[@for='powerStatus']/..") do
+    10.times do
+      if !has_content?(arg1)
+        find(:xpath, '//button[@value="Get status"]').click
+      else
+        break
+      end
+      sleep 3
+    end
+    fail unless has_content?(arg1)
+  end
+end
