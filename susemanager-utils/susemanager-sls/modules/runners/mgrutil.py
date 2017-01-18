9ae65ec6a886b3e4f15d8e8b44a6fe7e1ff1f22b
Matei Albu <malbu@suse.de>
2017-01-18 19:01:37 +0100

    runner to generate ssh key and execute cmd via proxies

--- susemanager-utils/susemanager-sls/modules/runners/mgrutil.py
+++ susemanager-utils/susemanager-sls/modules/runners/mgrutil.py
@@ -1,10 +1,11 @@
 from subprocess import Popen, PIPE
+import shlex
 import os
 
 
 def ssh_keygen(path):
     '''
-    Generate SSH keys using the give path
+    Generate SSH keys using the give path.
     :param path: the path
     :return: map containing returncode and stdout/stderr
     '''
@@ -13,8 +14,37 @@ def ssh_keygen(path):
     cmd = ['ssh-keygen', '-N', '', '-f', path, '-t', 'rsa', '-q']
     # if not os.path.isdir(os.path.dirname(path)):
     #     os.makedirs(os.path.dirname(path))
+    return _cmd(cmd)
+
+
+def chain_ssh_cmd(hosts=None, clientkey=None, proxykey=None, user="root", options=None, command=None, outputfile=None):
+    '''
+    Chain ssh calls over one or more hops to run a command on the last host in the chain.
+    :param hosts:
+    :param clientkey:
+    :param proxykey:
+    :param user:
+    :param options:
+    :param command:
+    :param outputfile:
+    :return:
+    '''
+    cmd = []
+    for idx, hostname in enumerate(hosts):
+        key = clientkey if idx == 0 else proxykey
+        opts = " ".join(["-o {}={}".format(opt, val) for opt, val in options.items()])
+        ssh = "/usr/bin/ssh -i {} {} -o User={} {}"\
+            .format(key, opts, user, hostname)
+        cmd.extend(shlex.split(ssh))
+    cmd.append(command)
+    ret = _cmd(cmd)
+    if outputfile:
+        with open(outputfile, "w") as out:
+            out.write(ret["stdout"])
+    return ret
+
+
+def _cmd(cmd):
     p = Popen(cmd, stdout=PIPE, stderr=PIPE)
     stdout, stderr = p.communicate()
-    if p.returncode != 0:
-        return {"returncode": p.returncode, "stderr": stderr}
-    return {"returncode": p.returncode, "stdout": stdout}
+    return {"returncode": p.returncode, "stdout": stdout, "stderr": stderr}
