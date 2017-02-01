8c52f010915248bdd39c8f1420bb88df49adf43e
Matei Albu <malbu@suse.de>
2017-02-01 16:52:51 +0100

    code cleanup & checkstyle

--- java/code/src/com/suse/manager/webui/websocket/json/ActionErrorEventDto.java
+++ java/code/src/com/suse/manager/webui/websocket/json/ActionErrorEventDto.java
@@ -1,21 +1,46 @@
+/**
+ * Copyright (c) 2017 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package com.suse.manager.webui.websocket.json;
 
 /**
- * Created by matei on 1/27/17.
+ * Error message DTO.
  */
 public class ActionErrorEventDto extends AbstractSaltEventDto {
 
     private String message;
 
-    public ActionErrorEventDto(String minionId, String message) {
-        super("error", minionId);
+    /**
+     * @param minionIdIn the minion id
+     * @param messageIn the error message
+     */
+    public ActionErrorEventDto(String minionIdIn, String messageIn) {
+        super("error", minionIdIn);
+        this.message = messageIn;
     }
 
+    /**
+     * @return the error message
+     */
     public String getMessage() {
         return message;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    /**
+     * @param messageIn the error message
+     */
+    public void setMessage(String messageIn) {
+        this.message = messageIn;
     }
 }
