d59fe45c2d685b4c58e74e9fd2b05d107e2be72f
Matei Albu <malbu@suse.de>
2017-02-01 16:52:51 +0100

    code cleanup & checkstyle (cherry picked from commit e86e1d1)

--- java/code/src/com/suse/manager/webui/websocket/RemoteMinionCommands.java
+++ java/code/src/com/suse/manager/webui/websocket/RemoteMinionCommands.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2017 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package com.suse.manager.webui.websocket;
 
 import com.redhat.rhn.domain.server.MinionServer;
@@ -6,14 +20,24 @@ import com.redhat.rhn.domain.user.User;
 import com.redhat.rhn.domain.user.UserFactory;
 import com.redhat.rhn.frontend.servlets.LocalizedEnvironmentFilter;
 import com.suse.manager.webui.services.impl.SaltService;
-import com.suse.manager.webui.websocket.json.*;
+import com.suse.manager.webui.websocket.json.AsyncJobStartEventDto;
+import com.suse.manager.webui.websocket.json.RemoteMinionCommandDto;
+import com.suse.manager.webui.websocket.json.MinionMatchResultEventDto;
+import com.suse.manager.webui.websocket.json.ActionTimedOutEventDto;
+import com.suse.manager.webui.websocket.json.MinionCommandResultEventDto;
+import com.suse.manager.webui.websocket.json.ActionErrorEventDto;
+import com.suse.manager.webui.websocket.json.AbstractSaltEventDto;
 import com.suse.salt.netapi.datatypes.target.Glob;
-import com.suse.salt.netapi.exception.SaltException;
 import com.suse.salt.netapi.results.Result;
 import com.suse.utils.Json;
 import org.apache.log4j.Logger;
 
-import javax.websocket.*;
+import javax.websocket.OnOpen;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnError;
+import javax.websocket.Session;
+import javax.websocket.EndpointConfig;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.List;
@@ -24,9 +48,10 @@ import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 
 /**
- * Created by matei on 9/26/16.
+ * Websocket endpoint for executing remote commands on Salt minions.
  */
-@ServerEndpoint(value = "/websocket/minion/remote-commands", configurator = ServletAwareConfig.class)
+@ServerEndpoint(value = "/websocket/minion/remote-commands",
+        configurator = ServletAwareConfigurator.class)
 public class RemoteMinionCommands {
 
     // Logger for this class
@@ -38,24 +63,38 @@ public class RemoteMinionCommands {
 
     private CompletableFuture failAfter;
 
+    /**
+     * Callback executed when the websocket is opened.
+     * @param session the websocket session
+     * @param config the endpoint config
+     */
     @OnOpen
-    public void onStart(Session session, EndpointConfig config) {
+    public void onOpen(Session session, EndpointConfig config) {
         this.userId = LocalizedEnvironmentFilter.getCurrentUserId();
         if (this.userId == null) {
             try {
                 LOG.debug("No userId available. Closing the web socket session.");
                 session.close();
-            } catch (IOException e) {
+            }
+            catch (IOException e) {
                 LOG.debug("Error closing web socket session", e);
             }
         }
     }
 
+    /**
+     * Callback executed when the websocket is closed.
+     */
     @OnClose
     public void onClose() {
         LOG.debug("Closing web socket session");
     }
 
+    /**
+     * Callback executed when a message is received.
+     * @param session the websocket session
+     * @param messageBody the message as string
+     */
     @OnMessage
     public void onMessage(Session session, String messageBody) {
         User user = UserFactory.lookupById(this.userId);
@@ -65,7 +104,8 @@ public class RemoteMinionCommands {
                 .map(MinionServer::getMinionId)
                 .collect(Collectors.toList());
 
-        RemoteMinionCommandDto msg = Json.GSON.fromJson(messageBody, RemoteMinionCommandDto.class);
+        RemoteMinionCommandDto msg = Json.GSON.fromJson(
+                messageBody, RemoteMinionCommandDto.class);
         int timeOut = 20; // TODO remove hardcoding
         try {
             if (msg.isPreview()) {
@@ -73,7 +113,8 @@ public class RemoteMinionCommands {
                 Map<String, CompletionStage<Result<Boolean>>> res = SALT_SERVICE
                         .matchAsync(msg.getTarget(), failAfter);
 
-                List<String> allMinions = res.keySet().stream().collect(Collectors.toList());
+                List<String> allMinions = res.keySet().stream()
+                        .collect(Collectors.toList());
                 allMinions.retainAll(allVisibleMinions);
                 sendMessage(session, new AsyncJobStartEventDto("preview", allMinions));
 
@@ -84,56 +125,72 @@ public class RemoteMinionCommands {
                         }
                         if (err != null) {
                             if (err instanceof TimeoutException) {
-                                sendMessage(session, new ActionTimedOutEventDto(minionId, "preview"));
-                                LOG.debug("Timed out waiting for response from minion " + minionId);
-                            } else {
+                                sendMessage(session,
+                                        new ActionTimedOutEventDto(minionId, "preview"));
+                                LOG.debug("Timed out waiting for response from minion " +
+                                        minionId);
+                            }
+                            else {
                                 LOG.error("Error waiting for minion " + minionId, err);
                                 sendMessage(session,
                                         new ActionErrorEventDto(minionId,
-                                                "Error waiting for matching: " + err.getMessage()));
-                            }
-                        }
-                    });
-                });
-            } else if (msg.isCancel()) {
-                if (this.failAfter != null) {
-                    this.failAfter.completeExceptionally(new TimeoutException("Canceled waiting"));
-                }
-            } else {
-                this.failAfter = SALT_SERVICE.failAfter(timeOut);
-                Map<String, CompletionStage<Result<String>>> res = SALT_SERVICE
-                        .completableRemoteCommandAsync(new Glob(msg.getTarget()), msg.getCommand(), failAfter);
-
-                List<String> allMinions = res.keySet().stream().collect(Collectors.toList());
-                sendMessage(session, new AsyncJobStartEventDto("run", allMinions));
-
-                res.forEach((minionId, future) -> {
-                    future.whenComplete((cmdResult, err) -> {
-                        if (cmdResult != null ) {
-                            if (cmdResult.result().isPresent()) {
-                                sendMessage(session, new MinionCommandResultEventDto(minionId,
-                                        cmdResult.result().get()));
-                            } else {
-                                sendMessage(session,
-                                        new ActionErrorEventDto(minionId,
-                                                "Command executed succesfully but no result was returned"));
-                            }
-                        }
-                        if (err != null) {
-                            if (err instanceof TimeoutException) {
-                                sendMessage(session, new ActionTimedOutEventDto(minionId, "run"));
-                                LOG.debug("Timed out waiting for response from minion " + minionId);
-                            } else {
-                                LOG.error("Error waiting for minion " + minionId, err);
-                                sendMessage(session,
-                                        new ActionErrorEventDto(minionId,
-                                                "Error waiting to execute command: " + err.getMessage()));
+                                                "Error waiting for matching: " +
+                                                        err.getMessage()));
                             }
                         }
                     });
                 });
             }
-        } catch (SaltException e) {
+            else if (msg.isCancel()) {
+                if (this.failAfter != null) {
+                    this.failAfter.completeExceptionally(
+                            new TimeoutException("Canceled waiting"));
+                }
+            }
+            else {
+                this.failAfter = SALT_SERVICE.failAfter(timeOut);
+                Map<String, CompletionStage<Result<String>>> res = SALT_SERVICE
+                        .runRemoteCommandAsync(new Glob(msg.getTarget()),
+                                msg.getCommand(), failAfter);
+
+                List<String> allMinions = res.keySet().stream()
+                        .collect(Collectors.toList());
+                sendMessage(session, new AsyncJobStartEventDto("run", allMinions));
+
+                res.forEach((minionId, future) -> {
+                    future.whenComplete((cmdResult, err) -> {
+                        if (cmdResult != null) {
+                            if (cmdResult.result().isPresent()) {
+                                sendMessage(session, new MinionCommandResultEventDto(
+                                        minionId, cmdResult.result().get()));
+                            }
+                            else {
+                                sendMessage(session,
+                                        new ActionErrorEventDto(minionId,
+                                            "Command executed succesfully" +
+                                                    " but no result was returned"));
+                            }
+                        }
+                        if (err != null) {
+                            if (err instanceof TimeoutException) {
+                                sendMessage(session,
+                                        new ActionTimedOutEventDto(minionId, "run"));
+                                LOG.debug("Timed out waiting for response from minion " +
+                                        minionId);
+                            }
+                            else {
+                                LOG.error("Error waiting for minion " + minionId, err);
+                                sendMessage(session,
+                                        new ActionErrorEventDto(minionId,
+                                                "Error waiting to execute command: " +
+                                                        err.getMessage()));
+                            }
+                        }
+                    });
+                });
+            }
+        }
+        catch (Exception e) {
             LOG.error("Error executing Salt async remote command", e);
             sendMessage(session, new ActionErrorEventDto(null, e.getMessage()));
         }
@@ -147,11 +204,17 @@ public class RemoteMinionCommands {
     private synchronized void sendMessage(Session session, AbstractSaltEventDto dto) {
         try {
             session.getBasicRemote().sendText(Json.GSON.toJson(dto));
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             LOG.error("Error sending websocket message", e);
         }
     }
 
+    /**
+     * Callback executed an error occurs.
+     * @param session the websocket session
+     * @param err the err that occurred
+     */
     @OnError
     public void onError(Session session, Throwable err) {
         LOG.error("Websocket endpoint error", err);
--- java/code/src/com/suse/manager/webui/websocket/json/AbstractSaltEventDto.java
+++ java/code/src/com/suse/manager/webui/websocket/json/AbstractSaltEventDto.java
@@ -1,7 +1,21 @@
+/**
+ * Copyright (c) 2017 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package com.suse.manager.webui.websocket.json;
 
 /**
- * Created by matei on 9/28/16.
+ * Base class for UI DTOs to be transferred via websockets.
  */
 public abstract class AbstractSaltEventDto {
 
@@ -11,42 +25,72 @@ public abstract class AbstractSaltEventDto {
 
     private String actionType;
 
-    public AbstractSaltEventDto(String type) {
-        this.type = type;
+    /**
+     * @param typeIn type of the event
+     */
+    public AbstractSaltEventDto(String typeIn) {
+        this.type = typeIn;
     }
 
-    public AbstractSaltEventDto(String type, String minion) {
-        this.type = type;
-        this.minion = minion;
+    /**
+     * @param typeIn type of the event
+     * @param minionIn minion id
+     */
+    public AbstractSaltEventDto(String typeIn, String minionIn) {
+        this.type = typeIn;
+        this.minion = minionIn;
     }
 
-    public AbstractSaltEventDto(String type, String minion, String actionType) {
-        this.type = type;
-        this.minion = minion;
-        this.actionType = actionType;
+    /**
+     * @param typeIn type of the event
+     * @param minionIn minion id
+     * @param actionTypeIn action type
+     */
+    public AbstractSaltEventDto(String typeIn, String minionIn, String actionTypeIn) {
+        this.type = typeIn;
+        this.minion = minionIn;
+        this.actionType = actionTypeIn;
     }
 
+    /**
+     * @return type of the event
+     */
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    /**
+     * @param typeIn type of the event
+     */
+    public void setType(String typeIn) {
+        this.type = typeIn;
     }
 
+    /**
+     * @return the minion id
+     */
     public String getMinion() {
         return minion;
     }
 
-    public void setMinion(String minion) {
-        this.minion = minion;
+    /**
+     * @param minionIn the minion id
+     */
+    public void setMinion(String minionIn) {
+        this.minion = minionIn;
     }
 
+    /**
+     * @return the action type
+     */
     public String getActionType() {
         return actionType;
     }
 
-    public void setActionType(String actionType) {
-        this.actionType = actionType;
+    /**
+     * @param actionTypeIn the action type
+     */
+    public void setActionType(String actionTypeIn) {
+        this.actionType = actionTypeIn;
     }
 }
