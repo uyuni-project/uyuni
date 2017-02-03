6de2758657e26f9b95d5096ad52f4f0ea1e7de8d
Michael Calmer <mc@suse.de>
2017-02-03 15:21:42 +0100

    test: fix starting and stopping postgresql

--- java/scripts/docker-testing-pgsql.sh
+++ java/scripts/docker-testing-pgsql.sh
@@ -14,9 +14,6 @@ fi
 
 # SUSE Manager initialization
 cp /root/rhn.conf /etc/rhn/rhn.conf
-#cp /manager/susemanager/rhn-conf/rhn_server_susemanager.conf /usr/share/rhn/config-defaults/rhn_server_susemanager.conf
-sysctl -w kernel.shmmax=18446744073709551615
-smdba system-check autotuning
 
 # Resolve libs and run tests
 cd /manager/java
@@ -26,4 +23,8 @@ cp buildconf/test/rhn.conf.postgresql-example buildconf/test/rhn.conf
 ant -f manager-build.xml refresh-branding-jar test
 
 # Postgres shutdown (avoid stale memory by shmget())
-su - postgres -c "/usr/lib/postgresql-init stop" ||:
+if [ -e /usr/lib/postgresql-init ]; then
+    su - postgres -c "/usr/lib/postgresql-init stop" ||:
+else
+    rcpostgresql stop
+fi
