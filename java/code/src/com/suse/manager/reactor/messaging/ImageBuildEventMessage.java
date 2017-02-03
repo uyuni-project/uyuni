781ebc7dbc66a19d710192ddafd678b6aacd880a
Kevin Walter <kwalter@suse.com>
2017-02-03 10:03:55 +0100

    add build event message

---
+++ java/code/src/com/suse/manager/reactor/messaging/ImageBuildEventMessage.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2015 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.suse.manager.reactor.messaging;
+
+import com.redhat.rhn.common.hibernate.HibernateFactory;
+import com.redhat.rhn.common.messaging.EventDatabaseMessage;
+import com.redhat.rhn.domain.image.ImageProfile;
+import org.hibernate.Transaction;
+
+/**
+ * An event to signal that a set of states is dirty and needs
+ * to be applied to a particular server
+ */
+public class ImageBuildEventMessage implements EventDatabaseMessage {
+
+    private final long serverId;
+    private final Long userId;
+    private final String tag;
+    private final ImageProfile imageProfile;
+    private final Transaction txn;
+
+    /**
+     * Constructor for creating a {@link ImageBuildEventMessage} for a given server.
+     *
+     * @param serverIdIn     the server
+     * @param userIdIn       the user
+     * @param tagIn          the tag
+     * @param imageProfileIn the image profile
+     */
+    public ImageBuildEventMessage(long serverIdIn, Long userIdIn,
+                                  String tagIn, ImageProfile imageProfileIn) {
+        serverId = serverIdIn;
+        userId = userIdIn;
+        tag = tagIn;
+        imageProfile = imageProfileIn;
+        txn = HibernateFactory.getSession().getTransaction();
+    }
+
+    /**
+     * Return the server id.
+     *
+     * @return the server id
+     */
+    public Long getServerId() {
+        return serverId;
+    }
+
+    @Override
+    public Long getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toText() {
+        return toString();
+    }
+
+    /**
+     * Gets image profile.
+     *
+     * @return the image profile
+     */
+    public ImageProfile getImageProfile() {
+        return imageProfile;
+    }
+
+    /**
+     * Gets tag.
+     *
+     * @return the tag
+     */
+    public String getTag() {
+        return tag;
+    }
+
+    @Override
+    public String toString() {
+        return "ImageProfileEventMessage[serverId: " + serverId + ", tag: " +
+                tag + ", label: " + imageProfile.getLabel() + "]";
+    }
+
+    @Override
+    public Transaction getTransaction() {
+        return txn;
+    }
+}
