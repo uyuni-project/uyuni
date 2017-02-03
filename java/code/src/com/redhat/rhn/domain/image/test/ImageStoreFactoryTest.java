a6620e8cd3d1fca022ad2fb4571397c8be8d19c5
Can Bulut Bayburt <cbbayburt@suse.com>
2017-02-03 11:01:46 +0100

    Add more unit tests for content management

--- java/code/src/com/redhat/rhn/domain/image/test/ImageStoreFactoryTest.java
+++ java/code/src/com/redhat/rhn/domain/image/test/ImageStoreFactoryTest.java
@@ -5,11 +5,13 @@ import com.redhat.rhn.domain.credentials.DockerCredentials;
 import com.redhat.rhn.domain.image.ImageStore;
 import com.redhat.rhn.domain.image.ImageStoreFactory;
 import com.redhat.rhn.domain.image.ImageStoreType;
+import com.redhat.rhn.domain.org.Org;
+import com.redhat.rhn.domain.org.OrgFactory;
 import com.redhat.rhn.testing.BaseTestCaseWithUser;
 
 import javax.persistence.NoResultException;
 import java.util.List;
-
+import java.util.Optional;
 
 public class ImageStoreFactoryTest extends BaseTestCaseWithUser {
 
@@ -22,6 +24,12 @@ public class ImageStoreFactoryTest extends BaseTestCaseWithUser {
 
         ImageStoreType stype = ImageStoreFactory.lookupStoreTypeByLabel(STORE_TYPE_DOCKER);
         assertEquals(expected, stype);
+
+        try {
+            ImageStoreFactory.lookupStoreTypeByLabel("non-existent-label");
+            fail("Should throw NoResultException");
+        }
+        catch (NoResultException ignored) { }
     }
 
     public void testListImageStore() throws Exception {
@@ -33,17 +41,77 @@ public class ImageStoreFactoryTest extends BaseTestCaseWithUser {
         ImageStoreFactory.save(iStore);
 
         List<ImageStore> iList = ImageStoreFactory.listImageStores(user.getOrg());
-        assertNotEmpty(iList);
 
-        for (ImageStore i : iList) {
-            if (i.getLabel().equals("myregistry")) {
-                assertEquals(iStore, i);
-                assertNull(i.getCreds());
-            }
-            else {
-                assertTrue("myregistry not found", false);
-            }
-        }
+        assertEquals(1, iList.size());
+        assertEquals(iStore, iList.get(0));
+        assertNull(iList.get(0).getCreds());
+    }
+
+    public void testListByTypeLabelAndOrg() throws Exception {
+        ImageStore iStore = new ImageStore();
+        iStore.setLabel("myregistry");
+        iStore.setUri("registry.domain.top");
+        iStore.setStoreType(ImageStoreFactory.lookupStoreTypeByLabel(STORE_TYPE_DOCKER));
+        iStore.setOrg(user.getOrg());
+        ImageStoreFactory.save(iStore);
+
+        List<ImageStore> iList =
+                ImageStoreFactory.listByTypeLabelAndOrg(STORE_TYPE_DOCKER, user.getOrg());
+
+        assertEquals(1, iList.size());
+        assertEquals(iStore, iList.get(0));
+        assertNull(iList.get(0).getCreds());
+
+        iList = ImageStoreFactory.listByTypeLabelAndOrg("non-existent-type", user.getOrg());
+        assertEquals(0, iList.size());
+
+        Org org = OrgFactory.createOrg();
+        org.setName("foreign org");
+        org = OrgFactory.save(org);
+
+        iList = ImageStoreFactory.listByTypeLabelAndOrg(STORE_TYPE_DOCKER, org);
+        assertEquals(0, iList.size());
+    }
+
+    public void testLookupById() throws Exception {
+        ImageStore iStore = new ImageStore();
+        iStore.setLabel("myregistry");
+        iStore.setUri("registry.domain.top");
+        iStore.setStoreType(ImageStoreFactory.lookupStoreTypeByLabel(STORE_TYPE_DOCKER));
+        iStore.setOrg(user.getOrg());
+        ImageStoreFactory.save(iStore);
+
+        assertNotNull(iStore.getId());
+
+        Optional<ImageStore> lookup = ImageStoreFactory.lookupById(iStore.getId());
+        assertTrue(lookup.isPresent());
+        assertEquals(iStore, lookup.get());
+
+        lookup = ImageStoreFactory.lookupById(-1);
+        assertFalse(lookup.isPresent());
+    }
+
+    public void testLookupByIdAndOrg() throws Exception {
+        ImageStore iStore = new ImageStore();
+        iStore.setLabel("myregistry");
+        iStore.setUri("registry.domain.top");
+        iStore.setStoreType(ImageStoreFactory.lookupStoreTypeByLabel(STORE_TYPE_DOCKER));
+        iStore.setOrg(user.getOrg());
+        ImageStoreFactory.save(iStore);
+
+        assertNotNull(iStore.getId());
+
+        Optional<ImageStore> lookup =
+                ImageStoreFactory.lookupByIdAndOrg(iStore.getId(), user.getOrg());
+        assertTrue(lookup.isPresent());
+        assertEquals(iStore, lookup.get());
+
+        Org org = OrgFactory.createOrg();
+        org.setName("foreign org");
+        org = OrgFactory.save(org);
+
+        lookup = ImageStoreFactory.lookupByIdAndOrg(iStore.getId(), org);
+        assertFalse(lookup.isPresent());
     }
 
     public void testLookupImageStore() throws Exception {
@@ -56,13 +124,18 @@ public class ImageStoreFactoryTest extends BaseTestCaseWithUser {
 
         ImageStore i = ImageStoreFactory.lookupBylabelAndOrg("myregistry", user.getOrg());
 
-        if (i.getLabel().equals("myregistry")) {
-            assertEquals(iStore, i);
-            assertNull(i.getCreds());
+        assertEquals(iStore, i);
+        assertNull(i.getCreds());
+
+        Org org = OrgFactory.createOrg();
+        org.setName("foreign org");
+        org = OrgFactory.save(org);
+
+        try {
+            ImageStoreFactory.lookupBylabelAndOrg("myregistry", org);
+            fail("Should throw NoResultException");
         }
-        else {
-            assertTrue("myregistry not found", false);
-        }
+        catch (NoResultException ignored) { }
     }
 
     public void testLookupImageStoreWithCredentials() throws Exception {
@@ -81,18 +154,8 @@ public class ImageStoreFactoryTest extends BaseTestCaseWithUser {
 
         ImageStore i = ImageStoreFactory.lookupBylabelAndOrg("myregistry", user.getOrg());
 
-        if (i.getLabel().equals("myregistry")) {
-            assertEquals(iStore, i);
-            if(i.getCreds().asDockerCredentials().isPresent()) {
-                assertEquals(creds, i.getCreds().asDockerCredentials().get());
-            }
-            else {
-                assertTrue("Credentials is not a Docker Credentials object", false);
-            }
-        }
-        else {
-            assertTrue("myregistry not found", false);
-        }
+        assertEquals(iStore, i);
+        assertTrue(i.getCreds().asDockerCredentials().isPresent());
     }
 
     public void testDelete() {
@@ -109,6 +172,7 @@ public class ImageStoreFactoryTest extends BaseTestCaseWithUser {
         ImageStoreFactory.delete(i);
         try {
             ImageStoreFactory.lookupBylabelAndOrg("myregistry", user.getOrg());
+            fail("Should throw NoResultException");
         }
         catch (NoResultException ignored) { }
     }
