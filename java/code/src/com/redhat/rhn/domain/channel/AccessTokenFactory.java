d60629ab7f055d8f93004763a549552e06766603
Mihai Dinca <mdinca@suse.de>
2017-02-03 10:56:05 +0100

    Set valid=false on unneeded tokens

--- java/code/src/com/redhat/rhn/domain/channel/AccessTokenFactory.java
+++ java/code/src/com/redhat/rhn/domain/channel/AccessTokenFactory.java
@@ -122,6 +122,7 @@ public class AccessTokenFactory extends HibernateFactory {
 
         unneededTokens.forEach(token -> {
             token.setMinion(null);
+            token.setValid(false);
             AccessTokenFactory.save(token);
         });
 
