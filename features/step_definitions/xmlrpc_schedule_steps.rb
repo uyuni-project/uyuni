4bb489c63e9ea892bc8970f8cdb0b13e21b7c138
Dario Maiocchi <MalloZup@users.noreply.github.com>
2017-02-08 17:08:32 +0100

    authenticate in the step not in the feature

--- features/step_definitions/xmlrpc_schedule_steps.rb
+++ features/step_definitions/xmlrpc_schedule_steps.rb
@@ -1,9 +1,8 @@
 # COPYRIGHT 2017 SUSE LLC
 require "xmlrpc/client"
-
 scdrpc = XMLRPCScheduleTest.new(ENV['TESTHOST'])
-scdrpc.login('admin', 'admin')
 
 Then(/^there should be no failed scheduled actions$/) do
+  scdrpc.login('admin', 'admin')
   assert_empty(scdrpc.listFailedActions)
 end
