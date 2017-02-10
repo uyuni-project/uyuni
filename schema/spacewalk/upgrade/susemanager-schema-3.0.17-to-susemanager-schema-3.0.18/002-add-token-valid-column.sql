b35cfdf3741039abb5f3480faa1614b6cb641be4
Kevin Walter <kwalter@suse.com>
2017-02-10 13:52:22 +0100

    merge valid column migration with create table

--- schema/spacewalk/upgrade/susemanager-schema-3.0.17-to-susemanager-schema-3.0.18/002-add-token-valid-column.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE suseChannelAccessToken ADD valid CHAR(1) DEFAULT ('N') NOT NULL;
-
-UPDATE suseChannelAccessToken
-    SET valid='Y'
-    WHERE minion_id IS NOT NULL;
-
-CREATE UNIQUE INDEX suse_accesstoken_token_uq
-    ON suseChannelAccessToken (token);
