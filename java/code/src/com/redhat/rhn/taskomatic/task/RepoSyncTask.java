25eb4a10a15a517b7d57a7e380372e4f2e3e3ac5
Michael Calmer <mc@suse.de>
2017-02-23 16:05:40 +0100

    checkstyle

--- java/code/src/com/redhat/rhn/taskomatic/task/RepoSyncTask.java
+++ java/code/src/com/redhat/rhn/taskomatic/task/RepoSyncTask.java
@@ -25,7 +25,6 @@ import org.quartz.JobExecutionException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
