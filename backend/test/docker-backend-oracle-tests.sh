9bdfed492889a2b4e211df159d5b623dd2f972b8
Michael Calmer <mc@suse.de>
2017-02-24 17:29:30 +0100

    create reports outdir

--- backend/test/docker-backend-oracle-tests.sh
+++ backend/test/docker-backend-oracle-tests.sh
@@ -2,6 +2,7 @@
 
 /manager/backend/test/oracle-init.sh
 cp /root/rhn.conf /etc/rhn/rhn.conf
+mkdir -p /manager/backend/reports
 nosetests --with-xunit --xunit-file /manager/backend/reports/oracle_tests.xml /manager/backend/test/runtests-oracle.py
 EXIT=$?
 /etc/init.d/oracle stop
--- backend/test/docker-backend-pgsql-tests.sh
+++ backend/test/docker-backend-pgsql-tests.sh
@@ -3,6 +3,7 @@
 /sbin/sysctl -w kernel.shmmax=4067832832
 su - postgres -c '/usr/lib/postgresql-init start'
 cp /root/rhn.conf /etc/rhn/rhn.conf
+mkdir -p /manager/backend/reports
 nosetests --with-xunit --xunit-file /manager/backend/reports/pgsql_tests.xml /manager/backend/test/runtests-postgresql.py
 EXIT=$?
 su - postgres -c '/usr/lib/postgresql-init stop'
