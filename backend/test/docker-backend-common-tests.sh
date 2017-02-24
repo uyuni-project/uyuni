35138d5f15702c1603dab8554c53f6ee533db555
Michael Calmer <mc@suse.de>
2017-02-24 17:29:30 +0100

    create reports outdir

--- backend/test/docker-backend-common-tests.sh
+++ backend/test/docker-backend-common-tests.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 touch /etc/rhn/rhn.conf
+mkdir -p /manager/backend/reports
 nosetests --with-xunit --xunit-file /manager/backend/reports/common_tests.xml -s /manager/backend/common/test/unit-test/
