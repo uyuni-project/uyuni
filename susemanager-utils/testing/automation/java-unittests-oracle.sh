b908d3b7d2fa59fcab0a01227fb7e48cf48a73fc
Michael Calmer <mc@suse.de>
2017-02-25 14:24:48 +0100

    add argument parsing to automation script

--- susemanager-utils/testing/automation/java-unittests-oracle.sh
+++ susemanager-utils/testing/automation/java-unittests-oracle.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
 
+TARGET="test-pr"
+if [ -n "$1" ]; then
+    TARGET=$1
+fi
+
 HERE=`dirname $0`
 . $HERE/VERSION
 GITROOT=`readlink -f $HERE/../../../`
 
 docker pull $REGISTRY/$ORACLE_CONTAINER
-docker run --privileged --rm=true -v "$GITROOT:/manager" $REGISTRY/$ORACLE_CONTAINER /manager/java/scripts/docker-testing-oracle.sh
+docker run --privileged --rm=true -v "$GITROOT:/manager" $REGISTRY/$ORACLE_CONTAINER /manager/java/scripts/docker-testing-oracle.sh $TARGET
--- susemanager-utils/testing/automation/java-unittests-pgsql.sh
+++ susemanager-utils/testing/automation/java-unittests-pgsql.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
 
+TARGET="test-pr"
+if [ -n "$1" ]; then
+    TARGET=$1
+fi
+
 HERE=`dirname $0`
 . $HERE/VERSION
 GITROOT=`readlink -f $HERE/../../../`
 
 docker pull $REGISTRY/$PGSQL_CONTAINER
-docker run --privileged --rm=true -v "$GITROOT:/manager" $REGISTRY/$PGSQL_CONTAINER "/manager/java/scripts/docker-testing-pgsql.sh test-pr"
+docker run --privileged --rm=true -v "$GITROOT:/manager" $REGISTRY/$PGSQL_CONTAINER /manager/java/scripts/docker-testing-pgsql.sh $TARGET
