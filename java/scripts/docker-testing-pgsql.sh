8e90a165cabc83e45e0239d5efd11dcdb9a88734
Michael Calmer <mc@suse.de>
2017-02-25 13:25:10 +0100

    add possibility to provide that test target to script

--- java/scripts/docker-testing-pgsql.sh
+++ java/scripts/docker-testing-pgsql.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+TARGET="test"
+if [ -n "$1" ]; then
+    TARGET=$1
+fi
+
 cd /manager/susemanager-utils/testing/docker/scripts/
 # Move Postgres database to tmpfs to speed initialization and testing up
 if [ ! -z $PG_TMPFS_DIR ]; then
@@ -20,7 +25,7 @@ cd /manager/java
 ant resolve-ivy
 
 cp buildconf/test/rhn.conf.postgresql-example buildconf/test/rhn.conf
-ant -f manager-build.xml refresh-branding-jar test
+ant -f manager-build.xml refresh-branding-jar $TARGET
 
 # Postgres shutdown (avoid stale memory by shmget())
 if [ -e /usr/lib/postgresql-init ]; then
