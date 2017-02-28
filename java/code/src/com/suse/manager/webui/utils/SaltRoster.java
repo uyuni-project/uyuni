be484c561dd653cb9b27318efb72109cca9bf003
Matei Albu <malbu@suse.de>
2017-02-28 17:51:01 +0100

    checkstyle fixes
    (cherry picked from commit cf3f10b)

--- java/code/src/com/suse/manager/webui/utils/SaltRoster.java
+++ java/code/src/com/suse/manager/webui/utils/SaltRoster.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.Collections;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
