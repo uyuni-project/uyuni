b5fd27914180d4f87b0211f4d9dcfb00293fb5b9
Michael Calmer <mc@suse.de>
2017-03-03 16:16:43 +0100

    fake usix in backend

--- susemanager-utils/testing/automation/susemanager-unittest.sh
+++ susemanager-utils/testing/automation/susemanager-unittest.sh
@@ -3,7 +3,20 @@
 HERE=`dirname $0`
 . $HERE/VERSION
 GITROOT=`readlink -f $HERE/../../../`
+
+# fake usix.py
+if [ ! -e $GITROOT/backend/common/usix.py ]; then
+    ln -sf ../../usix/common/usix.py $GITROOT/backend/common/usix.py
+fi
+
 DOCKER_RUN_EXPORT="PYTHONPATH=/manager/client/rhel/rhnlib/:/manager/client/rhel/rhn-client-tools/src"
 
 docker pull $REGISTRY/$PGSQL_CONTAINER
 docker run --rm=true -e $DOCKER_RUN_EXPORT -v "$GITROOT:/manager" $REGISTRY/$PGSQL_CONTAINER /bin/sh -c "cd /manager/susemanager; make -f Makefile.susemanager unittest_inside_docker"
+if [ $? -ne 0 ]; then
+   EXIT=1
+fi
+
+rm -f $GITROOT/backend/common/usix.py*
+
+exit $EXIT
