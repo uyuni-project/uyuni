e055ff6eed55a26fa94124800b29af4adcb527f2
Matei Albu <malbu@suse.de>
2017-03-06 13:50:21 +0100

    add comment
    (cherry picked from commit 0f00b49)

--- susemanager-utils/susemanager-sls/modules/runners/mgrutil.py
+++ susemanager-utils/susemanager-sls/modules/runners/mgrutil.py
@@ -60,17 +60,19 @@ def move_minion_uploaded_files(minion=None, dirtomove=None, basepath=None, actio
     scapstorepath = os.path.join(basepath, actionpath)
     susemanager_gid = grp.getgrnam(GROUP_OWNER).gr_gid
     if not os.path.exists(scapstorepath):
-        log.debug("Creating action directory: %s" % scapstorepath)
+        log.debug("Creating action directory: {0}".format(scapstorepath))
         try:
             os.makedirs(scapstorepath)
         except Exception as err:
-            log.error('Failed to create dir {1}'.format(scapstorepath), exc_info=True)
-            return {False: str(err)}
+            log.error('Failed to create dir {0}'.format(scapstorepath), exc_info=True)
+            return {False: 'Salt failed to create dir {0}: {1}'.format(scapstorepath, str(err))}
         # change group permissions to rwx and group owner to susemanager
         mode = stat.S_IRWXU | stat.S_IRWXG | stat.S_IROTH | stat.S_IXOTH
         subdirs = actionpath.split('/')
         for idx in range(1, len(subdirs)):
             if subdirs[0: idx] != '':
+                # ignore errors. If dir has owner != salt then chmod fails but the dir
+                # might still have the correct group owner
                 try:
                     os.chmod(os.path.join(basepath, *subdirs[0: idx]), mode)
                 except OSError:
@@ -88,7 +90,7 @@ def move_minion_uploaded_files(minion=None, dirtomove=None, basepath=None, actio
         for fl in os.listdir(scapstorepath):
             os.chown(os.path.join(scapstorepath, fl), -1, susemanager_gid)
     except Exception as err:
-        log.error('Failed to move {0} -> {1}'.format(srcdir, scapstorepath), exc_info=True)
+        log.error('Salt failed to move {0} -> {1}'.format(srcdir, scapstorepath), exc_info=True)
         return {False: str(err)}
     return {True: scapstorepath}
 
