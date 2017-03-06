b99840e56d11c2064d03981c6c7ef99de2af6dd3
Matei Albu <malbu@suse.de>
2017-03-06 16:02:52 +0100

    sql syntax fix for oracle

--- schema/spacewalk/upgrade/susemanager-schema-3.0.18-to-susemanager-schema-3.0.19/001-give-minions-scap-capability.sql
+++ schema/spacewalk/upgrade/susemanager-schema-3.0.18-to-susemanager-schema-3.0.19/001-give-minions-scap-capability.sql
@@ -1,7 +1,2 @@
-INSERT INTO rhnClientCapability(server_id, capability_name_id, version)
-SELECT s.id, (SELECT id FROM rhnClientCapabilityName WHERE name='scap.xccdf_eval'), 1
-FROM rhnServer s JOIN suseminioninfo mi ON s.id=mi.server_id
-	LEFT JOIN rhnClientCapability c ON (c.server_id=s.id and c.capability_name_id=(SELECT id FROM rhnClientCapabilityName WHERE name='scap.xccdf_eval'))
-WHERE
-	c.server_id IS NULL
-	;
+INSERT INTO rhnClientCapability(server_id, capability_name_id, version) SELECT s.id, (SELECT id FROM rhnClientCapabilityName WHERE name='scap.xccdf_eval'), 1 FROM rhnServer s, suseminioninfo mi WHERE s.id=mi.server_id AND NOT EXISTS (select 1 from rhnClientCapability c where c.capability_name_id=(SELECT id FROM rhnClientCapabilityName WHERE name='scap.xccdf_eval') and c.server_id=s.id);
+
