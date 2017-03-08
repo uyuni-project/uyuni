e55fe964d0298bc79ed537fb337c15c9f99f9d54
Matei Albu <malbu@suse.de>
2017-03-08 16:32:43 +0100

    use a small fixed pool so we don't overwhelm the salt-api with salt-ssh executions

--- java/code/src/com/suse/manager/webui/services/impl/SaltSSHService.java
+++ java/code/src/com/suse/manager/webui/services/impl/SaltSSHService.java
@@ -48,9 +48,7 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Executor;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 /**
@@ -73,8 +71,9 @@ public class SaltSSHService {
      */
     public SaltSSHService(SaltClient saltClientIn) {
         this.saltClient = saltClientIn;
-        asyncSaltSSHExecutor = new ThreadPoolExecutor(
-                0, 20, 60L, TimeUnit.SECONDS, new SynchronousQueue());
+        // use a small fixed pool so we don't overwhelm the salt-api
+        // with salt-ssh executions
+        asyncSaltSSHExecutor = Executors.newFixedThreadPool(3);
     }
 
     /**
@@ -138,6 +137,7 @@ public class SaltSSHService {
                 return callSyncSSH(call, target);
             }
             catch (SaltException e) {
+                LOG.error("Error calling async salt-ssh minions", e);
                 throw new RuntimeException(e);
             }
         }, asyncSaltSSHExecutor)
@@ -380,6 +380,7 @@ public class SaltSSHService {
                                 isSudoUser(getSSHUser())));
             }
             catch (SaltException e) {
+                LOG.error("Error matching salt-ssh minions", e);
                 throw new RuntimeException(e);
             }
         }, asyncSaltSSHExecutor);
