cdf2230a052f3f59b236d25697bf43d95f2c3f07
Michael Calmer <mc@suse.de>
2017-03-08 16:38:16 +0100

    add serializer for ImageStoreType and ImageStore

---
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/serializer/ImageStoreTypeSerializer.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2017 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.frontend.xmlrpc.serializer;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import redstone.xmlrpc.XmlRpcException;
+import redstone.xmlrpc.XmlRpcSerializer;
+
+import com.redhat.rhn.domain.image.ImageStoreType;
+import com.redhat.rhn.frontend.xmlrpc.serializer.util.SerializerHelper;
+
+
+/**
+ * ImageStoreTypeSerializer
+ *
+ * @xmlrpc.doc
+ * #struct("Image Store Type information")
+ *   #prop("int", "id")
+ *   #prop("string", "label")
+ *   #prop("string", "name")
+ * #struct_end()
+ */
+public class ImageStoreTypeSerializer extends RhnXmlRpcCustomSerializer {
+
+    /**
+     *
+     * {@inheritDoc}
+     */
+    protected void doSerialize(Object value, Writer output, XmlRpcSerializer serializer)
+       throws XmlRpcException, IOException {
+        SerializerHelper helper = new SerializerHelper(serializer);
+        ImageStoreType type = (ImageStoreType) value;
+        helper.add("id", type.getId());
+        helper.add("label", type.getLabel());
+        helper.add("name", type.getName());
+        helper.writeTo(output);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Class getSupportedClass() {
+        return ImageStoreType.class;
+    }
+}
