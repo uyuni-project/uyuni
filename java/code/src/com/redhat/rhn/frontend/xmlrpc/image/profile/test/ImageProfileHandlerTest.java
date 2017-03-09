8c1e091bff18b8ba357ea9ee6967988e0e1476f8
Michael Calmer <mc@suse.de>
2017-03-09 16:27:56 +0100

    add ImageProfileHandler and test

---
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/image/profile/test/ImageProfileHandlerTest.java
@@ -0,0 +1,8 @@
+package com.redhat.rhn.frontend.xmlrpc.image.profile.test;
+
+import com.redhat.rhn.frontend.xmlrpc.test.BaseHandlerTestCase;
+
+
+public class ImageProfileHandlerTest extends BaseHandlerTestCase {
+
+}
