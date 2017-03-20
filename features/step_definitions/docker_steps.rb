5f0f0cae5e69ee3d66e6cd3a32ab83e729583119
Dario Maiocchi <dmaiocchi@suse.com>
2017-03-20 12:36:34 +0100

    Add new class for namespace Image

--- features/step_definitions/docker_steps.rb
+++ features/step_definitions/docker_steps.rb
@@ -1,6 +1,11 @@
 # Copyright (c) 2017 Suse Linux
 # Licensed under the terms of the MIT license.
 
+require "xmlrpc/client"
+
+# container_operations
+cont_op = XMLRPCImageTest.new(ENV['TESTHOST'])
+
 And(/^I select sle-minion hostname in Build Host$/) do
   select($minion_fullhostname, :from => 'host')
 end
@@ -11,4 +16,8 @@ end
 
 And(/^I navigate to images build webpage$/) do
   visit("https://#{$server_fullhostname}/rhn/manager/cm/build")
+end
+
+And(/^I build the image via xmlrpc-call$/) do
+  puts cont_op.listImages
 end
