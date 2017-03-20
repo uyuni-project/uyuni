c0b917c4ee396d2e6ba147fc427236bb81d6638b
Michael Calmer <mc@suse.de>
2017-03-20 15:07:05 +0100

    DB: create suseCVEImageChannel table for CVE Audit on Images

---
+++ schema/spacewalk/common/tables/suseCVEImageChannel.sql
@@ -0,0 +1,43 @@
+--
+-- Copyright (c) 2017 SUSE LLC
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+-- Red Hat trademarks are not licensed under GPLv2. No permission is
+-- granted to use or replicate Red Hat trademarks that are incorporated
+-- in this software or its documentation.
+--
+
+CREATE TABLE suseCVEImageChannel
+(
+    image_info_id    NUMBER NOT NULL
+                     CONSTRAINT suse_cveimg_iid_fk
+                         REFERENCES suseImageInfo (id)
+                         ON DELETE CASCADE,
+    channel_id   NUMBER NOT NULL
+                     CONSTRAINT suse_cveimg_cid_fk
+                         REFERENCES rhnChannel (id)
+                         ON DELETE CASCADE,
+    channel_rank NUMBER NOT NULL,
+    created   timestamp with local time zone
+                  DEFAULT (current_timestamp) NOT NULL,
+    modified  timestamp with local time zone
+                  DEFAULT (current_timestamp) NOT NULL
+)
+ENABLE ROW MOVEMENT
+;
+
+CREATE UNIQUE INDEX suse_cveimg_iid_cid_uq
+    ON suseCVEImageChannel (image_info_id, channel_id)
+    TABLESPACE [[8m_tbs]];
+
+CREATE INDEX suse_cveimg_cid_idx
+    ON suseCVEImageChannel (channel_id)
+    TABLESPACE [[8m_tbs]]
+    NOLOGGING;
+
