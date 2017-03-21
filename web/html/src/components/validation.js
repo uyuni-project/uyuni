2c4b8d7fffa186cbfef0c6bd2fb532c477dfd7d2
Can Bulut Bayburt <cbbayburt@suse.com>
2017-03-21 16:12:28 +0100

    Import validator.js library statically

--- web/html/src/components/validation.js
+++ web/html/src/components/validation.js
@@ -2,13 +2,12 @@
 
 // https://github.com/chriso/validator.js
 
-const Validator = require("validator");
 const f = (fn) => (...args) => (str) => fn(str, ...args);
 const validations = {};
 
-Object.keys(Validator).forEach(v => {
-    if(typeof Validator[v] === 'function') {
-        validations[v] = f(Validator[v])
+Object.keys(validator).forEach(v => {
+    if(typeof validator[v] === 'function') {
+        validations[v] = f(validator[v])
     }
 });
 
