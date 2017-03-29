bcd70043f7ed55dc50680e56dfb8b36dbfb6a09e
Can Bulut Bayburt <cbbayburt@suse.com>
2017-03-29 13:56:33 +0200

    Fix checkstyle

--- java/code/src/com/redhat/rhn/domain/image/ImageProfile.java
+++ java/code/src/com/redhat/rhn/domain/image/ImageProfile.java
@@ -24,7 +24,23 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
 import java.util.Optional;
 import java.util.Set;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 /**
  * ImageStore
