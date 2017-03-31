65de23a2bf7b1b4947fe659733d9895823647277
Dario Maiocchi <dmaiocchi@suse.com>
2017-03-31 15:04:09 +0200

    Refractor steps

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -110,3 +110,23 @@ end
 Then(/^I add "([^"]*)" channel$/) do |channel|
     $server.run("echo -e \"admin\nadmin\n\" | mgr-sync add channel #{channel}")
 end
+
+# channel steps
+arch = "x86_64"
+When(/^I use spacewalk\-channel to add a valid child channel$/) do
+  child_channel = "test-channel-#{arch}-child-channel"
+  step %(I execute spacewalk\-channel and pass "--add -c #{child_channel} -u admin -p admin")
+end
+
+When(/^I use spacewalk\-channel to remove a valid child channel$/) do
+  child_channel = "test-channel-#{arch}-child-channel"
+  step %(I execute spacewalk\-channel and pass "--remove -c #{child_channel} -u admin -p admin")
+end
+
+Then(/^I want to see all valid child channels$/) do
+  step %(I want to get "test-channel-#{arch}-child-channel")
+end
+
+Then(/^I wont see any of the valid child channels$/) do
+  step %(I wont get "test-channel-#{arch}-child-channel")
+end
