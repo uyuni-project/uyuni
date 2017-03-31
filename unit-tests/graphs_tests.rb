9e5bc9460eccf73697b33fa8ae973274bb93116b
dario <darioszr@gmail.com>
2017-03-31 23:05:34 +0200

    Adding more doc to readme

--- unit-tests/graphs_tests.rb
+++ unit-tests/graphs_tests.rb
@@ -11,14 +11,14 @@ def check_duplicate_features(a)
     puts "2 or more yaml file contains same feature!"
     raise "features does contain duplicates"
   end
-end 
+end
 
 # load all yamls files
 feat_set = YAML.load_file('run_sets/v2/testsuite.yml')
 
 # load several files and add the final array
 
-# load the graph files and create a sum array 
+# load the graph files and create a sum array
 # then  check that all names from sum_array are contained grap == features
 # otherwise raise an error. When an user create a new feature and doesn't addi it on the graph.
 
