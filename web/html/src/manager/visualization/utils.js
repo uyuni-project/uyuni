649d6ec830d123c8b3b8342356a37b719733152f
Frantisek Kobzik <fkobzik@suse.de>
2017-04-07 14:40:01 +0200

    Refactor: Extract svg init function to utils

--- web/html/src/manager/visualization/utils.js
+++ web/html/src/manager/visualization/utils.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const Utils = require("./utils.js");
+
 function computeSvgDimensions() {
   const width = d3.select('#svg-wrapper').node().getBoundingClientRect().width - 2;
   const height = d3.select('.spacewalk-main-column-layout').node().getBoundingClientRect().height - 2 -
@@ -9,7 +11,29 @@ function computeSvgDimensions() {
   return [width, height];
 }
 
+function prepareDom() {
+  const dimensions = computeSvgDimensions();
+  const svg = d3.select('#svg-wrapper')
+    .append('svg')
+    .attr('width', dimensions[0])
+    .attr('height', dimensions[1]);
+  const container = svg.append("g");
+
+  // Zoom handling
+  svg.call(d3.zoom()
+      .scaleExtent([1 / 8, 16])
+      .on("zoom", zoomed))
+    .on("dblclick.zoom", null);
+  function zoomed(d) {
+    var event = d3.event;
+    container.attr("transform", event.transform);
+  }
+
+  return container;
+}
+
 module.exports = {
-    computeSvgDimensions: computeSvgDimensions
+    computeSvgDimensions: computeSvgDimensions,
+    prepareDom: prepareDom
 }
 
