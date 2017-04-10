06c7593cb93d7d28ed2f9275ad9ee95f027422d3
Frantisek Kobzik <fkobzik@suse.de>
2017-04-10 14:05:27 +0200

    Refactor: extract dataTree to standalone module, move hierarchy-view module to ui/

--- web/html/src/manager/visualization/ui/hierarchy-view.js
+++ web/html/src/manager/visualization/ui/hierarchy-view.js
@@ -1,88 +1,6 @@
 'use strict';
 
-const Filters = require('./data-processing/filters.js');
-const Partitioning = require('./data-processing/partitioning.js');
-const Preprocessing = require('./data-processing/preprocessing.js');
-const Utils = require('./utils.js');
-
-// Render hierarchy view - take data, transform with preprocessor, filters and
-// partitioning, render it in the container.
-//
-// Compulsory parameters:
-//  - data - data from the server to be rendered
-//  - container - where the tree will be rendered in the DOM
-//
-// Other parameters (settable via methods):
-//  - preprocessors - preparing the data so that it can be rendered by d3
-//  - filters - showing/hiding of nodes based on customized conditions
-//  - partitioning - displaying nodes in partitions with different CSS classes
-//  based on custom conditions
-//  - simulation - d3 force simulation
-//
-// Methods
-//  - refresh - transforms the data according to current preprocessor, filters
-//  and partitioning settings and refresh the DOM
-//
-function dataTree(data, container) {
-
-  let preprocessor = Preprocessing.stratify();
-  const dimensions = Utils.computeSvgDimensions();
-
-  let filters = Filters.filters();
-  let partitioning = Partitioning.partitioning();
-
-  partitioning.get()['default'] = myDeriveClass;
-
-  let simulation = d3.forceSimulation()
-    .force('charge', d3.forceManyBody().strength(d => strengthFromDepth(d.depth)))
-    .force('link', d3.forceLink())
-    .force('x', d3.forceX(dimensions[0] / 2))
-    .force('y', d3.forceY(dimensions[1] / 2));
-
-  const view = hierarchyView(container)
-    .simulation(simulation);
-
-  function instance() {
-  }
-
-  instance.data = function(d) {
-    return arguments.length ? (data = d, instance) : data;
-  }
-
-  instance.preprocessor = function(p) {
-    return arguments.length ? (preprocessor = p, instance) : preprocessor;
-  }
-
-  instance.filters = function(f) {
-    return arguments.length ? (filters = f, instance) : filters;
-  }
-
-  instance.partitioning = function(p) {
-    return arguments.length ? (partitioning = p, instance) : partitioning;
-  }
-
-  instance.view = function() {
-    return view;
-  }
-
-  // Recomputes the data transformation (preprocessor, filters, partitioning),
-  // turns the data to tree and refreshes the hierarchy view.
-  instance.refresh = function() {
-    preprocessor.data(data);
-    const newRoot = preprocessor();
-    treeify(newRoot, dimensions);
-    view.root(newRoot);
-    nodeVisible(newRoot, filters.predicate());
-    view.deriveClass(partitioning.computePartitionName)
-    view.refresh();
-  }
-
-  instance.simulation = function(sim) {
-    return arguments.length ? (simulation = sim, instance) : simulation;
-  }
-
-  return instance;
-}
+const Utils = require('../utils.js');
 
 // Display given hierarchy (given by it's root node) in given container using
 // force based layout.
@@ -214,18 +132,6 @@ function hierarchyView(container, rootIn) {
 // UTILS FUNCTIONS
 //
 
-function myDeriveClass(node) {
-  if (node.id == 'root') {
-    return 'root';
-  }
-
-  if (view == 'proxy-hierarchy' && node.depth == 1 || ['group', 'vhm'].includes(node.data.type)) {
-    return 'inner-node';
-  }
-
-  return 'system';
-}
-
 function unselectAllNodes() {
   d3.selectAll('g.node.selected')
     .each(function(d) {
@@ -300,36 +206,7 @@ function countChildren(node) {
   return node._allChildren ? ' [' + node.children.length + '/' + node._allChildren.length + ']' : '';
 }
 
-// turns the input data into tree, backups children
-function treeify(root, dimensions) {
-  const tree = d3.tree().size(dimensions);
-  tree(root);
-  root.each(d => {
-      d._allChildren = d.children;  // backup children
-  })
-}
-
-function nodeVisible(node, pred) { // todo move to tree!
-  // dfs
-  if (!node.children) {
-    return pred(node);
-  }
-
-  const visibleChildren = node._allChildren.filter(c => nodeVisible(c, pred));
-  node.children = visibleChildren;
-  return visibleChildren.length > 0 || pred(node);
-}
-
-// Simulation strength based on the node depth
-function strengthFromDepth(depth) {
-  switch (depth) {
-    case 0: return -450;
-    case 1: return -270;
-    default: return -135;
-  }
-}
-
 module.exports = {
-    dataTree: dataTree
+    hierarchyView: hierarchyView
 }
 
