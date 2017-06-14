de05c124bf5f0c87637015ba712e08b69ab90c54
dmaiocchi <dmaiocchi@suse.com>
2017-06-14 15:11:41 +0200

    Refractor minion and nagios steps

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -130,3 +130,35 @@ end
 And(/^I navigate to "([^"]*)" page$/) do |page|
   visit("https://#{$server_fullhostname}/#{page}")
 end
+
+# nagios steps
+
+When(/^I perform a nagios check patches$/) do
+  command = "/usr/lib/nagios/plugins/check_suma_patches #{$client_fullhostname} > /tmp/nagios.out"
+  $server.run(command, false, 600, 'root')
+end
+
+When(/^I perform a nagios check last event$/) do
+  command = "/usr/lib/nagios/plugins/check_suma_lastevent #{$client_fullhostname} > /tmp/nagios.out"
+  $server.run(command, false, 600, 'root')
+end
+
+When(/^I perform an invalid nagios check patches$/) do
+  command = "/usr/lib/nagios/plugins/check_suma_patches does.not.exist > /tmp/nagios.out"
+  $server.run(command, false, 600, 'root')
+end
+
+Then(/^I should see WARNING: 1 patch pending$/) do
+  command = "grep \"WARNING: 1 patch(es) pending\" /tmp/nagios.out"
+  $server.run(command, true, 600, 'root')
+end
+
+Then(/^I should see Completed: OpenSCAP xccdf scanning scheduled by admin/) do
+  command = "grep \"Completed: OpenSCAP xccdf scanning scheduled by admin\" /tmp/nagios.out"
+  $server.run(command, true, 600, 'root')
+end
+
+Then(/^I should see an unknown system message$/) do
+  command = "grep -i \"^Unknown system:.*does.not.exist\" /tmp/nagios.out 2>&1"
+  $server.run(command, true, 600, 'root')
+end
