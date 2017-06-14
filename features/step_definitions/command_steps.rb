608d68d9601cc0b57bb7bdc153c4682aeffa06b7
dmaiocchi <dmaiocchi@suse.com>
2017-06-14 15:13:04 +0200

    remove zypper steps

--- features/step_definitions/command_steps.rb
+++ features/step_definitions/command_steps.rb
@@ -363,3 +363,35 @@ When(/^I register this client for SSH push via tunnel$/) do
   $server.run("mv /etc/hosts.BACKUP /etc/hosts")
   $server.run("mv /etc/sysconfig/rhn/up2date.BACKUP /etc/sysconfig/rhn/up2date")
 end
+# zypper
+
+And(/^I remove pkg "([^"]*)" on this "(.*?)"$/) do |pkg, host|
+  node = get_target(host)
+  node.run("zypper -n rm #{pkg}")
+end
+
+Given(/^I enable repository "(.*?)" on this "(.*?)"$/) do |repo, host|
+  node = get_target(host)
+  node.run("zypper mr --enable #{repo}")
+end
+
+Then(/^I disable repository "(.*?)" on this "(.*?)"$/) do |repo, host|
+  node = get_target(host)
+  node.run("zypper mr --disable #{repo}")
+end
+
+Then(/^I install pkg "(.*?)" on this "(.*?)"$/) do |pkg, host|
+  node = get_target(host)
+  node.run("zypper in -y #{pkg}")
+end
+
+And(/^I wait until the package "(.*?)" has been cached on this "(.*?)"$/) do |pkg_name, host|
+  node = get_target(host)
+  Timeout.timeout(DEFAULT_TIMEOUT) do
+    loop do
+      _out, code = node.run("ls /var/cache/zypp/packages/susemanager:test-channel-x86_64/getPackage/#{pkg_name}.rpm", false)
+      break if code.zero?
+      sleep 1
+    end
+  end
+end
