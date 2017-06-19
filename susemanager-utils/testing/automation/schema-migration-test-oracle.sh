ada8fba8d90d967e4d155c110c983e201c38d211
Michael Calmer <mc@suse.de>
2017-06-19 17:58:10 +0200

    fix scripts to test with current oracle container

--- susemanager-utils/testing/automation/schema-migration-test-oracle.sh
+++ susemanager-utils/testing/automation/schema-migration-test-oracle.sh
@@ -4,6 +4,5 @@ HERE=`dirname $0`
 . $HERE/VERSION
 GITROOT=`readlink -f $HERE/../../../`
 
-# we need a special (old) baseimage to migrate to current schema
-docker pull $REGISTRY/suma-2.1-ora
-docker run --privileged --rm=true -v "$GITROOT:/manager" $REGISTRY/suma-2.1-ora /manager/susemanager-utils/testing/docker/scripts/schema_migration_test_oracle-21to31.sh
+docker pull $REGISTRY/$ORACLE_CONTAINER
+docker run --privileged --rm=true -v "$GITROOT:/manager" $REGISTRY/$ORACLE_CONTAINER /manager/susemanager-utils/testing/docker/scripts/schema_migration_test_oracle-21to31.sh
