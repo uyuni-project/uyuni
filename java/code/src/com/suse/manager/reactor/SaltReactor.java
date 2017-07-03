90a00ab417e5a19af5b8c9a57b6ec03874f8b8d1
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2017-07-03 12:52:18 +0100

    Create VirtpollerData object with JSON content instead null

--- java/code/src/com/suse/manager/reactor/SaltReactor.java
+++ java/code/src/com/suse/manager/reactor/SaltReactor.java
@@ -261,7 +261,7 @@ public class SaltReactor implements EventListener {
                 Map<String, JsonElement> data = beaconEvent.getData(tt);
                 MessageQueue.publish(new VirtpollerBeaconEventMessage(
                         beaconEvent.getMinionId(),
-                        JsonParser.GSON.fromJson(data.get("data"), VirtpollerData.class)));
+                        JsonParser.GSON.fromJson(data.toString(), VirtpollerData.class)));
             }
         };
     }
