69a6dec4d6f2e671687f50ac44f3f6d36abc33f1
Michael Calmer <mc@suse.de>
2017-07-08 10:44:30 +0200

    add update repo

--- susemanager-utils/testing/docker/3.1/suma-3.1-base/add_repositories.sh
+++ susemanager-utils/testing/docker/3.1/suma-3.1-base/add_repositories.sh
@@ -8,6 +8,6 @@ zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12-SP1:/Update/standard "SLE1
 zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12-SP2:/GA/standard "SLE12 SP2 GA"
 zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12-SP2:/Update/standard "SLE12 SP2 Update"
 zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12-SP2:/Update:/Products:/Manager31/standard/ "Manager31 Pool"
-#zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12-SP2:/Update:/Products:/Manager31:/Update/standard/ "Manager31 Update"
+zypper ar -f http://download.suse.de/ibs/SUSE:/SLE-12-SP2:/Update:/Products:/Manager31:/Update/standard/ "Manager31 Update"
 zypper ar -f http://download.suse.de/ibs/Devel:/Galaxy:/Manager:/3.1/SLE_12_SP2/ "Devel:Galaxy:Manager:3.1"
 zypper ar -f http://download.suse.de/ibs/Devel:/Galaxy:/pylint/SLE_12_SP2/ "pylint"
