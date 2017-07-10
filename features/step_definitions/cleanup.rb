f17443765058d5f4f211d1197f4b1ce7b2c86d05
dmaiocchi <dmaiocchi@suse.com>
2017-07-10 18:31:16 +0200

    Make distro_kickstart feature idempotent Add cleanups and check if client is a tunnel or not If it is a tunnel client, one feature is not avaible so skip

--- features/step_definitions/cleanup.rb
+++ features/step_definitions/cleanup.rb
@@ -2,5 +2,17 @@
 # Licensed under the terms of the MIT license.
 
 And(/^Cleanup for distro_clobber_feature$/) do
-  puts "clean wipe, clean, clean"
+  host = $server_fullhostname
+  @cli = XMLRPC::Client.new2('http://' + host + '/rpc/api')
+  @sid = @cli.call('auth.login', 'admin', 'admin')
+  # -------------------------------
+  # cleanup kickstart profiles and distros
+  distro_name = "fedora_kickstart_distro"
+  @cli.call('kickstart.tree.deleteTreeAndProfiles', @sid, distro_name)
+  @cli.call("auth.logout", @sid)
+  # -------------------------------
+  # remove not from suma managed profile
+  $server.run("cobbler profile remove --name \"testprofile\"")
+  # remove not from suma man. distro
+  $server.run("cobbler distro remove --name \"testdistro\"")
 end
