01dd57ce05323cc3eb86ddb6e50045603cb28519
Dario Maiocchi <dmaiocchi@suse.com>
2017-08-01 17:17:16 +0200

    Run linter via gulp.js

--- web/html/src/gulpfile.js
+++ web/html/src/gulpfile.js
@@ -8,7 +8,7 @@ var babelify = require('babelify');
 var glob  = require('glob');
 var es = require('event-stream');
 var rename = require('gulp-rename')
-
+const eslint = require('gulp-eslint');
 var bundlerOpts = null;
 
 gulp.task('devel-opts', function(done) {
@@ -53,6 +53,23 @@ gulp.task('bundle-manager', function(done) {
 
         es.merge(tasks).on('end', done);
     })
+});
+
+gulp.task('lint', () => {
+   // ESLint ignores files with "node_modules" paths.
+    // So, it's best to have gulp ignore the directory as well.
+    // Also, Be sure to return the stream from the task;
+    // Otherwise, the task may end before the stream has finished.
+    return gulp.src(['**/*.js','!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
 });
 
 
