bdd2a3c1877b1700b8583108f76dce290bba3fb7
Dario Leidi <dleidi@suse.com>
2017-08-04 14:10:10 +0200

    Do not use org.apache.commons.lang3

--- java/code/src/com/redhat/rhn/frontend/action/channel/manage/EditChannelAction.java
+++ java/code/src/com/redhat/rhn/frontend/action/channel/manage/EditChannelAction.java
@@ -46,8 +46,8 @@ import com.redhat.rhn.manager.download.DownloadManager;
 import com.redhat.rhn.manager.system.SystemManager;
 import com.redhat.rhn.manager.user.UserManager;
 
+import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang3.BooleanUtils;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
