8f33dd8230ea4bd35c368883e1c2e536921c60da
Can Bulut Bayburt <cbbayburt@suse.com>
2017-08-09 15:30:15 +0200

    Backend for importing images
    (cherry picked from commit da38898ec5c2b4ae5dfbe4b753028c6bf1ed37d8)

--- schema/spacewalk/upgrade/susemanager-schema-3.1.9-to-susemanager-schema-3.1.10/001-suseImageInfo-actionFk.sql
+++ schema/spacewalk/upgrade/susemanager-schema-3.1.9-to-susemanager-schema-3.1.10/001-suseImageInfo-actionFk.sql
@@ -1,7 +1,7 @@
 ALTER TABLE suseImageInfo DROP CONSTRAINT suse_imginfo_aid_fk;
 ALTER TABLE suseImageInfo DROP CONSTRAINT suse_imginfo_aid_insp_fk;
 
-ALTER TABLE suseImageInfo ADD CONSTRAINT suse_imginfo_bdlaid_fk
+ALTER TABLE suseImageInfo ADD CONSTRAINT suse_imginfo_bldaid_fk
     FOREIGN KEY (build_action_id)
     REFERENCES rhnAction(id)
     ON DELETE SET NULL;
