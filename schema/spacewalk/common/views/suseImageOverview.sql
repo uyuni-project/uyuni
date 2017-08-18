eec5c28125543a95388802f8a4ae1881a4181dab
Can Bulut Bayburt <cbbayburt@suse.com>
2017-08-18 16:07:41 +0200

    renumber sql migration files, fix sql migrations and schema
    (cherry picked from commit 73b84d9f49f3c050b91f1dc86deaf0b9a2b3c119)

--- schema/spacewalk/common/views/suseImageOverview.sql
+++ schema/spacewalk/common/views/suseImageOverview.sql
@@ -24,8 +24,8 @@ suseImageOverview
     profile_id,
     store_id,
     build_server_id,
-    external_image,
     curr_revision_num,
+    external_image,
     security_errata,
     bug_errata,
     enhancement_errata,
@@ -36,8 +36,8 @@ as
 select
     i.org_id, i.id, i.name, i.version, i.checksum_id, i.modified,
     ( select name from rhnServerArch where id = i.image_arch_id), i.build_action_id,
-    i.inspect_action_id, i.profile_id, i.store_id, i.build_server_id, i.external_image,
-    i.curr_revision_num,
+    i.inspect_action_id, i.profile_id, i.store_id, i.build_server_id, i.curr_revision_num,
+    i.external_image,
     ( select count(*) from rhnImageErrataTypeView ietv
       where
             ietv.image_id = i.id
