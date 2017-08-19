87fe964c7758f32632d88f6e1101bb36afcd9235
Michael Calmer <mc@suse.de>
2017-08-19 11:58:22 +0200

    test schema migration up to 3.2 level

--- susemanager-utils/testing/docker/scripts/schema_migration_test_oracle-21to31.sh
+++ susemanager-utils/testing/docker/scripts/schema_migration_test_oracle-21to31.sh
@@ -43,11 +43,6 @@ spacewalk-setup --clear-db --db-only --answer-file=clear-db-answers-oracle.txt -
 
 ./build-schema.sh
 
-################################################
-####### START COMMENT OUT
-####### IF A FIXED DESTINATION IS WANTED
-################################################
-
 RPMVERSION=`rpm -q --qf "%{version}" --specfile /manager/schema/spacewalk/susemanager-schema.spec`
 NEXTVERSION=`echo $RPMVERSION | awk '{ pre=post=$0; gsub("[0-9]+$","",pre); gsub(".*\\\\.","",post); print pre post+1; }'`
 
@@ -58,20 +53,25 @@ else
     export SUMA_TEST_SCHEMA_VERSION=$RPMVERSION
 fi
 
-# guessing the link
-# obsolete: we have this link now in the package.
-#for v in `seq 30 -1 11`; do
-#    minusone=$(($v-1))
-#    if [ -d /etc/sysconfig/rhn/schema-upgrade/susemanager-schema-3.0.$minusone-to-susemanager-schema-3.0.$v ]; then
-#        mkdir /etc/sysconfig/rhn/schema-upgrade/susemanager-schema-3.0.$v-to-susemanager-schema-3.1.0
-#        # set hard this destination
-#        export SUMA_TEST_SCHEMA_VERSION="3.1.1"
-#        break
-#    fi
-#done
+################################################
+####### START COMMENT OUT
+####### IF A FIXED DESTINATION IS WANTED
+################################################
+
+
+# guessing the link to next major version
+# THE NEXT BLOCK CAN BE COMMENTED OUT WHEN WE HAVE THE LINK PACKAGED
+for v in `seq 30 -1 2`; do
+    minusone=$(($v-1))
+    if [ -d /etc/sysconfig/rhn/schema-upgrade/susemanager-schema-3.1.$minusone-to-susemanager-schema-3.1.$v ]; then
+        mkdir /etc/sysconfig/rhn/schema-upgrade/susemanager-schema-3.1.$v-to-susemanager-schema-3.2.0
+        # set hard this destination
+        export SUMA_TEST_SCHEMA_VERSION="3.2.1"
+        break
+    fi
+done
 ###############################################
 ####### END
-#export SUMA_TEST_SCHEMA_VERSION="3.0"
 ###############################################
 
 # run the schema upgrade from git repo
