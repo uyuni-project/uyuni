spacewalkproject/spacewalk

commit dfe76f776c176daa5dc81e8eb55f1f6da7af8bf6

1439622 - don't let disabled user log in

backend/server/rhnUser.py
@@ -327,7 +327,8 @@ def search(user):
     """ search the database for a user """
     log_debug(3, user)
     userid = get_user_id(user)
-    if not userid:  # no user found
+    if not userid or is_user_disabled(user):
+        # no user found or is disabled
         return None
     ret = User(user, "")
     if not ret.reload(userid) == 0:

uyuni-project/uyuni

commit 239a2ff3132031889b88a037131663a6b6bd3f8a

Fix SP migration for clients registered by disabled users (bsc#1057126)
This commit fixes a regression on SP migration for traditional clients that was
introduced by dfe76f776c176daa5dc81e8eb55f1f6da7af8bf6, causing an error after
migration while /etc/sysconfig/rhn/system_id certificate is being updated. As a
consecuence, the migrated client will lose access to the channels.

The problem was that during the certificate update, a call is made to
rhnUser.search() looking for the old username who registered the system and is
contained in the present certificate to update. The regression would produce
a "None" output for the username and the XML response will crash.

This commit restores the rhnUser.search() method to do return the rhnUser
object even if the user is disabled, which would be the expected behavior for
a "search" function, and adds the "user_disabled" checks explicitely together
with the password checking on the affected methods.

backend/server/configFilesHandler.py
@@ -125,6 +125,11 @@ class ConfigFilesHandler(rhnHandler):
         self.user = rhnUser.search(username)
         if not self.user or not (self.user.check_password(password)):
             raise rhnFault(2)
+        if rhnUser.is_user_disabled(username):
+            msg = _("""
+                   %s Account has been deactivated on this server.
+                   Please contact your Org administrator for more help.""")
+            raise rhnFault(1, msg % username, explain=0)
 
         # Good to go
         session = self.user.create_session()

backend/server/handlers/app/packages.py
@@ -191,6 +191,11 @@ class Packages(RPC_Base):
         user = rhnUser.search(username)
         if not user or not user.check_password(password):
             raise rhnFault(2)
+        if rhnUser.is_user_disabled(username):
+            msg = _("""
+                   %s Account has been deactivated on this server.
+                   Please contact your Org administrator for more help.""")
+            raise rhnFault(1, msg % username, explain=0)
         if rhnUser.is_user_read_only(user.username):
             raise rhnFault(702)
         session = user.create_session()

backend/server/handlers/xmlrpc/registration.py
@@ -1137,6 +1137,12 @@ class Registration(rhnHandler):
             log_error("User password check failed", username)
             raise rhnFault(2)
 
+        if rhnUser.is_user_disabled(username):
+            msg = _("""
+                   %s Account has been deactivated on this server.
+                   Please contact your Org administrator for more help.""")
+            raise rhnFault(1, msg % username, explain=0)
+
         return 0
 
     def update_transactions(self, system_id, timestamp, transactions_hash):
@@ -1269,6 +1275,12 @@ class Registration(rhnHandler):
         if not user.check_password(password):
             log_error("User password check failed", username)
             raise rhnFault(2)
+
+        if rhnUser.is_user_disabled(username):
+            msg = _("""
+                   %s Account has been deactivated on this server.
+                   Please contact your Org administrator for more help.""")
+            raise rhnFault(1, msg % username, explain=0)
 
         server_arch = normalize_server_arch(arch)
         user_id = user.getid()

backend/server/importlib/userAuth.py
@@ -200,6 +200,12 @@ def getUserGroups(login, password):
         log_debug("user.check_password failed")
         raise rhnFault(2)
 
+    if rhnUser.is_user_disabled(username):
+        msg = _("""
+               %s Account has been deactivated on this server.
+               Please contact your Org administrator for more help.""")
+        raise rhnFault(1, msg % username, explain=0)
+
     return getUserGroupsFromUserInstance(user)
 
 

backend/server/rhnUser.py
@@ -327,8 +327,7 @@ def search(user):
     """ search the database for a user """
     log_debug(3, user)
     userid = get_user_id(user)
-    if not userid or is_user_disabled(user):
-        # no user found or is disabled
+    if not userid:  # no user found
         return None
     ret = User(user, "")
     if not ret.reload(userid) == 0: