63511b055ddbac861c79ccba1ab088242b72db71
Can Bulut Bayburt <cbbayburt@suse.com>
2017-10-16 17:16:06 +0200

    Add 'INITIALLY DEFERRED' to the unique constraint in suseVHMConfig table (bsc#1063185)
    (cherry picked from commit ebc7aaccb4e3a6a2201cd3e0bba8b9d685807fb4)

--- schema/spacewalk/postgres/tables/suseVHMConfig_alters.sql
+++
@@ -1,3 +0,0 @@
--- oracle equivalent source sha1 3e65bbed0d18f7def20d349d0b72b07a71c3051b
-
-alter table suseVHMConfig add constraint suse_vhmc_id_para_uq unique (virtual_host_manager_id, parameter) DEFERRABLE;
