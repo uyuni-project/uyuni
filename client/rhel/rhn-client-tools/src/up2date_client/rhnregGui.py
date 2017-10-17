spacewalkproject/spacewalk

commit 8ed741dfaf76b37dc88724692f90240aed7a85a5

allow rhn-client-tool use yum or dnf

client/rhel/rhn-client-tools/src/up2date_client/rhnregGui.py
@@ -73,6 +73,7 @@ import up2dateLog
 from rhn import rpclib
 from rhn.connections import idn_puny_to_unicode
 import rhnreg_constants
+from pmPlugin import PM_PLUGIN_NAME, PM_PLUGIN_CONF
 
 cfg = config.initUp2dateConfig()
 log = up2dateLog.initLog()
@@ -147,25 +148,25 @@ class ReviewLog:
         self.addText(rhnreg_constants.ACTIVATION_KEY % (keys))
         self.addText('') # adds newline
 
-    def yum_plugin_warning(self):
-        """ Add to review screen warning that yum-rhn-plugin is not installed """
+    def pm_plugin_warning(self):
+        """ Add to review screen warning that plugin is not installed """
         # prepending -> reverse order
         self.prependText('') # adds newline
-        self.prependText(rhnreg_constants.YUM_PLUGIN_WARNING)
+        self.prependText(rhnreg_constants.PM_PLUGIN_WARNING)
         self.prependBoldText(_("Warning"))
 
-    def yum_plugin_conf_changed(self):
-        """ Add to review screen warning that yum-rhn-plugin config file has been changed """
+    def pm_plugin_conf_changed(self):
+        """ Add to review screen warning that plugin config file has been changed """
         # prepending -> reverse order
         self.prependText('') # adds newline
-        self.prependText(rhnreg_constants.YUM_PLUGIN_CONF_CHANGED)
+        self.prependText(rhnreg_constants.PM_PLUGIN_CONF_CHANGED)
         self.prependBoldText(_("Notice"))
 
-    def yum_plugin_conf_error(self):
-        """ Add to review screen warning that yum-rhn-plugin config file can not be open """
+    def pm_plugin_conf_error(self):
+        """ Add to review screen warning that plugin config file can not be open """
         # prepending -> reverse order
         self.prependText('') # adds newline
-        self.prependText(rhnreg_constants.YUM_PLUGIN_CONF_ERROR)
+        self.prependText(rhnreg_constants.PM_PLUGIN_CONF_ERROR)
         self.prependBoldText(_("Warning"))
 
     def channels(self, subscribedChannels, failedChannels):
@@ -832,16 +833,16 @@ class CreateProfilePage:
                 # no channels subscribe
                 self.noChannels = 1
 
-        # enable yum-rhn-plugin
+        # enable yum-rhn-plugin / dnf-plugin-spacewalk
         try:
             present, conf_changed = rhnreg.pluginEnable()
             if not present:
-                reviewLog.yum_plugin_warning()
+                reviewLog.pm_plugin_warning()
             if conf_changed:
-                reviewLog.yum_plugin_conf_changed()
+                reviewLog.pm_lugin_conf_changed()
         except IOError, e:
-            errorWindow(_("Could not open /etc/yum/pluginconf.d/rhnplugin.conf\nyum-rhn-plugin is not enabled.\n") + e.errmsg)
-            reviewLog.yum_plugin_conf_error()
+            errorWindow(_("Could not open %s\n%s is not enabled.\n") % (PM_PLUGIN_CONF, PM_PLUGIN_NAME) + e.errmsg)
+            reviewLog.pm_plugin_conf_error()
         rhnreg.spawnRhnCheckForUI()
         pwin.setProgress(6,6)
         pwin.hide()

client/rhel/rhn-client-tools/src/bin/rhnreg_ks.py
@@ -35,6 +35,7 @@ from up2date_client import hardware
 from up2date_client import pkgUtils
 from up2date_client import up2dateErrors
 from up2date_client import rhncli
+from up2date_client.pmPlugin import PM_PLUGIN_NAME, PM_PLUGIN_CONF
 
 
 class RegisterKsCli(rhncli.RhnCli):
@@ -175,9 +176,9 @@ class RegisterKsCli(rhncli.RhnCli):
         try:
             present, conf_changed = rhnreg.pluginEnable()
             if not present:
-                sys.stderr.write(rhncli.utf8_encode(_("Warning: yum-rhn-plugin is not present, could not enable it.")))
+                sys.stderr.write(rhncli.utf8_encode(_("Warning: %s is not present, could not enable it.") % PM_PLUGIN_NAME))
         except IOError, e:
-            sys.stderr.write(rhncli.utf8_encode(_("Warning: Could not open /etc/yum/pluginconf.d/rhnplugin.conf\nyum-rhn-plugin is not enabled.\n") + e.errmsg))
+            sys.stderr.write(rhncli.utf8_encode(_("Warning: Could not open %s\n%s is not enabled.\n") % (PM_PLUGIN_CONF, PM_PLUGIN_NAME) + e.errmsg))
         RegisterKsCli.__runRhnCheck(self.options.verbose)
 
     @staticmethod

client/rhel/rhn-client-tools/src/up2date_client/Makefile
@@ -15,7 +15,7 @@ FILES		:= config gui hardware progress \
 		   messageWindow rhncli \
 		   rhnreg_constants \
                    pkgplatform debUtils pkgUtils \
-                   yumPlugin
+                   pmPlugin
 
 PYFILES 	:= $(addsuffix .py, $(FILES))
 PYCFILES	:= $(addsuffix .pyc, $(FILES))

client/rhel/rhn-client-tools/src/up2date_client/pmPlugin.py
@@ -1,58 +1,66 @@
-# Client code for enabling yum-rhn-plugin
-# Copyright (c) 2000--2012 Red Hat, Inc.
+# Client code for enabling plugin
+# Copyright (c) 2000--2015 Red Hat, Inc.
 
 import os
 import re
 import rpm
 
 # global variables
-YUM_PLUGIN_CONF = '/etc/yum/pluginconf.d/rhnplugin.conf'
+try:
+   from yum import __version__
+   PM_PLUGIN_CONF = '/etc/yum/pluginconf.d/rhnplugin.conf'
+   PM_PLUGIN_NAME = 'yum-rhn-plugin'
+   PM_NAME        = 'yum'
+except ImportError:
+   PM_PLUGIN_CONF = '/etc/dnf/plugins/spacewalk.conf'
+   PM_PLUGIN_NAME = 'dnf-plugin-spacewalk'
+   PM_NAME        = 'dnf'
 
 def pluginEnable():
-    """Enables yum-rhn-plugin, may throw IOError"""
+    """Enables plugin, may throw IOError"""
     conf_changed = 0
     plugin_present = 0
-    if YumRHNPluginPackagePresent():
+    if PluginPackagePresent():
         plugin_present = 1
-        if YumRHNPluginConfPresent():
-            if not YumRhnPluginEnabled():
-                enableYumRhnPlugin()
+        if PluginConfPresent():
+            if not PluginEnabled():
+                enablePlugin()
                 conf_changed = 1
         else:
-            createDefaultYumRHNPluginConf()
+            createDefaultPluginConf()
             conf_changed = 1
     elif os.path.exists("/usr/lib/zypp/plugins/services/spacewalk"):
         """SUSE zypp plugin is installed"""
         plugin_present = 1
     return plugin_present, conf_changed
 
-def YumRHNPluginPackagePresent():
-    """ Returns positive number if packaga yum-rhn-plugin is installed, otherwise it return 0 """
+def PluginPackagePresent():
+    """ Returns positive number if plugin package is installed, otherwise it return 0 """
     ts = rpm.TransactionSet()
-    headers = ts.dbMatch('providename', 'yum-rhn-plugin')
+    headers = ts.dbMatch('providename', PM_PLUGIN_NAME)
     return headers.count()
 
-def YumRHNPluginConfPresent():
-    """ Returns true if /etc/yum/pluginconf.d/rhnplugin.conf is presented """
+def PluginConfPresent():
+    """ Returns true if PM_PLUGIN_CONF is presented """
     try:
-        os.stat(YUM_PLUGIN_CONF)
+        os.stat(PM_PLUGIN_CONF)
         return True
     except OSError:
         return False
 
-def createDefaultYumRHNPluginConf():
-    """ Create file /etc/yum/pluginconf.d/rhnplugin.conf with default values """
-    f = open(YUM_PLUGIN_CONF, 'w')
+def createDefaultPluginConf():
+    """ Create file PM_PLUGIN_CONF, with default values """
+    f = open(PM_PLUGIN_CONF, 'w')
     f.write("""[main]
 enabled = 1
 gpgcheck = 1""")
     f.close()
 
-def YumRhnPluginEnabled():
-    """ Returns True if yum-rhn-plugin is enabled
+def PluginEnabled():
+    """ Returns True if plugin is enabled
         Can thrown IOError exception.
     """
-    f = open(YUM_PLUGIN_CONF, 'r')
+    f = open(PM_PLUGIN_CONF, 'r')
     lines = f.readlines()
     f.close()
     main_section = False
@@ -72,16 +80,15 @@ def YumRhnPluginEnabled():
                     result = False
     return result
 
-def enableYumRhnPlugin():
-    """ enable yum-rhn-plugin by setting enabled=1 in file
-        /etc/yum/pluginconf.d/rhnplugin.conf
+def enablePlugin():
+    """ enable plugin by setting enabled=1 in file PM_PLUGIN_CONF
         Can thrown IOError exception.
     """
-    f = open(YUM_PLUGIN_CONF, 'r')
+    f = open(PM_PLUGIN_CONF, 'r')
     lines = f.readlines()
     f.close()
     main_section = False
-    f = open(YUM_PLUGIN_CONF, 'w')
+    f = open(PM_PLUGIN_CONF, 'w')
     for line in lines:
         if re.match("^\[.*]", line):
             if re.match("^\[main]", line):

client/rhel/rhn-client-tools/src/up2date_client/rhnreg.py
@@ -646,5 +646,5 @@ if getPlatform() == 'deb':
         """On Debian no extra action for plugin is needed"""
         return 1, 0
 else:
-    from yumPlugin import pluginEnable
+    from pmPlugin import pluginEnable
 

client/rhel/rhn-client-tools/src/up2date_client/rhnreg_constants.py
@@ -7,6 +7,8 @@
 #       James Slagle <jslagle@redhat.com>
 
 
+from up2date_client.pmPlugin import PM_PLUGIN_NAME, PM_NAME
+
 import gettext
 t = gettext.translation('rhn-client-tools', fallback=True)
 _ = t.ugettext
@@ -148,7 +150,7 @@ SENDING_WINDOW = _("Sending Profile to Red Hat Satellite")
 
 # Finish Window
 FINISH_WINDOW           = _("Updates Configured")
-FINISH_WINDOW_TEXT_TUI  = _("You may now run 'yum update' from this system's "
+FINISH_WINDOW_TEXT_TUI  = _("You may now run '%s update' from this system's "
                             "command line to get the latest "
                             "software updates from Red Hat Satellite. You will need to run this "
                             "periodically to "
@@ -156,7 +158,7 @@ FINISH_WINDOW_TEXT_TUI  = _("You may now run 'yum update' from this system's "
                             "system for automatic software updates (also known as 'auto errata update') "
                             "via the Red Hat Satellite web interface.  (Instructions for this are in chapter 6 "
                             "of the RHN Reference Guide, available from the 'Help' button in the main Red "
-                            "Hat Network Satellite web interface.)")
+                            "Hat Network Satellite web interface.)") % PM_NAME
 
 # Review Window
 REVIEW_WINDOW           = _("Review Subscription")
@@ -176,12 +178,12 @@ CHANNELS_SAT_WARNING    = _("Warning: Only installed product listed above will r
                             "software channels. See Kbase article "
                             "for more details. "
                             "(http://kbase.redhat.com/faq/docs/DOC-11313)")
-YUM_PLUGIN_WARNING              = _("Warning: yum-rhn-plugin is not present, could not enable it.\n"
-                                                        "Automatic updates will not work.")
-YUM_PLUGIN_CONF_CHANGED = _("Note: yum-rhn-plugin has been enabled.")
-YUM_PLUGIN_CONF_ERROR   = _("Warning: An error occurred during enabling yum-rhn-plugin.\n"
-                                                        "yum-rhn-plugin is not enabled.\n"
-                                                        "Automatic updates will not work.")
+PM_PLUGIN_WARNING       = _("Warning: %s is not present, could not enable it.\n"
+                            "Automatic updates will not work.") % (PM_PLUGIN_NAME)
+PM_PLUGIN_CONF_CHANGED  = _("Note: %s has been enabled.") % (PM_PLUGIN_NAME)
+PM_PLUGIN_CONF_ERROR    = _("Warning: An error occurred during enabling %s.\n"
+                            "%s is not enabled.\n"
+                            "Automatic updates will not work.") % (PM_PLUGIN_NAME, PM_PLUGIN_NAME)
 FAILED_CHANNELS         = _("You were unable to be subscribed to the following "
                             "software channels because there were insufficient "
                             "subscriptions available in your account:")

client/rhel/rhn-client-tools/src/up2date_client/tui.py
@@ -35,7 +35,7 @@ from config import convert_url_from_puny
 import up2dateAuth
 from rhn import rpclib
 from rhn.connections import idn_puny_to_unicode
-
+from pmPlugin import PM_PLUGIN_NAME, PM_PLUGIN_CONF
 from rhnreg_constants import *
 
 log = up2dateLog.initLog()
@@ -1044,12 +1044,12 @@ class SendingWindow:
         # Send virtualization information to the server.
         rhnreg.sendVirtInfo(systemId)
 
-        # enable yum-rhn-plugin
+        # enable yum-rhn-plugin / dnf-plugin-spacewalk
         try:
-            self.tui.yum_plugin_present, self.tui.yum_plugin_conf_changed = rhnreg.pluginEnable()
+            self.tui.pm_plugin_present, self.tui.pm_plugin_conf_changed = rhnreg.pluginEnable()
         except IOError, e:
-            WarningWindow(self.screen, _("Could not open /etc/yum/pluginconf.d/rhnplugin.conf\nyum-rhn-plugin is not enabled.\n") + e.errmsg)
-            self.tui.yum_plugin_conf_error = 1
+            WarningWindow(self.screen, _("Could not open %s\n%s is not enabled.\n") % (PM_PLUGIN_CONF, PM_PLUGIN_NAME) + e.errmsg)
+            self.tui.pm_plugin_conf_error = 1
 
         rhnreg.spawnRhnCheckForUI()
         self.setScale(4, 4)
@@ -1117,12 +1117,12 @@ class ReviewWindow:
         toplevel = snack.GridForm(screen, REVIEW_WINDOW.encode('utf-8'), 1, 2)
         review_window_text = ''
 
-        if not self.tui.yum_plugin_present:
-            review_window_text += YUM_PLUGIN_WARNING + "\n\n"
-        if self.tui.yum_plugin_conf_error:
-            review_window_text += YUM_PLUGIN_CONF_ERROR + "\n\n"
-        if self.tui.yum_plugin_conf_changed:
-            review_window_text += YUM_PLUGIN_CONF_CHANGED + "\n\n"
+        if not self.tui.pm_plugin_present:
+            review_window_text += PM_PLUGIN_WARNING + "\n\n"
+        if self.tui.pm_plugin_conf_error:
+            review_window_text += PM_PLUGIN_CONF_ERROR + "\n\n"
+        if self.tui.pm_plugin_conf_changed:
+            review_window_text += PM_PLUGIN_CONF_CHANGED + "\n\n"
 
         # Build up the review_window_text based on the data in self.reg_info
         review_window_text += REVIEW_WINDOW_PROMPT + "\n\n"
@@ -1189,9 +1189,9 @@ class ReviewWindow:
 
         if result == "F12":
             button = "next"
-        if not self.tui.yum_plugin_present:
+        if not self.tui.pm_plugin_present:
             button = "exit"
-        if self.tui.yum_plugin_conf_error:
+        if self.tui.pm_plugin_conf_error:
             button = "exit"
 
         return button
@@ -1271,9 +1271,9 @@ class Tui:
         self.includePackages = 0
         self.packageList = []
         self.selectedPackages = []
-        self.yum_plugin_present = 1
-        self.yum_plugin_conf_error = 0
-        self.yum_plugin_conf_changed = 0
+        self.pm_plugin_present = 1
+        self.pm_plugin_conf_error = 0
+        self.pm_plugin_conf_changed = 0
 
     def run(self):
         log.log_debug("Running %s" % self.name)

client/rhel/rhn-client-tools/src/up2date_client/up2dateErrors.py
@@ -32,7 +32,7 @@ import sys
 sys.path = sys.path[1:] + sys.path[:1]
 
 if getPlatform() == 'deb' or dist()[0] == 'SuSE':
-    class YumBaseError(Exception):
+    class PmBaseError(Exception):
         def __init__(self, errmsg):
             self.value = errmsg
         def __getattr__(self, name):
@@ -43,15 +43,19 @@ if getPlatform() == 'deb' or dist()[0] == 'SuSE':
             else:
                 self.__dict__[name] = value
 else:
-    from yum.Errors import YumBaseError
+    try:
+        from yum.Errors import YumBaseError as PmBaseError
+    except ImportError:
+        from dnf.exceptions import Error as PmBaseError
 
-class Error(YumBaseError):
+
+class Error(PmBaseError):
     """base class for errors"""
     premsg = ''
     def __init__(self, errmsg):
         if not isinstance(errmsg, unicode):
             errmsg = unicode(errmsg, 'utf-8')
-        YumBaseError.__init__(self, errmsg)
+        PmBaseError.__init__(self, errmsg)
         self.value = 'rhn-plugin: ' + self.premsg + errmsg
         self.log = up2dateLog.initLog()
 
@@ -64,9 +68,9 @@ class Error(YumBaseError):
         if name == 'errmsg':
             return self.value
         else:
-            if hasattr(YumBaseError, '__getattr__'):
+            if hasattr(PmBaseError, '__getattr__'):
                 # rhel5 has no __getattribute__()
-                return YumBaseError.__getattr__(self, name)
+                return PmBaseError.__getattr__(self, name)
             else:
                 if name in self.__dict__:
                     return self.__dict__[name]
@@ -78,9 +82,9 @@ class Error(YumBaseError):
         if name == 'errmsg':
             self.__dict__['value'] = value
         else:
-            if hasattr(YumBaseError, '__setattr__'):
+            if hasattr(PmBaseError, '__setattr__'):
                 # rhel5 has no __setattr__()
-                YumBaseError.__setattr__(self, name, value)
+                PmBaseError.__setattr__(self, name, value)
             else:
                 self.__dict__[name] = value
 
@@ -90,7 +94,10 @@ class DebAndSuseRepoError(Error):
 if getPlatform() == 'deb' or dist()[0] == 'SuSE':
     RepoError = DebAndSuseRepoError
 else:
-    from yum.Errors import RepoError
+    try:
+        from yum.Errors import RepoError
+    except ImportError:
+        from dnf.exceptions import RepoError
 
 class RpmError(Error):
     """rpm itself raised an error condition"""

uyuni-project/uyuni

commit 77bba2ada4f91c61ef31ecf410712ed1d05c3d0d

fixed typo introduced in 8ed741dfaf76b37dc88724692f90240aed7a85a5

client/rhel/rhn-client-tools/src/up2date_client/rhnregGui.py
@@ -852,7 +852,7 @@ class CreateProfilePage:
             if not present:
                 reviewLog.pm_plugin_warning()
             if conf_changed:
-                reviewLog.pm_lugin_conf_changed()
+                reviewLog.pm_plugin_conf_changed()
         except IOError:
             e = sys.exc_info()[1]
             errorWindow(_("Could not open %s\n%s is not enabled.\n") % (PM_PLUGIN_CONF, PM_PLUGIN_NAME) + e.errmsg)