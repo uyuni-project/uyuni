582d4455585e2864c353371dc5bab90971d027ec
Abid Mehmood <amehmood@suse.de>
2017-10-17 16:24:34 +0200

    Add config files capabilities to Minions and also add feature config(ftr_config) for Salt Entitled systems
    Removed restriction enabled for Minions

--- schema/spacewalk/common/data/rhnServerGroupTypeFeature.sql
+++ schema/spacewalk/common/data/rhnServerGroupTypeFeature.sql
@@ -243,3 +243,8 @@ insert into rhnServerGroupTypeFeature (server_group_type_id, feature_id,
                                        created, modified)
 values (lookup_sg_type('salt_entitled'), lookup_feature_type('ftr_power_management'),
         current_timestamp,current_timestamp);
+
+insert into rhnServerGroupTypeFeature (server_group_type_id, feature_id,
+                                       created, modified)
+values (lookup_sg_type('salt_entitled'), lookup_feature_type('ftr_config'),
+        current_timestamp,current_timestamp);
