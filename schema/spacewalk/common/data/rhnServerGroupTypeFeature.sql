afbbef8f7571cb7afe0bfaca95c7e4573fe500ca
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2017-10-20 16:46:27 +0100

    Enable 'ftr_power_management' on Salt minions

--- schema/spacewalk/common/data/rhnServerGroupTypeFeature.sql
+++ schema/spacewalk/common/data/rhnServerGroupTypeFeature.sql
@@ -238,3 +238,8 @@ insert into rhnServerGroupTypeFeature (server_group_type_id, feature_id,
                                        created, modified)
 values (lookup_sg_type('salt_entitled'), lookup_feature_type('ftr_system_custom_values'),
         current_timestamp,current_timestamp);
+
+insert into rhnServerGroupTypeFeature (server_group_type_id, feature_id,
+                                       created, modified)
+values (lookup_sg_type('salt_entitled'), lookup_feature_type('ftr_power_management'),
+        current_timestamp,current_timestamp);
