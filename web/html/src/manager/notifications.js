0323cb605c9d59be837bec2ab059dc48997475fa
Dario Leidi <dleidi@suse.com>
2017-10-24 14:41:08 +0200

    Notifications: add a websocket endpoint in the header-bar

---
+++ web/html/src/manager/notifications.js
@@ -0,0 +1,100 @@
+'use strict';
+
+const React = require("react");
+const ReactDOM = require("react-dom");
+const Network = require("../utils/network");
+
+const Notifications = React.createClass({
+
+  getInitialState: function () {
+    return {
+      messages: [],
+      websocket: null
+    }
+  },
+
+  onBeforeUnload: function(e) {
+      this.setState({
+          pageUnloading: true
+      });
+  },
+
+  componentDidMount: function() {
+    var port = window.location.port;
+    var url = "wss://" +
+      window.location.hostname +
+      (port ? ":" + port : "") +
+       "/rhn/websocket/notifications";
+    var ws = new WebSocket(url);
+    ws.onopen = () => {
+        console.log('Websocket onOpen');
+      this.setState({
+      });
+    };
+    ws.onclose = (e) => {
+        console.log('Websocket onClose');
+      var errs = this.state.errors ? this.state.errors : [];
+      if (!this.state.pageUnloading && !this.state.websocketErr) {
+          errs.push(t("Websocket connection closed. Refresh the page to try again."));
+      }
+      this.setState({
+          errors: errs,
+          websocket: null
+      });
+    };
+    ws.onerror = (e) => {
+      console.log("Websocket error: " + e);
+      this.setState({
+         errors: [t("Error connecting to server. Refresh the page to try again.")],
+         websocketErr: true
+      });
+    };
+    ws.onmessage = (e) => {
+        console.log('Websocket message=' + e.data);
+        var messagesList = this.state.messages;
+        messagesList.push(e.data);
+        this.setState({messages: messagesList})
+    };
+    window.addEventListener("beforeunload", this.onBeforeUnload)
+
+    this.setState({
+        websocket: ws
+    });
+  },
+
+  componentWillUnmount: function() {
+    window.removeEventListener("beforeunload", this.onBeforeUnload)
+  },
+  
+  // tester function: click on the widget to test the send/receive of a message via websocket
+  onClick: function() {
+    this.state.websocket.send('ping');
+  },
+
+  render: function() {
+    return (
+      <div onClick={this.onClick}><i className="fa fa-envelope"></i>{this.state.messages.length}</div>
+    );
+  }
+});
+
+// override the existing errorMessageByStatus from utils/network.js 
+function errorMessageByStatus(status) {
+  if (status == 401) {
+    return [t("Session expired, please reload the page to receive notifications in real-time.")];
+  }
+  else if (status == 403) {
+    return [t("Authorization error, please reload the page or try to logout/login again.")];
+  }
+  else if (status >= 500) {
+    return [t("Server error, please check log files.")];
+  }
+  else {
+    return [];
+  }
+}
+
+ReactDOM.render(
+  <Notifications />,
+  document.getElementById('notifications')
+);
