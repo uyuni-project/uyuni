730ed97f4f9e4f39168da43eb227a354d76a7c95
Dario Leidi <dleidi@suse.com>
2017-10-31 11:03:43 +0100

    Remove stale import

--- java/code/src/com/redhat/rhn/domain/notification/NotificationMessageFactory.java
+++ java/code/src/com/redhat/rhn/domain/notification/NotificationMessageFactory.java
@@ -21,7 +21,6 @@ import com.suse.manager.webui.websocket.Notification;
 
 import org.apache.log4j.Logger;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
