2148add5717fd2a358e98a43fbd414862960cf6b
Dario Leidi <dleidi@suse.com>
2017-11-05 19:24:29 +0100

    Add notification message type

--- schema/spacewalk/common/tables/suseNotificationMessage.sql
+++ schema/spacewalk/common/tables/suseNotificationMessage.sql
@@ -5,6 +5,9 @@
 CREATE TABLE public.susenotificationmessage
 (
   id numeric NOT NULL,
+  notifmess_type_id numeric NOT NULL
+    CONSTRAINT suse_notifmess_ctid_fk
+    REFERENCES suseNotificationMessageType (id),
   description character varying(256) NOT NULL,
   isRead boolean NOT NULL DEFAULT false,
   created timestamp with time zone NOT NULL DEFAULT now(),
