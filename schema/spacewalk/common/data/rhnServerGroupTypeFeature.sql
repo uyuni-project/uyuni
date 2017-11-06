e4c788d27cd707dc44bdc1489c31de5790fdd757
Abid Mehmood <amehmood@suse.de>
2017-11-06 16:30:48 +0100

    Add ftr_channel_config_subscription' and 'ftr_channel_deploy_diff' for minions

--- schema/spacewalk/common/data/rhnServerGroupTypeFeature.sql
+++ schema/spacewalk/common/data/rhnServerGroupTypeFeature.sql
@@ -233,7 +233,14 @@ insert into rhnServerGroupTypeFeature (server_group_type_id, feature_id,
                                        created, modified)
 values (lookup_sg_type('salt_entitled'), lookup_feature_type('ftr_channel_membership'),
         current_timestamp,current_timestamp);
-
+insert into rhnServerGroupTypeFeature (server_group_type_id, feature_id,
+                                       created, modified)
+values (lookup_sg_type('salt_entitled'), lookup_feature_type('ftr_channel_config_subscription'),
+        current_timestamp,current_timestamp);
+insert into rhnServerGroupTypeFeature (server_group_type_id, feature_id,
+                                       created, modified)
+values (lookup_sg_type('salt_entitled'), lookup_feature_type('ftr_channel_deploy_diff'),
+        current_timestamp,current_timestamp);
 insert into rhnServerGroupTypeFeature (server_group_type_id, feature_id,
                                        created, modified)
 values (lookup_sg_type('salt_entitled'), lookup_feature_type('ftr_system_custom_values'),
