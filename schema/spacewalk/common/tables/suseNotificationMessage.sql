e71589444349e663b228a3f2e573987283c7a118
Kevin Walter <kwalter@suse.com>
2017-12-08 11:44:03 +0100

    fix sql end migration

--- schema/spacewalk/common/tables/suseNotificationMessage.sql
+++ schema/spacewalk/common/tables/suseNotificationMessage.sql
@@ -1,28 +1,10 @@
-CREATE TABLE susenotificationmessage
+CREATE TABLE suseNotificationMessage
 (
-  id NUMBER NOT NULL
-    CONSTRAINT suse_notifmess_pk PRIMARY KEY,
-  type VARCHAR2(32) NOT NULL,
-  data VARCHAR2(1024) NOT NULL,
-  created timestamp with time zone NOT NULL DEFAULT now()
+  id                NUMBER NOT NULL
+                        CONSTRAINT suse_notifmess_pk PRIMARY KEY,
+  type              VARCHAR2(32) NOT NULL,
+  data              VARCHAR2(1024) NOT NULL,
+  created           timestamp with local time zone default(current_timestamp) NOT NULL
 );
 
-CREATE SEQUENCE suse_notification_message_id_seq;
-
-CREATE TABLE suseusernotification
-(
-    id              NUMBER NOT NULL
-                        CONSTRAINT suse_user_notif_pk PRIMARY KEY, 
-    user_id         NUMBER NOT NULL
-                        CONSTRAINT suse_user_notif_uid_fk
-                        REFERENCES web_contact (id)
-                        ON DELETE CASCADE,
-    message_id      NUMBER NOT NULL
-                        CONSTRAINT suse_user_notif_mid_fk
-                        REFERENCES susenotificationmessage (id)
-                        ON DELETE CASCADE,
-    read CHAR(1) DEFAULT ('N') NOT NULL,
-    UNIQUE (user_id, message_id)
-);
-
-CREATE SEQUENCE suse_user_notification_id_seq;
+CREATE SEQUENCE suse_notif_message_id_seq;
