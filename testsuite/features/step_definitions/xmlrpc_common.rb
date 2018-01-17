5a33032927d19c38b6406f840576af42000e737b
Eric Bischoff <ebischoff@suse.com>
2018-01-17 13:08:14 +0100

    TESTSUITE: config management of minions: XML-RPC API

--- testsuite/features/step_definitions/xmlrpc_common.rb
+++ testsuite/features/step_definitions/xmlrpc_common.rb
@@ -501,3 +501,38 @@ end
 Then(/^I logout from XML\-RPC cve audit namespace$/) do
   assert(@rpctest.logout)
 end
+
+# configchannel namespace
+
+cfgtest = XMLRPCConfigChannelTest.new(ENV['SERVER'])
+
+Given(/^I am logged in via XML\-RPC configchannel as user "([^"]*)" and password "([^"]*)"$/) do |luser, password|
+  cfgtest.login(luser, password)
+end
+
+Then(/^channel "([^"]*)" should exist$/) do |channel|
+  assert_equal(1, cfgtest.channel_exists(channel))
+end
+
+Then(/^channel "([^"]*)" should contain file "([^"]*)"$/) do |channel, file|
+  result = cfgtest.list_files(channel)
+  assert_equal(1, result.count { |item| item['path'] == file })
+end
+
+Then(/^"([^"]*)" should be subscribed to channel "([^"]*)"$/) do |host, channel|
+  node = get_target(host)
+  result = cfgtest.list_subscribed_systems(channel)
+  assert_equal(1, result.count { |item| item['name'] == node.full_hostname })
+end
+
+When(/^I add file "([^"]*)" containing "([^"]*)" to channel "([^"]*)"$/) do |file, contents, channel|
+  cfgtest.create_or_update_path(channel, file, contents)
+end
+
+When(/^I deploy all systems registered to channel "([^"]*)"$/) do |channel|
+  cfgtest.deploy_all_systems(channel)
+end
+
+When(/^I logout from XML\-RPC configchannel namespace$/) do
+  cfgtest.logout
+end
