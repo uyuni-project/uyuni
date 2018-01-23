ac3cd2fcb5151859e5c7868435a23d9a29cdd677
Frantisek Kobzik <fkobzik@suse.de>
2018-01-23 16:14:40 +0100

    Create state channels from custom states and adjust assignment, drop old states and their assignments, relabel the configuration channels that conflict with custom states

--- schema/spacewalk/postgres/tables/suseCustomState_index.sql
+++
@@ -1,6 +0,0 @@
--- oracle equivalent source sha1 df2f6fdeca774648428333d2dbf3dd82e2c7d220
--- functional index for suseCustomState
-
-CREATE UNIQUE INDEX suse_custom_state_name_org_uq
-ON suseCustomState (org_id, state_name)
-WHERE state_deleted='N';
