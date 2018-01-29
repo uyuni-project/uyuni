e18b9d4eb3e75121886e4844c6140b8fac687afe
Matei Albu <malbu@suse.de>
2018-01-29 21:58:09 +0100

    single system and SSM UI for scheduling software channels change
    The change of the base and child channels have been unified in a single step.

---
+++ java/code/src/com/suse/manager/webui/utils/gson/SsmScheduleChannelChangesJson.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2017 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.suse.manager.webui.utils.gson;
+
+import com.redhat.rhn.manager.ssm.ChannelChangeDto;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Object holding info about SSM channel changes.
+ */
+public class SsmScheduleChannelChangesJson {
+
+    private Optional<LocalDateTime> earliest = Optional.empty();
+    private List<ChannelChangeDto> changes = new ArrayList<>();
+
+    /**
+     * @return earliest date/time to execute
+     */
+    public Optional<LocalDateTime> getEarliest() {
+        return earliest;
+    }
+
+    /**
+     * @return list of changes to be applied
+     */
+    public List<ChannelChangeDto> getChanges() {
+        return changes;
+    }
+}
---
+++ java/code/src/com/suse/manager/webui/utils/gson/SubscribeChannelsJson.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2017 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.suse.manager.webui.utils.gson;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Subscribe to Channels JSON class
+ */
+public class SubscribeChannelsJson {
+
+    private Optional<ChannelsJson.ChannelJson> base;
+    private List<ChannelsJson.ChannelJson> children;
+    private Optional<LocalDateTime> earliest = Optional.empty();
+
+    /**
+     * @return the base channel to set
+     */
+    public Optional<ChannelsJson.ChannelJson> getBase() {
+        return base;
+    }
+
+    /**
+     * @return the child channels to set
+     */
+    public List<ChannelsJson.ChannelJson> getChildren() {
+        return children;
+    }
+
+    /**
+     * @return The earliest execution date
+     */
+    public Optional<LocalDateTime> getEarliest() {
+        return earliest;
+    }
+}
