87da79d67cf395411fdf71e23748a380fa1f6277
Eric Bischoff <ebischoff@suse.com>
2018-02-05 17:37:19 +0100

    TESTSUITE: no check for prerequistes
    The prerequistites are assumed to be always verified now that we have
    reorganized in core features and idempotent features. There is therefore
    no need to check for them.

--- testsuite/features/step_definitions/docker_steps.rb
+++ testsuite/features/step_definitions/docker_steps.rb
@@ -208,25 +208,3 @@ Then(/I create "([^"]*)" random "([^"]*)" containers$/) do |count, image_input|
     cont_op.schedule_image_build(image, version_build, build_hostid, date_build)
   end
 end
-
-And(/^I check that sles-minion exists otherwise bootstrap it$/) do
-  ck_minion =  "salt #{$minion.full_hostname} test.ping"
-  _out, code = $server.run(ck_minion, false)
-  if code.nonzero?
-    # bootstrap minion
-    steps %(
-      Given I am authorized
-      When I follow "Salt"
-      Then I should see a "Bootstrapping" text
-      And I follow "Bootstrapping"
-      Then I should see a "Bootstrap Minions" text
-      And  I enter the hostname of "sle-minion" as hostname
-      And I enter "22" as "port"
-      And I enter "root" as "user"
-      And I enter "linux" as "password"
-      And I click on "Bootstrap"
-      And I wait for "150" seconds
-      Then I should see a "Successfully bootstrapped host! Your system should appear in System Overview shortly." text
-    )
-  end
-end
