e0beed8b3d858bb6b4bfb1929acb2d9344b9e84e
Frantisek Kobzik <fkobzik@suse.de>
2018-02-07 12:01:19 +0100

    Fix the new FormulaForm usage

--- web/html/src/manager/group-formula.js
+++ web/html/src/manager/group-formula.js
@@ -8,35 +8,6 @@ var FormulaFormModule = require("../components/FormulaForm");
 var FormulaForm = FormulaFormModule.FormulaForm;
 var toTitle = FormulaFormModule.toTitle;
 
-function saveFormula(component, values) {
-    var formData = {};
-    formData.type = "GROUP";
-    formData.id = groupId;
-    formData.formula_name = component.state.formulaName;
-    formData.content = component.extractValues();
-
-    Network.post(
-        "/rhn/manager/api/formulas/save",
-        JSON.stringify(formData),
-        "application/json"
-    ).promise.then((data) => {
-        component.setState({
-            messages: data
-        });
-    },
-    (xhr) => {
-        try {
-            component.setState({
-                errors: [JSON.parse(xhr.responseText)]
-            })
-        } catch (err) {
-            component.setState({
-                errors: [Network.errorMessageByStatus(xhr.status)]
-            })
-        }
-    });
-}
-
 function addFormulaNavBar(formulaList, activeId) {
     $("#formula-nav-bar").remove();
 
@@ -51,11 +22,12 @@ function addFormulaNavBar(formulaList, activeId) {
 ReactDOM.render(
     <FormulaForm
           dataUrl={"/rhn/manager/api/formulas/form/GROUP/" + groupId + "/" + formulaId}
+          saveUrl="/rhn/manager/api/formulas/save"
           addFormulaNavBar={addFormulaNavBar}
           formulaId={formulaId}
+          systemId={groupId}
           getFormulaUrl={function(id) {return "/rhn/manager/groups/details/formula/" + id + "?sgid=" + groupId;}}
-          saveFormula={saveFormula}
-          currentScope="group" />,
+          scope="group" />,
     document.getElementById('formula')
 );
 
--- web/html/src/manager/minion-formula.js
+++ web/html/src/manager/minion-formula.js
@@ -8,35 +8,6 @@ var FormulaFormModule = require("../components/FormulaForm");
 var FormulaForm = FormulaFormModule.FormulaForm;
 var toTitle = FormulaFormModule.toTitle;
 
-function saveFormula(component, values) {
-    var formData = {};
-    formData.type = "SERVER";
-    formData.id = serverId;
-    formData.formula_name = component.state.formulaName;
-    formData.content = component.extractValues();
-
-    Network.post(
-        "/rhn/manager/api/formulas/save",
-        JSON.stringify(formData),
-        "application/json"
-    ).promise.then((data) => {
-        component.setState({
-            messages: data
-        });
-    },
-    (xhr) => {
-        try {
-            component.setState({
-                errors: [JSON.parse(xhr.responseText)]
-            })
-        } catch (err) {
-            component.setState({
-                errors: [Network.errorMessageByStatus(xhr.status)]
-            })
-        }
-    });
-}
-
 function addFormulaNavBar(formulaList, activeId) {
     $("#formula-nav-bar").remove();
 
@@ -51,11 +22,12 @@ function addFormulaNavBar(formulaList, activeId) {
 ReactDOM.render(
     <FormulaForm
           dataUrl={"/rhn/manager/api/formulas/form/SERVER/" + serverId + "/" + formulaId}
+          saveUrl="/rhn/manager/api/formulas/save"
           addFormulaNavBar={addFormulaNavBar}
           formulaId={formulaId}
+          systemId={serverId}
           getFormulaUrl={function(id) {return "/rhn/manager/systems/details/formula/" + id + "?sid=" + serverId;}}
-          saveFormula={saveFormula}
-          currentScope="system" />,
+          scope="system" />,
     document.getElementById('formula')
 );
 
