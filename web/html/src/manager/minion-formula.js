c5a2357ce12dfb70fef7daed3a5636f2481beb9a
Frantisek Kobzik <fkobzik@suse.de>
2018-02-07 16:09:45 +0100

    Fix the 'capitalize' usage

--- web/html/src/manager/minion-formula.js
+++ web/html/src/manager/minion-formula.js
@@ -6,7 +6,7 @@ const ReactDOM = require("react-dom");
 var Network = require("../utils/network");
 var FormulaFormModule = require("../components/FormulaForm");
 var FormulaForm = FormulaFormModule.FormulaForm;
-var toTitle = FormulaFormModule.toTitle;
+const capitalize = require("../utils/functions").Utils.capitalize;
 
 function addFormulaNavBar(formulaList, activeId) {
     $("#formula-nav-bar").remove();
@@ -14,7 +14,7 @@ function addFormulaNavBar(formulaList, activeId) {
     var navBar = "<ul class='nav nav-tabs nav-tabs-pf' id='formula-nav-bar'>\n"
     navBar += "<li><a href='/rhn/manager/systems/details/formulas?sid=" + serverId + "'>Formulas</a></li>\n";
     for (var i in formulaList)
-        navBar += "<li" + (i == activeId ? " class='active'>" : ">") + "<a href='/rhn/manager/systems/details/formula/" + i + "?sid=" + serverId + "'>" + toTitle(formulaList[i]) + "</a></li>\n";
+        navBar += "<li" + (i == activeId ? " class='active'>" : ">") + "<a href='/rhn/manager/systems/details/formula/" + i + "?sid=" + serverId + "'>" + capitalize(formulaList[i]) + "</a></li>\n";
     navBar += "</ul>"
     $(".spacewalk-content-nav").append(navBar);
 }
