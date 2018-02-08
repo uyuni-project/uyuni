f4bbcc2ba30cd3b9f0276a67ac7cc5aeea0d3997
dleidi <dleidi@suse.com>
2018-02-08 17:27:43 +0100

    Squash the query and fix the package logic extraction

--- susemanager/src/fix-rpm-buildtime.py
+++ susemanager/src/fix-rpm-buildtime.py
@@ -9,63 +9,36 @@ import os, rpm
 _package_path_prefix = '/var/spacewalk/'
 
 #
-# - find every sync after install/update to 3.1
+# - find first sync after install/update to 3.1
+# - get the list of package ids created after install/update to 3.1
 #
-_query_find_all_affected_sync = rhnSQL.Statement("""
-    select
-        created,
-        version
-    from rhnVersionInfo vi
-        join rhnPackageEVR evr
-            on vi.evr_id = evr.id
-    where evr.version like '3.1.%'
-    order by created
-""")
-
-def get_affected_syncs():
-    h = rhnSQL.prepare(_query_find_all_affected_sync)
-    h.execute()
-    affectedsyncs = h.fetchall_dict()
-    return affectedsyncs
-
-
-#
-# - get the list of package ids for certain version
-#
-_query_all_package_ids_by_version = rhnSQL.Statement("""
-    select id
-    from rhnPackageEVR
-    where version = :version
-""")
-
-def get_all_package_ids_by_version(version):
-    h = rhnSQL.prepare(_query_all_package_ids_by_version)
-    h.execute(version=version)
-    affected_package_ids = h.fetchall_dict()
-    return affected_package_ids
-
-
-#
-# - every found package (rhnPackage column) has "path" and "buildtime".
-#
-_query_package_by_id = rhnSQL.Statement("""
+_query_all_possible_affected_packages = rhnSQL.Statement("""
     select
         p.id,
         p.path,
         p.build_time,
         pn.name
-    from rhnPackage p
+    from rhnpackage p
         join rhnPackageName pn
             on p.name_id = pn.id
-    where p.id = :id
+    where created >=
+    (
+        select
+            created
+        from rhnVersionInfo vi
+            join rhnPackageEVR evr
+                on vi.evr_id = evr.id
+        where evr.version like '3.1.%'
+        order by created
+        limit 1
+    )
 """)
 
-def get_package_by_id(id):
-    h = rhnSQL.prepare(_query_package_by_id)
-    h.execute(id=id)
-    package = h.fetchone_dict()
-    return package
-
+def get_all_possible_affected_packages():
+    h = rhnSQL.prepare(_query_all_possible_affected_packages)
+    h.execute()
+    affected_packages = h.fetchall_dict()
+    return affected_packages
 
 #
 # - find rpm via "path", parse the correct build time
@@ -141,61 +114,50 @@ if __name__ == '__main__':
 
     something_changed = False
 
-    # find every sync after install/update to 3.1
-    all_syncs = get_affected_syncs()
+    # find all packages
+    package_list = get_all_possible_affected_packages()
+    print ''
+    print 'Scanning', len(package_list), 'packages'
 
-    print 'Affected syncs versions:'
+    # iterate on each package
+    for package_blob in package_list[:2]:
 
-    for sync in all_syncs :
-        print '-', sync['version']
+        package_id = package_blob['id']
 
-        # find all package id list for the current sync version
-        package_id_list = get_all_package_ids_by_version(sync['version'])
-        print ''
-        print 'Scanning', len(package_id_list), 'packages for the', sync['version'], ' sync version..'
+        # get the absolute path of the package on the filesystem
+        absolute_package_path = _package_path_prefix + package_blob['path']
 
-        # iterate on each package found on the sync version
-        for package_id_blob in package_id_list:
+        # extract the rpm_tag_build_time from the rpm
+        rpm_tag_build_time = datetime(*time.gmtime(get_rpm_tag_build_time(absolute_package_path))[:6])
 
-            package_id = package_id_blob['id']
+        # compare RPM vs DB "buildtime"s
+        if package_blob['build_time'] != rpm_tag_build_time:
+            print ''
+            print '###############'
+            print 'id =', package_id
+            print 'name =', package_blob['name']
+            print 'path =', absolute_package_path
+            print ''
+            print '>>> DB package build time  :', package_blob['build_time']
+            print '<<< RPM package build time :', rpm_tag_build_time
+            print ''
 
-            # extract the package from the database
-            current_db_package = get_package_by_id(package_id)
-            
-            # get the absolute path of the package on the filesystem
-            absolute_package_path = _package_path_prefix + current_db_package['path']
+            # update the row in DB
+            ret = update_package_build_time_by_id(package_id, rpm_tag_build_time)
+            if ret == 1:
+                print 'build_time updated for the package id', package_id
+                something_changed = True
+            else:
+                print 'something went wrong during update of the build_time for the package id', package_id
 
-            # extract the rpm_tag_build_time from the rpm
-            rpm_tag_build_time = datetime(*time.gmtime(get_rpm_tag_build_time(absolute_package_path))[:6])
-
-            # compare RPM vs DB "buildtime"s
-            if current_db_package['build_time'] != rpm_tag_build_time:
-                print ''
-                print '###############'
-                print 'id =', package_id
-                print 'name =', current_db_package['name']
-                print 'path =', absolute_package_path
-                print ''
-                print '>>> DB package build time  :', current_db_package['build_time']
-                print '<<< RPM package build time :', rpm_tag_build_time
-                print ''
-
-                # update the row in DB
-                ret = update_package_build_time_by_id(package_id, rpm_tag_build_time)
-                if ret == 1:
-                    print 'build_time updated for the package id', package_id
-                    something_changed = True
-                else:
-                  print 'something went wrong during update of the build_time for the package id', package_id
-
-                # remove the stored metadata snippet for this package
-                ret = delete_repodata_snippet_by_id(package_id)
-                if ret == 1:
-                    print 'repodata snipped removed for the package id', package_id
-                    something_changed = True
-                else:
-                    print 'something went wrong during remove of the repodata snippet for the package id', package_id
-                print '###############'
+            # remove the stored metadata snippet for this package
+            ret = delete_repodata_snippet_by_id(package_id)
+            if ret == 1:
+                print 'repodata snipped removed for the package id', package_id
+                something_changed = True
+            else:
+                print 'something went wrong during remove of the repodata snippet for the package id', package_id
+            print '###############'
 
     # finally trigger regeneration of all metadata for all channels
     if something_changed:
