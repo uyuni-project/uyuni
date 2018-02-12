fc2deb89cda5f98dc927bd1157c2f390af514a0e
dleidi <dleidi@suse.com>
2018-02-12 17:10:15 +0100

    Add frontend and backend endpoint for the new products page

---
+++ java/code/src/com/suse/manager/webui/controllers/ProductsController.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2018 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.suse.manager.webui.controllers;
+
+import com.redhat.rhn.domain.user.User;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Date;
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+/**
+ * Controller class providing backend code for the Products
+ */
+public class ProductsController {
+
+    private static final Gson GSON = new GsonBuilder()
+            .registerTypeAdapter(Date.class, new ECMAScriptDateAdapter())
+            .serializeNulls()
+            .create();
+
+    private ProductsController() { }
+
+    /**
+     * Displays the Products page
+     *
+     * @param request the request
+     * @param response the response
+     * @param user the user
+     * @return the ModelAndView object to render the page
+     */
+    public static ModelAndView show(Request request, Response response, User user) {
+        return new ModelAndView(new HashMap<>(), "products/show.jade");
+    }
+}
