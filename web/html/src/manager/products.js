197191623e63bce22527565eb42bc240437485ea
dleidi <dleidi@suse.com>
2018-02-12 17:10:15 +0100

    Add frontend and backend endpoint for the new products page

---
+++ web/html/src/manager/products.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+const Products = React.createClass({
+  render: function() {
+    
+  }
+});
+
+ReactDOM.render(
+  <Products />,
+  document.getElementById("products")
+);
