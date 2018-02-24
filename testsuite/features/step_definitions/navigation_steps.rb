9de9c78b38413a38be53816364fcae847b924ce3
Eric Bischoff <ebischoff@suse.com>
2018-02-24 18:41:21 +0100

    TESTSUITE: 5 minutes are not always enough

--- testsuite/features/step_definitions/navigation_steps.rb
+++ testsuite/features/step_definitions/navigation_steps.rb
@@ -514,7 +514,7 @@ end
 When(/^I wait until the table contains a "([^"]*)" text in its first row, refreshing the page$/) do |text|
   # this step is used for long operations like refreshing caches, repositories, etc.
   # therefore we use a non-standard timeout
-  refresh_timeout = 300
+  refresh_timeout = 360
   begin
     Timeout.timeout(refresh_timeout) do
       loop do
