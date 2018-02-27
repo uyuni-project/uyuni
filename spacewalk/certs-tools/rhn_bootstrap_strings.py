uyuni-project/uyuni

commit 2bb311a6c4fc7ac5a548544d98fd5d079943dab4

Allow passing multiple GPG keys to rhn-bootstrap

spacewalk/certs-tools/rhn_bootstrap_strings.py
@@ -173,11 +173,9 @@ def getHeader(productName, activation_keys, org_gpg_key,
               overrides, hostname, orgCACert, isRpmYN,
               using_ssl, using_gpg,
               allow_config_actions, allow_remote_commands, up2dateYN, pubname):
-    #2/14/06 wregglej 181407 If the org_gpg_key option has the path to the file
-    #in it, remove it. It will cause the $FETCH to fail.
-    path_list = os.path.split(org_gpg_key)
-    if path_list[0] and path_list[0] != '':
-        org_gpg_key = path_list[1]
+    # 11/22/16 options.gpg_key is now a comma-separated list of path.
+    # Removing paths from options.gpg_key
+    org_gpg_key = ",".join([os.path.basename(gpg_key) for gpg_key in options.gpg_key.split(",")])
 
     if not activation_keys:
         exit_call = "exit 1"

spacewalk/certs-tools/rhn_bootstrap.py
@@ -212,7 +212,7 @@ def getOptionsTable():
         Option('--gpg-key',
                action='store',
                type='string', default=defopts['gpg-key'],
-               help='path to corporate public GPG key, if used. It will be copied to the location specified by the --pub-tree option. (currently: %s)' % repr(defopts['gpg-key'])),
+               help='path to corporate public GPG key, if used. It will be copied to the location specified by the --pub-tree option. Format is GPG_KEY1,GPG_KEY2 (currently: %s)' % repr(defopts['gpg-key'])),
         Option('--http-proxy',
                action='store',
                type='string', default=defopts['http-proxy'],
@@ -358,9 +358,11 @@ ERROR: the value of --overrides and --script cannot be the same!
         sys.stderr.write("ERROR: CA SSL certificate file or RPM not found\n")
         sys.exit(errnoCANotFound)
 
-    if not options.no_gpg and options.gpg_key and not os.path.exists(options.gpg_key):
-        sys.stderr.write("ERROR: corporate public GPG key file not found\n")
-        sys.exit(errnoGPGNotFound)
+    if not options.no_gpg and options.gpg_key:
+        for gpg_key in options.gpg_key.split(","):
+            if not os.path.exists(gpg_key):
+                sys.stderr.write("ERROR: corporate public GPG key file '{0}' not found\n".format(gpg_key))
+                sys.exit(errnoGPGNotFound)
 
     if options.http_proxy != "":
         options.http_proxy = parseHttpProxyString(options.http_proxy)
@@ -419,18 +421,19 @@ def copyFiles(options):
             if writeYN:
                 copyFile(options.ssl_cert, dest)
 
-    # corp GPG key
+    # corp GPG keys
     if not options.no_gpg and options.gpg_key:
-        writeYN = 1
-        dest = os.path.join(pubDir, os.path.basename(options.gpg_key))
-        if os.path.dirname(options.gpg_key) != pubDir:
-            if os.path.isfile(dest) \
-              and getFileChecksum('md5', options.gpg_key) != getFileChecksum('md5', dest):
-                rotateFile(dest, options.verbose)
-            elif os.path.isfile(dest):
-                writeYN = 0
-            if writeYN:
-                copyFile(options.gpg_key, dest)
+        for gpg_key in options.gpg_key.split(","):
+            writeYN = 1
+            dest = os.path.join(pubDir, os.path.basename(gpg_key))
+            if os.path.dirname(gpg_key) != pubDir:
+                if os.path.isfile(dest) \
+                  and getFileChecksum('md5', gpg_key) != getFileChecksum('md5', dest):
+                    rotateFile(dest, options.verbose)
+                elif os.path.isfile(dest):
+                    writeYN = 0
+                if writeYN:
+                    copyFile(gpg_key, dest)
 
 
 def writeClientConfigOverrides(options):

commit 867bb5c20e7ae0f351845eb189214f9e7221ddba

options are not defined
bug introduced in 2bb311a6c4fc7ac5a548544d98fd5d079943dab4

ERROR: unhandled exception occurred:
Traceback (most recent call last):
  File "/usr/bin/rhn-bootstrap", line 46, in <module>
    sys.exit(mod.main() or 0)
  File "/usr/share/rhn/certs/rhn_bootstrap.py", line 637, in main
    generateBootstrapScript(options)
  File "/usr/share/rhn/certs/rhn_bootstrap.py", line 577, in generateBootstrapScript
    1 - options.no_up2date, pubname)
  File "/usr/share/rhn/certs/rhn_bootstrap_strings.py", line 178, in getHeader
    org_gpg_key = ",".join([os.path.basename(gpg_key) for gpg_key in options.gpg_key.split(",")])
NameError: global name 'options' is not defined

spacewalk/certs-tools/rhn_bootstrap_strings.py
@@ -175,7 +175,7 @@ def getHeader(productName, activation_keys, org_gpg_key,
               allow_config_actions, allow_remote_commands, up2dateYN, pubname):
     # 11/22/16 options.gpg_key is now a comma-separated list of path.
     # Removing paths from options.gpg_key
-    org_gpg_key = ",".join([os.path.basename(gpg_key) for gpg_key in options.gpg_key.split(",")])
+    org_gpg_key = ",".join([os.path.basename(gpg_key) for gpg_key in org_gpg_key.split(",")])
 
     if not activation_keys:
         exit_call = "exit 1"