24d9d09399d24e9bd5e2e59a58baa6eba0d1c7aa
MalloZup <dmaiocchi@suse.com>
2018-03-16 12:08:15 +0100

    add debug on failed scenarios

--- testsuite/features/support/env.rb
+++ testsuite/features/support/env.rb
@@ -97,5 +97,14 @@ After do |scenario|
   if scenario.failed?
     encoded_img = page.driver.render_base64(:png, :full => true)
     embed("data:image/png;base64,#{encoded_img}", 'image/png')
+    debug_server_on_realtime_failure
   end
+end
+
+def debug_server_on_realtime_failure
+  puts
+  puts '#' * 55 + 'var/log/rhn_web_ui.log' + '#' * 55
+  out, _code = $server.run('tail -n35 /var/log/rhn/rhn_web_ui.log')
+  puts out
+  puts '#' * 131
 end
