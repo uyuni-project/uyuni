e391c2e4e6b41f60678338fd51fef86805eabb73
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2018-04-04 13:57:43 +0100

    Throw a customized XMLRPC exception instead of RuntimeException

---
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/NoActionInScheduleException.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2018 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.redhat.rhn.frontend.xmlrpc;
+
+import com.redhat.rhn.FaultException;
+
+/**
+ * No action in schedule result
+ * @version $Rev$
+ */
+public class NoActionInScheduleException extends FaultException  {
+
+    /**
+     * Constructor
+     */
+    public NoActionInScheduleException() {
+        super(-255 , "noActionInSchedule" , "No action in schedule result");
+        // begin member variable initialization
+    }
+
+    /**
+     * Constructor
+     * @param cause the cause (which is saved for later retrieval
+     * by the Throwable.getCause() method). (A null value is
+     * permitted, and indicates that the cause is nonexistent or
+     * unknown.)
+     */
+    public NoActionInScheduleException(Throwable cause) {
+        super(-255 , "noActionInSchedule" , "No action in schedule result", cause);
+        // begin member variable initialization
+    }
+
+    /**
+     * Constructor with message.
+     * @param message exception message.
+     */
+    public NoActionInScheduleException(String message) {
+        super(-255 , "noActionInSchedule" , message);
+    }
+
+}
