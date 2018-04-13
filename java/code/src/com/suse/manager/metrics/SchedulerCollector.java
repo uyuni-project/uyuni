4b067d4767f53d26db7a202ea221692e7f72257b
Silvio Moioli <smoioli@suse.de>
2018-04-13 16:05:22 +0200

    Javadoc and Checkstyle fixes

--- java/code/src/com/suse/manager/metrics/SchedulerCollector.java
+++ java/code/src/com/suse/manager/metrics/SchedulerCollector.java
@@ -1,46 +1,70 @@
+/**
+ * Copyright (c) 2018 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package com.suse.manager.metrics;
 
+import io.prometheus.client.Collector;
 import org.apache.log4j.Logger;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
+
 import java.util.ArrayList;
 import java.util.List;
-import io.prometheus.client.Collector;
 
-public class SchedulerCollector extends Collector{
+import static com.suse.manager.metrics.CustomCollectorUtils.gaugeFor;
 
- // Logger
+/**
+ * Collector for a Taskomatic Scheduler.
+ */
+public class SchedulerCollector extends Collector {
+
     private static final Logger LOG = Logger.getLogger(PrometheusExporter.class);
 
     private Scheduler scheduler;
     private String schedulerId;
 
-    public SchedulerCollector(Scheduler scheduler, String schedulerId) {
-        this.scheduler = scheduler;
-        this.schedulerId = schedulerId;
+    /**
+     * Standard constructor.
+     * @param schedulerIn a scheduler
+     * @param schedulerIdIn a unique ID for the scheduler
+     */
+    public SchedulerCollector(Scheduler schedulerIn, String schedulerIdIn) {
+        this.scheduler = schedulerIn;
+        this.schedulerId = schedulerIdIn;
     }
 
     @Override
     public List<MetricFamilySamples> collect() {
         List<MetricFamilySamples> out = new ArrayList<>();
 
-        //TODO: add metrics here
         try {
-            out.add(CustomCollectorUtils.generateGaugeFor("thread_pool_size",
-                    "Thread pool size", this.scheduler.getMetaData().getThreadPoolSize(),
+            out.add(gaugeFor("thread_pool_size",
+                    "Thread pool size",
+                    this.scheduler.getMetaData().getThreadPoolSize(),
                     this.schedulerId));
-            out.add(CustomCollectorUtils.generateGaugeFor("executed_jobs_total_count",
+            out.add(gaugeFor("executed_jobs_total_count",
                     "Executed jobs total count",
                     this.scheduler.getMetaData().getNumberOfJobsExecuted(),
                     this.schedulerId));
-            out.add(CustomCollectorUtils.generateGaugeFor("currently_executing_jobs_count",
+            out.add(gaugeFor("currently_executing_jobs_count",
                     "Currently executing jobs count",
-                    this.scheduler.getCurrentlyExecutingJobs().size(), this.schedulerId));
+                    this.scheduler.getCurrentlyExecutingJobs().size(),
+                    this.schedulerId));
         }
         catch (SchedulerException e) {
             LOG.warn("Unable to collect scheduler info ", e);
         }
         return out;
     }
-
 }
--- java/code/src/com/suse/manager/metrics/ThreadPoolCollector.java
+++ java/code/src/com/suse/manager/metrics/ThreadPoolCollector.java
@@ -1,31 +1,52 @@
+/**
+ * Copyright (c) 2018 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package com.suse.manager.metrics;
+
+import io.prometheus.client.Collector;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadPoolExecutor;
 
-import io.prometheus.client.Collector;
-
-public class ThreadPoolCollector extends Collector{
+/**
+ * Collector for a ThreadPool.
+ */
+public class ThreadPoolCollector extends Collector {
 
     private ThreadPoolExecutor pool;
     private String poolId;
 
-    public ThreadPoolCollector(ThreadPoolExecutor pool, String poolId) {
-        this.pool = pool;
-        this.poolId = poolId;
+    /**
+     * Standard constructor.
+     * @param poolIn a thread pool
+     * @param poolIdIn a unique ID for the pool
+     */
+    public ThreadPoolCollector(ThreadPoolExecutor poolIn, String poolIdIn) {
+        this.pool = poolIn;
+        this.poolId = poolIdIn;
     }
 
     @Override
     public List<MetricFamilySamples> collect() {
         List<MetricFamilySamples> out = new ArrayList<>();
 
-        out.add(CustomCollectorUtils.generateGaugeFor("thread_pool_threads",
+        out.add(CustomCollectorUtils.gaugeFor("thread_pool_threads",
                 "Threads total count", this.pool.getPoolSize(), this.poolId));
-        out.add(CustomCollectorUtils.generateGaugeFor("thread_pool_threads_active",
+        out.add(CustomCollectorUtils.gaugeFor("thread_pool_threads_active",
                 "Active threads count", this.pool.getActiveCount(), this.poolId));
 
         return out;
     }
-
 }
