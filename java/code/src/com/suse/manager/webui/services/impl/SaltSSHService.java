672adbedece5b13e9d0fcec155039f7bd61afdeb
Michael Calmer <mc@suse.de>
2018-04-14 15:38:40 +0200

    Do not use wipe for salt-ssh call (bsc#1088861)
    Sice we have better code to detect code changes and the need for a
    re-deployment this should not be needed anymore.

--- java/code/src/com/suse/manager/webui/services/impl/SaltSSHService.java
+++ java/code/src/com/suse/manager/webui/services/impl/SaltSSHService.java
@@ -558,7 +558,6 @@ public class SaltSSHService {
                     .rosterFile(rosterPath.getFileName().toString())
                     .priv(SSH_KEY_PATH)
                     .sudo(sudo)
-                    .wipe(true)
                     .refreshCache(true)
                     .build();
 
