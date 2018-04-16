7a6140e8b6665c3fd62b52a1e988d82329fcf6e0
Julio Gonzalez Gil <jgonzalez@suse.com>
2018-04-17 00:52:19 +0200

    Updated products and repositories JSON files

--- java/code/src/com/redhat/rhn/manager/content/test/refresh_scc_data.py
+++ java/code/src/com/redhat/rhn/manager/content/test/refresh_scc_data.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 
+import argparse
 import base64
 import httplib
 import json
 import re
 import sys
+
+def parse_arguments():
+  description = 'Generate JSON files with data from SCC'
+  parser = argparse.ArgumentParser(description=description)
+  parser.add_argument("-u", "--username", dest="username", help="SCC username",
+                      required=True)
+  parser.add_argument("-p", "--password", dest="password", help="SCC password",
+                      required=True)
+  parser.add_argument("-r", "--real-tokens", dest="real_tokens",
+                      action='store_true',
+                      help="When present, store URLs with real tokens at " +
+                           "repositories.json (WARNING: This could leak " +
+                           "private data!")
+  results = parser.parse_args()
+  return(results)
 
 def find_next_path(resp):
   link = resp.getheader("Link")
@@ -34,8 +50,10 @@ def save_json(content, path):
   with open(path, 'w') as out_file:
     json.dump(content, out_file, sort_keys=True, indent=2, separators=(',', ': '))
 
+args = parse_arguments()
+
 connection = httplib.HTTPSConnection("scc.suse.com")
-token = base64.b64encode(b"UCUSER:UCPASSWORD").decode("ascii")
+token = base64.b64encode(b"%s:%s" %(args.username, args.password)).decode("ascii")
 headers = { 'Authorization' : 'Basic %s' %  token }
 
 products = get_paginated(connection, headers, "/connect/organizations/products/unscoped")
@@ -43,6 +61,15 @@ save_json(products, "products.json")
 print("products.json refreshed")
 
 repositories = get_paginated(connection, headers, "/connect/organizations/repositories")
+if args.real_tokens:
+  print("WARNING: Storing real repository tokens at repositories.json!")
+  print("         This could leak private data!")
+else:
+  repo_url_regex = re.compile(r'^(https:\/\/.+\/)\?(.+)$')
+  for repo_num in range(0,len(repositories)):
+    repositories[repo_num]['url'] = repo_url_regex.sub(r'\1?my-fake-token',
+                                                       repositories[repo_num]['url'])
+
 save_json(repositories, "repositories.json")
 print("repositories.json refreshed")
 
