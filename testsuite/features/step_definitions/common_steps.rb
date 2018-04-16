f6a0e4cfa2ce1d28f2de449b558b3115976dd072
dleidi <dleidi@suse.com>
2018-04-16 11:25:52 +0200

    Adapt the test to the real data

--- testsuite/features/step_definitions/common_steps.rb
+++ testsuite/features/step_definitions/common_steps.rb
@@ -694,7 +694,7 @@ And(/^I click on the "([^"]*)" recommended toggler$/) do |target_status|
 end
 
 And(/^I should see the child channel "([^"]*)" "([^"]*)"$/) do |target_channel, target_status|
-  step %(I should see a "#{target_channel}" text)
+  step %(I should see a text like "#{target_channel}")
 
   xpath = "//label[contains(text(), '#{target_channel}')]"
   channel_checkbox_id = find(:xpath, xpath)['for']
