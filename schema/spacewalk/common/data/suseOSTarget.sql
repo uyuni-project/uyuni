373ae746258a48efc78b23732263e561ccd386b0
Michael Calmer <mc@suse.de>
2018-04-21 15:29:00 +0200

    OSTarget for aarch64 and add schema migrations

--- schema/spacewalk/common/data/suseOSTarget.sql
+++ schema/spacewalk/common/data/suseOSTarget.sql
@@ -80,6 +80,12 @@ insert into suseOSTarget (id, os, target, channel_arch_id) values
 insert into suseOSTarget (id, os, target, channel_arch_id) values
 (sequence_nextval('suse_ostarget_id_seq'), 'sle-15-ppc64le', 'sle-15-ppc64le', LOOKUP_CHANNEL_ARCH('channel-ppc64le'));
 
+insert into suseOSTarget (id, os, target, channel_arch_id) values
+(sequence_nextval('suse_ostarget_id_seq'), 'sle-15-aarch64', 'sle-15-aarch64', LOOKUP_CHANNEL_ARCH('channel-aarch64'));
+
+insert into suseOSTarget (id, os, target, channel_arch_id) values
+(sequence_nextval('suse_ostarget_id_seq'), 'sle-12-aarch64', 'sle-12-aarch64', LOOKUP_CHANNEL_ARCH('channel-aarch64'));
+
 commit;
 
 --
