82f00381483dd5eda16d0fa4d444348f09d3691f
Ondrej Holecek <oholecek@suse.com>
2018-04-26 15:00:24 +0200

    Allow image store path to be passed as a parameter

--- susemanager-utils/susemanager-sls/modules/runners/kiwi-image-collect.py
+++ susemanager-utils/susemanager-sls/modules/runners/kiwi-image-collect.py
@@ -10,8 +10,6 @@ from mgrutil import move_minion_uploaded_files
 
 log = logging.getLogger(__name__)
 
-os_image_store_dir = '/srv/www/os-image'
-
 def upload_file_from_minion(minion, filetoupload, targetdir):
     grains = list(__salt__['cache.grains'](tgt=minion).values())[0]
     addr4 = None
@@ -33,14 +31,14 @@ def upload_file_from_minion(minion, filetoupload, targetdir):
       rsh='ssh -o IdentityFile=/srv/susemanager/salt/salt_ssh/mgr_ssh_id -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
     )
 
-def kiwi_collect_image(minion, filepath):
-    if not os.path.exists(os_image_store_dir):
-        raise salt.exceptions.SaltRunnerError('Target directory {} does not exists'.format(os_image_store_dir))
+def kiwi_collect_image(minion, filepath, image_store_dir='/srv/www/os-image'):
+    if not os.path.exists(image_store_dir):
+        raise salt.exceptions.SaltRunnerError('Target directory {} does not exists'.format(image_store_dir))
 
     pillars = list(__salt__['cache.pillar'](tgt=minion).values())[0]
     if pillars.get('use_salt_trasport'):
       return move_minion_uploaded_files(minion = minion,
                                         dirtomove=filepath,
-                                        basepath=os_image_store_dir)
+                                        basepath=image_store_dir)
 
-    return upload_file_from_minion(minion, filepath, os_image_store_dir)
+    return upload_file_from_minion(minion, filepath, image_store_dir)
