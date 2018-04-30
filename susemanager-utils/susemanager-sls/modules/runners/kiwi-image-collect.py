ab133533cf438864e41bb16797d398a0113f005a
Ondrej Holecek <oholecek@suse.com>
2018-04-30 14:29:18 +0200

    Do not use mgrutil move function
    - mgrutil.move_minion_uploaded_files is designed to move whole directories
      and with SCAP files in mind with features like directory creating we
      don't want

--- susemanager-utils/susemanager-sls/modules/runners/kiwi-image-collect.py
+++ susemanager-utils/susemanager-sls/modules/runners/kiwi-image-collect.py
@@ -6,7 +6,6 @@
 import os
 import salt.exceptions
 import logging
-from mgrutil import move_minion_uploaded_files
 
 log = logging.getLogger(__name__)
 
@@ -31,14 +30,18 @@ def upload_file_from_minion(minion, filetoupload, targetdir):
       rsh='ssh -o IdentityFile=/srv/susemanager/salt/salt_ssh/mgr_ssh_id -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
     )
 
+def move_file_from_minion_cache(minion, filetomove, targetdir):
+    src = os.path.join(__opts__['cachedir'], 'minions', minion, 'files', filetomove.lstrip('/'))
+    return __salt__['salt.cmd']('file.move', src, targetdir);
+
 def kiwi_collect_image(minion, filepath, image_store_dir):
     if not os.path.exists(image_store_dir):
-        raise salt.exceptions.SaltRunnerError('Target directory {} does not exists'.format(image_store_dir))
+      raise salt.exceptions.SaltRunnerError('Target directory {} does not exists'.format(image_store_dir))
 
     pillars = list(__salt__['cache.pillar'](tgt=minion).values())[0]
     if pillars.get('use_salt_transport'):
-      return move_minion_uploaded_files(minion = minion,
-                                        dirtomove=filepath,
-                                        basepath=image_store_dir)
+      log.info('Collecting image "{}" from minion cache'.format(filepath))
+      return move_file_from_minion_cache(minion, filepath, image_store_dir)
 
+    log.info('Collecting image "{}" from minion using rsync'.format(filepath))
     return upload_file_from_minion(minion, filepath, image_store_dir)
