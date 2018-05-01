94669598481d945e249383086d5305d9c8023c85
Michael Calmer <mc@suse.de>
2018-05-01 16:04:08 +0200

    use renamed scripts

--- susemanager-utils/testing/automation/schema-migration-test-pgsql.sh
+++ susemanager-utils/testing/automation/schema-migration-test-pgsql.sh
@@ -6,4 +6,4 @@ GITROOT=`readlink -f $HERE/../../../`
 
 # we need a special (old) baseimage to migrate to current schema
 docker pull $REGISTRY/$PGSQL_CONTAINER
-docker run --privileged --rm=true -v "$GITROOT:/manager" $REGISTRY/$PGSQL_CONTAINER /manager/susemanager-utils/testing/docker/scripts/schema_migration_test_pgsql-21to31.sh
+docker run --privileged --rm=true -v "$GITROOT:/manager" $REGISTRY/$PGSQL_CONTAINER /manager/susemanager-utils/testing/docker/scripts/schema_migration_test_pgsql-30to32.sh
