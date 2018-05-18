e77c54d4f142faea38e92c414d1e945f8df16976
Bo Maryniuk <bo@suse.de>
2018-05-18 14:33:18 +0200

    Remove RH defaults

--- spacewalk/setup/lib/Spacewalk/Setup.pm
+++ spacewalk/setup/lib/Spacewalk/Setup.pm
@@ -99,16 +99,8 @@ use constant ORACLE_RHNCONF_BACKUP =>
 
 use constant EMBEDDED_DB_ANSWERS =>
   '/usr/share/spacewalk/setup/defaults.d/embedded-postgresql.conf';
-our $DEFAULT_DOC_ROOT = "/var/www/html";
-our $CA_TRUST_DIR = '/etc/pki/ca-trust/source/anchors';
-if ( -d '/etc/pki/trust/anchors/' )
-{
-    $CA_TRUST_DIR = '/etc/pki/trust/anchors';
-}
-if ( -d '/srv/www/htdocs' )
-{
-    $DEFAULT_DOC_ROOT = '/srv/www/htdocs';
-}
+our $DEFAULT_DOC_ROOT = "/srv/www/htdocs";
+our $CA_TRUST_DIR = '/etc/pki/trust/anchors/';
 use constant DEFAULT_SUSEMANAGER_CONF =>
   '/usr/share/rhn/config-defaults/rhn_server_susemanager.conf';
 
