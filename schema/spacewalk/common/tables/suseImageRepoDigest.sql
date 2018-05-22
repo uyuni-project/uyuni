317e911574b8b700b42f8c6cb84c081b4e447e20
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2018-05-22 15:27:22 +0100

    Change initial schema for suseImageRepoDigest table

--- schema/spacewalk/common/tables/suseImageRepoDigest.sql
+++ schema/spacewalk/common/tables/suseImageRepoDigest.sql
@@ -30,7 +30,7 @@ ENABLE ROW MOVEMENT
 ;
 
 CREATE UNIQUE INDEX suse_img_repodigest_idx
-    ON suseImageRepoDigest(repo_digest)
+    ON suseImageRepoDigest(repo_digest, image_history_id)
     TABLESPACE [[64k_tbs]]
     NOLOGGING;
 
