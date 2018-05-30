ac23a42afd0cc4a6839a1ab74bc3f4e98db381e1
Michael Calmer <mc@suse.de>
2018-05-30 09:18:28 +0200

    adapt network access code

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/system/SystemHandler.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/system/SystemHandler.java
@@ -5500,6 +5500,7 @@ public class SystemHandler extends BaseHandler {
         // Set network device information to the server
         Set<NetworkInterface> set = new HashSet<NetworkInterface>();
         for (HashMap<String, String> map : netDevices) {
+            // FIXME: why do we need this?
             CobblerNetworkInterface device = cmd.new CobblerNetworkInterface();
             device.setName(map.get("name"));
             device.setIpaddr(map.get("ip"));
