4068af77e4f3a8b68d2dae9f4159e87e2f912860
dleidi <dleidi@suse.com>
2018-06-06 13:58:13 +0200

    Remove useless setter method

--- java/code/src/com/redhat/rhn/manager/visualization/json/System.java
+++ java/code/src/com/redhat/rhn/manager/visualization/json/System.java
@@ -142,17 +142,6 @@ public class System {
     }
 
     /**
-     * Sets the type.
-     *
-     * @param typeIn - the type
-     * @return this
-     */
-    public System setType(String typeIn) {
-        type = typeIn;
-        return this;
-    }
-
-    /**
      * Gets the id.
      *
      * @return id
