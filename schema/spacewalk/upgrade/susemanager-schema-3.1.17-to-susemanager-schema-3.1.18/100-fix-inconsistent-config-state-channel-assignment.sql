4eff75b40b854e4eea2b5fd170d37827931f92e6
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2018-06-11 12:35:14 +0100

    Delete inconsistencies on 'rhnServerConfigChannel' after migration

--- schema/spacewalk/upgrade/susemanager-schema-3.1.17-to-susemanager-schema-3.1.18/100-fix-inconsistent-config-state-channel-assignment.sql
+++ schema/spacewalk/upgrade/susemanager-schema-3.1.17-to-susemanager-schema-3.1.18/100-fix-inconsistent-config-state-channel-assignment.sql
@@ -8,9 +8,15 @@ NOT EXISTS (SELECT 1
             AND sr.creator_id = wc.id
             AND wc.org_id = cc.org_id);
 
+DELETE from rhnServerConfigChannel sscc WHERE
+NOT EXISTS (SELECT 1
+            FROM rhnConfigChannel cc, rhnserver s
+            WHERE cc.id = sscc.config_channel_id
+            AND sscc.server_id = s.id
+            AND s.org_id = cc.org_id);
 
 -- Recalculate positions for entries after removing some of them on the previous step.
--- This SQL query will keep the actual position ordering but ensuring there is no missing position.
+-- These SQL queries will keep the actual position ordering but ensuring there is no missing position.
 UPDATE suseStateRevisionConfigChannel
 SET position = (
     SELECT rank
@@ -20,3 +26,14 @@ SET position = (
     ) ranking
     WHERE config_channel_id = suseStateRevisionConfigChannel.config_channel_id
     AND state_revision_id = suseStateRevisionConfigChannel.state_revision_id);
+
+
+UPDATE rhnServerConfigChannel
+SET position = (
+    select rank
+    from (
+        SELECT server_id, config_channel_id, position, (row_number() OVER (PARTITION BY server_id ORDER BY position ASC)) rank
+        FROM rhnServerConfigChannel sscc
+    ) ranking
+    WHERE config_channel_id = rhnServerConfigChannel.config_channel_id
+    AND server_id = rhnServerConfigChannel.server_id);
