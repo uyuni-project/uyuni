174dd43645294c5c815617d2b0c563aaf5d8f0e8
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2018-06-11 12:34:17 +0100

    Remove tabs and clean up on the migration file

--- schema/spacewalk/upgrade/susemanager-schema-3.1.17-to-susemanager-schema-3.1.18/100-fix-inconsistent-config-state-channel-assignment.sql
+++ schema/spacewalk/upgrade/susemanager-schema-3.1.17-to-susemanager-schema-3.1.18/100-fix-inconsistent-config-state-channel-assignment.sql
@@ -1,22 +1,22 @@
 -- Remove inconsistent config channel state revisions where config channel org is
 -- not equal to staterevision org.
-DELETE FROM susestaterevisionconfigchannel scc WHERE
+DELETE FROM suseStateRevisionConfigChannel scc WHERE
 NOT EXISTS (SELECT 1
-			FROM rhnconfigchannel cc, susestaterevision sr, web_contact wc
-			WHERE cc.id = scc.config_channel_id
-			AND scc.state_revision_id = sr.id
-			AND sr.creator_id = wc.id
-			AND wc.org_id = cc.org_id);
+            FROM rhnConfigChannel cc, suseStateRevision sr, web_contact wc
+            WHERE cc.id = scc.config_channel_id
+            AND scc.state_revision_id = sr.id
+            AND sr.creator_id = wc.id
+            AND wc.org_id = cc.org_id);
 
 
 -- Recalculate positions for entries after removing some of them on the previous step.
 -- This SQL query will keep the actual position ordering but ensuring there is no missing position.
 UPDATE suseStateRevisionConfigChannel
 SET position = (
-	SELECT rank
-	FROM (
-		SELECT state_revision_id, config_channel_id, position, (row_number() OVER (PARTITION BY state_revision_id ORDER BY position ASC)) rank
-		FROM susestaterevisionconfigchannel scc
-	) ranking
-	WHERE config_channel_id = suseStateRevisionConfigChannel.config_channel_id
-	AND state_revision_id = suseStateRevisionConfigChannel.state_revision_id);
+    SELECT rank
+    FROM (
+        SELECT state_revision_id, config_channel_id, position, (row_number() OVER (PARTITION BY state_revision_id ORDER BY position ASC)) rank
+        FROM suseStateRevisionConfigChannel scc
+    ) ranking
+    WHERE config_channel_id = suseStateRevisionConfigChannel.config_channel_id
+    AND state_revision_id = suseStateRevisionConfigChannel.state_revision_id);
