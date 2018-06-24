92e2f560a9139914ddf4f78ce8e820d11d7661c2
Luis Neves <luneves@suse.de>
2018-06-24 23:22:37 +0100

    correct wrong indentation on file action-schedule.js

--- web/html/src/components/action-schedule.js
+++ web/html/src/components/action-schedule.js
@@ -47,18 +47,18 @@ class ActionSchedule extends React.Component<ActionScheduleProps, ActionSchedule
   }
 
   onDateTimeChanged = (date: Date) => {
-      this.setState({
-        type: "earliest",
-        earliest: date
-      });
-      this.props.onDateTimeChanged(date);
+    this.setState({
+      type: "earliest",
+      earliest: date
+    });
+    this.props.onDateTimeChanged(date);
   }
 
   onActionChainChanged = (idOrNewLabel: ?string) => {
     let selectedActionChain: ?ActionChain;
     let actionChains = this.state.actionChains;
     if (idOrNewLabel) {
-        selectedActionChain = this.props.actionChains.find((ac) => ac.id.toString() == idOrNewLabel);
+      selectedActionChain = this.props.actionChains.find((ac) => ac.id.toString() == idOrNewLabel);
     }
     if (selectedActionChain) {
       this.props.onActionChainChanged(selectedActionChain);
@@ -86,27 +86,27 @@ class ActionSchedule extends React.Component<ActionScheduleProps, ActionSchedule
     return (
       <div className="spacewalk-scheduler">
         <div className="form-horizontal">
-            <div className="form-group">
-                <div className="col-sm-3 control-label">
-                    <input type="radio" name="use_date" value="true" checked={this.state.type == "earliest"} id="schedule-by-date" onChange={this.onSelectEarliest}/>
-                    <label htmlFor="schedule-by-date">{t("Earliest:")}</label>
-                </div>
-                <div className="col-sm-6">
-                    <DateTimePicker onChange={this.onDateTimeChanged} value={this.state.earliest} timezone={this.props.timezone} />
-                </div>
+          <div className="form-group">
+            <div className="col-sm-3 control-label">
+              <input type="radio" name="use_date" value="true" checked={this.state.type == "earliest"} id="schedule-by-date" onChange={this.onSelectEarliest}/>
+              <label htmlFor="schedule-by-date">{t("Earliest:")}</label>
             </div>
-            <div className="form-group">
-                <div className="col-sm-3 control-label">
-                    <input type="radio" name="action_chain" value="false" checked={this.state.type == "actionChain"} id="schedule-by-action-chain" onChange={this.onSelectActionChain}/>
-                    <label htmlFor="schedule-by-action-chain">{t("Add to:")}</label>
-                </div>
-                <div className="col-sm-6">
-                    <Combobox id="action-chain" name="action_chain" value={this.state.actionChain ? this.state.actionChain.id : 0}
-                      onFocus={this.onSelectActionChain} data={this.state.actionChains} onSelect={this.onActionChainChanged} />
-                </div>
+            <div className="col-sm-6">
+              <DateTimePicker onChange={this.onDateTimeChanged} value={this.state.earliest} timezone={this.props.timezone} />
             </div>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-3 control-label">
+              <input type="radio" name="action_chain" value="false" checked={this.state.type == "actionChain"} id="schedule-by-action-chain" onChange={this.onSelectActionChain}/>
+              <label htmlFor="schedule-by-action-chain">{t("Add to:")}</label>
+            </div>
+            <div className="col-sm-6">
+              <Combobox id="action-chain" name="action_chain" value={this.state.actionChain ? this.state.actionChain.id : 0}
+                        onFocus={this.onSelectActionChain} data={this.state.actionChains} onSelect={this.onActionChainChanged} />
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
     );
   }
 
