6c72a6825e17793bddfaa80d4784a4262c716336
Luis Neves <luneves@suse.de>
2018-06-26 01:43:56 +0100

    React Select implemented

--- web/html/src/components/combobox.js
+++ web/html/src/components/combobox.js
@@ -3,6 +3,7 @@
 
 const React = require("react");
 const ReactDOM = require("react-dom");
+import Select from 'react-select';
 
 declare function $(param: any): any;
 
@@ -25,70 +26,36 @@ type ComboboxState = {
 };
 
 class Combobox extends React.Component<ComboboxProps, ComboboxState> {
+  render() {
+    const options = this.props.data.map(item => ({value: item.id, label: item.text}));
 
-    combobox: any;
+    const colourStyles = {
+      option: (styles, { data, isDisabled, isFocused, isSelected }) => {
 
-    constructor(props: ComboboxProps) {
-        super(props);
-        this.combobox = null;
-        this.state = {
-          focused: false
-        };
-    }
-
-    componentDidMount() {
-      this.combobox = $(ReactDOM.findDOMNode(this));
-
-      // init widget
-      this.combobox.select2({
-        width: "20em",
-        data: this.props.data,
-        createSearchChoice: function(term: string, data: Array<ComboboxItem>) {
-          // returns a new search choice if term is new
-          let matchingChoices = data.filter(item => item.text.localeCompare(term) == 0);
-
-          if (matchingChoices.length == 0) {
-            var sanitizedTerm = term.replace(/[',]/g, "");
-            return {id: sanitizedTerm, text: sanitizedTerm};
-          }
-        },
-        maximumInputLength: 256
-      });
-
-      // init initial selection
-      this.combobox.select2("val", this.props.value);
-
-      // select radio button when combobox has focus
-      this.combobox.on("select2-focus", (event) => {
-        if(!this.state.focused) {
-          this.setState({focused: true});
-          this.props.onFocus && this.props.onFocus();
+        if (isFocused) {
+          return Object.assign(styles, {backgroundColor: "#052940", color: "#ffffff"});
         }
-      });
 
-      this.combobox.on("select2-blur", (event) => {
-        this.setState({focused: false});
-      });
+        if (isSelected) {
+          return Object.assign(styles, {backgroundColor: "#063559", color: "#ffffff"});
+        }
 
-      this.combobox.on("select2-selecting", (event) => {
-        this.props.onSelect(event.val);
-      })
+        return styles;
+      },
+    };
 
-      this.combobox.on("change", (event) => {
-        this.props.onSelect(event.val);
-      });
-    }
-
-    render() {
-      return (
-        <input type="hidden"
-            id={this.props.id}
-            name={this.props.name}
-        />);
-    }
-
+    return <Select
+      id={this.props.id}
+      name={this.props.name}
+      onFocus={this.props.onFocus}
+      onChange={(selectedOption) => this.props.onSelect(selectedOption.value)}
+      value={options.find(option => option.value === this.props.value)}
+      options={options}
+      styles={colourStyles}
+    />;
+  }
 }
 
 module.exports = {
-    Combobox : Combobox
+  Combobox : Combobox
 }
