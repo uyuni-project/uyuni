d3116af407e195a9286dd7831a8a186c93e6a099
Frantisek Kobzik <fkobzik@suse.de>
2018-07-26 11:46:22 +0200

    Test creating configuration channels via struts action

--- java/code/src/com/redhat/rhn/frontend/action/configuration/test/ChannelOverviewActionTest.java
+++ java/code/src/com/redhat/rhn/frontend/action/configuration/test/ChannelOverviewActionTest.java
@@ -15,14 +15,21 @@
 package com.redhat.rhn.frontend.action.configuration.test;
 
 import com.redhat.rhn.domain.config.ConfigChannel;
+import com.redhat.rhn.domain.config.ConfigChannelType;
 import com.redhat.rhn.domain.config.ConfigFile;
+import com.redhat.rhn.domain.config.ConfigurationFactory;
 import com.redhat.rhn.domain.role.RoleFactory;
+import com.redhat.rhn.frontend.struts.RhnAction;
 import com.redhat.rhn.manager.configuration.ChannelSummary;
 import com.redhat.rhn.testing.ConfigTestUtils;
 import com.redhat.rhn.testing.RhnMockStrutsTestCase;
 import com.redhat.rhn.testing.UserTestUtils;
+import servletunit.HttpServletRequestSimulator;
 
 public class ChannelOverviewActionTest extends RhnMockStrutsTestCase {
+
+    public static final String FWD_SUCCESS = "/configuration/ChannelOverview.do?ccid=";
+    public static final String FWD_ERROR = "/WEB-INF/pages/configuration/channel/channelcreate.jsp";
 
     public void testExecuteNoFiles() throws Exception {
         UserTestUtils.addUserRole(user, RoleFactory.CONFIG_ADMIN);
@@ -43,4 +50,44 @@ public class ChannelOverviewActionTest extends RhnMockStrutsTestCase {
         assertTrue(request.getAttribute("summary") instanceof ChannelSummary);
     }
 
+    public void testCreateChannelWithValidLabel() {
+        ConfigChannel channel = doCreateChannelAction("channel1");
+        assertNotNull(channel);
+
+        String forwardPath = getActualForward();
+        assertTrue(forwardPath.startsWith(FWD_SUCCESS));
+    }
+
+    public void testCreateChannelWithDotsInLabel() {
+        ConfigChannel channel = doCreateChannelAction("channel.with.dots");
+        assertNull(channel);
+
+        String forwardPath = getActualForward();
+        assertTrue(forwardPath.startsWith(FWD_ERROR));
+    }
+
+    public void testCreateChannelWithSpacesInLabel() {
+        ConfigChannel channel = doCreateChannelAction("invalid_channel_123 with spaces");
+        assertNull(channel);
+
+        String forwardPath = getActualForward();
+        assertTrue(forwardPath.startsWith(FWD_ERROR));
+    }
+
+    private ConfigChannel doCreateChannelAction(String channelLabel) {
+        // create a config channel via action and return it
+        request.setMethod(HttpServletRequestSimulator.POST);
+        setRequestPathInfo("/configuration/ChannelCreate");
+        addRequestParameter(RhnAction.SUBMITTED, Boolean.TRUE.toString());
+        addRequestParameter("created", Boolean.TRUE.toString());
+        addRequestParameter("type", ConfigChannelType.normal().getLabel());
+        addRequestParameter("cofLabel", channelLabel);
+        addRequestParameter("cofName", "Conf channel name");
+        addRequestParameter("cofDescription", "This is an awesome channel!");
+
+        actionPerform();
+
+        return ConfigurationFactory.lookupConfigChannelByLabel(channelLabel, user.getOrg(),
+                ConfigChannelType.normal());
+    }
 }
