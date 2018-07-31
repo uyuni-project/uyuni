de24ca1ae18556dbc456cdf074677db5cdf6ff45
Mihai Dinca <mdinca@suse.de>
2018-07-31 13:11:43 +0200

    Allow salt systems to be registered as proxies

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/proxy/ProxyHandler.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/proxy/ProxyHandler.java
@@ -23,7 +23,7 @@ import com.redhat.rhn.frontend.xmlrpc.InvalidProxyVersionException;
 import com.redhat.rhn.frontend.xmlrpc.MethodInvalidParamException;
 import com.redhat.rhn.frontend.xmlrpc.NotSupportedException;
 import com.redhat.rhn.frontend.xmlrpc.ProxyAlreadyRegisteredException;
-import com.redhat.rhn.frontend.xmlrpc.ProxyNeedManagementException;
+import com.redhat.rhn.frontend.xmlrpc.ProxyMissingEntitlementException;
 import com.redhat.rhn.frontend.xmlrpc.ProxyNotActivatedException;
 import com.redhat.rhn.frontend.xmlrpc.ProxySystemIsSatelliteException;
 import com.redhat.rhn.manager.entitlement.EntitlementManager;
@@ -111,7 +111,7 @@ public class ProxyHandler extends BaseHandler {
      * @throws ProxySystemIsSatelliteException thrown if client certificate is
      * for a satellite
      * @throws InvalidProxyVersionException thrown if version is not supported.
-     * @throws ProxyNeedManagementException thrown if system does not have the
+     * @throws ProxyMissingEntitlementException thrown if system does not have the
      * management entitlement.
      *
      * @xmlrpc.doc Activates the proxy identified by the given client
@@ -130,8 +130,8 @@ public class ProxyHandler extends BaseHandler {
             throw new ProxyAlreadyRegisteredException();
         }
 
-        if (!(server.hasEntitlement(EntitlementManager.MANAGEMENT))) {
-            throw new ProxyNeedManagementException();
+        if (!(server.hasEntitlement(EntitlementManager.MANAGEMENT) || server.hasEntitlement(EntitlementManager.SALT))) {
+            throw new ProxyMissingEntitlementException();
         }
 
         SystemManager.activateProxy(server, version);
