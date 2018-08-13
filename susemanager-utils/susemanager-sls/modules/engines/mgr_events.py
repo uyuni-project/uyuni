d44710788a29b94f718b8e94e5f931d7c2bf8b6f
Jochen Breuer <jbreuer@suse.de>
2018-08-13 10:53:04 +0200

    mgr_events: add dynamic calculation of commit interval
    Committing is now triggerd in the IOLoop. Every event addition to the queue
    triggers a re-calculation of the commit interval. If events are comming in
    fast, the commit inverval increases (gets slower). If events are comming in
    slowly, the commit inverval decreases (gets faster). If nothing happens for
    a long time, the commit interval also decreases gradually until a minimum is
    reached.
    
    Co-authored-by: Mihai Dinca <Mihai.Dinca@suse.com>

--- susemanager-utils/susemanager-sls/modules/engines/mgr_events.py
+++ susemanager-utils/susemanager-sls/modules/engines/mgr_events.py
@@ -17,31 +17,54 @@ import tornado.ioloop
 
 log = logging.getLogger(__name__)
 
+FREQUENCY_MIN = 0.2
+FREQUENCY_MAX = 10
+ATTENUATION_FACTOR = 0.9
+
+
 class Responder:
     def __init__(self, event_bus):
+        self.queue = []
+        self.time_of_last_event = time.time()
+        self.frequency_average = FREQUENCY_MIN
+        self.commit_interval = 1 / self.frequency_average
         self.event_bus = event_bus
-        self.last_commit_time = time.time()
-        self.commit_scheduled = False
+        self.event_bus.io_loop.call_later(self.commit_interval, self.commit)
+
+    def debug_log(self):
+        log.debug("####################################################################################")
+        log.debug("******** time_of_last_event: {}".format(self.time_of_last_event))
+        log.debug("******** fequency_average: {}".format(self.frequency_average))
+        log.debug("******** fequency_commit_interval: {}".format(self.commit_interval))
 
     @tornado.gen.coroutine
-    def check(self, raw):
+    def add_event_to_queue(self, raw):
+        # TODO: Remove next two lines. Just for testing.
         tag, data = self.event_bus.unpack(raw, self.event_bus.serial)
-        ret = {'data': data, 'tag': tag}
-        log.debug("INSERT INTO suseSaltEvent ({});".format(tag))
-        time_since_commit = time.time() - self.last_commit_time
-        if time_since_commit >= 1:
-            self.commit()
-        else:
-            if not self.commit_scheduled:
-                log.debug("Delaying commit by {} seconds!".format(1-time_since_commit))
-                self.event_bus.io_loop.call_later(1 - time_since_commit, self.commit)
-                self.commit_scheduled = True
+        log.debug("******** Adding event to queue: {}".format(tag))
+        self.queue.append(raw)
+        current_time = time.time()
+        self.frequency_average = ATTENUATION_FACTOR * self.frequency_average + (1 - ATTENUATION_FACTOR) * (current_time - self.time_of_last_event)
+        self.commit_interval = min(0.5 / self.frequency_average, FREQUENCY_MAX)
+        self.time_of_last_event = current_time
+        self.debug_log()
 
     @tornado.gen.coroutine
     def commit(self):
-        log.debug("COMMITTING!")
-        self.last_commit_time = time.time()
-        self.commit_scheduled = False
+        """
+        Committing to the database.
+        """
+        while len(self.queue) > 0:
+            entry = self.queue.pop()
+            tag, data = self.event_bus.unpack(entry, self.event_bus.serial)
+            ret = {'data': data, 'tag': tag}
+            log.debug("******* INSERT INTO suseSaltEvent ({});".format(tag))
+        log.debug("************** COMMIT! ")
+        # Slowly decrease (speed up) the `commit_interval` again. This is
+        # important for cases where new events come in very slowly.
+        self.commit_interval = max(0.9 * self.commit_interval, 0.1)
+        self.event_bus.io_loop.call_later(self.commit_interval, self.commit)
+
 
 def start():
     '''
@@ -55,6 +78,6 @@ def start():
             io_loop=io_loop)
     log.debug('mgr_events engine started')
     responder = Responder(event_bus)
-    event_bus.set_event_handler(responder.check)
+    event_bus.set_event_handler(responder.add_event_to_queue)
 
     io_loop.start()
