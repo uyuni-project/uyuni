daedc489a13d12e3d725d971c7e5ced938df5f82
Michael Calmer <mc@suse.de>
2018-08-17 17:35:38 +0200

    fix calling steps

--- testsuite/features/step_definitions/navigation_steps.rb
+++ testsuite/features/step_definitions/navigation_steps.rb
@@ -750,7 +750,7 @@ When(/^I click on "([^"]*)" in "([^"]*)" modal$/) do |btn, title|
 end
 
 When(/^I enter uri, username and password for portus$/) do
-  step %(And I enter "#{ENV['PORTUS_URI']}" as "uri")
-  step %(And I enter "#{ENV['PORTUS_USER']}" as "username")
-  step %(And I enter "#{ENV['PORTUS_PASS']}" as "password")
+  step %(I enter "#{ENV['PORTUS_URI']}" as "uri")
+  step %(I enter "#{ENV['PORTUS_USER']}" as "username")
+  step %(I enter "#{ENV['PORTUS_PASS']}" as "password")
 end
