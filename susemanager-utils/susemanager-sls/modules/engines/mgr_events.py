9f6ea4a97ea9be72a5125f9ea780bcb2ad688ca9
Jochen Breuer <jbreuer@suse.de>
2018-08-17 17:04:23 +0200

    mgr_events: first draft for the database integration
    Removed Python in-memory queue and replaced it with INSERTs. Everything is
    still hard-coded. No error handling.
    
    Co-authored-by: Mihai Dinca <Mihai.Dinca@suse.com>

--- susemanager-utils/susemanager-sls/modules/engines/mgr_events.py
+++ susemanager-utils/susemanager-sls/modules/engines/mgr_events.py
@@ -7,6 +7,7 @@ A simple test engine, not intended for real use but as an example
 from __future__ import absolute_import, print_function, unicode_literals
 import logging
 import time
+import psycopg2
 
 # Import salt libs
 import salt.utils.event
@@ -18,9 +19,8 @@ import tornado.ioloop
 log = logging.getLogger(__name__)
 
 SCALING_FACTOR = 0.1
-COMMIT_INTERVAL_MAX = 10
-COMMIT_INTERVAL_MIN = 0.1
-QUEUE_SIZE_LIMIT = 5000
+COMMIT_INTERVAL_MAX = 5
+COMMIT_INTERVAL_MIN = 1
 
 # DELAY FACTOR VALUES
 # 0.9 would be approx. averaging time difference between the last 10 events -> higher fluctuation
@@ -30,32 +30,35 @@ DELAY_FACTOR = 0.1
 
 class Responder:
     def __init__(self, event_bus):
-        self.queue = []
-        self.frequency_average = 0.2
-        self.commit_interval = 1 / self.frequency_average
+        self.counter = 0
+        self.commit_interval = COMMIT_INTERVAL_MIN
         self.event_bus = event_bus
         self.commit_timer_handle = self.event_bus.io_loop.call_later(self.commit_interval, self.commit)
+        self.timer = time.time()
+        self._connect_to_database()
+
+    def _connect_to_database(self):
+        self.connection = psycopg2.connect("dbname='susemanager' user='spacewalk' host='localhost' password='spacewalk'")
+        self.cursor = self.connection.cursor()
+
+    def _insert(self, tag, data):
+        self.cursor.execute(
+            'INSERT INTO suseSaltEvent (tag, data) VALUES (%s, %s);',
+            (tag, salt.utils.json.dumps(data))
+        )
+        log.debug("******** Adding event to queue: {}".format(tag))
+        log.debug(self.cursor.query)
+        self.counter += 1
 
     def debug_log(self):
         log.debug("####################################################################################")
-        log.debug("******** queue_size: {}".format(len(self.queue)))
-        log.debug("******** fequency_average: {}".format(self.frequency_average))
+        log.debug("******** queue_size: {}".format(self.counter))
         log.debug("******** fequency_commit_interval: {}".format(self.apply_limits(self.commit_interval)))
 
     @tornado.gen.coroutine
     def add_event_to_queue(self, raw):
-        # TODO: Remove next two lines. Just for testing.
         tag, data = self.event_bus.unpack(raw, self.event_bus.serial)
-        log.debug("******** Adding event to queue: {}".format(tag))
-        self.queue.append(raw)
-        # Checking, if we are above the max. queue size.
-        if (len(self.queue)) >= QUEUE_SIZE_LIMIT:
-            # Unschedule next commit, so that we don't get two commits at the
-            # same time. Is re-scheduled after the commit.
-            self.event_bus.io_loop.remove_timeout(self.commit_timer_handle)
-            self.commit()
-	    self.commit_timer_handle = self.event_bus.io_loop.call_later(COMMIT_INTERVAL_MAX, self.commit)
-            
+        self._insert(tag, data)
 
     @staticmethod
     def apply_limits(commit_interval):
@@ -67,18 +70,14 @@ class Responder:
         Committing to the database.
         """
         log.debug("************** COMMIT! ")
-        time_between_events = self.commit_interval / (len(self.queue) or 0.1)
+        time_between_events = self.commit_interval / (self.counter or 0.1)
         commit_interval_limited = 1 / time_between_events * SCALING_FACTOR
         self.commit_interval = DELAY_FACTOR * self.commit_interval + (1 - DELAY_FACTOR) * commit_interval_limited
         self.debug_log()
-        while len(self.queue) > 0:
-            entry = self.queue.pop()
-            tag, data = self.event_bus.unpack(entry, self.event_bus.serial)
-            ret = {'data': data, 'tag': tag}
-            log.debug("******* INSERT INTO suseSaltEvent ({});".format(tag))
-
-	self.commit_timer_handle = self.event_bus.io_loop.call_later(
-	    self.apply_limits(self.commit_interval), self.commit)
+        self.connection.commit()
+        self.counter = 0
+        self.commit_timer_handle = self.event_bus.io_loop.call_later(
+        self.apply_limits(self.commit_interval), self.commit)
 
 
 def start():
@@ -95,4 +94,3 @@ def start():
     responder = Responder(event_bus)
     event_bus.set_event_handler(responder.add_event_to_queue)
     io_loop.start()
-
