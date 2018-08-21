1bddddc0ecf6f621c3da525730d45a5431427673
Eric Bischoff <ebischoff@suse.com>
2018-08-21 15:07:43 +0200

    Testsuite: docker steps cleanup

--- testsuite/features/min_docker_xmlrpc.feature
+++ testsuite/features/min_docker_xmlrpc.feature
@@ -5,13 +5,21 @@ Feature: XML-RPC image namespace for containers
 
   Scenario: Test image.store namespace
     Given I am authorized as "admin" with password "admin"
-    Then I run image.store tests via XML-RPC
+    When I create and delete an image store via XML-RPC
+    And I list image store types and image stores via XML-RPC
+    And I set and get details of image store via XML-RPC
 
   Scenario: Test image.profiles namespace
     Given I am authorized as "admin" with password "admin"
-    Then I run image.profiles tests via XML-RPC
+    When I create and delete profiles via XML-RPC
+    And I create and delete profile custom values via XML-RPC
+    And I list image profiles via XML-RPC
+    And I set and get profile details via XML-RPC
 
-  Scenario: Cleanup: remove custom system info key
+# Note: image building via XML-RPC is not tested here
+#       it is tested as another feature
+
+  Scenario: Cleanup: remove custom system info
     Given I am authorized as "admin" with password "admin"
     When I follow "Systems"
     And I follow "Custom System Info"
--- testsuite/features/step_definitions/docker_steps.rb
+++ testsuite/features/step_definitions/docker_steps.rb
@@ -6,64 +6,9 @@ require 'time'
 require 'date'
 require 'timeout'
 
-# this module test image_profile
-module ImageProfile
-  def create_delete
-    cont_op = XMLRPCImageTest.new(ENV['SERVER'])
-    cont_op.login('admin', 'admin')
-    # create delete profile test
-    cont_op.create_profile('fakeone', 'dockerfile', 'galaxy-registry', 'BiggerPathBiggerTest', '')
-    cont_op.delete_profile('fakeone')
-    cont_op.create_profile('fakeone', 'dockerfile', 'galaxy-registry', 'BiggerPathBiggerTest', '1-DOCKER-TEST')
-    cont_op.delete_profile('fakeone')
-  end
-
-  def custom_values
-    cont_op = XMLRPCImageTest.new(ENV['SERVER'])
-    cont_op.login('admin', 'admin')
-    cont_op.create_profile('fakeone', 'dockerfile', 'galaxy-registry', 'BiggerPathBiggerTest', '')
-    cont_op.create_custom_key('arancio', 'test containers')
-    values = {}
-    values['arancio'] = 'arancia xmlrpc tests'
-    cont_op.set_profile_custom_values('fakeone', values)
-    pro_det = cont_op.get_profile_custom_values('fakeone')
-    assert_equal(pro_det['arancio'], 'arancia xmlrpc tests', 'setting custom profile value failed')
-    pro_type = cont_op.list_image_profile_types
-    assert_equal(pro_type.length, 1, 'support at moment only one type of Profile!')
-    assert_equal(pro_type[0], 'dockerfile', 'type is not dockerfile?')
-    key = ['arancio']
-    cont_op.delete_profile_custom_values('fakeone', key)
-  end
-
-  def image_profiles_xmlrpc
-    cont_op = XMLRPCImageTest.new(ENV['SERVER'])
-    cont_op.login('admin', 'admin')
-    # create delete tests
-    create_delete
-    # set get delete Custom Values
-    custom_values
-    puts cont_op.list_image_profiles
-    # test listImageProfiles method
-    ima_profiles = cont_op.list_image_profiles
-    imagelabel = ima_profiles.select { |image| image['label'] = 'fakeone' }
-    assert_equal(imagelabel[0]['label'], 'fakeone', 'label of container should be fakeone!')
-    # test set value and get value call
-    details = {}
-    details['storeLabel'] = 'galaxy-registry'
-    details['path'] = 'TestForFun'
-    details['activationKey'] = ''
-    cont_op.set_profile_details('fakeone', details)
-    cont_detail = cont_op.get_details('fakeone')
-    assert_equal(cont_detail['label'], 'fakeone', 'label test fail!')
-    assert_equal(cont_detail['imageType'], 'dockerfile', 'imagetype test fail!')
-    cont_op.delete_profile('fakeone')
-  end
-end
-
-World(ImageProfile)
-
-# container_operations
+# container operations
 cont_op = XMLRPCImageTest.new(ENV['SERVER'])
+
 # retrieve minion id, needed for scheduleImageBuild call
 def retrieve_minion_id
   sysrpc = XMLRPCSystemTest.new(ENV['SERVER'])
@@ -77,19 +22,19 @@ def retrieve_minion_id
   minion_id
 end
 
-And(/^I select sle-minion hostname in Build Host$/) do
+When(/^I select sle-minion hostname in Build Host$/) do
   select($minion.full_hostname, from: 'buildHostId')
 end
 
-And(/^I navigate to images webpage$/) do
+When(/^I navigate to images webpage$/) do
   visit("https://#{$server.full_hostname}/rhn/manager/cm/images")
 end
 
-And(/^I navigate to images build webpage$/) do
+When(/^I navigate to images build webpage$/) do
   visit("https://#{$server.full_hostname}/rhn/manager/cm/build")
 end
 
-And(/^container "([^"]*)" built successfully$/) do |name|
+Then(/^container "([^"]*)" built successfully$/) do |name|
   cont_op.login('admin', 'admin')
   images_list = cont_op.list_images
   image_id = 0
@@ -115,7 +60,7 @@ And(/^container "([^"]*)" built successfully$/) do |name|
   end
 end
 
-And(/^all "([^"]*)" container images should be built correctly in the GUI$/) do |count|
+Then(/^all "([^"]*)" container images are built correctly in the GUI$/) do |count|
   def ck_container_imgs(count)
     build_timeout = 320
     Timeout.timeout(build_timeout) do
@@ -131,7 +76,7 @@ And(/^all "([^"]*)" container images should be built correctly in the GUI$/) do 
   ck_container_imgs(count) unless sle11family($minion)
 end
 
-And(/^I schedule the build of image "([^"]*)" via XML-RPC calls$/) do |image|
+When(/^I schedule the build of image "([^"]*)" via XML-RPC calls$/) do |image|
   cont_op.login('admin', 'admin')
   # empty by default
   version_build = ''
@@ -141,7 +86,7 @@ And(/^I schedule the build of image "([^"]*)" via XML-RPC calls$/) do |image|
   cont_op.schedule_image_build(image, version_build, build_hostid, date_build)
 end
 
-And(/^I schedule the build of image "([^"]*)" with version "([^"]*)" via XML-RPC calls$/) do |image, version|
+When(/^I schedule the build of image "([^"]*)" with version "([^"]*)" via XML-RPC calls$/) do |image, version|
   cont_op.login('admin', 'admin')
   # empty by default
   version_build = version
@@ -151,7 +96,7 @@ And(/^I schedule the build of image "([^"]*)" with version "([^"]*)" via XML-RPC
   cont_op.schedule_image_build(image, version_build, build_hostid, date_build)
 end
 
-And(/^I delete the image "([^"]*)" with version "([^"]*)" via XML-RPC calls$/) do |image_name_todel, version|
+When(/^I delete the image "([^"]*)" with version "([^"]*)" via XML-RPC calls$/) do |image_name_todel, version|
   cont_op.login('admin', 'admin')
   images_list = cont_op.list_images
   refute_nil(images_list, 'ERROR: no images at all were retrieved.')
@@ -168,7 +113,7 @@ And(/^I delete the image "([^"]*)" with version "([^"]*)" via XML-RPC calls$/) d
   end
 end
 
-And(/^the image "([^"]*)" with version "([^"]*)" doesn't exist via XML-RPC calls$/) do |image_non_exist, version|
+Then(/^the image "([^"]*)" with version "([^"]*)" doesn't exist via XML-RPC calls$/) do |image_non_exist, version|
   cont_op.login('admin', 'admin')
   images_list = cont_op.list_images
   images_list.each do |element|
@@ -176,22 +121,27 @@ And(/^the image "([^"]*)" with version "([^"]*)" doesn't exist via XML-RPC calls
   end
 end
 
-# images stores tests
-And(/^I run image.store tests via XML-RPC$/) do
+# image stores tests
+When(/^I create and delete an image store via XML-RPC$/) do
+  # create and delete a store, even with invalid URI
   cont_op.login('admin', 'admin')
-  # Test create and delete calls
-  # create and delete a store, even with invalid uri.
   cont_op.create_store('fake_store', 'https://github.com/SUSE/spacewalk-testsuite-base', 'registry')
   cont_op.delete_store('fake_store')
-  # test list images and list image types call
+end
+
+When(/^I list image store types and image stores via XML-RPC$/) do
+  cont_op.login('admin', 'admin')
   store_typ = cont_op.list_image_store_types
   assert_equal(store_typ.length, 1, 'we have only type support for Registry! New method added?! please update the tests')
   assert_equal(store_typ[0]['label'], 'registry', 'imagestore label type should be registry!')
   registry_list = cont_op.list_image_stores
-  # print just for debug
   puts registry_list
   assert_equal(registry_list[0]['label'], 'galaxy-registry', 'label is galaxy!')
   assert_equal(registry_list[0]['uri'], 'registry.mgr.suse.de', 'uri should be registry.mgr.suse.de')
+end
+
+When(/^I set and get details of image store via XML-RPC$/) do
+  cont_op.login('admin', 'admin')
   # test setDetails call
   # delete if test fail in the middle. delete image doesn't raise an error if image doesn't exists
   cont_op.create_store('Norimberga', 'https://github.com/SUSE/spacewalk-testsuite-base', 'registry')
@@ -200,12 +150,56 @@ And(/^I run image.store tests via XML-RPC$/) do
   details_store['username'] = ''
   details_store['password'] = ''
   cont_op.set_details('Norimberga', details_store)
-  # test get_details call
+  # test getDetails call
   details = cont_op.get_details_store('Norimberga')
   assert_equal(details['uri'], 'Germania', 'uri should be Germania')
   assert_equal(details['username'], '', 'username should be empty')
   cont_op.delete_store('Norimberga')
 end
 
-# Profiles tests using module
-And(/^I run image.profiles tests via XML-RPC$/, :image_profiles_xmlrpc)
+# profiles tests
+When(/^I create and delete profiles via XML-RPC$/) do
+  cont_op.login('admin', 'admin')
+  cont_op.create_profile('fakeone', 'dockerfile', 'galaxy-registry', 'BiggerPathBiggerTest', '')
+  cont_op.delete_profile('fakeone')
+  cont_op.create_profile('fakeone', 'dockerfile', 'galaxy-registry', 'BiggerPathBiggerTest', '1-DOCKER-TEST')
+  cont_op.delete_profile('fakeone')
+end
+
+When(/^I create and delete profile custom values via XML-RPC$/) do
+  cont_op.login('admin', 'admin')
+  cont_op.create_profile('fakeone', 'dockerfile', 'galaxy-registry', 'BiggerPathBiggerTest', '')
+  cont_op.create_custom_key('arancio', 'test containers')
+  values = {}
+  values['arancio'] = 'arancia xmlrpc tests'
+  cont_op.set_profile_custom_values('fakeone', values)
+  pro_det = cont_op.get_profile_custom_values('fakeone')
+  assert_equal(pro_det['arancio'], 'arancia xmlrpc tests', 'setting custom profile value failed')
+  pro_type = cont_op.list_image_profile_types
+  assert_equal(pro_type.length, 2, 'support for Dockerfile and Kiwi profiles')
+  assert_equal(pro_type[0], 'dockerfile', 'type is not dockerfile?')
+  assert_equal(pro_type[1], 'kiwi', 'type is not kiwi?')
+  key = ['arancio']
+  cont_op.delete_profile_custom_values('fakeone', key)
+end
+
+When(/^I list image profiles via XML-RPC$/) do
+  cont_op.login('admin', 'admin')
+  puts cont_op.list_image_profiles
+  ima_profiles = cont_op.list_image_profiles
+  imagelabel = ima_profiles.select { |image| image['label'] = 'fakeone' }
+  assert_equal(imagelabel[0]['label'], 'fakeone', 'label of container should be fakeone!')
+end
+
+When(/^I set and get profile details via XML-RPC$/) do
+  cont_op.login('admin', 'admin')
+  details = {}
+  details['storeLabel'] = 'galaxy-registry'
+  details['path'] = 'TestForFun'
+  details['activationKey'] = ''
+  cont_op.set_profile_details('fakeone', details)
+  cont_detail = cont_op.get_details('fakeone')
+  assert_equal(cont_detail['label'], 'fakeone', 'label test fail!')
+  assert_equal(cont_detail['imageType'], 'dockerfile', 'imagetype test fail!')
+  cont_op.delete_profile('fakeone')
+end
