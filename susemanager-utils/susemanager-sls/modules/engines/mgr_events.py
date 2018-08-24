4993ea6e8f07dea09d8b70ba574d173f64c5a134
Jochen Breuer <jbreuer@suse.de>
2018-08-24 09:54:22 +0200

    mgr_events: use engine configuration, add __virtual__ and basic error handling
    * Reading configuration values from Salt master engine configuration with
      sane defaults.
    * Handline import error of psycopg2 in __virtual__ function.
    * Very basic error handling for INSERTing events.
    
    Co-authored-by: Mihai Dinca <Mihai.Dinca@suse.com>

--- susemanager-utils/susemanager-sls/modules/engines/mgr_events.py
+++ susemanager-utils/susemanager-sls/modules/engines/mgr_events.py
@@ -8,7 +8,12 @@ from __future__ import absolute_import, print_function, unicode_literals
 import logging
 import time
 import fnmatch
-import psycopg2
+
+try:
+    import psycopg2
+    HAS_PSYCOPG2 = True
+except ImportError:
+    HAS_PSYCOPG2 = False
 
 # Import salt libs
 import salt.utils.event
@@ -19,59 +24,74 @@ import tornado.ioloop
 
 log = logging.getLogger(__name__)
 
-SCALING_FACTOR = 0.1
-COMMIT_INTERVAL_MAX = 5
-COMMIT_INTERVAL_MIN = 1
+DEFAULT_SCALING_FACTOR = 0.1
+DEFAULT_COMMIT_INTERVAL_MAX = 5
+DEFAULT_COMMIT_INTERVAL_MIN = 1
 
 # DELAY FACTOR VALUES
 # 0.9 would be approx. averaging time difference between the last 10 events -> higher fluctuation
 # 0.98 would be approx. averaging time difference between the last 50 events -> lower fluctuation
-DELAY_FACTOR = 0.1
+DEFAULT_DELAY_FACTOR = 0.1
+
+
+def __virtual__():
+    return HAS_PSYCOPG2
 
 
 class Responder:
-    def __init__(self, event_bus):
+    def __init__(self, event_bus, config):
+        self.config = config
+        self.config.setdefault('commit_interval_min', DEFAULT_COMMIT_INTERVAL_MIN)
+        self.config.setdefault('commit_interval_max', DEFAULT_COMMIT_INTERVAL_MAX)
+        self.config.setdefault('scaling_factor', DEFAULT_SCALING_FACTOR)
+        self.config.setdefault('delay_factor', DEFAULT_DELAY_FACTOR)
         self.counter = 0
-        self.commit_interval = COMMIT_INTERVAL_MIN
+        self.commit_interval = config['commit_interval_min']
         self.event_bus = event_bus
         self.commit_timer_handle = self.event_bus.io_loop.call_later(self.commit_interval, self.commit)
         self.timer = time.time()
         self._connect_to_database()
 
     def _connect_to_database(self):
-        self.connection = psycopg2.connect("dbname='susemanager' user='spacewalk' host='localhost' password='spacewalk'")
+        db_config = self.config.get('postgres_db')
+        conn_string = "dbname='{dbname}' user='{user}' host='{host}' password='{password}'".format(**db_config)
+        log.debug(conn_string)
+        self.connection = psycopg2.connect(conn_string)
         self.cursor = self.connection.cursor()
 
     def _insert(self, tag, data):
-        if not (
-            fnmatch.fnmatch(tag, "salt/minion/*/start") or
-            fnmatch.fnmatch(tag, "salt/job/*/ret/*") or
-            fnmatch.fnmatch(tag, "salt/beacon/*") or
+        if any([
+            fnmatch.fnmatch(tag, "salt/minion/*/start"),
+            fnmatch.fnmatch(tag, "salt/job/*/ret/*"),
+            fnmatch.fnmatch(tag, "salt/beacon/*"),
             fnmatch.fnmatch(tag, "suse/systemid/generate")
-        ):
-            log.debug("******** DISCARDING: {}".format(tag))
-            return
-        self.cursor.execute(
-            'INSERT INTO suseSaltEvent (data) VALUES (%s);',
-            (salt.utils.json.dumps({"tag": tag, "data": data}),)
-        )
-        log.debug("******** Adding event to queue: {}".format(tag))
-        log.debug(self.cursor.query)
-        self.counter += 1
+        ]):
+            try:
+                self.cursor.execute(
+                    'INSERT INTO suseSaltEvent (tag, data) VALUES (%s, %s);',
+                    (tag, salt.utils.json.dumps(data),)
+                )
+                log.debug("******** Adding event to queue: %s", tag)
+                log.debug(self.cursor.query)
+            except Exception as exp:
+                # FIXME: Too broad!!!
+                self.connection.rollback()
+            self.counter += 1
+        else:
+            log.debug("******** DISCARDING: %s", tag)
 
     def debug_log(self):
         log.debug("####################################################################################")
-        log.debug("******** queue_size: {}".format(self.counter))
-        log.debug("******** fequency_commit_interval: {}".format(self.apply_limits(self.commit_interval)))
+        log.debug("******** queue_size: %s", self.counter)
+        log.debug("******** fequency_commit_interval: %s", self.apply_limits(self.commit_interval))
 
     @tornado.gen.coroutine
     def add_event_to_queue(self, raw):
         tag, data = self.event_bus.unpack(raw, self.event_bus.serial)
         self._insert(tag, data)
 
-    @staticmethod
-    def apply_limits(commit_interval):
-	return max(min(commit_interval, COMMIT_INTERVAL_MAX), COMMIT_INTERVAL_MIN)
+    def apply_limits(self, commit_interval):
+        return max(min(commit_interval, self.config['commit_interval_max']), self.config['commit_interval_min'])
 
     @tornado.gen.coroutine
     def commit(self):
@@ -80,19 +100,19 @@ class Responder:
         """
         log.debug("************** COMMIT! ")
         time_between_events = self.commit_interval / (self.counter or 0.1)
-        commit_interval_limited = 1 / time_between_events * SCALING_FACTOR
-        self.commit_interval = DELAY_FACTOR * self.commit_interval + (1 - DELAY_FACTOR) * commit_interval_limited
+        commit_interval_limited = 1 / time_between_events * self.config['scaling_factor']
+        self.commit_interval = self.config['delay_factor'] * self.commit_interval + (1 - self.config['delay_factor']) * commit_interval_limited
         self.debug_log()
         self.connection.commit()
         self.cursor.execute(
-            'NOTIFY suseSaltEvent;'
+            'NOTIFY {};'.format(self.config['postgres_db']['notify_channel'])
         )
         self.counter = 0
         self.commit_timer_handle = self.event_bus.io_loop.call_later(
         self.apply_limits(self.commit_interval), self.commit)
 
 
-def start():
+def start(**config):
     '''
     Listen to events and write them to the Postgres database
     '''
@@ -103,6 +123,6 @@ def start():
             listen=True,
             io_loop=io_loop)
     log.debug('mgr_events engine started')
-    responder = Responder(event_bus)
+    responder = Responder(event_bus, config)
     event_bus.set_event_handler(responder.add_event_to_queue)
     io_loop.start()
