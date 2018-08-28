27e081cb0e0b1f45952cd9b4c8feb7e1fbd16c59
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2018-08-28 16:08:37 +0100

    Initial fix for using python-python-pam instead of PyPAM

--- backend/server/rhnAuthPAM.py
+++ backend/server/rhnAuthPAM.py
@@ -13,60 +13,23 @@
 # in this software or its documentation.
 #
 
-import PAM
+import pam
 import sys
 
 from spacewalk.common.usix import raise_with_tb
 from spacewalk.common.rhnLog import log_error
 from spacewalk.common.rhnException import rhnException
 
-__username = None
-__password = None
-
-
-def __pam_conv(auth, query_list):
-    global __username, __password
-    # Build a list of responses to be passed back to PAM
-    resp = []
-    for query, type in query_list:
-        if type == PAM.PAM_PROMPT_ECHO_ON:
-            # Prompt for a username
-            resp.append((__username, 0))
-        elif type == PAM.PAM_PROMPT_ECHO_OFF:
-            # Prompt for a password
-            resp.append((__password, 0))
-        else:
-            # Unknown PAM type
-            log_error("Got unknown PAM type %s (query=%s)" % (type, query))
-            return None
-
-    return resp
-
 
 def check_password(username, password, service):
-    global __username, __password
-    auth = PAM.pam()
-    auth.start(service, username, __pam_conv)
-
-    # Save the username and passwords in the globals, the conversation
-    # function needs access to them
-    __username = username
-    __password = password
-
     try:
-        try:
-            auth.authenticate()
-            auth.acct_mgmt()
-        finally:
-            # Something to be always executed - cleanup
-            __username = __password = None
-    except PAM.error:
-        e = sys.exc_info()[1]
-        resp, code = e.args[:2]
-        log_error("Password check failed (%s): %s" % (code, resp))
-        return 0
+        auth = pam.pam()
+        if not auth.authenticate(username, password, service=service):
+            e = sys.exc_info()[1]
+            resp, code = e.args[:2]
+            log_error("Password check failed (%s): %s" % (code, resp))
+            return 0
+        else:
+            return 1
     except:
         raise_with_tb(rhnException('Internal PAM error'), sys.exc_info()[2])
-    else:
-        # Good password
-        return 1
