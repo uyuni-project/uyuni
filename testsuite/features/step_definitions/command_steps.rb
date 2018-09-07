4772c922c9e347ab91c99446bc390dc137b51b61
Abid Mehmood <amehmood@suse.de>
2018-09-07 17:28:07 +0200

    Cucumber tests for new functions added in spacecmd for state channels

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -569,3 +569,24 @@ Then(/^The metadata buildtime from package "(.*?)" match the one in the rpm on "
   raise "Command failed: #{cmd}" unless return_code.zero?
   raise "Wrong buildtime in metadata: #{metadata_buildtime} != #{rpm_buildtime}" unless metadata_buildtime == rpm_buildtime
 end
+
+When(/^I create channel "([^"]*)" from spacecmd of type "([^"]*)"$/)  do |name, type|
+  command = "spacecmd -u admin -p admin -- configchannel_create -n #{name} -t  #{type}"
+  out, _code = $server.run(command)
+end
+
+Then(/^I update init.sls from spacecmd with content "([^"]*)" for channel "([^"]*)"$/) do |content , label|
+  filepath = "/tmp/#{label}"
+  $server.run("echo -e \"#{content}\" > #{filepath}", true, 600, 'root')
+  command = "spacecmd -u admin -p admin -- configchannel_updateinitsls -c #{label} -f  #{filepath} -y"
+  out, _code = $server.run(command)
+  file_delete($server, filepath)
+end
+
+And(/^I schedule apply configchannels for "([^"]*)"$/)  do |host|
+  node = get_target(host)
+  command = "spacecmd -y -u admin -p admin -- system_scheduleapplyconfigchannels  #{node.full_hostname}"
+  print "..."
+  print command
+  out, _code = $server.run(command)
+end
