7d529d659860596afd19327ea911cab43ca18fa4
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2018-09-17 13:49:39 +0100

    Port to Python3: Get rid of custom mock_open implementation

--- susemanager-utils/susemanager-sls/src/tests/mockery.py
+++ susemanager-utils/susemanager-sls/src/tests/mockery.py
@@ -27,20 +27,3 @@ def get_test_data(filename):
     :return:
     '''
     return open(os.path.sep.join([os.path.abspath(''), 'data', filename]), 'r').read()
-
-
-def mock_open(data=None):
-    '''
-    Mock "open" function.
-
-    :param data:
-    :return:
-    '''
-    data = StringIO(data)
-    mock = MagicMock(spec=file)
-    handle = MagicMock(spec=file)
-    handle.write.return_value = None
-    handle.__enter__.return_value = data or handle
-    mock.return_value = handle
-
-    return mock
