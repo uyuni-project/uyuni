a17a26ae3f79411306f432bf4e5e7cd0ffbea368
Frantisek Kobzik <fkobzik@suse.de>
2018-10-01 10:13:41 +0200

    Misc: improve formatting, remove unneccessary type

--- java/code/src/com/redhat/rhn/domain/formula/FormulaFactory.java
+++ java/code/src/com/redhat/rhn/domain/formula/FormulaFactory.java
@@ -451,18 +451,14 @@ public class FormulaFactory {
         if (!dataFile.exists()) {
             dataFile.getParentFile().mkdirs();
             dataFile.createNewFile();
-            serverFormulas = new HashMap<String, List<String>>();
+            serverFormulas = new HashMap<>();
         }
         else {
-            serverFormulas =
-                    GSON.fromJson(new BufferedReader(new FileReader(dataFile)),
-                            Map.class);
+            serverFormulas = GSON.fromJson(new BufferedReader(new FileReader(dataFile)), Map.class);
         }
 
         // Remove formula data for unselected formulas
-        List<String> deletedFormulas =
-                new LinkedList<>(serverFormulas.getOrDefault(minionId,
-                        new LinkedList<>()));
+        List<String> deletedFormulas = new LinkedList<>(serverFormulas.getOrDefault(minionId, new LinkedList<>()));
         deletedFormulas.removeAll(selectedFormulas);
         for (String deletedFormula : deletedFormulas) {
             deleteServerFormulaData(minionId, deletedFormula);
