4d3d1202e136de5e34c796536cdbe0745bfd6a45
Michael Calmer <mc@suse.de>
2018-10-03 16:10:55 +0200

    remove SUSE Manager container build files

--- susemanager-utils/testing/docker/head/sles12sp2python/add_packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# make sure the package repository is up to date
-zypper --non-interactive --gpg-auto-import-keys ref
-
-zypper --non-interactive install --no-recommends python python-xml
