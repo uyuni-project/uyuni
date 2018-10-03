f68571da72a6cd28ca597caca67e99660b5606a8
Michael Calmer <mc@suse.de>
2018-10-03 11:15:59 +0200

    define isRhel8() as KickstartInstallType

--- java/code/src/com/redhat/rhn/domain/kickstart/KickstartInstallType.java
+++ java/code/src/com/redhat/rhn/domain/kickstart/KickstartInstallType.java
@@ -32,6 +32,7 @@ public class KickstartInstallType extends BaseDomainHelper {
     public static final String RHEL_5 = "rhel_5";
     public static final String RHEL_6 = "rhel_6";
     public static final String RHEL_7 = "rhel_7";
+    public static final String RHEL_8 = "rhel_8";
     public static final String GENERIC_RPM = "generic_rpm";
 
     // Spacewalk's install type prefixes for some multi-version
@@ -72,6 +73,13 @@ public class KickstartInstallType extends BaseDomainHelper {
         // we need to reverse logic here
         return (!isRhel2() && !isRhel3() && !isRhel4() && !isFedora() && !isGeneric() &&
                 !isSUSE());
+    }
+
+    /**
+     * @return true if the installer type is rhel 8
+     */
+    public boolean isRhel8() {
+        return RHEL_8.equals(getLabel());
     }
 
     /**
@@ -120,7 +128,7 @@ public class KickstartInstallType extends BaseDomainHelper {
      * @return true if the installer type is rhel
      */
     public boolean isRhel() {
-        return isRhel2() || isRhel3() || isRhel4() || isRhel5() || isRhel6() || isRhel7();
+        return isRhel2() || isRhel3() || isRhel4() || isRhel5() || isRhel6() || isRhel7() || isRhel8();
     }
 
     /**
