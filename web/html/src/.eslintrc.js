204405fcb3ff00676540b3d9d20a9004d1f3bdba
Luis Neves <luneves@suse.de>
2018-10-15 00:23:09 +0100

    Improvements to flow/eslint to work with webpack alias and disable eslint rule "react/destructuring-assignment"

--- web/html/src/.eslintrc.js
+++ web/html/src/.eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
     parser: "babel-eslint",
+    root: true,
     extends: [
         "airbnb"
     ],
@@ -8,9 +9,37 @@ module.exports = {
         "flowtype-errors"
     ],
 
+    env: {
+      "browser": true,
+    },
+
+    "globals": {
+      "t": true,
+    },
+
     rules: {
       "flowtype-errors/show-errors": "error",
       "flowtype-errors/show-warnings": "warn",
-    }
+      "import/no-extraneous-dependencies": ["error", {"packageDir": "../../../susemanager-frontend/susemanager-nodejs-sdk-devel/"}],
+      "max-len": ['error', 120, 2, {
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      }],
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+      "react/destructuring-assignment": "off",
+    },
 
+    settings: {
+      'import/resolver': {
+        alias: {
+          map: [
+            ['components', './components'],
+            ['utils', './utils'],
+          ]
+        }
+      }
+    },
 }
