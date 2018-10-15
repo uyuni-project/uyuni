6d928c6f4dccd3cf71ff52e13430621f68dc8e93
Cédric Bosdonnat <cbosdonnat@suse.com>
2018-10-15 17:44:52 +0200

    ESlint fixes of guests-edit.renderer.js

--- web/html/src/manager/virtualization/guests/edit/guests-edit.renderer.js
+++ web/html/src/manager/virtualization/guests/edit/guests-edit.renderer.js
@@ -1,18 +1,21 @@
-/* eslint-disable */
 // @flow
-"use strict";
 
-const {GuestsEdit} = require("./guests-edit");
-const React = require("react");
-const ReactDOM = require("react-dom");
+const React = require('react');
+const ReactDOM = require('react-dom');
+const { GuestsEdit } = require('./guests-edit');
 
 window.pageRenderers = window.pageRenderers || {};
 window.pageRenderers.guests = window.pageRenderers.guests || {};
 window.pageRenderers.guests.edit = window.pageRenderers.guests.edit || {};
-window.pageRenderers.guests.edit.guestsEditRenderer = (id, {host, guest}) => ReactDOM.render(
-  <GuestsEdit
-    guest={guest}
-    host={host}
-  />,
-  document.getElementById(id)
-);
+window.pageRenderers.guests.edit.guestsEditRenderer = (id, { host, guest }) => {
+  const element = document.getElementById(id);
+  if (element !== null) {
+    ReactDOM.render(
+      <GuestsEdit
+        guest={guest}
+        host={host}
+      />,
+      element,
+    );
+  }
+};
