d287195fa07482f2f529e918d5fde62faa6d38f2
Cédric Bosdonnat <cbosdonnat@suse.com>
2018-10-15 12:07:22 +0200

    Eslint fixes and flowification of virtualization-guest-action-api.js

--- web/html/src/manager/virtualization/guests/virtualization-guest-action-api.js
+++ web/html/src/manager/virtualization/guests/virtualization-guest-action-api.js
@@ -1,61 +1,71 @@
-/* eslint-disable */
 // @flow
 
-const React = require("react");
-const Network = require("utils/network");
-const MessagesUtils = require("components/messages").Utils;
+const React = require('react');
+const Network = require('utils/network');
+const MessagesUtils = require('components/messages').Utils;
 
-class VirtualizationGuestActionApi extends React.Component {
-  constructor(props) {
+declare function t(msg: string, ...args: Array<any>): string;
+
+type Props = {
+  hostid: string,
+  children: Function,
+  bounce?: string,
+  callback?: Function,
+};
+
+type State = {
+  messages: Array<Object>,
+};
+
+class VirtualizationGuestActionApi extends React.Component<Props, State> {
+  static defaultProps = {
+    bounce: undefined,
+    callback: undefined,
+  };
+
+  constructor(props: Props) {
     super(props);
     this.state = {
       messages: [],
-      results: {},
     };
+  }
 
-    this.onAction = (action, uuids, parameters) => {
-      const messageData = Object.assign({}, parameters, {'uuids': uuids});
-      Network.post(`/rhn/manager/api/systems/details/virtualization/guests/${this.props.hostid}/${action}`,
-                   JSON.stringify(messageData), "application/json").promise
-        .then(response => {
-          if (Object.values(response).includes("Failed")) {
-            this.setState({
-              messages: [MessagesUtils.error(t(`Failed to trigger ${action}`))]
-            });
-          } else {
-            if (this.props.callback) {
-              this.props.callback(response);
-            }
-            if (this.props.bounce) {
-              window.location.replace(this.props.bounce)
-            }
-          }
-        }, (xhr) => {
-          const errMessages = xhr.status === 0 ?
-            [MessagesUtils.error(
-              t("Request interrupted or invalid response received from the server. Please try again."))]
-            : [MessagesUtils.error(Network.errorMessageByStatus(xhr.status))];
+  onAction = (action: string, uuids: Array<string>, parameters: Object) => {
+    const messageData = Object.assign({ }, parameters, { uuids });
+    Network.post(`/rhn/manager/api/systems/details/virtualization/guests/${this.props.hostid}/${action}`,
+      JSON.stringify(messageData), 'application/json').promise
+      .then((response) => {
+        if (Object.values(response).includes('Failed')) {
           this.setState({
-            messages: errMessages,
-          })
+            messages: [MessagesUtils.error(t(`Failed to trigger ${action}`))],
+          });
+        } else {
+          if (this.props.callback) {
+            this.props.callback(response);
+          }
+          if (this.props.bounce) {
+            window.location.replace(this.props.bounce);
+          }
+        }
+      }, (xhr) => {
+        const errMessages = xhr.status === 0
+          ? [MessagesUtils.error(
+            t('Request interrupted or invalid response received from the server. Please try again.'),
+          )]
+          : [MessagesUtils.error(Network.errorMessageByStatus(xhr.status))];
+        this.setState({
+          messages: errMessages,
         });
-    };
+      });
   }
 
   render() {
     return this.props.children({
       onAction: this.onAction,
       messages: this.state.messages,
-    })
+    });
   }
 }
-
-VirtualizationGuestActionApi.propTypes = {
-  hostid: React.PropTypes.string.isRequired,
-  children: React.PropTypes.func.isRequired,
-  bounce: React.PropTypes.string,
-  callback: React.PropTypes.func,
-};
 
 module.exports = {
   VirtualizationGuestActionApi,
