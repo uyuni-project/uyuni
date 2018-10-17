4cd683852909b7e37b784d278fbd7e1e74f4e819
dleidi <dleidi@suse.com>
2018-10-17 15:36:00 +0200

    Use channel name for ChannelJson object to show in the UI

--- web/html/src/utils/functions.js
+++ web/html/src/utils/functions.js
@@ -54,10 +54,6 @@ function LocalDateTime(date: Date): string {
            "T" + padTo(hours) + ":" + padTo(minutes) + ":" + padTo(seconds);
 }
 
-function sortString(s1: string, s2: string) {
-  return s1 > s2;
-}
-
 function sortById(aRaw: Object, bRaw: Object): number {
   const aId = aRaw["id"];
   const bId = bRaw["id"];
@@ -159,7 +155,6 @@ function deepCopy(e) {
 module.exports = {
     Utils: {
         cancelable: cancelable,
-        sortString: sortString,
         sortById: sortById,
         sortByText: sortByText,
         dateWithTimezone: dateWithTimezone,
