0c701321f3c44430f8c4f0892daeead7a09986e4
dleidi <dleidi@suse.com>
2018-10-23 14:46:25 +0100

    Fix lint style
    (cherry picked from commit 345f44f835735f85468c8f7e78dda3054ba1d648)

--- web/html/src/components/loading/loading.js
+++ web/html/src/components/loading/loading.js
@@ -1,27 +1,31 @@
-"use strict";
-const React = require("react");
-const Loading = (props) => {
+
+
+const React = require('react');
+
+type LoadingProps = {
+  text: string,
+  withBorders: bool
+}
+
+function Loading({ withBorders, text } : LoadingProps) {
   return (
     <div className="panel-body text-center">
       {
-        props.withBorders ?
-          <div className="line-separator"></div>
-          : null
-      }
-      <i className='fa fa-spinner fa-spin fa-1-5x'></i>
-      <h4>{props.text}</h4>
+          withBorders
+            ? <div className="line-separator" />
+            : null
+        }
+      <i className="fa fa-spinner fa-spin fa-1-5x" />
+      <h4>{text}</h4>
       {
-        props.withBorders ?
-          <div className="line-separator"></div>
+        withBorders
+          ? <div className="line-separator" />
           : null
       }
     </div>
-  )
+  );
 }
-Loading.propTypes = {
-  text: React.PropTypes.string.isRequired,
-  withBorders: React.PropTypes.bool
-};
+
 module.exports = {
-  Loading: Loading
-}
+  Loading,
+};
