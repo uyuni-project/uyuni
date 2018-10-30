50f8b192023820d53f526f91a9763f48547cbdb8
Johannes Renner <jrenner@suse.com>
2018-10-30 16:43:37 +0100

    Notifications cleanup: taskomatic job implementation

--- java/code/src/com/redhat/rhn/common/conf/ConfigDefaults.java
+++ java/code/src/com/redhat/rhn/common/conf/ConfigDefaults.java
@@ -245,6 +245,10 @@ public class ConfigDefaults {
      */
     public static final String KIWI_OS_IMAGE_BUILDING_ENABLED = "java.kiwi_os_image_building_enabled";
 
+    /**
+     * Lifetime of notification messages in days
+     */
+    public static final String NOTIFICATIONS_LIFETIME = "java.notifications_lifetime";
 
     /**
      * If true, signing metadata is enabled, otherwise metadata will not be signed
@@ -852,5 +856,13 @@ public class ConfigDefaults {
      */
     public boolean isMetadataSigningEnabled() {
         return Config.get().getBoolean(SIGN_METADATA);
+    }
+
+    /**
+     * Returns the notifications lifetime.
+     * @return notifications lifetime
+     */
+    public int getNotificationsLifetime() {
+        return Config.get().getInt(NOTIFICATIONS_LIFETIME, 30);
     }
 }
