9971d17d75feafa4fb8d6d50c8841b92a3f5f4d6
Luis Neves <luneves@suse.de>
2018-10-31 02:46:42 +0000

    Add lodash library and an eslint plugin to force native over lodash
    (cherry picked from commit c77392156d12b0c1d2c951b36d1d9ec8b30db457)

--- web/html/src/vendors/vendors.js
+++ web/html/src/vendors/vendors.js
@@ -1,10 +1,11 @@
 const vendors = [
-    "@babel/polyfill/dist/polyfill",
-    "react",
-    "react-dom",
-    "prop-types",
-    "core-js/shim",
-    "regenerator-runtime/runtime",
+  "@babel/polyfill/dist/polyfill",
+  "react",
+  "react-dom",
+  "prop-types",
+  "core-js/shim",
+  "regenerator-runtime/runtime",
+  "lodash"
 ];
 
 module.exports = vendors;
