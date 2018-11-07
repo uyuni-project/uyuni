ec02dcc92ac0aa6e51aff0504cded5d4124bc68d
Luis Neves <luneves@suse.de>
2018-11-07 15:05:48 +0000

    javascript build and obs fixes

--- susemanager-frontend/susemanager-nodejs-sdk-devel/setup.sh
+++ susemanager-frontend/susemanager-nodejs-sdk-devel/setup.sh
@@ -1,2 +1,3 @@
+(cd susemanager-frontend/susemanager-nodejs-sdk-devel; rm -rf node_modules)
 yarn install --frozen-lockfile
 echo "susemanager-nodejs-modules.tar.gz"
--- web/html/src/build/webpack.config.vendors.js
+++ web/html/src/build/webpack.config.vendors.js
@@ -3,6 +3,8 @@ const webpack = require("webpack");
 const vendors = require("../vendors/vendors");
 const LicenseCheckerWebpackPlugin = require("license-checker-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
 
 module.exports = (env, argv) => {
 
@@ -32,12 +34,16 @@ module.exports = (env, argv) => {
   return {
     name: "vendors",
     entry: {vendors},
+
     output: {
       path: path.resolve(__dirname, "../dist"),
       filename: "vendors/vendors.bundle.js",
       sourceMapFilename: "[name].map",
       pathinfo: true,
       library: '[name]_dll'
+    },
+    optimization: {
+      minimizer: [new TerserPlugin({extractComments: true})],
     },
     plugins: pluginsInUse,
   }
