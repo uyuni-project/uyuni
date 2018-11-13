c32c1ecbe1fe701465af785c96bb4c3aef96edab
Cédric Bosdonnat <cbosdonnat@suse.com>
2018-11-13 09:31:48 +0100

    GuestProperties: use VirtualizationPoolsListRefreshApi
    The guest properties component will need to fetch the list of storage
    pools in order to add new disks to the VM. Introduce this in a separate
    commit in order to ease the review of the patches actually adding
    feature since this one mostly moves a lot of code.

--- web/html/src/manager/virtualization/guests/guest-properties.js
+++ web/html/src/manager/virtualization/guests/guest-properties.js
@@ -14,6 +14,7 @@ const { GuestPropertiesForm } = require('./properties/guest-properties-form');
 const { GuestPropertiesTraditional } = require('./properties/guest-properties-traditional');
 const { VirtualizationDomainsCapsApi } = require('./virtualization-domains-caps-api');
 const { VirtualizationNetsListRefreshApi } = require('../nets/virtualization-nets-list-refresh-api');
+const { VirtualizationPoolsListRefreshApi } = require('../pools/virtualization-pools-list-refresh-api');
 
 declare function t(msg: string, ...args: Array<any>): string;
 
@@ -61,90 +62,99 @@ class GuestProperties extends React.Component<Props> {
             networks,
             error: netListError,
           }) => (
-            <VirtualizationDomainsCapsApi hostId={this.props.host.id}>
+            <VirtualizationPoolsListRefreshApi serverId={this.props.host.id}>
               {
                 ({
-                  domainsCaps,
-                  messages,
-                }) => {
-                  const allMessages = [].concat(this.props.messages, messages, netListError)
-                    .filter(item => item);
-                  const { initialModel } = this.props;
+                  pools,
+                  error: poolListError,
+                }) => (
+                  <VirtualizationDomainsCapsApi hostId={this.props.host.id}>
+                    {
+                      ({
+                        domainsCaps,
+                        messages,
+                      }) => {
+                        const allMessages = [].concat(this.props.messages, messages, netListError, poolListError)
+                          .filter(item => item);
+                        const { initialModel } = this.props;
 
-                  if (initialModel != null && networks != null && domainsCaps.length > 0
-                      && allMessages.length === 0) {
-                    return (
-                      <GuestPropertiesForm
-                        submitText={this.props.submitText}
-                        submit={this.props.submit}
-                        initialModel={this.props.initialModel}
-                        validationChecks={this.validationChecks}
-                        messages={allMessages}
-                        localTime={this.props.localTime}
-                        timezone={this.props.timezone}
-                        actionChains={this.props.actionChains}
-                      >
-                        {
-                          ({ model, changeModel }) => {
-                            const vmTypes = domainsCaps.map(cap => cap.domain)
-                              .filter((vmType, idx, array) => array.indexOf(vmType) === idx);
-                            const vmType = model.vmType
-                              || initialModel.vmType
-                              || (vmTypes.includes('kvm') ? 'kvm' : vmTypes[0]);
-                            const arch = initialModel.arch || this.props.host.cpu.arch;
-                            const caps = domainsCaps.find(cap => cap.arch === arch && cap.domain === vmType);
+                        if (initialModel != null && networks != null && pools != null
+                            && domainsCaps.length > 0 && allMessages.length === 0) {
+                          return (
+                            <GuestPropertiesForm
+                              submitText={this.props.submitText}
+                              submit={this.props.submit}
+                              initialModel={initialModel}
+                              validationChecks={this.validationChecks}
+                              messages={allMessages}
+                              localTime={this.props.localTime}
+                              timezone={this.props.timezone}
+                              actionChains={this.props.actionChains}
+                            >
+                              {
+                                ({ model, changeModel }) => {
+                                  const vmTypes = domainsCaps.map(cap => cap.domain)
+                                    .filter((vmType, idx, array) => array.indexOf(vmType) === idx);
+                                  const vmType = model.vmType
+                                    || initialModel.vmType
+                                    || (vmTypes.includes('kvm') ? 'kvm' : vmTypes[0]);
+                                  const arch = initialModel.arch || this.props.host.cpu.arch;
+                                  const caps = domainsCaps.find(cap => cap.arch === arch && cap.domain === vmType);
 
-                            return [
-                              <Panel key="general" title={t('General')} headingLevel="h2">
-                                <Text
-                                  name="memory"
-                                  label={t('Maximum Memory (MiB)')}
-                                  required
-                                  invalidHint={t('A positive integer is required')}
-                                  labelClass="col-md-3"
-                                  divClass="col-md-6"
-                                  validators={[Validation.isInt({ gt: 0 })]}
-                                />
-                                <Text
-                                  name="vcpu"
-                                  label={t('Virtual CPU Count')}
-                                  required
-                                  invalidHint={t('A positive integer is required')}
-                                  labelClass="col-md-3"
-                                  divClass="col-md-6"
-                                  validators={[Validation.isInt({ gt: 0 })]}
-                                />
-                              </Panel>,
-                              guestNicsPanel(model, changeModel, networks),
-                              <Panel key="graphics" title={t('Graphics')} headingLevel="h2">
-                                <Select
-                                  labelClass="col-md-3"
-                                  divClass="col-md-6"
-                                  label={t('Type')}
-                                  name="graphicsType"
-                                >
-                                  {
-                                    [{ key: 'vnc', display: 'VNC' }, { key: 'spice', display: 'Spice' }]
-                                      .filter(entry => caps !== undefined
-                                        && caps.devices.graphics.type.includes(entry.key))
-                                      .map(entry => (
-                                        <option key={entry.key} value={entry.key}>
-                                          {entry.display}
-                                        </option>))
-                                  }
-                                  <option key="" value="">{t('None')}</option>
-                                </Select>
-                              </Panel>,
-                            ];
-                          }
+                                  return [
+                                    <Panel key="general" title={t('General')} headingLevel="h2">
+                                      <Text
+                                        name="memory"
+                                        label={t('Maximum Memory (MiB)')}
+                                        required
+                                        invalidHint={t('A positive integer is required')}
+                                        labelClass="col-md-3"
+                                        divClass="col-md-6"
+                                        validators={[Validation.isInt({ gt: 0 })]}
+                                      />
+                                      <Text
+                                        name="vcpu"
+                                        label={t('Virtual CPU Count')}
+                                        required
+                                        invalidHint={t('A positive integer is required')}
+                                        labelClass="col-md-3"
+                                        divClass="col-md-6"
+                                        validators={[Validation.isInt({ gt: 0 })]}
+                                      />
+                                    </Panel>,
+                                    guestNicsPanel(model, changeModel, networks),
+                                    <Panel key="graphics" title={t('Graphics')} headingLevel="h2">
+                                      <Select
+                                        labelClass="col-md-3"
+                                        divClass="col-md-6"
+                                        label={t('Type')}
+                                        name="graphicsType"
+                                      >
+                                        {
+                                          [{ key: 'vnc', display: 'VNC' }, { key: 'spice', display: 'Spice' }]
+                                            .filter(entry => caps !== undefined
+                                              && caps.devices.graphics.type.includes(entry.key))
+                                            .map(entry => (
+                                              <option key={entry.key} value={entry.key}>
+                                                {entry.display}
+                                              </option>))
+                                        }
+                                        <option key="" value="">{t('None')}</option>
+                                      </Select>
+                                    </Panel>,
+                                  ];
+                                }
+                              }
+                            </GuestPropertiesForm>
+                          );
                         }
-                      </GuestPropertiesForm>
-                    );
-                  }
-                  return <Loading text={t('Loading...')} withBorders={false} />;
-                }
+                        return <Loading text={t('Loading...')} withBorders={false} />;
+                      }
+                    }
+                  </VirtualizationDomainsCapsApi>
+                )
               }
-            </VirtualizationDomainsCapsApi>
+            </VirtualizationPoolsListRefreshApi>
           )
         }
       </VirtualizationNetsListRefreshApi>
