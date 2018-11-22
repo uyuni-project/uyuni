9973593c1fb19410f167f513e7ee14f60320ab07
Eric Bischoff <ebischoff@suse.com>
2018-11-22 08:26:49 +0100

    Leave 3 seconds for filtered package list to appear

--- testsuite/features/step_definitions/salt_steps.rb
+++ testsuite/features/step_definitions/salt_steps.rb
@@ -228,6 +228,7 @@ Then(/^I click on the css "(.*)" until page does not contain "([^"]*)" text$/) d
           break
         end
         find(css).click
+        sleep 3
       end
     end
   rescue Timeout::Error
@@ -246,7 +247,7 @@ Then(/^I click on the css "(.*)" until page does contain "([^"]*)" text$/) do |c
           break
         end
         find(css).click
-        sleep 1
+        sleep 3
       end
     end
   rescue Timeout::Error
