c37c676e14f37e89de216c97057294d7f8aba9b6
Luis Neves <luneves@suse.de>
2018-11-23 01:17:26 +0000

    ant task deploy-static-resources now aborts when yarn returns an error

--- web/html/src/build.js
+++ web/html/src/build.js
@@ -12,6 +12,7 @@ fillSpecFile()
     const { stdout } = shell.exec("(cd ../../;hash git && git ls-files -m)");
 
     if(stdout && stdout.includes("spacewalk-web.spec")) {
-      throw new Error("It seems the most recent spacewalk-web.spec file isn't on git, run build again and commit the new generated susemanager-web-libs.spec file ");
+      shell.echo("It seems the most recent spacewalk-web.spec file isn't on git, run build again and commit the new generated susemanager-web-libs.spec file ");
+      shell.exit(1);
     }
   });
