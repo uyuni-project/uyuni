9dcbdf784dc5cc27f3a56e59edf5b69360b9a512
dleidi <dleidi@suse.com>
2018-12-06 10:11:37 +0100

    Add listeners to log frontend load/leave page events

--- web/html/src/manager/frontend-log.js
+++ web/html/src/manager/frontend-log.js
@@ -11,6 +11,14 @@ const FrontendLog = React.createClass({
       headers.set('X-CSRF-Token', document.getElementsByName('csrf_token')[0].value);
       return headers;
     }
+    // store a log message about the page has been loaded
+    window.addEventListener('load', function(event) {
+      Loggerhead.info('[' + new Date().toUTCString() + '] - Loading `' + window.location + '`');
+    });
+    // store a log message about a page has been left
+    window.addEventListener('unload', function(event) {
+      Loggerhead.info('[' + new Date().toUTCString() + '] - Leaving `' + window.location + '`');
+    });
     // store a log message about the error that just happened
     window.addEventListener('error', function(event) {
       // Note that col & error are new to the HTML 5 and may not be supported in every browser.
