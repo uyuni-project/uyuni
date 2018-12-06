e5d229da88b5429d0712876e1d4c5baa211dfdac
dleidi <dleidi@suse.com>
2018-12-06 10:11:08 +0100

    Add listener to log frontend error events

--- web/html/src/manager/frontend-log.js
+++ web/html/src/manager/frontend-log.js
@@ -11,6 +11,14 @@ const FrontendLog = React.createClass({
       headers.set('X-CSRF-Token', document.getElementsByName('csrf_token')[0].value);
       return headers;
     }
+    // store a log message about the error that just happened
+    window.addEventListener('error', function(event) {
+      // Note that col & error are new to the HTML 5 and may not be supported in every browser.
+      var extra = !event.colno ? '' : '\ncolumn: ' + event.colno;
+      extra += !event.error ? '' : '\nerror: ' + event.error;
+      const errorMessage = event.message + '\nurl: ' + event.filename + '\nline: ' + event.lineno + extra;
+      Loggerhead.error(errorMessage);
+    });
   },
 
   render: function() {
