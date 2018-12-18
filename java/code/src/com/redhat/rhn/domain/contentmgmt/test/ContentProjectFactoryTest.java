ab2d1dd5a6da048851fd5dd90b20c5dcf75e02c4
Michael Calmer <mc@suse.de>
2018-12-18 17:05:52 +0100

    test environments

--- java/code/src/com/redhat/rhn/domain/contentmgmt/test/ContentProjectFactoryTest.java
+++ java/code/src/com/redhat/rhn/domain/contentmgmt/test/ContentProjectFactoryTest.java
@@ -15,9 +15,12 @@
 
 package com.redhat.rhn.domain.contentmgmt.test;
 
+import com.redhat.rhn.domain.contentmgmt.ContentEnvironment;
 import com.redhat.rhn.domain.contentmgmt.ContentProject;
 import com.redhat.rhn.domain.contentmgmt.ContentProjectFactory;
 import com.redhat.rhn.testing.BaseTestCaseWithUser;
+
+import java.util.Set;
 
 public class ContentProjectFactoryTest extends BaseTestCaseWithUser {
 
@@ -30,5 +33,49 @@ public class ContentProjectFactoryTest extends BaseTestCaseWithUser {
         ContentProjectFactory.getInstance().save(cp);
 
         System.out.println(ContentProjectFactory.getInstance());
+    }
+
+    public void testEnvironment() throws Exception {
+        ContentProjectFactory contentProdjectFactory = ContentProjectFactory.getInstance();
+        ContentProject cp = new ContentProject();
+        cp.setLabel("project1");
+        cp.setDescription("This is project 1");
+        contentProdjectFactory.save(cp);
+
+        ContentEnvironment envdev = new ContentEnvironment();
+        envdev.setLabel("dev");
+        envdev.setName("Development");
+        envdev.setContentProject(cp);
+        contentProdjectFactory.save(envdev);
+
+        ContentEnvironment envtest = new ContentEnvironment();
+        envtest.setLabel("test");
+        envtest.setName("Test");
+        envtest.setContentProject(cp);
+        contentProdjectFactory.save(envtest);
+
+        envdev.setNextEnvironment(envtest);
+        contentProdjectFactory.save(envdev);
+
+        ContentProject p1 = contentProdjectFactory.lookupContentProjectByLabel("project1");
+        assertEquals("project1", p1.getLabel());
+        assertEquals("This is project 1", p1.getDescription());
+
+        Set<ContentEnvironment> envs = p1.getEnvironments();
+        ContentEnvironment first = envs.iterator().next();
+        if (first.getLabel().equals("dev")) {
+            assertEquals("Development", first.getName());
+            ContentEnvironment next = first.getNextEnvironment();
+            assertEquals("test", next.getLabel());
+            assertEquals("Test", next.getName());
+            assertTrue(next.getPrevEnvironment().equals(first));
+        }
+        else if (first.getLabel().equals("test")) {
+            assertEquals("Test", first.getName());
+            ContentEnvironment prev = first.getPrevEnvironment();
+            assertEquals("dev", prev.getLabel());
+            assertEquals("Development", prev.getName());
+            assertTrue(prev.getNextEnvironment().equals(first));
+        }
     }
 }
