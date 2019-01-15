ff3c6b9b40bf1e421c8da0a9a3ca2fd0a3ab0adb
Bo Maryniuk <bo@suse.de>
2019-01-15 15:33:14 +0100

    Add zypposync lib

---
+++ backend/satellite_tools/repo_plugins/zypposync.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+"""
+Reposync via Salt library.
+"""
+from __future__ import absolute_import, unicode_literals, print_function
+
+import salt.client
+import salt.config
+
+
+class ZyppoSync(object):
+    """
+    Wrapper for underlying package manager for the reposync via Salt.
+
+    Example usage:
+
+    >>> zyppo = ZyppoSync()
+    >>> for idx, repo_meta in enumerate(zyppo.list_repos().values()):
+    >>>    print(idx + 1, repo_meta["name"])
+    >>>    print("  ", repo_meta["baseurl"])
+    
+    """
+    def __init__(self, cfg_path="/etc/salt/minion"):
+        self._conf = salt.config.minion_config(cfg_path)
+        self._conf["file_client"] = "local"
+        self._caller = salt.client.Caller(mopts=self._conf)
+
+    def _get_call(self, key):
+        """
+        Prepare a call to the pkg module.
+        """
+        def make_call(*args, **kwargs):
+            """
+            Makes a call to the underlying package.
+            """
+            return self._caller.cmd("pkg.{}".format(key), *args, **kwargs)
+        return make_call
+
+    def __getattr__(self, attr):
+        """
+        Prepare a callable on the requested
+        attribute name.
+        """
+        return self._get_call(attr)
+
+    
