adec4e6ddbf7b6456d7d5440746c49a26eafe6ac
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2019-01-18 16:08:23 +0000

    Add UpdateNotice class implementation to parse updateinfo

--- backend/satellite_tools/repo_plugins/zypposync.py
+++ backend/satellite_tools/repo_plugins/zypposync.py
@@ -76,6 +76,168 @@ class ZyppoSync:
         return self._get_call(attr)
 
 
+class UpdateNoticeException(Exception):
+    """ An exception thrown for bad UpdateNotice data. """
+    pass
+
+
+class UpdateNotice(object):
+    def __init__(self, elem=None, repoid=None, vlogger=None):
+        self._md = {
+            'from'             : '',
+            'type'             : '',
+            'title'            : '',
+            'release'          : '',
+            'status'           : '',
+            'version'          : '',
+            'pushcount'        : '',
+            'update_id'        : '',
+            'issued'           : '',
+            'updated'          : '',
+            'description'      : '',
+            'rights'           : '',
+            'severity'         : '',
+            'summary'          : '',
+            'solution'         : '',
+            'references'       : [],
+            'pkglist'          : [],
+            'reboot_suggested' : False
+        }
+
+        if elem:
+            self._parse(elem)
+
+    def __getitem__(self, item):
+        """ Allows scriptable metadata access (ie: un['update_id']). """
+        if type(item) is int:
+            return sorted(self._md)[item]
+        ret = self._md.get(item)
+        if ret == '':
+            ret = None
+        return ret
+
+    def __setitem__(self, item, val):
+        self._md[item] = val
+
+    def _parse(self, elem):
+        """
+        Parse an update element::
+            <!ELEMENT update (id, synopsis?, issued, updated,
+                              references, description, rights?,
+                              severity?, summary?, solution?, pkglist)>
+                <!ATTLIST update type (errata|security) "errata">
+                <!ATTLIST update status (final|testing) "final">
+                <!ATTLIST update version CDATA #REQUIRED>
+                <!ATTLIST update from CDATA #REQUIRED>
+        """
+        if elem.tag == 'update':
+            for attrib in ('from', 'type', 'status', 'version'):
+                self._md[attrib] = elem.attrib.get(attrib)
+            for child in elem:
+                if child.tag == 'id':
+                    if not child.text:
+                        raise UpdateNoticeException("No id element found")
+                    self._md['update_id'] = child.text
+                elif child.tag == 'pushcount':
+                    self._md['pushcount'] = child.text
+                elif child.tag == 'issued':
+                    self._md['issued'] = child.attrib.get('date')
+                elif child.tag == 'updated':
+                    self._md['updated'] = child.attrib.get('date')
+                elif child.tag == 'references':
+                    self._parse_references(child)
+                elif child.tag == 'description':
+                    self._md['description'] = child.text
+                elif child.tag == 'rights':
+                    self._md['rights'] = child.text
+                elif child.tag == 'severity':
+                    self._md[child.tag] = child.text
+                elif child.tag == 'summary':
+                    self._md['summary'] = child.text
+                elif child.tag == 'solution':
+                    self._md['solution'] = child.text
+                elif child.tag == 'pkglist':
+                    self._parse_pkglist(child)
+                elif child.tag == 'title':
+                    self._md['title'] = child.text
+                elif child.tag == 'release':
+                    self._md['release'] = child.text
+        else:
+            raise UpdateNoticeException('No update element found')
+
+    def _parse_references(self, elem):
+        """
+        Parse the update references::
+            <!ELEMENT references (reference*)>
+            <!ELEMENT reference>
+                <!ATTLIST reference href CDATA #REQUIRED>
+                <!ATTLIST reference type (self|other|cve|bugzilla) "self">
+                <!ATTLIST reference id CDATA #IMPLIED>
+                <!ATTLIST reference title CDATA #IMPLIED>
+        """
+        for reference in elem:
+            if reference.tag == 'reference':
+                data = {}
+                for refattrib in ('id', 'href', 'type', 'title'):
+                    data[refattrib] = reference.attrib.get(refattrib)
+                self._md['references'].append(data)
+            else:
+                raise UpdateNoticeException('No reference element found')
+
+    def _parse_pkglist(self, elem):
+        """
+        Parse the package list::
+            <!ELEMENT pkglist (collection+)>
+            <!ELEMENT collection (name?, package+)>
+                <!ATTLIST collection short CDATA #IMPLIED>
+                <!ATTLIST collection name CDATA #IMPLIED>
+            <!ELEMENT name (#PCDATA)>
+        """
+        for collection in elem:
+            data = { 'packages' : [] }
+            if 'short' in collection.attrib:
+                data['short'] = collection.attrib.get('short')
+            for item in collection:
+                if item.tag == 'name':
+                    data['name'] = item.text
+                elif item.tag == 'package':
+                    data['packages'].append(self._parse_package(item))
+            self._md['pkglist'].append(data)
+
+    def _parse_package(self, elem):
+        """
+        Parse an individual package::
+            <!ELEMENT package (filename, sum, reboot_suggested)>
+                <!ATTLIST package name CDATA #REQUIRED>
+                <!ATTLIST package version CDATA #REQUIRED>
+                <!ATTLIST package release CDATA #REQUIRED>
+                <!ATTLIST package arch CDATA #REQUIRED>
+                <!ATTLIST package epoch CDATA #REQUIRED>
+                <!ATTLIST package src CDATA #REQUIRED>
+            <!ELEMENT reboot_suggested (#PCDATA)>
+            <!ELEMENT filename (#PCDATA)>
+            <!ELEMENT sum (#PCDATA)>
+                <!ATTLIST sum type (md5|sha1) "sha1">
+        """
+        package = {}
+        for pkgfield in ('arch', 'epoch', 'name', 'version', 'release', 'src'):
+            package[pkgfield] = elem.attrib.get(pkgfield)
+
+        #  Bad epoch and arch data is the most common (missed) screwups.
+        # Deal with bad epoch data.
+        if not package['epoch'] or package['epoch'][0] not in '0123456789':
+            package['epoch'] = None
+
+        for child in elem:
+            if child.tag == 'filename':
+                package['filename'] = child.text
+            elif child.tag == 'sum':
+                package['sum'] = (child.attrib.get('type'), child.text)
+            elif child.tag == 'reboot_suggested':
+                self._md['reboot_suggested'] = True
+        return package
+
+
 class ContentSource:
     def __init__(self, url, name, insecure=False, interactive=True,
                  yumsrc_conf=None, org="1", channel_label="",
