1da511dd1e11d1ee3b70d25a283bac039f371ee5
Frantisek Kobzik <fkobzik@suse.de>
2019-01-18 11:33:12 +0100

    Tests: Content Environment functionality

--- java/code/src/com/redhat/rhn/domain/contentmgmt/test/ContentManagerTest.java
+++ java/code/src/com/redhat/rhn/domain/contentmgmt/test/ContentManagerTest.java
@@ -15,9 +15,11 @@
 
 package com.redhat.rhn.domain.contentmgmt.test;
 
+import com.redhat.rhn.domain.contentmgmt.ContentEnvironment;
 import com.redhat.rhn.domain.contentmgmt.ContentManagementException;
 import com.redhat.rhn.domain.contentmgmt.ContentManager;
 import com.redhat.rhn.domain.contentmgmt.ContentProject;
+import com.redhat.rhn.domain.contentmgmt.ContentProjectFactory;
 import com.redhat.rhn.domain.org.Org;
 import com.redhat.rhn.domain.user.User;
 import com.redhat.rhn.testing.BaseTestCaseWithUser;
@@ -25,6 +27,8 @@ import com.redhat.rhn.testing.UserTestUtils;
 
 import java.util.Optional;
 
+import static java.util.Arrays.asList;
+import static java.util.Optional.empty;
 import static java.util.Optional.of;
 
 /**
@@ -101,5 +105,59 @@ public class ContentManagerTest extends BaseTestCaseWithUser {
         }
 
         assertEquals(0, ContentManager.removeContentProject(cp.getLabel(), anotherUser));
+    }
+
+    /**
+     * Tests creating, looking up and removing Environments in a Project
+     */
+    public void testContentEnvironmentLifecycle() {
+        ContentProject cp = ContentManager.createContentProject("cplabel", "cpname", "description", user);
+
+        ContentEnvironment fst = ContentManager.
+                createContentEnvironment(cp.getLabel(), empty(), "fst", "first env", "desc", user);
+        ContentEnvironment snd = ContentManager
+                .createContentEnvironment(cp.getLabel(), of(fst.getLabel()), "snd", "second env", "desc2", user);
+        assertEquals(asList(fst, snd), ContentProjectFactory.listProjectEnvironments(cp));
+
+        ContentEnvironment mid = ContentManager.
+                createContentEnvironment(cp.getLabel(), of(fst.getLabel()), "mid", "middle env", "desc", user);
+        assertEquals(asList(fst, mid, snd), ContentProjectFactory.listProjectEnvironments(cp));
+
+        int numRemoved = ContentManager.removeContentEnvironment(fst.getLabel(), cp.getLabel(), user);
+        assertEquals(1, numRemoved);
+        assertEquals(asList(mid, snd), ContentProjectFactory.listProjectEnvironments(cp));
+        assertEquals(mid, cp.getFirstEnvironmentOpt().get());
+    }
+
+    /**
+     * Test behavior when appending a Content Environment behind non-existing Content Environment
+     */
+    public void testAddingEnvironmentAfterMismatchedPredecessor() {
+        ContentProject cp = ContentManager.createProject("cplabel", "cpname", "description", user);
+        ContentManager.createEnvironment(cp.getLabel(), empty(), "fst", "first env", "desc", user);
+        try {
+            ContentManager.createEnvironment(cp.getLabel(), of("NONEXISTING"), "snd", "snd env", "desc",
+                    user);
+            fail("An exception should have been thrown");
+        }
+        catch (ContentManagementException e) {
+            // expected
+        }
+    }
+
+    /**
+     * Test updating a Content Environment
+     */
+    public void testUpdateContentEnvironment() {
+        ContentProject cp = ContentManager.createContentProject("cplabel", "cpname", "description", user);
+        ContentManager
+                .createContentEnvironment(cp.getLabel(), empty(), "fst", "first env", "desc", user);
+        ContentManager.updateContentEnvironment("fst", "cplabel", of("new env name"),
+                of("new description"), user);
+        ContentEnvironment fromDb = ContentManager
+                .lookupContentEnvironment("fst", "cplabel", user).get();
+
+        assertEquals("new env name", fromDb.getName());
+        assertEquals("new description", fromDb.getDescription());
     }
 }
