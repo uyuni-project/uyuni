6947211a985ad419dfe6d5e0df05859dce5af20d
Luis Neves <luneves@suse.de>
2019-01-21 03:15:37 +0000

    New way to automatically add new frontend routes/pages

--- web/html/src/manager/index.js
+++ web/html/src/manager/index.js
@@ -1,5 +1,15 @@
 /* eslint-disable */
-const pages = {
+
+/*
+How to add a new route:
+All the routes exported on the files '<*folder_name*>/index.js' will be automatically registered.
+Check the file content-management/index.js for an example
+*/
+
+const { readdirSync, statSync } = require('fs')
+const { join } = require('path')
+
+const oldPagesEntries = {
   'polyfill': './manager/polyfill.js',
   'errors/not-found': './manager/errors/not-found.js',
   'visualization/hierarchy': './manager/visualization/hierarchy.js',
@@ -43,11 +53,40 @@ const pages = {
   'activation-key/activation-key-channels.renderer': './manager/activation-key/activation-key-channels.renderer.js',
 }
 
-Object.keys(pages).forEach((key) => {
-  pages["javascript/manager/" + key] = pages[key];
-  delete pages[key]
+const readDirs = p => readdirSync(p).filter(f => statSync(join(p, f)).isDirectory())
+const dirs = readDirs("./manager");
+
+const dirsWithRoutes = dirs.filter(dir => {
+  try {
+    return require(`./${dir}`).entries
+  } catch(e){
+    return false;
+  };
+})
+
+
+const newPagesEntries = dirsWithRoutes.reduce((newRoutes, nextDir) => {
+  const nextDirRoutes = require(`./${nextDir}`).entries;
+  const nextDirRoutesFormated = nextDirRoutes.reduce((routesFormated, nextRoute) => {
+    const formatedKey = `${nextDir}/${nextRoute.split('.').slice(0, -1).join('.')}`;
+    const formatedValue = `./manager/${nextDir}/${nextRoute}`;
+
+    return {
+      ...routesFormated,
+      ...{[formatedKey]: formatedValue}
+    }
+  }, {});
+
+  return {...newRoutes, ...nextDirRoutesFormated};
+}, {});
+
+const allPages = {...oldPagesEntries, ...newPagesEntries};
+
+Object.keys(allPages).forEach((key) => {
+  allPages["javascript/manager/" + key] = allPages[key];
+  delete allPages[key]
 });
 
 module.exports = {
-  pages
+  pages: allPages
 }
