a4c8bda008817081e9e648c298dd2c60bc784b19
Marcelo Chiaradia <mchiaradia@suse.com>
2019-01-22 11:07:02 +0100

    Synchronous calls: use existing batching support

--- java/code/src/com/suse/manager/webui/services/impl/SaltService.java
+++ java/code/src/com/suse/manager/webui/services/impl/SaltService.java
@@ -93,6 +93,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.Set;
@@ -743,12 +744,13 @@ public class SaltService {
         }
 
         if (!regularMinionIds.isEmpty()) {
+            List<Map<String, Result<T>>> callResult =
+                    adaptException(call.callSync(SALT_CLIENT,
+                            new MinionList(regularMinionIds), PW_AUTH, defaultBatch));
             results.putAll(
-                adaptException(
-                    call.callSync(SALT_CLIENT,
-                            new MinionList(regularMinionIds),
-                            PW_AUTH)
-                )
+                    callResult.stream().flatMap(map -> map.entrySet().stream())
+                            .collect(Collectors.toMap(Entry<String, Result<T>>::getKey,
+                                    Entry<String, Result<T>>::getValue))
             );
         }
 
