bd73c235d102ba1cf2730d6d8cea631533d8efb8
Bo Maryniuk <bo@suse.de>
2019-01-29 16:44:34 +0100

    Implement credentials getter

--- backend/satellite_tools/ulnauth.py
+++ backend/satellite_tools/ulnauth.py
@@ -2,6 +2,8 @@
 """
 Oracle ULN (Unbreakable Linux Network) authentication library.
 """
+import os
+import configparser
 import urllib.parse
 
 
@@ -24,8 +26,29 @@ class ULNAuth:
         :returns: tuple (hostname, label)
         """
         if not url.startswith("uln://"):
-            RhnSyncException("URL must start with 'uln://'.")
+            raise RhnSyncException("URL must start with 'uln://'.")
         p_url = urllib.parse.urlparse(url)
-        return p_url.netloc or self.DEFAULT_ULN_HOST, p_url.path
+        return p_url.netloc or self.ULN_DEFAULT_HOST, p_url.path
 
-    
+    def _get_credentials(self) -> tuple:
+        """
+        Get credentials from the uln.conf
+
+        :raises AssertionError: if configuration does not contain required sections.
+        :returns: tuple of username and password
+        """
+        if not os.path.exists(self.ULN_CONF_PATH):
+            raise RhnSyncException("'{}' does not exists".format(self.ULN_CONF_PATH))
+        elif not os.access(self.ULN_CONF_PATH, os.R_OK):
+            raise RhnSyncException("Permission denied to '{}'".format(self.ULN_CONF_PATH))
+
+        config = configparser.ConfigParser()
+        config.read(self.ULN_CONF_PATH)
+        if "main" in config:
+            sct = config["main"]
+            username, password = sct.get("username"), sct.get("password")
+        else:
+            username = password = None
+        assert username is not None and password is not None, "Credentials were not found in the configuration"
+
+        return username, password
