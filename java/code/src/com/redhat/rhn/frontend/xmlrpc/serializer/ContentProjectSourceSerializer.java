1f3b736ac7a09f4413cefbe02471c24dba782680
Frantisek Kobzik <fkobzik@suse.de>
2019-01-31 16:44:47 +0100

    XMLRPC: Allow attaching/detaching Sources to/from Project

---
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/serializer/ContentProjectSourceSerializer.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2019 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.redhat.rhn.frontend.xmlrpc.serializer;
+
+import com.redhat.rhn.domain.contentmgmt.ProjectSource;
+import com.redhat.rhn.frontend.xmlrpc.serializer.util.SerializerHelper;
+import redstone.xmlrpc.XmlRpcException;
+import redstone.xmlrpc.XmlRpcSerializer;
+
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * Serializer for {@link com.redhat.rhn.domain.contentmgmt.ProjectSource} class and subclasses
+ */
+public class ContentProjectSourceSerializer extends RhnXmlRpcCustomSerializer {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Class getSupportedClass() {
+        return ProjectSource.class;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void doSerialize(Object obj, Writer writer, XmlRpcSerializer serializer)
+            throws XmlRpcException, IOException {
+        ProjectSource source = (ProjectSource) obj;
+
+        SerializerHelper helper = new SerializerHelper(serializer);
+        helper.add("contentProjectLabel", source.getContentProject().getLabel());
+        helper.add("type", ProjectSource.Type.lookupBySourceClass(source.getClass()).getLabel());
+        source.asSoftwareSource().ifPresent(s -> {
+            helper.add("channelLabel", s.getChannel().getLabel());
+        });
+        helper.writeTo(writer);
+    }
+}
