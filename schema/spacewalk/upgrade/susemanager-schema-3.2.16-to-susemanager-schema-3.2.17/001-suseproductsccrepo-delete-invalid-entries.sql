5130779236212829a4ebf8b09a51d05c52014263
Kevin Walter <kwalter@suse.de>
2019-02-20 15:36:47 +0100

    removing invalid suse-openstack-cloud-6 and suse-packagehub-12-sp4 channel_labels (bsc#1125451)

---
+++ schema/spacewalk/upgrade/susemanager-schema-3.2.16-to-susemanager-schema-3.2.17/001-suseproductsccrepo-delete-invalid-entries.sql
@@ -0,0 +1,14 @@
+delete from suseproductsccrepository where channel_label = 'suse-openstack-cloud-6-pool-x86_64-sp1';
+delete from suseproductsccrepository where channel_label = 'suse-openstack-cloud-6-updates-x86_64-sp1';
+delete from suseproductsccrepository where channel_label = 'suse-openstack-cloud-6-debuginfo-pool-x86_64-sp1';
+delete from suseproductsccrepository where channel_label = 'suse-openstack-cloud-6-debuginfo-updates-x86_64-sp1';
+
+delete from suseproductsccrepository where channel_label = 'suse-packagehub-12-sp4-aarch64';
+delete from suseproductsccrepository where channel_label = 'suse-packagehub-12-sp4-aarch64-hpc-sp4';
+delete from suseproductsccrepository where channel_label = 'suse-packagehub-12-sp4-ppc64le';
+delete from suseproductsccrepository where channel_label = 'suse-packagehub-12-sp4-ppc64le-sap-sp4';
+delete from suseproductsccrepository where channel_label = 'suse-packagehub-12-sp4-s390x';
+delete from suseproductsccrepository where channel_label = 'suse-packagehub-12-sp4-x86_64';
+delete from suseproductsccrepository where channel_label = 'suse-packagehub-12-sp4-x86_64-sled-sp4';
+delete from suseproductsccrepository where channel_label = 'suse-packagehub-12-sp4-x86_64-sap-sp4';
+delete from suseproductsccrepository where channel_label = 'suse-packagehub-12-sp4-x86_64-hpc-sp4';
