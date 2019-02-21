38f4a2295bf79765e11d9fa70122d469777db1dd
Luis Neves <luneves@suse.de>
2019-02-21 22:29:27 +0000

    add "yarn autoclean" and better error handling for .sh scripts

--- susemanager-frontend/susemanager-nodejs-sdk-devel/setup.sh
+++ susemanager-frontend/susemanager-nodejs-sdk-devel/setup.sh
@@ -1,3 +1,6 @@
+set -euxo pipefail
 (cd susemanager-frontend/susemanager-nodejs-sdk-devel; rm -rf node_modules)
 yarn install --frozen-lockfile
+(cd susemanager-frontend/susemanager-nodejs-sdk-devel; yarn zip)
+yarn autoclean --force
 echo "susemanager-nodejs-modules.tar.gz"
