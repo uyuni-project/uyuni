e7ff9679befd0e253a90028e61647e988c2b5177
Luis Neves <luneves@suse.de>
2019-02-25 18:07:03 +0000

    Create project page implemented

--- web/html/src/manager/content-management/create-project/create-project.js
+++ web/html/src/manager/content-management/create-project/create-project.js
@@ -1,22 +1,53 @@
 // @flow
-import React, { Component } from 'react';
+// $FlowFixMe  // upgrade flow
+import React, { useState } from 'react';
 import { hot } from 'react-hot-loader/root';
+import useProjectActionsApi from "../shared/api/use-project-actions-api";
+import {TopPanel} from "components/panels/TopPanel";
+import TopPanelButtons from "./top-panel-buttons";
+import PropertiesCreate from "../shared/components/panels/properties/properties-create";
+import {handlePropertiesChange} from "../shared/state/project/project.state";
+import {showErrorToastr} from "components/toastr/toastr";
 
-type Props = {
-};
 
-type State = {
-};
+const CreateProject = () => {
 
-// eslint-disable-next-line react/prefer-stateless-function
-class CreateProject extends Component<Props, State> {
-  render() {
-    return (
-      <div>
-        Create Project
-      </div>
-    );
-  }
+  const [project, setProject] = useState({
+      properties: {
+        label: null,
+        name: null,
+        description: null,
+        historyEntries: [],
+      },
+  });
+  const { onAction } = useProjectActionsApi({});
+
+  return (
+          <TopPanel
+            title={t('Create Project')}
+            icon="fa-plus"
+            button={
+              <TopPanelButtons
+                onCreate={() =>
+                  onAction(project, "create")
+                    .then(
+                      (data) => {
+                        window.location.href = `/rhn/manager/contentmanagement/project/${project.properties.label}`
+                      }
+                    )
+                    .catch((error) => {
+                      showErrorToastr(error);
+                    })
+                }
+              />
+            }
+          >
+            <PropertiesCreate
+              properties={project.properties}
+              onChange={(newProperties) => setProject(handlePropertiesChange(project, newProperties))}
+            />
+          </TopPanel>
+  )
 }
 
 export default hot(CreateProject);
