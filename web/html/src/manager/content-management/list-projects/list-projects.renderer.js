9d4c94cf4a2ea090051416ab90667bc4377bee22
Luis Neves <luneves@suse.de>
2019-02-25 17:06:53 +0000

    Implement listing of projects

--- web/html/src/manager/content-management/list-projects/list-projects.renderer.js
+++ web/html/src/manager/content-management/list-projects/list-projects.renderer.js
@@ -5,9 +5,18 @@ import ListProjects from './list-projects';
 window.pageRenderers = window.pageRenderers || {};
 window.pageRenderers.contentManagement = window.pageRenderers.contentManagement || {};
 window.pageRenderers.contentManagement.listProjects = window.pageRenderers.contentManagement.listProjects || {};
-window.pageRenderers.contentManagement.listProjects.renderer = (id) => {
+window.pageRenderers.contentManagement.listProjects.renderer = (id, {projects, flashMessage}) => {
+
+  let projectsJson = [];
+  try{
+    projectsJson = JSON.parse(projects);
+  }  catch(error) {}
+
   ReactDOM.render(
-    <ListProjects />,
-    document.getElementById(id),
-  );
+      <ListProjects
+        projects={projectsJson}
+        flashMessage={flashMessage}
+      />,
+      document.getElementById(id),
+    );
 };
