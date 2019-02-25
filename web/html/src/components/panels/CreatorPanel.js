661bde25ddf2f147358729afa30cdd3731f933cf
Luis Neves <luneves@suse.de>
2019-02-25 18:06:11 +0000

    New panel to support the creation of items in Modals

---
+++ web/html/src/components/panels/CreatorPanel.js
@@ -0,0 +1,132 @@
+// @flow
+// $FlowFixMe  // upgrade flow
+import React, {useState, useEffect} from 'react';
+import {Panel} from "./Panel";
+import {ModalLink} from "../dialog/ModalLink";
+import {Dialog, closeDialog} from "../dialog/Dialog";
+import {Button} from "../buttons";
+
+type Props = {
+  id: string,
+  title: string,
+  creatingText: string,
+  panelLevel: string,
+  onSave: Function,
+  renderCreationContent: Function,
+  renderContent: Function,
+  className?: string,
+  onCancel?: Function,
+  onOpen?: Function,
+  onDelete?: Function,
+  disableOperations?: boolean,
+  collapsible?: boolean,
+};
+
+const panelLevels = {
+  "1": "h1",
+  "2": "h2",
+  "3": "h3",
+  "4": "h4",
+}
+
+const CreatorPanel = (props: Props) => {
+
+  const [open, setOpen] = useState(false);
+  const [item, setItem] = useState({});
+
+  const setStateItem = (item: Object) => setItem({...item});
+  const modalNameId = `${props.id}-modal`;
+  const panelCollapseId = props.collapsible && `${props.id}-panel`;
+
+  // We could delegate this to react, but it would be quite a challenge to integrate it with the bootstrap animations.
+  useEffect(() => {
+    $('#' + modalNameId).on('shown.bs.modal', function () {
+      $('#' + modalNameId + ' :input:visible:enabled:first').focus();
+    })
+  }, [])
+
+  return (
+    <React.Fragment>
+      <Panel
+        headingLevel={panelLevels[props.panelLevel]}
+        collapseId={panelCollapseId}
+        title={props.title}
+        className={props.className}
+        buttons={
+          <ModalLink
+            id={`${props.id}-modal-link`}
+            icon="fa-plus"
+            className="btn-link"
+            text={props.creatingText}
+            target={modalNameId}
+            onClick={() => {
+              setOpen(true)
+              props.onOpen && props.onOpen({setItem: setStateItem})
+            }}
+          />
+        }>
+
+        {props.renderContent()}
+
+      </Panel>
+
+
+      <Dialog id={modalNameId}
+              title={props.title}
+              closableModal={false}
+              content={
+                props.renderCreationContent({
+                  open,
+                  item,
+                  setItem: setStateItem
+                })
+              }
+              onClosePopUp={() => setOpen(false)}
+              buttons={
+                <React.Fragment>
+                  <div className="btn-group col-lg-6">
+                    {
+                      props.onDelete && <Button
+                        id={`${props.id}-modal-delete-button`}
+                        className="btn-danger"
+                        text={t('Delete')}
+                        disabled={props.disableOperations}
+                        handler={() => props.onDelete && props.onDelete({
+                          item,
+                          closeDialog: () => closeDialog(modalNameId)
+                        })}
+                      />
+                    }
+                  </div>
+                  <div className="col-lg-6">
+                    <div className="pull-right btn-group">
+                      <Button
+                        id={`${props.id}-modal-cancel-button`}
+                        className="btn-default"
+                        text={t('Cancel')}
+                        handler={() => {
+                          if (props.onCancel) {
+                            props.onCancel();
+                          }
+                          closeDialog(modalNameId);
+                        }}
+                      />
+                      <Button
+                        id={`${props.id}-modal-save-button`}
+                        className="btn-primary"
+                        text={t('Save')}
+                        disabled={props.disableOperations}
+                        handler={() => props.onSave({
+                          item,
+                          closeDialog: () => closeDialog(modalNameId)
+                        })}
+                      />
+                    </div>
+                  </div>
+                </React.Fragment>
+              } />
+    </React.Fragment>
+  );
+};
+
+export default CreatorPanel;
