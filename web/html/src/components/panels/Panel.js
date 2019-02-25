99e2f21308d4c3fd46481606ea70966330d3424f
Luis Neves <luneves@suse.de>
2019-02-25 18:04:36 +0000

    Support for collapsible panels

--- web/html/src/components/panels/Panel.js
+++ web/html/src/components/panels/Panel.js
@@ -2,7 +2,9 @@ const React = require('react');
 
 type Props = {
   headingLevel: string,
+  collapseId?: string,
   title?: string,
+  className?: string,
   icon?: string,
   header?: string,
   footer?: string,
@@ -12,39 +14,64 @@ type Props = {
 
 function Panel(props: Props) {
   const { headingLevel: HeadingLevel } = props;
+
+  const titleContent = props.title && <React.Fragment>
+    { props.icon && <i className={`fa ${props.icon}`} /> }
+    { props.title }
+  </React.Fragment>
+
+  const bodyContent = <React.Fragment>
+    <div className="panel-body">
+      { props.children }
+    </div>
+    { props.footer
+      && (
+        <div className="panel-footer">
+          {props.footer}
+        </div>
+      )
+    }
+  </React.Fragment>
+
+
+  //TODO [LN] REVIEW 5PX retirado!!!!
   return (
-    <div className="panel panel-default">
+    <div className={"panel " + (props.className ? props.className : "panel-default")}>
       {(props.title || props.header || props.buttons)
         && (
-        <div className="panel-heading">
+        <div className="panel-heading accordion-toggle">
           { props.buttons
             && (
-              <div className="pull-right btn-group" style={{ top: '-5px' }}>
+              <div className="pull-right btn-group" style={{ paddingLeft: '0px 10px 0px 100px' }}>
                 { props.buttons }
               </div>
             )
           }
-          { props.title
-            && (
-              <HeadingLevel>
-                { props.icon && <i className={`fa ${props.icon}`} /> }
-                { props.title }
-              </HeadingLevel>
-            )
+          {
+            <HeadingLevel>
+              {
+                props.collapseId ?
+                  <div data-toggle="collapse" href={`#${props.collapseId}-panel-closable`} className="accordion-toggle">
+                    <i className="fa fa-chevron-right show-on-collapsed" />
+                    <i className="fa fa-chevron-down hide-on-collapsed" />
+                    {titleContent}
+                  </div>
+                  : titleContent
+              }
+            </HeadingLevel>
           }
           { props.header && <span>{props.header}</span>}
         </div>)
       }
-      <div className="panel-body">
-        { props.children }
-      </div>
-      { props.footer
-        && (
-          <div className="panel-footer">
-            {props.footer}
+
+      {
+        props.collapseId ?
+          <div id={`${props.collapseId}-panel-closable`} className="panel-collapse collapse in">
+            {bodyContent}
           </div>
-        )
+          : bodyContent
       }
+
     </div>
   );
 }
