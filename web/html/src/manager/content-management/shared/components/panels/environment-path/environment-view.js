dff736d640a2246a8819175fa23a70ce57465c28
Luis Neves <luneves@suse.de>
2019-02-25 18:09:21 +0000

    Environments edition implemented

---
+++ web/html/src/manager/content-management/shared/components/panels/environment-path/environment-view.js
@@ -0,0 +1,32 @@
+//@flow
+import React from 'react';
+
+type Props = {
+  environment: Object,
+}
+
+// $FlowFixMe  // upgrade flow
+const EnvironmentView = React.memo((props: Props) => {
+  return (
+    <React.Fragment>
+      <dl className="row">
+        <dt className="col-xs-3">Description:</dt>
+        <dd className="col-xs-9">{props.environment.description}</dd>
+      </dl>
+      <dl className="row">
+        <dt className="col-xs-3">Registered Systems:</dt>
+        <dd className="col-xs-9">{0}</dd>
+      </dl>
+      <dl className="row">
+        <dt className="col-xs-3">Version:</dt>
+        <dd className="col-xs-9">{props.environment.version || "not built"}</dd>
+      </dl>
+      <dl className="row">
+        <dt className="col-xs-3">Compilation:</dt>
+        <dd className="col-xs-9"><i className="fa fa-times-circle"></i></dd>
+      </dl>
+    </React.Fragment>
+  );
+});
+
+export default EnvironmentView;
