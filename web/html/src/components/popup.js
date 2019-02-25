ccb377be3a809cb98dd1d8b7cdb2b40098f21c50
Luis Neves <luneves@suse.de>
2019-02-25 17:12:59 +0000

    Support unclosable modals

--- web/html/src/components/popup.js
+++ web/html/src/components/popup.js
@@ -5,31 +5,40 @@ const PropTypes = require('prop-types');
 const React = require("react");
 
 class PopUp extends React.Component {
-  static propTypes = {
-    id: PropTypes.string.isRequired, // the id of the html div tag
-    onClosePopUp: PropTypes.func, // a callback function with no parameters
-    className: PropTypes.string, // the css className for the 'modal-dialog' div
-    title: PropTypes.string,
-    content: PropTypes.node, // the body of the popup
-    footer: PropTypes.node,
-    hideHeader: PropTypes.bool, // Whether to show the title and close button
-  };
 
   componentDidMount() {
     $("#" + this.props.id).on("hidden.bs.modal", this.props.onClosePopUp);
   }
 
+  close() {
+    $("#" + this.props.id).modal().hide();
+  }
+
   render() {
+
+    let bootStrapModalOptionalProps = {};
+
+    if(!this.props.closableModal) {
+      bootStrapModalOptionalProps = {
+        ...bootStrapModalOptionalProps,
+        "data-backdrop": "static",
+        "data-keyboard": "false"
+      }
+    }
+
     return (
-      <div className="modal fade" tabIndex="-1" role="dialog" id={this.props.id}>
+      <div className="modal fade" tabIndex="-1" role="dialog" id={this.props.id} {...bootStrapModalOptionalProps}>
         <div className={"modal-dialog " + (this.props.className ? this.props.className : "")}>
           <div className="modal-content">
             {!this.props.hideHeader
               && (
                 <div className="modal-header">
-                  <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
+                  {
+                    this.props.closableModal &&
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  }
                   {this.props.title ? <h4 className="modal-title">{this.props.title}</h4> : null}
                 </div>
               )
@@ -43,6 +52,21 @@ class PopUp extends React.Component {
   }
 }
 
+PopUp.propTypes = {
+  id: PropTypes.string.isRequired, // the id of the html div tag
+  onClosePopUp: PropTypes.func, // a callback function with no parameters
+  className: PropTypes.string, // the css className for the 'modal-dialog' div
+  title: PropTypes.string,
+  content: PropTypes.node, // the body of the popup
+  footer: PropTypes.node,
+  closableModal: PropTypes.bool,
+  hideHeader: PropTypes.bool,
+};
+
+PopUp.defaultProps = {
+  closableModal: true
+}
+
 module.exports = {
-    PopUp : PopUp
+  PopUp : PopUp
 }
