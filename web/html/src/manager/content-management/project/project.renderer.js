62bf5b0f9578efd9481fed642fc0268774398e0c
Luis Neves <luneves@suse.de>
2019-02-25 18:08:32 +0000

    Project edition page structure

--- web/html/src/manager/content-management/project/project.renderer.js
+++ web/html/src/manager/content-management/project/project.renderer.js
@@ -5,9 +5,17 @@ import Project from './project';
 window.pageRenderers = window.pageRenderers || {};
 window.pageRenderers.contentManagement = window.pageRenderers.contentManagement || {};
 window.pageRenderers.contentManagement.project = window.pageRenderers.contentManagement.project || {};
-window.pageRenderers.contentManagement.project.renderer = (id) => {
+window.pageRenderers.contentManagement.project.renderer = (id, {project, wasFreshlyCreatedMessage} = {}) => {
+  let projectJson = {};
+  try{
+    projectJson = JSON.parse(project);
+  }  catch(error) {}
+
   ReactDOM.render(
-    <Project />,
+    <Project
+      project={projectJson}
+      { ...( wasFreshlyCreatedMessage && { wasFreshlyCreatedMessage } ) }
+    />,
     document.getElementById(id),
   );
 };
