4d1ff2ceddae4ba00f5795c20eeba540cc15f7a7
Luis Neves <luneves@suse.de>
2019-02-25 18:08:56 +0000

    Properties edition in a modal implemented

---
+++ web/html/src/manager/content-management/shared/components/panels/properties/properties-view.js
@@ -0,0 +1,56 @@
+// @flow
+import React from 'react';
+
+type Props = {
+  properties: Object,
+}
+
+const PropertiesView= (props: Props) => {
+
+  //TODO: CORY HOUSE -> DATA FROM API -> normalize and error handling?
+  const historyEntries = props.properties.historyEntries || [];
+  const sortVersionsHistoryBackwards = [...historyEntries.sort((a, b) => b.version - a.version)]
+
+  return (
+    <div>
+      <React.Fragment>
+        <dl className="row">
+          <dt className="col-xs-2">Label:</dt>
+          <dd className="col-xs-6">{props.properties.label}</dd>
+        </dl>
+        <dl className="row">
+          <dt className="col-xs-2">Name</dt>
+          <dd className="col-xs-10">{props.properties.name}</dd>
+        </dl>
+        <dl className="row">
+          <dt className="col-xs-2">Description</dt>
+          <dd className="col-xs-10">{props.properties.description}</dd>
+        </dl>
+        <dl className="row">
+          <dt className="col-xs-2">Versions history:</dt>
+          <dd className="col-xs-10">
+            <ul className="list-unstyled">
+              {
+                sortVersionsHistoryBackwards.map((history, index) => {
+                  const versionMessage = `Version ${history.version}: ${history.message}`
+                  return (
+                    <li>
+                      {
+                        index === 0
+                          ? <strong>{versionMessage}</strong>
+                          : versionMessage
+                      }
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </dd>
+        </dl>
+      </React.Fragment>
+    </div>
+  );
+};
+
+
+export default PropertiesView;
