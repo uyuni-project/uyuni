3265d123083f02f3a20aee04504853eaee02ceeb
Luis Neves <luneves@suse.de>
2019-02-25 18:08:56 +0000

    Properties edition in a modal implemented

---
+++ web/html/src/manager/content-management/shared/components/panels/properties/properties-edit.js
@@ -0,0 +1,63 @@
+// @flow
+import React from 'react';
+import CreatorPanel from "../../../../../../components/panels/CreatorPanel";
+import PropertiesForm from "./properties-form";
+import {Loading} from "components/loading/loading";
+import useProjectActionsApi from "../../../api/use-project-actions-api";
+import {showErrorToastr, showSuccessToastr} from "components/toastr/toastr";
+import PropertiesView from "./properties-view";
+
+type Props = {
+  projectId: string,
+  properties: Object,
+  onChange: Function,
+};
+
+const PropertiesEdit = (props: Props) => {
+  const {onAction, cancelAction, isLoading} = useProjectActionsApi({
+    projectId: props.projectId, nestedResource:"properties"
+  });
+
+  return (
+      <CreatorPanel
+        id="properties"
+        creatingText="Edit"
+        panelLevel="2"
+        title={t('Project Properties')}
+        collapsible
+        onOpen={({ setItem }) => setItem(props.properties)}
+        disableOperations={isLoading}
+        onSave={({ item, closeDialog }) => {
+          return onAction(item, "update")
+            .then((editedProject) => {
+              closeDialog();
+              showSuccessToastr(t("Project properties updated successfully"));
+              props.onChange(editedProject)
+            })
+            .catch((error) => {
+              showErrorToastr(error);
+            })
+        }}
+        onCancel={() => cancelAction()}
+        renderContent={() => <PropertiesView properties={props.properties}/>}
+        renderCreationContent={({ open, item, setItem }) => {
+
+          if (isLoading) {
+            return (
+              <Loading text='Editing properties..'/>
+            )
+          }
+
+          return (
+            <PropertiesForm
+              properties={item}
+              onChange={(editedProperties) => setItem(editedProperties)}
+              editing
+            />
+          )
+        }}
+      />
+  );
+}
+
+export default PropertiesEdit;
