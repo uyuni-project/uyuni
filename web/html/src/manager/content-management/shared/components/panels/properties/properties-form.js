d896e758fb958788f1ed7dc0d770c207995c102a
Luis Neves <luneves@suse.de>
2019-02-25 18:07:03 +0000

    Create project page implemented

---
+++ web/html/src/manager/content-management/shared/components/panels/properties/properties-form.js
@@ -0,0 +1,50 @@
+// @flow
+import React from 'react';
+
+import {Form} from "components/input/Form";
+import {Text} from "components/input/Text";
+
+type Props = {
+  properties: Object,
+  onChange: Function,
+  editing?: boolean
+};
+
+const PropertiesForm = (props: Props) => (
+  <Form
+    model={props.properties}
+    onChange={model => {
+      props.onChange(model);
+    }}
+  >
+    <div className="row">
+      <Text
+        required
+        name="label"
+        label={t("Label")}
+        labelClass="col-md-2"
+        divClass="col-md-10"
+        disabled={props.editing}
+      />
+    </div>
+    <div className="row">
+      <Text
+        required
+        name="name"
+        label={t("Name")}
+        labelClass="col-md-2"
+        divClass="col-md-10"
+      />
+    </div>
+    <div className="row">
+      <Text
+        name="description"
+        label={t("Description")}
+        labelClass="col-md-2"
+        divClass="col-md-10"
+      />
+    </div>
+  </Form>
+);
+
+export default PropertiesForm;
