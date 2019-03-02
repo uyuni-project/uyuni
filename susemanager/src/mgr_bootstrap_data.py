a04795eff7378e86f5d8cebbb0a37b8053ec5964
Michael Calmer <mc@suse.de>
2019-03-02 14:14:14 +0100

    add bootstrap repo definition for SLE15 SP1

--- susemanager/src/mgr_bootstrap_data.py
+++ susemanager/src/mgr_bootstrap_data.py
@@ -650,6 +650,22 @@ DATA = {
         'PDID' : [1576, 1712], 'PKGLIST' : PKGLIST15 + PKGLIST15_NO_Z,
         'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/0/bootstrap/'
     },
+    'SLE-15-SP1-aarch64' : {
+        'PDID' : [1769, 1709], 'PKGLIST' : PKGLIST15 + PKGLIST15_NO_Z,
+        'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/1/bootstrap/'
+    },
+    'SLE-15-SP1-ppc64le' : {
+        'PDID' : [1770, 1710], 'PKGLIST' : PKGLIST15 + PKGLIST15_NO_Z,
+        'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/1/bootstrap/'
+    },
+    'SLE-15-SP1-s390x' : {
+        'PDID' : [1771, 1711], 'PKGLIST' : PKGLIST15,
+        'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/1/bootstrap/'
+    },
+    'SLE-15-SP1-x86_64' : {
+        'PDID' : [1772, 1712], 'PKGLIST' : PKGLIST15 + PKGLIST15_NO_Z,
+        'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/1/bootstrap/'
+    },
     'openSUSE-Leap-42.3-x86_64' : {
         'BASECHANNEL' : 'opensuse_leap42_3-x86_64', 'PKGLIST' : PKGLIST12 + ONLYOPENSUSE42 + ENHANCE12SP1,
         'DEST' : '/srv/www/htdocs/pub/repositories/opensuse/42/3/bootstrap/'
