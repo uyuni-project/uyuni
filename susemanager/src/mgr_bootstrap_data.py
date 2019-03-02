03a38c8e6e16da1f5b1b22c439ebaedb693ef911
Michael Calmer <mc@suse.de>
2019-03-02 14:14:51 +0100

    move python-setuptools to ENHANCE12SP1 group
    does not exist for SLES12 GA

--- susemanager/src/mgr_bootstrap_data.py
+++ susemanager/src/mgr_bootstrap_data.py
@@ -108,7 +108,6 @@ PKGLIST12 = [
                 "python",
                 "python-cffi",
                 "python-cryptography",
-                "python-setuptools",
                 "python-dmidecode",
                 "python-ethtool",
                 "python-gobject2",
@@ -183,6 +182,7 @@ ENHANCE12SP1 = [
                 "python-enum34",
                 "python-idna",
                 "python-ipaddress",
+                "python-setuptools",
                ]
 
 RES6 = [
