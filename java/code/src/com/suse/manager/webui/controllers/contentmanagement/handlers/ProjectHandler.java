dfc00541d6364fa52f5d8d3965979308785bf585
Luis Neves <luneves@suse.de>
2019-03-12 02:45:28 +0000

    Java linting / javadoc

--- java/code/src/com/suse/manager/webui/controllers/contentmanagement/handlers/ProjectHandler.java
+++ java/code/src/com/suse/manager/webui/controllers/contentmanagement/handlers/ProjectHandler.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2019 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package com.suse.manager.webui.controllers.contentmanagement.handlers;
 
 import com.suse.manager.webui.controllers.contentmanagement.request.ProjectPropertiesRequest;
@@ -15,9 +29,19 @@ import java.util.HashMap;
 import spark.Request;
 import spark.Spark;
 
+/**
+ * Utility class to help the handling of the ProjectApiController
+ */
 public class ProjectHandler {
     private static final Gson GSON = Json.GSON;
 
+    private ProjectHandler() { }
+
+    /**
+     * map request into the project properties  request bean
+     * @param req the http request
+     * @return project properties request bean
+     */
     public static ProjectPropertiesRequest getProjectPropertiesRequest(Request req) {
         try {
             return GSON.fromJson(req.body(), ProjectPropertiesRequest.class);
@@ -27,6 +51,11 @@ public class ProjectHandler {
         }
     }
 
+    /**
+     * map  request into the project request bean
+     * @param req the http request
+     * @return project request bean
+     */
     public static ProjectRequest getProjectRequest(Request req) {
         try {
             return GSON.fromJson(req.body(), ProjectRequest.class);
@@ -36,6 +65,11 @@ public class ProjectHandler {
         }
     }
 
+    /**
+     * map validate project properties request bean
+     * @param projPropsRequest the project properties request bean
+     * @return validation errors
+     */
     public static HashMap<String, String> validateProjectPropertiesRequest(ProjectPropertiesRequest projPropsRequest) {
         HashMap<String, String> requestErrors = new HashMap<>();
 
@@ -50,6 +84,11 @@ public class ProjectHandler {
         return requestErrors;
     }
 
+    /**
+     * map validate project request bean
+     * @param projectRequest the project request bean
+     * @return validation errors
+     */
     public static HashMap<String, String> validateProjectRequest(ProjectRequest projectRequest) {
         HashMap<String, String> requestErrors = new HashMap<>();
 
--- java/code/src/com/suse/manager/webui/controllers/contentmanagement/response/EnvironmentResponse.java
+++ java/code/src/com/suse/manager/webui/controllers/contentmanagement/response/EnvironmentResponse.java
@@ -1,7 +1,22 @@
+/**
+ * Copyright (c) 2019 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package com.suse.manager.webui.controllers.contentmanagement.response;
 
-import java.util.Optional;
-
+/**
+ * JSON response wrapper for an environment of a content project.
+ */
 public class EnvironmentResponse {
 
     private String projectLabel;
@@ -9,19 +24,19 @@ public class EnvironmentResponse {
     private String name;
     private String description;
 
-    public void setProjectLabel(String projectLabel) {
-        this.projectLabel = projectLabel;
+    public void setProjectLabel(String projectLabelIn) {
+        this.projectLabel = projectLabelIn;
     }
 
-    public void setLabel(String label) {
-        this.label = label;
+    public void setLabel(String labelIn) {
+        this.label = labelIn;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String nameIn) {
+        this.name = nameIn;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setDescription(String descriptionIn) {
+        this.description = descriptionIn;
     }
 }
