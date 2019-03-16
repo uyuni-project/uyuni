fdd2546ceb4c47b4705cd9e532cae4705bae6fc2
Michael Calmer <mc@suse.de>
2019-03-16 16:49:05 +0100

    Use activation key with test repo
    This is to prevent using default repo (SLES12 SP3...) which
    are only half synced at this point in time.
    Using them cause any kind of problems like
    
      Valid metadata not found at specified URL
    
    or using of packages which are not yet synced.

--- testsuite/features/step_definitions/xmlrpc_common.rb
+++ testsuite/features/step_definitions/xmlrpc_common.rb
@@ -23,7 +23,7 @@ end
 When(/^I call system\.bootstrap\(\) on host "([^"]*)" and salt\-ssh "([^"]*)"$/) do |host, salt_ssh_enabled|
   system_name = get_system_name(host)
   salt_ssh = (salt_ssh_enabled == 'enabled')
-  result = systest.bootstrap_system(system_name, '', salt_ssh)
+  result = systest.bootstrap_system(system_name, '1-SUSE-DEV-x86_64', salt_ssh)
   assert(result == 1, 'Bootstrap return code not equal to 1.')
 end
 
