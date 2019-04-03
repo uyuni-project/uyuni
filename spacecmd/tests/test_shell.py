476a1e1917dea6efd4093ed8539912594f4bdfa5
Bo Maryniuk <bo@suse.de>
2019-04-03 16:44:36 +0200

    add unit test for delimiters control

--- spacecmd/tests/test_shell.py
+++ spacecmd/tests/test_shell.py
@@ -20,3 +20,28 @@ class TestSCShell:
         Test history length.
         """
         assert SpacewalkShell.HISTORY_LENGTH == 1024
+
+    @patch("spacecmd.shell.atexit", MagicMock())
+    @patch("spacecmd.shell.readline.get_completer_delims",
+           MagicMock(return_value=readline.get_completer_delims()))
+    @patch("spacecmd.shell.sys.exit", MagicMock())
+    def test_shell_delimeters(self):
+        """
+        Test shell delimieters are set without hyphens
+        or colons during the tab completion.
+        """
+        cfg_dir = "/tmp/shell/{}/conf".format(int(time.time()))
+        m_logger = MagicMock()
+
+        cpl_setter = MagicMock()
+        with patch("spacecmd.shell.logging", m_logger) as lgr, \
+            patch("spacecmd.shell.readline.set_completer_delims", cpl_setter):
+            options = MagicMock()
+            options.nohistory = True
+            shell = SpacewalkShell(options, cfg_dir, None)
+
+            assert shell.history_file == "{}/history".format(cfg_dir)
+            assert not m_logger.error.called
+            assert cpl_setter.call_args[0][0] != readline.get_completer_delims()
+            assert cpl_setter.call_args[0][0] == ' \t\n`~!@#$%^&*()=+[{]}\\|;\'",<>?'
+
