bbbdfbd4bdd5e3e754e97357e983b17f7cb8a847
Frantisek Kobzik <fkobzik@suse.de>
2019-04-04 10:51:27 +0200

    Fix: Do not implicitly set parent channel when cloning (bsc#1130492)
    The parent channel should be explicitly set instead.

---
+++ java/code/src/com/redhat/rhn/manager/channel/test/CloneChannelCommandTest.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2019 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.redhat.rhn.manager.channel.test;
+
+import com.redhat.rhn.domain.channel.Channel;
+import com.redhat.rhn.domain.channel.ChannelFactory;
+import com.redhat.rhn.manager.channel.CloneChannelCommand;
+import com.redhat.rhn.testing.BaseTestCaseWithUser;
+import com.redhat.rhn.testing.ChannelTestUtils;
+
+public class CloneChannelCommandTest extends BaseTestCaseWithUser {
+
+    /**
+     * Tests that cloning a channel does NOT respect the parent of the original
+     *
+     * @throws Exception if anything goes wrong
+     */
+    public void testNoParentOnClone() throws Exception {
+        Channel originalBase = ChannelTestUtils.createBaseChannel(user);
+        Channel originalChild = ChannelTestUtils.createChildChannel(user, originalBase);
+        originalChild.setChecksumType(ChannelFactory.findChecksumTypeByLabel("sha256"));
+        originalChild.setSummary("summary");
+        CloneChannelCommand ccc = new CloneChannelCommand(true, originalChild);
+        ccc.setUser(user);
+
+        Channel clone = ccc.create();
+        assertNull(clone.getParentChannel());
+    }
+}
