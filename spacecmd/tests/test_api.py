c618a417ffed193dfb0b016d282949de4ceb540e
Bo Maryniuk <bo@suse.de>
2019-04-05 14:15:29 +0200

    Add UT for checking output option argument

--- spacecmd/tests/test_api.py
+++ spacecmd/tests/test_api.py
@@ -2,8 +2,9 @@
 """
 Test suite for spacecmd.api
 """
-from mock import MagicMock, patch
+from mock import MagicMock, patch, mock_open
 from spacecmd import api
+import helpers
 
 
 class TestSCAPI:
@@ -18,3 +19,25 @@ class TestSCAPI:
         shell.help_api = MagicMock()
         api.do_api(shell, "")
         assert shell.help_api.called
+
+    def test_args_output(self):
+        """
+        Test output option.
+        """
+        shell = MagicMock()
+        shell.help_api = MagicMock()
+        shell.client = MagicMock()
+        shell.client.call = MagicMock(return_value=["one", "two", "three"])
+
+        log = MagicMock()
+        out = helpers.FileHandleMock()
+        with patch("spacecmd.api.open", out, create=True) as mop, \
+             patch("spacecmd.api.logging", log) as mlog:
+            api.do_api(shell, "call -o /tmp/spacecmd.log")
+
+        assert not mlog.warning.called
+        assert out.get_content() == '[\n  "one",\n  "two",\n  "three"\n]'
+        assert out.get_init_kwargs() == {}
+        assert out.get_init_args() == ('/tmp/spacecmd.log', 'w')
+
+        
