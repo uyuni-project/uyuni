1d0abb03b98a4e79ce5eb7ec1c253016eef4f59c
Bo Maryniuk <bo@suse.de>
2019-04-08 14:52:52 +0200

    Add unit test on addentitlements method calls addEntitlements API

--- spacecmd/tests/test_activationkey.py
+++ spacecmd/tests/test_activationkey.py
@@ -249,3 +249,21 @@ class TestSCActivationKeyMethods:
         assert shell.help_activationkey_addentitlements.called
         assert not shell.client.activationkey.addEntitlements.called
 
+    def test_do_activationkey_addentitlements_args(self, shell):
+        """
+        Test "addentitlements" method calls "addEntitlements" API call.
+        """
+        shell.help_activationkey_addentitlements = MagicMock()
+        shell.client = MagicMock()
+        shell.client.activationkey = MagicMock()
+        shell.client.activationkey.addEntitlements = MagicMock()
+
+        spacecmd.activationkey.do_activationkey_addentitlements(shell, "key entitlement")
+        assert not shell.help_activationkey_addentitlements.called
+        assert shell.client.activationkey.addEntitlements.called
+        session, fun, args = shell.client.activationkey.addEntitlements.call_args_list[0][0]
+        assert session == shell.session
+        assert fun == "key"
+        assert isinstance(args, list)
+        assert len(args) == 1
+        assert args == ['entitlement']
