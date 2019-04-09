147050695f9037e1371218eff5e8bc0a04dd849b
Bo Maryniuk <bo@suse.de>
2019-04-09 16:29:24 +0200

    Add unit test for removeconfigchannels command triggers help on insufficient args.

--- spacecmd/tests/test_activationkey.py
+++ spacecmd/tests/test_activationkey.py
@@ -654,3 +654,14 @@ class TestSCActivationKeyMethods:
         spacecmd.activationkey.do_activationkey_removeconfigchannels(shell, "")
         assert shell.help_activationkey_removeconfigchannels.called
         assert not shell.client.activationkey.removeConfigChannels.called
+    
+    def test_do_activationkey_removeconfigchannels_insuff_args(self, shell):
+        """
+        Test removeconfigchannels command triggers help on insufficient args.
+        """
+        shell.help_activationkey_removeconfigchannels = MagicMock()
+        shell.client.activationkey.removeConfigChannels = MagicMock()
+
+        spacecmd.activationkey.do_activationkey_removeconfigchannels(shell, "key")
+        assert shell.help_activationkey_removeconfigchannels.called
+        assert not shell.client.activationkey.removeConfigChannels.called
