1ef6ddfdc77864155bdb0550683c4a65ed4879ae
Bo Maryniuk <bo@suse.de>
2019-04-12 15:34:53 +0200

    Add unit test of do_filepreservation_list no arguments passed.

---
+++ spacecmd/tests/test_filepreservation.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+"""
+Test suite for spacecmd.filepreservation
+"""
+from mock import MagicMock, patch, mock_open
+import spacecmd.filepreservation
+from helpers import shell
+
+
+class TestSCFilePreservation:
+    """
+    Test class for testing spacecmd file preservation.
+    """
+    def test_do_filepreservation_list_return(self, shell):
+        """
+        Test do_filepreservation_list no arguments passed.
+        """
+        shell.client.kickstart.filepreservation.listAllFilePreservations = MagicMock(
+            return_value=[
+                {"name": "list_one"},
+                {"name": "list_two"},
+                {"name": "list_three"},
+            ]
+        )
+
+        mprint = MagicMock()
+        with patch("spacecmd.filepreservation.print", mprint):
+            ret = spacecmd.filepreservation.do_filepreservation_list(shell, "", doreturn=False)
+        assert ret is None
+        assert mprint.call_args_list[0][0][0] == "list_one\nlist_three\nlist_two"
