dca144880ce091ea0ff6858dde36f56da139c3c8
Bo Maryniuk <bo@suse.de>
2019-04-12 16:14:52 +0200

    Add unit test for do_filepreservation_list return data.

--- spacecmd/tests/test_filepreservation.py
+++ spacecmd/tests/test_filepreservation.py
@@ -28,3 +28,21 @@ class TestSCFilePreservation:
             ret = spacecmd.filepreservation.do_filepreservation_list(shell, "", doreturn=False)
         assert ret is None
         assert mprint.call_args_list[0][0][0] == "list_one\nlist_three\nlist_two"
+
+    def test_do_filepreservation_list_return(self, shell):
+        """
+        Test do_filepreservation_list return data.
+        """
+        shell.client.kickstart.filepreservation.listAllFilePreservations = MagicMock(
+            return_value=[
+                {"name": "list_one"},
+                {"name": "list_two"},
+                {"name": "list_three"},
+            ]
+        )
+
+        mprint = MagicMock()
+        with patch("spacecmd.filepreservation.print", mprint):
+            ret = spacecmd.filepreservation.do_filepreservation_list(shell, "", doreturn=True)
+        assert not mprint.called
+        assert ret == ['list_one', 'list_two', 'list_three']
