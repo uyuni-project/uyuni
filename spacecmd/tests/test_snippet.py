6010fea5d49aa1c0fa825ae7577256044c03d1b7
Bo Maryniuk <bo@suse.de>
2019-04-30 14:48:56 +0200

    Add UT for test snippet list with no args

---
+++ spacecmd/tests/test_snippet.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+"""
+Test suite for snippet source
+"""
+from mock import MagicMock, patch, mock_open
+from spacecmd import snippet
+from helpers import shell
+import pytest
+
+
+class TestSCSnippets:
+    """
+    Test for snippet API.
+    """
+    def test_snippet_list_noarg(self, shell):
+        """
+        Test snippet list noargs
+        """
+        snippets = [
+            {"name": "snippet - 3"},
+            {"name": "snippet - 1"},
+            {"name": "snippet - 2"},
+        ]
+
+        mprint = MagicMock()
+        shell.client.kickstart.snippet.listCustom = MagicMock(return_value=snippets)
+        with patch("spacecmd.snippet.print", mprint):
+            out = snippet.do_snippet_list(shell, "")
+
+        assert out is None
+        assert mprint.call_args_list[0][0] == ('snippet - 1\nsnippet - 2\nsnippet - 3',)
+
+    @pytest.mark.skip(reason="Not implemented yet")
+    def test_snippet_list_args(self, shell):
+        """
+        Test snippet list args
+        """
+
+    @pytest.mark.skip(reason="Not implemented yet")
+    def test_snippet_details_noarg(self, shell):
+        """
+        Test snippet details no args
+        """
+
+    @pytest.mark.skip(reason="Not implemented yet")
+    def test_snippet_details_args(self, shell):
+        """
+        Test snippet details args
+        """
