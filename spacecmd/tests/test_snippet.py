4d62cd0dbf383d2b071344fee85e574cf162ce61
Bo Maryniuk <bo@suse.de>
2019-04-30 14:52:49 +0200

    Add unit test when return is expected

--- spacecmd/tests/test_snippet.py
+++ spacecmd/tests/test_snippet.py
@@ -30,11 +30,23 @@ class TestSCSnippets:
         assert out is None
         assert mprint.call_args_list[0][0] == ('snippet - 1\nsnippet - 2\nsnippet - 3',)  # Sorted
 
-    @pytest.mark.skip(reason="Not implemented yet")
     def test_snippet_list_args(self, shell):
         """
-        Test snippet list args
+        Test snippet list with the args
         """
+        snippets = [
+            {"name": "snippet - 3"},
+            {"name": "snippet - 1"},
+            {"name": "snippet - 2"},
+        ]
+
+        mprint = MagicMock()
+        shell.client.kickstart.snippet.listCustom = MagicMock(return_value=snippets)
+        with patch("spacecmd.snippet.print", mprint):
+            out = snippet.do_snippet_list(shell, "", doreturn=True)
+
+        assert out is not None
+        assert out == ['snippet - 1', 'snippet - 2', 'snippet - 3']  # Sorted
 
     @pytest.mark.skip(reason="Not implemented yet")
     def test_snippet_details_noarg(self, shell):
