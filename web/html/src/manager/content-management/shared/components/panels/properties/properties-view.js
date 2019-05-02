b156e53c758c813e50325f0b11a776a797ba836e
Luis Neves <luneves@suse.de>
2019-05-02 14:45:04 +0100

    Properties history - hide if more than 5 and show long list on modal

--- web/html/src/manager/content-management/shared/components/panels/properties/properties-view.js
+++ web/html/src/manager/content-management/shared/components/panels/properties/properties-view.js
@@ -4,15 +4,37 @@ import produce from "immer";
 
 import type {ProjectPropertiesType} from '../../../type/project.type.js';
 import {getVersionMessage} from "./properties.utils";
+import {ModalLink} from "components/dialog/ModalLink";
+import {Dialog} from "components/dialog/Dialog";
 
 type Props = {
   properties: ProjectPropertiesType,
 }
 
-const PropertiesView= (props: Props) => {
+const NUMBER_HISTORY_ENTRIES = 5;
+
+const PropertiesHistoryEntries = (props) =>
+  <ul className="list-unstyled">
+    {
+      props.entries.map((history, index) => {
+        const versionMessage = getVersionMessage(history)
+        return (
+          <li key={`historyentries_${props.id}_${index}`}>
+            {
+              index === 0
+                ? <strong>{versionMessage}</strong>
+                : versionMessage
+            }
+          </li>
+        )
+      })
+    }
+  </ul>
+
+const PropertiesView = (props: Props) => {
 
   let propertiesToShow = produce(props.properties, draftProperties => {
-      draftProperties.historyEntries.sort((a, b) => b.version - a.version)
+    draftProperties.historyEntries.sort((a, b) => b.version - a.version)
   });
 
   return (
@@ -33,22 +55,29 @@ const PropertiesView= (props: Props) => {
         <dl className="row">
           <dt className="col-xs-2">Versions history:</dt>
           <dd className="col-xs-10">
-            <ul className="list-unstyled">
-              {
-                propertiesToShow.historyEntries.map((history, index) => {
-                  const versionMessage = getVersionMessage(history)
-                  return (
-                    <li key={`historyentries_${index}`}>
-                      {
-                        index === 0
-                          ? <strong>{versionMessage}</strong>
-                          : versionMessage
-                      }
-                    </li>
-                  )
-                })
-              }
-            </ul>
+            <PropertiesHistoryEntries
+              id="resume"
+              entries={propertiesToShow.historyEntries.slice(0,NUMBER_HISTORY_ENTRIES)} />
+
+            {
+              propertiesToShow.historyEntries.length > NUMBER_HISTORY_ENTRIES &&
+              <>
+                <ModalLink
+                  id={`properties-longlist-modal-button`}
+                  text="show more"
+                  target="properties-longlist-modal-content"
+                />
+                <Dialog
+                  id="properties-longlist-modal-content"
+                  content={
+                    <PropertiesHistoryEntries
+                      id="longlist"
+                      entries={propertiesToShow.historyEntries} />
+                  }
+                  title="Versions history"
+                />
+              </>
+            }
           </dd>
         </dl>
       </React.Fragment>
