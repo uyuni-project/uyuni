cf5e1fa082086581e07ab5403ff51c9ed1a5b21e
Bo Maryniuk <bo@suse.de>
2019-05-03 16:03:24 +0200

    Add integration test for configuration file is saved to the disk and can be read.

--- utils/tests/test_spacewalk_manage_channel_lifecycle.py
+++ utils/tests/test_spacewalk_manage_channel_lifecycle.py
@@ -2,8 +2,9 @@
 """
 Tests for spacewalk-manager-channel-lifecycle script.
 """
-
+import os
 import pytest
+import tempfile
 from mock import MagicMock, patch
 from . import helpers
 
@@ -33,9 +34,29 @@ class TestSMCL:
         :return:
         """
 
-    def test_configuration_saved(self):
+    def test_configuration_saved_read(self):
         """
-        Test configuration file is saved to the disk.
+        Test configuration file is saved to the disk and can be read.
 
         :return:
         """
+        with tempfile.TemporaryDirectory(prefix="smcl-", dir="/tmp") as tmpdir:
+            smcl.CONF_DIR = os.path.join(tmpdir, ".spacewalk-manage-channel-lifecycle")
+            smcl.USER_CONF_FILE = os.path.join(smcl.CONF_DIR, "settings.conf")
+            smcl.SESSION_CACHE = os.path.join(smcl.CONF_DIR, "session")
+
+            config = smcl.Config(smcl.USER_CONF_FILE)
+            config.set("Millenium Falcon", "space speed", "75 MGLT")
+            config.set("Millenium Falcon", "atmospheric speed", "1050 km/h")
+            smcl.setup_config(config)
+
+            # Save
+            assert os.path.exists(os.path.join(tmpdir, ".spacewalk-manage-channel-lifecycle/settings.conf"))
+
+            r_cfg = smcl.Config(smcl.USER_CONF_FILE)
+            smcl.setup_config(r_cfg)
+
+            assert r_cfg.get("Millenium Falcon", "space speed") == "75 MGLT"
+            assert r_cfg.get("Millenium Falcon", "atmospheric speed") == "1050 km/h"
+            assert r_cfg.get("general", "phases") == "dev, test, prod"
+            assert r_cfg.get("general", "exclude channels") == ""
