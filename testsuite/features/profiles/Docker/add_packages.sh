628c1a3093ee79bacf3dd6c1ecbf36640996079c
Eric Bischoff <ebischoff@suse.com>
2019-05-06 17:31:50 +0200

    Enable avahi in the containers

--- testsuite/features/profiles/Docker/add_packages.sh
+++ testsuite/features/profiles/Docker/add_packages.sh
@@ -6,6 +6,4 @@ cp /root/avahi-daemon.conf /etc/avahi/avahi-daemon.conf
 /usr/sbin/avahi-daemon -D
 
 zypper --non-interactive --gpg-auto-import-keys ref
-zypper --non-interactive in hoag-dummy orion-dummy
-zypper --non-interactive up milkyway-dummy
-
+zypper --non-interactive in aaa_base aaa_base-extras net-tools timezone vim less sudo tar python python-xml
--- testsuite/features/profiles/Docker/serverhost/add_packages.sh
+++ testsuite/features/profiles/Docker/serverhost/add_packages.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+rpm -ih --nodeps /root/*.rpm
+cp /root/avahi-daemon.conf /etc/avahi/avahi-daemon.conf
+/usr/sbin/avahi-daemon -D
+
 zypper --non-interactive --gpg-auto-import-keys ref
 zypper --non-interactive in hoag-dummy orion-dummy
 zypper --non-interactive up milkyway-dummy
