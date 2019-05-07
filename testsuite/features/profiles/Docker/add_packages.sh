a8f9e527e9af68c3211586c5c01e7c604f50da61
Eric Bischoff <ebischoff@suse.com>
2019-05-08 00:46:33 +0200

    We have to take avahi files from the network

--- testsuite/features/profiles/Docker/add_packages.sh
+++ testsuite/features/profiles/Docker/add_packages.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -e
 
-rpm -ih --nodeps /root/*.rpm
+# temporarily disable non-working repo
+zypper mr --disable Test-Channel-x86_64
+zypper --non-interactive --gpg-auto-import-keys ref
+
+# install, configure, and start avahi
+zypper --non-interactive in avahi
 cp /root/avahi-daemon.conf /etc/avahi/avahi-daemon.conf
 /usr/sbin/avahi-daemon -D
 
+# re-enable normal repo and remove helper repo
+zypper mr --enable Test-Channel-x86_64
+zypper rr sles12sp4
+
+# do the real test
 zypper --non-interactive --gpg-auto-import-keys ref
 zypper --non-interactive in aaa_base aaa_base-extras net-tools timezone vim less sudo tar python python-xml
--- testsuite/features/profiles/Docker/serverhost/add_packages.sh
+++ testsuite/features/profiles/Docker/serverhost/add_packages.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -e
 
-rpm -ih --nodeps /root/*.rpm
+# temporarily disable non-working repo
+zypper mr --disable Test-Channel-x86_64
+zypper --non-interactive --gpg-auto-import-keys ref
+
+# install, configure, and start avahi
+zypper --non-interactive in avahi
 cp /root/avahi-daemon.conf /etc/avahi/avahi-daemon.conf
 /usr/sbin/avahi-daemon -D
 
+# re-enable normal repo and remove helper repo
+zypper mr --enable Test-Channel-x86_64
+zypper rr sles12sp4
+
+# do the real test
 zypper --non-interactive --gpg-auto-import-keys ref
 zypper --non-interactive in hoag-dummy orion-dummy
 zypper --non-interactive up milkyway-dummy
-
