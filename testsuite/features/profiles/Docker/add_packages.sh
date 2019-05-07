ea86c0c9fbcef1fc2016132531054168bf4bb6cd
Eric Bischoff <ebischoff@suse.com>
2019-05-08 01:56:57 +0200

    The test channel might be absent

--- testsuite/features/profiles/Docker/add_packages.sh
+++ testsuite/features/profiles/Docker/add_packages.sh
@@ -2,7 +2,7 @@
 set -e
 
 # temporarily disable non-working repo
-zypper mr --disable Test-Channel-x86_64
+zypper mr --disable Test-Channel-x86_64 || :
 zypper --non-interactive --gpg-auto-import-keys ref
 
 # install, configure, and start avahi
@@ -11,7 +11,7 @@ cp /root/avahi-daemon.conf /etc/avahi/avahi-daemon.conf
 /usr/sbin/avahi-daemon -D
 
 # re-enable normal repo and remove helper repo
-zypper mr --enable Test-Channel-x86_64
+zypper mr --enable Test-Channel-x86_64 || :
 zypper rr sles12sp4
 
 # do the real test
