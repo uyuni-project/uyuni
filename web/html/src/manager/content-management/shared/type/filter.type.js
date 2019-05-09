de9c0ce951e9867fc88000b82e9f9f51e5d209ab
Luis Neves <luneves@suse.de>
2019-05-10 00:52:42 +0100

    New flow types to support new filter options

--- web/html/src/manager/content-management/shared/type/filter.type.js
+++ web/html/src/manager/content-management/shared/type/filter.type.js
@@ -1,7 +1,27 @@
-export type FilterType = {
+// @flow
+export type FilterServerType = {
+  entityType: string,
+  matcher: string,
+  id?: number,
   name?: string,
-  target?: string,
+  criteriaKey?: string,
+  criteriaValue?: string,
+  deny?: boolean,
+  projects?: Array<string>
+}
+
+export type FilterFormType = {
+  type: string,
+  matcher: string,
+  deny?: boolean,
+  id?: number,
+  name?: string,
+  projects?: Array<string>,
+  packageName?: string,
+  epoch?: string,
+  version?: string,
+  release?: string,
+  architecture?: string,
+  advisoryName?: string,
   criteria?: string,
-  deny: boolean,
-  projects: Array<String>
 }
