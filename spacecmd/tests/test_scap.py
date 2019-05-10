25415dde07f939b185bb2a99a8d0d360623d4a1f
Bo Maryniuk <bo@suse.de>
2019-05-10 12:10:45 +0200

    Add unit test for calling scap listxccdfscans without arguments.

--- spacecmd/tests/test_scap.py
+++ spacecmd/tests/test_scap.py
@@ -6,6 +6,7 @@ Test suite for Scap commands at spacecmd.
 from unittest.mock import MagicMock, patch
 from helpers import shell
 import pytest
+import spacecmd.scap
 
 
 class TestScap:
@@ -20,3 +21,19 @@ class TestScap:
         :param shell:
         :return:
         """
+        shell.help_scap_listxccdfscans = MagicMock()
+        shell.ssm = MagicMock()
+        shell.expand_systems = MagicMock()
+        shell.get_system_id = MagicMock()
+        shell.client.system.scap.listXccdfScans = MagicMock()
+
+        mprint = MagicMock()
+        with patch("spacecmd.scap.print", mprint):
+            spacecmd.scap.do_scap_listxccdfscans(shell, "")
+
+        assert shell.help_scap_listxccdfscans.called
+        assert not shell.ssm.keys.called
+        assert not shell.expand_systems.called
+        assert not shell.get_system_id.called
+        assert not shell.client.system.scap.listXccdfScans.called
+        assert not mprint.called
