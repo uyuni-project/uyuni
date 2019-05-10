0e8a2a2fc73bf652de9445e9b3f6be92c71cb714
Bo Maryniuk <bo@suse.de>
2019-05-10 14:06:16 +0200

    Add unit test for calling scap listxccdfscans with a system name argument.

--- spacecmd/tests/test_scap.py
+++ spacecmd/tests/test_scap.py
@@ -4,7 +4,7 @@ Test suite for Scap commands at spacecmd.
 """
 
 from unittest.mock import MagicMock, patch
-from helpers import shell
+from helpers import shell, assert_expect
 import pytest
 import spacecmd.scap
 
@@ -64,3 +64,49 @@ class TestScap:
         assert not shell.client.system.scap.listXccdfScans.called
         assert not mprint.called
 
+    def test_scap_listxccdfscans_system_arg(self, shell):
+        """
+        Test calling scap listxccdfscans with a system name argument.
+
+        :param shell:
+        :return:
+        """
+        shell.help_scap_listxccdfscans = MagicMock()
+        shell.SEPARATOR = "---"
+        shell.ssm = MagicMock()
+        shell.ssm.keys = MagicMock(return_value=[])
+        shell.expand_systems = MagicMock(return_value=["chair-1", "table-2"])
+        shell.get_system_id = MagicMock(side_effect=["001", "002"])
+        shell.client.system.scap.listXccdfScans = MagicMock(side_effect=[
+            [
+                {"xid": 1, "profile": "001", "path": "/etc/first", "completed": "true"},
+                {"xid": 2, "profile": "001", "path": "/etc/second", "completed": "false"},
+            ],
+            [
+                {"xid": 3, "profile": "002", "path": "/opt/etc/third", "completed": "false"},
+                {"xid": 4, "profile": "002", "path": "/opt/etc/fourth", "completed": "true"},
+            ],
+        ])
+
+        mprint = MagicMock()
+        with patch("spacecmd.scap.print", mprint):
+            spacecmd.scap.do_scap_listxccdfscans(shell, "chair table")
+
+        assert not shell.help_scap_listxccdfscans.called
+        assert not shell.ssm.keys.called
+        assert shell.expand_systems.called
+        assert shell.get_system_id.called
+        assert shell.client.system.scap.listXccdfScans.called
+        assert mprint.called
+
+        expectations = [
+            'System: chair-1', '',
+            'XID: 1 Profile: 001 Path: (/etc/first) Completed: true',
+            'XID: 2 Profile: 001 Path: (/etc/second) Completed: false',
+            shell.SEPARATOR,
+            'System: table-2',
+            '',
+            'XID: 3 Profile: 002 Path: (/opt/etc/third) Completed: false',
+            'XID: 4 Profile: 002 Path: (/opt/etc/fourth) Completed: true',
+        ]
+        assert_expect(mprint.call_args_list, *expectations)
