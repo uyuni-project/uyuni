4ad2d0c8a4338890e58e36d2c8f9f322ff68bb6e
Bo Maryniuk <bo@suse.de>
2019-05-10 12:16:23 +0200

    Add unit test for calling scap listxccdfscans with ssm argument.

--- spacecmd/tests/test_scap.py
+++ spacecmd/tests/test_scap.py
@@ -37,3 +37,30 @@ class TestScap:
         assert not shell.get_system_id.called
         assert not shell.client.system.scap.listXccdfScans.called
         assert not mprint.called
+
+    def test_scap_listxccdfscans_ssm_arg(self, shell):
+        """
+        Test calling scap listxccdfscans with ssm argument.
+        No systems has been scanned.
+
+        :param shell:
+        :return:
+        """
+        shell.help_scap_listxccdfscans = MagicMock()
+        shell.ssm = MagicMock()
+        shell.ssm.keys = MagicMock(return_value=[])
+        shell.expand_systems = MagicMock()
+        shell.get_system_id = MagicMock()
+        shell.client.system.scap.listXccdfScans = MagicMock()
+
+        mprint = MagicMock()
+        with patch("spacecmd.scap.print", mprint):
+            spacecmd.scap.do_scap_listxccdfscans(shell, "ssm")
+
+        assert not shell.help_scap_listxccdfscans.called
+        assert shell.ssm.keys.called
+        assert not shell.expand_systems.called
+        assert not shell.get_system_id.called
+        assert not shell.client.system.scap.listXccdfScans.called
+        assert not mprint.called
+
