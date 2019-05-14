aa53654c4fc10bee4e97d1318e84e3d33adb2b07
Can Bulut Bayburt <cbbayburt@suse.com>
2019-05-14 20:52:42 +0200

    Move Virtual Host Manager Spark route definitions in its own method

--- java/code/src/com/suse/manager/webui/Router.java
+++ java/code/src/com/suse/manager/webui/Router.java
@@ -21,7 +21,6 @@ import static com.suse.manager.webui.utils.SparkApplicationHelper.withOrgAdmin;
 import static com.suse.manager.webui.utils.SparkApplicationHelper.withProductAdmin;
 import static com.suse.manager.webui.utils.SparkApplicationHelper.withUser;
 import static com.suse.manager.webui.utils.SparkApplicationHelper.withUserPreferences;
-import static spark.Spark.delete;
 import static spark.Spark.exception;
 import static spark.Spark.get;
 import static spark.Spark.head;
@@ -92,6 +91,9 @@ public class Router implements SparkApplication {
         get("/manager/api/audit/cve.csv", withUser(CVEAuditController::cveAuditCSV));
 
         initContentManagementRoutes(jade);
+
+        // Virtual Host Managers
+        VirtualHostManagerController.initRoutes(jade);
 
         // Virtualization Routes
         initVirtualizationRoutes(jade);
@@ -183,37 +185,6 @@ public class Router implements SparkApplication {
         get("/manager/api/states/packages/match", StatesAPI::matchPackages);
         get("/manager/api/states/highstate", StatesAPI::showHighstate);
         get("/manager/api/states/:channelId/content", withUser(StatesAPI::stateContent));
-
-        // Virtual Host Managers
-        get("/manager/vhms",
-                withUserPreferences(withCsrfToken(withOrgAdmin(VirtualHostManagerController::list))),
-                jade);
-        post("/manager/api/vhms/kubeconfig/validate",
-                withOrgAdmin(VirtualHostManagerController::validateKubeconfig));
-        post("/manager/api/vhms/create/kubernetes",
-                withUser(VirtualHostManagerController::createKubernetes));
-        post("/manager/api/vhms/update/kubernetes",
-                withUser(VirtualHostManagerController::updateKubernetes));
-        get("/manager/api/vhms/kubeconfig/:id/contexts",
-                withOrgAdmin(VirtualHostManagerController::getKubeconfigContexts));
-        post("/manager/api/vhms/:id/refresh",
-                withOrgAdmin(VirtualHostManagerController::refresh));
-        get("/manager/api/vhms/:id/nodes",
-                withOrgAdmin(VirtualHostManagerController::getNodes));
-        get("/manager/api/vhms/modules",
-                withOrgAdmin(VirtualHostManagerController::getModules));
-        get("/manager/api/vhms/module/:name/params",
-                withOrgAdmin(VirtualHostManagerController::getModuleParams));
-        get("/manager/api/vhms",
-                withOrgAdmin(VirtualHostManagerController::get));
-        get("/manager/api/vhms/:id",
-                withOrgAdmin(VirtualHostManagerController::getSingle));
-        post("/manager/api/vhms/create",
-                withOrgAdmin(VirtualHostManagerController::create));
-        post("/manager/api/vhms/update/:id",
-                withOrgAdmin(VirtualHostManagerController::update));
-        delete("/manager/api/vhms/delete/:id",
-                withOrgAdmin(VirtualHostManagerController::delete));
 
         // Subscription Matching
         get("/manager/subscription-matching",
