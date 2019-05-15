1e1f4afa5c03af1b4555659c6ae5e44d18c2ccec
Frantisek Kobzik <fkobzik@suse.de>
2019-05-15 16:09:25 +0200

    Rename the 'Country' key 'Country Code' in the Advanced search

--- java/code/src/com/redhat/rhn/frontend/action/systems/SystemSearchAction.java
+++ java/code/src/com/redhat/rhn/frontend/action/systems/SystemSearchAction.java
@@ -95,7 +95,7 @@ public class SystemSearchAction extends BaseSearchAction implements Listable {
                     { SystemSearchHelper.INSTALLED_PACKAGES,
                                     SystemSearchHelper.NEEDED_PACKAGES },
                     /* location */
-                    { SystemSearchHelper.LOC_COUNTRY, SystemSearchHelper.LOC_STATE,
+                    { SystemSearchHelper.LOC_COUNTRY_CODE, SystemSearchHelper.LOC_STATE,
                                     SystemSearchHelper.LOC_CITY,
                                     SystemSearchHelper.LOC_ADDRESS,
                                     SystemSearchHelper.LOC_BUILDING,
