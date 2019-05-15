0df1b72cd2d46b4dd0db172bf3a1924532c2fde5
Bo Maryniuk <bo@suse.de>
2019-05-15 14:17:00 +0200

    Add unit test for do_ssm_add a system that does not exists.

--- spacecmd/tests/test_ssm.py
+++ spacecmd/tests/test_ssm.py
@@ -34,3 +34,27 @@ class TestSCSSM:
         assert not logger.warning.called
         assert not logger.debug.called
         assert shell.help_ssm_add.called
+
+    def test_ssm_add_system_not_found(self, shell):
+        """
+        Test do_ssm_add a system that does not exists.
+
+        :param shell:
+        :return:
+        """
+        shell.help_ssm_add = MagicMock()
+        shell.expand_systems = MagicMock(return_value=[])
+        shell.ssm = {}
+        shell.get_system_id = MagicMock(return_value=None)
+
+        logger = MagicMock()
+        save_cache = MagicMock()
+        with patch("spacecmd.ssm.logging", logger) as lgr, \
+            patch("spacecmd.ssm.save_cache", save_cache) as svc:
+            ssm.do_ssm_add(shell, "example.com")
+
+        assert logger.warning.called
+        assert not logger.debug.called
+        assert not shell.help_ssm_add.called
+
+        assert_expect(logger.warning.call_args_list, "No systems found")
