6c9f57c898dd2170254e67e2bc722fdd055460e1
Bo Maryniuk <bo@suse.de>
2019-05-16 14:49:23 +0200

    Add unit test for do_cryptokey_create without arguments (interactive, wrong key type).

--- spacecmd/tests/test_cryptokey.py
+++ spacecmd/tests/test_cryptokey.py
@@ -71,3 +71,33 @@ class TestSCCryptokey:
         assert logger.error.called
 
         assert_expect(logger.error.call_args_list, "No contents of the file")
+
+    def test_cryptokey_create_interactive_wrong_key_type(self, shell):
+        """
+        Test do_cryptokey_create without arguments (interactive, wrong key type).
+
+        :param shell:
+        :return:
+        """
+        shell.help_cryptokey_create = MagicMock()
+        shell.client.kickstart.keys.create = MagicMock()
+        shell.user_confirm = MagicMock(return_value=True)
+        read_file = MagicMock(return_value="contents")
+        prompt_user = MagicMock(side_effect=["x", "interactive descr", "/tmp/file.txt"])
+        editor = MagicMock()
+        logger = MagicMock()
+
+        with patch("spacecmd.cryptokey.prompt_user", prompt_user) as pmu, \
+            patch("spacecmd.cryptokey.read_file", read_file) as rfl, \
+            patch("spacecmd.cryptokey.editor", editor) as edt, \
+            patch("spacecmd.cryptokey.logging", logger) as lgr:
+            spacecmd.cryptokey.do_cryptokey_create(shell, "")
+
+        assert not shell.help_cryptokey_create.called
+        assert not shell.client.kickstart.keys.create.called
+        assert read_file.called
+        assert prompt_user.called
+        assert not editor.called
+        assert logger.error.called
+
+        assert_expect(logger.error.call_args_list, "Invalid key type")
