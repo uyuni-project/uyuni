17ec8b38b9c1d36690fbeb16bb69c09faf7fdae5
Luis Neves <luneves@suse.de>
2019-05-17 14:52:16 +0100

    Reorganize minion keys javascript files to its own feature folder

--- web/html/src/manager/salt/index.js
+++ web/html/src/manager/salt/index.js
@@ -2,6 +2,7 @@ module.exports = {
   entries: [
     'cmd/remote-commands.js',
     'formula-catalog/org-formula-catalog.js',
-    'formula-catalog/org-formula-details.js'
+    'formula-catalog/org-formula-details.js',
+    'keys/key-management.js'
   ]
 };
