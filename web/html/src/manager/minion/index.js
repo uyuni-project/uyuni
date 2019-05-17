440af69d7d7455759b6c340c0e7a83685cf04480
Luis Neves <luneves@suse.de>
2019-05-17 14:48:09 +0100

    Reorganize minion package stats javascript files to its own feature folder

--- web/html/src/manager/minion/index.js
+++ web/html/src/manager/minion/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   entries: [
     'formula/minion-formula.js',
-    'formula/minion-formula-selection.js'
+    'formula/minion-formula-selection.js',
+    'packages/package-states.js'
   ]
 };
