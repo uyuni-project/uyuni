3a5fc8e55808b10a588ead15779e30e14277ec20
Luis Neves <luneves@suse.de>
2019-05-17 14:56:18 +0100

    Reorganize systems delete javascript files to its own feature folder

--- web/html/src/manager/systems/index.js
+++ web/html/src/manager/systems/index.js
@@ -2,6 +2,8 @@ module.exports = {
   entries: [
     'activation-key/activation-key-channels.renderer.js',
     'bootstrap/bootstrap-minions.js',
-    'virtualhostmanager/virtualhostmanager.js'
+    'virtualhostmanager/virtualhostmanager.js',
+    'delete-system-confirm.js',
+    'duplicate-systems-compare-delete.js',
   ]
 };
