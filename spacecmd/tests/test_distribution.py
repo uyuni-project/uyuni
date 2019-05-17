9acd933b952fd640f8e1c27b9cd997c36a8ea8cd
Bo Maryniuk <bo@suse.de>
2019-05-17 16:11:10 +0200

    Add unit test for do_distribution_create with no args (interactive)

---
+++ spacecmd/tests/test_distribution.py
@@ -0,0 +1,65 @@
+# coding: utf-8
+"""
+Test distribution
+"""
+
+from unittest.mock import MagicMock, patch
+from helpers import shell, assert_expect
+import pytest
+import spacecmd.distribution
+
+
+class TestSCDistribution:
+    """
+    Test suite for distribution commands of the spacecmd.
+    """
+    def test_distribution_create_no_args(self, shell):
+        """
+        Test do_distribution_create with no args.
+
+        :param shell:
+        :return:
+        """
+        shell.client.kickstart.tree.listInstallTypes = MagicMock(return_value=[
+            {"label": "image"},
+        ])
+        shell.client.kickstart.tree.update = MagicMock()
+        shell.client.kickstart.tree.create = MagicMock()
+        shell.list_base_channels = MagicMock(return_value=["base-channel"])
+
+        mprint = MagicMock()
+        prompt = MagicMock(side_effect=[
+            "name", "/path/tree", "base-channel", "image"
+        ])
+        logger = MagicMock()
+
+        with patch("spacecmd.distribution.print", mprint) as prn, \
+            patch("spacecmd.distribution.prompt_user", prompt) as prmt, \
+            patch("spacecmd.distribution.logging", logger) as lgr:
+            spacecmd.distribution.do_distribution_create(shell, "")
+
+        assert mprint.called
+        assert prompt.called
+        assert shell.client.kickstart.tree.listInstallTypes.called
+        assert shell.client.kickstart.tree.create.called
+        assert not shell.client.kickstart.tree.update.called
+
+        # Check STDOUT consistency
+        exp = [
+            '', 'Base Channels',
+            '-------------', 'base-channel', '', '',
+            'Install Types', '-------------', 'image', ''
+        ]
+        for call in mprint.call_args_list:
+            assert_expect([call], next(iter(exp)))
+            exp.pop(0)
+        assert not exp
+
+        for call in shell.client.kickstart.tree.create.call_args_list:
+            args, kw = call
+            assert args == (shell.session, "name", "/path/tree", "base-channel", "image")
+            assert not kw
+
+        assert_expect(shell.client.kickstart.tree.listInstallTypes.call_args_list,
+                      shell.session)
+
