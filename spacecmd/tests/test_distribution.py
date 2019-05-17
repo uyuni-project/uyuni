6d714951cf90cfb788604fe5ce42dd5bdc9b9c11
Bo Maryniuk <bo@suse.de>
2019-05-17 16:17:03 +0200

    Add unit test for do_distribution_create with no args with update mode.

--- spacecmd/tests/test_distribution.py
+++ spacecmd/tests/test_distribution.py
@@ -63,3 +63,36 @@ class TestSCDistribution:
         assert_expect(shell.client.kickstart.tree.listInstallTypes.call_args_list,
                       shell.session)
 
+    def test_distribution_create_no_args_update_mode(self, shell):
+        """
+        Test do_distribution_create with no args with update mode.
+
+        :param shell:
+        :return:
+        """
+        shell.client.kickstart.tree.listInstallTypes = MagicMock(return_value=[
+            {"label": "image"},
+        ])
+        shell.client.kickstart.tree.update = MagicMock()
+        shell.client.kickstart.tree.create = MagicMock()
+        shell.list_base_channels = MagicMock(return_value=["base-channel"])
+
+        mprint = MagicMock()
+        prompt = MagicMock(side_effect=[
+            "name", "/path/tree", "base-channel", "image"
+        ])
+        logger = MagicMock()
+
+        with patch("spacecmd.distribution.print", mprint) as prn, \
+            patch("spacecmd.distribution.prompt_user", prompt) as prmt, \
+            patch("spacecmd.distribution.logging", logger) as lgr:
+            spacecmd.distribution.do_distribution_create(shell, "", update=True)
+
+        assert not mprint.called
+        assert not prompt.called
+        assert not shell.client.kickstart.tree.listInstallTypes.called
+        assert not shell.client.kickstart.tree.create.called
+        assert not shell.client.kickstart.tree.update.called
+        assert logger.error.called
+
+        assert_expect(logger.error.call_args_list, "The name of the distribution is required")
