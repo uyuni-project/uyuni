e8b8998e75171ae80b9396bcc789ccb6901820dd
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2019-05-17 17:23:27 +0100

    Delegate authentication to ULNAuth and inject token into HTTP headers

--- backend/satellite_tools/repo_plugins/uln_src.py
+++ backend/satellite_tools/repo_plugins/uln_src.py
@@ -22,55 +22,38 @@ ULN plugin for spacewalk-repo-sync.
 # pylint: disable=E0012, C0413
 import sys
 sys.path.append('/usr/share/rhn')
-from up2date_client.rpcServer import RetryServer, ServerList
 
 from spacewalk.satellite_tools.repo_plugins.yum_src import ContentSource as yum_ContentSource
 from spacewalk.satellite_tools.syncLib import RhnSyncException
+from spacewalk.satellite_tools.ulnauth import ULNAuth
 
 ULNSRC_CONF = '/etc/rhn/spacewalk-repo-sync/uln.conf'
-DEFAULT_UP2DATE_URL = "linux-update.oracle.com"
-
 
 class ContentSource(yum_ContentSource):
 
-    def __init__(self, url, name, insecure=False, interactive=True, yumsrc_conf=ULNSRC_CONF, org=1, channel_label="",
-                 ca_cert_file=None, client_cert_file=None, no_mirrors=False):
+    def __init__(self, url, name, insecure=False, interactive=True, yumsrc_conf=ULNSRC_CONF,
+                 org=1, channel_label="", no_mirrors=False,
+                 ca_cert_file=None, client_cert_file=None, client_key_file=None):
         if url[:6] != "uln://":
             raise RhnSyncException("url format error, url must start with uln://")
-        yum_ContentSource.__init__(self, url, name, insecure, interactive, ULNSRC_CONF, org, channel_label,
-                                   client_cert_file, client_key_file, no_mirrors)
-        self.uln_url = None
-        self.uln_user = None
-        self.uln_pass = None
-        self.key = None
+        yum_ContentSource.__init__(self, url=url, name=name, insecure=insecure,
+                                   interactive=interactive, yumsrc_conf=ULNSRC_CONF, org=org,
+                                   channel_label=channel_label, no_mirrors=no_mirrors,
+                                   ca_cert_file=ca_cert_file, client_cert_file=client_cert_file,
+                                   client_key_file=client_key_file)
+        self.uln_token = None
 
     def _authenticate(self, url):
-        if url.startswith("uln:///"):
-            self.uln_url = "https://" + DEFAULT_UP2DATE_URL
-            label = url[7:]
-        elif url.startswith("uln://"):
-            parts = url[6:].split("/")
-            self.uln_url = "https://" + parts[0]
-            label = parts[1]
-        else:
-            raise RhnSyncException("url format error, url must start with uln://")
-        self.uln_user = self.yumbase.conf.username
-        self.uln_pass = self.yumbase.conf.password
-        self.url = self.uln_url + "/XMLRPC/GET-REQ/" + label
+        self._url_orig = url
+        self._uln_auth = ULNAuth()
+        self.uln_token = self._uln_auth.authenticate(url)
+        self.http_headers = {'X-ULN-Api-User-Key': self.uln_token}
+        hostname, label = self._uln_auth.get_hostname(url)
+        self.url = self._uln_auth.url + "/XMLRPC/GET-REQ/" + label
         print(("The download URL is: " + self.url))
-        if self.proxy_addr:
-            print(("Trying proxy " + self.proxy_addr))
-        slist = ServerList([self.uln_url+"/rpc/api",])
-        s = RetryServer(slist.server(),
-                        refreshCallback=None,
-                        proxy=self.proxy_addr,
-                        username=self.proxy_user,
-                        password=self.proxy_pass,
-                        timeout=5)
-        s.addServerList(slist)
-        self.key = s.auth.login(self.uln_user, self.uln_pass)
+        if self.proxy_url:
+            print(("Trying proxy " + self.proxy_url))
 
     # pylint: disable=arguments-differ
     def setup_repo(self, repo, *args, **kwargs):
-        repo.http_headers = {'X-ULN-Api-User-Key': self.key}
-        yum_ContentSource.setup_repo(self, repo, *args, **kwargs)
+        yum_ContentSource.setup_repo(self, repo, *args, uln_repo=True, **kwargs)
