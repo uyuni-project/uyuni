136f534c32a8b117d43f495a1f96cba359842248
Julio Gonzalez Gil <jgonzalez@suse.com>
2019-05-27 18:52:27 +0200

    Use new RPM for schema migration

--- susemanager-utils/testing/docker/scripts/schema_migration_test_pgsql-31to40.sh
+++ susemanager-utils/testing/docker/scripts/schema_migration_test_pgsql-31to40.sh
@@ -16,7 +16,7 @@ fi
 
 # Database schema creation
 
-rpm -ivh /root/susemanager-schema-4.0.1-1.2.noarch.rpm
+rpm -ivh /root/susemanager-schema-4.0.12-1.1.noarch.rpm
 
 export PERLLIB=/manager/spacewalk/setup/lib/:/manager/web/modules/rhn/:/manager/web/modules/pxt/
 export PATH=/manager/schema/spacewalk/:/manager/spacewalk/setup/bin/:$PATH
