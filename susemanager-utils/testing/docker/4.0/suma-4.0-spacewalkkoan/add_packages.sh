ff50856f24b34cdca3638e433770ec1f207be13c
Julio Gonzalez Gil <jgonzalez@suse.com>
2019-05-27 19:40:49 +0200

    Fix koan docker container

--- susemanager-utils/testing/docker/4.0/suma-4.0-spacewalkkoan/add_packages.sh
+++ susemanager-utils/testing/docker/4.0/suma-4.0-spacewalkkoan/add_packages.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
+# make sure the package repository is up to date
+zypper --non-interactive --gpg-auto-import-keys ref
+
 # Packages required to run spacewalk-setup inside of the container
-zypper in -y file koan
+zypper in -y file gzip koan
