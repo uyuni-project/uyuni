c27ccc3dabc720b1ebd421c5cf701e7a81dcf04e
Bo Maryniuk <bo@suse.de>
2019-05-28 16:44:07 +0200

    Add unit test for do_org_rename org not found (None).

--- spacecmd/tests/test_org.py
+++ spacecmd/tests/test_org.py
@@ -181,3 +181,37 @@ class TestSCOrg:
         assert not shell.get_org_id.called
         assert not shell.client.org.updateName.called
         assert shell.help_org_rename.called
+
+    def test_org_rename_no_org_found(self, shell):
+        """
+        Test do_org_rename org not found (None).
+
+            :param self:
+            :param shell:
+        """
+        shell.help_org_rename = MagicMock()
+        shell.get_org_id = MagicMock(return_value=None)
+        shell.client.org.updateName = MagicMock()
+
+        mprint = MagicMock()
+        logger = MagicMock()
+        with patch("spacecmd.org.print", mprint) as prn, \
+            patch("spacecmd.org.logging", logger) as lgr:
+            spacecmd.org.do_org_rename(shell, "ACME-Enterprises Big-Stuff")
+
+        assert not shell.client.org.updateName.called
+        assert not shell.help_org_rename.called
+        assert shell.get_org_id.called
+        assert logger.warning.called
+        assert mprint.called
+
+        assert_expect(mprint.call_args_list,
+                      "Organisation 'ACME-Enterprises' was not found")
+        expectations = [
+            ('No organisation found for the name %s', 'ACME-Enterprises')
+        ]
+        for call in logger.warning.call_args_list:
+            args, kw = call
+            assert args == next(iter(expectations))
+            expectations.pop(0)
+        assert not expectations
