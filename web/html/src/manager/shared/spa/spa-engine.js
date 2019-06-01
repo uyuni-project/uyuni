2199cf80ed348bc954dd6de71d135134c2e552b2
Luis Neves <luneves@suse.de>
2019-06-01 09:58:52 +0100

    Enable/disable SPA engine by a flag

--- web/html/src/manager/shared/spa/spa-engine.js
+++ web/html/src/manager/shared/spa/spa-engine.js
@@ -3,21 +3,28 @@ import "senna/build/senna.css"
 import "./spa-engine.css"
 import SpaRenderer from "core/spa/spa-renderer";
 
-var appInstance = new App();
-appInstance.setLinkSelector("a.js-spa");
-appInstance.setFormSelector("");
-appInstance.addSurfaces('left-menu-data');
-appInstance.addSurfaces('ssm-box');
-appInstance.addSurfaces('page-body');
-appInstance.addRoutes([{
-  path: /.*/,
-  handler: function (route, a, b) {
-    const screen = new HtmlScreen();
-    screen.setCacheable(false);
-    return screen;
-  }
-}]);
+window.pageRenderers = window.pageRenderers || {};
+window.pageRenderers.spa = window.pageRenderers.spa || {};
 
-appInstance.on('endNavigate', function() {
-  SpaRenderer.onSpaEndNavigation();
-});
+window.pageRenderers.spa.init = function init() {
+  const appInstance = new App();
+  appInstance.setLinkSelector("a.js-spa");
+  appInstance.setFormSelector("");
+  appInstance.addSurfaces('left-menu-data');
+  appInstance.addSurfaces('ssm-box');
+  appInstance.addSurfaces('page-body');
+  appInstance.addRoutes([{
+    path: /.*/,
+    handler: function (route, a, b) {
+      const screen = new HtmlScreen();
+      screen.setCacheable(false);
+      return screen;
+    }
+  }]);
+
+  appInstance.on('endNavigate', function() {
+    SpaRenderer.onSpaEndNavigation();
+  });
+
+  return appInstance;
+}
