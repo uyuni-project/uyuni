a3cf4782f816d33e8556da728184ad10cd14d0f7
Bo Maryniuk <bo@suse.de>
2019-06-03 14:25:55 +0200

    Add unit test for do_group_addsystems with SSM argument, without systems

--- spacecmd/tests/test_group.py
+++ spacecmd/tests/test_group.py
@@ -40,4 +40,28 @@ class TestSCGroup:
         assert not logger.error.called
         assert shell.help_group_addsystems.called
 
+    def test_group_addsystems_ssm_no_systems(self, shell):
+        """
+        Test do_group_addsystems with SSM argument, without systems.
 
+        :param shell:
+        :return:
+        """
+        shell.help_group_addsystems = MagicMock()
+        shell.get_system_id = MagicMock()
+        shell.expand_systems = MagicMock()
+        shell.client.systemgroup.addOrRemoveSystems = MagicMock()
+        shell.ssm.keys = MagicMock(return_value=[])
+        mprint = MagicMock()
+        logger = MagicMock()
+        with patch("spacecmd.group.print", mprint) as prn, \
+            patch("spacecmd.group.logging", logger) as lgr:
+            spacecmd.group.do_group_addsystems(shell, "groupname ssm")
+
+        assert not shell.get_system_id.called
+        assert not shell.expand_systems.called
+        assert not shell.client.systemgroup.addOrRemoveSystems.called
+        assert not mprint.called
+        assert not logger.error.called
+        assert not shell.help_group_addsystems.called
+        assert shell.ssm.keys.called
