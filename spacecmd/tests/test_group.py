8475edb0044edb8340dcba4c5b781f9f6f17ade9
Bo Maryniuk <bo@suse.de>
2019-06-03 14:35:46 +0200

    Add unit test for do_group_removesystems without arguments

--- spacecmd/tests/test_group.py
+++ spacecmd/tests/test_group.py
@@ -120,3 +120,30 @@ class TestSCGroup:
 
         assert_args_expect(shell.client.systemgroup.addOrRemoveSystems.call_args_list,
                            [((shell.session, 'groupname', ['1000010000', '1000010001'], True), {})])
+
+    def test_group_removesystems_noargs(self, shell):
+        """
+        Test do_group_removesystems without arguments.
+
+        :param shell:
+        :return:
+        """
+
+        shell.help_group_removesystems = MagicMock()
+        shell.get_system_id = MagicMock()
+        shell.expand_systems = MagicMock()
+        shell.client.systemgroup.addOrRemoveSystems = MagicMock()
+        shell.ssm.keys = MagicMock()
+        mprint = MagicMock()
+        logger = MagicMock()
+        with patch("spacecmd.group.print", mprint) as prn, \
+            patch("spacecmd.group.logging", logger) as lgr:
+            spacecmd.group.do_group_removesystems(shell, "")
+
+        assert not shell.get_system_id.called
+        assert not shell.ssm.keys.called
+        assert not shell.client.systemgroup.addOrRemoveSystems.called
+        assert not shell.expand_systems.called
+        assert not mprint.called
+        assert not logger.error.called
+        assert shell.help_group_removesystems.called
