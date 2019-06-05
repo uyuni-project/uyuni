59dd7f281e6cf4a2672e9d9874f2d1cac0461d7f
Silvio Moioli <smoioli@suse.de>
2019-06-05 07:18:37 +0200

    Testsuite: unbreak test after refactoring

--- testsuite/features/step_definitions/docker_steps.rb
+++ testsuite/features/step_definitions/docker_steps.rb
@@ -46,7 +46,7 @@ When(/^I wait at most (\d+) seconds until container "([^"]*)" is built successfu
   end
   raise 'unable to find the image id' if image_id.zero?
 
-  repeat_until_timeout(timeout: timeout, message: 'image build did not complete') do
+  repeat_until_timeout(timeout: timeout.to_i, message: 'image build did not complete') do
     idetails = cont_op.get_image_details(image_id)
     break if idetails['buildStatus'] == 'completed' && idetails['inspectStatus'] == 'completed'
     raise 'image build failed.' if idetails['buildStatus'] == 'failed'
@@ -57,7 +57,7 @@ end
 
 When(/^I wait at most (\d+) seconds until all "([^"]*)" container images are built correctly in the GUI$/) do |timeout, count|
   def ck_container_imgs(timeout, count)
-    repeat_until_timeout(timeout: timeout, message: 'at least one image was not built correctly') do
+    repeat_until_timeout(timeout: timeout.to_i, message: 'at least one image was not built correctly') do
       step %(I navigate to images webpage)
       step %(I wait until I do not see "There are no entries to show." text)
       raise 'error detected while building images' if has_xpath?("//*[contains(@title, 'Failed')]")
