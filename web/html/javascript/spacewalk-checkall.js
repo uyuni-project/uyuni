48a538cc3e0be0b38a4e01b36b431c4d816de3dd
Luis Neves <luneves@suse.de>
2019-06-05 15:40:37 +0100

    Remove unused code related to  spacewalk-bar

--- web/html/javascript/spacewalk-checkall.js
+++ web/html/javascript/spacewalk-checkall.js
@@ -203,11 +203,6 @@ function pageResponse(data) {
     }
 }
 
-// the function we call to read the number of systems selected and hide/show the toolbar
-function updateSsmToolbarOpacity() {
-  $(".spacewalk-bar").animate({"opacity": "100"}, 0, function() {});
-}
-
 function sortColumn(sortByWidget, sortByValue, sortDirWidget, sortDirValue) {
      var sortBy = document.getElementById(sortByWidget); 
      sortBy.value = sortByValue;
