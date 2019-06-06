78af6dfc76ef53b23a0628cc980a202e7c2f0dc2
Michael Calmer <mc@suse.de>
2019-06-06 16:22:32 +0200

    Uyuni is now based on Leap 15.1

--- susemanager-utils/testing/docker/master/uyuni-push-to-obs/add_repositories.sh
+++ susemanager-utils/testing/docker/master/uyuni-push-to-obs/add_repositories.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-zypper ar -f https://download.opensuse.org/repositories/home:/mcalmer:/tito/openSUSE_Leap_15.0/home:mcalmer:tito.repo
-zypper ar -f http://download.suse.de/ibs/SUSE:/CA/openSUSE_Leap_15.0/SUSE:CA.repo
+zypper ar -f https://download.opensuse.org/repositories/home:/mcalmer:/tito/openSUSE_Leap_15.1/home:mcalmer:tito.repo
+zypper ar -f http://download.suse.de/ibs/SUSE:/CA/openSUSE_Leap_15.1/SUSE:CA.repo
