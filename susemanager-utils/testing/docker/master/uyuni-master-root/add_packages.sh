84e1fcd61c9c47a5c174e20134b98d20bc3ee754
Michael Calmer <mc@suse.de>
2019-06-06 16:22:32 +0200

    Uyuni is now based on Leap 15.1

--- susemanager-utils/testing/docker/master/uyuni-master-root/add_packages.sh
+++ susemanager-utils/testing/docker/master/uyuni-master-root/add_packages.sh
@@ -4,19 +4,16 @@ set -e
 # Packages required to run the pylint
 zypper in -y  make \
               python3 \
-	      python3-PyYAML \
+              python3-PyYAML \
               python3-base \
               python3-configobj \
               python3-devel \
               python3-mock \
-	      python3-nose \
+              python3-nose \
               python3-pylint \
-              python3-pytest
-
-# python3-urlgabber is not part of neither SLE or openSUSE 15.X
-curl https://download.opensuse.org/repositories/systemsmanagement:/Uyuni:/Master/openSUSE_Leap_15.0/noarch/python3-urlgrabber.rpm -o /tmp/python3-urlgrabber.rpm && \
-rpm -i /tmp/python3-urlgrabber.rpm && \
-rm -rf python3-urlgrabber.rpm
+              python3-pytest \
+              python3-urlgrabber \
+              curl
 
 zypper -n in vim less
 
