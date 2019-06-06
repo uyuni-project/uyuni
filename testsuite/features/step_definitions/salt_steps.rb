4c9ef41629f2704a958ce8afa511df975e5c8010
Can Bulut Bayburt <cbbayburt@suse.com>
2019-06-06 16:24:07 +0200

    Kill timed-out image build jobs on cleanup

--- testsuite/features/step_definitions/salt_steps.rb
+++ testsuite/features/step_definitions/salt_steps.rb
@@ -724,3 +724,11 @@ When(/^I install "([^"]*)" to custom formula metadata directory "([^"]*)"$/) do 
   raise 'File injection failed' unless return_code.zero?
   $server.run("chmod 644 " + dest)
 end
+
+When(/^I kill remaining Salt jobs on "([^"]*)"$/) do |minion|
+  system_name = get_system_name(minion)
+  out, _code = $server.run("salt #{system_name} saltutil.kill_all_jobs")
+  if out.include?(system_name) && out.include?('Signal 9 sent to job')
+    puts out
+  end
+end
