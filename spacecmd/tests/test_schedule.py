77df7780ae7ac02fcf3e62db43f1f21d1fc34a20
Bo Maryniuk <bo@suse.de>
2019-06-06 14:09:49 +0200

    Update unit tests, add unit test for do_schedule_reschedule without arguments

--- spacecmd/tests/test_schedule.py
+++ spacecmd/tests/test_schedule.py
@@ -13,7 +13,7 @@ class TestSCSchedule:
     Test suite for "schedule" module.
     """
 
-    def test_schedule_reschedule(self, shell):
+    def test_schedule_cancel(self, shell):
         """
         Test do_schedule_cancel without arguments.
 
@@ -21,9 +21,34 @@ class TestSCSchedule:
         :return:
         """
 
-        shell.help_schedule_reschedule = MagicMock()
+        shell.help_schedule_cancel = MagicMock()
         shell.client.schedule.listInProgressActions = MagicMock()
         shell.client.schedule.cancelActions = MagicMock()
+        shell.user_confirm = MagicMock()
+        mprint = MagicMock()
+        logger = MagicMock()
+
+        with patch("spacecmd.schedule.print", mprint) as prt, \
+            patch("spacecmd.schedule.logging", logger) as lgr:
+            spacecmd.schedule.do_schedule_cancel(shell, "")
+
+        assert not shell.client.schedule.listInProgressActions.called
+        assert not shell.client.schedule.cancelActions.called
+        assert not shell.user_confirm.called
+        assert not mprint.called
+        assert not logger.warning.called
+        assert shell.help_schedule_cancel.called
+
+    def test_schedule_reschedule(self, shell):
+        """
+        Test do_schedule_reschedule without arguments.
+
+        :param shell:
+        :return:
+        """
+
+        shell.help_schedule_reschedule = MagicMock()
+        shell.client.schedule.listInProgressActions = MagicMock()
         shell.client.schedule.rescheduleActions = MagicMock()
         shell.user_confirm = MagicMock()
         mprint = MagicMock()
@@ -34,10 +59,8 @@ class TestSCSchedule:
             spacecmd.schedule.do_schedule_reschedule(shell, "")
 
         assert not shell.client.schedule.listInProgressActions.called
-        assert not shell.client.schedule.cancelActions.called
         assert not shell.client.schedule.rescheduleActions.called
         assert not shell.user_confirm.called
         assert not mprint.called
         assert not logger.warning.called
         assert shell.help_schedule_reschedule.called
-
