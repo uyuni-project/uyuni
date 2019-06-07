2245d45011c9c7f7f349d287ae415b10631b33de
Bo Maryniuk <bo@suse.de>
2019-06-07 11:57:24 +0200

    Add unit test for do_schedule_reschedule with failed actions

--- spacecmd/tests/test_schedule.py
+++ spacecmd/tests/test_schedule.py
@@ -157,6 +157,41 @@ class TestSCSchedule:
         assert shell.client.schedule.listFailedActions.called
         assert shell.user_confirm.called
 
+    def test_schedule_reschedule_failed_actions(self, shell):
+        """
+        Test do_schedule_reschedule with failed actions.
+
+        :param shell:
+        :return:
+        """
+
+        shell.help_schedule_reschedule = MagicMock()
+        shell.client.schedule.listFailedActions = MagicMock(return_value=[
+            {"id": 1}, {"id": 2}, {"id": 3}, {"id": 4}
+        ])
+        shell.client.schedule.rescheduleActions = MagicMock()
+        shell.user_confirm = MagicMock(return_value=False)
+        mprint = MagicMock()
+        logger = MagicMock()
+
+        with patch("spacecmd.schedule.print", mprint) as prt, \
+            patch("spacecmd.schedule.logging", logger) as lgr:
+            spacecmd.schedule.do_schedule_reschedule(shell, "one 2 3, 5 and 4")
+
+        assert shell.client.schedule.rescheduleActions.called
+        assert mprint.called
+        assert logger.warning.called
+        assert not shell.help_schedule_reschedule.called
+        assert shell.client.schedule.listFailedActions.called
+        assert not shell.user_confirm.called
+
+        assert_expect(mprint.call_args_list, "Rescheduled 2 action(s)")
+        assert_list_args_expect(logger.warning.call_args_list,
+                                ['"one" is not a valid ID',
+                                 '"3," is not a valid ID',
+                                 '"5" is not a failed action',
+                                 '"and" is not a valid ID'])
+
     def test_schedule_details_noargs(self, shell):
         """
         Test do_schedule_details without arguments.
