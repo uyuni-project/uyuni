e7725b7a99bd13f64005ffbbd58e06057a0abfdc
Luis Neves <luneves@suse.de>
2019-06-07 09:38:51 +0100

    Fix js linting and java unit tests after SPA

--- java/code/src/com/redhat/rhn/frontend/taglibs/test/NavDialogMenuTagTest.java
+++ java/code/src/com/redhat/rhn/frontend/taglibs/test/NavDialogMenuTagTest.java
@@ -120,7 +120,7 @@ public class NavDialogMenuTagTest extends RhnBaseTestCase {
 
     private String getReturnValue() {
         return "<div class=\"spacewalk-content-nav\"><ul class=\"nav nav-tabs\">" +
-               "<li class=\"active\"><a href=\"/\">Sign In</a></li>\n" +
+                "<li class=\"active\"><a href=\"/\" class=\"js-spa\">Sign In</a></li>\n" +
                "<li><a href=\"/rhn/help/about.do\">About</a></li>\n" +
                "</ul>\n" +
                "</div>\n";
--- web/html/src/manager/notifications/notification-messages.js
+++ web/html/src/manager/notifications/notification-messages.js
@@ -2,7 +2,6 @@
 "use strict";
 
 const React = require("react");
-const ReactDOM = require("react-dom");
 const MessageContainer = require("components/messages").Messages;
 const {Table, Column, SearchField, Highlight} = require("components/table");
 const Network = require("utils/network");
