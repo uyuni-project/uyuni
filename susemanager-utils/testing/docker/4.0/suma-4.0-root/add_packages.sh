1860364affd28b54398518affc29c97f946f3df3
Jochen Breuer <jbreuer@suse.de>
2019-06-07 08:23:25 +0200

    Install pytest in root container
    pytest is needed to execute cobbler tests.

--- susemanager-utils/testing/docker/4.0/suma-4.0-root/add_packages.sh
+++ susemanager-utils/testing/docker/4.0/suma-4.0-root/add_packages.sh
@@ -4,13 +4,14 @@ set -e
 # Packages required to run the pylint
 zypper in -y  make \
               python3 \
-	      python3-PyYAML \
+              python3-PyYAML \
               python3-base \
               python3-configobj \
               python3-devel \
               python3-mock \
-	      python3-nose \
+              python3-nose \
               python3-pylint \
+              python3-pytest \
               python3-urlgrabber
 
 zypper -n in vim less
