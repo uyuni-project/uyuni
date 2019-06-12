25cba883e6879dc1f9f1c6586ddba3f9cbd313a9
Bo Maryniuk <bo@suse.de>
2019-06-12 13:14:59 +0200

    Add unit test for do_errata_list return no data

---
+++ spacecmd/tests/test_errata.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+"""
+Test suite for the errata module.
+"""
+
+from unittest.mock import MagicMock, patch
+from helpers import shell, assert_expect, assert_list_args_expect, assert_args_expect
+import spacecmd.errata
+from xmlrpc import client as xmlrpclib
+
+
+class TestSCErrata:
+    """
+    Test suite for "errata" module.
+    """
+    def test_errata_list_nodata(self, shell):
+        """
+        Test do_errata_list return no data
+
+        :param shell:
+        :return:
+        """
+        shell.generate_errata_cache = MagicMock()
+        shell.all_errata = {"one": None, "two": None}
+        mprint = MagicMock()
+
+        with patch("spacecmd.errata.print", mprint) as prt:
+            out = spacecmd.errata.do_errata_list(shell, "")
+
+        assert out is None
+        assert shell.generate_errata_cache.called
+        assert_expect(mprint.call_args_list, "one\ntwo")
