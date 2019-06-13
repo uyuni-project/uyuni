d6be8f5839a9d38d93379da9f0756c5db2bce4e6
Bo Maryniuk <bo@suse.de>
2019-06-13 14:30:47 +0200

    Add unit test for do_errata_details erratum data.

--- spacecmd/tests/test_errata.py
+++ spacecmd/tests/test_errata.py
@@ -275,3 +275,169 @@ class TestSCErrata:
                                (("cve-one is not a valid erratum",), {}),
                                (("cve-two is not a valid erratum",), {}),
                            ])
+
+    def test_errata_details_erratum_data(self, shell):
+        """
+        Test do_errata_details erratum data.
+
+        :param shell:
+        :return:
+        """
+        shell.help_errata_details = MagicMock()
+        shell.client.errata.getDetails = MagicMock(side_effect=[
+            {
+                "product": "PRODUCT-1", "type": "TYPE-1", "issue_date": "DATE-1",
+                "topic": "The quick brown fox jumped over the lazy dog",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                               "Morbi volutpat felis sem, nec condimentum magna facilisis sed. "
+                               "Vestibulum id ultrices nisi, mattis laoreet turpis. "
+                               "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices "
+                               "posuere cubilia Curae; Nam tincidunt quam quis tellus convallis, "
+                               "auctor aliquet leo porta. Integer ac justo arcu.",
+                "notes": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                         "Morbi volutpat felis sem, nec condimentum magna facilisis sed. "
+                         "Vestibulum id ultrices nisi, mattis laoreet turpis. Vestibulum ante "
+                         "ipsum primis in faucibus orci luctus et ultrices posuere cubilia "
+                         "Curae; Nam tincidunt quam quis tellus convallis, auctor aliquet leo porta. "
+                         "Integer ac justo arcu.",
+                "solution": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                            "Morbi volutpat felis sem, nec condimentum magna facilisis sed. "
+                            "Vestibulum id ultrices nisi, mattis laoreet turpis. "
+                            "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices "
+                            "posuere cubilia Curae; Nam tincidunt quam quis tellus convallis, "
+                            "auctor aliquet leo porta. Integer ac justo arcu.",
+                "references": "AAA:AAA00-00.00 "
+                              "URL:http://foo.test.com/pub/advisory/00 "
+                              "BID:0000 "
+                              "URL:http://www.securityfocus.com/bid/0000 "
+                              "XF:weblogic-http-response-information(00000) "
+                              "URL:http://www.iss.net/security_center/static/00000.php ",
+
+            },
+            {
+                "product": "PRODUCT-2", "type": "TYPE-2", "issue_date": "DATE-2",
+                "topic": "The quick brown fox jumped over the lazy dog",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                               "Morbi volutpat felis sem, nec condimentum magna facilisis sed. "
+                               "Vestibulum id ultrices nisi, mattis laoreet turpis. "
+                               "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices "
+                               "posuere cubilia Curae; Nam tincidunt quam quis tellus convallis, "
+                               "auctor aliquet leo porta. Integer ac justo arcu.",
+                "notes": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                         "Morbi volutpat felis sem, nec condimentum magna facilisis sed. "
+                         "Vestibulum id ultrices nisi, mattis laoreet turpis. Vestibulum ante "
+                         "ipsum primis in faucibus orci luctus et ultrices posuere cubilia "
+                         "Curae; Nam tincidunt quam quis tellus convallis, auctor aliquet leo porta. "
+                         "Integer ac justo arcu.",
+                "solution": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                            "Morbi volutpat felis sem, nec condimentum magna facilisis sed. "
+                            "Vestibulum id ultrices nisi, mattis laoreet turpis. "
+                            "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices "
+                            "posuere cubilia Curae; Nam tincidunt quam quis tellus convallis, "
+                            "auctor aliquet leo porta. Integer ac justo arcu.",
+                "references": "AAA:AAA11-11.11 "
+                              "URL:http://foo.test.com/pub/advisory/11 "
+                              "BID:1111 "
+                              "URL:http://www.securityfocus.com/bid/1111 "
+                              "XF:weblogic-http-response-information(11111) "
+                              "URL:http://www.iss.net/security_center/static/11111.php ",
+
+            },
+        ])
+        shell.client.errata.listPackages = MagicMock(side_effect=[
+            [
+                {"name": "vim", "version": "42", "release": "123", "arch": "x86"},
+                {"name": "pico", "version": "1", "release": "234", "arch": "x86"},
+            ],
+            [
+                {"name": "vim", "version": "28", "release": "45", "arch": "x86"},
+                {"name": "pico", "version": "2", "release": "12", "arch": "x86"},
+            ],
+        ])
+        shell.client.errata.listAffectedSystems = MagicMock(side_effect=[
+            [10001000, 10001002, 10001005],
+            [10001000, 10001001],
+        ])
+        shell.client.errata.listCves = MagicMock(side_effect=[
+            ["CVE-1", "CVE-1a"],
+            ["CVE-2", "CVE-2a", "CVE-2b"],
+        ])
+        shell.client.errata.applicableToChannels = MagicMock(side_effect=[
+            [
+                {"label": "base_channel"}, {"label": "editors_channel"}
+            ],
+            [
+                {"label": "another_base_channel"}, {"label": "special_editors_channel"}
+            ],
+        ])
+        shell.expand_errata = MagicMock(return_value=["cve-one", "cve-two"])
+        mprint = MagicMock()
+        logger = MagicMock()
+
+        with patch("spacecmd.errata.print", mprint) as prt, \
+                patch("spacecmd.errata.logging", logger) as lgr:
+            spacecmd.errata.do_errata_details(shell, "cve*")
+
+        assert not logger.warning.called
+        assert not shell.help_errata_details.called
+        assert shell.client.errata.getDetails.called
+        assert shell.client.errata.listPackages.called
+        assert shell.client.errata.listAffectedSystems.called
+        assert shell.client.errata.listCves.called
+        assert shell.client.errata.applicableToChannels.called
+        assert mprint.called
+
+        assert_list_args_expect(mprint.call_args_list,
+                                ['Name:       cve-one', 'Product:    PRODUCT-1', 'Type:       TYPE-1',
+                                 'Issue Date: DATE-1', '', 'Topic', '-----', 'The quick brown fox jumped over the '
+                                                                             'lazy dog', '', 'Description',
+                                 '-----------',
+                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi\nvolutpat felis sem, '
+                                 'nec condimentum magna facilisis sed. Vestibulum id\nultrices nisi, mattis laoreet '
+                                 'turpis. Vestibulum ante ipsum primis in\nfaucibus orci luctus et ultrices posuere '
+                                 'cubilia Curae; Nam tincidunt\nquam quis tellus convallis, auctor aliquet leo porta. '
+                                 'Integer ac justo\narcu.', '', 'Notes', '-----',
+                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi\nvolutpat felis sem, '
+                                 'nec condimentum magna facilisis sed. Vestibulum id\nultrices nisi, mattis laoreet '
+                                 'turpis. Vestibulum ante ipsum primis in\nfaucibus orci luctus et ultrices posuere '
+                                 'cubilia Curae; Nam tincidunt\nquam quis tellus convallis, auctor aliquet leo porta. '
+                                 'Integer ac justo\narcu.',
+                                 '', 'CVEs', '----', 'CVE-1\nCVE-1a', '', 'Solution', '--------',
+                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi\nvolutpat felis sem, '
+                                 'nec condimentum magna facilisis sed. Vestibulum id\nultrices nisi, mattis laoreet '
+                                 'turpis. Vestibulum ante ipsum primis in\nfaucibus orci luctus et ultrices posuere '
+                                 'cubilia Curae; Nam tincidunt\nquam quis tellus convallis, auctor aliquet leo porta. '
+                                 'Integer ac justo\narcu.', '', 'References', '----------',
+                                 'AAA:AAA00-00.00 URL:http://foo.test.com/pub/advisory/00 BID:0000\n'
+                                 'URL:http://www.securityfocus.com/bid/0000 XF:weblogic-http-response-\n'
+                                 'information(00000)\nURL:http://www.iss.net/security_center/static/00000.php', '',
+                                 'Affected Channels', '-----------------', 'base_channel\neditors_channel', '',
+                                 'Affected Systems', '----------------', '3', '', 'Affected Packages',
+                                 '-----------------', 'pico-1-234:None.x86\nvim-42-123:None.x86', '----------',
+                                 'Name:       cve-two', 'Product:    PRODUCT-2', 'Type:       TYPE-2',
+                                 'Issue Date: DATE-2', '', 'Topic', '-----', 'The quick brown fox jumped over '
+                                                                             'the lazy dog',
+                                 '', 'Description', '-----------',
+                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi\nvolutpat felis sem, '
+                                 'nec condimentum magna facilisis sed. Vestibulum id\nultrices nisi, mattis laoreet '
+                                 'turpis. Vestibulum ante ipsum primis in\nfaucibus orci luctus et ultrices posuere '
+                                 'cubilia Curae; Nam tincidunt\nquam quis tellus convallis, auctor aliquet leo porta. '
+                                 'Integer ac justo\narcu.', '', 'Notes', '-----',
+                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi\nvolutpat felis sem, '
+                                 'nec condimentum magna facilisis sed. Vestibulum id\nultrices nisi, mattis laoreet '
+                                 'turpis. Vestibulum ante ipsum primis in\nfaucibus orci luctus et ultrices posuere '
+                                 'cubilia Curae; Nam tincidunt\nquam quis tellus convallis, auctor aliquet leo porta. '
+                                 'Integer ac justo\narcu.',
+                                 '', 'CVEs', '----', 'CVE-2\nCVE-2a\nCVE-2b', '', 'Solution', '--------',
+                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi\nvolutpat felis sem, '
+                                 'nec condimentum magna facilisis sed. Vestibulum id\nultrices nisi, mattis laoreet '
+                                 'turpis. Vestibulum ante ipsum primis in\nfaucibus orci luctus et ultrices posuere '
+                                 'cubilia Curae; Nam tincidunt\nquam quis tellus convallis, auctor aliquet leo porta. '
+                                 'Integer ac justo\narcu.', '', 'References', '----------',
+                                 'AAA:AAA11-11.11 URL:http://foo.test.com/pub/advisory/11 BID:1111\n'
+                                 'URL:http://www.securityfocus.com/bid/1111 XF:weblogic-http-response-\n'
+                                 'information(11111)\nURL:http://www.iss.net/security_center/static/11111.php',
+                                 '', 'Affected Channels', '-----------------', 'another_base_channel\n'
+                                                                               'special_editors_channel',
+                                 '', 'Affected Systems', '----------------', '2', '', 'Affected Packages',
+                                 '-----------------', 'pico-2-12:None.x86\nvim-28-45:None.x86'])
