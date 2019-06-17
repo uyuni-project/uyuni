29c02cc506297ed2cdba483f07b895146129c1ff
Bo Maryniuk <bo@suse.de>
2019-06-17 15:42:37 +0200

    Add unit test for do_user_create, missing user name

--- spacecmd/tests/test_user.py
+++ spacecmd/tests/test_user.py
@@ -61,3 +61,26 @@ class TestSCUser:
         assert_args_expect(shell.client.user.create.call_args_list,
                            [((shell.session, 'lksw', '1234567890', 'Luke',
                               'Skywalker', 'l.skywalker@suse.com', 0), {})])
+
+    def test_user_create_no_username(self, shell):
+        """
+        Test do_user_create, missing user name
+
+        :param shell:
+        :return:
+        """
+        shell.client.user.create = MagicMock()
+        shell.user_confirm = MagicMock(return_value=True)
+        logger = MagicMock()
+        getps = MagicMock(return_value="1234567890")
+        prompter = MagicMock(side_effect=Exception("Should not happen"))
+        with patch("spacecmd.user.logging", logger) as lgr, \
+                patch("spacecmd.user.prompt_user", prompter) as pmt, \
+                patch("spacecmd.user.getpass", getps) as gpw:
+            spacecmd.user.do_user_create(shell, "-f Luke -l Skywalker "
+                                                "-e l.skywalker@suse.com -p 1234567890")
+
+        assert not shell.client.user.create.called
+        assert not logger.warning.called
+        assert logger.error.called
+        assert_expect(logger.error.call_args_list, "A username is required")
