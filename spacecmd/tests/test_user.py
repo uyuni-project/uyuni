a94108e6ffb19d3ea5523584829002120917f18d
Bo Maryniuk <bo@suse.de>
2019-06-17 15:43:19 +0200

    Add unit test for do_user_create, missing email address

--- spacecmd/tests/test_user.py
+++ spacecmd/tests/test_user.py
@@ -130,3 +130,26 @@ class TestSCUser:
         assert not logger.warning.called
         assert logger.error.called
         assert_expect(logger.error.call_args_list, "A last name is required")
+
+    def test_user_create_no_email(self, shell):
+        """
+        Test do_user_create, missing email address
+
+        :param shell:
+        :return:
+        """
+        shell.client.user.create = MagicMock()
+        shell.user_confirm = MagicMock(return_value=True)
+        logger = MagicMock()
+        getps = MagicMock(return_value="1234567890")
+        prompter = MagicMock(side_effect=Exception("Should not happen"))
+        with patch("spacecmd.user.logging", logger) as lgr, \
+                patch("spacecmd.user.prompt_user", prompter) as pmt, \
+                patch("spacecmd.user.getpass", getps) as gpw:
+            spacecmd.user.do_user_create(shell, "-u lksw -f Luke -l Skywalker "
+                                                "-p 1234567890")
+
+        assert not shell.client.user.create.called
+        assert not logger.warning.called
+        assert logger.error.called
+        assert_expect(logger.error.call_args_list, "An email address is required")
