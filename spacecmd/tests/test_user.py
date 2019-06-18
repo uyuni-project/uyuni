ec4ed5ab8751f3c781cee11f3d2b83b32f9ca7f0
Bo Maryniuk <bo@suse.de>
2019-06-18 16:24:34 +0200

    Add unit test for do_user_listavailableroles, no data return, roles found.

--- spacecmd/tests/test_user.py
+++ spacecmd/tests/test_user.py
@@ -471,3 +471,22 @@ class TestSCUser:
         assert not mprint.called
         assert not logger.error.called
         assert out == []
+
+    def test_user_listavailableroles_with_data_with_roles(self, shell):
+        """
+        test do_user_listavailableroles, no data return, roles found.
+
+        :param shell:
+        :return:
+        """
+        shell.client.user.listAssignableRoles = MagicMock(return_value=["bofh", "coffee"])
+        mprint = MagicMock()
+        logger = MagicMock()
+
+        with patch("spacecmd.user.print", mprint) as prt, \
+                patch("spacecmd.user.logging", logger) as lgr:
+            out = spacecmd.user.do_user_listavailableroles(shell, "", doreturn=True)
+        assert out is not None
+        assert not logger.error.called
+        assert not mprint.called
+        assert out == ["bofh", "coffee"]
