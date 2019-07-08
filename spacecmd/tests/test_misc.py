eb4676f59d157357283737c0d7dda14a8c45a138
Bo Maryniuk <bo@suse.de>
2019-07-08 10:53:23 +0200

    Add unit test for clear caches.

---
+++ spacecmd/tests/test_misc.py
@@ -0,0 +1,38 @@
+# coding utf-8
+"""
+Test spacecmd.misc
+"""
+from unittest.mock import MagicMock, patch, mock_open
+import pytest
+from helpers import shell, assert_expect, assert_list_args_expect, assert_args_expect
+import spacecmd.misc
+from xmlrpc import client as xmlrpclib
+import os
+import tempfile
+import shutil
+import datetime
+import pickle
+import hashlib
+import time
+
+
+class TestSCMisc:
+    """
+    Test suite for misc methods/funtions.
+    """
+    def test_clear_caches(self, shell):
+        """
+        Test clear caches.
+
+        :param shell:
+        :return:
+        """
+        shell.clear_system_cache = MagicMock()
+        shell.clear_package_cache = MagicMock()
+        shell.clear_errata_cache = MagicMock()
+
+        spacecmd.misc.do_clear_caches(shell, "")
+
+        assert shell.clear_system_cache.called
+        assert shell.clear_package_cache.called
+        assert shell.clear_errata_cache.called
