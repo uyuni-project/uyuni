c46aa2d2beb76a87fca6e73572faef624fd56b8c
Bo Maryniuk <bo@suse.de>
2019-07-11 11:52:35 +0200

    Add unit test for configuration channel list, returning data

--- spacecmd/tests/test_configchannel.py
+++ spacecmd/tests/test_configchannel.py
@@ -36,3 +36,25 @@ class TestSCConfigChannel:
 
         assert_expect(mprint.call_args_list,
                       'and_some_channel\nanother_channel\nbase_channel\nboese_channel\nother_channel\nze_channel')
+
+    def test_configchannel_list_data(self, shell):
+        """
+        Test configuration channel list, return data.
+
+        :param shell:
+        :return:
+        """
+        shell.client.configchannel.listGlobals = MagicMock(return_value=[
+            {"label": "base_channel"}, {"label": "boese_channel"},
+            {"label": "other_channel"}, {"label": "ze_channel"},
+            {"label": "and_some_channel"}, {"label": "another_channel"}
+        ])
+        mprint = MagicMock()
+        with patch("spacecmd.configchannel.print", mprint) as prt:
+            ret = spacecmd.configchannel.do_configchannel_list(shell, "", doreturn=True)
+
+        assert not mprint.called
+        assert ret is not None
+        assert shell.client.configchannel.listGlobals.called
+        assert ret == ['and_some_channel', 'another_channel', 'base_channel',
+                       'boese_channel', 'other_channel', 'ze_channel']
