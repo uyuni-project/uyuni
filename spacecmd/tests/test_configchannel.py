245e6151e331f4e87c12d9eac9f9be0e22b20fa4
Bo Maryniuk <bo@suse.de>
2019-07-11 11:52:11 +0200

    Add unit test for configuration channel listing with no data return

---
+++ spacecmd/tests/test_configchannel.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+"""
+Configchannel module unit tests.
+"""
+
+from unittest.mock import MagicMock, patch, mock_open
+from helpers import shell, assert_expect, assert_list_args_expect, assert_args_expect
+import spacecmd.configchannel
+from xmlrpc import client as xmlrpclib
+import datetime
+
+
+class TestSCConfigChannel:
+    """
+    Test configuration channel.
+    """
+    def test_configchannel_list_noret(self, shell):
+        """
+        Test configuration channel list, no data return.
+
+        :param shell:
+        :return:
+        """
+        shell.client.configchannel.listGlobals = MagicMock(return_value=[
+            {"label": "base_channel"}, {"label": "boese_channel"},
+            {"label": "other_channel"}, {"label": "ze_channel"},
+            {"label": "and_some_channel"}, {"label": "another_channel"}
+        ])
+        mprint = MagicMock()
+        with patch("spacecmd.configchannel.print", mprint) as prt:
+            ret = spacecmd.configchannel.do_configchannel_list(shell, "", doreturn=False)
+
+        assert ret is None
+        assert mprint.called
+        assert shell.client.configchannel.listGlobals.called
+
+        assert_expect(mprint.call_args_list,
+                      'and_some_channel\nanother_channel\nbase_channel\nboese_channel\nother_channel\nze_channel')
