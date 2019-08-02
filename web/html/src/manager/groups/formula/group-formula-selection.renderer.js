d6a28c87e3921bad851cc7a535add7ac54ee9a24
Ricardo Mateus <rmateus@suse.com>
2019-08-02 11:16:24 +0100

    UI refactor for minion and group formulas (bsc#1142309)
    Signed-off-by: Ricardo Mateus <rmateus@suse.com>

--- web/html/src/manager/groups/formula/group-formula-selection.renderer.js
+++ web/html/src/manager/groups/formula/group-formula-selection.renderer.js
@@ -8,59 +8,70 @@ const Network = require("../../../utils/network");
 const FormulaSelection = require("../../../components/formula-selection").FormulaSelection;
 const capitalize = require("../../../utils/functions").Utils.capitalize;
 
-const messageTexts = {
-  "formulas_saved" : <p>{t("Formula saved. Apply the ")}<a href={'/rhn/manager/groups/details/highstate?sgid=' + groupId}>{t("Highstate")}</a>{t(" for the changes to take effect.")}</p>,
-  "error_invalid_target" : t("Invalid target type.")
-}
+window.pageRenderers = window.pageRenderers || {};
+window.pageRenderers.group = window.pageRenderers.group || {};
+window.pageRenderers.group.formulaSelection = window.pageRenderers.group.formulaSelection || {};
+window.pageRenderers.group.formulaSelection.renderer = (renderId, {groupId, warningMessage}) => {
 
-function getMessageText(msg) {
-  return messageTexts[msg] ? t(messageTexts[msg]) : msg;
-}
+  const messageTexts = {
+    "formulas_saved": <p>{t("Formula saved. Apply the ")}<a
+      href={'/rhn/manager/groups/details/highstate?sgid=' +
+        groupId}>{t("Highstate")}</a>{t(" for the changes to take effect.")}
+    </p>,
+    "error_invalid_target": t("Invalid target type.")
+  }
 
-function saveRequest(component, selectedFormulas) {
+  function getMessageText(msg) {
+    return messageTexts[msg] ? t(messageTexts[msg]) : msg;
+  }
+
+  function saveRequest(component, selectedFormulas) {
     const formData = {};
     formData.type = "GROUP";
     formData.id = groupId;
     formData.selected = selectedFormulas;
 
     return Network.post(
-        "/rhn/manager/api/formulas/select",
-        JSON.stringify(formData),
-        "application/json"
+      "/rhn/manager/api/formulas/select",
+      JSON.stringify(formData),
+      "application/json"
     ).promise.then(data => {
         component.setState({
-            messages: data.map(msg => getMessageText(msg))
+          messages: data.map(msg => getMessageText(msg))
         });
-    },
-    (xhr) => {
+      },
+      (xhr) => {
         try {
-            component.setState({
-                errors: [JSON.parse(xhr.responseText)]
-            })
+          component.setState({
+            errors: [JSON.parse(xhr.responseText)]
+          })
         } catch (err) {
-            component.setState({
-                errors: [Network.errorMessageByStatus(xhr.status)]
-            })
+          component.setState({
+            errors: [Network.errorMessageByStatus(xhr.status)]
+          })
         }
-    });
-}
+      });
+  }
 
-function addFormulaNavBar(formulaList) {
+  function addFormulaNavBar(formulaList) {
     $("#formula-nav-bar").remove();
 
     var navBar = "<ul class='nav nav-tabs nav-tabs-pf' id='formula-nav-bar'>\n"
     navBar += "<li class='active'><a href='/rhn/manager/groups/details/formulas?sgid=" + groupId + "'>" + t("Formulas") + "</a></li>\n";
     for (var i in formulaList)
-        navBar += "<li><a href='/rhn/manager/groups/details/formula/" + i + "?sgid=" + groupId + "'>" + capitalize(formulaList[i]) + "</a></li>\n";
+      navBar += "<li><a href='/rhn/manager/groups/details/formula/" + i + "?sgid=" + groupId + "'>" + capitalize(formulaList[i]) + "</a></li>\n";
     navBar += "</ul>"
     $(".spacewalk-content-nav").append(navBar);
-}
+  }
 
-ReactDOM.render(
-  <FormulaSelection
-    dataUrl={"/rhn/manager/api/formulas/list/GROUP/" + groupId}
-    saveRequest={saveRequest}
-    systemGroupId={groupId}
-    addFormulaNavBar={addFormulaNavBar} />,
-  document.getElementById('formulas')
-);
+  ReactDOM.render(
+    <FormulaSelection
+      dataUrl={"/rhn/manager/api/formulas/list/GROUP/" + groupId}
+      saveRequest={saveRequest}
+      systemGroupId={groupId}
+      addFormulaNavBar={addFormulaNavBar}
+      warningMessage={warningMessage}
+    />,
+    document.getElementById(renderId)
+  );
+}
--- web/html/src/manager/groups/index.js
+++ web/html/src/manager/groups/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   entries: [
-    'formula/group-formula.js',
-    'formula/group-formula-selection.js',
+    'formula/group-formula.renderer.js',
+    'formula/group-formula-selection.renderer.js',
     'config-channels/group-config-channels.js',
   ]
 };
