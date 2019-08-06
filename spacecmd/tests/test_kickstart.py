c28203959447b3bb693a28df6cf42e5e2bd62fa4
Bo Maryniuk <bo@suse.de>
2019-08-06 16:51:27 +0200

    Add unit test for do_kickstart_removeactivationkeys wrong args, interactive, accept.

--- spacecmd/tests/test_kickstart.py
+++ spacecmd/tests/test_kickstart.py
@@ -761,3 +761,17 @@ echo 'some more hello'
         assert not shell.help_kickstart_removeactivationkeys.called
         assert not shell.client.kickstart.profile.keys.removeActivationKey.called
         assert shell.user_confirm.called
+
+    def test_kickstart_removeactivationkeys_interactive_accept(self, shell):
+        """
+        Test do_kickstart_removeactivationkeys wrong args, interactive, accept.
+
+        :param shell:
+        :return:
+        """
+        shell.user_confirm = MagicMock(return_value=True)
+        spacecmd.kickstart.do_kickstart_removeactivationkeys(shell, "profile key1 key2")
+
+        assert not shell.help_kickstart_removeactivationkeys.called
+        assert shell.client.kickstart.profile.keys.removeActivationKey.called
+        assert shell.user_confirm.called
