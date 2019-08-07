adf5a2ae5271b501bc0b5d1ac65eb7ae675ff5cd
Luis Neves <luneves@suse.de>
2019-08-07 13:02:57 +0100

    Fix comments for user localization react components

--- web/html/src/core/user-localization/use-user-localization.js
+++ web/html/src/core/user-localization/use-user-localization.js
@@ -1,5 +1,6 @@
 // @flow
-// Note: To use this component make sure it's used in a placed wrapped by the roles-context-provider
+// Note: To use this component make sure it's used in a page wrapped by the UserLocalizationProvider
+// (user-localization-context.js)
 import {useContext} from 'react';
 import {UserLocalizationContext} from "core/user-localization/user-localization-context";
 
