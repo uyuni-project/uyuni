885b689b33bf8e33f18d7913f93817c9fdb9f9d1
Luis Neves <luneves@suse.de>
2019-08-08 15:00:20 +0100

    clean TODO and revert removed javascript function

--- web/html/src/core/spa/spa-renderer.js
+++ web/html/src/core/spa/spa-renderer.js
@@ -27,7 +27,6 @@ function renderNavigationReact(element: ReactElement<any>, container: Element) {
       container,
       clean: () => ReactDOM.unmountComponentAtNode(container)
     });
-  // TODO: review this onDocumentReadyInitOldJS to run only once
   ReactDOM.render(element, container, () => onDocumentReadyInitOldJS());
 }
 
