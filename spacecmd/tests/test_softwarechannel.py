3fce5babb412ddbc7f7f5458635137c4afadc870
Bo Maryniuk <bo@suse.de>
2019-08-08 14:05:12 +0200

    Add unit test for do_softwarechannel_list without args.

--- spacecmd/tests/test_softwarechannel.py
+++ spacecmd/tests/test_softwarechannel.py
@@ -31,3 +31,20 @@ class TestSCSoftwareChannel:
         assert not shell.list_child_channels.called
         assert not shell.list_base_channels.called
 
+    def test_softwarechannel_list_noreturn_nolabels(self, shell):
+        """
+        Test do_softwarechannel_list without args.
+
+        :param shell:
+        :return:
+        """
+        shell.client.channel.listAllChannels = MagicMock(return_value=[])
+        mprint = MagicMock()
+        with patch("spacecmd.softwarechannel.print", mprint) as prt:
+            out = spacecmd.softwarechannel.do_softwarechannel_list(shell, "", doreturn=False)
+        assert out is None
+        assert not shell.help_softwarechannel_list.called
+        assert not shell.client.channel.software.getDetails.called
+        assert not shell.list_child_channels.called
+        assert not shell.list_base_channels.called
+
