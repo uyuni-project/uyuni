46826f6c8a7f5cbd6405e69f060c84a708036f80
Frantisek Kobzik <fkobzik@suse.de>
2019-08-12 15:33:37 +0200

    Backend: Filter errata by synopsis (contains matcher) in content lifecycle mgmt

--- java/code/src/com/redhat/rhn/domain/contentmgmt/ErrataFilter.java
+++ java/code/src/com/redhat/rhn/domain/contentmgmt/ErrataFilter.java
@@ -58,6 +58,13 @@ public class ErrataFilter extends ContentFilter<Errata> {
                     default:
                         throw new UnsupportedOperationException("Matcher " + matcher + " not supported");
                 }
+            case "synopsis":
+                switch (matcher) {
+                    case EQUALS:
+                        return getField(erratum, field, String.class).equals(value);
+                    default:
+                        throw new UnsupportedOperationException("Matcher " + matcher + " not supported");
+                }
             default:
                 throw new UnsupportedOperationException("Field " + field + " not supported");
         }
@@ -69,6 +76,8 @@ public class ErrataFilter extends ContentFilter<Errata> {
                 return type.cast(erratum.getAdvisoryName());
             case "issue_date":
                 return type.cast(erratum.getIssueDate().toInstant().atZone(ZoneId.systemDefault()));
+            case "synopsis":
+                return type.cast(erratum.getSynopsis());
             default:
                 throw new UnsupportedOperationException("Field " + field + " not supported");
         }
