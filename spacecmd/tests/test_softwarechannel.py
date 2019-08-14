275b08a112d22fb9e40d7cd2f6d725e9a29da4f2
Bo Maryniuk <bo@suse.de>
2019-08-14 15:38:26 +0200

    Add unit test for do_softwarechannel_list with label, no return data, standard output

--- spacecmd/tests/test_softwarechannel.py
+++ spacecmd/tests/test_softwarechannel.py
@@ -6,7 +6,7 @@ Test software channel module.
 from mock import MagicMock, patch
 import spacecmd.softwarechannel
 from xmlrpc import client as xmlrpclib
-from helpers import shell
+from helpers import shell, assert_expect, assert_list_args_expect, assert_args_expect
 
 
 class TestSCSoftwareChannel:
@@ -47,4 +47,30 @@ class TestSCSoftwareChannel:
         assert not shell.client.channel.software.getDetails.called
         assert not shell.list_child_channels.called
         assert not shell.list_base_channels.called
+
+    def test_softwarechannel_list_noreturn_labels_std(self, shell):
+        """
+        Test do_softwarechannel_list with label, no return data, standard output.
+
+        :param shell:
+        :return:
+        """
+        shell.client.channel.listAllChannels = MagicMock(return_value=[
+            {"label": "label-one"}, {"label": "label-last"},
+            {"label": "label-two"}, {"label": "label-three"},
+        ])
+
+        mprint = MagicMock()
+        with patch("spacecmd.softwarechannel.print", mprint) as prt:
+            out = spacecmd.softwarechannel.do_softwarechannel_list(
+                shell, "", doreturn=False)
+
+        assert out is None
+        assert not shell.help_softwarechannel_list.called
+        assert not shell.client.channel.software.getDetails.called
+        assert not shell.list_child_channels.called
+        assert shell.client.channel.listAllChannels.called
+
+        assert_list_args_expect(mprint.call_args_list,
+                                ["label-last", "label-one", "label-three", "label-two"])
 
