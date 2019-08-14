0e5cf8939a9e077dd6821c55347726ef439de722
Frantisek Kobzik <fkobzik@suse.de>
2019-08-14 15:00:24 +0200

    Refactor: move the cloneErrata method

--- java/code/src/com/redhat/rhn/manager/errata/ErrataManager.java
+++ java/code/src/com/redhat/rhn/manager/errata/ErrataManager.java
@@ -2249,4 +2249,52 @@ public class ErrataManager extends BaseManager {
         params.put("earliest", new Timestamp(System.currentTimeMillis()));
         mode.executeUpdate(params);
     }
+
+    public static void cloneErrata(Long channelId, Collection<Long> errataToClone, boolean requestRepodataRegen,
+            User user) {
+        Channel channel = ChannelFactory.lookupById(channelId);
+        if (channel == null) {
+            log.error("Failed to clone errata " + errataToClone + " Didn't find channel with id: " +
+                    channelId.toString());
+            return;
+        }
+        Collection<Long> list = errataToClone;
+        List<Long> cids = new ArrayList<Long>();
+        cids.add(channel.getId());
+        // let's avoid deadlocks please
+        ChannelFactory.lock(channel);
+
+        for (Long eid : list) {
+            Errata errata = ErrataFactory.lookupById(eid);
+            // we merge custom errata directly (non Redhat and cloned)
+            if (errata.getOrg() != null) {
+                errata.addChannel(channel);
+                ErrataCacheManager.insertCacheForChannelErrata(cids, errata);
+                errata.addChannelNotification(channel, new Date());
+            }
+            else {
+                Set<Channel> channelSet = new HashSet<>();
+                channelSet.add(channel);
+
+                List<Errata> clones = lookupPublishedByOriginal(user, errata);
+                if (clones.size() == 0) {
+                    log.debug("Cloning errata");
+                    Errata published = PublishErrataHelper.cloneErrataFast(errata, user.getOrg());
+                    published.setChannels(channelSet);
+                    ErrataCacheManager.insertCacheForChannelErrata(cids, published);
+                    published.addChannelNotification(channel, new Date());
+                }
+                else {
+                    log.debug("Re-publishing clone");
+                    publish(clones.get(0), cids, user);
+                }
+            }
+        }
+        // Trigger channel repodata re-generation
+        if (list.size() > 0 && requestRepodataRegen) {
+            channel.setLastModified(new Date());
+            ChannelFactory.save(channel);
+            ChannelManager.queueChannelChange(channel.getLabel(), "java::cloneErrata", "Errata cloned");
+        }
+    }
 }
