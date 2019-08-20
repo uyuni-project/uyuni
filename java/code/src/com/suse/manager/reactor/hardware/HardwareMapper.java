00ac28b4b395757564e412d8b27f9c9e7a4e67a5
Cédric Bosdonnat <cbosdonnat@suse.com>
2019-08-20 16:21:00 +0200

    Handle refreshing hardware of VM with changed UUID
    For some reason, like an external script redefining VMs, we may end up
    with a VM that changed UUID. In such a case the hardware mapper would
    create a new entry for in the rhnVirtualInstance table and we would get
    an error like:
    
        More than one row with the given identifier was found: 1000012177,
        for class: com.redhat.rhn.domain.server.VirtualInstance.
    
    In such a case, the VM was already a registered guest and already has a
    virtual system id that we can use to retrieve the VirtualInstance and
    update it.

--- java/code/src/com/suse/manager/reactor/hardware/HardwareMapper.java
+++ java/code/src/com/suse/manager/reactor/hardware/HardwareMapper.java
@@ -640,10 +640,26 @@ public class HardwareMapper {
             }
 
             if (virtualInstances.isEmpty()) {
-                VirtualInstanceManager.addGuestVirtualInstance(
-                        virtUuid, server.getName(), type,
-                        VirtualInstanceFactory.getInstance().getRunningState(),
-                        null, server);
+                // For some reason, the uuid of the VM may have changed.
+                // Check if we already have a VM with the same virtual_system_id.
+                VirtualInstance virtualInstance = VirtualInstanceFactory.getInstance().lookupByGuestId(server.getId());
+                if (virtualInstance == null) {
+                    VirtualInstanceManager.addGuestVirtualInstance(
+                            virtUuid, server.getName(), type,
+                            VirtualInstanceFactory.getInstance().getRunningState(),
+                            null, server);
+                }
+                else {
+                    String name = virtualInstance.getName();
+                    virtualInstance.setUuid(virtUuid);
+                    if (StringUtils.isBlank(name)) {
+                        // use minion name only when the hypervisor name is unknown
+                        name = server.getName();
+                    }
+                    VirtualInstanceManager.updateGuestVirtualInstance(virtualInstance, name,
+                            VirtualInstanceFactory.getInstance().getRunningState(),
+                            virtualInstance.getHostSystem(), server);
+                }
             }
             else {
                 virtualInstances.forEach(virtualInstance -> {
