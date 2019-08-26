3c6edb785e2e781ce1749f3fa674f01d29d2bb6a
Michele Bologna <michele.bologna@suse.com>
2019-08-26 21:50:48 +0200

    Refactor: remove everything not used anymore

--- web/html/javascript/spacewalk-essentials.js
+++ web/html/javascript/spacewalk-essentials.js
@@ -429,22 +429,6 @@ function enterKeyHandler(event, $button) {
     }
 }
 
-/**
- * Translates a string, implemented now as a 'true-bypass',
- * with placeholder replacement like Java's MessageFormat class.
- * Accepts any number of arguments after key.
- */
-function t(key) {
-  var result = key;
-
-  // Minimal implementation of https://docs.oracle.com/javase/7/docs/api/java/text/MessageFormat.html
-  for (var i=1; i<arguments.length; i++) {
-    result = result.replace('{' + (i-1) + '}', arguments[i]);
-  }
-
-  return result;
-}
-
 function adjustSpacewalkContent() {
   alignContentDimensions();
   // trigger the 'spacewalk-section-toolbar' (sst) handler on content change
