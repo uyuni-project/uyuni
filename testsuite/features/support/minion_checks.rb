da23e330d48669129197c241f9aa01791c447ca6
Eric Bischoff <ebischoff@suse.com>
2019-09-04 15:44:41 +0200

    Accept openSUSE as OS in client summary

--- testsuite/features/support/minion_checks.rb
+++
@@ -1,7 +0,0 @@
-# check if the minion is a SLE-15
-def minion_is_sle15
-  node = get_target('sle-minion')
-  os_version, os_family = get_os_version(node)
-  return false if os_version.nil? || os_family.nil?
-  (os_version =~ /^15/) && (os_family =~ /^sles/)
-end
