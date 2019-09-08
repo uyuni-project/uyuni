6f53e7860af58edb13115137e357ad9ad7defbf1
Luis Neves <luneves@suse.de>
2019-09-09 00:38:44 +0100

    Move all dependencies to nodejs-sdk and clean before upload to OBS

--- susemanager-frontend/susemanager-nodejs-sdk-devel/setup.sh
+++ susemanager-frontend/susemanager-nodejs-sdk-devel/setup.sh
@@ -2,5 +2,7 @@ set -euxo pipefail
 (cd susemanager-frontend/susemanager-nodejs-sdk-devel; rm -rf node_modules)
 yarn install --frozen-lockfile
 yarn autoclean --force
+(cd susemanager-frontend/susemanager-nodejs-sdk-devel; yarn run remove-packages-before-obs-zip)
 (cd susemanager-frontend/susemanager-nodejs-sdk-devel; yarn zip)
+(cd susemanager-frontend/susemanager-nodejs-sdk-devel; yarn run restore-packages-after-obs-zip)
 echo "susemanager-nodejs-modules.tar.gz"
