6131eb631aa0eaa5579902303ebc40d2d3eb1467
Luis Neves <luneves@suse.de>
2019-09-15 21:16:37 +0100

    New js entry points for virtualization

--- web/html/src/manager/virtualization/pools/list/pools-list.renderer.js
+++ web/html/src/manager/virtualization/pools/list/pools-list.renderer.js
@@ -2,10 +2,7 @@ import * as React from 'react';
 import { PoolsList } from './pools-list';
 import SpaRenderer from "core/spa/spa-renderer";
 
-window.pageRenderers = window.pageRenderers || {};
-window.pageRenderers.pools = window.pageRenderers.pools || {};
-window.pageRenderers.pools.list = window.pageRenderers.pools.list || {};
-window.pageRenderers.pools.list.renderer = (id, { serverId, pageSize }) => {
+export const renderer = (id, { serverId, pageSize }) => {
   SpaRenderer.renderNavigationReact(
     <PoolsList
       refreshInterval={5 * 1000}
