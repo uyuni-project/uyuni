65b2e8b31a945ea71b2b3d98f8d6f8c6e26273a9
Luis Neves <luneves@suse.de>
2019-09-15 21:18:42 +0100

    New js entry points for notifications

--- web/html/src/manager/notifications/notification-messages.renderer.js
+++ web/html/src/manager/notifications/notification-messages.renderer.js
@@ -2,7 +2,7 @@ const SpaRenderer  = require("core/spa/spa-renderer").default;
 const React = require('react');
 const { NotificationMessages } = require('./notification-messages');
 
-SpaRenderer.renderNavigationReact(
+export const renderer = () => SpaRenderer.renderNavigationReact(
   <NotificationMessages />,
   document.getElementById('notification-messages'),
 );
