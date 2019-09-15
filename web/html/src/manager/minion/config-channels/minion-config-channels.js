2866b6cec812c2f0c1e9347cf69fef43daef7186
Luis Neves <luneves@suse.de>
2019-09-15 21:24:29 +0100

    New js entry points for minions

--- web/html/src/manager/minion/config-channels/minion-config-channels.js
+++ web/html/src/manager/minion/config-channels/minion-config-channels.js
@@ -46,7 +46,7 @@ function saveRequest(states) {
     );
 }
 
-SpaRenderer.renderNavigationReact(
+export const renderer = () => SpaRenderer.renderNavigationReact(
   <ConfigChannels matchUrl={matchUrl} saveRequest={saveRequest} applyRequest={applyRequest}/>,
   document.getElementById('config-channels')
 );
