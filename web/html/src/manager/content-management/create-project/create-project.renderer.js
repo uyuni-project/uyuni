1c91b841d0a737cb01ed686ced6f6586c74d943e
Luis Neves <luneves@suse.de>
2019-09-15 21:15:36 +0100

    New js entry points for content lifecycle management

--- web/html/src/manager/content-management/create-project/create-project.renderer.js
+++ web/html/src/manager/content-management/create-project/create-project.renderer.js
@@ -3,10 +3,7 @@ import CreateProject from './create-project';
 import {RolesProvider} from "core/auth/roles-context";
 import SpaRenderer from "core/spa/spa-renderer";
 
-window.pageRenderers = window.pageRenderers || {};
-window.pageRenderers.contentManagement = window.pageRenderers.contentManagement || {};
-window.pageRenderers.contentManagement.createProject = window.pageRenderers.contentManagement.createProject || {};
-window.pageRenderers.contentManagement.createProject.renderer = (id) => {
+export const renderer = (id) => {
   SpaRenderer.renderNavigationReact(
     <RolesProvider>
       <CreateProject />
--- web/html/src/manager/content-management/project/project.renderer.js
+++ web/html/src/manager/content-management/project/project.renderer.js
@@ -3,10 +3,7 @@ import Project from './project';
 import {RolesProvider} from "core/auth/roles-context";
 import SpaRenderer from "core/spa/spa-renderer";
 
-window.pageRenderers = window.pageRenderers || {};
-window.pageRenderers.contentManagement = window.pageRenderers.contentManagement || {};
-window.pageRenderers.contentManagement.project = window.pageRenderers.contentManagement.project || {};
-window.pageRenderers.contentManagement.project.renderer = (id, {project, wasFreshlyCreatedMessage} = {}) => {
+export const renderer = (id, {project, wasFreshlyCreatedMessage} = {}) => {
   let projectJson = {};
   try{
     projectJson = JSON.parse(project);
