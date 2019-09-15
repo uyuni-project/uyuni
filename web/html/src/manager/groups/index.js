3672c6961c7e6a59377afa7e65cbafee108a3f90
Luis Neves <luneves@suse.de>
2019-09-15 21:24:47 +0100

    New js entry points for groups

--- web/html/src/manager/groups/index.js
+++ web/html/src/manager/groups/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-  entries: [
-    'formula/group-formula.renderer.js',
-    'formula/group-formula-selection.renderer.js',
-    'config-channels/group-config-channels.js',
-  ]
-};
+export default {
+  'groups/formula/group-formula': () => import('./formula/group-formula.renderer'),
+  'groups/formula/group-formula-selection': () => import('./formula/group-formula-selection.renderer'),
+  'groups/config-channels/group-config-channels': () => import('./config-channels/group-config-channels')
+}
