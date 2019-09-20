0ed845528677cf22cf24c3cd20ccc6a65108a4d2
Bo Maryniuk <bo@suse.de>
2019-09-20 16:56:59 +0200

    Trim URL query from target filename

--- backend/satellite_tools/repo_plugins/deb_src.py
+++ backend/satellite_tools/repo_plugins/deb_src.py
@@ -126,7 +126,7 @@ class DebRepo(object):
                                     verify=self.sslcacert)
                 if not data.ok:
                     return ''
-                filename = self.basecachedir + '/' + os.path.basename(url)
+                filename = self.basecachedir + '/' + os.path.basename(urlparse.urlparse(url).path)
                 fd = open(filename, 'wb')
                 try:
                     for chunk in data.iter_content(chunk_size=1024):
