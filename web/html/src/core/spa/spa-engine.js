fb1631cbb6fbed293778dfa86a288e3ee86b74af
Luis Neves <luneves@suse.de>
2019-09-23 23:41:37 +0100

    SPA - redirect to login page when the direct link is from the login page

--- web/html/src/core/spa/spa-engine.js
+++ web/html/src/core/spa/spa-engine.js
@@ -54,6 +54,12 @@ window.pageRenderers.spaengine.init = function init() {
       $('.modal').remove();
       $('.modal-backdrop').remove();
       $('body').removeClass( "modal-open" );
+
+      let urlParser = document.createElement('a');
+      urlParser.href = navigation.path;
+      if(isLoginPage(urlParser.pathname)) {
+        window.location = navigation.path;
+      }
     })
 
     appInstance.on('endNavigate', function(navigation) {
