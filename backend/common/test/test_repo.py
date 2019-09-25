4141851c2df0db5e699174c9d14ef357eef62f8b
Bo Maryniuk <bo@suse.de>
2019-09-25 11:57:56 +0200

    Add unit test for decompression for Packages.gz file failure handling.

--- backend/common/test/test_repo.py
+++ backend/common/test/test_repo.py
@@ -212,3 +212,24 @@ Some more irrelevant data
         assert not zdcmp.called
         assert xdcmp.called
         assert out == "макарони"
+
+    @patch("spacewalk.common.repo.DpkgRepo.get_pkg_index_raw", MagicMock(return_value=("Packages.gz", "content")))
+    def test_decompress_pkg_index_gz_failure(self):
+        """
+        Test decompression for Packages.gz file failure handling.
+
+        :return:
+        """
+        zdcmp = MagicMock(side_effect=DpkgRepoException("Too many symlinks found in binary data"))
+        xdcmp = MagicMock(side_effect=DpkgRepoException(""))
+        with patch("spacewalk.common.repo.zlib.decompress", zdcmp) as m_zlib, \
+            patch("spacewalk.common.repo.lzma.decompress", xdcmp) as m_lzma:
+            with pytest.raises(DpkgRepoException) as exc:
+                DpkgRepo("http://dummy_url").decompress_pkg_index()
+
+        assert not xdcmp.called
+        assert zdcmp.called
+
+        err = str(exc.value)
+        assert err.startswith("Unhandled exception occurred while decompressing Packages.gz:")
+        assert "symlinks" in err
