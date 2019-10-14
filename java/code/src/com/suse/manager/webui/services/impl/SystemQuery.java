73c001245a12914cdd1a2ea785add39ee5765026
Kevin Walter <kwalter@suse.de>
2019-10-14 16:05:09 +0200

    replace callAsync with specific methods

--- java/code/src/com/suse/manager/webui/services/impl/SystemQuery.java
+++ java/code/src/com/suse/manager/webui/services/impl/SystemQuery.java
@@ -82,6 +82,8 @@ public interface SystemQuery {
 
     Optional<List<String>> cleanupMinion(MinionServer minion, int timeout);
 
+    void notifySystemIdGenerated(MinionServer minion) throws InstantiationException, SaltException;
+
     @Deprecated
     <R> Optional<R> callSync(LocalCall<R> call, String minionId);
     @Deprecated
@@ -89,8 +91,6 @@ public interface SystemQuery {
     @Deprecated
     <T> Optional<LocalAsyncResult<T>> callAsync(LocalCall<T> callIn, Target<?> target,
                                                        Optional<ScheduleMetadata> metadataIn) throws SaltException;
-    @Deprecated
-    <T> Optional<LocalAsyncResult<T>> callAsync(LocalCall<T> callIn, Target<?> target) throws SaltException;
 
     Optional<MgrUtilRunner.ExecResult> collectKiwiImage(MinionServer minion, String filepath,
                                                                String imageStore);
