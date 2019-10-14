b3432e89dbe39b5009a27a25b448f97b4db65020
Kevin Walter <kwalter@suse.de>
2019-10-14 14:43:35 +0200

    refine getAllContainers

--- java/code/src/com/suse/manager/webui/services/impl/SaltService.java
+++ java/code/src/com/suse/manager/webui/services/impl/SaltService.java
@@ -1127,7 +1127,7 @@ public class SaltService implements SystemQuery {
      * @param context kubeconfig context to use
      * @return a list of containers
      */
-    public Optional<MgrK8sRunner.ContainersList> getAllContainers(String kubeconfig,
+    public Optional<List<MgrK8sRunner.Container>> getAllContainers(String kubeconfig,
                                                         String context) {
         RunnerCall<MgrK8sRunner.ContainersList> call =
                 MgrK8sRunner.getAllContainers(kubeconfig, context);
@@ -1158,7 +1158,7 @@ public class SaltService implements SystemQuery {
                         throw new NoSuchElementException();
                     }
                 )
-        );
+        ).map(s -> s.getContainers());
     }
 
     /**
