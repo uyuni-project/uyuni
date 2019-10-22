278007e08fad52f4a41b6e09b0ac9afb3f4faea8
Michael Calmer <mc@suse.de>
2019-10-22 11:31:26 +0200

    split PKGLIST15 into salt and traditional and use only salt for not uyuni openSUSE

--- susemanager/src/mgr_bootstrap_data.py
+++ susemanager/src/mgr_bootstrap_data.py
@@ -294,7 +294,36 @@ RES7 = [
     "dmidecode",
 ]
 
-PKGLIST15 = [
+PKGLIST15_SALT = [
+    "libpgm-5_2-0",
+    "libsodium23",
+    "libzmq5",
+    "python3-Babel",
+    "python3-certifi",
+    "python3-chardet",
+    "python3-idna",
+    "python3-Jinja2",
+    "python3-MarkupSafe",
+    "python3-msgpack",
+    "python3-psutil",
+    "python3-py",
+    "python3-pycrypto",
+    "python3-pytz",
+    "python3-PyYAML",
+    "python3-pyzmq",
+    "python3-requests",
+    "python3-rpm",
+    "python3-simplejson",
+    "python3-six",
+    "python3-tornado",
+    "python3-urllib3",
+    "timezone",
+    "salt",
+    "python3-salt",
+    "salt-minion",
+]
+
+PKGLIST15_TRAD = [
     "libgudev-1_0-0",
     "libnewt0_52",
     "libslang2",
@@ -304,7 +333,6 @@ PKGLIST15 = [
     "python3-cryptography",
     "python-dmidecode",
     "python3-dmidecode",
-    "python3-idna",
     "python3-libxml2-python",
     "python3-netifaces",
     "python3-newt",
@@ -312,7 +340,6 @@ PKGLIST15 = [
     "python3-pycparser",
     "python3-pyOpenSSL",
     "python3-pyudev",
-    "python3-rpm",
     "python3-packaging",
     "python3-setuptools",
     "python3-appdirs",
@@ -333,30 +360,6 @@ PKGLIST15 = [
     "zypp-plugin-spacewalk",
     "python3-zypp-plugin",
     "python3-zypp-plugin-spacewalk",
-    "libpgm-5_2-0",
-    "libsodium23",
-    "libzmq5",
-    "python3-Babel",
-    "python3-certifi",
-    "python3-chardet",
-    "python3-Jinja2",
-    "python3-MarkupSafe",
-    "python3-msgpack",
-    "python3-psutil",
-    "python3-py",
-    "python3-pycrypto",
-    "python3-pytz",
-    "python3-PyYAML",
-    "python3-pyzmq",
-    "python3-requests",
-    "python3-simplejson",
-    "python3-six",
-    "python3-tornado",
-    "python3-urllib3",
-    "timezone",
-    "salt",
-    "python3-salt",
-    "salt-minion",
 ]
 
 PKGLIST15_X86_ARM = [
@@ -735,35 +738,35 @@ DATA = {
         'DEST' : '/srv/www/htdocs/pub/repositories/sle/12/3/bootstrap/'
     },
     'SLE-15-aarch64' : {
-        'PDID' : [1589, 1709], 'PKGLIST' : PKGLIST15 + PKGLIST15_X86_ARM,
+        'PDID' : [1589, 1709], 'PKGLIST' : PKGLIST15_TRAD + PKGLIST15_SALT + PKGLIST15_X86_ARM,
         'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/0/bootstrap/'
     },
     'SLE-15-ppc64le' : {
-        'PDID' : [1588, 1710], 'PKGLIST' : PKGLIST15 + PKGLIST15_PPC,
+        'PDID' : [1588, 1710], 'PKGLIST' : PKGLIST15_TRAD + PKGLIST15_SALT + PKGLIST15_PPC,
         'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/0/bootstrap/'
     },
     'SLE-15-s390x' : {
-        'PDID' : [1587, 1711], 'PKGLIST' : PKGLIST15 + PKGLIST15_Z,
+        'PDID' : [1587, 1711], 'PKGLIST' : PKGLIST15_TRAD + PKGLIST15_SALT + PKGLIST15_Z,
         'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/0/bootstrap/'
     },
     'SLE-15-x86_64' : {
-        'PDID' : [1576, 1712], 'PKGLIST' : PKGLIST15 + PKGLIST15_X86_ARM,
+        'PDID' : [1576, 1712], 'PKGLIST' : PKGLIST15_TRAD + PKGLIST15_SALT + PKGLIST15_X86_ARM,
         'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/0/bootstrap/'
     },
     'SLE-15-SP1-aarch64' : {
-        'PDID' : [1769, 1709], 'PKGLIST' : PKGLIST15 + PKGLIST15_X86_ARM,
+        'PDID' : [1769, 1709], 'PKGLIST' : PKGLIST15_TRAD + PKGLIST15_SALT + PKGLIST15_X86_ARM,
         'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/1/bootstrap/'
     },
     'SLE-15-SP1-ppc64le' : {
-        'PDID' : [1770, 1710], 'PKGLIST' : PKGLIST15 + PKGLIST15_PPC,
+        'PDID' : [1770, 1710], 'PKGLIST' : PKGLIST15_TRAD + PKGLIST15_SALT + PKGLIST15_PPC,
         'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/1/bootstrap/'
     },
     'SLE-15-SP1-s390x' : {
-        'PDID' : [1771, 1711], 'PKGLIST' : PKGLIST15 + PKGLIST15_Z,
+        'PDID' : [1771, 1711], 'PKGLIST' : PKGLIST15_TRAD + PKGLIST15_SALT + PKGLIST15_Z,
         'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/1/bootstrap/'
     },
     'SLE-15-SP1-x86_64' : {
-        'PDID' : [1772, 1712], 'PKGLIST' : PKGLIST15 + PKGLIST15_X86_ARM,
+        'PDID' : [1772, 1712], 'PKGLIST' : PKGLIST15_TRAD + PKGLIST15_SALT + PKGLIST15_X86_ARM,
         'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/1/bootstrap/'
     },
     'openSUSE-Leap-42.3-x86_64' : {
@@ -775,15 +778,15 @@ DATA = {
         'DEST' : '/srv/www/htdocs/pub/repositories/opensuse/15/0/bootstrap/'
     },
     'openSUSE-Leap-15.1-x86_64-uyuni' : {
-        'BASECHANNEL' : 'opensuse_leap15_1-x86_64', 'PKGLIST' : PKGLIST15 + PKGLIST15_X86_ARM,
+        'BASECHANNEL' : 'opensuse_leap15_1-x86_64', 'PKGLIST' : PKGLIST15_TRAD + PKGLIST15_SALT + PKGLIST15_X86_ARM,
         'DEST' : '/srv/www/htdocs/pub/repositories/opensuse/15/1/bootstrap/'
     },
     'openSUSE-Leap-15.1-x86_64' : {
-        'PDID' : [1929], 'PKGLIST' : PKGLIST15 + PKGLIST15_X86_ARM,
+        'PDID' : [1929], 'PKGLIST' : PKGLIST15_SALT + PKGLIST15_X86_ARM,
         'DEST' : '/srv/www/htdocs/pub/repositories/opensuse/15/1/bootstrap/'
     },
     'openSUSE-Leap-15.2-x86_64' : {
-        'PDID' : [2001], 'PKGLIST' : PKGLIST15 + PKGLIST15_X86_ARM,
+        'PDID' : [2001], 'PKGLIST' : PKGLIST15_SALT + PKGLIST15_X86_ARM,
         'DEST' : '/srv/www/htdocs/pub/repositories/opensuse/15/2/bootstrap/'
     },
     'centos-6-x86_64' : {
