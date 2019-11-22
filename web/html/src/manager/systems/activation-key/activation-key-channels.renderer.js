cd1dd4c647e46b77b7afc2a920166623e5a7bd20
dleidi <dleidi@suse.com>
2019-11-22 14:51:36 +0100

    Get rid of global variables and functions to read server parameters

--- web/html/src/manager/systems/activation-key/activation-key-channels.renderer.js
+++ web/html/src/manager/systems/activation-key/activation-key-channels.renderer.js
@@ -1,16 +1,10 @@
 /* eslint-disable */
-/* global getActivationKeyId */
 import ActivationKeyChannels from './activation-key-channels';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SpaRenderer from "core/spa/spa-renderer";
 
-// receive parameters from the backend
-// if nothing from the backend, fallback on defaults
-window.pageRenderers = window.pageRenderers || {};
-const customValues = window.pageRenderers.customValues || {DOMid: 'activation-key-channels'};
-
-export const renderer = () => SpaRenderer.renderNavigationReact(
-    <ActivationKeyChannels activationKeyId={getActivationKeyId() ? getActivationKeyId() : -1} />,
-    document.getElementById(customValues.DOMid)
+export const renderer = (id, {activationKeyId}) => SpaRenderer.renderNavigationReact(
+    <ActivationKeyChannels activationKeyId={activationKeyId} />,
+    document.getElementById(id)
 );
