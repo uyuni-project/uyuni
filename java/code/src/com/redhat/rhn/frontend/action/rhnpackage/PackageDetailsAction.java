e5a2b4bae665c2b47d21bd95e5a6034dbba667da
Matei Albu <malbu@suse.de>
2019-11-28 19:04:04 +0100

    Show additional headers and dependencies for deb packages
    (cherry picked from commit 2149d043c0694f808eb3758a60248ce13b95e948)

--- java/code/src/com/redhat/rhn/frontend/action/rhnpackage/PackageDetailsAction.java
+++ java/code/src/com/redhat/rhn/frontend/action/rhnpackage/PackageDetailsAction.java
@@ -40,6 +40,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -141,6 +142,10 @@ public class PackageDetailsAction extends RhnAction {
 
             request.setAttribute("erratumEmpty", pkg.getPublishedErrata().isEmpty());
             request.setAttribute("erratum", pkg.getPublishedErrata());
+
+            request.setAttribute("extraTags", pkg.getExtraTags()
+                    .entrySet().stream().map(e -> Map.entry(e.getKey().getName(), e.getValue()))
+                    .collect(Collectors.toList()));
 
             return mapping.findForward(RhnHelper.DEFAULT_FORWARD);
         }
