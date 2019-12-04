b59604e12136362d426a8cb6d957461d7b9d1770
Pascal Arlt <parlt@suse.com>
2019-12-04 11:47:24 +0100

    Add database schema changes
    Schema changes for recurring-state-apply actions
    
    Signed-off-by: Pascal Arlt <parlt@suse.com>

--- schema/spacewalk/common/data/rhnTaskoBunch.sql
+++ schema/spacewalk/common/data/rhnTaskoBunch.sql
@@ -108,4 +108,7 @@ INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
 INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
    VALUES (sequence_nextval('rhn_tasko_bunch_id_seq'), 'minion-checkin-bunch', 'Perform a regular check-in on minions', null);
 
+INSERT INTO RhnTaskoBunch (id, name, description, org_bunch)
+   VALUES (sequence_nextval('rhn_tasko_bunch_id_seq'), 'recurring-state-apply-bunch', 'Applies salt state to minion/group/org', null);
+
 commit;
