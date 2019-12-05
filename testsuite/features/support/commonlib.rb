f1d8eb27aa7c6e7927a76294598cf7055d535fdb
dleidi <dleidi@suse.com>
2019-12-05 12:02:10 +0100

    Increase from 1 to 5 seconds the max_wait_time for async waiting clicks

--- testsuite/features/support/commonlib.rb
+++ testsuite/features/support/commonlib.rb
@@ -106,7 +106,7 @@ end
 def click_button_and_wait(locator = nil, **options)
   click_button(locator, options)
   begin
-    raise 'Timeout: Waiting AJAX transition (click link)' unless has_no_css?('.senna-loading', wait: 1)
+    raise 'Timeout: Waiting AJAX transition (click link)' unless has_no_css?('.senna-loading', wait: 5)
   rescue StandardError => e
     puts e.message # Skip errors related to .senna-loading element
   end
@@ -115,7 +115,7 @@ end
 def click_link_and_wait(locator = nil, **options)
   click_link(locator, options)
   begin
-    raise 'Timeout: Waiting AJAX transition (click link)' unless has_no_css?('.senna-loading', wait: 1)
+    raise 'Timeout: Waiting AJAX transition (click link)' unless has_no_css?('.senna-loading', wait: 5)
   rescue StandardError => e
     puts e.message # Skip errors related to .senna-loading element
   end
@@ -124,7 +124,7 @@ end
 def click_link_or_button_and_wait(locator = nil, **options)
   click_link_or_button(locator, options)
   begin
-    raise 'Timeout: Waiting AJAX transition (click link)' unless has_no_css?('.senna-loading', wait: 1)
+    raise 'Timeout: Waiting AJAX transition (click link)' unless has_no_css?('.senna-loading', wait: 5)
   rescue StandardError => e
     puts e.message # Skip errors related to .senna-loading element
   end
@@ -135,7 +135,7 @@ module CapybaraNodeElementExtension
   def click
     super
     begin
-      raise 'Timeout: Waiting AJAX transition (click link)' unless has_no_css?('.senna-loading', wait: 1)
+      raise 'Timeout: Waiting AJAX transition (click link)' unless has_no_css?('.senna-loading', wait: 5)
     rescue StandardError => e
       puts e.message # Skip errors related to .senna-loading element
     end
