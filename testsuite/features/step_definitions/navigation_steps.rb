98f58e2236be04f322b93b3f93b8a373ac7f5d1d
dleidi <dleidi@suse.com>
2019-12-06 08:40:50 +0100

    Dynamic check depending on the product name

--- testsuite/features/step_definitions/navigation_steps.rb
+++ testsuite/features/step_definitions/navigation_steps.rb
@@ -441,7 +441,9 @@ end
 
 Then(/^I am logged in$/) do
   raise 'User is not logged in' unless find(:xpath, "//a[@href='/rhn/Logout.do']").visible?
-  raise 'The welcome message is not shown' unless has_content?('You have just created your first SUSE Manager user. To finalize your installation please use the Setup Wizard')
+  text = 'You have just created your first $PRODUCT user. To finalize your installation please use the Setup Wizard'
+  text.gsub! '$PRODUCT', $product # TODO: Rid of this substitution, using another step
+  raise 'The welcome message is not shown' unless has_content?(text)
 end
 
 Given(/^I am on the patches page$/) do
