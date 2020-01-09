0d6b99a48900988e2a3f290e7d40c212c235b771
Cédric Bosdonnat <cbosdonnat@suse.com>
2020-01-09 21:00:29 +0100

    Create an ActionApi out of virtualization-guest-action-api
    Since a lot of the virtualization-guest-action-api code is common enough
    to be reused as is, extract this as a component to allow easly sending
    an HTTP POST request.

---
+++ web/html/src/manager/virtualization/ActionApi.js
@@ -0,0 +1,61 @@
+// @flow
+
+import * as React from 'react';
+import * as Network from 'utils/network';
+import { Utils as MessagesUtils } from 'components/messages';
+
+type Props = {
+  /** URL path part with @ACTION@ as action placeholder. */
+  urlTemplate: string,
+  /**
+   * Function rendering the children taking an object parameter with
+   * an onAction and a messages property
+   */
+  children: ({onAction: (action: string, parameters: Object) => void, messages: Array<React.Node>}) => React.Node,
+  /**
+   * URL of the page to show after the action has been successfully performed.
+   * If undefined, the page is not redirected at all.
+   */
+  bounce?: string,
+  /** Function with the response as parameter in case of success of the HTTP request */
+  callback?: Function,
+};
+
+/** Helper component offering an API to run a POST HTTP resquest */
+export function ActionApi(props: Props) {
+  const [messages, setMessages] = React.useState([]);
+
+  const onAction = (action: string, parameters: Object) => {
+    Network.post(props.urlTemplate.replace('@ACTION@', action),
+      JSON.stringify(parameters), 'application/json').promise
+      .then((response) => {
+        if (Object.values(response).includes('Failed')) {
+          setMessages([MessagesUtils.error(t(`Failed to trigger ${action}`))]);
+        } else {
+          if (props.callback) {
+            props.callback(response);
+          }
+          if (props.bounce) {
+            window.location.replace(props.bounce);
+          }
+        }
+      }, (xhr) => {
+        const errMessages = xhr.status === 0
+          ? [MessagesUtils.error(
+            t('Request interrupted or invalid response received from the server. Please try again.'),
+          )]
+          : [MessagesUtils.error(Network.errorMessageByStatus(xhr.status))];
+        setMessages(errMessages);
+      });
+  };
+
+  return props.children({
+    onAction: onAction,
+    messages,
+  });
+}
+
+ActionApi.defaultProps = {
+  bounce: undefined,
+  callback: undefined,
+};
