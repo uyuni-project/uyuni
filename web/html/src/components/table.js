e41fc6d478ee7175aaf31f572496b2bf856267b1
Cédric Bosdonnat <cbosdonnat@suse.com>
2020-01-23 10:54:17 +0100

    Document the table Header component

--- web/html/src/components/table.js
+++ web/html/src/components/table.js
@@ -125,19 +125,41 @@ Column.defaultProps = {
   criteria: undefined,
 };
 
-const Header = (props) => {
+type HeaderProps = {
+  /** key differenciating a header from its sibblings */
+  key?: string | number,
+  /** CSS value for the column width */
+  width?: string,
+  /** class name to use for the cell */
+  className?: string,
+  /** Row comparison function. See sortBy functions in utils/functions.js */
+  comparator?: (Object, Object, string, number) => number,
+  /** negative number for descending, positive for ascending, 0 for no direction */
+  sortDirection: number,
+  /** Function called when the sort direction is changed. */
+  onSortChange?: (columnKey?: string, sortDirection: number) => void,
+  /** children nodes */
+  children?: React.Node,
+  /** identifier for the column */
+  columnKey?: string,
+};
+
+/** Represents a header cell in the table.
+ *  This component should only be used internally by Table.
+ */
+function Header(props: HeaderProps) {
   const thStyle = props.width ? { width: props.width } : null;
 
-  let thClass = props.className || "";
+  let thClass = props.className || '';
 
   if (props.comparator) {
-    thClass += (thClass ? " " : "") + (props.sortDirection == 0 ? "" : (props.sortDirection > 0 ? "ascSort" : "descSort"));
-    const newDirection = props.sortDirection == 0 ? 1 : props.sortDirection * -1;
+    thClass += (thClass ? " " : "") + (props.sortDirection === 0 ? "" : (props.sortDirection > 0 ? "ascSort" : "descSort"));
+    const newDirection = props.sortDirection === 0 ? 1 : props.sortDirection * -1;
 
     return (
       <th style={ thStyle } className={ thClass }>
         <a href="#" className="orderBy"
-            onClick={() => props.onSortChange(props.columnKey, newDirection)}>
+            onClick={() => props.onSortChange && props.onSortChange(props.columnKey, newDirection)}>
           {props.children}
         </a>
       </th>
@@ -145,6 +167,12 @@ const Header = (props) => {
   }
   return <th style={ thStyle } className={ thClass }>{props.children}</th>;
 }
+Header.defaultProps = {
+  width: undefined,
+  columnClass: undefined,
+  comparator: undefined,
+  sortDirection: 0,
+};
 
 const Highlight = (props) => {
   let text = props.text;
