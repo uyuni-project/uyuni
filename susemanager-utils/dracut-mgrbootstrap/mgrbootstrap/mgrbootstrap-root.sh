f1ff8edf4ba40f10c1da8f2a918987fc6ad605c7
Michael Calmer <mc@suse.de>
2020-01-30 15:16:18 +0100

    minimize process to steps which are really needed

--- susemanager-utils/dracut-mgrbootstrap/mgrbootstrap/mgrbootstrap-root.sh
+++ susemanager-utils/dracut-mgrbootstrap/mgrbootstrap/mgrbootstrap-root.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-root=saltboot
+root=mgrbootstrap
 rootok=1
 
-# If we don't have a server, we need dhcp
-if [ -z "$server" ] ; then
-    DHCPORSERVER="1"
-fi;
-
-export DISABLE_UNIQUE_SUFFIX=$(getarg DISABLE_UNIQUE_SUFFIX=)
-export kiwidebug=$(getarg kiwidebug=)
-export spacewalk_activationkey=$(getarg spacewalk_activationkey=)
-export spacewalk_hostname=$(getarg spacewalk_hostname=)
--- susemanager-utils/dracut-mgrbootstrap/mgrbootstrap/mgrbootstrap.sh
+++ susemanager-utils/dracut-mgrbootstrap/mgrbootstrap/mgrbootstrap.sh
@@ -1,49 +1,81 @@
 #!/bin/bash
 
+test -s /etc/rc.status && . /etc/rc.status && rc_reset
+
 if ! declare -f Echo > /dev/null ; then
   Echo() {
     echo "$@"
   }
 fi
 
-if ! [ -s /etc/resolv.conf ] ; then
-    Echo "No network, skipping saltboot..."
-    exit 0
+
+function register() {
+    local mycert_file="RHN-ORG-TRUSTED-SSL-CERT"
+    local mycert="/usr/share/rhn/$mycert_file"
+    if ! /usr/bin/wget http://$SW_HOSTNAME/pub/$mycert_file -O $mycert; then
+        return 1
+    fi
+    for f in /etc/sysconfig/rhn/*; do
+        if [ -f "$f" ]; then
+	    sed -i "s/RHNS-CA-CERT/$mycert_file/g" "$f"
+        fi
+    done
+    if ! /usr/sbin/rhnreg_ks --serverUrl=https://$SW_HOSTNAME/XMLRPC --activationkey=$ACTIVATION_KEY --force; then
+        return 1
+    fi
+    return 0
+}
+
+# fix hostname for postfix
+REALHOSTNAME=`hostname -f`
+if [ -z "$REALHOSTNAME" ]; then
+	for i in `ip -f inet -o addr show scope global | awk '{print $4}' | awk -F \/ '{print $1}'`; do
+		for j in `dig +noall +answer +time=2 +tries=1 -x $i | awk '{print $5}' | sed 's/\.$//'`; do
+			if [ -n "$j" ]; then
+				REALHOSTNAME=$j
+				break 2
+			fi
+		done
+	done
+fi
+if [ -n "$REALHOSTNAME" ]; then
+	echo "$REALHOSTNAME" > /etc/hostname
 fi
 
-IFCONFIG="$(compgen -G '/tmp/leaseinfo.*.dhcp.ipv*')"
-if [ -f "$IFCONFIG" ]; then
-    . "$IFCONFIG"
-else
-    Echo "No network details available, skipping saltboot";
-    exit 0
+ACTIVATION_KEY=$(rc_cmdline spacewalk_activationkey)
+ACTIVATION_KEY=${ACTIVATION_KEY#spacewalk_activationkey=}
+SW_HOSTNAME=$(rc_cmdline spacewalk_hostname)
+SW_HOSTNAME=${SW_HOSTNAME#spacewalk_hostname=}
+
+READY=1
+if [ -z "$ACTIVATION_KEY" ]; then
+    Echo "Activation Key missing"
+    sleep 10
+    READY=0
+fi
+if [ -z "$SW_HOSTNAME" ]; then
+    Echo "Spacewalk Hostname is missing"
+    sleep 10
+    READY=0
 fi
 
-NEWROOT=${NEWROOT:-/mnt}
-export NEWROOT
-
-if [ -e /usr/bin/plymouth ] ; then
-    mkfifo /progress
-    bash -c 'while true ; do read msg < /progress ; plymouth message --text="$msg" ; done ' &
-    PROGRESS_PID=$!
-else
-    mkfifo /progress
-    bash -c 'while true ; do read msg < /progress ; echo -n -e "\033[2K$msg\015" >/dev/console ; done ' &
-    PROGRESS_PID=$!
+if [ $READY -eq 1 ]; then
+    if ! register; then
+        sleep 10
+    else
+        Echo "Successfully registered!"
+	sleep 2
+    fi
 fi
 
-rm /etc/machine-id
-mkdir -p /var/lib/dbus
-rm /var/lib/dbus/machine-id
-dbus-uuidgen --ensure
-systemd-machine-id-setup
+FINALLY=$(rc_cmdline spacewalk_finally)
+FINALLY=${FINALLY#spacewalk_finally=}
 
-DIG_OPTIONS="+short"
-if dig -h | grep -q '\[no\]cookie'; then
-    DIG_OPTIONS="+nocookie +short"
-fi
+case "$FINALLY" in
+    running) Echo "keep instance running";;
+    *) reboot --halt -p -f;;
+esac
 
-[ -n "$PROGRESS_PID" ] && kill $PROGRESS_PID
-
-/usr/sbin/preregister
+rc_status -v
+rc_exit
 
