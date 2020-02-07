ec8a975b50f4d6ca192013c01e1ae2947f3a55c9
Abid Mehmood <amehmood@suse.de>
2020-02-07 18:54:51 +0100

    add option to minion config file, in order to disable salt mine

--- spacewalk/certs-tools/rhn_bootstrap_strings.py
+++ spacewalk/certs-tools/rhn_bootstrap_strings.py
@@ -966,6 +966,7 @@ if [ $REGISTER_THIS_BOX -eq 1 ] ; then
     echo "server_id_use_crc: adler32" >> "$SUSEMANAGER_MASTER_FILE"
     echo "enable_legacy_startup_events: False" >> "$SUSEMANAGER_MASTER_FILE"
     echo "enable_fqdns_grains: False" >> "$SUSEMANAGER_MASTER_FILE"
+    echo "mine_enabled: False" >> "$SUSEMANAGER_MASTER_FILE"
 
     if [ -n "$ACTIVATION_KEYS" ] ; then
         cat <<EOF >>"$SUSEMANAGER_MASTER_FILE"
