7a912073fdb03d856af17921b2c341adef9949e9
Michael Calmer <mc@suse.de>
2020-02-10 17:13:53 +0100

    join dblink migrations

--- schema/spacewalk/upgrade/spacewalk-schema-1.6-to-spacewalk-schema-1.7/109-dblink.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.6-to-spacewalk-schema-1.7/109-dblink.sql
@@ -5,3 +5,6 @@ create temporary table store_searchpath as select setting from pg_settings where
 
 update pg_settings set setting = (select setting from store_searchpath) where name = 'search_path';
 drop table store_searchpath;
+
+create extension dblink;
+
