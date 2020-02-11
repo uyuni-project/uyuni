96b04050e9a34c93c9d355c19ac8e68b505cec0a
Silvio Moioli <moio@suse.com>
2020-02-11 17:05:38 +0100

    Use reflections to find out actors

--- java/code/src/com/suse/manager/tasks/actors/GuardianActor.java
+++ java/code/src/com/suse/manager/tasks/actors/GuardianActor.java
@@ -3,14 +3,26 @@ package com.suse.manager.tasks.actors;
 import static akka.actor.typed.javadsl.Behaviors.receive;
 import static akka.actor.typed.javadsl.Behaviors.same;
 import static akka.actor.typed.javadsl.Behaviors.setup;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 
 import com.suse.manager.tasks.Command;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.log4j.Logger;
+import org.jose4j.jwk.Use;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
 
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.BehaviorBuilder;
 
 public class GuardianActor {
 
@@ -21,12 +33,59 @@ public class GuardianActor {
     }
 
     private Behavior<Command> buildBehavior(ActorContext<Command> context) {
-        var restartSatellite = context.spawn(new RestartSatelliteActor().create(), "restartSatellite");
-        var traceBack = context.spawn(new TraceBackActor().create(), "traceBack");
-        return receive(Command.class)
-                .onMessage(RestartSatelliteActor.Message.class, message -> tellChild(restartSatellite, message))
-                .onMessage(TraceBackActor.Message.class, message -> tellChild(traceBack, message))
-                .build();
+        var reflections = new Reflections(GuardianActor.class.getPackageName(), new SubTypesScanner(false));
+        var allClasses = reflections.getSubTypesOf(Object.class);
+
+        var actorClasses = allClasses.stream()
+                .filter(c -> !c.equals(GuardianActor.class))
+                .filter(c -> !c.isMemberClass())
+                .collect(toSet());
+
+        var actorMap = actorClasses.stream().collect(toMap(
+                identity(),
+                classToActor(context))
+        );
+
+        var basicReceiveBuilder = receive(Command.class);
+        var receiveBuilder = actorMap.entrySet().stream().reduce(
+                basicReceiveBuilder,
+                this::addHandlers,
+                (memo1, memo2) -> memo2
+        );
+
+        return receiveBuilder.build();
+    }
+
+    private BehaviorBuilder<Command> addHandlers(BehaviorBuilder<Command> behaviorBuilder, Map.Entry<Class<?>, ActorRef<Command>> entry) {
+        var actorClass = entry.getKey();
+        var messageClasses = Arrays.stream(actorClass.getNestMembers())
+                .filter(nm -> Arrays.asList(nm.getInterfaces()).contains(Command.class))
+                .map(c -> (Class<Command>) c)
+                .collect(toSet());
+
+        var actor = entry.getValue();
+
+        var augmentedBehaviorBuilder = messageClasses.stream().reduce(
+                behaviorBuilder,
+                (memo, messageClass) -> memo.onMessage(messageClass, message -> tellChild(actor, message)),
+                (memo1, memo2) -> memo2
+        );
+        return augmentedBehaviorBuilder;
+    }
+
+    private Function<Class<?>, ActorRef<Command>> classToActor(ActorContext<Command> context) {
+        return clazz -> {
+            try {
+                var createMethod = clazz.getMethod("create");
+                var instance = clazz.getConstructor().newInstance();
+                var actor = context.spawn((Behavior<Command>) createMethod.invoke(instance), clazz.getSimpleName());
+                return actor;
+            }
+            catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                LOG.error(e);
+                throw new RuntimeException(e);
+            }
+        };
     }
 
     public Behavior<Command> tellChild(ActorRef<Command> a, Command c) {
