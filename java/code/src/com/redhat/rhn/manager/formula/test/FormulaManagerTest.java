0d0e8d1753a258ce26f7db2313dc7bd46edf2d04
Johannes Renner <jrenner@suse.com>
2020-02-13 15:21:53 +0100

    Test the conditions for monitoring cleanup

--- java/code/src/com/redhat/rhn/manager/formula/test/FormulaManagerTest.java
+++ java/code/src/com/redhat/rhn/manager/formula/test/FormulaManagerTest.java
@@ -14,15 +14,19 @@
  */
 package com.redhat.rhn.manager.formula.test;
 
-import com.redhat.rhn.common.hibernate.LookupException;
+import static com.redhat.rhn.domain.formula.FormulaFactory.PROMETHEUS_EXPORTERS;
+
 import com.redhat.rhn.common.security.PermissionException;
 import com.redhat.rhn.domain.formula.FormulaFactory;
 import com.redhat.rhn.domain.server.ManagedServerGroup;
 import com.redhat.rhn.domain.server.MinionServer;
+import com.redhat.rhn.domain.server.ServerGroup;
 import com.redhat.rhn.domain.server.test.MinionServerFactoryTest;
+import com.redhat.rhn.domain.server.test.ServerGroupTest;
 import com.redhat.rhn.domain.user.User;
 import com.redhat.rhn.manager.formula.FormulaManager;
 import com.redhat.rhn.manager.formula.InvalidFormulaException;
+import com.redhat.rhn.manager.system.SystemManager;
 import com.redhat.rhn.testing.ServerGroupTestUtils;
 import com.redhat.rhn.testing.TestUtils;
 import com.redhat.rhn.testing.UserTestUtils;
@@ -30,11 +34,22 @@ import com.redhat.rhn.testing.JMockBaseTestCaseWithUser;
 import com.suse.manager.webui.services.impl.SaltService;
 import com.suse.salt.netapi.datatypes.target.MinionList;
 import com.suse.utils.Json;
+
+import org.apache.commons.io.IOUtils;
 import org.cobbler.test.MockConnection;
 import org.jmock.Expectations;
 import org.jmock.lib.legacy.ClassImposteriser;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -50,6 +65,12 @@ public class FormulaManagerTest extends JMockBaseTestCaseWithUser {
     static final String formulaName = "dhcpd";
     private SaltService saltServiceMock;
     private FormulaManager manager = FormulaManager.getInstance();
+    private Path metadataDir;
+
+    public FormulaManagerTest() throws Exception {
+        metadataDir = Files.createTempDirectory("metadata");
+        createMetadataFiles();
+    }
 
     @Override
     public void setUp() throws Exception {
@@ -160,6 +181,62 @@ public class FormulaManagerTest extends JMockBaseTestCaseWithUser {
             fail( "Exception expected but didn't throw" );
         } catch (PermissionException ex) {
             //expected exception
+        }
+    }
+
+    /**
+     * Test the conditions in FormulaManager.isMonitoringCleanupNeeded().
+     * @throws Exception
+     */
+    public void testIsMonitoringCleanupNeeded() throws Exception {
+        MinionServer minion = MinionServerFactoryTest.createTestMinionServer(user);
+        FormulaFactory.setDataDir(tmpSaltRoot.resolve(TEMP_PATH).toString());
+        FormulaFactory.setMetadataDirOfficial(metadataDir.toString() + File.separator);
+
+        // No group or system level assignment of the `prometheus-exporters` Formula
+        assertFalse(manager.isMonitoringCleanupNeeded(minion));
+
+        // Create a group level assignment of the Formula
+        ServerGroup group = ServerGroupTest.createTestServerGroup(user.getOrg(), null);
+        SystemManager.addServerToServerGroup(minion, group);
+        FormulaFactory.saveGroupFormulas(group.getId(), Arrays.asList(PROMETHEUS_EXPORTERS), user.getOrg());
+
+        // Save data that enables monitoring
+        Map<String, Object> formulaData = new HashMap<>();
+        formulaData.put("node_exporter", Collections.singletonMap("enabled", true));
+        formulaData.put("postgres_exporter", Collections.singletonMap("enabled", false));
+        formulaData.put("apache_exporter", Collections.singletonMap("enabled", false));
+        FormulaFactory.saveGroupFormulaData(formulaData, group.getId(), user.getOrg(), PROMETHEUS_EXPORTERS);
+        assertTrue(manager.isMonitoringCleanupNeeded(minion));
+
+        // Save data that disables monitoring
+        formulaData.put("node_exporter", Collections.singletonMap("enabled", false));
+        formulaData.put("postgres_exporter", Collections.singletonMap("enabled", false));
+        formulaData.put("apache_exporter", Collections.singletonMap("enabled", false));
+        FormulaFactory.saveGroupFormulaData(formulaData, group.getId(), user.getOrg(), PROMETHEUS_EXPORTERS);
+        assertFalse(manager.isMonitoringCleanupNeeded(minion));
+
+        // Create a system level assignment of the Formula
+        FormulaFactory.saveServerFormulas(minion.getMinionId(), Arrays.asList(PROMETHEUS_EXPORTERS));
+        FormulaFactory.saveServerFormulaData(formulaData, minion.getMinionId(), PROMETHEUS_EXPORTERS);
+        assertTrue(manager.isMonitoringCleanupNeeded(minion));
+    }
+
+    // Copy the pillar.example file to a temp dir used as metadata directory (in FormulaFactory)
+    private void createMetadataFiles() {
+        try {
+            Path prometheusDir = metadataDir.resolve("prometheus-exporters");
+            Files.createDirectories(prometheusDir);
+            try (
+                InputStream src = this.getClass().getResourceAsStream("prometheus-exporters-pillar.example");
+                OutputStream dst = new FileOutputStream(prometheusDir.resolve("pillar.example").toFile())
+            ) {
+                IOUtils.copy(src, dst);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
         }
     }
 }
