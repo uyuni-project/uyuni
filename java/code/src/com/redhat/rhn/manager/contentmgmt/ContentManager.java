ee1d09c9643775b28beda52a1ef171d8bd84fe61
Frantisek Kobzik <fkobzik@suse.de>
2020-02-20 09:07:34 +0100

    Fix: Propagate the async flag correctly on Content Project promote

--- java/code/src/com/redhat/rhn/manager/contentmgmt/ContentManager.java
+++ java/code/src/com/redhat/rhn/manager/contentmgmt/ContentManager.java
@@ -228,7 +228,7 @@ public class ContentManager {
                     // populating first environment will be implemented as soon as backward-promote is implemented
                     predecessor.ifPresent(p -> {
                         if (!p.getTargets().isEmpty()) {
-                            promoteProject(projectLabel, p.getLabel(), true, user);
+                            promoteProject(projectLabel, p.getLabel(), async, user);
                         }
                     });
                     return newEnv;
