43b78c025a2ce55edd6cd4847a291375dcea8b4b
Frantisek Kobzik <fkobzik@suse.de>
2020-02-20 13:08:33 +0100

    Frontend: Prevent build/promote on Content Projects which have colliding build/promote in progress

--- web/html/src/manager/content-management/shared/components/panels/promote/promote.js
+++ web/html/src/manager/content-management/shared/components/panels/promote/promote.js
@@ -17,6 +17,7 @@ type Props = {
   projectId: string,
   environmentPromote: ProjectEnvironmentType,
   environmentTarget: ProjectEnvironmentType,
+  environmentNextTarget: ProjectEnvironmentType,
   versionToPromote: number,
   historyEntries: Array<ProjectHistoryEntry>,
   onChange: Function
@@ -37,13 +38,18 @@ const Promote = (props: Props) => {
   }, [open]);
 
   const modalNameId = `${props.environmentPromote.label}-cm-promote-env-modal`;
+
   const disabled =
     !hasEditingPermissions
     || !props.environmentPromote.version
-    || props.environmentPromote.version <= props.environmentTarget.version;
+    || props.environmentPromote.version <= props.environmentTarget.version
+    || props.environmentPromote.status === "building"
+    || props.environmentTarget.status === "building"
+    || (props.environmentNextTarget || {}).status === "building";
+
   return (
     <div
-      {...(disabled ? {title: t("No version to promote")} : {})}
+      {...(disabled ? {title: t("No version to promote or colliding environment build in progress")} : {})}
     >
       <DownArrow/>
       <div className="text-center">
