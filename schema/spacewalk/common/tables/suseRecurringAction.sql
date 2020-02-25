d3d7758da1aa53469e56312c5492f1e1ec5ff642
Frantisek Kobzik <fkobzik@suse.de>
2020-02-25 13:07:40 +0100

    Postgres syntax

--- schema/spacewalk/common/tables/suseRecurringAction.sql
+++ schema/spacewalk/common/tables/suseRecurringAction.sql
@@ -1,28 +1,27 @@
 CREATE TABLE suseRecurringAction
 (
-  id                number NOT NULL
+  id                NUMERIC NOT NULL
                     CONSTRAINT suse_recurring_action_id_pk PRIMARY KEY,
-  target_type        VARCHAR2(32) NOT NULL,
-  minion_id         number
+  target_type       VARCHAR(32) NOT NULL,
+  minion_id         NUMERIC
                     CONSTRAINT suse_rec_action_minion_fk
                       REFERENCES suseMinionInfo(server_id)
                       ON DELETE CASCADE,
-  group_id          number
+  group_id          NUMERIC
                     CONSTRAINT suse_rec_action_group_fk
                       REFERENCES rhnServerGroup(id)
                       ON DELETE CASCADE,
-  org_id            number
+  org_id            NUMERIC
                     CONSTRAINT suse_rec_action_org_fk
                       REFERENCES web_customer(id)
                       ON DELETE CASCADE,
-  creator_id        number
+  creator_id        NUMERIC
                     CONSTRAINT suse_rec_action_creator_fk
                       REFERENCES web_contact(id)
                       ON DELETE CASCADE,
   active            CHAR(1) DEFAULT ('Y') NOT NULL,
   test_mode         CHAR(1) DEFAULT ('Y') NOT NULL
-)
-ENABLE ROW MOVEMENT
-;
+);
 
 CREATE SEQUENCE suse_recurring_action_id_seq;
+
