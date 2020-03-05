777f788cf0a1df11aa89aa276a79ea4b1b385ec5
Cédric Bosdonnat <cbosdonnat@suse.com>
2020-03-05 14:28:06 +0100

    Add virtual pool refresh action
    Add the base virtual pool action class and the refresh action

--- schema/spacewalk/common/data/rhnActionType.sql
+++ schema/spacewalk/common/data/rhnActionType.sql
@@ -79,6 +79,7 @@ insert into rhnActionType values (505, 'image.inspect', 'Inspect an Image', 'N',
 insert into rhnActionType values (506, 'channels.subscribe', 'Subscribe to channels', 'N', 'N');
 insert into rhnActionType values (507, 'virt.delete', 'Deletes a virtual domain.', 'N', 'N');
 insert into rhnActionType values (508, 'virt.create', 'Creates a virtual domain.', 'N', 'N');
+insert into rhnActionType values (509, 'virt.pool_refresh', 'Refresh a virtual storage pool', 'N', 'N');
 --
 --
 -- Revision 1.25  2004/10/29 05:07:52  pjones
