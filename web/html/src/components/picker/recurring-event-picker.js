d3fc3688a12f0cf677c6cfb4464278e08d7d8c7d
Pascal Arlt <parlt@suse.com>
2020-03-10 15:35:26 +0100

    Improve alignment of elements in the picker
    Signed-off-by: Pascal Arlt <parlt@suse.com>

--- web/html/src/components/picker/recurring-event-picker.js
+++ web/html/src/components/picker/recurring-event-picker.js
@@ -310,10 +310,10 @@ class RecurringEventPicker extends React.Component<RecurringEventPickerProps, Re
                     </div>
                     <div className="panel-body">
                         <div className="form-horizontal">
-                            <div className="form-group">
+                            <div className={`form-group ${styles.center}`}>
                                 <div className="col-sm-3 control-label">
                                     <input type="radio" name="minutes" value="false" checked={this.state.type === "hourly"} id="schedule-hourly" onChange={this.onSelectHourly}/>
-                                    <label htmlFor="schedule-hourly">{t("Hourly:")}</label>
+                                    <label className={styles.radio} htmlFor="schedule-hourly">{t("Hourly:")}</label>
                                 </div>
                                 <div className="col-sm-3">
                                     <input className="form-control" name="minutes" type="number" value={this.state.minutes.id} min="0" max="59" onChange={this.onSelectMinutes} />
@@ -322,19 +322,19 @@ class RecurringEventPicker extends React.Component<RecurringEventPickerProps, Re
                                     <HelpLink text={t("The action will be executed every hour at the specified minute")}/>
                                 </div>
                             </div>
-                            <div className="form-group">
+                            <div className={`form-group ${styles.center}`}>
                                 <div className="col-sm-3 control-label">
                                     <input type="radio" name="date_daily" value="false" checked={this.state.type === "daily"} id="schedule-daily" onChange={this.onSelectDaily}/>
-                                    <label htmlFor="schedule-daily">{t("Daily:")}</label>
+                                    <label className={styles.radio} htmlFor="schedule-daily">{t("Daily:")}</label>
                                 </div>
                                 <div className="col-sm-3">
                                     <DateTimePicker onChange={this.onDailyTimeChanged} value={this.state.time} timezone={this.props.timezone} hideDatePicker={true}/>
                                 </div>
                             </div>
-                            <div className="form-group">
+                            <div className={`form-group ${styles.center}`}>
                                 <div className="col-sm-3 control-label">
                                     <input type="radio" name="date_weekly" value="false" checked={this.state.type === "weekly"} id="schedule-weekly" onChange={this.onSelectWeekly}/>
-                                    <label htmlFor="schedule-weekly">{t("Weekly:")}</label>
+                                    <label className={styles.radio} htmlFor="schedule-weekly">{t("Weekly:")}</label>
                                 </div>
                                 <div className="col-sm-3">
                                     <Combobox id="weekly-day-picker" name="date_weekly" selectedId={this.state.weekDay.id}
@@ -347,7 +347,7 @@ class RecurringEventPicker extends React.Component<RecurringEventPickerProps, Re
                                     <DateTimePicker onChange={this.onWeeklyTimeChanged} value={this.state.time} timezone={this.props.timezone} hideDatePicker={true}/>
                                 </div>
                             </div>
-                            <div className="form-group">
+                            <div className={`form-group ${styles.center}`}>
                                 <div className="col-sm-3 control-label">
                                     <input type="radio" name="date_monthly" value="false" checked={this.state.type === "monthly"} id="schedule-monthly" onChange={this.onSelectMonthly}/>
                                     <label className={styles.radio} htmlFor="schedule-monthly">{t("Monthly:")}</label>
@@ -366,10 +366,10 @@ class RecurringEventPicker extends React.Component<RecurringEventPickerProps, Re
                                     <HelpLink text={t("Days are limited to 28 to have a recurring schedule available for all the months")}/>
                                 </div>
                             </div>
-                            <div className="form-group">
+                            <div className={`form-group ${styles.center}`}>
                                 <div className="col-sm-3 control-label">
                                     <input type="radio" name="date_cron" value="false" checked={this.state.type === "cron"} id="schedule-cron" onChange={this.onSelectCustom}/>
-                                    <label htmlFor="schedule-cron">{t("Custom Quartz format:")}</label>
+                                    <label className={styles.radio} htmlFor="schedule-cron">{t("Custom Quartz format:")}</label>
                                 </div>
                                 <div className="col-sm-3">
                                     <input className="form-control" type="text" name="cron" value={this.state.cron} placeholder={t("e.g. \"0 15 2 ? * 7\"")} id="custom-cron" onChange={this.onCronChanged}/>
