bda61a969b569f2f287ef5e73f079e7f71f18f53
Pascal Arlt <parlt@suse.com>
2020-03-12 14:54:45 +0100

    Replace html table with Table component
    Signed-off-by: Pascal Arlt <parlt@suse.com>

--- web/html/src/manager/state/recurring-states-list.js
+++ web/html/src/manager/state/recurring-states-list.js
@@ -8,6 +8,8 @@ const Button = require("components/buttons").Button;
 const {Toggler} = require("components/toggler");
 const ModalButton = require("components/dialog/ModalButton").ModalButton;
 const DeleteDialog = require("components/dialog/DeleteDialog").DeleteDialog;
+const {Column} = require("components/table/Column");
+const {Table} = require("components/table/Table");
 
 // todo extract to utils
 const targetTypeToString = (targetType) => {
@@ -20,7 +22,7 @@ const targetTypeToString = (targetType) => {
             return "Organization";
     }
     return null;
-}
+};
 
 class RecurringStatesList extends React.Component {
 
@@ -38,61 +40,6 @@ class RecurringStatesList extends React.Component {
         });
     }
 
-    tableBody = () => {
-        const elements = [];
-        const data = this.props.data;
-        if (data !== undefined) {
-            data.map(row => {
-                elements.push(
-                    <tr>
-                        <td>
-                            <Toggler value={row.active} className="btn" handler={() => this.props.onToggleActive(row)} />
-                        </td>
-                        <td className="text-center">{row.scheduleName}</td>
-                        <td className="text-center">{row.cron}</td>
-                        {this.props.disableCreate ? <td className="text-center">{t(targetTypeToString(row.targetType))}</td> : null}
-                        <td className="text-right">
-                            <div className="btn-group">
-                                <Button
-                                    className="btn-default btn-sm"
-                                    title={t("Details")}
-                                    icon="fa-list"
-                                    handler={() =>  this.props.onSelect(row)}
-                                />
-                                <Button
-                                    className="btn-default btn-sm"
-                                    title={t("Edit")}
-                                    icon="fa-edit"
-                                    handler={() => this.props.onEdit(row)}
-                                />
-                                <ModalButton
-                                    className="btn-default btn-sm"
-                                    title={t("Delete")}
-                                    icon="fa-trash"
-                                    target="delete-modal"
-                                    item={row}
-                                    onClick={i => this.selectToDelete(i)}
-                                />
-                            </div>
-                        </td>
-                    </tr>
-                );
-            });
-        }
-
-        return (
-            <tbody className="table-content">
-            {elements.length > 0 ? elements :
-                <tr>
-                    <td colSpan="4">
-                        <div>{t("No schedules created." + (this.props.disableCreate ? "" : " Use Create to add a schedule."))}</div>
-                    </td>
-                </tr>
-            }
-            </tbody>
-        );
-    };
-
     render() {
         const createButton = [
             <div className="btn-group pull-right">
@@ -105,7 +52,6 @@ class RecurringStatesList extends React.Component {
                 />
             </div>
         ];
-        const targetType = this.props.disableCreate ? <th className="text-center">{t("Target Type")}</th> : null;
 
         return (
             <div>
@@ -116,25 +62,86 @@ class RecurringStatesList extends React.Component {
                                 <h3>Schedules</h3>
                             </div>
                         </div>
-                        <div className="panel-body">
-                            <table className="table table-striped">
-                                <thead>
-                                <tr>
-                                    <th>{t("Active")}</th>
-                                    <th className="text-center">{t("Schedule Name")}</th>
-                                    <th className="text-center">{t("Frequency")}</th>
-                                    {targetType}
-                                    <th className="text-right">{t("Actions")}</th>
-                                </tr>
-                                </thead>
-                                {this.tableBody()}
-                            </table>
-                            <DeleteDialog
-                                id="delete-modal"
-                                title={t("Delete Recurring State Schedule")}
-                                content={<span>{t("Are you sure you want to delete the selected item?")}</span>}
-                                onConfirm={() => this.props.onDelete(this.state.itemToDelete)}
-                                onClosePopUp={() => this.selectToDelete(null)}
+                        <div>
+                            <Table
+                                data={this.props.data}
+                                identifier={action => action.recurringActionId}
+                                //initialSortColumnKey="scheduleName"
+                                initialItemsPerPage={this.props.disableCreate ? userPrefPageSize : 0}
+                                emptyText={t("No schedules created." + (this.props.disableCreate ? "" : " Use Create to add a schedule."))}
+                            >
+                                <Column
+                                    columnKey="active"
+                                    header={t('Active')}
+                                    cell={(row, criteria) => {return (
+                                        <Toggler
+                                            value={row.active}
+                                            className="btn"
+                                            handler={() => this.props.onToggleActive(row)}
+                                        />
+                                    );
+                                    }}
+                                />
+                                <Column
+                                    columnClass="text-center"
+                                    headerClass="text-center"
+                                    columnKey="scheduleName"
+                                    header={t('Schedule Name')}
+                                    cell={(row, criteria) => row.scheduleName}
+                                />
+                                <Column
+                                    columnClass="text-center"
+                                    headerClass="text-center"
+                                    columnKey="frequency"
+                                    header={t('Frequency')}
+                                    cell={(row, criteria) => row.cron}
+                                />
+                                { this.props.disableCreate ?
+                                    <Column
+                                        columnClass="text-center"
+                                        headerClass="text-center"
+                                        columnKey="targetType"
+                                        header={t('Target Type')}
+                                        cell={(row, criteria) => targetTypeToString(row.targetType)}
+                                    />
+                                    : null }
+                                <Column
+                                    columnClass="text-right"
+                                    headerClass="text-right"
+                                    header={t('Actions')}
+                                    cell={(row, criteria) => {
+                                        return (
+                                            <div className="btn-group">
+                                                <Button
+                                                    className="btn-default btn-sm"
+                                                    title={t("Details")}
+                                                    icon="fa-list"
+                                                    handler={() => {this.props.onSelect(row)}}
+                                                />
+                                                <Button
+                                                    className="btn-default btn-sm"
+                                                    title={t("Edit")}
+                                                    icon="fa-edit"
+                                                    handler={() => {this.props.onEdit(row)}}
+                                                />
+                                                <ModalButton
+                                                    className="btn-default btn-sm"
+                                                    title={t("Delete")}
+                                                    icon="fa-trash"
+                                                    target="delete-modal"
+                                                    item={row}
+                                                    onClick={i => this.selectToDelete(i)}
+                                                />
+                                            </div>
+                                        );
+                                    }}
+                                />
+                            </Table>
+                            <DeleteDialog id="delete-modal"
+                                          title={t("Delete Recurring State Schedule")}
+                                          content={<span>{t("Are you sure you want to delete the selected item?")}</span>}
+                                          onConfirm={() => this.props.onDelete(this.state.itemToDelete)}
+                                          onClosePopUp={() => this.selectToDelete(null)}
                             />
                         </div>
                     </div>
