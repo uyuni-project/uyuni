59438d6e1d02215f8248a6f9a16f9fb9425a8ac9
Michael Calmer <mc@suse.de>
2020-03-13 14:51:24 +0100

    add new type to message

--- web/html/src/manager/notifications/notification-messages.js
+++ web/html/src/manager/notifications/notification-messages.js
@@ -154,6 +154,7 @@ class NotificationMessages extends React.Component {
       case 'OnboardingFailed': typeText = t('Onboarding failed'); break;
       case 'ChannelSyncFailed': typeText = t('Channel sync failed'); break;
       case 'ChannelSyncFinished': typeText = t('Channel sync finished'); break;
+      case 'CreateBootstrapRepoFailed': typeText = t('Creating Bootstrap Repository failed'); break;
     }
     return typeText;
   };
