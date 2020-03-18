91ea1f3287ba192f11732631ff145443cfb80cc6
Hubert Mantel <1080885+mantel@users.noreply.github.com>
2020-03-18 17:15:58 +0100

    move vital proxy templates to a safe place outside of docu (bsc#1166284)

--- proxy/installer/configure-proxy.sh
+++ proxy/installer/configure-proxy.sh
@@ -348,7 +348,7 @@ fi
 
 SYSTEM_ID=$(/usr/bin/xsltproc /usr/share/rhn/get_system_id.xslt $SYSTEMID_PATH | cut -d- -f2)
 
-DIR=/usr/share/doc/proxy/conf-template
+DIR=/usr/share/rhn/proxy-template
 HOSTNAME=$(hostname -f)
 
 default_or_input "SUSE Manager Parent" RHN_PARENT $PROPOSED_PARENT
