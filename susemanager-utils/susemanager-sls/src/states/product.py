3d91a9ef12cf56ca33f1cb757025dd9634c5d7c9
Can Bulut Bayburt <cbbayburt@suse.com>
2020-03-19 18:41:22 +0100

    Make 'product' state module only available for minions with zypper >= 1.8.13 (bsc#1166699)
    (cherry picked from commit 89373251b83737dd50795caeb2c5370aee0f16f7)

--- susemanager-utils/susemanager-sls/src/states/product.py
+++ susemanager-utils/susemanager-sls/src/states/product.py
@@ -6,7 +6,7 @@ Only supported with :mod:`zypper <salt.modules.zypper>`
 
 import logging
 
-import salt.utils.path
+from salt.utils.versions import version_cmp
 from salt.exceptions import CommandExecutionError
 
 log = logging.getLogger(__name__)
@@ -19,9 +19,16 @@ def __virtual__():
     '''
     if __grains__.get('os_family', '') != 'Suse':
         return (False, "Module product: non SUSE OS not supported")
+
     # Not all versions of SUSE use zypper, check that it is available
-    if not salt.utils.path.which('zypper'):
+    try:
+        zypp_info = __salt__['pkg.info_installed']('zypper')['zypper']
+    except CommandExecutionError:
         return (False, "Module product: zypper package manager not found")
+
+    # Minimum version that supports 'zypper search --provides'
+    if version_cmp(zypp_info['version'], '1.8.13') < 0:
+        return (False, "Module product: zypper 1.8.13 or greater required")
     return __virtualname__
 
 
