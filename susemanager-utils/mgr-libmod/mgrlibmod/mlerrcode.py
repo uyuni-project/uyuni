8efd0d47fc0a39ccf6356c168c4967c4b6670ce3
Bo Maryniuk <bo@suse.de>
2020-03-25 18:18:44 +0100

    Add exception types with error codes

--- susemanager-utils/mgr-libmod/mgrlibmod/mlerrcode.py
+++ susemanager-utils/mgr-libmod/mgrlibmod/mlerrcode.py
@@ -6,3 +6,31 @@ MLERR_OK = 0
 MLERR_GENERAL_ERROR = 1
 MLERR_MODULE_NOT_FOUND = 201
 MLERR_STREAM_NOT_FOUND = 202
+MLERR_REQUEST_ERROR = 301
+
+
+class MlGeneralException(Exception):
+    """
+    General exception
+    """
+    code = MLERR_GENERAL_ERROR
+
+
+class MlModuleNotFound(Exception):
+    """
+    Module was not found exception
+    """
+    code = MLERR_MODULE_NOT_FOUND
+
+
+class MlStreamNotFound(Exception):
+    """
+    Stream was not found exception
+    """
+    code = MLERR_STREAM_NOT_FOUND
+
+class MlRequestError(Exception):
+    """
+    Wrong request definition
+    """
+    code = MLERR_REQUEST_ERROR
