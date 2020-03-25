64c288b25b325d172b1b5b8f6414822be6890658
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2020-03-25 13:40:25 +0000

    Use int instead of long for either Python 2 or 3

--- client/rhel/spacewalk-client-tools/src/bin/rhn_check.py
+++ client/rhel/spacewalk-client-tools/src/bin/rhn_check.py
@@ -67,7 +67,6 @@ try: # python2
     import xmlrpclib
 except ImportError: # python3
     import xmlrpc.client as xmlrpclib
-    long = int
 
 if 'sgmlop' in sys.modules:
     del sys.modules['sgmlop']
@@ -359,7 +358,7 @@ class CheckCli(rhncli.RhnCli):
                 pass
 
         # We need to fit into xmlrpc's integer limits
-        if status_report['uptime'][1] > long(2)**31-1:
+        if status_report['uptime'][1] > int(2)**31-1:
             status_report['uptime'][1] = -1
 
         return status_report
