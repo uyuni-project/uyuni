f86347b3b454097711154714ebe59380b4f9b1fd
Ondrej Holecek <oholecek@suse.com>
2020-04-27 23:33:22 +0200

    Pass primary minion ip to the salt runner
    collectKiwiImage runner needs reachable address for rsync. Instead
    of parsing and evaulating all IPs from grains, rely on primary IP
    address stored at database.

--- susemanager-utils/susemanager-sls/modules/runners/kiwi-image-collect.py
+++ susemanager-utils/susemanager-sls/modules/runners/kiwi-image-collect.py
@@ -1,37 +1,36 @@
 # SUSE Manager
-# Copyright (c) 2018,2019 SUSE LLC
+# Copyright (c) 2018--2020 SUSE LLC
 
 # runner to collect image from build host
 
 import os
-import salt.exceptions
 import logging
 
 log = logging.getLogger(__name__)
 
-def upload_file_from_minion(minion, filetoupload, targetdir):
-    src = 'root@' + minion + ':' + filetoupload
-    result = __salt__['salt.cmd'](
+def upload_file_from_minion(minion, minion_ip, filetoupload, targetdir):
+    fqdn = __salt__['cache.grains'](tgt=minion).get(minion, {}).get('fqdn')
+    log.info('Collecting image "{}" from minion {} (FQDN: {}, IP: {})'.format(filetoupload, minion, fqdn, minion_ip))
+    if not fqdn or fqdn == 'localhost':
+        fqdn = minion_ip
+    src = 'root@{}:{}'.format(fqdn, filetoupload)
+    return __salt__['salt.cmd'](
       'rsync.rsync',
       src, targetdir,
       rsh='ssh -o IdentityFile=/srv/susemanager/salt/salt_ssh/mgr_ssh_id -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
     )
-    if result['retcode'] != 0:
-      raise ConnectionError('Failed to transfer image from minion {}: {}'.format(minion, result['stderr']))
-    return result
 
 def move_file_from_minion_cache(minion, filetomove, targetdir):
     src = os.path.join(__opts__['cachedir'], 'minions', minion, 'files', filetomove.lstrip('/'))
+    log.info('Collecting image from minion cache "{}"'.format(src))
     # file.move throws an exception in case of error
-    return __salt__['salt.cmd']('file.move', src, targetdir);
+    return __salt__['salt.cmd']('file.move', src, targetdir)
 
-def kiwi_collect_image(minion, filepath, image_store_dir):
+def kiwi_collect_image(minion, minion_ip, filepath, image_store_dir):
     __salt__['salt.cmd']('file.mkdir', image_store_dir)
 
-    pillars = list(__salt__['cache.pillar'](tgt=minion).values())[0]
-    if pillars.get('use_salt_transport'):
-      log.info('Collecting image "{}" from minion cache'.format(filepath))
-      return move_file_from_minion_cache(minion, filepath, image_store_dir)
+    use_salt_transport = __salt__['cache.pillar'](tgt=minion).get(minion, {}).get('use_salt_transport')
+    if use_salt_transport:
+        return move_file_from_minion_cache(minion, filepath, image_store_dir)
 
-    log.info('Collecting image "{}" from minion using rsync'.format(filepath))
-    return upload_file_from_minion(minion, filepath, image_store_dir)
+    return upload_file_from_minion(minion, minion_ip, filepath, image_store_dir)
