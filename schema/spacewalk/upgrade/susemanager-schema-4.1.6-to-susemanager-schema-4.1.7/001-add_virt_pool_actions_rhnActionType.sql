fa5ca4166a71f9aaa4a74832561e593bf563a185
Cédric Bosdonnat <cbosdonnat@suse.com>
2020-04-28 15:04:37 +0200

    Virtualization pool actions schema upgrade scripts fix
    The virtualization pool action schema upgrade scripts mistakenly went
    into 4.1.4 to 4.1.5 folder instead of 4.1.5 to 4.1.6. Adding them to
    the latest folder will at least get the next releases right.

--- schema/spacewalk/upgrade/susemanager-schema-4.1.6-to-susemanager-schema-4.1.7/001-add_virt_pool_actions_rhnActionType.sql
+++ schema/spacewalk/upgrade/susemanager-schema-4.1.6-to-susemanager-schema-4.1.7/001-add_virt_pool_actions_rhnActionType.sql
@@ -13,6 +13,36 @@
 --
 
 insert into rhnActionType (id, label, name, trigger_snapshot, unlocked_only) (
+    select 509, 'virt.pool_refresh', 'Refresh a virtual storage pool', 'N', 'N'
+    from dual
+    where not exists (select 1 from rhnActionType where id = 509)
+);
+
+insert into rhnActionType (id, label, name, trigger_snapshot, unlocked_only) (
+    select 510, 'virt.pool_start', 'Starts a virtual storage pool', 'N', 'N'
+    from dual
+    where not exists (select 1 from rhnActionType where id = 510)
+);
+
+insert into rhnActionType (id, label, name, trigger_snapshot, unlocked_only) (
+    select 511, 'virt.pool_stop', 'Stops a virtual storage pool', 'N', 'N'
+    from dual
+    where not exists (select 1 from rhnActionType where id = 511)
+);
+
+insert into rhnActionType (id, label, name, trigger_snapshot, unlocked_only) (
+    select 512, 'virt.pool_delete', 'Deletes a virtual storage pool', 'N', 'N'
+    from dual
+    where not exists (select 1 from rhnActionType where id = 512)
+);
+
+insert into rhnActionType (id, label, name, trigger_snapshot, unlocked_only) (
+    select 513, 'virt.pool_create', 'Creates a virtual storage pool', 'N', 'N'
+    from dual
+    where not exists (select 1 from rhnActionType where id = 513)
+);
+
+insert into rhnActionType (id, label, name, trigger_snapshot, unlocked_only) (
     select 514, 'virt.volume_delete', 'Deletes a virtual storage volume', 'N', 'N'
     from dual
     where not exists (select 1 from rhnActionType where id = 514)
