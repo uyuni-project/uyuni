aee98703d6d0c6031623f1a6626843e8bde8b6dd
Pascal Arlt <parlt@suse.com>
2020-04-28 10:09:26 +0200

    Add ui for listing maintenance schedules
    Signed-off-by: Pascal Arlt <parlt@suse.com>

---
+++ web/html/src/manager/maintenance/maintenance-schedules-list.js
@@ -0,0 +1,131 @@
+/* eslint-disable */
+'use strict';
+
+const React = require("react");
+const ReactDOM = require("react-dom");
+const { InnerPanel } = require('components/panels/InnerPanel');
+const Button = require("components/buttons").Button;
+const ModalButton = require("components/dialog/ModalButton").ModalButton;
+const DeleteDialog = require("components/dialog/DeleteDialog").DeleteDialog;
+const {Column} = require("components/table/Column");
+const {Table} = require("components/table/Table");
+
+class MaintenanceSchedulesList extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            itemsToDelete: []
+        };
+    }
+
+    selectToDelete(item) {
+        this.setState({
+            itemToDelete: item
+        });
+    }
+
+    render() {
+        const createButton = [
+            <div className="btn-group pull-right">
+                <Button
+                    className="btn-default"
+                    icon="fa-plus"
+                    text={t("Create")}
+                    title="Create a new maintenance schedule"
+                    handler={() => this.props.onActionChanged("create")}
+                />
+            </div>
+        ];
+
+        return (
+            <div>
+                <InnerPanel title={t("Maintenance Schedules")} icon="spacewalk-icon-salt" buttons={this.props.disableCreate ? null : createButton}>
+                    <div className="panel panel-default">
+                        <div className="panel-heading">
+                            <div>
+                                <h3>Schedules</h3>
+                            </div>
+                        </div>
+                        <div>
+                            <Table
+                                data={this.props.data}
+                                identifier={row => row.scheduleId}
+                                initialItemsPerPage={userPrefPageSize}
+                                emptyText={t("No schedules created. Use Create to add a schedule.")}
+                            >
+                                <Column
+                                    columnKey="scheduleName"
+                                    header={t('Schedule Name')}
+                                    cell={(row) => row.scheduleName}
+                                />
+                                <Column
+                                    columnClass="text-center"
+                                    headerClass="text-center"
+                                    columnKey="column2"
+                                    header={t('Column2')}
+                                    cell={(row) => row.column2}
+                                />
+                                <Column
+                                    columnClass="text-center"
+                                    headerClass="text-center"
+                                    columnKey="column3"
+                                    header={t('Column3')}
+                                    cell={(row) => row.column3}
+                                />
+                                <Column
+                                    columnClass="text-center"
+                                    headerClass="text-center"
+                                    columnKey="column4"
+                                    header={t('Column4')}
+                                    cell={(row) => row.column4}
+                                />
+                                <Column
+                                    columnClass="text-right"
+                                    headerClass="text-right"
+                                    header={t('Actions')}
+                                    cell={(row) =>
+                                        <div className="btn-group">
+                                            <Button
+                                                className="btn-default btn-sm"
+                                                title={t("Details")}
+                                                icon="fa-list"
+                                                handler={() => {this.props.onSelect(row)}}
+                                            />
+                                            <Button
+                                                className="btn-default btn-sm"
+                                                title={t("Edit")}
+                                                icon="fa-edit"
+                                                handler={() => {this.props.onEdit(row)}}
+                                            />
+                                            <ModalButton
+                                                className="btn-default btn-sm"
+                                                title={t("Delete")}
+                                                icon="fa-trash"
+                                                target="delete-modal"
+                                                item={row}
+                                                onClick={i => this.selectToDelete(i)}
+                                            />
+                                        </div>
+                                    }
+                                />
+                            </Table>
+                            <DeleteDialog id="delete-modal"
+                                          title={t("Delete maintenance schedule")}
+                                          content={t("Are you sure you want to delete the selected item? \n" +
+                                          "Deleting the schedule will unassign all systems from this schedule.")}
+                                          onConfirm={() => this.props.onDelete(this.state.itemToDelete)}
+                                          onClosePopUp={() => this.selectToDelete(null)}
+                            />
+                        </div>
+                    </div>
+                </InnerPanel>
+            </div>
+        );
+    }
+}
+
+module.exports = {
+    MaintenanceSchedulesList: MaintenanceSchedulesList
+};
