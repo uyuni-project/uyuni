9cf5bedb02339edadbab75a68964547ba7ddccc1
Cédric Bosdonnat <cbosdonnat@suse.com>
2020-04-28 15:04:37 +0200

    Virtualization pool actions schema upgrade scripts fix
    The virtualization pool action schema upgrade scripts mistakenly went
    into 4.1.4 to 4.1.5 folder instead of 4.1.5 to 4.1.6. Adding them to
    the latest folder will at least get the next releases right.

---
+++ schema/spacewalk/upgrade/susemanager-schema-4.1.6-to-susemanager-schema-4.1.7/005-rhnActionVirtPoolDelete.sql
@@ -0,0 +1,32 @@
+-- Copyright (c) 2020 SUSE LLC
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+-- Red Hat trademarks are not licensed under GPLv2. No permission is
+-- granted to use or replicate Red Hat trademarks that are incorporated
+-- in this software or its documentation.
+--
+
+CREATE TABLE IF NOT EXISTS rhnActionVirtPoolDelete
+(
+    action_id            NUMERIC NOT NULL
+                             CONSTRAINT rhn_action_virt_pool_delete_aid_fk
+                                 REFERENCES rhnAction (id)
+                                 ON DELETE CASCADE
+                             CONSTRAINT rhn_action_virt_pool_delete_aid_pk
+                                 PRIMARY KEY,
+    pool_name            VARCHAR(256),
+    purge                CHAR(1)
+                            DEFAULT ('Y') NOT NULL
+                            CONSTRAINT rhn_avpdelete_purge_ck
+                                CHECK (purge in ('Y','N'))
+)
+;
+
+CREATE UNIQUE INDEX IF NOT EXISTS rhn_action_virt_pool_delete_aid_uq
+    ON rhnActionVirtPoolDelete (action_id);
