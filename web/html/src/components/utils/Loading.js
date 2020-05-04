afab1804e56f445abbda994609d2700c56a9afcd
dleidi <dleidi@suse.com>
2020-05-04 21:50:02 +0200

    Use eos-icons svg in the loading component

--- web/html/src/components/utils/Loading.js
+++ web/html/src/components/utils/Loading.js
@@ -4,18 +4,29 @@ type LoadingProps = {
   /** Text to be displayed with the loading spinner */
   text?: string,
   /** whether to show borders around the component */
-  withBorders?: bool
+  withBorders?: bool,
+  /** Text to display as a tooltip */
+  title?: string,
+  /** whether to choose the size of the icon */
+  size?: string
 }
 
-export function Loading({ withBorders, text } : LoadingProps) {
+const _SIZES = {
+  S: "12px",
+  M: "24px",
+  L: "36px",
+  XL: "48px"
+};
+
+export function Loading({ withBorders, text, title, size } : LoadingProps) {
   return (
-    <div className="panel-body text-center">
+    <div className="panel-body text-center" title={title ? title : text}>
       {
           withBorders
             ? <div className="line-separator" />
             : null
         }
-      <i className="fa fa-spinner fa-spin fa-1-5x" />
+      <img src="/css/eos-icons/animated-svg/loading.svg" with={_SIZES[size.toUpperCase()]} height={_SIZES[size.toUpperCase()]} />
       <h4>{text}</h4>
       {
         withBorders
@@ -29,4 +40,6 @@ export function Loading({ withBorders, text } : LoadingProps) {
 Loading.defaultProps = {
   text: undefined,
   withBorders: false,
+  title: undefined,
+  size: "M",
 }
--- web/html/src/components/utils/Loading.stories.js
+++ web/html/src/components/utils/Loading.stories.js
@@ -8,4 +8,5 @@ export default {
 
 export const normal = () => <Loading />;
 export const withText = () => <Loading text="loading text" />;
+export const withText = () => <Loading text="loading text" title="loading title" />;
 export const withBorder = () => <Loading text="loading text" withBorders/>;
