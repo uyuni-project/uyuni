fc119e0d2a94e5855d7a090cafeb37ee97347953
Frantisek Kobzik <fkobzik@suse.de>
2020-05-05 16:00:48 +0200

    Manager: Assigning/retracting maintenance schedules to/from systems

--- java/code/src/com/suse/manager/maintenance/test/MaintenanceManagerTest.java
+++ java/code/src/com/suse/manager/maintenance/test/MaintenanceManagerTest.java
@@ -14,9 +14,21 @@
  */
 package com.suse.manager.maintenance.test;
 
+import static com.redhat.rhn.domain.role.RoleFactory.ORG_ADMIN;
+
+import com.redhat.rhn.common.security.PermissionException;
 import com.redhat.rhn.common.util.FileUtils;
+import com.redhat.rhn.domain.action.Action;
+import com.redhat.rhn.domain.action.ActionFactory;
+import com.redhat.rhn.domain.action.server.test.ServerActionTest;
+import com.redhat.rhn.domain.action.test.ActionFactoryTest;
+import com.redhat.rhn.domain.org.Org;
+import com.redhat.rhn.domain.server.Server;
+import com.redhat.rhn.domain.server.test.MinionServerFactoryTest;
+import com.redhat.rhn.domain.user.User;
 import com.redhat.rhn.testing.BaseTestCaseWithUser;
 import com.redhat.rhn.testing.TestUtils;
+import com.redhat.rhn.testing.UserTestUtils;
 
 import com.suse.manager.maintenance.MaintenanceManager;
 import com.suse.manager.model.maintenance.MaintenanceCalendar;
@@ -28,6 +40,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 
 public class MaintenanceManagerTest extends BaseTestCaseWithUser {
@@ -155,5 +168,128 @@ public class MaintenanceManagerTest extends BaseTestCaseWithUser {
         assertEquals("test server", dbSchedule.getName());
         assertEquals(ScheduleType.MULTI, dbSchedule.getScheduleType());
         assertEquals(dbCal, dbSchedule.getCalendarOpt().orElse(null));
+    }
+
+    /**
+     * Test assigning a {@link MaintenanceSchedule} to {@link Server}s and listing it.
+     *
+     * @throws Exception
+     */
+    public void testListSystemsWithSchedule() throws Exception {
+        user.addPermanentRole(ORG_ADMIN);
+        MaintenanceManager mm = MaintenanceManager.instance();
+        MaintenanceSchedule schedule = mm.createMaintenanceSchedule(
+                user, "test-schedule-1", MaintenanceSchedule.ScheduleType.SINGLE, Optional.empty());
+
+        Server withSchedule = MinionServerFactoryTest.createTestMinionServer(user);
+        Server withoutSchedule = MinionServerFactoryTest.createTestMinionServer(user);
+
+        mm.assignScheduleToSystems(user, schedule, Set.of(withSchedule.getId()));
+
+        assertEquals(
+                List.of(withSchedule.getId()),
+                mm.listSystemIdsWithSchedule(user, schedule)
+        );
+    }
+
+    /**
+     * Test retracting a {@link MaintenanceSchedule} from {@link Server}
+     *
+     * @throws Exception
+     */
+    public void testRetractScheduleFromSystems() throws Exception {
+        user.addPermanentRole(ORG_ADMIN);
+        MaintenanceManager mm = MaintenanceManager.instance();
+        MaintenanceSchedule schedule = MaintenanceManager.instance().createMaintenanceSchedule(
+                user, "test-schedule-1", MaintenanceSchedule.ScheduleType.SINGLE, Optional.empty());
+
+        // assign the schedule to both systems
+        Server system1 = MinionServerFactoryTest.createTestMinionServer(user);
+        Server system2 = MinionServerFactoryTest.createTestMinionServer(user);
+        mm.assignScheduleToSystems(user, schedule, Set.of(system1.getId(), system2.getId()));
+
+        // retract it from one system
+        mm.retractScheduleFromSystems(user, Set.of(system1.getId()));
+
+        // check, that the other system still has it
+        assertEquals(
+                List.of(system2.getId()),
+                mm.listSystemIdsWithSchedule(user, schedule)
+        );
+    }
+
+    /**
+     * Test the behavior when user tries to assign a schedule to a system, that has already some offending actions
+     * pending.
+     *
+     * @throws Exception
+     */
+    public void testAssignScheduleToSystemWithPendingActions() throws Exception {
+        user.addPermanentRole(ORG_ADMIN);
+        MaintenanceManager mm = MaintenanceManager.instance();
+        MaintenanceSchedule schedule = MaintenanceManager.instance().createMaintenanceSchedule(
+                user, "test-schedule-1", MaintenanceSchedule.ScheduleType.SINGLE, Optional.empty());
+
+        Server sys1 = MinionServerFactoryTest.createTestMinionServer(user);
+        Server sys2 = MinionServerFactoryTest.createTestMinionServer(user);
+
+        // assign an offending action to one system
+        Action disallowedAction = ActionFactoryTest.createAction(user, ActionFactory.TYPE_APPLY_STATES);
+        ServerActionTest.createServerAction(sys1, disallowedAction);
+
+        assertExceptionThrown(
+                () -> mm.assignScheduleToSystems(user, schedule, Set.of(sys1.getId(), sys2.getId())),
+                IllegalArgumentException.class);
+    }
+
+    /**
+     * Test schedules assigning in a cross-organization context.
+     *
+     * @throws Exception
+     */
+    public void testAssignScheduleCrossOrg() throws Exception {
+        MaintenanceManager mm = MaintenanceManager.instance();
+        user.addPermanentRole(ORG_ADMIN);
+        Org acmeOrg = UserTestUtils.createNewOrgFull("acme-123");
+        User user2 = UserTestUtils.createUser("user-321", acmeOrg.getId());
+        user2.addPermanentRole(ORG_ADMIN);
+
+        MaintenanceSchedule schedule1 = MaintenanceManager.instance().createMaintenanceSchedule(
+                user, "test-schedule-1", MaintenanceSchedule.ScheduleType.SINGLE, Optional.empty());
+        MaintenanceSchedule schedule2 = MaintenanceManager.instance().createMaintenanceSchedule(
+                user2, "test-schedule-2", MaintenanceSchedule.ScheduleType.SINGLE, Optional.empty());
+
+        Server system1 = MinionServerFactoryTest.createTestMinionServer(user);
+        Server system2 = MinionServerFactoryTest.createTestMinionServer(user2);
+
+        // user 2 assigns schedule 1
+        assertExceptionThrown(
+                () -> mm.assignScheduleToSystems(user2, schedule1, Set.of(system2.getId())),
+                PermissionException.class);
+
+        // user 2 retracts from system1
+        assertExceptionThrown(
+                () -> mm.retractScheduleFromSystems(user2, Set.of(system1.getId())),
+                PermissionException.class);
+
+        // user 2 assigns to system 1
+        assertExceptionThrown(
+                () -> mm.assignScheduleToSystems(user2, schedule2, Set.of(system1.getId())),
+                PermissionException.class);
+
+        // user 2 lists systems with schedule 1
+        assertExceptionThrown(
+                () -> mm.listSystemIdsWithSchedule(user2, schedule1),
+                PermissionException.class);
+    }
+
+    private void assertExceptionThrown(Runnable body, Class exceptionClass) {
+        try {
+            body.run();
+            fail("An exceptions should have been thrown.");
+        }
+        catch (Exception e) {
+            assertEquals("The exception should be of class: " + exceptionClass, exceptionClass, e.getClass());
+        }
     }
 }
