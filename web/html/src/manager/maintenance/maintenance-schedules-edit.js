57dc106ae29b3fec3597cb9a9daea6f08aef6dc0
Pascal Arlt <parlt@suse.com>
2020-05-19 13:09:23 +0200

    Make 'Add Calendar' button instead of checkbox
    Signed-off-by: Pascal Arlt <parlt@suse.com>

--- web/html/src/manager/maintenance/maintenance-schedules-edit.js
+++ web/html/src/manager/maintenance/maintenance-schedules-edit.js
@@ -68,7 +68,6 @@ class MaintenanceSchedulesEdit extends React.Component {
             scheduleName: model.scheduleName,
             scheduleType: model.scheduleType,
             calendarName: model.calendarName || this.state.calendarName,
-            calendarAdded: model.calendarAdded
         });
     };
 
@@ -77,7 +76,6 @@ class MaintenanceSchedulesEdit extends React.Component {
             scheduleName: this.state.scheduleName,
             scheduleType: this.state.scheduleType,
             calendarName: this.state.calendarName,
-            calendarAdded: this.state.calendarAdded,
         }
     }
 
@@ -162,7 +160,7 @@ class MaintenanceSchedulesEdit extends React.Component {
         const hover = this.state.hover ? {cursor: "pointer"} : {};
         return (
             <InnerPanel title={t("Schedule Maintenance Window")} icon="spacewalk-icon-salt" buttonsLeft={buttonsLeft} buttons={buttons} >
-                <Form onChange={this.onFormChanged} model={this.setModel}>
+                <Form onChange={this.onFormChanged} model={this.setModel()}>
                     <Text name="scheduleName" required type="text" label={t("Schedule Name")} labelClass="col-sm-3" divClass="col-sm-6" />
                     <Radio name="scheduleType" inline={true} label={t('Type')} labelClass="col-md-3" divClass="col-md-6"
                            items={[
@@ -170,68 +168,80 @@ class MaintenanceSchedulesEdit extends React.Component {
                                {label: <b>{t('Multi')}</b>, value: 'multi'},
                            ]}
                     />
-                    <Check name="calendarAdded" label={<b>{t("Add calendar")}</b>} divClass="col-md-6 col-md-offset-3" />
-                    {/* TODO: Render reschedule strategy picker */}
-                    {this.state.calendarAdded &&
-                    <div className="panel panel-default">
-                        <div className="panel-heading">
-                            <h3>{t("Select calendar")}</h3>
-                        </div>
-                        <div className="panel-body">
-                            <div className="spacewalk-content-nav" id="config-channels-tabs">
-                                <ul className="nav nav-tabs">
-                                    <li className={this.state.view === 'existing' || this.state.view === '' ? 'active' : ''}>
-                                        <a onClick={() => this.setView('existing')} style={hover}
-                                           onMouseEnter={this.onToggleHover}
-                                           onMouseLeave={this.onToggleHover}>{t("Select existing")}</a>
-                                    </li>
-                                    <li className={this.state.view === 'new' ? 'active' : ''}>
-                                        <a onClick={() => this.setView('new')} style={hover}
-                                           onMouseEnter={this.onToggleHover}
-                                           onMouseLeave={this.onToggleHover}>{t("Create new")}</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            {this.state.view === "new" ?
-                                <div className="form-horizontal">
-                                    <Text name="calendarName" required type="text" label={t("Calendar Name")}
-                                          labelClass="col-md-3" divClass="col-md-6"/>
-                                    <div className="form-group">
-                                        <label className="col-md-3 control-label">{t("Calendar data")}:</label>
-                                        <div className="col-md-4">
-                                            <input type="text" className="form-control"
-                                                   placeholder={t("Enter Url to ical file or...")}
-                                                   value={this.state.fileName}
-                                                   disabled={this.state.calendarData || this.state.calendarUrl}
-                                                   onChange={this.onFileDestChanged}/>
-                                        </div>
-                                        <div className="col-md-1">
-                                            <Button id="ical-upload-btn" className="btn-default" text={t("Attach file")}
-                                                    handler={this.handleFileAttach}
-                                            />
-                                        </div>
-                                        <div className="col-md-1">
-                                            <Button id="ical-rm-btn" className="btn-default" text={t("Remove file")}
-                                                    handler={this.onIcalFileRemove}/>
-                                        </div>
-                                    </div>
-                                </div>
+                    <div className="form-group">
+                        <div className="col-md-6 col-md-offset-3">
+                            {!this.state.calendarAdded ?
+                                <Button className="btn-default" text="Add Calendar" icon="fa-chevron-down "
+                                        handler={() => this.setState({calendarAdded: !this.state.calendarAdded})} />
                                 :
-                                <div className="form-horizontal">
-                                    <div className="form-group">
-                                        <label className="col-md-3 control-label">{t("Calendar")}:</label>
-                                        <div className="col-md-6">
-                                            <Combobox id="calendarSelect" name="calendarSelect"
-                                                      data={this.state.calendar}
-                                                      onSelect={this.onSelectCalendar}
-                                            />
+                                <div className="panel panel-default">
+                                    <div className="panel-heading" style={{padding: "0px"}}>
+                                        <Button text="Add Calendar" icon="fa-chevron-up"
+                                                handler={() => this.setState({calendarAdded: !this.state.calendarAdded})} />
+                                    </div>
+                                    <div className="panel-body">
+                                        <div className="spacewalk-content-nav" id="config-channels-tabs">
+                                            <ul className="nav nav-tabs">
+                                                <li className={this.state.view === 'existing' || this.state.view === '' ? 'active' : ''}>
+                                                    <a onClick={() => this.setView('existing')} style={hover}
+                                                       onMouseEnter={this.onToggleHover}
+                                                       onMouseLeave={this.onToggleHover}>{t("Select existing")}</a>
+                                                </li>
+                                                <li className={this.state.view === 'new' ? 'active' : ''}>
+                                                    <a onClick={() => this.setView('new')} style={hover}
+                                                       onMouseEnter={this.onToggleHover}
+                                                       onMouseLeave={this.onToggleHover}>{t("Create new")}</a>
+                                                </li>
+                                            </ul>
                                         </div>
+                                        {this.state.view === "new" ?
+                                            <div className="form-horizontal">
+                                                <Text name="calendarName" required type="text" label={t("Calendar Name")}
+                                                      labelClass="col-md-3" divClass="col-md-8"/>
+                                                <div className="form-group">
+                                                    <label className="col-md-3 control-label">{t("Calendar data")}:</label>
+                                                    <div className="col-md-6" style={{alignItems: "center", display: "flex"}}>
+                                                        <input type="text" className="form-control"
+                                                               placeholder={t("Enter Url to ical file")}
+                                                               value={this.state.fileName}
+                                                               disabled={this.state.calendarData || this.state.calendarUrl}
+                                                               onChange={this.onFileDestChanged}/>
+                                                        {!this.state.calendarData && <b style={{paddingLeft: "25px"}}>or</b>}
+                                                    </div>
+                                                    {!this.state.calendarData ?
+                                                        <div className="col-md-1">
+                                                            <Button id="ical-upload-btn" className="btn-default"
+                                                                    text={t("Attach file")}
+                                                                    handler={this.handleFileAttach}
+                                                            />
+                                                        </div>
+                                                        :
+                                                        <div className="col-md-1">
+                                                            <Button id="ical-rm-btn" className="btn-default" text={t("Remove file")}
+                                                                    handler={this.onIcalFileRemove}/>
+                                                        </div>
+                                                    }
+                                                </div>
+                                            </div>
+                                            :
+                                            <div className="form-horizontal">
+                                                <div className="form-group">
+                                                    <label className="col-md-3 control-label">{t("Calendar")}:</label>
+                                                    <div className="col-md-7">
+                                                        <Combobox id="calendarSelect" name="calendarSelect"
+                                                                  data={this.props.calendarNames}
+                                                                  onSelect={this.onSelectCalendar}
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        }
                                     </div>
                                 </div>
                             }
                         </div>
                     </div>
-                    }
+                    {/* TODO: Render reschedule strategy picker */}
                 </Form>
                 <input type="file" id="ical-data-upload" style={{display: "none"}} onChange={this.onIcalFileAttach}/>
             </InnerPanel>
