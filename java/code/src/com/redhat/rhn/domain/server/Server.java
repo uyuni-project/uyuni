0bc1a1746ce39216215a4580f498492e6324f5fa
Martin Seidl <mseidl@suse.de>
2020-05-25 14:01:22 +0200

    Check if minion is in maintenance mode when running recurring actions

--- java/code/src/com/redhat/rhn/domain/server/Server.java
+++ java/code/src/com/redhat/rhn/domain/server/Server.java
@@ -54,6 +54,7 @@ import java.util.TreeSet;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
