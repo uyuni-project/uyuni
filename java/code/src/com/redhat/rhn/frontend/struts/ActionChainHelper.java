00bbfbbb16e290fcc55f54c541040f0c71776e94
Frantisek Kobzik <fkobzik@suse.de>
2020-05-27 15:03:38 +0200

    Struts: Refactor: USE_DATE -> SCHEDULE_TYPE field in the form
    This will allow support of multiple action schedule selectors (not just
    date picker and action chains).

--- java/code/src/com/redhat/rhn/frontend/struts/ActionChainHelper.java
+++ java/code/src/com/redhat/rhn/frontend/struts/ActionChainHelper.java
@@ -71,7 +71,7 @@ public class ActionChainHelper {
      * @return the action chain
      */
     public static ActionChain readActionChain(DynaActionForm form, User user) {
-        if (Boolean.FALSE.equals(form.get(DatePicker.USE_DATE))) {
+        if (DatePicker.ScheduleType.ACTION_CHAIN.asString().equals(form.get(DatePicker.SCHEDULE_TYPE))) {
             String label = sanitizeLabel((String) form.get(LABEL_PROPERTY_NAME));
 
             if (!StringUtils.isBlank(label)) {
