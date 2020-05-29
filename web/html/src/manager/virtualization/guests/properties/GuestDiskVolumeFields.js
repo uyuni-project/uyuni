236340047871596a6ca5dee5e23e15d9e74836fb
Cédric Bosdonnat <cbosdonnat@suse.com>
2020-05-29 16:42:29 +0200

    virt: add volumes listbox for disks of volume type
    To allow the user to select an existing volume add a volume listbox in
    the GuestDiskVolumeFields component.

--- web/html/src/manager/virtualization/guests/properties/GuestDiskVolumeFields.js
+++ web/html/src/manager/virtualization/guests/properties/GuestDiskVolumeFields.js
@@ -17,6 +17,16 @@ export function GuestDiskVolumeFields(props: Props) : React.Node {
   const formContext = React.useContext(FormContext);
 
   const first_pool = props.pools.length > 0 ? props.pools[0].name : '';
+
+  const selected_pool = props.pools.find(pool => pool.name === formContext.model[`disk${props.index}_source_pool`]);
+
+  const onVolumeChange = (name: string, value: string) => {
+    if (value) {
+      formContext.setModelValue(`disk${props.index}_source_size`, undefined);
+      formContext.setModelValue(`disk${props.index}_source_template`, undefined);
+    }
+  }
+
   const volume = formContext.model[`disk${props.index}_source_file`];
 
   return (
@@ -33,6 +43,25 @@ export function GuestDiskVolumeFields(props: Props) : React.Node {
       >
         {
           props.pools.map(k => <option key={k.name} value={k.name}>{k.name}</option>)
+        }
+      </Select>
+      <Select
+        key={`disk${props.index}_source_volume`}
+        name={`disk${props.index}_source_volume`}
+        label={t('Source Volume')}
+        labelClass="col-md-3"
+        divClass="col-md-6"
+        disabled={!props.onlyHandledDisks || !Object.keys(formContext.model).includes(`disk${props.index}_editable`) }
+        onChange={onVolumeChange}
+      >
+        <option></option>
+        {/* Adding the value is needed in case the pool is shut down and we don't have the volumes */}
+        { (!selected_pool || selected_pool.volumes.length === 0) && volume &&
+          <option key={volume} value={volume}>{volume}</option>
+        }
+        {
+            selected_pool &&
+              selected_pool.volumes.map(vol => <option key={vol.name} value={vol.name}>{vol.name}</option>)
         }
       </Select>
       { Object.keys(formContext.model).includes(`disk${props.index}_editable`) &&
