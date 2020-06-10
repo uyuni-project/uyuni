18098e036e837faf4025c537a1861c400b1097c7
Julio Gonzalez Gil <jgonzalez@suse.com>
2020-06-10 12:13:38 +0200

    Automatic commit of package [mgr-libmod] release [4.1.3-1].

--- susemanager-utils/mgr-libmod/mgr-libmod.spec
+++ susemanager-utils/mgr-libmod/mgr-libmod.spec
@@ -16,7 +16,7 @@
 #
 
 Name:           mgr-libmod
-Version:        4.1.2
+Version:        4.1.3
 Release:        1
 Summary:        libmod app
 License:        MIT
--- susemanager-utils/mgr-libmod/setup.py
+++ susemanager-utils/mgr-libmod/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="mgrlibmod",
-    version="4.1.2",
+    version="4.1.3",
     packages=["mgrlibmod",],
     license="MIT",
 )
