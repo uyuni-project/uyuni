12999f76f63f5a6f294aa15150339f72947e2973
Michael Calmer <mc@suse.de>
2020-06-15 13:36:51 +0200

    introduce maintenance_mode_only flag in rhnActionType

--- schema/spacewalk/common/data/rhnActionType.sql
+++ schema/spacewalk/common/data/rhnActionType.sql
@@ -17,105 +17,75 @@
 --
 -- data for rhnActionType
 
--- last two values are for TRIGGER_SNAPSHOT, UNLOCKED_ONLY
-insert into rhnActionType values (1, 'packages.refresh_list', 'Package List Refresh', 'Y', 'N');
-insert into rhnActionType values (2, 'hardware.refresh_list', 'Hardware List Refresh', 'N', 'N');
-insert into rhnActionType values (3, 'packages.update', 'Package Install', 'Y', 'Y');
-insert into rhnActionType values (4, 'packages.remove', 'Package Removal', 'Y', 'Y');
-insert into rhnActionType values (5, 'errata.update', 'Patch Update', 'Y', 'Y');
-insert into rhnActionType values (6, 'up2date_config.get', 'Get server up2date config', 'Y', 'Y');
-insert into rhnActionType values (7, 'up2date_config.update', 'Update server up2date config', 'Y', 'Y');
-insert into rhnActionType values (8, 'packages.delta', 'Package installation and removal in one RPM transaction', 'Y', 'Y');
-insert into rhnActionType values (9, 'reboot.reboot', 'System reboot', 'N', 'Y');
-insert into rhnActionType values (10, 'rollback.config', 'Enable or Disable RPM Transaction Rollback', 'N', 'Y');
-insert into rhnActionType values (11, 'rollback.listTransactions', 'Refresh server-side transaction list', 'N', 'N');
-insert into rhnActionType values (12, 'rollback.rollback', 'RPM Transaction Rollback', 'Y', 'Y');
-insert into rhnActionType values (13, 'packages.autoupdate', 'Automatic package installation', 'Y', 'Y');
-insert into rhnActionType values (14, 'packages.runTransaction', 'Package Synchronization', 'Y', 'Y');
-insert into rhnActionType values (15, 'configfiles.upload', 'Upload config file data to server', 'N', 'N');
-insert into rhnActionType values (16, 'configfiles.deploy', 'Deploy config files to system', 'Y', 'Y');
-insert into rhnActionType values (17, 'configfiles.verify', 'Verify deployed config files', 'N', 'N');
-insert into rhnActionType values (18, 'configfiles.diff', 'Show differences between profiled config files and deployed config files', 'N', 'N');
-insert into rhnActionType values (19, 'kickstart.initiate', 'Initiate an auto installation', 'N', 'Y');
-insert into rhnActionType values (20, 'kickstart.schedule_sync', 'Schedule a package sync for auto installations', 'N', 'N');
-insert into rhnActionType values (21, 'activation.schedule_pkg_install', 'Schedule a package install for activation key', 'N', 'N');
-insert into rhnActionType values (22, 'activation.schedule_deploy', 'Schedule a config deploy for activation key', 'N', 'N');
-insert into rhnActionType values (23, 'configfiles.mtime_upload', 'Upload config file data based upon mtime to server', 'N', 'N');
-insert into rhnActionType values (24, 'solarispkgs.install', 'Solaris Package Install', 'Y', 'Y');
-insert into rhnActionType values (25, 'solarispkgs.remove', 'Solaris Package Removal', 'Y', 'Y');
-insert into rhnActionType values (26, 'solarispkgs.patchInstall', 'Solaris Patch Install', 'Y', 'Y');
-insert into rhnActionType values (27, 'solarispkgs.patchRemove', 'Solaris Patch Removal', 'Y', 'Y');
-insert into rhnActionType values (28, 'solarispkgs.patchClusterInstall', 'Solaris Patch Cluster Install', 'Y', 'Y');
-insert into rhnActionType values (29, 'solarispkgs.patchClusterRemove', 'Solaris Patch Cluster Removal', 'Y', 'Y');
-insert into rhnActionType values (30, 'script.run', 'Run an arbitrary script', 'N', 'N');
-insert into rhnActionType values (31, 'solarispkgs.refresh_list', 'Solaris Package List Refresh','Y','Y');
-insert into rhnActionType values (32, 'rhnsd.configure', 'SUSE Manager Network Daemon Configuration','N','N');
-insert into rhnActionType values (33, 'packages.verify', 'Verify deployed packages','N','N');
-insert into rhnActionType values (34, 'rhn_applet.use_satellite', 'Allows for rhn-applet use with an Spacewalk','N','N');
-insert into rhnActionType values (35, 'kickstart_guest.initiate', 'Initiate an auto installation for a virtual guest.','N','Y');
-insert into rhnActionType values (36, 'virt.shutdown', 'Shuts down a virtual domain.', 'N', 'N');
-insert into rhnActionType values (37, 'virt.start', 'Starts up a virtual domain.', 'N', 'N');
-insert into rhnActionType values (38, 'virt.suspend', 'Suspends a virtual domain.', 'N', 'N');
-insert into rhnActionType values (39, 'virt.resume', 'Resumes a virtual domain.', 'N', 'N');
-insert into rhnActionType values (40, 'virt.reboot', 'Reboots a virtual domain.', 'N', 'N');
-insert into rhnActionType values (41, 'virt.destroy', 'Destroys a virtual domain.', 'N', 'N');
-insert into rhnActionType values (42, 'virt.setMemory', 'Sets the maximum memory usage for a virtual domain.', 'N', 'N');
-insert into rhnActionType values (43, 'virt.schedulePoller', 'Sets when the poller should run.', 'N', 'N');
-insert into rhnActionType values (44, 'kickstart_host.schedule_virt_host_pkg_install', 'Schedule a package install of host specific functionality.', 'N', 'N');
-insert into rhnActionType values (45, 'kickstart_guest.schedule_virt_guest_pkg_install', 'Schedule a package install of guest specific functionality.', 'N', 'N');
-insert into rhnActionType values (46, 'kickstart_host.add_tools_channel', 'Subscribes a server to the Spacewalk Tools channel associated with its base channel.', 'N', 'N');
-insert into rhnActionType values (47, 'kickstart_guest.add_tools_channel', 'Subscribes a virtualization guest to the Spacewalk Tools channel associated with its base channel.', 'N', 'N');
-insert into rhnActionType values (48, 'virt.setVCPUs', 'Sets the Vcpu usage for a virtual domain.', 'N', 'N');
+-- last three boolean values are for TRIGGER_SNAPSHOT, UNLOCKED_ONLY, MAINTENANCE_MODE_ONLY
+insert into rhnActionType values (1, 'packages.refresh_list', 'Package List Refresh', 'Y', 'N', 'N');
+insert into rhnActionType values (2, 'hardware.refresh_list', 'Hardware List Refresh', 'N', 'N', 'N');
+insert into rhnActionType values (3, 'packages.update', 'Package Install', 'Y', 'Y', 'Y');
+insert into rhnActionType values (4, 'packages.remove', 'Package Removal', 'Y', 'Y', 'Y');
+insert into rhnActionType values (5, 'errata.update', 'Patch Update', 'Y', 'Y', 'Y');
+insert into rhnActionType values (6, 'up2date_config.get', 'Get server up2date config', 'Y', 'Y', 'Y');
+insert into rhnActionType values (7, 'up2date_config.update', 'Update server up2date config', 'Y', 'Y', 'Y');
+insert into rhnActionType values (8, 'packages.delta', 'Package installation and removal in one RPM transaction', 'Y', 'Y', 'N');
+insert into rhnActionType values (9, 'reboot.reboot', 'System reboot', 'N', 'Y', 'Y');
+insert into rhnActionType values (10, 'rollback.config', 'Enable or Disable RPM Transaction Rollback', 'N', 'Y', 'Y');
+insert into rhnActionType values (11, 'rollback.listTransactions', 'Refresh server-side transaction list', 'N', 'N', 'N');
+insert into rhnActionType values (12, 'rollback.rollback', 'RPM Transaction Rollback', 'Y', 'Y', 'Y');
+insert into rhnActionType values (13, 'packages.autoupdate', 'Automatic package installation', 'Y', 'Y', 'Y');
+insert into rhnActionType values (14, 'packages.runTransaction', 'Package Synchronization', 'Y', 'Y', 'Y');
+insert into rhnActionType values (15, 'configfiles.upload', 'Upload config file data to server', 'N', 'N', 'N');
+insert into rhnActionType values (16, 'configfiles.deploy', 'Deploy config files to system', 'Y', 'Y', 'Y');
+insert into rhnActionType values (17, 'configfiles.verify', 'Verify deployed config files', 'N', 'N', 'N');
+insert into rhnActionType values (18, 'configfiles.diff', 'Show differences between profiled config files and deployed config files', 'N', 'N', 'N');
+insert into rhnActionType values (19, 'kickstart.initiate', 'Initiate an auto installation', 'N', 'Y', 'Y');
+insert into rhnActionType values (20, 'kickstart.schedule_sync', 'Schedule a package sync for auto installations', 'N', 'N', 'N');
+insert into rhnActionType values (21, 'activation.schedule_pkg_install', 'Schedule a package install for activation key', 'N', 'N', 'N');
+insert into rhnActionType values (22, 'activation.schedule_deploy', 'Schedule a config deploy for activation key', 'N', 'N', 'N');
+insert into rhnActionType values (23, 'configfiles.mtime_upload', 'Upload config file data based upon mtime to server', 'N', 'N', 'N');
+insert into rhnActionType values (24, 'solarispkgs.install', 'Solaris Package Install', 'Y', 'Y', 'Y');
+insert into rhnActionType values (25, 'solarispkgs.remove', 'Solaris Package Removal', 'Y', 'Y', 'Y');
+insert into rhnActionType values (26, 'solarispkgs.patchInstall', 'Solaris Patch Install', 'Y', 'Y', 'Y');
+insert into rhnActionType values (27, 'solarispkgs.patchRemove', 'Solaris Patch Removal', 'Y', 'Y', 'Y');
+insert into rhnActionType values (28, 'solarispkgs.patchClusterInstall', 'Solaris Patch Cluster Install', 'Y', 'Y', 'Y');
+insert into rhnActionType values (29, 'solarispkgs.patchClusterRemove', 'Solaris Patch Cluster Removal', 'Y', 'Y', 'Y');
+insert into rhnActionType values (30, 'script.run', 'Run an arbitrary script', 'N', 'N', 'Y');
+insert into rhnActionType values (31, 'solarispkgs.refresh_list', 'Solaris Package List Refresh','Y','Y', 'Y');
+insert into rhnActionType values (32, 'rhnsd.configure', 'SUSE Manager Network Daemon Configuration','N','N', 'N');
+insert into rhnActionType values (33, 'packages.verify', 'Verify deployed packages','N','N', 'N');
+insert into rhnActionType values (34, 'rhn_applet.use_satellite', 'Allows for rhn-applet use with an Spacewalk','N','N', 'N');
+insert into rhnActionType values (35, 'kickstart_guest.initiate', 'Initiate an auto installation for a virtual guest.','N','Y', 'N');
+insert into rhnActionType values (36, 'virt.shutdown', 'Shuts down a virtual domain.', 'N', 'N', 'N');
+insert into rhnActionType values (37, 'virt.start', 'Starts up a virtual domain.', 'N', 'N', 'N');
+insert into rhnActionType values (38, 'virt.suspend', 'Suspends a virtual domain.', 'N', 'N', 'N');
+insert into rhnActionType values (39, 'virt.resume', 'Resumes a virtual domain.', 'N', 'N', 'N');
+insert into rhnActionType values (40, 'virt.reboot', 'Reboots a virtual domain.', 'N', 'N', 'N');
+insert into rhnActionType values (41, 'virt.destroy', 'Destroys a virtual domain.', 'N', 'N', 'N');
+insert into rhnActionType values (42, 'virt.setMemory', 'Sets the maximum memory usage for a virtual domain.', 'N', 'N', 'N');
+insert into rhnActionType values (43, 'virt.schedulePoller', 'Sets when the poller should run.', 'N', 'N', 'N');
+insert into rhnActionType values (44, 'kickstart_host.schedule_virt_host_pkg_install', 'Schedule a package install of host specific functionality.', 'N', 'N', 'N');
+insert into rhnActionType values (45, 'kickstart_guest.schedule_virt_guest_pkg_install', 'Schedule a package install of guest specific functionality.', 'N', 'N', 'N');
+insert into rhnActionType values (46, 'kickstart_host.add_tools_channel', 'Subscribes a server to the Spacewalk Tools channel associated with its base channel.', 'N', 'N', 'N');
+insert into rhnActionType values (47, 'kickstart_guest.add_tools_channel', 'Subscribes a virtualization guest to the Spacewalk Tools channel associated with its base channel.', 'N', 'N', 'N');
+insert into rhnActionType values (48, 'virt.setVCPUs', 'Sets the Vcpu usage for a virtual domain.', 'N', 'N', 'N');
+insert into rhnActionType values (49, 'proxy.deactivate', 'Deactivate Proxy', 'N', 'N', 'N');
+insert into rhnActionType values (50, 'scap.xccdf_eval', 'OpenSCAP xccdf scanning', 'N', 'Y', 'N');
+insert into rhnActionType values (51, 'clientcert.update_client_cert', 'Update Client Certificate', 'N', 'Y', 'Y');
+insert into rhnActionType values (500, 'image.deploy', 'Deploy an image to a virtual host.', 'N', 'N', 'N');
+insert into rhnActionType values (501, 'distupgrade.upgrade', 'Service Pack Migration', 'N', 'N', 'Y');
+insert into rhnActionType values (502, 'packages.setLocks', 'Lock packages', 'N', 'N', 'N');
+insert into rhnActionType values (503, 'states.apply', 'Apply states', 'N', 'N', 'Y');
+insert into rhnActionType values (504, 'image.build', 'Build an Image Profile', 'N', 'N', 'N');
+insert into rhnActionType values (505, 'image.inspect', 'Inspect an Image', 'N', 'N', 'N');
+insert into rhnActionType values (506, 'channels.subscribe', 'Subscribe to channels', 'N', 'N', 'N');
+insert into rhnActionType values (507, 'virt.delete', 'Deletes a virtual domain.', 'N', 'N', 'N');
+insert into rhnActionType values (508, 'virt.create', 'Creates a virtual domain.', 'N', 'N', 'N');
+insert into rhnActionType values (509, 'virt.pool_refresh', 'Refresh a virtual storage pool', 'N', 'N', 'N');
+insert into rhnActionType values (510, 'virt.pool_start', 'Starts a virtual storage pool', 'N', 'N', 'N');
+insert into rhnActionType values (511, 'virt.pool_stop', 'Stops a virtual storage pool', 'N', 'N', 'N');
+insert into rhnActionType values (512, 'virt.pool_delete', 'Deletes a virtual storage pool', 'N', 'N', 'N');
+insert into rhnActionType values (513, 'virt.pool_create', 'Creates a virtual storage pool', 'N', 'N', 'N');
+insert into rhnActionType values (514, 'virt.volume_delete', 'Deletes a virtual storage volume', 'N', 'N', 'N');
+insert into rhnActionType values (515, 'cluster.group_refresh_nodes', 'Refresh cluster group nodes', 'N', 'N', 'Y');
+insert into rhnActionType values (516, 'cluster.join_node', 'Join node to cluster', 'N', 'N', 'Y');
+insert into rhnActionType values (517, 'cluster.remove_node', 'Remove node from cluster', 'N', 'N', 'Y');
+insert into rhnActionType values (518, 'cluster.upgrade_cluster', 'Upgrade cluster', 'N', 'N', 'Y');
 commit;
-insert into rhnActionType values (49, 'proxy.deactivate', 'Deactivate Proxy', 'N', 'N');
-insert into rhnActionType values (50, 'scap.xccdf_eval', 'OpenSCAP xccdf scanning', 'N', 'Y');
-insert into rhnActionType values (51, 'clientcert.update_client_cert', 'Update Client Certificate', 'N', 'Y');
-insert into rhnActionType values (500, 'image.deploy', 'Deploy an image to a virtual host.', 'N', 'N');
-insert into rhnActionType values (501, 'distupgrade.upgrade', 'Service Pack Migration', 'N', 'N');
-insert into rhnActionType values (502, 'packages.setLocks', 'Lock packages', 'N', 'N');
-insert into rhnActionType values (503, 'states.apply', 'Apply states', 'N', 'N');
-insert into rhnActionType values (504, 'image.build', 'Build an Image Profile', 'N', 'N');
-insert into rhnActionType values (505, 'image.inspect', 'Inspect an Image', 'N', 'N');
-insert into rhnActionType values (506, 'channels.subscribe', 'Subscribe to channels', 'N', 'N');
-insert into rhnActionType values (507, 'virt.delete', 'Deletes a virtual domain.', 'N', 'N');
-insert into rhnActionType values (508, 'virt.create', 'Creates a virtual domain.', 'N', 'N');
-insert into rhnActionType values (509, 'virt.pool_refresh', 'Refresh a virtual storage pool', 'N', 'N');
-insert into rhnActionType values (510, 'virt.pool_start', 'Starts a virtual storage pool', 'N', 'N');
-insert into rhnActionType values (511, 'virt.pool_stop', 'Stops a virtual storage pool', 'N', 'N');
-insert into rhnActionType values (512, 'virt.pool_delete', 'Deletes a virtual storage pool', 'N', 'N');
-insert into rhnActionType values (513, 'virt.pool_create', 'Creates a virtual storage pool', 'N', 'N');
-insert into rhnActionType values (514, 'virt.volume_delete', 'Deletes a virtual storage volume', 'N', 'N');
-insert into rhnActionType values (515, 'cluster.group_refresh_nodes', 'Refresh cluster group nodes', 'N', 'N');
-insert into rhnActionType values (516, 'cluster.join_node', 'Join node to cluster', 'N', 'N');
-insert into rhnActionType values (517, 'cluster.remove_node', 'Remove node from cluster', 'N', 'N');
-insert into rhnActionType values (518, 'cluster.upgrade_cluster', 'Upgrade cluster', 'N', 'N');
---
---
--- Revision 1.25  2004/10/29 05:07:52  pjones
--- bugzilla: 136675 -- remove the new action type we created but aren't using
---
--- Revision 1.24  2004/09/23 16:38:02  pjones
--- bugzilla: 133354 -- fix Robin's engrish.
---
--- Revision 1.23  2004/09/23 14:20:08  pjones
--- bugzilla: 133354 -- add action type to support proxy activation
---
--- Revision 1.22  2004/04/13 16:40:31  bretm
--- bugzilla:  119871
---
--- action type for the applet+satellite scheduled action
---
--- Revision 1.21  2004/04/13 16:26:16  pjones
--- bugzilla: 101315 -- add action type entries for package verify
---
--- Revision 1.20  2004/03/16 19:51:43  misa
--- New action type rhnsd.configure
---
--- Revision 1.19  2004/03/03 16:52:47  pjones
--- bugzilla: none -- something went wrong tagging this, starting over.
---
--- Revision 1.18.2.1  2004/03/03 15:43:13  pjones
--- bugzilla: 117023 -- add solarispkgs.refresh_list
---
--- Revision 1.18  2004/02/17 00:22:59  pjones
--- bugzilla: 115898 -- add the action type for arbitrary scripts
