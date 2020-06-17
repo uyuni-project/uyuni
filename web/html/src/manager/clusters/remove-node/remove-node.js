949c48e1668619ed5155380cb84ae1850e5dd6bf
Frantisek Kobzik <fkobzik@suse.de>
2020-06-17 14:21:07 +0200

    Make cluster UI maintenance-windows aware

--- web/html/src/manager/clusters/remove-node/remove-node.js
+++ web/html/src/manager/clusters/remove-node/remove-node.js
@@ -76,6 +76,7 @@ const RemoveNode = (props: Props) => {
                                         }
                                         schedule={scheduleRemove}
                                         scheduleButtonLabel={t("Remove")}
+                                        actionType="cluster.remove_node"
                                         onPrev={back}                                
                                     /> : goTo()
                             }
