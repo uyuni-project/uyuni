836a4f842fd3b760a17a484ecc16f88442c18cfd
Cédric Bosdonnat <cbosdonnat@suse.com>
2020-06-22 10:44:00 +0200

    virt pools: trigger refresh only when getting a websocket event
    To avoid polling the DB and Salt minion every 5s while the page is
    opened, move to an event based refresh.

--- web/html/src/manager/virtualization/pools/list/pools-list.js
+++ web/html/src/manager/virtualization/pools/list/pools-list.js
@@ -13,7 +13,7 @@ import { Utils as MessagesUtils } from 'components/messages';
 import { ActionStatus } from 'components/action/ActionStatus';
 import { ModalButton } from 'components/dialog/ModalButton';
 import { ActionConfirm } from 'components/dialog/ActionConfirm';
-import { VirtualizationPoolsListRefreshApi } from '../virtualization-pools-list-refresh-api';
+import { VirtualizationListRefreshApi } from '../../virtualization-list-refresh-api';
 import { VirtualizationPoolsActionApi } from '../virtualization-pools-action-api';
 import { useVirtNotification } from '../../useVirtNotification.js';
 import { HypervisorCheck } from '../../HypervisorCheck';
@@ -22,7 +22,6 @@ import type {MessageType} from 'components/messages';
 
 type Props = {
   serverId: string,
-  refreshInterval: number,
   pageSize: number,
   hypervisor: string,
 };
@@ -149,8 +148,15 @@ const DeleteActionConfirm = (props) => {
 export function PoolsList(props: Props) {
   const [selected, setSelected] = React.useState({});
   const [errors, setErrors] = React.useState([]);
+  const [lastRefresh, setLastRefresh] = React.useState(Date.now());
 
-  const [actionsResults, setActionsResults] = useVirtNotification(errors, setErrors, props.serverId, () => {});
+  const refresh = (type: string) => {
+    if (type === "pool") {
+      setLastRefresh(Date.now());
+    }
+  }
+
+  const [actionsResults, setActionsResults] = useVirtNotification(errors, setErrors, props.serverId, refresh);
 
   const actionCallback = (results: Object) => {
     const newActions = Object.keys(results).reduce((actions, poolName) => {
@@ -190,14 +196,15 @@ export function PoolsList(props: Props) {
         messages
       }) => {
         return (
-          <VirtualizationPoolsListRefreshApi
+          <VirtualizationListRefreshApi
             serverId={props.serverId}
-            refreshInterval={props.refreshInterval}
+            lastRefresh={lastRefresh}
+            type="pools"
           >
           {
             ({
-              pools,
-              errors: refreshError,
+              data: pools,
+              error: refreshError,
             }) => {
               function renderPool(pool: Object, renderNameColumn: Function): React.Node {
                 return [
@@ -390,7 +397,7 @@ export function PoolsList(props: Props) {
               );
             }
           }
-          </VirtualizationPoolsListRefreshApi>
+          </VirtualizationListRefreshApi>
         );
       }
     }
