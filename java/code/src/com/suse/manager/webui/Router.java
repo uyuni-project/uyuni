f5c73d14cde6f934c9e037646b3d0d06e4b5963a
Frantisek Kobzik <fkobzik@suse.de>
2020-06-23 14:38:38 +0200

    Unify the Maintenance windows controllers

--- java/code/src/com/suse/manager/webui/Router.java
+++ java/code/src/com/suse/manager/webui/Router.java
@@ -40,7 +40,6 @@ import com.suse.manager.webui.controllers.ImageBuildController;
 import com.suse.manager.webui.controllers.ImageProfileController;
 import com.suse.manager.webui.controllers.ImageStoreController;
 import com.suse.manager.webui.controllers.MaintenanceController;
-import com.suse.manager.webui.controllers.MaintenanceWindowController;
 import com.suse.manager.webui.controllers.MinionController;
 import com.suse.manager.webui.controllers.MinionsAPI;
 import com.suse.manager.webui.controllers.NotificationMessageController;
@@ -198,9 +197,6 @@ public class Router implements SparkApplication {
 
         // Maintenance windows
         MaintenanceController.initRoutes(jade);
-
-        // Maintenance Window
-        MaintenanceWindowController.initRoutes();
     }
 
     private void  initNotFoundRoutes(JadeTemplateEngine jade) {
