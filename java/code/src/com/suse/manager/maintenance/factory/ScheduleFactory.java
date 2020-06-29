5ca4d08fbaccdae64fbb6cf86f027ec4d29d4ffc
Frantisek Kobzik <fkobzik@suse.de>
2020-06-29 15:32:57 +0200

    Refactor: Split the MaintenanceFactory to ScheduleFactory and CalendarFactory

--- java/code/src/com/suse/manager/maintenance/factory/ScheduleFactory.java
+++ java/code/src/com/suse/manager/maintenance/factory/ScheduleFactory.java
@@ -13,7 +13,7 @@
  * in this software or its documentation.
  */
 
-package com.suse.manager.maintenance;
+package com.suse.manager.maintenance.factory;
 
 import static java.util.Collections.emptySet;
 import static java.util.stream.Collectors.toSet;
@@ -33,17 +33,9 @@ import java.util.Set;
 /**
  * {@link HibernateFactory} for Maintenance Windows-related objects.
  */
-public class MaintenanceFactory extends HibernateFactory {
+public class ScheduleFactory extends HibernateFactory {
 
-    private static Logger log = Logger.getLogger(MaintenanceFactory.class);
-
-    /**
-     * Save a MaintenanceCalendar
-     * @param calendar the calendar
-     */
-    public void save(MaintenanceCalendar calendar) {
-        getSession().save(calendar);
-    }
+    private static Logger log = Logger.getLogger(ScheduleFactory.class);
 
     /**
      * Save a MaintenanceSchedule
@@ -62,68 +54,6 @@ public class MaintenanceFactory extends HibernateFactory {
     }
 
     /**
-     * Remove a {@link MaintenanceCalendar}
-     * @param calendar to remove
-     */
-    public void remove(MaintenanceCalendar calendar) {
-        removeObject(calendar);
-    }
-
-    @Override
-    protected Logger getLogger() {
-        return log;
-    }
-
-    /**
-     * List Maintenance Calendar Labels belonging to the given User
-     * @param user the user
-     * @return a list of Calendar labels
-     */
-    public List<String> listCalendarLabelsByUser(User user) {
-        return getSession()
-                .createQuery("SELECT label FROM MaintenanceCalendar WHERE org = :org ORDER BY label ASC")
-                .setParameter("org", user.getOrg())
-                .list();
-    }
-
-    /**
-     * List Maintenance Calendars belonging to the given User
-     * @param user the user
-     * @return a list of Maintenance Calendars
-     */
-    public List<MaintenanceCalendar> listCalendarsByUser(User user) {
-        return getSession()
-                .createQuery("FROM MaintenanceCalendar WHERE org = :org")
-                .setParameter("org", user.getOrg())
-                .list();
-    }
-
-    /**
-     * Lookup Maintenance Calendar by User and Label
-     * @param user the user
-     * @param label the label of the calendar
-     * @return Optional Maintenance Calendar
-     */
-    public Optional<MaintenanceCalendar> lookupCalendarByUserAndLabel(User user, String label) {
-        return getSession().createNamedQuery("MaintenanceCalendar.lookupByUserAndName")
-                .setParameter("orgId", user.getOrg().getId())
-                .setParameter("label", label).uniqueResultOptional();
-    }
-
-    /**
-     * Lookup Maintenance Calendar by User and id
-     * @param user the user
-     * @param id the id of the calendar
-     * @return Optional Maintenance Calendar
-     */
-    //todo merge with ^?
-    public Optional<MaintenanceCalendar> lookupCalendarByUserAndId(User user, Long id) {
-        return getSession().createQuery("FROM MaintenanceCalendar WHERE org = :org AND id = :id")
-                .setParameter("org", user.getOrg())
-                .setParameter("id", id).uniqueResultOptional();
-    }
-
-    /**
      * List schedules by User and Calendar
      * @param user the User
      * @param calendar the Calendar
@@ -131,7 +61,9 @@ public class MaintenanceFactory extends HibernateFactory {
      */
     public List<MaintenanceSchedule> listSchedulesByUserAndCalendar(User user, MaintenanceCalendar calendar) {
         return getSession()
-                .createQuery("from MaintenanceSchedule WHERE org = :org and calendar = :calendar ORDER BY name ASC")
+                .createQuery("FROM MaintenanceSchedule " +
+                        "WHERE org = :org and calendar = :calendar " +
+                        "ORDER BY name ASC")
                 .setParameter("org", user.getOrg())
                 .setParameter("calendar", calendar).getResultList();
     }
@@ -196,7 +128,8 @@ public class MaintenanceFactory extends HibernateFactory {
     public Optional<MaintenanceSchedule> lookupScheduleByUserAndId(User user, Long id) {
         return getSession().createQuery("FROM MaintenanceSchedule WHERE org = :org AND id = :id")
                 .setParameter("org", user.getOrg())
-                .setParameter("id", id).uniqueResultOptional();
+                .setParameter("id", id)
+                .uniqueResultOptional();
     }
 
     /**
@@ -233,5 +166,10 @@ public class MaintenanceFactory extends HibernateFactory {
                 .setParameter("systemIds", systemIds)
                 .stream()
                 .collect(toSet());
+    }
+
+    @Override
+    protected Logger getLogger() {
+        return log;
     }
 }
