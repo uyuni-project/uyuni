c36c43ca19088f5219d8ff3a6a408a06dea1859e
Pascal Arlt <parlt@suse.com>
2020-07-01 17:39:43 +0200

    Split details component into schedule and calendar details
    Signed-off-by: Pascal Arlt <parlt@suse.com>

--- web/html/src/manager/maintenance/maintenance-windows-details.js
+++ web/html/src/manager/maintenance/maintenance-windows-details.js
@@ -4,14 +4,11 @@
 const React = require("react");
 const {Button} = require("components/buttons");
 const {ModalButton} = require("components/dialog/ModalButton");
-const {DeleteDialog} = require("components/dialog/DeleteDialog");
 const {TopPanel} = require('components/panels/TopPanel');
 const {Messages} = require("components/messages");
-const {Form} = require("components/input/Form");
-const {Check} = require("components/input/Check");
-const {BootstrapPanel} = require('components/panels/BootstrapPanel');
-const {Table} = require("components/table/Table");
-const {Column} = require("components/table/Column");
+
+import MaintenanceScheduleDetails from "./schedule-details";
+import MaintenanceCalendarDetails from "./calendar-details";
 
 class MaintenanceWindowsDetails extends React.Component {
 
@@ -20,133 +17,8 @@ class MaintenanceWindowsDetails extends React.Component {
 
         this.state = {
             type: props.type,
-            calendarData: this.props.data.calendarData,
             messages: [],
         };
-    }
-
-    setCheck = (model) => {
-        /* strategy gets initialized as empty string, but we want the initial value to be false.
-        * Is equivalent to: if strategy is "" then set it to false */
-        model.strategy === "" && (model.strategy = false);
-        this.setState(model);
-    };
-
-    addStrategy = () => {
-        const item = this.props.data;
-        item.strategy = this.state.strategy ? "Cancel" : "Fail";
-        return item;
-    };
-
-    renderScheduleDetails(data) {
-        const tableData = [
-            {left: t("Schedule Name:"), right: data.scheduleName},
-            {left: t("Assigned Calendar:"), right: data.calendarName},
-            {left: t("Schedule Type:"), right: data.scheduleType === "SINGLE" ? t("Single") : t("Multi")},
-        ];
-
-        return (
-            <div>
-                <BootstrapPanel title={t("Schedule Details")}>
-                    <Table
-                        data={tableData}
-                        identifier={row => tableData.indexOf(row)}
-                        initialItemsPerPage={0}
-                    >
-                        <Column
-                            columnKey="left"
-                            cell={(row) => row.left}
-                        />
-                        <Column
-                            columnKey="right"
-                            cell={(row) => row.right}
-                        />
-                    </Table>
-                </BootstrapPanel>
-                {
-                    data.maintenanceWindows !== undefined && data.maintenanceWindows.length > 0 &&
-                    <BootstrapPanel title={t("Upcoming Maintenance Windows")}>
-                        <Table
-                            data={data.maintenanceWindows}
-                            identifier={row => data.maintenanceWindows.indexOf(row)}
-                            initialItemsPerPage={0}
-                        >
-                            <Column
-                                header={t("Start")}
-                                columnKey="start"
-                                cell={(row) => row.start}
-                            />
-                            <Column
-                                header={t("End")}
-                                columnKey="end"
-                                cell={(row) => row.end}
-                            />
-                        </Table>
-                    </BootstrapPanel>
-                }
-                <DeleteDialog id="delete-modal"
-                              title={t("Delete maintenance schedule")}
-                              content={t("Are you sure you want to delete the selected item? \n" +
-                                  "This will remove the current schedule from all the systems assigned to it.")}
-                              onConfirm={() => this.props.onDelete(this.props.data)}
-                />
-            </div>
-        );
-    }
-
-    renderCalendarDetails(data) {
-        const tableData = [
-            {left: t("Calendar Name:"), right: data.calendarName},
-            {left: t("Used by Schedule:"), right: data.scheduleNames.map(name => name.name).join(", ")},
-        ];
-        data.calendarUrl && tableData.push({left: "Url:", right: data.calendarUrl});
-
-        return (
-            <div>
-                <BootstrapPanel title={t("Calendar Details")}>
-                    <Table
-                        data={tableData}
-                        identifier={row => tableData.indexOf(row)}
-                        initialItemsPerPage={0}
-                    >
-                        <Column
-                            columnKey="left"
-                            cell={(row) => row.left}
-                        />
-                        <Column
-                            columnKey="right"
-                            cell={(row) => row.right}
-                        />
-                    </Table>
-                </BootstrapPanel>
-                {
-                    this.props.data.calendarData &&
-                    <div className="panel panel-default">
-                        <div className="panel-heading">
-                            <h4>
-                                {this.props.data.calendarName}
-                            </h4>
-                        </div>
-                        <div className="panel-body">
-                        <pre>
-                            {this.props.data.calendarData}
-                        </pre>
-                        </div>
-                    </div>
-                }
-                <DeleteDialog id="delete-modal"
-                              title={t("Delete maintenance calendar")}
-                              content={
-                                  <Form model={{strategy: this.state.strategy}} onChange={this.setCheck}>
-                                      <div>{t("Are you sure you want to delete the selected item?")}</div>
-                                      <div>{t("This will remove the current schedule from all the systems assigned to it.")}</div>
-                                      <Check name="strategy" label={<b>{t("Cancel affected actions?")}</b>} divClass="col-md-6" />
-                                  </Form>
-                              }
-                              onConfirm={() => this.props.onDelete(this.addStrategy())}
-                />
-            </div>
-        );
     }
 
     render() {
@@ -192,15 +64,28 @@ class MaintenanceWindowsDetails extends React.Component {
             >
                 <Messages items={this.state.messages}/>
                 {
-                    this.state.type === "schedule"
-                        ? this.renderScheduleDetails(this.props.data)
-                        : this.renderCalendarDetails(this.props.data)
+                    this.state.type === "schedule" &&
+                    <MaintenanceScheduleDetails
+                        scheduleName={this.props.data.scheduleName}
+                        calendarName={this.props.data.calendarName}
+                        scheduleType={this.props.data.scheduleType}
+                        maintenanceWindows={this.props.data.maintenanceWindows}
+                        onDelete={this.props.onDelete}
+                    /> ||
+                    this.state.type === "calendar" &&
+                    <MaintenanceCalendarDetails
+                        calendarName={this.props.data.calendarName}
+                        scheduleNames={this.props.data.scheduleNames}
+                        calendarUrl={this.props.data.calendarUrl}
+                        calendarData={this.props.data.calendarData}
+                        onDelete={this.props.onDelete}
+                    />
                 }
             </TopPanel>
         );
     }
 }
 
-module.exports = {
-    MaintenanceWindowsDetails: MaintenanceWindowsDetails
+export {
+    MaintenanceWindowsDetails
 };
---
+++ web/html/src/manager/maintenance/schedule-details.js
@@ -0,0 +1,72 @@
+/* eslint-disable */
+'use strict';
+
+import React from "react";
+import {BootstrapPanel} from "components/panels/BootstrapPanel";
+import {Table} from "components/table/Table";
+import {Column} from "components/table/Column";
+import {DeleteDialog} from "components/dialog/DeleteDialog";
+
+const MaintenanceScheduleDetails = (props) => {
+    return (
+        <>
+            <DeleteDialog
+                id="delete-modal"
+                title={t("Delete maintenance schedule")}
+                content={t("Are you sure you want to delete the selected item?\n" +
+                    "This will remove the current schedule from all the systems assigned to it.")}
+                onConfirm={() => props.onDelete({scheduleName: props.scheduleName})}
+            />
+            <MaintenanceScheduleOverview
+                scheduleName={props.scheduleName}
+                calendarName={props.calendarName}
+                scheduleType={props.scheduleType}
+                maintenanceWindows={props.maintenanceWindows}
+            />
+        </>
+    );
+}
+
+type OverviewProps = {
+    scheduleName: string,
+    calendarName: string,
+    scheduleType: 'SINGLE' | 'MULTI',
+    maintenanceWindows?: Array
+}
+const MaintenanceScheduleOverview = (props) => {
+    const tableData = [
+        {left: t("Schedule name") + ":", right: props.scheduleName},
+        {left: t("Assigned calendar") + ":", right: props.calendarName},
+        {left: t("Schedule type") + ":", right: props.scheduleType === "SINGLE" ? t("Single") : t("Multi")}
+    ];
+
+    return (
+        <div>
+            <BootstrapPanel title={t("Schedule Details")}>
+                <Table
+                    data={tableData}
+                    identifier={row => tableData.indexOf(row)}
+                    initialItemsPerPage={0}
+                >
+                    <Column columnKey="left" cell={row => row.left}/>
+                    <Column columnKey="right" cell={row => row.right}/>
+                </Table>
+            </BootstrapPanel>
+            {
+                props.maintenanceWindows && props.maintenanceWindows.length > 0 &&
+                <BootstrapPanel title={t("Upcoming Maintenance Windows")}>
+                    <Table
+                        data={props.maintenanceWindows}
+                        identifier={row => props.maintenanceWindows.indexOf(row)}
+                        initialItemsPerPage={0}
+                    >
+                        <Column header={t("Start")} columnKey="start" cell={row => row.start}/>
+                        <Column header={t("End")} columnKey="end" cell={row => row.end}/>
+                    </Table>
+                </BootstrapPanel>
+            }
+        </div>
+    );
+}
+
+export default MaintenanceScheduleDetails;
