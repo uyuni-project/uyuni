4b6e735188f1a4185145df8eb8f78ad2278deb14
Pascal Arlt <parlt@suse.com>
2020-07-01 18:41:01 +0200

    Split list component into schedule and calendar list
    Signed-off-by: Pascal Arlt <parlt@suse.com>

--- web/html/src/manager/maintenance/maintenance-windows-list.js
+++ web/html/src/manager/maintenance/maintenance-windows-list.js
@@ -2,15 +2,11 @@
 'use strict';
 
 const React = require("react");
-const ReactDOM = require("react-dom");
 const { InnerPanel } = require('components/panels/InnerPanel');
 const Button = require("components/buttons").Button;
-const ModalButton = require("components/dialog/ModalButton").ModalButton;
-const DeleteDialog = require("components/dialog/DeleteDialog").DeleteDialog;
-const {Column} = require("components/table/Column");
-const {Form} = require("components/input/Form");
-const {Check} = require("components/input/Check");
-const {Table} = require("components/table/Table");
+
+import MaintenanceScheduleList from "./schedule-list";
+import MaintenanceCalendarList from "./calendar-list";
 
 class MaintenanceWindowsList extends React.Component {
 
@@ -19,165 +15,7 @@ class MaintenanceWindowsList extends React.Component {
 
         this.state = {
             type: props.type,
-            itemToDelete: {}
         };
-    }
-
-    selectToDelete(item) {
-        this.setState({
-            itemToDelete: item
-        });
-    }
-
-    setCheck = (model) => {
-        model.strategy === "" && (model.strategy = false);
-        this.setState(model);
-    };
-
-    addStrategy = () => {
-        const item = this.state.itemToDelete;
-        item.strategy = (this.state.strategy ? "Cancel" : "Fail");
-        return item;
-    };
-
-    renderScheduleTable() {
-        return <div>
-            <Table
-                data={this.props.data}
-                identifier={row => row.scheduleId}
-                initialItemsPerPage={userPrefPageSize}
-                emptyText={t("No schedules created. Use Create to add a schedule.")}
-            >
-                <Column
-                    columnKey="scheduleName"
-                    header={t('Schedule Name')}
-                    cell={(row) => row.scheduleName}
-                />
-                <Column
-                    columnClass="text-center"
-                    headerClass="text-center"
-                    columnKey="calendarName"
-                    header={t('Calendar')}
-                    cell={(row) =>
-                        row.calendarId &&
-                        <a className="link-tag"
-                           href={t("/rhn/manager/schedule/maintenance/calendars#/details/" + row.calendarId)}>
-                            {row.calendarName}
-                        </a>
-                    }
-                />
-                <Column
-                    columnClass="text-right"
-                    headerClass="text-right"
-                    header={t('Actions')}
-                    cell={(row) =>
-                        <div className="btn-group">
-                            <Button
-                                className="btn-default btn-sm"
-                                title={t("Details")}
-                                icon="fa-list"
-                                handler={() => {this.props.onSelect(row.scheduleId)}}
-                            />
-                            <Button
-                                className="btn-default btn-sm"
-                                disabled={!isAdmin}
-                                title={t("Edit")}
-                                icon="fa-edit"
-                                handler={() => {this.props.onEdit(row.scheduleId)}}
-                            />
-                            <ModalButton
-                                className="btn-default btn-sm"
-                                disabled={!isAdmin}
-                                title={t("Delete")}
-                                icon="fa-trash"
-                                target="delete-modal"
-                                item={row}
-                                onClick={i => this.selectToDelete(i)}
-                            />
-                        </div>
-                    }
-                />
-            </Table>
-            <DeleteDialog id="delete-modal"
-                          title={t("Delete maintenance schedule")}
-                          content={t("Are you sure you want to delete the selected item? \n" +
-                              "This will remove the schedule from all the systems assigned to it.")}
-                          onConfirm={() => this.props.onDelete(this.state.itemToDelete)}
-                          onClosePopUp={() => this.selectToDelete({})}
-            />
-        </div>
-    }
-
-    renderCalendarTable() {
-        return <div>
-            <Table
-                data={this.props.data}
-                identifier={row => row.calendarName}
-                initialItemsPerPage={userPrefPageSize}
-                emptyText={t("No calendars created. Use Create to add a calendar.")}
-            >
-                <Column
-                    columnKey="calendarName"
-                    header={t('Calendar Name')}
-                    cell={(row) => row.calendarName}
-                />
-                <Column
-                    columnClass="text-center"
-                    headerClass="text-center"
-                    columnKey="usedBySchedule"
-                    header={t('Used by Schedule')}
-                    cell={(row) => row.scheduleNames.map(name =>
-                        <a className="link-tag"
-                           href={"/rhn/manager/schedule/maintenance/schedules#/details/" + name.id}>
-                            {name.name}
-                        </a>
-                    )}
-                />
-                <Column
-                    columnClass="text-right"
-                    headerClass="text-right"
-                    header={t('Actions')}
-                    cell={(row) =>
-                        <div className="btn-group">
-                            <Button
-                                className="btn-default btn-sm"
-                                title={t("Details")}
-                                icon="fa-list"
-                                handler={() => {this.props.onSelect(row.calendarId)}}
-                            />
-                            <Button
-                                className="btn-default btn-sm"
-                                disabled={!isAdmin}
-                                title={t("Edit")}
-                                icon="fa-edit"
-                                handler={() => {this.props.onEdit(row.calendarId)}}
-                            />
-                            <ModalButton
-                                className="btn-default btn-sm"
-                                disabled={!isAdmin}
-                                title={t("Delete")}
-                                icon="fa-trash"
-                                target="delete-modal"
-                                item={row}
-                                onClick={i => this.selectToDelete(i)}
-                            />
-                        </div>
-                    }
-                />
-            </Table>
-            <DeleteDialog id="delete-modal"
-                          title={t("Delete maintenance calendar")}
-                          content={
-                              <Form model={{strategy: this.state.strategy}} onChange={this.setCheck}>
-                                  <div>{t("Are you sure you want to delete the selected item?")}</div>
-                                  <div>{t("This will unassign all schedules from this calendar.")}</div>
-                                  <Check name="strategy" label={<b>{t("Cancel affected actions?")}</b>} divClass="col-md-6" />
-                              </Form>
-                          }
-                          onConfirm={() => this.props.onDelete(this.addStrategy())}
-                          onClosePopUp={() => this.selectToDelete({})}
-            />
-        </div>
     }
 
     render() {
@@ -203,7 +41,22 @@ class MaintenanceWindowsList extends React.Component {
                                 <h3>{t(this.state.type === "schedule" ? "Schedules" : "Calendars")}</h3>
                             </div>
                         </div>
-                        {this.state.type === "schedule" ? this.renderScheduleTable() : this.renderCalendarTable()}
+                        {
+                            this.state.type === "schedule" &&
+                            <MaintenanceScheduleList
+                                data={this.props.data}
+                                onSelect={this.props.onSelect}
+                                onEdit={this.props.onEdit}
+                                onDelete={this.props.onDelete}
+                            /> ||
+                            this.state.type === "calendar" &&
+                            <MaintenanceCalendarList
+                                data={this.props.data}
+                                onSelect={this.props.onSelect}
+                                onEdit={this.props.onEdit}
+                                onDelete={this.props.onDelete}
+                            />
+                        }
                     </div>
                 </InnerPanel>
             </div>
@@ -211,6 +64,6 @@ class MaintenanceWindowsList extends React.Component {
     }
 }
 
-module.exports = {
-    MaintenanceWindowsList: MaintenanceWindowsList
+export {
+    MaintenanceWindowsList
 };
