38b4fcdb00ce477ed29c47674552a761b9d090d7
Cédric Bosdonnat <cbosdonnat@suse.com>
2020-07-01 17:16:28 +0200

    Extract generic action code from VirtualPoolsController
    Since network actions are likely to be similar to the pool ones, share
    the code used to handle the REST API for the actions.

--- java/code/src/com/suse/manager/webui/controllers/virtualization/AbstractVirtualizationController.java
+++ java/code/src/com/suse/manager/webui/controllers/virtualization/AbstractVirtualizationController.java
@@ -14,20 +14,45 @@
  */
 package com.suse.manager.webui.controllers.virtualization;
 
+import static com.suse.manager.webui.utils.SparkApplicationHelper.json;
+
 import com.redhat.rhn.common.hibernate.LookupException;
+import com.redhat.rhn.domain.action.Action;
+import com.redhat.rhn.domain.action.ActionChain;
+import com.redhat.rhn.domain.action.ActionChainFactory;
 import com.redhat.rhn.domain.server.Server;
 import com.redhat.rhn.domain.user.User;
 import com.redhat.rhn.manager.rhnset.RhnSetDecl;
 import com.redhat.rhn.manager.system.SystemManager;
+import com.redhat.rhn.manager.system.VirtualizationActionCommand;
+import com.redhat.rhn.taskomatic.TaskomaticApiException;
 
+import com.suse.manager.reactor.utils.LocalDateTimeISOAdapter;
+import com.suse.manager.reactor.utils.OptionalTypeAdapterFactory;
+import com.suse.manager.webui.controllers.ECMAScriptDateAdapter;
 import com.suse.manager.webui.errors.NotFoundException;
 import com.suse.manager.webui.services.iface.VirtManager;
+import com.suse.manager.webui.utils.MinionActionUtils;
+import com.suse.manager.webui.utils.gson.ScheduledRequestJson;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParseException;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpStatus;
+import org.apache.log4j.Logger;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import spark.ModelAndView;
 import spark.Request;
@@ -38,6 +63,14 @@ import spark.Spark;
  * Base controller class offering convenience functions for other virtualization-related controllers
  */
 public abstract class AbstractVirtualizationController {
+    private static final Logger LOG = Logger.getLogger(AbstractVirtualizationController.class);
+
+    private static final Gson GSON = new GsonBuilder()
+            .registerTypeAdapter(Date.class, new ECMAScriptDateAdapter())
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeISOAdapter())
+            .registerTypeAdapterFactory(new OptionalTypeAdapterFactory())
+            .serializeNulls()
+            .create();
 
     private final String jadeTemplatesPath;
     protected final VirtManager virtManager;
@@ -116,5 +149,63 @@ public abstract class AbstractVirtualizationController {
         /* For the rest of the template */
 
         return new ModelAndView(data, String.format("%s/%s.jade", jadeTemplatesPath, template));
+    }
+
+    protected String action(Request request, Response response, User user,
+                          BiFunction<ScheduledRequestJson, String, Action> actionCreator,
+                          Function<ScheduledRequestJson, List<String>> actionKeysGetter,
+                          Class<? extends ScheduledRequestJson> jsonClass) {
+        long serverId;
+
+        try {
+            serverId = Long.parseLong(request.params("sid"));
+        }
+        catch (NumberFormatException e) {
+            throw new NotFoundException();
+        }
+        Server host = SystemManager.lookupByIdAndUser(serverId, user);
+
+        ScheduledRequestJson data;
+        try {
+            data = GSON.fromJson(request.body(), jsonClass);
+        }
+        catch (JsonParseException e) {
+            throw Spark.halt(HttpStatus.SC_BAD_REQUEST);
+        }
+
+        List<String> actionKeys = actionKeysGetter.apply(data);
+        if (!actionKeys.isEmpty()) {
+            Map<String, String> actionsResults = actionKeys.stream().collect(
+                    Collectors.toMap(Function.identity(),
+                            key -> scheduleAction(key, user, host, actionCreator, data)
+                    ));
+            return json(response, actionsResults);
+        }
+
+        String result = scheduleAction(null, user, host, actionCreator, data);
+        return json(response, result);
+    }
+
+    protected String scheduleAction(String key, User user, Server host,
+                                  BiFunction<ScheduledRequestJson, String, Action> actionCreator,
+                                  ScheduledRequestJson data) {
+        Action action = actionCreator.apply(data, key);
+        action.setOrg(user.getOrg());
+        action.setSchedulerUser(user);
+        action.setEarliestAction(MinionActionUtils.getScheduleDate(data.getEarliest()));
+
+        Optional<ActionChain> actionChain = data.getActionChain()
+                .filter(StringUtils::isNotEmpty)
+                .map(label -> ActionChainFactory.getOrCreateActionChain(label, user));
+
+        String status = "Failed";
+        try {
+            VirtualizationActionCommand.schedule(action, host, actionChain);
+            status = action.getId().toString();
+        }
+        catch (TaskomaticApiException e) {
+            LOG.error("Could not schedule virtualization action:", e);
+        }
+        return status;
     }
 }
