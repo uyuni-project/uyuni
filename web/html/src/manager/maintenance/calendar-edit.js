221e7d135656ada52e982215a380a69952e22fa3
Pascal Arlt <parlt@suse.com>
2020-07-03 11:29:41 +0200

    Split edit component into schedule and calendar edit
    Signed-off-by: Pascal Arlt <parlt@suse.com>

---
+++ web/html/src/manager/maintenance/calendar-edit.js
@@ -0,0 +1,170 @@
+/* eslint-disable */
+'use strict';
+
+import React, {useState, useEffect, forwardRef, useImperativeHandle} from "react";
+import {Check} from "components/input/Check";
+import {Form} from "components/input/Form";
+import {Text} from "components/input/Text";
+import {Button} from "components/buttons";
+import {ModalButton} from "components/dialog/ModalButton";
+import {DangerDialog} from "components/dialog/DangerDialog";
+
+const MaintenanceCalendarEdit = forwardRef((props, ref) => {
+    const [calendarName, setCalendarName] = useState("");
+    const [calendarData, setCalendarData] = useState();
+    const [calendarDataText, setCalendarDataText] = useState("");
+    const [icalLoading, setIcalLoading] = useState(false);
+    const [strategy, setStrategy] = useState(false);
+
+    useEffect(() => {
+        if (props.isEdit) {
+            setCalendarName(props.calendar.calendarName);
+            setCalendarData(props.calendar.calendarData);
+            setCalendarDataText(props.calendar.calendarUrl || "");
+        }
+    }, [props.calendar]);
+
+    const model = {
+        calendarName: calendarName,
+        strategy: strategy
+    };
+
+    const onFormChanged = (model) => {
+        /* strategy gets initialized as empty string, but we want the initial value to be false.
+         * Is equivalent to: if strategy is "" then set it to false */
+        model.strategy === "" && (model.strategy = false);
+        setCalendarName(model.calendarName);
+        setStrategy(model.strategy);
+    };
+
+    const onCalendarDataTextChanged = (event) => {
+        setCalendarDataText(event.target.value);
+    };
+
+    const onIcalFileAttach = (event) => {
+        props.isLoading(true);
+        const reader = new FileReader();
+        reader.onload = (e) => icalFileLoaded(e.target.result);
+        reader.readAsText(event.target.files[0]);
+        !props.isEdit && setCalendarDataText(event.target.files[0].name);
+    };
+
+    const onIcalFileRemove = () => {
+        document.getElementById("ical-data-upload").value = "";
+        setCalendarData(undefined);
+        setCalendarDataText("");
+    };
+
+    const handleFileAttach = () => {
+        document.getElementById("ical-data-upload").click();
+    };
+
+    const icalFileLoaded = (fileString) => {
+        setCalendarData(fileString);
+        props.isLoading(false);
+    };
+
+    const onConfirmRefresh = () => {
+        setIcalLoading(true);
+        props.onRefresh({
+            calendarId: props.calendar.calendarId,
+            calendarName: props.calendar.calendarName,
+            calendarUrl: props.calendar.calendarUrl,
+            strategy: strategy ? "Cancel" : "Fail"
+        });
+    };
+
+    useImperativeHandle(ref, () => ({
+        onEdit() {
+            const params = {
+                calendarName: calendarName,
+                calendarData: calendarData,
+                calendarUrl: calendarDataText
+            };
+            if (props.isEdit) {
+                params.calendarId = props.calendar.calendarId;
+                params.strategy = strategy ? "Cancel" : "Fail";
+            }
+            props.onEdit(params);
+        }
+    }));
+
+    return (
+        <Form onChange={model => onFormChanged(model)} model={model}>
+            <Text name="calendarName" required type="text" label={t("Calendar Name")}
+                  labelClass="col-md-3" divClass="col-md-6" disabled={props.isEdit}/>
+            {(props.isEdit && !props.calendar.calendarUrl) &&
+            <Check name="strategy" label={<b>{t("Cancel affected actions")}</b>} divClass="col-md-6 col-md-offset-3" />
+            }
+            <div className="form-horizontal">
+                <div className="form-group">
+                    <label className="col-md-3 control-label">{t("Calendar data")}:</label>
+                    {(!props.isEdit || props.calendar.calendarUrl) &&
+                    <div className={"align-middle col-md-" + (props.isEdit ? "5" : "4")} >
+                        <input type="text" className="form-control text-truncate"
+                               placeholder={t("Enter Url to ical file")}
+                               value={calendarDataText}
+                               disabled={!props.isEdit && calendarData}
+                               onChange={onCalendarDataTextChanged}/>
+                        {!calendarData && <b className="pl-4">or</b>}
+                    </div>
+                    }
+                    {!(props.isEdit && props.calendar.calendarUrl) ? (
+                            !calendarData ?
+                                <div className="pl-0 col-md-1">
+                                    <Button id="ical-upload-btn" className="btn-default"
+                                            text={t("Attach file")}
+                                            handler={handleFileAttach}
+                                    />
+                                </div>
+                                :
+                                <div className="col-md-1">
+                                    <Button id="ical-rm-btn" className="btn-default" text={t("Remove file")}
+                                            handler={onIcalFileRemove}/>
+                                </div>
+                        ) :
+                        <div className="col-md-1">
+                            <ModalButton id="url-refresh-btn"
+                                         className="btn-default btn-sm"
+                                         icon={icalLoading ? "fa fa-circle-o-notch fa-spin" : "fa-refresh"}
+                                         target="confirm-modal"
+                                         title={t("Refresh data from url")}
+                                         disabled={props.calendar.calendarUrl !== calendarDataText}
+                            />
+                            <DangerDialog id="confirm-modal"
+                                          title="Confirm calendar refresh"
+                                          content={
+                                              <div>
+                                                  <div>{t("Refreshing the calendar will reschedule all affected actions.")}</div>
+                                                  <div>{t("Confirm if you want to proceed.")}</div>
+                                                  <Check name="strategy" label={<b>{t("Cancel affected actions?")}</b>} divClass="col-md-6" />
+                                              </div>
+                                          }
+                                          onConfirm={() => onConfirmRefresh()}
+                                          submitText="Confirm"
+                                          submitIcon="fa-check"
+                            />
+                        </div>
+                    }
+                </div>
+                <input className="hidden" type="file" id="ical-data-upload" onChange={onIcalFileAttach}/>
+            </div>
+            {(props.isEdit && calendarData) &&
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4>
+                        {props.calendar.calendarName}
+                    </h4>
+                </div>
+                <div className="panel-body">
+                        <pre>
+                            {calendarData}
+                        </pre>
+                </div>
+            </div>
+            }
+        </Form>
+    );
+});
+
+export default MaintenanceCalendarEdit;
--- web/html/src/manager/maintenance/maintenance-windows-edit.js
+++ web/html/src/manager/maintenance/maintenance-windows-edit.js
@@ -2,267 +2,33 @@
 'use strict';
 
 const React = require("react");
-
-const ReactDOM = require("react-dom");
 const AsyncButton = require("components/buttons").AsyncButton;
 const Button = require("components/buttons").Button;
-const ModalButton = require("components/dialog/ModalButton").ModalButton;
-const DangerDialog = require("components/dialog/DangerDialog").DangerDialog;
-const { Combobox } = require("components/combobox");
-const { InnerPanel } = require("components/panels/InnerPanel");
-const { Form } = require('components/input/Form');
-const { Text } = require('components/input/Text');
-const { Radio } = require('components/input/Radio');
-const { Check } = require('components/input/Check');
+const {InnerPanel} = require("components/panels/InnerPanel");
+
+import MaintenanceScheduleEdit from "./schedule-edit";
+import MaintenanceCalendarEdit from "./calendar-edit";
 
 class MaintenanceWindowsEdit extends React.Component {
     constructor(props) {
         super(props);
 
-        if (this.isEdit()) {
-            this.state = props.schedule;
-            this.state.type = props.type;
-            this.state.calendarDataText = props.schedule.calendarUrl;
-            this.state.calendarAdded = props.schedule.calendarName ? true : false;
-            (this.state.type === "schedule" && this.props.calendarNames.length > 1 && this.props.schedule.calendarName)
-                ? this.state.selectedCalendar = this.props.calendarNames
-                    .filter(name => name.text === this.props.schedule.calendarName)[0].id
-                : this.state.selectedCalendar = 0;
-        } else {
-            this.state = {
-                type: props.type,
-                calendarDataText: "",
-                selectedCalendar: 0
-            }
+        this.child = React.createRef();
+
+        this.state = {
+            type: props.type
         }
     }
-
-    setCalendarName = () => {
-        return this.state.type === "schedule" ?
-            ((this.state.calendarAdded && this.state.calendarName) ? this.state.calendarName : "")
-            : this.state.calendarName;
-    };
 
     isEdit = () => {
         return this.props.schedule ? true : false;
     };
 
-    onEdit = () => {
-        return this.props.onEdit({
-            scheduleId: this.state.scheduleId,
-            scheduleName: this.state.scheduleName,
-            scheduleType: this.state.scheduleType,
-            calendarId: this.state.calendarId,
-            calendarName: this.setCalendarName(),
-            calendarData: this.state.calendarData,
-            calendarUrl: (!this.isEdit() && this.state.calendarData) ? "" : this.state.calendarDataText,
-            /* Reschedule strategy. false == 'FAIL', true == 'CANCEL' */
-            strategy: this.state.strategy ? "Cancel" : "Fail"
-        });
-    };
-
-    onFormChanged = (model) => {
-        model.strategy === "" && (model.strategy = false);
-        this.setState(model);
-    };
-
-    onCalendarDataTextChanged = (event) => {
-        this.setState({
-            calendarDataText: event.target.value
-        });
-    };
-
-    onIcalFileAttach = (event) => {
-        this.setState({
-            icalLoading: true
-        });
-        const reader = new FileReader();
-        reader.onload = (e) => this.icalFileLoaded(e.target.result);
-        reader.readAsText(event.target.files[0]);
-        this.setState({
-            calendarDataText: event.target.files[0].name
-        });
-    };
-
-    onIcalFileRemove = () => {
-        document.getElementById("ical-data-upload").value = "";
-        this.setState({
-            calendarData: undefined,
-            calendarDataText: "",
-        });
-    };
-
-    handleFileAttach = () => {
-        document.getElementById("ical-data-upload").click();
-    };
-
-    icalFileLoaded = (fileString) => {
-        this.setState({
-            calendarData: fileString,
-            icalLoading: false
-        });
-    };
-
-    onSelectCalendar = (item) => {
-        this.setState({
-            selectedCalendar: item.id,
-            calendarName: item.text === "<None>" ? "" : item.text
-        })
-    };
-
-    onConfirmRefresh = () => {
-        this.setState({
-            icalLoading: true
-        });
-        this.props.onRefresh({
-            calendarId: this.state.calendarId,
-            calendarName: this.state.calendarName,
-            strategy: this.state.strategy ? "Cancel" : "Fail",
-            calendarUrl: this.state.calendarUrl
-        })
-    }
-
-    renderScheduleEdit() {
-        const model = {
-            scheduleName: this.state.scheduleName,
-            scheduleType: this.state.scheduleType,
-            calendarName: this.state.calendarName,
-            strategy: this.state.strategy,
-        }
-        return (
-            <Form onChange={this.onFormChanged} model={model}>
-                <Text name="scheduleName" required type="text" label={t("Schedule Name")}
-                      labelClass="col-sm-3" divClass="col-sm-6"
-                      disabled={this.isEdit()} />
-                <Radio defaultValue="SINGLE" name="scheduleType" inline={true} label={t('Type')} labelClass="col-md-3" divClass="col-md-6"
-                       items={[
-                           {label: <b>{t('Single')}</b>, value: 'SINGLE'},
-                           {label: <b>{t('Multi')}</b>, value: 'MULTI'},
-                       ]}
-                />
-                {this.isEdit() &&
-                <Check name="strategy" label={<b>{t("Cancel affected actions")}</b>} divClass="col-md-6 col-md-offset-3" />
-                }
-                <div className="form-group">
-                    <div className="col-md-6 col-md-offset-3">
-                        {!this.state.calendarAdded ?
-                            <Button className="btn-default" text="Add Calendar" icon="fa-chevron-down "
-                                    handler={() => this.setState({calendarAdded: !this.state.calendarAdded})} />
-                            :
-                            <div className="panel panel-default">
-                                <div className="panel-heading no-padding">
-                                    <Button text="Add Calendar" icon="fa-chevron-up"
-                                            handler={() => this.setState({calendarAdded: !this.state.calendarAdded})} />
-                                </div>
-                                <div className="panel-body">
-                                    <div className="form-horizontal">
-                                        <div className="form-group">
-                                            <label className="col-md-3 control-label">{t("Calendar")}:</label>
-                                            <div className="col-md-7">
-                                                <Combobox id="calendarSelect" name="calendarSelect"
-                                                          data={this.props.calendarNames}
-                                                          selectedId={this.state.selectedCalendar}
-                                                          onSelect={this.onSelectCalendar}
-                                                />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        }
-                    </div>
-                </div>
-            </Form>
-        )
-    }
-
-    renderCalendarEdit() {
-        const model = {
-            calendarName: this.state.calendarName,
-            strategy: this.state.strategy
-        }
-        return (
-            <Form onChange={model => this.onFormChanged(model)} model={model}>
-                <Text name="calendarName" required type="text" label={t("Calendar Name")}
-                      labelClass="col-md-3" divClass="col-md-6" disabled={this.isEdit()}/>
-                {(this.isEdit() && !this.state.calendarUrl) &&
-                <Check name="strategy" label={<b>{t("Cancel affected actions")}</b>} divClass="col-md-6 col-md-offset-3" />
-                }
-                <div className="form-horizontal">
-                    <div className="form-group">
-                        <label className="col-md-3 control-label">{t("Calendar data")}:</label>
-                        {(!this.isEdit() || this.state.calendarUrl) &&
-                        <div className={"align-middle col-md-" + (this.isEdit() ? "5" : "4")} >
-                            <input type="text" className="form-control text-truncate"
-                                   placeholder={t("Enter Url to ical file")}
-                                   value={this.state.calendarDataText}
-                                   disabled={this.state.calendarData && !this.state.calendarUrl}
-                                   onChange={this.onCalendarDataTextChanged}/>
-                            {!this.state.calendarData && <b className="pl-4">or</b>}
-                        </div>
-                        }
-                        {!(this.isEdit() && this.state.calendarUrl) ? (
-                                !this.state.calendarData ?
-                                    <div className="pl-0 col-md-1">
-                                        <Button id="ical-upload-btn" className="btn-default"
-                                                text={t("Attach file")}
-                                                handler={this.handleFileAttach}
-                                        />
-                                    </div>
-                                    :
-                                    <div className="col-md-1">
-                                        <Button id="ical-rm-btn" className="btn-default" text={t("Remove file")}
-                                                handler={this.onIcalFileRemove}/>
-                                    </div>
-                            ) :
-                            <div className="col-md-1">
-                                <ModalButton id="url-refresh-btn"
-                                             className="btn-default btn-sm"
-                                             icon={this.state.icalLoading ? "fa fa-circle-o-notch fa-spin" : "fa-refresh"}
-                                             target="confirm-modal"
-                                             title={t("Refresh data from url")}
-                                             disabled={this.state.calendarUrl !== this.state.calendarDataText}
-                                />
-                                <DangerDialog id="confirm-modal"
-                                              title="Confirm calendar refresh"
-                                              content={
-                                                  <div>
-                                                      <div>{t("Refreshing the calendar will reschedule all affected actions.")}</div>
-                                                      <div>{t("Confirm if you want to proceed.")}</div>
-                                                      <Check name="strategy" label={<b>{t("Cancel affected actions?")}</b>} divClass="col-md-6" />
-                                                  </div>
-                                              }
-                                              onConfirm={() => this.onConfirmRefresh()}
-                                              submitText="Confirm"
-                                              submitIcon="fa-check"
-                                />
-                            </div>
-                        }
-                    </div>
-                </div>
-                {(this.isEdit() && this.state.calendarData) &&
-                <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h4>
-                            {this.state.calendarName}
-                        </h4>
-                    </div>
-                    <div className="panel-body">
-                        <pre>
-                            {this.state.calendarData}
-                        </pre>
-                    </div>
-                </div>
-                }
-            </Form>
-        )
-    }
-
     render() {
         const buttons = [
             <div className="btn-group pull-right">
-                <AsyncButton action={this.onEdit} defaultType="btn-success"
-                             disabled={this.state.icalLoading}
+                <AsyncButton id={"editButton"} action={() => this.child.current.onEdit()} defaultType="btn-success"
+                             disabled={this.state.icalLoading === true}
                              text={(this.isEdit() ? t("Update ") : t("Create ")) +
                              (this.state.type === "schedule" ? t("Schedule") : t("Calendar"))}
                 />
@@ -274,15 +40,32 @@ class MaintenanceWindowsEdit extends React.Component {
             </div>
         ];
         return (
-            <InnerPanel title={t("Maintenance ") +(this.state.type === "schedule" ? t("Schedule") : t("Calendar"))}
+            <InnerPanel title={t("Maintenance ") + (this.state.type === "schedule" ? t("Schedule") : t("Calendar"))}
                         icon="spacewalk-icon-schedule" buttonsLeft={buttonsLeft} buttons={buttons} >
-                {this.state.type === "schedule" ? this.renderScheduleEdit() : this.renderCalendarEdit()}
-                <input className="hidden" type="file" id="ical-data-upload" onChange={this.onIcalFileAttach}/>
+                {
+                    this.state.type === "schedule" &&
+                    <MaintenanceScheduleEdit
+                        ref={this.child}
+                        isEdit={this.isEdit()}
+                        schedule={this.props.schedule}
+                        calendarNames={this.props.calendarNames}
+                        onEdit={this.props.onEdit}
+                    /> ||
+                    this.state.type === "calendar" &&
+                    <MaintenanceCalendarEdit
+                        ref={this.child}
+                        isEdit={this.isEdit()}
+                        calendar={this.props.schedule}
+                        onRefresh={this.props.onRefresh}
+                        onEdit={this.props.onEdit}
+                        isLoading={i => this.setState({icalLoading: i})}
+                    />
+                }
             </InnerPanel>
         );
     }
 }
 
-module.exports = {
-    MaintenanceWindowsEdit: MaintenanceWindowsEdit
+export {
+    MaintenanceWindowsEdit
 };
