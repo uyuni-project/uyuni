e81195f55dcee6dce1236e9f159ced96b372aec1
Pascal Arlt <parlt@suse.com>
2020-07-03 16:56:56 +0200

    Refactor MaintenanceWindows to use react hooks
    Signed-off-by: Pascal Arlt <parlt@suse.com>

--- web/html/src/manager/maintenance/details/maintenance-windows-details.js
+++ web/html/src/manager/maintenance/details/maintenance-windows-details.js
@@ -1,90 +1,78 @@
 /* eslint-disable */
 'use strict';
 
-const React = require("react");
-const {Button} = require("components/buttons");
-const {ModalButton} = require("components/dialog/ModalButton");
-const {TopPanel} = require('components/panels/TopPanel');
-const {Messages} = require("components/messages");
+import React, {useState} from "react";
+import {Button} from "components/buttons";
+import {ModalButton} from "components/dialog/ModalButton";
+import {TopPanel} from "components/panels/TopPanel";
 
 import MaintenanceScheduleDetails from "./schedule-details";
 import MaintenanceCalendarDetails from "./calendar-details";
 
-class MaintenanceWindowsDetails extends React.Component {
+const MaintenanceWindowsDetails = (props) => {
+    const [type] = useState(props.type);
 
-    constructor(props) {
-        super(props);
+    const buttons = [
+        <div className="btn-group pull-right">
+            <Button
+                text={t("Back")}
+                icon="fa-chevron-left"
+                title={t("Back")}
+                className="btn-default"
+                handler={() => props.onCancel("back")}
+            />
+            <Button
+                text={t("Edit")}
+                disabled={!isAdmin}
+                icon="fa-edit"
+                title={t("Edit")}
+                className="btn-default"
+                handler={() => props.onEdit(
+                    type === "schedule" ? props.data.scheduleId : props.data.calendarId
+                )}
+            />
+            <ModalButton
+                text={t("Delete")}
+                disabled={!isAdmin}
+                icon="fa-trash"
+                title={t("Delete")}
+                target="delete-modal"
+                className="btn-default"
+            />
+        </div>
+    ];
 
-        this.state = {
-            type: props.type,
-            messages: [],
-        };
-    }
-
-    render() {
-        const buttons = [
-            <div className="btn-group pull-right">
-                <Button
-                    text={t("Back")}
-                    icon="fa-chevron-left"
-                    title={t("Back")}
-                    className="btn-default"
-                    handler={() => this.props.onCancel("back")}
+    return (
+        <TopPanel
+            title={props.type === "schedule"
+                ? props.data.scheduleName
+                : props.data.calendarName
+            }
+            icon="spacewalk-icon-schedule"
+            helpUrl=""
+            button={buttons}
+        >
+            {
+                type === "schedule" &&
+                <MaintenanceScheduleDetails
+                    scheduleName={props.data.scheduleName}
+                    calendarName={props.data.calendarName}
+                    scheduleType={props.data.scheduleType}
+                    maintenanceWindows={props.data.maintenanceWindows}
+                    onDelete={props.onDelete}
+                /> ||
+                type === "calendar" &&
+                <MaintenanceCalendarDetails
+                    calendarName={props.data.calendarName}
+                    scheduleNames={props.data.scheduleNames}
+                    calendarUrl={props.data.calendarUrl}
+                    calendarData={props.data.calendarData}
+                    onDelete={props.onDelete}
                 />
-                <Button
-                    text={t("Edit")}
-                    disabled={!isAdmin}
-                    icon="fa-edit"
-                    title={t("Edit")}
-                    className="btn-default"
-                    handler={() => this.props.onEdit(
-                        this.state.type === "schedule" ? this.props.data.scheduleId : this.props.data.calendarId
-                    )}
-                />
-                <ModalButton
-                    text={t("Delete")}
-                    disabled={!isAdmin}
-                    icon="fa-trash"
-                    title={t("Delete")}
-                    target="delete-modal"
-                    className="btn-default"
-                />
-            </div>
-        ];
-
-        return (
-            <TopPanel
-                title={this.props.type === "schedule"
-                    ? this.props.data.scheduleName
-                    : this.props.data.calendarName
-                }
-                icon="spacewalk-icon-schedule"
-                helpUrl=""
-                button={buttons}
-            >
-                <Messages items={this.state.messages}/>
-                {
-                    this.state.type === "schedule" &&
-                    <MaintenanceScheduleDetails
-                        scheduleName={this.props.data.scheduleName}
-                        calendarName={this.props.data.calendarName}
-                        scheduleType={this.props.data.scheduleType}
-                        maintenanceWindows={this.props.data.maintenanceWindows}
-                        onDelete={this.props.onDelete}
-                    /> ||
-                    this.state.type === "calendar" &&
-                    <MaintenanceCalendarDetails
-                        calendarName={this.props.data.calendarName}
-                        scheduleNames={this.props.data.scheduleNames}
-                        calendarUrl={this.props.data.calendarUrl}
-                        calendarData={this.props.data.calendarData}
-                        onDelete={this.props.onDelete}
-                    />
-                }
-            </TopPanel>
-        );
-    }
-}
+            }
+        </TopPanel>
+    );
+};
 
 export {
     MaintenanceWindowsDetails
--- web/html/src/manager/maintenance/list/maintenance-windows-list.js
+++ web/html/src/manager/maintenance/list/maintenance-windows-list.js
@@ -1,67 +1,58 @@
 /* eslint-disable */
 'use strict';
 
-const React = require("react");
-const { InnerPanel } = require('components/panels/InnerPanel');
-const Button = require("components/buttons").Button;
+import React, {useState} from "react";
+import {InnerPanel} from "components/panels/InnerPanel";
+import {Button} from "components/buttons";
 
 import MaintenanceScheduleList from "./schedule-list";
 import MaintenanceCalendarList from "./calendar-list";
 
-class MaintenanceWindowsList extends React.Component {
+const MaintenanceWindowsList = (props) => {
+    const [type] = useState(props.type);
 
-    constructor(props) {
-        super(props);
+    const createButton = [
+        <div className="btn-group pull-right">
+            <Button
+                className="btn-default"
+                disabled={!isAdmin}
+                icon="fa-plus"
+                text={t("Create")}
+                title="Create a new maintenance schedule"
+                handler={() => props.onActionChanged("create")}
+            />
+        </div>
+    ];
 
-        this.state = {
-            type: props.type,
-        };
-    }
-
-    render() {
-        const createButton = [
-            <div className="btn-group pull-right">
-                <Button
-                    className="btn-default"
-                    disabled={!isAdmin}
-                    icon="fa-plus"
-                    text={t("Create")}
-                    title="Create a new maintenance schedule"
-                    handler={() => this.props.onActionChanged("create")}
-                />
-            </div>
-        ];
-
-        return (
-            <div>
-                <InnerPanel title={t("Maintenance " +  (this.state.type === "schedule" ? "Schedules" : "Calendars"))} icon="spacewalk-icon-schedule" buttons={this.props.disableCreate ? null : createButton}>
-                    <div className="panel panel-default">
-                        <div className="panel-heading">
-                            <div>
-                                <h3>{t(this.state.type === "schedule" ? "Schedules" : "Calendars")}</h3>
-                            </div>
+    return (
+        <div>
+            <InnerPanel title={t("Maintenance " +  (type === "schedule" ? "Schedules" : "Calendars"))} icon="spacewalk-icon-schedule" buttons={createButton}>
+                <div className="panel panel-default">
+                    <div className="panel-heading">
+                        <div>
+                            <h3>{t(type === "schedule" ? "Schedules" : "Calendars")}</h3>
                         </div>
-                        {
-                            this.state.type === "schedule" &&
-                            <MaintenanceScheduleList
-                                data={this.props.data}
-                                onSelect={this.props.onSelect}
-                                onEdit={this.props.onEdit}
-                                onDelete={this.props.onDelete}
-                            /> ||
-                            this.state.type === "calendar" &&
-                            <MaintenanceCalendarList
-                                data={this.props.data}
-                                onSelect={this.props.onSelect}
-                                onEdit={this.props.onEdit}
-                                onDelete={this.props.onDelete}
-                            />
-                        }
                     </div>
-                </InnerPanel>
-            </div>
-        );
-    }
+                    {
+                        type === "schedule" &&
+                        <MaintenanceScheduleList
+                            data={props.data}
+                            onSelect={props.onSelect}
+                            onEdit={props.onEdit}
+                            onDelete={props.onDelete}
+                        /> ||
+                        type === "calendar" &&
+                        <MaintenanceCalendarList
+                            data={props.data}
+                            onSelect={props.onSelect}
+                            onEdit={props.onEdit}
+                            onDelete={props.onDelete}
+                        />
+                    }
+                </div>
+            </InnerPanel>
+        </div>
+    );
 }
 
 export {
