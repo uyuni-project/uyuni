415625ac7e5223be960dab23583a9aed80981f5c
Michael Calmer <mc@suse.de>
2020-07-06 13:30:30 +0200

    drop function in the public schema and create it in rhn_entitlement

---
+++ schema/spacewalk/upgrade/susemanager-schema-4.1.10-to-susemanager-schema-4.1.11/002-entitle-server.sql
@@ -0,0 +1,48 @@
+-- drop function in the public schema created there by accident
+drop function if exists entitle_server;
+
+-- setup search_path so that these functions are created in appropriate schema.
+update pg_settings set setting = 'rhn_entitlements,' || setting where name = 'search_path';
+
+    create or replace function entitle_server (
+        server_id_in in numeric,
+        type_label_in in varchar
+    ) returns void
+as $$
+    declare
+      sgid  numeric := 0;
+
+    begin
+
+      if rhn_entitlements.can_entitle_server(server_id_in,
+                                             type_label_in) = 1 then
+         sgid := rhn_entitlements.find_compatible_sg (server_id_in,
+                                                      type_label_in);
+         if sgid is not null then
+            insert into rhnServerHistory ( id, server_id, summary, details )
+            values ( nextval('rhn_event_id_seq'), server_id_in,
+                     'added system entitlement ',
+                      case type_label_in
+                       when 'enterprise_entitled' then 'Management'
+                       when 'bootstrap_entitled' then 'Bootstrap'
+                       when 'salt_entitled' then 'Salt'
+                       when 'foreign_entitled' then 'Foreign'
+                       when 'virtualization_host' then 'Virtualization'
+                       when 'container_build_host' then 'Container'
+                       when 'osimage_build_host' then 'OS Image'
+                       when 'monitoring_entitled' then 'Monitoring'
+                      end  );
+
+            perform rhn_server.insert_into_servergroup (server_id_in, sgid);
+
+         else
+            perform rhn_exception.raise_exception ('no_available_server_group');
+         end if;
+      else
+         perform rhn_exception.raise_exception ('invalid_entitlement');
+      end if;
+   end$$
+language plpgsql;
+
+-- restore the original setting
+update pg_settings set setting = overlay( setting placing '' from 1 for (length('rhn_entitlements')+1) ) where name = 'search_path';
