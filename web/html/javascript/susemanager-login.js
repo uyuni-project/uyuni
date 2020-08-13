01d39993c8bc7311c5f1c636a0ab976543544114
dleidi <dleidi@suse.com>
2020-08-13 15:34:00 +0200

    Login fixes after jQuery upgrade (bsc#1175224)

--- web/html/javascript/susemanager-login.js
+++ web/html/javascript/susemanager-login.js
@@ -1,20 +1,20 @@
-$(document).on("ready", function() {
+jQuery(document).ready(function() {
   // move the footer to the end of the body
-  const footer = $('footer');
+  const footer = jQuery('footer');
   footer.remove();
-  $('body').append(footer);
-  $("#scroll-top").remove();
-  $("aside").remove();
-  $('.navbar-toggle').remove();
+  jQuery('body').append(footer);
+  jQuery("#scroll-top").remove();
+  jQuery("aside").remove();
+  jQuery('.navbar-toggle').remove();
   formFocus('loginForm', 'username');
-  $("body").addClass('login-page');
-  $('header').wrapInner('<div class="wrap"></div>').addClass('Raleway-font');
+  jQuery("body").addClass('login-page');
+  jQuery('header').wrapInner('<div class="wrap"></div>').addClass('Raleway-font');
 
   // move and keep only the logo at the beginning of the header
-  const logo = $('.navbar-brand');
-  $('#breadcrumb').remove();
-  $('header .wrap').prepend(logo);
+  const logo = jQuery('.navbar-brand');
+  jQuery('#breadcrumb').remove();
+  jQuery('header .wrap').prepend(logo);
 
-  $('section').wrapInner('<div class="wrap"></div>');
-  $('footer .wrapper').addClass('wrap');
+  jQuery('section').wrapInner('<div class="wrap"></div>');
+  jQuery('footer .wrapper').addClass('wrap');
 });
