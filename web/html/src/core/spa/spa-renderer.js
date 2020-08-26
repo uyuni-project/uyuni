70286fc0a355508a26d6f8f8fa92a8b4a32dd01a
Pascal Arlt <parlt@suse.com>
2020-08-26 10:17:17 +0200

    Add translation enablement for react t()
    Signed-off-by: Pascal Arlt <parlt@suse.com>

--- web/html/src/core/spa/spa-renderer.js
+++ web/html/src/core/spa/spa-renderer.js
@@ -3,6 +3,7 @@
 import type {Element as ReactElement} from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {getTranslationData} from "utils/translate";
 
 declare var onDocumentReadyInitOldJS: Function;
 
@@ -19,6 +20,7 @@ window.pageRenderers.spa.reactRenderers = window.pageRenderers.spa.reactRenderer
 window.pageRenderers.spa.previousReactRenderers = window.pageRenderers.spa.previousReactRenderers || [];
 
 function addReactApp(appName: string) {
+  getTranslationData();
   window.pageRenderers.spa.reactAppsName.push(appName);
 }
 
@@ -30,6 +32,8 @@ function renderGlobalReact(element: ReactElement<any>, container: ?Element) {
   if (container == null) {
     throw new Error('The DOM element is not present.');
   }
+
+  getTranslationData();
 
   function registerGlobalRender(instance) {
     window.pageRenderers.spa.globalRenderersToUpdate.push(instance);
