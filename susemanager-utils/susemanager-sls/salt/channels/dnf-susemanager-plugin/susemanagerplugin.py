20be249ed96ea6e7dc6a81b759aca457e1847b75
Matei Albu <malbu@suse.de>
2020-08-26 09:51:54 +0200

    dnf plugin wasn't adding the token to the http header (bsc#1175724)

--- susemanager-utils/susemanager-sls/salt/channels/dnf-susemanager-plugin/susemanagerplugin.py
+++ susemanager-utils/susemanager-sls/salt/channels/dnf-susemanager-plugin/susemanagerplugin.py
@@ -6,8 +6,7 @@ class Susemanager(dnf.Plugin):
 
     def __init__(self, base, cli):
         super(Susemanager, self).__init__(base, cli)
-        with dnf.base.Base() as base:
-            base.read_all_repos()
+        base.read_all_repos()
         for repo in base.repos.get_matching("susemanager:*"):
             try:
                 susemanager_token = repo.cfg.getValue(section=repo.id, key="susemanager_token")
