75127dded88712e573e8dd34695830c99c73c84c
Pascal Arlt <parlt@suse.com>
2020-08-26 13:37:55 +0200

    Fix ISE on about page
    Signed-off-by: Pascal Arlt <parlt@suse.com>

--- java/code/src/com/suse/manager/webui/menu/MenuTree.java
+++ java/code/src/com/suse/manager/webui/menu/MenuTree.java
@@ -597,7 +597,7 @@ public class MenuTree {
     }
 
     /**
-     * Get the users current locale
+     * Get the users current locale. If no user is available return the config default
      *
      * @param pageContext the current PageContext
      * @return the users locale
@@ -605,8 +605,13 @@ public class MenuTree {
     public String getCurrentLocale(PageContext pageContext) {
         HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
         User user = new RequestContext(request).getCurrentUser();
-        String currentLocale = user.getPreferredLocale();
-        return currentLocale;
+
+        if (checkAcl(user, "user_authenticated()")) {
+            return user.getPreferredLocale();
+        }
+        else {
+            return ConfigDefaults.get().getDefaultLocale();
+        }
     }
 
     /**
