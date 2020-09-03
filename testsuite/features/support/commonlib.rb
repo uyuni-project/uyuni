1a436f874b2f1b7bd397ae05182615ed5e3ecce8
Lukas Kotek <lkotek@suse.com>
2020-09-03 15:32:37 +0200

    testsuite - update of kiwi images

--- testsuite/features/support/commonlib.rb
+++ testsuite/features/support/commonlib.rb
@@ -42,7 +42,10 @@ def compute_image_filename
   when nil
     'Kiwi/POS_Image-JeOS6_head'
   when 'sles15sp2', 'sles15sp2o'
-    'Kiwi/POS_Image-JeOS7_head'
+    # Same kiwi image version is used in case of 4.0 and 4.1
+    'Kiwi/POS_Image-JeOS7_40'
+  when 'sles15sp1', 'sles15sp1o'
+    raise 'This is not supported image version.'
   else
     'Kiwi/POS_Image-JeOS6_head'
   end
@@ -53,7 +56,9 @@ def compute_image_name
   when nil
     'POS_Image_JeOS6_head'
   when 'sles15sp2', 'sles15sp2o'
-    'POS_Image_JeOS7_head'
+    'POS_Image_JeOS7_40'
+  when 'sles15sp1', 'sles15sp1o'
+    raise 'This is not supported image version.'
   else
     'POS_Image_JeOS6_head'
   end
