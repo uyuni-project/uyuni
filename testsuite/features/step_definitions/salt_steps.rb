13ac51b96c2876f2e57c35a4986d81cd4df35321
Eric Bischoff <ebischoff@suse.com>
2020-09-17 10:45:14 +0200

    Make rubocop happy

--- testsuite/features/step_definitions/salt_steps.rb
+++ testsuite/features/step_definitions/salt_steps.rb
@@ -547,7 +547,7 @@ Then(/^the pillar data for "([^"]*)" should (be|contain|not contain) "([^"]*)" o
   if verb == 'be' && value == ''
     raise "Output has more than one line: #{output}" unless output.split("\n").length == 1
   elsif verb == 'be'
-    raise "Output value not found : #{output}" unless output.split("\n").length() > 1
+    raise "Output value wasn't found: #{output}" unless output.split("\n").length > 1
     raise "Output value is different than #{value}: #{output}" unless output.split("\n")[1].strip == value
   elsif verb == 'contain'
     raise "Output doesn't contain #{value}: #{output}" unless output.include? value
