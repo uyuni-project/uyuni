5256b855760618e933820eb988ed4c7f65e10802
Frantisek Kobzik <fkobzik@suse.de>
2020-10-05 14:34:28 +0200

    Endpoint reporting architecture label in list of installed packages
    The original endpoint listPackages reports architecture name, which is
    incompatible with other endpoints.
    
    This commit adds a new endpoint listInstalledPackages, that reports
    architecture label.
    
    The listPackage endpoint is preserved for API backwards compatibility.

--- java/code/src/com/redhat/rhn/manager/system/SystemManager.java
+++ java/code/src/com/redhat/rhn/manager/system/SystemManager.java
@@ -418,7 +418,18 @@ public class SystemManager extends BaseManager {
      * @return Returns a list of packages for a system
      */
     public static DataResult<Map<String, Object>> installedPackages(Long sid) {
-        SelectMode m = ModeFactory.getMode("System_queries", "system_installed_packages", Map.class);
+        return installedPackages(sid, false);
+    }
+
+    /**
+     * Gets the installed packages on a system
+     * @param sid The system in question
+     * @return Returns a list of packages for a system
+     */
+    public static DataResult<Map<String, Object>> installedPackages(Long sid, boolean archAsLabel) {
+        String suffix = archAsLabel ? "_arch_as_label" : "";
+        String query = "system_installed_packages" + suffix;
+        SelectMode m = ModeFactory.getMode("System_queries", query, Map.class);
         Map<String, Object> params = new HashMap<String, Object>();
         params.put("sid", sid);
         DataResult<Map<String, Object>> pkgs = m.execute(params);
