336919d5e3e13d89e9d5c68dff5fa9c2b3c743b1
Cédric Bosdonnat <cbosdonnat@suse.com>
2020-10-21 16:55:32 +0200

    Better VM changes notifications in VirtualInstanceManager
    So far VM refresh events where sent only when the plan was applied in
    VirtualInstanceManager. Send the refresh event in the functions actually
    adding / deleting / updating the VM in the database.

--- java/code/src/com/redhat/rhn/manager/system/VirtualInstanceManager.java
+++ java/code/src/com/redhat/rhn/manager/system/VirtualInstanceManager.java
@@ -135,10 +135,6 @@ public class VirtualInstanceManager extends BaseManager {
                 deleteGuestVirtualInstance(virtualInstance);
             });
         }
-
-        if (!plan.isEmpty()) {
-            VirtNotifications.spreadRefresh("guest");
-        }
     }
 
     /**
@@ -203,6 +199,7 @@ public class VirtualInstanceManager extends BaseManager {
         else {
             VirtualInstanceFactory.getInstance().deleteVirtualInstanceOnly(virtualInstance);
         }
+        VirtNotifications.spreadRefresh("guest");
     }
 
     /**
@@ -274,6 +271,8 @@ public class VirtualInstanceManager extends BaseManager {
 
             VirtualInstanceFactory.getInstance()
                     .saveVirtualInstance(virtualInstance);
+
+            VirtNotifications.spreadRefresh("guest");
         }
         else {
             log.warn("Preventing creation of a duplicated VirtualInstance " +
@@ -352,6 +351,8 @@ public class VirtualInstanceManager extends BaseManager {
         virtualInstance.setNumberOfCPUs(vCpus);
         virtualInstance.setTotalMemory(memory);
         VirtualInstanceFactory.getInstance().saveVirtualInstance(virtualInstance);
+
+        VirtNotifications.spreadRefresh("guest");
     }
 
     /**
