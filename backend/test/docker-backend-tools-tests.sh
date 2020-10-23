a7f6459d39e437f287fb1757cd720e418c70fbfc
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2020-10-23 11:21:34 +0100

    Add verbose to nosetests for satellite_tools tests

--- backend/test/docker-backend-tools-tests.sh
+++ backend/test/docker-backend-tools-tests.sh
@@ -2,4 +2,4 @@
 
 touch /etc/rhn/rhn.conf
 mkdir -p /manager/backend/reports
-nosetests --with-xunit --xunit-file /manager/backend/reports/satellite_tools_tests.xml -s /manager/backend/satellite_tools/test/unit/
+nosetests -v --with-xunit --xunit-file /manager/backend/reports/satellite_tools_tests.xml -s /manager/backend/satellite_tools/test/unit/
