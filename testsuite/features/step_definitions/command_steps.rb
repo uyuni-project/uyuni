33fa24718e00a95b70258fd852c94cdd7a0894be
Eric Bischoff <ebischoff@suse.com>
2020-10-29 10:24:03 +0100

    New sanity check: HTTP proxy

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -34,6 +34,12 @@ Then(/^it should be possible to reach the test packages$/) do
   $server.run("curl --insecure --location #{url} --output /dev/null")
 end
 
+Then(/^it should be possible to use the HTTP proxy$/) do
+  url = 'http://www.suse.com'
+  proxy = "suma:P4$$word@#{$server_http_proxy}"
+  $server.run("curl --insecure --proxy '#{proxy}' --location '#{url}' --output /dev/null")
+end
+
 Then(/^it should be possible to reach the build sources$/) do
   if $product == 'Uyuni'
     # TODO: move that internal resource to some other external location
