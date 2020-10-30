f9830149ab506fa7fb34d4cc9403a4c572680b01
Tino Calancha <ccalancha@suse.com>
2020-10-30 10:07:37 +0100

    Testsuite: Allow to override default http request timeouts
    Set this timeout as 180 s to align with uyuni.
    We will revert back to 60 s once bsc#1178294 get fixed.
    
    - testsuite/features/support/env.rb:
    Override default read_timeout for http requests.

--- testsuite/features/support/env.rb
+++ testsuite/features/support/env.rb
@@ -51,6 +51,9 @@ MultiTest.disable_autorun
 
 # register chromedriver headless mode
 Capybara.register_driver(:headless_chrome) do |app|
+  client = Selenium::WebDriver::Remote::Http::Default.new
+  # WORKAROUND failure at Scenario: Test IPMI functions: increase from 60 s to 180 s
+  client.read_timeout = 180
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     chromeOptions: { args: %w[headless no-sandbox disable-dev-shm-usage disable-gpu window-size=2048,2048, js-flags=--max_old_space_size=2048] },
     unexpectedAlertBehaviour: 'accept',
@@ -60,7 +63,8 @@ Capybara.register_driver(:headless_chrome) do |app|
   Capybara::Selenium::Driver.new(
     app,
     browser: :chrome,
-    desired_capabilities: capabilities
+    desired_capabilities: capabilities,
+    http_client: client
   )
 end
 
