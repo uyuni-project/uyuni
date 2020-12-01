94785f346cb4b0faeda95b33559f333ab7543c15
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2020-12-01 13:01:53 +0000

    Adjust java tests after backporting to 4.0

--- java/code/src/com/redhat/rhn/manager/channel/test/BaseRepoCommandTest.java
+++ java/code/src/com/redhat/rhn/manager/channel/test/BaseRepoCommandTest.java
@@ -13,9 +13,6 @@ package com.redhat.rhn.manager.channel.test;
 import com.redhat.rhn.domain.org.Org;
 import com.redhat.rhn.domain.user.User;
 import com.redhat.rhn.frontend.xmlrpc.channel.repo.InvalidRepoLabelException;
-import com.redhat.rhn.frontend.xmlrpc.channel.repo.InvalidRepoTypeException;
-import com.redhat.rhn.frontend.xmlrpc.channel.repo.InvalidRepoUrlException;
-import com.redhat.rhn.frontend.xmlrpc.channel.repo.InvalidRepoUrlInputException;
 import com.redhat.rhn.manager.channel.repo.BaseRepoCommand;
 import com.redhat.rhn.manager.channel.repo.CreateRepoCommand;
 import com.redhat.rhn.testing.RhnBaseTestCase;
@@ -36,99 +33,6 @@ public class BaseRepoCommandTest extends RhnBaseTestCase {
         user = UserTestUtils.createUser("testUser", oid);
         Org org = user.getOrg();
         ccc = new CreateRepoCommand(org);
-    }
-
-    public void testVerifyUrlInput() {
-
-        // Url must begin with a valid protocol (http, https, ftp, ...),
-        //  end with a valid TLD and contain only valid characters
-
-        // YUM - I N V A L I D
-        invalidUrlInput("", "yum");
-        invalidUrlInput("example.com", "yum");
-        invalidUrlInput("htp://some_test_url.com", "yum");
-        invalidUrlInput("www.example.com", "yum");
-
-        // YUM - V A L I D
-        validUrlInput("file:///srv/mirror", "yum");
-        validUrlInput("http://localhost", "yum");
-        validUrlInput("http://localhost:8080", "yum");
-        validUrlInput("http://localhost/pub", "yum");
-        validUrlInput("http://localhost.com.x/pub", "yum");
-        validUrlInput("http://www.example.com", "yum");
-        validUrlInput("http://www.example.co.uk", "yum");
-        validUrlInput("https://www3.example.com", "yum");
-        validUrlInput("https://test123.com", "yum");
-        validUrlInput("http://example.com", "yum");
-        validUrlInput("http://another-one.de/example", "yum");
-        validUrlInput("https://another-one.de/example%20", "yum");
-        validUrlInput("http://another-one.de/example?v=10", "yum");
-        validUrlInput("ftp://exampleABC.com", "yum");
-
-        // ULN - V A L I D
-        validUrlInput("uln://foo/example_label", "uln");
-        validUrlInput("uln:///example_label", "uln");
-
-        // ULN - I N V A L I D
-        invalidUrlInput("", "uln");
-        invalidUrlInput("example.com", "uln");
-        invalidUrlInput("htp://some_test_url.com", "uln");
-        invalidUrlInput("www.example.com", "uln");
-    }
-
-    private void invalidUrlInput(String url, String type) {
-        // give it an invalid url
-        ccc.setUrl(url);
-        // give it a valid label
-        ccc.setLabel("valid-label-name");
-        // need to specify a type
-        ccc.setType(type);
-        // need to specify MetadataSigned
-        ccc.setMetadataSigned(Boolean.FALSE);
-
-        try {
-            ccc.store();
-            fail("invalid url should have thrown error: " + url);
-        }
-        catch (InvalidRepoUrlException e) {
-            fail("non duplicate url caused error: " + url);
-        }
-        catch (InvalidRepoUrlInputException expected) {
-            // expected
-        }
-        catch (InvalidRepoLabelException e) {
-            fail("valid repo label caused error: " + url);
-        }
-        catch (InvalidRepoTypeException e) {
-            fail("valid repo type caused error: " + url);
-        }
-    }
-
-    private void validUrlInput(String url, String type) {
-        // give it a valid url
-        ccc.setUrl(url);
-        // need to create unique label names.
-        ccc.setLabel("valid-label-name-" + label_count++);
-        // need to specify a type
-        ccc.setType(type);
-        // need to specify MetadataSigned
-        ccc.setMetadataSigned(Boolean.FALSE);
-
-        try {
-            ccc.store();
-        }
-        catch (InvalidRepoUrlException e) {
-            fail("non duplicate url caused error: " + url);
-        }
-        catch (InvalidRepoUrlInputException e) {
-            fail("valid repo url input caused error: " + url);
-        }
-        catch (InvalidRepoLabelException e) {
-            fail("valid repo label caused error: " + url);
-        }
-        catch (InvalidRepoTypeException e) {
-            fail("valid repo type caused error: " + url);
-        }
     }
 
     public void testRepoLabelInput() {
@@ -168,17 +72,8 @@ public class BaseRepoCommandTest extends RhnBaseTestCase {
         try {
             ccc.store();
         }
-        catch (InvalidRepoUrlException e) {
-            fail("non duplicate url caused error");
-        }
-        catch (InvalidRepoUrlInputException e) {
-            fail("valid repo url input caused error");
-        }
         catch (InvalidRepoLabelException e) {
             fail("valid repo label caused error");
-        }
-        catch (InvalidRepoTypeException e) {
-            fail("valid repo type caused error");
         }
     }
 
@@ -195,17 +90,8 @@ public class BaseRepoCommandTest extends RhnBaseTestCase {
         try {
             ccc.store();
         }
-        catch (InvalidRepoUrlException e) {
-            fail("non duplicate url caused error");
-        }
-        catch (InvalidRepoUrlInputException e) {
-            fail("valid repo url input caused error");
-        }
         catch (InvalidRepoLabelException e) {
             // expected
-        }
-        catch (InvalidRepoTypeException e) {
-            fail("valid repo type caused error");
         }
     }
 }
