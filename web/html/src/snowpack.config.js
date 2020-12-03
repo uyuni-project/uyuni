c3442628562e984b312ab373dadeb74509f605b0
Etheryte <karl.tarvas@suse.com>
2020-12-03 12:58:13 +0100

    Stash initial hacks

---
+++ web/html/src/snowpack.config.js
@@ -0,0 +1,41 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+
+/*
+// See https://github.com/snowpackjs/snowpack/blob/a02b504968fa6f050153f7eea7e6a93cf72a5da9/www/_template/guides/routing.md#scenario-2-proxy-api-paths
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createServer({
+  context: ['!/sockjs-node/**'],
+  target: 'https://server.tf.local',
+  ws: true,
+  secure: false
+});
+*/
+
+module.exports = {
+  mount: {
+    '../': "/",
+    // src: "/_dist_",
+  },
+  // plugins: ["@snowpack/plugin-react-refresh"],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    context: ['!/sockjs-node/**'],
+    target: 'https://server.tf.local',
+    ws: true,
+    secure: false
+  },
+  alias: {
+    /* ... */
+  },
+};
