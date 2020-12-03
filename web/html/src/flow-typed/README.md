08a5e533917cef02ad939feb7e3550b2211c5bce
Etheryte <karl.tarvas@suse.com>
2020-12-03 18:18:38 +0100

    Remove flow-typed

--- web/html/src/flow-typed/README.md
+++
@@ -1,3 +0,0 @@
-### Community libraries types for libraries without flow
-
-Check how to add new library types definitions here https://github.com/flow-typed/flow-typed
--- web/html/src/flow-typed/globals.js
+++
@@ -1,4 +0,0 @@
-
-declare function t(msg: string, ...args?: Array<any>): string;
-declare function jQuery(...args?: any): any;
-declare var moment: Class<moment$Moment>;
