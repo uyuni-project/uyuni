81ac27d35415059ba44eb3eb8fc4896c44369e42
Etheryte <karl.tarvas@suse.com>
2020-12-17 12:45:26 +0200

    Swap to node

---
+++ web/html/src/build/migrate-ts/index.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+const args = require("./args");
+
+(async () => {
+  const { inputs, isVerbose } = args.parse(process.argv);
+
+  const cwd = process.cwd();
+  // Make all paths absolute to avoid any issues
+  const inputPaths = inputs.map(item => path.resolve(cwd, item));
+  if (isVerbose) {
+    console.log(`got inputs:\n${inputPaths.join("\n")}`);
+  }
+
+  console.log('migrate flow');
+  try {
+      // await exec(`yarn flow-to-ts ${inputPaths}`);
+  } catch(error) {
+
+  }
+})();
