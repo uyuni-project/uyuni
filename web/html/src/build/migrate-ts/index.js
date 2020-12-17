a7656d87beb1961d384195dca42ab3ba6e9a34a8
Etheryte <karl.tarvas@suse.com>
2020-12-17 13:53:25 +0200

    Further work on scripting

--- web/html/src/build/migrate-ts/index.js
+++ web/html/src/build/migrate-ts/index.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const path = require("path");
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
@@ -6,13 +7,17 @@ const args = require("./args");
 
 (async () => {
   try {
-    const { inputs, isVerbose } = args.parse(process.argv);
+    const { inputs: rawInputs, isVerbose } = args.parse(process.argv);
 
     async function execAndLog(...args) {
       const result = await exec(...args);
       const { stdout, stderr } = result;
       if (isVerbose) {
-        console.log(stdout);
+        if ((stdout || "").trim()) {
+          console.log(stdout);
+        } else {
+          console.log("(no output)");
+        }
       }
       if (stderr) {
         console.error("step had errors:");
@@ -24,33 +29,70 @@ const args = require("./args");
 
     const cwd = process.cwd();
     // Make all paths absolute to avoid any issues
-    const inputPaths = inputs.map(item => path.resolve(cwd, item));
+    const inputPaths = rawInputs.map(item => path.resolve(cwd, item));
+    const inputs = inputPaths.join(" ");
     if (isVerbose) {
       console.log(`got inputs:\n${inputPaths.join("\n")}`);
     }
 
     console.log("migrate flow");
-    {
-      const { stdout, stderr } = await execAndLog(`yarn flow-to-ts ${inputPaths.join(" ")}`);
-    }
+    await execAndLog(`yarn flow-to-ts ${inputs}`);
 
-    console.log("untyped annotated files");
-    {
-      // This find untyped annotated files across the whole project, not only the inputs we have
-      const { stdout, stderr } = await execAndLog(
-        `(yarn tsc 2>&1 || true) | grep 'can only be used in TypeScript files' | sed -e 's/\.js.*/.js/' | uniq`
-      );
-      const paths = stdout
-        .split("\n")
-        .filter(item => !!item.trim())
-        // Tsc gives out relative paths, make them absolute
-        .map(item => path.resolve(cwd, item))
-        // Trim the result to our input paths
-        .filter(item => inputPaths.indexOf(item) !== -1);
-      if (isVerbose) {
-        console.log(`got untyped annotated\n:${paths.join("\n")}`);
+    // Find which files we failed to migrate and rename them so other toolsets can work with them
+    console.log("finding orphans");
+    for (const item of inputPaths) {
+      try {
+        // If the file exists, it needs to be renamed
+        await fs.promises.access(item);
+        // Blindly move to tsx, the content will need to be manually inspected anyway
+        const newName = item.replace(/.jsx?$/, ".tsx");
+        await fs.promises.rename(item, newName);
+        console.warn(`needs manual review: ${newName}`);
+      } catch {
+        // File was already migrated to a new name, do nothing
       }
     }
+
+    // At this point, our input paths are no longer correct since they're js or jsx files, but we have output ts or tsx files
+    const tsPaths = [];
+    console.log("finding outputs");
+    for (const item of inputPaths) {
+      try {
+        // This is the most likely output
+        const tsxPath = item.replace(/.jsx?$/, ".tsx");
+        await fs.promises.access(tsxPath);
+        tsPaths.push(tsxPath);
+      } catch {
+        try {
+          const tsPath = item.replace(/.jsx?$/, ".ts");
+          await fs.promises.access(tsPath);
+          tsPaths.push(tsPath);
+        } catch (error) {
+          console.error(`failed to find output for: ${item}`);
+        }
+      }
+    }
+    if (isVerbose) {
+      console.log(`got ts inputs:\n${tsPaths.join("\n")}`);
+      console.log(tsPaths.length === inputPaths.length ? "lengths match" : "lengths do not match");
+    }
+    const tsInputs = tsPaths.join(" ");
+
+    console.log("migrate object to any");
+
+    await execAndLog(`sed -i '' -e 's/: Object\\([^\\.]\\)/: any\\1/g' ${tsInputs}`);
+
+    console.log("migrate untyped use state");
+    await execAndLog(`sed -i '' -e 's/React.useState(undefined)/React.useState<any>(undefined)/' ${tsInputs}`);
+
+    console.log("migrate React.ReactNode to JSX.Element");
+    await execAndLog(`sed -i '' -e 's/=> React.ReactNode/=> JSX.Element/' ${tsInputs}`);
+    await execAndLog(`sed -i '' -e 's/: React.ReactNode {/: JSX.Element {/' ${tsInputs}`);
+
+    console.log("migrate object array to any array");
+    await execAndLog(`sed -i '' -e 's/Array<Object>/Array<any>/' ${tsInputs}`);
+
+    console.log('\ndone with automations, try running `yarn tsc` to find any remaining issues\n');
   } catch (error) {
     console.error(error);
     process.exit(1);
