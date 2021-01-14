76b83e7e06d09ece8bc68f6a3a600f25cb322909
Etheryte <karl.tarvas@suse.com>
2021-01-14 19:48:10 +0200

    Add formatting to migrations

--- web/html/src/utils/data-providers/types.ts
+++ web/html/src/utils/data-providers/types.ts
@@ -1,6 +1,6 @@
 export type PagedData = {
-    items: Array<any>;
-    total: number;
+  items: Array<any>;
+  total: number;
 };
 
 export type Comparator = (a: any, b: any, key: string, direction: number) => number;
