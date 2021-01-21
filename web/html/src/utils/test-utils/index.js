233b7eaf165602b80ba870546bb852dc6b61dcdb
Etheryte <karl.tarvas@suse.com>
2021-01-21 16:22:05 +0100

    Add @testing-library/user-event JS test dependency

--- web/html/src/utils/test-utils/index.js
+++ web/html/src/utils/test-utils/index.js
@@ -1,8 +1,28 @@
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 
+// See https://testing-library.com/docs/ecosystem-user-event/#api
+import userEvent from "@testing-library/user-event";
+
 // Reexport everything so we can get all of our utilities from a single location
+// See https://testing-library.com/docs/react-testing-library/api/
 export * from "@testing-library/react";
+
+// @testing-library/user-event has messed up exports, just manually reexport everything
+export const {
+  click,
+  dblClick,
+  type,
+  upload,
+  clear,
+  selectOptions,
+  deselectOptions,
+  tab,
+  hover,
+  unhover,
+  paste,
+  specialChars,
+} = userEvent;
 
 const server = setupServer();
 
