e7df1f7871b8f6b78648fc8c2a878bca110f0b5e
Etheryte <karl.tarvas@suse.com>
2021-01-25 16:38:05 +0200

    Migrate manager/errors and add missing alias

--- web/html/src/build/webpack.alias.js
+++ web/html/src/build/webpack.alias.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   components: path.resolve(__dirname, "../components/"),
   core: path.resolve(__dirname, "../core/"),
+  manager: path.resolve(__dirname, "../manager/"),
   utils: path.resolve(__dirname, "../utils/"),
   jquery: path.resolve(__dirname, "./inject.global.jquery.js"),
   // Support HRM with hooks, see https://github.com/gaearon/react-hot-loader#hot-loaderreact-dom
