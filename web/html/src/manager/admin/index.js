b78e7269969ed9d13cb6d3869de4d4fdf6104557
Etheryte <karl.tarvas@suse.com>
2021-01-25 15:44:45 +0200

    Migrate login

--- web/html/src/manager/admin/index.js
+++ web/html/src/manager/admin/index.js
@@ -1,5 +1,5 @@
 export default {
-  'admin/config/monitoring': () => import('./config/monitoring-renderer'),
+  'admin/config/monitoring': () => import('./config/monitoring.renderer'),
   'admin/setup/products/products': () => import('./setup/products/products'),
   'admin/task-engine-status/taskotop': () => import('./task-engine-status/taskotop')
 }
--- web/html/src/manager/login/login-footer.tsx
+++ web/html/src/manager/login/login-footer.tsx
@@ -1,25 +1,22 @@
-// @flow
-import * as React from 'react';
-import styles from './login.css';
+import * as React from "react";
+import styles from "./login.css";
 
 type Props = {
-  productName: string,
-  webVersion: string,
-  customFooter: string,
-}
+  productName: string;
+  webVersion: string;
+  customFooter: React.ReactNode;
+};
 
 const LoginFooter = (props: Props) => (
   <footer className={styles.footer_wrapper}>
     <div className={styles.footer_fixed_bottom}>
       <div className="wrapper wrap">
         <div className="footer-copyright">
-          <a href="/rhn/help/Copyright.do">Copyright Notice</a>{' '}
+          <a href="/rhn/help/Copyright.do">Copyright Notice</a>{" "}
         </div>
         <div className={`footer-release ${styles.footer_release_container}`}>
           {`${props.productName} release `}
-          <a href="/rhn/help/dispatcher/release_notes">
-            {props.webVersion}
-          </a>
+          <a href="/rhn/help/dispatcher/release_notes">{props.webVersion}</a>
         </div>
         <div>{props.customFooter}</div>
       </div>
