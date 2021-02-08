1898a0a805de44c3204dd804cc0f020c7d866734
Etheryte <karl.tarvas@suse.com>
2021-02-08 11:50:10 +0100

    Migrate validation

---
+++ web/html/src/components/validation.ts
@@ -0,0 +1,12 @@
+// https://github.com/chriso/validator.js
+
+const f = fn => (...args) => str => fn(str, ...args);
+const validations: Record<string, any> = {};
+
+Object.keys(validator).forEach(v => {
+  if (typeof validator[v] === "function") {
+    validations[v] = f(validator[v]);
+  }
+});
+
+export default validations;
