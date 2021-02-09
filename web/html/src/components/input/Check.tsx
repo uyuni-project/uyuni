869ca15302b2b21c965e590cb27084308ff006ee
Etheryte <karl.tarvas@suse.com>
2021-02-09 16:59:36 +0100

    Share common props

--- web/html/src/components/input/Check.tsx
+++ web/html/src/components/input/Check.tsx
@@ -1,47 +1,19 @@
 import * as React from "react";
-import { InputBase } from "./InputBase";
+import { InputBase, InputBaseProps } from "./InputBase";
 import { FormContext } from "./Form";
 
-type Props = {
+type Props = InputBaseProps & {
   /** CSS class for the <input> element */
   inputClass?: string;
 
   /** name of the field to map in the form model */
   name: string;
-
-  /** Default value if none is set */
-  defaultValue?: string;
-
-  /** Label to display for the field */
-  label?: string;
-
-  /** Hint string to display */
-  hint?: string;
-
-  /** CSS class to use for the label */
-  labelClass?: string;
-
-  /** CSS class to use for the <div> element wrapping the field input part */
-  divClass?: string;
-
-  /** Indicates whether the field is required in the form */
-  required?: boolean;
-
-  /** Indicates whether the field is disabled */
-  disabled?: boolean;
-
-  /** Hint to display on a validation error */
-  invalidHint?: string;
-
-  /** Function to call when the data model needs to be changed.
-   *  Takes a name and a value parameter.
-   */
-  onChange?: (name: string, value: string) => void;
 };
 
 export function Check(props: Props) {
   const { label, inputClass, ...propsToPass } = props;
   const formContext = React.useContext(FormContext);
+
   return (
     <InputBase {...propsToPass}>
       {({ setValue, onBlur }) => {
