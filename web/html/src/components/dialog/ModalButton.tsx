1f70ddc22453f0a6fec7428aea5a763f4bf1da42
Etheryte <karl.tarvas@suse.com>
2021-02-09 17:55:37 +0100

    Migrate modal button & link

---
+++ web/html/src/components/dialog/ModalButton.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { Button } from "../buttons";
+import { showDialog } from "./util";
+
+// TODO: Extend Button's props here once that branch gets merged
+type Props = {
+  target: string;
+  id?: string;
+  className?: string;
+  title?: string;
+  text: React.ReactNode;
+  icon?: string;
+  disabled?: boolean;
+  item?: any;
+  onClick: (...args: any[]) => any;
+};
+
+/**
+ * Button to launch a modal dialog
+ */
+export function ModalButton(props: Props) {
+  return (
+    <Button
+      id={props.id}
+      className={props.className}
+      title={props.title}
+      text={props.text}
+      icon={props.icon}
+      disabled={props.disabled}
+      handler={() => {
+        props.onClick?.(props.item);
+        showDialog(props.target);
+      }}
+    />
+  );
+}
