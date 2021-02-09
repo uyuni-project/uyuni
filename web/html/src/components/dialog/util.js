cabc9929980e61f1d12cb4ecf953598461135ee6
Etheryte <karl.tarvas@suse.com>
2021-02-09 17:56:18 +0100

    Migrate util

--- web/html/src/components/dialog/util.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-export function showDialog(dialogId) {
-  jQuery('#' + dialogId).modal('show');
-}
-
-export function hideDialog(dialogId) {
-  jQuery('#' + dialogId).modal('hide');
-}
