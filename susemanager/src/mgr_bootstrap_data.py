ed3dd2587bba851b01102a1a1cc6ec5ee7260cd9
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2021-02-11 14:05:37 +0100

    add bootstrap data for SLE 15 SP3 products
    fixes https://github.com/SUSE/spacewalk/issues/13819
    fixes bsc#1182008
    
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- susemanager/src/mgr_bootstrap_data.py
+++ susemanager/src/mgr_bootstrap_data.py
@@ -938,6 +938,22 @@ DATA = {
         'PDID' : [1946, 1712], 'PKGLIST' : PKGLIST15_TRAD + PKGLIST15_SALT + PKGLIST15_X86_ARM,
         'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/2/bootstrap/'
     },
+    'SLE-15-SP3-aarch64' : {
+        'PDID' : [2142, 1709], 'PKGLIST' : PKGLIST15_TRAD + PKGLIST15_SALT + PKGLIST15_X86_ARM,
+        'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/3/bootstrap/'
+    },
+    'SLE-15-SP3-ppc64le' : {
+        'PDID' : [2143, 1710], 'PKGLIST' : PKGLIST15_TRAD + PKGLIST15_SALT + PKGLIST15_PPC,
+        'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/3/bootstrap/'
+    },
+    'SLE-15-SP3-s390x' : {
+        'PDID' : [2144, 1711], 'PKGLIST' : PKGLIST15_TRAD + PKGLIST15_SALT + PKGLIST15_Z,
+        'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/3/bootstrap/'
+    },
+    'SLE-15-SP3-x86_64' : {
+        'PDID' : [2145, 1712], 'PKGLIST' : PKGLIST15_TRAD + PKGLIST15_SALT + PKGLIST15_X86_ARM,
+        'DEST' : '/srv/www/htdocs/pub/repositories/sle/15/3/bootstrap/'
+    },
     'openSUSE-Leap-42.3-x86_64' : {
         'BASECHANNEL' : 'opensuse_leap42_3-x86_64', 'PKGLIST' : PKGLIST12 + ONLYOPENSUSE42 + ENHANCE12SP1 + PKGLIST12_X86_ARM,
         'DEST' : '/srv/www/htdocs/pub/repositories/opensuse/42/3/bootstrap/'
