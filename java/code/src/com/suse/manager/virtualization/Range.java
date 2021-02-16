a4f8ddaa69205e6dec6c9495353da4f8272f5336
Cédric Bosdonnat <cbosdonnat@suse.com>
2021-02-16 14:49:17 +0100

    Add virtual network definition classes
    Use a common object definition for the communication with the UI and the
    internal data for virtual network creation.

---
+++ java/code/src/com/suse/manager/virtualization/Range.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2021 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.suse.manager.virtualization;
+
+public class Range<T> {
+    private T start;
+    private T end;
+
+    /**
+     * @return start value of the range
+     */
+    public T getStart() {
+        return start;
+    }
+
+    /**
+     * @param startIn start value of the range
+     */
+    public void setStart(T startIn) {
+        start = startIn;
+    }
+
+    /**
+     * @return end value of the range
+     */
+    public T getEnd() {
+        return end;
+    }
+
+    /**
+     * @param endIn end value of the range
+     */
+    public void setEnd(T endIn) {
+        end = endIn;
+    }
+}
