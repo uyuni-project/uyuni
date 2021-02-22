a5a44929418cddcadc09d1651b414685e6e3e1a3
Etheryte <karl.tarvas@suse.com>
2021-02-22 16:07:28 +0100

    Migrate manager/admin/setup/products + network layer updates

--- web/html/src/manager/admin/setup/products/products.test.ts
+++ web/html/src/manager/admin/setup/products/products.test.ts
@@ -1,37 +1,42 @@
-import {searchCriteriaInExtension} from "./products.utils";
+import { searchCriteriaInExtension } from "./products.utils";
 
 const extension = {
   label: "suse base 1 2 asd",
-  channels: [{summary: 'suse base 1 channel 1  '}, {summary: 'suse base 1 channel 2  '}],
+  channels: [{ summary: "suse base 1 channel 1  " }, { summary: "suse base 1 channel 2  " }],
   extensions: [
     {
       label: "suse extension 1 child 1",
-      channels: [{summary: 'suse extension 1 child 1 channel 1'}, {summary: 'suse extension 1 child 1 channel 2'}],
-      extensions: []
+      channels: [{ summary: "suse extension 1 child 1 channel 1" }, { summary: "suse extension 1 child 1 channel 2" }],
+      extensions: [],
     },
     {
       label: "suse extension 1 child 2",
-      channels: [{summary: 'suse extension 1 child 2 channel 1'}, {summary: 'suse extension 1 child 2 channel 2'}],
+      channels: [{ summary: "suse extension 1 child 2 channel 1" }, { summary: "suse extension 1 child 2 channel 2" }],
       extensions: [
         {
           label: "suse extension 2 child 1",
-          channels: [{summary: 'suse extension 2 child 1 channel 1'}, {summary: 'suse extension 2 child 1 channel 2'}],
+          channels: [
+            { summary: "suse extension 2 child 1 channel 1" },
+            { summary: "suse extension 2 child 1 channel 2" },
+          ],
           extensions: [
             {
               label: "suse extension 3 child 1",
-              channels: [{summary: 'suse extension 3 child 1 channel 1'}, {summary: 'suse extension 3 child 1 channel 2'}],
-              extensions: []
-            }
-          ]
-        }
-      ]
-    }
-  ]
+              channels: [
+                { summary: "suse extension 3 child 1 channel 1" },
+                { summary: "suse extension 3 child 1 channel 2" },
+              ],
+              extensions: [],
+            },
+          ],
+        },
+      ],
+    },
+  ],
 };
 
-describe('Testing searchCriteriaInExtension', () => {
-  test('check criteria in base extension label and channels', () => {
-
+describe("Testing searchCriteriaInExtension", () => {
+  test("check criteria in base extension label and channels", () => {
     const criteriaLabel = "base 1";
     const criteriaChannel1 = "base 1 channel 1";
     const criteriaChannel2 = "base 1 channel 2";
@@ -41,10 +46,9 @@ describe('Testing searchCriteriaInExtension', () => {
     expect(searchCriteriaInExtension(extension, criteriaChannel1)).toBeTruthy();
     expect(searchCriteriaInExtension(extension, criteriaChannel2)).toBeTruthy();
     expect(searchCriteriaInExtension(extension, criteriaWrong)).toBeFalsy();
-  })
+  });
 
-  test('check criteria in first level extension label and channels', () => {
-
+  test("check criteria in first level extension label and channels", () => {
     const criteriaLabel = "extension 1 child 1";
     const criteriaChannel1 = "extension 1 child 1 channel 1";
     const criteriaChannel2 = "extension 1 child 1 channel 1";
@@ -54,10 +58,9 @@ describe('Testing searchCriteriaInExtension', () => {
     expect(searchCriteriaInExtension(extension, criteriaChannel1)).toBeTruthy();
     expect(searchCriteriaInExtension(extension, criteriaChannel2)).toBeTruthy();
     expect(searchCriteriaInExtension(extension, criteriaWrong)).toBeFalsy();
-  })
+  });
 
-  test('check criteria in second level extension label and channels', () => {
-
+  test("check criteria in second level extension label and channels", () => {
     const criteriaLabel = "suse extension 2 child 1";
     const criteriaChannel1 = "extension 2 child 1 channel 1";
     const criteriaChannel2 = "suse extension 2 child 1 channel 2";
@@ -67,10 +70,9 @@ describe('Testing searchCriteriaInExtension', () => {
     expect(searchCriteriaInExtension(extension, criteriaChannel1)).toBeTruthy();
     expect(searchCriteriaInExtension(extension, criteriaChannel2)).toBeTruthy();
     expect(searchCriteriaInExtension(extension, criteriaWrong)).toBeFalsy();
-  })
+  });
 
-  test('check criteria in third level extension label and channels', () => {
-
+  test("check criteria in third level extension label and channels", () => {
     const criteriaLabel = "extension 3";
     const criteriaChannel1 = "extension 3";
     const criteriaChannel2 = "extension 3";
@@ -80,5 +82,5 @@ describe('Testing searchCriteriaInExtension', () => {
     expect(searchCriteriaInExtension(extension, criteriaChannel1)).toBeTruthy();
     expect(searchCriteriaInExtension(extension, criteriaChannel2)).toBeTruthy();
     expect(searchCriteriaInExtension(extension, criteriaWrong)).toBeFalsy();
-  })
-})
+  });
+});
