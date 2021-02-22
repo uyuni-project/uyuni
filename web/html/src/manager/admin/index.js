54da8fa6f21ec1a0d0bc1b1c7b27156543b76582
Etheryte <karl.tarvas@suse.com>
2021-02-22 16:14:51 +0100

    Migrate remaining manager/admin

--- web/html/src/manager/admin/index.js
+++
@@ -1,5 +0,0 @@
-export default {
-  'admin/config/monitoring': () => import('./config/monitoring.renderer'),
-  'admin/setup/products/products': () => import('./setup/products/products'),
-  'admin/task-engine-status/taskotop': () => import('./task-engine-status/taskotop')
-}
