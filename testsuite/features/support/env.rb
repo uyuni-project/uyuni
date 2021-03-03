41ac49467e92a08913de4430100835a94164b3a1
Tino Calancha <ccalancha@suse.com>
2021-03-03 15:00:33 +0100

    Testsuite: Fix check for env. vars set by terracumber
    Terracumber sets them as the strings 'true' or 'false'.
    
    - testsuite/features/support/env.rb:
    Check explicitely for either 'true' or 'false'.
    Delete out-of-date comment.

--- testsuite/features/support/env.rb
+++ testsuite/features/support/env.rb
@@ -18,12 +18,9 @@ require 'multi_test'
 
 server = ENV['SERVER']
 $debug_mode = true if ENV['DEBUG']
-$long_tests_enabled = true if ENV['LONG_TESTS']
+$long_tests_enabled = true if ENV['LONG_TESTS'] == 'true'
 puts "Executing long running tests" if $long_tests_enabled
-$service_pack_migration_enabled = true if ENV['SERVICE_PACK_MIGRATION']
-# To disable SP migration, comment the previous line, uncoment the next one
-# and adjust testsuite/run_sets/init_clients.yml
-# $service_pack_migration_enabled = false
+$service_pack_migration_enabled = true if ENV['SERVICE_PACK_MIGRATION'] == 'true'
 puts "Executing service pack migrations" if $service_pack_migration_enabled
 
 # maximal wait before giving up
