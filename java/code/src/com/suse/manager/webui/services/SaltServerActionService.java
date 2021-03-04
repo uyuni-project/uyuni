9e86c1845e4454298b2eda36db527139928dab29
Cédric Bosdonnat <cbosdonnat@suse.com>
2021-03-04 09:51:12 +0100

    Add Virtual Network edit action

--- java/code/src/com/suse/manager/webui/services/SaltServerActionService.java
+++ java/code/src/com/suse/manager/webui/services/SaltServerActionService.java
@@ -2150,6 +2150,7 @@ public class SaltServerActionService {
                                 }
                                 pillar.put("dns", dns);
                             });
+                            pillar.put("action_type", def.getUuid().isPresent() ? "defined" : "running");
 
                             return State.apply(
                                     Collections.singletonList("virt.network-create"),
