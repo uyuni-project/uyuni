d5f438ba473abaf18951cce7182b71c9b55ef544
Frantisek Kobzik <fkobzik@suse.de>
2021-03-08 11:25:58 +0100

    DB: View for retracted packages (both installed and not installed) in server channels

---
+++ schema/spacewalk/common/views/suseServerChannelsRetractedPackagesView.sql
@@ -0,0 +1,17 @@
+--
+-- Returns the retracted packages (no matter if installed or not installed)
+-- from channels of given server.
+--
+
+CREATE OR REPLACE VIEW suseServerChannelsRetractedPackagesView AS
+ SELECT DISTINCT p.id AS pid,
+    s.id AS sid
+   FROM rhnserver s
+     JOIN rhnserverchannel sc ON s.id = sc.server_id
+     JOIN rhnchannel c ON c.id = sc.channel_id
+     JOIN rhnchannelerrata ce ON ce.channel_id = c.id
+     JOIN rhnerrata e ON e.id = ce.errata_id
+     JOIN rhnerratapackage ep ON ep.errata_id = e.id
+     JOIN rhnpackage p ON p.id = ep.package_id
+  WHERE e.advisory_status::text = 'retracted'::text;
+
