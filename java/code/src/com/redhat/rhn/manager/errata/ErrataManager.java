d5de7779c60dcb070ac047919fee4cb3529d6786
Kevin Walter <kwalter@suse.de>
2021-03-08 13:55:03 +0100

    Backend: Fail scheduleApplyErrata on retracted errata

--- java/code/src/com/redhat/rhn/manager/errata/ErrataManager.java
+++ java/code/src/com/redhat/rhn/manager/errata/ErrataManager.java
@@ -41,6 +41,7 @@ import com.redhat.rhn.domain.action.ActionChainFactory;
 import com.redhat.rhn.domain.action.errata.ErrataAction;
 import com.redhat.rhn.domain.channel.Channel;
 import com.redhat.rhn.domain.channel.ChannelFactory;
+import com.redhat.rhn.domain.errata.AdvisoryStatus;
 import com.redhat.rhn.domain.errata.Bug;
 import com.redhat.rhn.domain.errata.ClonedErrata;
 import com.redhat.rhn.domain.errata.Errata;
@@ -68,6 +69,7 @@ import com.redhat.rhn.frontend.events.NewCloneErrataEvent;
 import com.redhat.rhn.frontend.listview.PageControl;
 import com.redhat.rhn.frontend.xmlrpc.InvalidErrataException;
 import com.redhat.rhn.frontend.xmlrpc.InvalidParameterException;
+import com.redhat.rhn.frontend.xmlrpc.RetractedErrataException;
 import com.redhat.rhn.manager.BaseManager;
 import com.redhat.rhn.manager.action.ActionManager;
 import com.redhat.rhn.manager.action.MinionActionManager;
@@ -1904,6 +1906,12 @@ public class ErrataManager extends BaseManager {
 
         // compute id to errata map
         List<Errata> errataList = ErrataManager.lookupErrataByIds(errataIds, user);
+        List<Errata> retracted = errataList.stream()
+                .filter(e -> e.getAdvisoryStatus() == AdvisoryStatus.RETRACTED)
+                .collect(toList());
+        if (!retracted.isEmpty()) {
+            throw new RetractedErrataException(retracted.stream().map(Errata::getId).collect(toList()));
+        }
 
         Map<Long, Errata> errataMap = errataList.stream()
             .collect(toMap(
