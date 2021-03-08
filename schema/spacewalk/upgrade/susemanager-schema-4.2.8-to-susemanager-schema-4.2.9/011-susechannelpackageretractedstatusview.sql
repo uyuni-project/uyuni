ea6a92add719bbdaf08ea951a1546100db0370fa
Frantisek Kobzik <fkobzik@suse.de>
2021-03-08 11:25:01 +0100

    DB: View for channel - package associations with retracted status

---
+++ schema/spacewalk/upgrade/susemanager-schema-4.2.8-to-susemanager-schema-4.2.9/011-susechannelpackageretractedstatusview.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE VIEW suseChannelPackageRetractedStatusView AS
+ SELECT
+    cp.channel_id,
+    cp.package_id,
+    CASE
+        WHEN (EXISTS ( SELECT 1
+           FROM rhnchannelerrata ce
+             JOIN rhnerrata e ON ce.errata_id = e.id
+             JOIN rhnerratapackage ep ON ep.errata_id = e.id
+          WHERE e.advisory_status::text = 'retracted'::text AND ce.channel_id = cp.channel_id AND ep.package_id = cp.package_id)) THEN 1
+        ELSE 0
+    END AS is_retracted
+   FROM rhnchannelpackage cp;
+
