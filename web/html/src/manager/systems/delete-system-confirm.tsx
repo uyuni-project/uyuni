00186b655abf824bc6c70b7e571eec22f9fb9db4
Etheryte <karl.tarvas@suse.com>
2021-03-11 14:22:47 +0100

    Migrate delete system

---
+++ web/html/src/manager/systems/delete-system-confirm.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { Utils } from "utils/functions";
+import { DeleteSystem } from "./delete-system";
+import SpaRenderer from "core/spa/spa-renderer";
+
+// See java/code/webapp/WEB-INF/pages/systems/sdc/delete_confirm.jsp
+declare global {
+  interface Window {
+    getServerIdToDelete: () => any;
+  }
+}
+
+export const renderer = (id: string) =>
+  SpaRenderer.renderNavigationReact(
+    <DeleteSystem
+      serverId={window.getServerIdToDelete()}
+      onDeleteSuccess={() => Utils.urlBounce("/rhn/systems/Overview.do")}
+      buttonText={t("Delete Profile")}
+      buttonClass="btn-danger"
+    />,
+    document.getElementById(id)
+  );
