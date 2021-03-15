ad964e6dce430ca54e1698a03113cffa75c23328
Frantisek Kobzik <fkobzik@suse.de>
2021-03-15 09:37:02 +0100

    Remove unused imports

--- java/code/src/com/redhat/rhn/frontend/dto/ErrataOverview.java
+++ java/code/src/com/redhat/rhn/frontend/dto/ErrataOverview.java
@@ -22,9 +22,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * ErrataOverview
