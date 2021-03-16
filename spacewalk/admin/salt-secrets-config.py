657f80b16129da1faca7e622cf884d1d863398bd
Ondrej Holecek <oholecek@suse.com>
2021-03-16 11:18:53 +0100

    Add external SQL pillar for salt

--- spacewalk/admin/salt-secrets-config.py
+++ spacewalk/admin/salt-secrets-config.py
@@ -38,6 +38,16 @@ mgr_events_config = {
     }]
 }
 
+salt_postgres_pillar = {
+    "postgres": {
+         "host": CFG.db_host,
+         "port": CFG.db_port,
+         "db":   CFG.db_name,
+         "user": CFG.db_user,
+         "pass": CFG.db_password
+    }
+}
+
 initCFG('server')
 
 secret_hash = hashlib.sha512(CFG.secret_key.encode('ascii')).hexdigest()
@@ -46,6 +56,11 @@ os.umask(0o66)
 
 with open("/etc/salt/master.d/susemanager_engine.conf", "w") as f:
     f.write(yaml.safe_dump(mgr_events_config, default_flow_style=False, allow_unicode=True))
+    os.fchown(f.fileno(), pwd.getpwnam("salt").pw_uid, grp.getgrnam("salt").gr_gid)
+    os.fchmod(f.fileno(), 0o640)
+
+with open("/etc/salt/master.d/susemanager_db.conf", "w") as f:
+    f.write(yaml.safe_dump(salt_postgres_pillar, default_flow_style=False, allow_unicode=True))
     os.fchown(f.fileno(), pwd.getpwnam("salt").pw_uid, grp.getgrnam("salt").gr_gid)
     os.fchmod(f.fileno(), 0o640)
 
