97cfce6b85e2f688c27ba9c7a57783d19c8e8ad9
Oscar Barrios <obarrios@suse.com>
2021-03-18 09:34:24 +0100

    Let's include QA Squad as reviewers for any changes inside the testsuite folder

--- .github/scripts/needs_qa_squad_review.rb
+++ .github/scripts/needs_qa_squad_review.rb
@@ -3,7 +3,7 @@ require 'json'
 
 # if true we exit with 0, so the PR needs QA Squad as a reviewer
 def needs_qa_squad_review?(files)
-  files.reject! { |f| f.include? '.feature' }
+  # files.reject! { |f| f.include? '.feature' }
   files.any? { |f| f.include? 'testsuite' } 
 end
 
