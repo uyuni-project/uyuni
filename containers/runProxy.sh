07dd6bb9ba1909357fd13423de2e6540775044bd
Michael Calmer <mc@suse.de>
2021-03-22 14:36:14 +0100

    container start script

--- containers/runProxy.sh
+++ containers/runProxy.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 set -x
+CURDIR=$(dirname $(realpath $0))
 
 IMAGE=proxy
+CONFIG_DIR="$CURDIR/proxy-config"
+
 
 docker run --rm=true -ti \
 	-e UYUNI_MASTER='suma-refhead-srv.mgr.suse.de' \
 	-e UYUNI_ACTIVATON_KEY='1-proxy' \
+	-e UYUNI_PROXY_MINION_ID='mc-proxy.suse.de' \
+	-e UYUNI_MACHINE_ID='208923e4fc9e46889bd39c4861f0ff0e' \
+	-v $CONFIG_DIR:/config \
 	--name uyuni_proxy \
         $IMAGE
 
