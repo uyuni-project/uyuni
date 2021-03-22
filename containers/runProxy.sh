7d5a72b011713afb48eeb23a7ff6d9dd6da5c893
Johannes Renner <jrenner@suse.com>
2021-03-22 15:21:53 +0100

    Fix typo (UYUNI_ACTIVATION_KEY)

--- containers/runProxy.sh
+++ containers/runProxy.sh
@@ -9,7 +9,7 @@ CONFIG_DIR="$CURDIR/proxy-config"
 
 docker run --rm=true -ti \
 	-e UYUNI_MASTER='suma-refhead-srv.mgr.suse.de' \
-	-e UYUNI_ACTIVATON_KEY='1-proxy' \
+	-e UYUNI_ACTIVATION_KEY='1-proxy' \
 	-e UYUNI_PROXY_MINION_ID='mc-proxy.suse.de' \
 	-e UYUNI_MACHINE_ID='208923e4fc9e46889bd39c4861f0ff0e' \
 	-v $CONFIG_DIR:/config \
