e651e7b3bdf4800ae0a94ae7770ba9921996a3b3
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2021-03-22 17:27:42 +0100

    Get obs user and password from config file
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- susemanager-utils/testing/automation/obs-project.py
+++ susemanager-utils/testing/automation/obs-project.py
@@ -5,15 +5,16 @@ import sys
 import urllib.request
 import xml.etree.ElementTree as ET
 import datetime
+import configparser
 
 parser = argparse.ArgumentParser(description="This utility helps you manage an obs project for a Pull Request")
 parser.add_argument('--api', help="Build Service API, defaults to https://api.opensuse.org", default="https://api.opensuse.org")
 parser.add_argument('--project', help="Project from which to \"branch\" from, defaults to systemsmanagement:Uyuni:Master", default="systemsmanagement:Uyuni:Master")
 parser.add_argument('--prproject', help="Project to branch to \"branch\", defaults to systemsmanagement:Uyuni:Master:PR", default="systemsmanagement:Uyuni:Master:PR")
 parser.add_argument('pullnumber', help="Pull Request number, for example 1")
-parser.add_argument('--username', help="Username in the build service, defaults to OBS_USER environment variable", default="")
-parser.add_argument('--password', help="Password in the build service, defaults to OBS_PASSWORD environment variable", default="")
+parser.add_argument('--configfile', help="Config file where username and password are store, by default ~/.oscrc", default="~/.oscrc")
 parser.add_argument('--setmaintainer', help="Set maintainer", default="")
+
 
 args = parser.parse_args()
 api = args.api
@@ -21,19 +22,26 @@ project = args.project
 pr_project = args.prproject
 pull_number = args.pullnumber
 auth_user = args.username
-if (auth_user == ""):
-    if ("OBS_USER" not in os.environ.keys()):
-        print("Please set OBS_USER environment variable or pass it as a parameter")
-        sys.exit(-1)
-    auth_user = os.environ["OBS_USER"]
-auth_passwd = args.password
-if (auth_passwd == ""):
-    if ("OBS_PASSWORD" not in os.environ.keys()):
-        print("Please set OBS_PASSWORD environment variable or pass it as a parameter")
-        sys.exit(-1)
-    auth_passwd = os.environ["OBS_PASSWORD"]
 maintainer=args.setmaintainer
 pr_project = pr_project + ":" + pull_number
+config_file = args.configfile
+
+if (not os.path.exists(config_file)):
+    print("ERROR: config file {} not found".format(config_file))
+    sys.exit(-1)
+
+try:
+    config.read(config_file)
+except IOError as e:
+    print("ERROR: Can't read config file ".format(e))
+    sys.exit(-1)
+
+auth_user = config[api]["user"]
+auth_passwd = config[api]["pass"]
+
+if (auth_user == "" || auth_passwd == ""):
+    print("ERROR: could not find user or password in config file")
+    sys.exit(-1)
 
 print("DEBUG: getting api version for debugging purposes")
 req = urllib.request.Request("{}/about".format(api))
