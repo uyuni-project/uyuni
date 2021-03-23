8b3c13100ccfb9ba205682fcb205388f0537c8b0
Johannes Renner <jrenner@suse.com>
2021-03-23 18:16:42 +0100

    Generate minion keys and pre-accept

--- containers/create-proxy-config.py
+++ containers/create-proxy-config.py
@@ -22,6 +22,21 @@ ca_file = ssl_build_path + "/RHN-ORG-TRUSTED-SSL-CERT"
 shutil.copy(ca_file, config_path)
 shutil.copy(glob.glob('/root/ssl-build/proxy/*.noarch.rpm')[-1], config_path)
 
+# Generate minion keys and pre-accept
+os.system('salt-key --gen-keys=proxy.example.com --gen-keys-dir=/tmp')
+shutil.copy("/tmp/proxy.example.com.pub", "/etc/salt/pki/master/minions/proxy.example.com")
+
+# Copy minion keys to proxy-config
+os.mkdir(config_path + "/salt")
+os.mkdir(config_path + "/salt/pki")
+os.mkdir(config_path + "/salt/pki/minion")
+minion_config_path = config_path + "/salt/pki/minion"
+shutil.copy("/tmp/proxy.example.com.pub", minion_config_path)
+shutil.copy("/tmp/proxy.example.com.pem", minion_config_path)
+
+# Copy the master pub key
+shutil.copy("/etc/salt/pki/master/master.pub", minion_config_path + "/minion_master.pub")
+
 # Print some output and environment variables
 print("\nThe proxy config files were created, environment variables:")
 print("UYUNI_MASTER=%s" % socket.getfqdn())
