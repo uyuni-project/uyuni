96fdb59c3cb84b46a84045b7fdcc6fba3c188834
Michael Calmer <mc@suse.de>
2021-03-23 15:29:49 +0100

    define certs variables

--- containers/runProxy.sh
+++ containers/runProxy.sh
@@ -12,6 +12,8 @@ $RUNNER run --rm=true -ti \
 	-e UYUNI_ACTIVATION_KEY='1-proxy' \
 	-e UYUNI_MINION_ID='mc-proxy.suse.de' \
 	-e UYUNI_MACHINE_ID='488de1bd7b08472cba12c6e3c775d4bb' \
+	-e UYUNI_CA_CERTS='/config/RHN-ORG-TRUSTED-SSL-CERT' \
+	-e UYUNI_SRV_CERT='/config/rhn-org-httpd-ssl-key-pair-proxy-1.0-1.noarch.rpm' \
 	-v $CONFIG_DIR:/config \
 	--name uyuni_proxy \
         $IMAGE
