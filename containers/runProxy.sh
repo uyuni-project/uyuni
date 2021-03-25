64f73723003a85664bef889e009d5cf87d1d4622
Michael Calmer <mc@suse.de>
2021-03-25 11:05:57 +0100

    create and mount permanent storage

--- containers/runProxy.sh
+++ containers/runProxy.sh
@@ -6,6 +6,20 @@ CURDIR=$(dirname $(realpath $0))
 IMAGE=localhost/proxy
 CONFIG_DIR="$CURDIR/proxy-config"
 RUNNER=podman
+STORAGE=/fast/mc/
+
+if [ ! -d $STORAGE/www ]; then
+	mkdir -p $STORAGE/www
+fi
+if [ ! -d $STORAGE/squid ]; then
+	mkdir -p $STORAGE/squid
+fi
+if [ ! -d $STORAGE/log ]; then
+	mkdir -p $STORAGE/log
+fi
+if [ ! -d $STORAGE/proxy ]; then
+	mkdir -p $STORAGE/proxy
+fi
 
 $RUNNER run --rm=true -ti \
 	-e UYUNI_MASTER='suma-refhead-srv.mgr.suse.de' \
@@ -15,6 +29,10 @@ $RUNNER run --rm=true -ti \
 	-e UYUNI_CA_CERTS='/config/RHN-ORG-TRUSTED-SSL-CERT' \
 	-e UYUNI_SRV_CERT='/config/'`find $CONFIG_DIR -name "rhn-org-httpd-ssl-key-pair-*.rpm" -printf "%f"` \
 	-v $CONFIG_DIR:/config \
+	-v $STORAGE/www:/srv/www/htdocs/pub \
+	-v $STORAGE/squid:/var/cache/squid \
+	-v $STORAGE/log:/var/log_perm \
+	-v $STORAGE/proxy:/var/spool/rhn-proxy \
 	-p 80:80 \
 	-p 443:443 \
 	-p 4505:4505 \
