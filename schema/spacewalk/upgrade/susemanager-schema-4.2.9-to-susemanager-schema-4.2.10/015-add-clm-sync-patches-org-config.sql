6fdfc4a2be2af652536e140c44fe552d9895c70c
Frantisek Kobzik <fkobzik@suse.de>
2021-04-09 09:40:17 +0200

    DB: Add CLM sync patches organization option

---
+++ schema/spacewalk/upgrade/susemanager-schema-4.2.9-to-susemanager-schema-4.2.10/015-add-clm-sync-patches-org-config.sql
@@ -0,0 +1,4 @@
+ALTER TABLE rhnOrgConfiguration ADD COLUMN IF NOT EXISTS clm_sync_patches CHAR(1)
+    DEFAULT('Y') NOT NULL
+    CONSTRAINT rhn_org_conf_clm_sync_patches
+    CHECK (clm_sync_patches in ('Y', 'N'));
