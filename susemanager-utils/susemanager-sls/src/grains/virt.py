de91caede1b5aaa28562b58ab7a12a18d18f3379
Cédric Bosdonnat <cbosdonnat@suse.com>
2021-04-09 10:19:46 +0200

    Report whether a cluster is installed in the virtualization features grain

--- susemanager-utils/susemanager-sls/src/grains/virt.py
+++ susemanager-utils/susemanager-sls/src/grains/virt.py
@@ -1,3 +1,9 @@
+"""
+Grains for virtualization hosts
+"""
+
+import subprocess
+from xml.etree import ElementTree
 import salt.modules.virt
 
 
@@ -7,8 +13,28 @@ def __virtual__():
 
 def features():
     """returns the features map of the virt module"""
+    try:
+        in_cluster = subprocess.check_call(["crm", "status"]) == 0
+    except Exception:
+        in_cluster = False
+
+    try:
+        ra_conf = ElementTree.fromstring(
+            subprocess.Popen(
+                ["crm_resource", "--show-metadata", "ocf:heartbeat:VirtualDomain"],
+                stdout=subprocess.PIPE,
+            ).communicate()[0]
+        )
+        start_resources_ra = (
+            ra_conf.find(".//parameter[@name='start_resources']") is not None
+        )
+    except Exception:
+        start_resources_ra = False
+
     return {
         "virt_features": {
             "enhanced_network": "network_update" in salt.modules.virt.__dict__,
+            "cluster": in_cluster,
+            "resource_agent_start_resources": start_resources_ra,
         },
     }
--- susemanager-utils/susemanager-sls/src/tests/test_grains_virt.py
+++ susemanager-utils/susemanager-sls/src/tests/test_grains_virt.py
@@ -6,12 +6,43 @@ mockery.setup_environment()
 from ..grains import virt
 
 @pytest.mark.parametrize("network", [True, False])
-def test_features(network):
+def test_features_network(network):
     """
-    test the features function
+    test the network part of the features function
     """
     virt_funcs = {}
     if network:
         virt_funcs["network_update"] = MagicMock(return_value = True)
     with patch.dict(virt.salt.modules.virt.__dict__, virt_funcs):
         assert virt.features()["virt_features"]["enhanced_network"] == network
+
+
+@pytest.mark.parametrize("cluster, start_resources", [(True, True), (True, False), (False, False)])
+def test_features_cluster(cluster, start_resources):
+    """
+    test the cluster part of the features function
+    """
+    param = "" if not start_resources else """
+      <parameter name="start_resources">
+        <content type="boolean" default="false"/>
+      </parameter>"""
+    crm_resources = """<?xml version="1.0"?>
+<!DOCTYPE resource-agent SYSTEM "ra-api-1.dtd">
+<resource-agent name="VirtualDomain">
+  <parameters>  
+    {}
+  </parameters>
+</resource-agent>
+""".format(param)
+    popen_mock = MagicMock(side_effect=FileNotFoundError())
+    check_call_mock = MagicMock(side_effect=FileNotFoundError())
+    if cluster:
+        popen_mock = MagicMock()
+        popen_mock.return_value.communicate.return_value = (crm_resources, None)
+        check_call_mock = MagicMock(return_value = 0)
+
+    with patch.object(virt.subprocess, "check_call", check_call_mock):
+        with patch.object(virt.subprocess, "Popen", popen_mock):
+            assert virt.features()["virt_features"]["cluster"] == cluster
+            assert virt.features()["virt_features"]["resource_agent_start_resources"] == start_resources
+
