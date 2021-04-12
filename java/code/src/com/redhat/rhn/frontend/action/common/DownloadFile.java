533fbefbcfcee90020590502e4d34300813371a9
Michael Calmer <mc@suse.de>
2021-04-12 10:08:07 +0200

    for a SUSE system get metadata and package from same source (bsc#1184475)

--- java/code/src/com/redhat/rhn/frontend/action/common/DownloadFile.java
+++ java/code/src/com/redhat/rhn/frontend/action/common/DownloadFile.java
@@ -576,28 +576,30 @@ public class DownloadFile extends DownloadAction {
                 channel = child;
             }
 
-            String byteRange = request.getHeader("Range");
-            if (byteRange != null) {
-                Pattern rangeRegex = Pattern.compile("bytes=(\\d+)-(\\d+)",
-                        Pattern.CASE_INSENSITIVE);
-                Matcher match = rangeRegex.matcher(byteRange);
-                int newHeaderEnd = 0;
-                int newHeaderStart = 0;
+            if (child != null || !tree.getInstallType().isSUSE() ||
+                    tree.getKernelOptions().contains("useonlinerepo")) {
+                String byteRange = request.getHeader("Range");
+                if (byteRange != null) {
+                    Pattern rangeRegex = Pattern.compile("bytes=(\\d+)-(\\d+)",
+                            Pattern.CASE_INSENSITIVE);
+                    Matcher match = rangeRegex.matcher(byteRange);
+                    int newHeaderEnd = 0;
+                    int newHeaderStart = 0;
 
-                if (match.find()) {
-                    newHeaderStart = Integer.parseInt(match.group(1));
-                    newHeaderEnd = Integer.parseInt(match.group(2));
-                    int modulo = newHeaderEnd % 8;
-                    if (modulo > 0) {
-                        newHeaderEnd = newHeaderEnd + 8 - modulo;
+                    if (match.find()) {
+                        newHeaderStart = Integer.parseInt(match.group(1));
+                        newHeaderEnd = Integer.parseInt(match.group(2));
+                        int modulo = newHeaderEnd % 8;
+                        if (modulo > 0) {
+                            newHeaderEnd = newHeaderEnd + 8 - modulo;
+                        }
                     }
+                    rpmPackage = ChannelFactory.lookupPackageByFilenameAndRange(
+                            channel, fileName, newHeaderStart, newHeaderEnd);
                 }
-                rpmPackage = ChannelFactory.lookupPackageByFilenameAndRange(
-                        channel, fileName, newHeaderStart, newHeaderEnd);
-            }
-            else {
-                rpmPackage = ChannelFactory.lookupPackageByFilename(
-                        channel, fileName);
+                else {
+                    rpmPackage = ChannelFactory.lookupPackageByFilename(channel, fileName);
+                }
             }
 
             if (rpmPackage != null) {
