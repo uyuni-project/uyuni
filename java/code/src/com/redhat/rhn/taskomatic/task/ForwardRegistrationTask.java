545240c3d2864c70a1e45e7190d8e76a4de1688a
Michael Calmer <mc@suse.de>
2021-04-16 13:30:21 +0200

    implement ForwardRegistration taskomatic task

---
+++ java/code/src/com/redhat/rhn/taskomatic/task/ForwardRegistrationTask.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2021 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.taskomatic.task;
+
+import com.redhat.rhn.domain.product.SUSEProduct;
+import com.redhat.rhn.domain.scc.SCCCachingFactory;
+import com.redhat.rhn.domain.scc.SCCRegCacheItem;
+import com.redhat.rhn.domain.server.Server;
+
+import com.suse.scc.model.SCCMinProductJson;
+import com.suse.scc.model.SCCRegisterSystemJson;
+import com.suse.utils.Opt;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+public class ForwardRegistrationTask extends RhnJavaJob {
+
+    @Override
+    public void execute(JobExecutionContext arg0) throws JobExecutionException {
+        List<SCCRegCacheItem> forwardRegistration = SCCCachingFactory.findSystemsToForwardRegistration();
+        List<SCCRegCacheItem> deregister = SCCCachingFactory.listDeregisterItems();
+
+        for (SCCRegCacheItem rci : forwardRegistration) {
+            SCCRegisterSystemJson payload = getPayload(rci);
+        }
+    }
+
+    public SCCRegisterSystemJson getPayload(SCCRegCacheItem rci) {
+        Server srv = rci.getOptServer().get();
+        List<SCCMinProductJson> products = Opt.fold(srv.getInstalledProductSet(),
+                () -> {
+                    return new LinkedList<SUSEProduct>();
+                },
+                s -> {
+                    List<SUSEProduct> prd = new LinkedList<>();
+                    prd.add(s.getBaseProduct());
+                    prd.addAll(s.getAddonProducts());
+                    return prd;
+                }
+                ).stream()
+        .map(p -> new SCCMinProductJson(p))
+        .collect(Collectors.toList());
+
+        Map<String, String> hwinfo = new HashMap<>();
+        Optional.ofNullable(srv.getCpu().getNrCPU()).ifPresent(c -> hwinfo.put("cpus", c.toString()));
+        Optional.ofNullable(srv.getCpu().getNrsocket()).ifPresent(c -> hwinfo.put("sockets", c.toString()));
+        hwinfo.put("arch", srv.getServerArch().getLabel().split("-")[0]);
+        if (srv.isVirtualGuest()) {
+            String cloud = "";
+            String hv = srv.getVirtualInstance().getType().getLabel();
+            switch(hv) {
+            case "fully_virtualized":
+            case "para_virtualized":
+                hv = "XEN";
+                break;
+            case "qemu":
+                hv = "KVM";
+                break;
+            case "vmware":
+            case "hyperv":
+                break;
+            case "nutanix":
+                hv = "Nutanix";
+                break;
+            case "azure":
+                cloud = "Microsoft";
+                break;
+            case "aws":
+                cloud = "Amazon";
+                break;
+            case "gce":
+                cloud = "Google";
+                break;
+            default:
+                break;
+            }
+            hwinfo.put("hypervisor", hv);
+            hwinfo.put("cloud_provider", cloud);
+            Optional.ofNullable(srv.getVirtualInstance().getUuid()).ifPresent(uuid -> {
+                hwinfo.put("uuid", uuid.replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})",
+                        "$1-$2-$3-$4-$5"));
+            });
+        }
+        else {
+            // null == physical instance
+            hwinfo.put("hypervisor", null);
+        }
+
+        String passwd = rci.getOptSccPasswd().orElseGet(() -> {
+            String pw = RandomStringUtils.randomAlphanumeric(64);
+            rci.setSccPasswd(pw);
+            SCCCachingFactory.saveRegCacheItem(rci);
+            return pw;
+        });
+
+        return new SCCRegisterSystemJson(srv.getId().toString(), passwd, srv.getHostname(),
+                hwinfo, products);
+    }
+}
---
+++ java/code/src/com/suse/scc/model/SCCRegisterSystemJson.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2021 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.suse.scc.model;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This is a System Item send to SCC for registration.
+ */
+public class SCCRegisterSystemJson {
+
+    private String login;
+    private String password;
+    private String hostname;
+    private Map<String, String> hwinfo;
+    private List<SCCMinProductJson> products;
+    private List<String> regcodes = new LinkedList<>();
+
+    /**
+     * Constructor
+     *
+     * @param login the login
+     * @param password the password
+     * @param hostname the hostname
+     * @param hwinfo the hardware data
+     * @param products the products
+     */
+    public SCCRegisterSystemJson(String loginIn, String passwdIn, String hostnameIn,
+            Map<String, String> hwinfoIn, List<SCCMinProductJson> productsIn) {
+        login = loginIn;
+        password = passwdIn;
+        hostname = hostnameIn;
+        hwinfo = hwinfoIn;
+        products = productsIn;
+    }
+
+    /**
+     * @return Returns the login.
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @return Returns the password.
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @return Returns the hostname.
+     */
+    public String getHostname() {
+        return hostname;
+    }
+
+    /**
+     * @return Returns the hwinfo.
+     */
+    public Map<String, String> getHwinfo() {
+        return hwinfo;
+    }
+
+    /**
+     * @return Returns the products.
+     */
+    public List<SCCMinProductJson> getProducts() {
+        return products;
+    }
+
+    /**
+     * @return Returns the regcodes.
+     */
+    public List<String> getRegcodes() {
+        return regcodes;
+    }
+}
