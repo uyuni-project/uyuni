dcf6ecb1b4340511eab5a96bba966406f595b10b
dleidi <dleidi@suse.com>
2021-04-19 11:52:51 +0200

    New Tab cycle for AnsibleControlNode

--- web/html/src/manager/minion/index.js
+++ web/html/src/manager/minion/index.js
@@ -2,5 +2,6 @@ export default {
   'minion/config-channels/minion-config-channels': () => import('./config-channels/minion-config-channels'),
   'minion/formula/minion-formula': () => import('./formula/minion-formula.renderer'),
   'minion/formula/minion-formula-selection': () => import('./formula/minion-formula-selection.renderer'),
-  'minion/packages/package-states': () => import('./packages/package-states.renderer')
+  'minion/packages/package-states': () => import('./packages/package-states.renderer'),
+  "minion/ansible": () => import("./ansible/ansible-control-node")
 }
