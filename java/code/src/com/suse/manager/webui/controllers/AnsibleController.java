f43266e0b4eb8a9e76cb454be5e011b37f68a61a
Frantisek Kobzik <fkobzik@suse.de>
2021-04-20 10:23:17 +0200

    Controller for AnsiblePath

--- java/code/src/com/suse/manager/webui/controllers/AnsibleController.java
+++ java/code/src/com/suse/manager/webui/controllers/AnsibleController.java
@@ -15,19 +15,32 @@
 
 package com.suse.manager.webui.controllers;
 
+import static com.suse.manager.webui.utils.SparkApplicationHelper.json;
 import static com.suse.manager.webui.utils.SparkApplicationHelper.withCsrfToken;
 import static com.suse.manager.webui.utils.SparkApplicationHelper.withDocsLocale;
 import static com.suse.manager.webui.utils.SparkApplicationHelper.withUser;
 import static spark.Spark.get;
+import static spark.Spark.post;
+
+import com.redhat.rhn.common.localization.LocalizationService;
+import com.redhat.rhn.domain.server.Server;
+import com.redhat.rhn.domain.server.ServerFactory;
+import com.redhat.rhn.domain.server.ansible.AnsiblePath;
+import com.redhat.rhn.domain.user.User;
+import com.redhat.rhn.manager.system.SystemManager;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.redhat.rhn.domain.server.Server;
-import com.redhat.rhn.domain.server.ServerFactory;
-import com.redhat.rhn.domain.user.User;
-import java.util.HashMap;
-import java.util.Map;
+import com.suse.manager.webui.utils.gson.AnsiblePathJson;
+import com.suse.manager.webui.utils.gson.ResultJson;
+
 import org.apache.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -39,6 +52,7 @@ import spark.template.jade.JadeTemplateEngine;
 public class AnsibleController {
 
     private static final Gson GSON = new GsonBuilder().create();
+    private static final LocalizationService LOCAL = LocalizationService.getInstance();
 
     private static Logger log = Logger.getLogger(AnsibleController.class);
 
@@ -52,6 +66,12 @@ public class AnsibleController {
     public static void initRoutes(JadeTemplateEngine jade) {
         get("/manager/systems/details/ansible",
                 withCsrfToken(withDocsLocale(withUser(AnsibleController::view))), jade);
+
+        get("/manager/api/systems/details/ansible/paths/:minionId",
+                withUser(AnsibleController::listAnsiblePathsByMinion));
+        // todo no CSRF?
+        post("/manager/api/systems/details/ansible/paths/save",
+                withUser(AnsibleController::saveAnsiblePath));
     }
 
     /**
@@ -70,4 +90,48 @@ public class AnsibleController {
 
         return new ModelAndView(data, "templates/ansible/view.jade");
     }
+
+    /**
+     * List Ansible Paths by minion
+     *
+     * @param req the request object
+     * @param res the response object
+     * @param user the authorized user
+     * @return string with JSON representation of the paths
+     */
+    public static String listAnsiblePathsByMinion(Request req, Response res, User user) {
+        long minionId = Long.parseLong(req.params("minionId"));
+        List<AnsiblePathJson> paths = SystemManager.listAnsiblePaths(minionId, user).stream()
+                .map(AnsiblePathJson::new)
+                .collect(Collectors.toList());
+        return json(res, paths);
+    }
+
+    /**
+     * Save or update (depends of the json.getId() contents) an Ansible path
+     *
+     * @param req the request object
+     * @param res the response object
+     * @param user the authorized user
+     * @return the string with JSON response
+     */
+    // todo path validation: path format + existence (in case of update) + constraints. do it in SystemManager
+    // and reuse in xmlrpc
+    public static String saveAnsiblePath(Request req, Response res, User user) {
+        AnsiblePathJson json = GSON.fromJson(req.body(), AnsiblePathJson.class);
+
+        if (json.getId() == null) {
+            SystemManager.createAnsiblePath(AnsiblePath.Type.fromLabel(json.getType()),
+                    json.getMinionId(),
+                    json.getPath(),
+                    user);
+        }
+        else {
+            SystemManager.updateAnsiblePath(json.getId(),
+                    json.getPath(),
+                    user);
+        }
+        return json(res, ResultJson.success());
+    }
+
 }
---
+++ java/code/src/com/suse/manager/webui/utils/gson/AnsiblePathJson.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2021 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.suse.manager.webui.utils.gson;
+
+import com.redhat.rhn.domain.server.ansible.AnsiblePath;
+
+/**
+ * DTO for AnsiblePath
+ */
+public class AnsiblePathJson {
+
+    private Long id;
+    private Long minionId;
+    private String type;
+    private String path;
+
+    /**
+     * Standard constructor
+     *
+     * @param entity the backend entity
+     */
+    public AnsiblePathJson(AnsiblePath entity) {
+        this.id = entity.getId();
+        this.minionId = entity.getMinionServer().getId();
+        this.type = entity.getEntityType().getLabel();
+        this.path = entity.getPath().toString();
+    }
+
+    /**
+     * Gets the id.
+     *
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Gets the minionId.
+     *
+     * @return minionId
+     */
+    public Long getMinionId() {
+        return minionId;
+    }
+
+    /**
+     * Gets the type.
+     *
+     * @return type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Gets the path.
+     *
+     * @return path
+     */
+    public String getPath() {
+        return path;
+    }
+}
