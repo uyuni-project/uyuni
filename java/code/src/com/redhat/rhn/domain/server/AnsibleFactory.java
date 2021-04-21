6305a6186176900754595eef8eb3791b3b6b682d
Frantisek Kobzik <fkobzik@suse.de>
2021-04-21 15:56:30 +0200

    Refactor: Factory: Move the ansible-related code to a separate factory class

---
+++ java/code/src/com/redhat/rhn/domain/server/AnsibleFactory.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2021 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.redhat.rhn.domain.server;
+
+import com.redhat.rhn.common.hibernate.HibernateFactory;
+import com.redhat.rhn.domain.server.ansible.AnsiblePath;
+import com.redhat.rhn.domain.server.ansible.InventoryPath;
+import com.redhat.rhn.domain.server.ansible.PlaybookPath;
+
+import org.apache.log4j.Logger;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Hibernate factory with Ansible-related methods
+ */
+public class AnsibleFactory extends HibernateFactory {
+
+    private static Logger log = Logger.getLogger(AnsibleFactory.class);
+
+    /**
+     * Lookup an {@link AnsiblePath} by id
+     *
+     * @param id the id
+     * @return the {@link AnsiblePath}
+     */
+    public static Optional<AnsiblePath> lookupAnsiblePathById(long id) {
+        return HibernateFactory.getSession()
+                .createQuery("SELECT p FROM AnsiblePath p WHERE id = :id")
+                .setParameter("id", id)
+                .uniqueResultOptional();
+    }
+
+    /**
+     * Lookup {@link AnsiblePath} by path and minion id
+     *
+     * @param path the path
+     * @param minionServerId the minion id
+     * @return optional of {@link AnsiblePath}
+     */
+    public static Optional<AnsiblePath> lookupAnsiblePathByPathAndMinion(Path path, long minionServerId) {
+        return HibernateFactory.getSession().createQuery("SELECT p FROM AnsiblePath p " +
+                "WHERE p.path = :path " +
+                "AND p.minionServer.id = :minionServerId")
+                .setParameter("path", path)
+                .setParameter("minionServerId", minionServerId)
+                .uniqueResultOptional();
+    }
+
+    /**
+     * List {@link AnsiblePath}s associated with a {@link MinionServer} with given id
+     *
+     * @param minionServerId the id of {@link MinionServer}
+     * @return the list of {@link AnsiblePath}s
+     */
+    public static List<AnsiblePath> listAnsiblePaths(long minionServerId) {
+        return HibernateFactory.getSession()
+                .createQuery("SELECT p FROM AnsiblePath p " +
+                        "WHERE p.minionServer.id = :sid ")
+                .setParameter("sid", minionServerId)
+                .list();
+    }
+
+    /**
+     * List {@link PlaybookPath}s associated with a {@link MinionServer} with given id
+     *
+     * @param minionId the id of {@link MinionServer}
+     * @return the list of {@link PlaybookPath}s
+     */
+    public static List<PlaybookPath> listAnsiblePlaybookPaths(long minionId) {
+        return HibernateFactory.getSession()
+                .createQuery("SELECT p FROM PlaybookPath p " +
+                        "WHERE p.minionServer.id = :mid ")
+                .setParameter("mid", minionId)
+                .list();
+    }
+
+    /**
+     * List {@link InventoryPath}s associated with a {@link MinionServer} with given id
+     *
+     * @param minionId the id of {@link MinionServer}
+     * @return the list of {@link PlaybookPath}s
+     */
+    public static List<InventoryPath> listAnsibleInventoryPaths(long minionId) {
+        return HibernateFactory.getSession()
+                .createQuery("SELECT p FROM InventoryPath p " +
+                        "WHERE p.minionServer.id = :mid ")
+                .setParameter("mid", minionId)
+                .list();
+    }
+
+    /**
+     * Save an {@link AnsiblePath}
+     *
+     * @param path the {@link AnsiblePath}
+     * @return the updated {@link AnsiblePath}
+     */
+    public static AnsiblePath saveAnsiblePath(AnsiblePath path) {
+        HibernateFactory.getSession().saveOrUpdate(path);
+        return path;
+    }
+
+    /**
+     * Remove an {@link AnsiblePath}
+     *
+     * @param path the {@link AnsiblePath}
+     */
+    public static void removeAnsiblePath(AnsiblePath path) {
+        HibernateFactory.getSession().delete(path);
+    }
+
+    @Override
+    protected Logger getLogger() {
+        return log;
+    }
+
+}
---
+++ java/code/src/com/redhat/rhn/domain/server/test/AnsibleFactoryTest.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2021 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.redhat.rhn.domain.server.test;
+
+import com.redhat.rhn.common.hibernate.HibernateFactory;
+import com.redhat.rhn.domain.server.AnsibleFactory;
+import com.redhat.rhn.domain.server.MinionServer;
+import com.redhat.rhn.domain.server.ansible.AnsiblePath;
+import com.redhat.rhn.domain.server.ansible.InventoryPath;
+import com.redhat.rhn.domain.server.ansible.PlaybookPath;
+import com.redhat.rhn.testing.BaseTestCaseWithUser;
+
+import java.nio.file.Path;
+
+/**
+ * Tests for {@link AnsibleFactory}
+ */
+public class AnsibleFactoryTest extends BaseTestCaseWithUser {
+
+    /**
+     * Test basic save - read cycle of AnsiblePath implementations
+     * @throws Exception
+     */
+    public void testSaveAndFindAnsiblePath() throws Exception {
+        MinionServer minionServer1 = MinionServerFactoryTest.createTestMinionServer(user);
+        MinionServer minionServer2 = MinionServerFactoryTest.createTestMinionServer(user);
+
+        AnsiblePath inventoryPath = new InventoryPath(minionServer1);
+        inventoryPath.setPath(Path.of("/tmp/test1"));
+        AnsiblePath playbookPath = new PlaybookPath(minionServer2);
+        playbookPath.setPath(Path.of("/tmp/test2"));
+
+        inventoryPath = AnsibleFactory.saveAnsiblePath(inventoryPath);
+        playbookPath = AnsibleFactory.saveAnsiblePath(playbookPath);
+        assertNotNull(inventoryPath.getId());
+        assertNotNull(playbookPath.getId());
+
+        HibernateFactory.getSession().flush();
+        // let's get those entities out of the session
+        HibernateFactory.getSession().evict(inventoryPath);
+        HibernateFactory.getSession().evict(playbookPath);
+
+        assertEquals(inventoryPath, AnsibleFactory.lookupAnsiblePathById(inventoryPath.getId()).get());
+        assertEquals(inventoryPath, AnsibleFactory.lookupAnsiblePathByPathAndMinion(Path.of("/tmp/test1"), minionServer1.getId()).get());
+        assertEquals(playbookPath, AnsibleFactory.lookupAnsiblePathById(playbookPath.getId()).get());
+        assertEquals(1, AnsibleFactory.listAnsiblePaths(minionServer1.getId()).size());
+        assertEquals(inventoryPath, AnsibleFactory.listAnsiblePaths(minionServer1.getId()).iterator().next());
+    }
+
+    /**
+     * Test removing AnsiblePath
+     */
+    public void testRemoveAnsiblePath() throws Exception {
+        MinionServer minionServer1 = MinionServerFactoryTest.createTestMinionServer(user);
+        AnsiblePath inventoryPath = new InventoryPath(minionServer1);
+        inventoryPath.setPath(Path.of("/tmp/test1"));
+        inventoryPath = AnsibleFactory.saveAnsiblePath(inventoryPath);
+
+        AnsibleFactory.removeAnsiblePath(inventoryPath);
+        assertTrue(AnsibleFactory.lookupAnsiblePathById(inventoryPath.getId()).isEmpty());
+    }
+}
