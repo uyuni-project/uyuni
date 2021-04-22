27a123993243fe53fb0903ca5128965e17f8954e
Kevin Walter <kwalter@suse.de>
2021-04-22 16:42:31 +0200

    refactor hypervisor and cloud logic for VirtualInstanceType

--- java/code/src/com/redhat/rhn/domain/server/VirtualInstanceType.java
+++ java/code/src/com/redhat/rhn/domain/server/VirtualInstanceType.java
@@ -16,7 +16,7 @@ package com.redhat.rhn.domain.server;
 
 import com.redhat.rhn.domain.Label;
 
-
+import java.util.Optional;
 
 
 /**
@@ -27,4 +27,29 @@ public class VirtualInstanceType extends Label {
     VirtualInstanceType() {
     }
 
+    public Optional<String> getCloudProvider() {
+        switch(getLabel()) {
+            case "azure": return Optional.of("Microsoft");
+            case "aws": return Optional.of("Amazon");
+            case "gce": return Optional.of("Google");
+            default: return Optional.empty();
+        }
+    }
+
+    public Optional<String> getHypervisor() {
+        switch(getLabel()) {
+            case "fully_virtualized": return Optional.of("Xen");
+            case "para_virtualized": return Optional.of("Xen");
+            case "qemu": return Optional.of("KVM");
+            case "vmware":return Optional.of("VMware");
+            case "hyperv": return Optional.of("Hyper-V");
+            case "nutanix": return Optional.of("Nutanix");
+            case "virtualbox": return Optional.of("VirtualBox");
+            case "virtualpc": return Optional.of("VirtualPC");
+            case "virtage": return Optional.of("Virtage");
+            default: return Optional.empty();
+        }
+    }
 }
+
+
