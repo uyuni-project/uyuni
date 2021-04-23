938ebfbd3ca13f4da46a5b45e6082acbc7562550
dleidi <dleidi@suse.com>
2021-04-23 15:13:52 +0200

    Fetch path content from the backend

--- web/html/src/manager/minion/ansible/accordion-path-content.tsx
+++ web/html/src/manager/minion/ansible/accordion-path-content.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { AnsiblePath } from "./ansible-path-type";
 import { Panel } from "components/panels/Panel";
+import Network from "utils/network";
+import { Messages, Utils } from "components/messages";
 
 type PropsType = {
   path: AnsiblePath;
@@ -9,25 +11,52 @@ type PropsType = {
 type StateType = {
   open: boolean; 
   content: Map<string, any>;
+  errors: string[];
 };
+
+function getURL(path: AnsiblePath) {
+  let baseUrl: string;
+  if (path.type === "playbook") {
+    baseUrl = "/rhn/manager/api/systems/details/ansible/discover-playbooks/";
+  }
+  else {
+    baseUrl = "/rhn/manager/api/systems/details/ansible/introspect-inventory/";
+  }
+  return baseUrl + path.id;
+}
 
 class AccordionPathContent extends React.Component<PropsType, StateType> {
   constructor(props) {
     super(props);
 
-    this.state = { open: false, content: new Map<string, any>() };
+    this.state = {
+      open: false,
+      content: new Map<string, any>(),
+      errors: [],
+    };
   }
 
   onOpen() {
-    this.loadPathContent(this.props.path);
-    this.setState({ open: !this.state.open });
-  }
-
-  loadPathContent(path: AnsiblePath) {
-    // Network.get("/rhn/manager/api/systems/details/ansible/path/" + path.id)
-    // .promise.then(data => {
-      this.setState({ content: this.state.content.set(path.path, ["element 1", "element 2"]) });
-    // });
+    if (!this.state.open) {
+      const path: AnsiblePath = this.props.path;
+      if (Object.keys(this.state.content).includes(path.path)) {
+        this.setState({ open: true });
+      }
+      else {
+        Network.get(getURL(path))
+        .promise.then(data => {
+          if (data.success) {
+            this.setState({ content: this.state.content.set(path.path, ["element 1", "element 2"]), open: true});
+          }
+          else {
+            this.setState({ errors: data.messages });
+          }
+        });
+      }
+    }
+    else {
+      this.setState({ open: false });
+    }
   }
 
   render() {
@@ -35,9 +64,12 @@ class AccordionPathContent extends React.Component<PropsType, StateType> {
       <div className="accordion-toggle" onClick={() => this.onOpen()}>
         { this.props.path.path }
       </div>;
+
+    const errors = this.state.errors.length > 0 ? <Messages items={Utils.error(this.state.errors)} /> : null;
     return (
-      <Panel header={header} headingLevel="h5">   
+      <Panel header={header} headingLevel="h5">
         <div>
+          {errors}
           { this.state.open ? 
               <ul>
                 {
