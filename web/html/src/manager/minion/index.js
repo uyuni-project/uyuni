3ff3284603dbd745baa8162f51a9207dd6ba3a2c
Karl Tarvas <Etheryte@users.noreply.github.com>
2021-04-26 10:48:38 +0200

    Migrate `manager/minion` (#3346)
    * Migrate config-channels
    
    * Migrate formula
    
    * Migrate packages
    
    * Migrate index
    
    * Shim FormulaForm types
    
    * Fix translations, remove log

--- web/html/src/manager/minion/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-  'minion/config-channels/minion-config-channels': () => import('./config-channels/minion-config-channels'),
-  'minion/formula/minion-formula': () => import('./formula/minion-formula.renderer'),
-  'minion/formula/minion-formula-selection': () => import('./formula/minion-formula-selection.renderer'),
-  'minion/packages/package-states': () => import('./packages/package-states.renderer'),
-  'minion/ansible/control-node': () => import('./ansible/ansible-control-node'),
-  'minion/ansible/path-content': () => import('./ansible/ansible-path-content')
-}
