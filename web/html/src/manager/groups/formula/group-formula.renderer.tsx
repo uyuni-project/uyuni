480a1adc477aa1ced3632b2f6fc8b733832274eb
Karl Tarvas <Etheryte@users.noreply.github.com>
2021-04-26 11:07:29 +0200

    Migrate `manager/groups` (#3332)
    * Migrate manager/groups
    
    * Cleanup

---
+++ web/html/src/manager/groups/formula/group-formula.renderer.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+
+import FormulaForm from "components/FormulaForm";
+import { Utils } from "utils/functions";
+import SpaRenderer from "core/spa/spa-renderer";
+import { DEPRECATED_unsafeEquals } from "utils/legacy";
+
+const capitalize = Utils.capitalize;
+
+export const renderer = (renderId, { groupId, formulaId }) => {
+  const msgMap = {
+    formula_saved: (
+      <p>
+        {t("Formula saved. Apply the ")}
+        <a href={"/rhn/manager/groups/details/highstate?sgid=" + groupId}>{t("Highstate")}</a>
+        {t(" for the changes to take effect.")}
+      </p>
+    ),
+    error_invalid_target: t("Invalid target type."),
+  };
+
+  function addFormulaNavBar(formulaList, activeId) {
+    jQuery("#formula-nav-bar").remove();
+
+    var navBar = "<ul class='nav nav-tabs nav-tabs-pf' id='formula-nav-bar'>\n";
+    navBar +=
+      "<li><a href='/rhn/manager/groups/details/formulas?sgid=" + groupId + "'>" + t("Formulas") + "</a></li>\n";
+    for (var i in formulaList)
+      navBar +=
+        "<li" +
+        (DEPRECATED_unsafeEquals(i, activeId) ? " class='active'>" : ">") +
+        "<a href='/rhn/manager/groups/details/formula/" +
+        i +
+        "?sgid=" +
+        groupId +
+        "'>" +
+        capitalize(formulaList[i]) +
+        "</a></li>\n";
+    navBar += "</ul>";
+    jQuery(".spacewalk-content-nav").append(navBar);
+  }
+
+  SpaRenderer.renderNavigationReact(
+    <FormulaForm
+      dataUrl={"/rhn/manager/api/formulas/form/GROUP/" + groupId + "/" + formulaId}
+      saveUrl="/rhn/manager/api/formulas/save"
+      addFormulaNavBar={addFormulaNavBar}
+      formulaId={formulaId}
+      systemId={groupId}
+      getFormulaUrl={function(id) {
+        return "/rhn/manager/groups/details/formula/" + id + "?sgid=" + groupId;
+      }}
+      scope="group"
+      messageTexts={msgMap}
+    />,
+    document.getElementById(renderId)
+  );
+};
---
+++ web/html/src/manager/groups/index.ts
@@ -0,0 +1,5 @@
+export default {
+  "groups/formula/group-formula": () => import("./formula/group-formula.renderer"),
+  "groups/formula/group-formula-selection": () => import("./formula/group-formula-selection.renderer"),
+  "groups/config-channels/group-config-channels": () => import("./config-channels/group-config-channels"),
+};
