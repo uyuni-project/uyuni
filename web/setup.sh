033d258c576ad417c9bcfad832d6c0708770cbb2
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2021-04-29 19:41:01 +0200

    Build sources (tito) in parallel
    Building the sources of all the packages in the CI takes 20 minutes.
    If we run this in parallel, it should take 5 minutes instead.
    
    One yarnrc for each package, instead of one global yarnc. This way, we can
    build the packages in parallel.
    
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- web/setup.sh
+++ web/setup.sh
@@ -1,4 +1,6 @@
 set -euxo pipefail
+touch /tmp/uyuni-yarn-install.lock
 (cd web/html/src; yarn install --frozen-lockfile)
 (cd web/html/src; yarn build:novalidate)
+rm /tmp/uyuni-yarn-install.lock
 echo ""
