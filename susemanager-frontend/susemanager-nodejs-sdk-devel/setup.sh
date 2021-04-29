0b259d85f856407a02b51af26e624bfa24ed1041
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2021-04-29 20:34:11 +0200

    try creating a lock file to prevent concurrent yarn install
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- susemanager-frontend/susemanager-nodejs-sdk-devel/setup.sh
+++ susemanager-frontend/susemanager-nodejs-sdk-devel/setup.sh
@@ -2,10 +2,15 @@ set -euxo pipefail
 # FIXME HACK. This sets no concurrency for yarn but it will set this into ~/.yarnrc, overwriting any value you may have...
 export CHILD_CONCURRENCY=1
 yarn config set child-concurrency 1
+while [ -f /tmp/uyuni-yarn-install.lock ];do
+    sleep 30s
+done
+touch /tmp/uyuni-yarn-install.lock
 (cd susemanager-frontend/susemanager-nodejs-sdk-devel; rm -rf node_modules)
 yarn install --frozen-lockfile
 yarn autoclean --force
 (cd susemanager-frontend/susemanager-nodejs-sdk-devel; yarn run remove-packages-before-obs-zip)
 (cd susemanager-frontend/susemanager-nodejs-sdk-devel; yarn zip)
 (cd susemanager-frontend/susemanager-nodejs-sdk-devel; yarn run restore-packages-after-obs-zip)
+rm /tmp/uyuni-yarn-install.lock
 echo "susemanager-nodejs-modules.tar.gz"
--- web/setup.sh
+++ web/setup.sh
@@ -2,6 +2,11 @@ set -euxo pipefail
 # FIXME HACK. This sets no concurrency for yarn but it will set this into ~/.yarnrc, overwriting any value you may have...
 export CHILD_CONCURRENCY=1
 yarn config set child-concurrency 1
+while [ -f /tmp/uyuni-yarn-install.lock ];do
+    sleep 30s
+done
+touch /tmp/uyuni-yarn-install.lock
 (cd web/html/src; yarn install --frozen-lockfile)
 (cd web/html/src; yarn build:novalidate)
+rm /tmp/uyuni-yarn-install.lock
 echo ""
