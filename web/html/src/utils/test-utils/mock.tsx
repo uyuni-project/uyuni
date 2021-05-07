1a4f8db256d52dd989e61df77cc12141c4c95b02
dleidi <dleidi@suse.com>
2021-05-07 14:59:46 +0200

    Add tests for accordion path content

--- web/html/src/utils/test-utils/mock.tsx
+++ web/html/src/utils/test-utils/mock.tsx
@@ -9,3 +9,13 @@ jest.mock("components/datetimepicker", () => {
     },
   };
 });
+
+// Mock the datetime picker to avoid it causing issues due to missing jQuery/Bootstrap parts
+jest.mock("components/ace-editor", () => {
+  return {
+    __esModule: true,
+    AceEditor: () => {
+      return <div>My AceEditor mockup</div>;
+    },
+  };
+});
