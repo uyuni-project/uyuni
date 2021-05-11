83787f8ffaedb9d510a44b945ab6342624e85aa3
Etheryte <karl.tarvas@suse.com>
2021-05-11 19:13:15 +0200

    Migrate data-processing

--- web/html/src/manager/visualization/data-processing/partitioning.ts
+++ web/html/src/manager/visualization/data-processing/partitioning.ts
@@ -1,28 +1,23 @@
-/* eslint-disable */
-'use strict';
-
 // Holds functions needed for computing node partitions (user e.g. for deriving
 // CSS class)
-function partitioning(initPartitioning) {
-  const my = {};
+function partitioning<T>(initPartitioning?: T) {
+  const my: any = {};
   const partitioning = initPartitioning || {};
 
   // get the partitioning functions
   my.get = function(c) {
     return partitioning;
-  }
+  };
 
   // compute the partition name
   my.computePartitionName = function(d) {
     return Object.values(partitioning)
-      .map(f => f(d) || '')
-      .reduce((v1, v2) => v1 + ' ' + v2, '')
+      .map((f: any) => f(d) || "")
+      .reduce((v1, v2) => v1 + " " + v2, "")
       .trim();
-  }
+  };
 
   return my;
 }
 
-export {
-  partitioning,
-};
+export { partitioning };
