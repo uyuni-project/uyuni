41f6a7e0b71fd72ea4c90747bb925394dc7291e8
Etheryte <karl.tarvas@suse.com>
2021-05-11 19:13:15 +0200

    Migrate data-processing

--- web/html/src/manager/visualization/data-processing/filters.ts
+++ web/html/src/manager/visualization/data-processing/filters.ts
@@ -1,31 +1,26 @@
-/* eslint-disable */
-'use strict';
-
 // Stores filters
-function filters(initFilters) {
+function filters<T>(initFilters?: T) {
   const filters = initFilters || {};
-  const my = {};
+  const my: any = {};
 
   my.put = function(filterName, filter) {
     filters[filterName] = filter;
-  }
+  };
 
   my.remove = function(filterName) {
     delete filters[filterName];
-  }
+  };
 
   // construct a predicate function from filters
   // (i.e. return a  function that connects all predicates with logical AND)
   my.predicate = function() {
-    const fs = Object.values(filters);
+    const fs: any[] = Object.values(filters);
     return function(node) {
-      return fs.map(f => f(node)).reduce((v1, v2) => v1 && v2, true)
-    }
-  }
+      return fs.map(f => f(node)).reduce((v1, v2) => v1 && v2, true);
+    };
+  };
 
   return my;
 }
 
-export {
-    filters,
-};
+export { filters };
