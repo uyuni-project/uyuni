d7dc1771bc971355617a53f187864aec6ac4ae69
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2021-05-12 17:35:25 +0200

    test: do n ot merge

--- web/html/src/core/utils/objects.ts
+++ web/html/src/core/utils/objects.ts
@@ -1,4 +1,5 @@
 // TODO: This is obsolete, see https://github.com/SUSE/spacewalk/issues/13648
+// TEST
 export function objectDefaultValueHandler(defaultValue: any) {
   return {
     get: function(target: any, name: string) {
