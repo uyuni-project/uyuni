ceb9135eb10978857ad35d7e406804bdf9d93453
Etheryte <karl.tarvas@suse.com>
2021-05-16 22:29:15 +0200

    Migrate virtualization

---
+++ web/html/src/manager/virtualization/HypervisorCheck.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+
+import { Messages } from "components/messages";
+import { Utils as MessagesUtils } from "components/messages";
+
+type Props = {
+  hypervisor: string;
+  saltVirtHost: boolean;
+};
+
+export function HypervisorCheck(props: Props) {
+  if (!props.saltVirtHost) {
+    return null;
+  }
+
+  const virtMissing = props.hypervisor.includes("'virt' __virtual__ returned False");
+  if (virtMissing) {
+    return <Messages items={MessagesUtils.error(t("Please install libvirt python module."))} />;
+  }
+
+  const error =
+    props.hypervisor === ""
+      ? MessagesUtils.error(
+          t("Neither KVM nor Xen is running. Ensure they are installed and the kernel modules are loaded.")
+        )
+      : [];
+  return <Messages items={error} />;
+}
+
+HypervisorCheck.defaultProps = {
+  saltVirtHost: true,
+};
--- web/html/src/manager/virtualization/useVirtNotification.ts
+++ web/html/src/manager/virtualization/useVirtNotification.ts
@@ -1,17 +1,17 @@
-// @flow
-import * as React from 'react';
+import * as React from "react";
 
 export function useVirtNotification(
-    errors: Array<string>,
-    setErrors: Function,
-    serverId: string,
-    refresh: (type: string) => void,
-    listen: boolean = true): [Object, Function] {
+  errors: Array<string>,
+  setErrors: Function,
+  serverId: string,
+  refresh: (type: string) => void,
+  listen: boolean = true
+): [Object, Function] {
   const [actionsResults, setActionsResults] = React.useState({});
   const [webSocketErr, setWebSocketErr] = React.useState(false);
   const [pageUnloading, setPageUnloading] = React.useState(false);
 
-  let websocket = { };
+  let websocket: any = {};
 
   // FIXME this is a hack:
   // in the onmessage callback we don't see the actionsResults changes.
@@ -20,21 +20,21 @@ export function useVirtNotification(
   // I am not React-ive enough to figure out what is wrong here, leaving it
   // as is since it works... maybe someone with ReactJS super powers will see
   // the problem later.
-  let actions = {};
+  let actions: any = {};
 
-  const updateActions = (newActions: Object): void => {
+  const updateActions = (newActions: any): void => {
     actions = newActions;
     setActionsResults(newActions);
-  }
+  };
 
   const onBeforeUnload = () => {
     setPageUnloading(true);
-  }
+  };
 
   React.useEffect(() => {
     if (listen) {
       const { port } = window.location;
-      const url = `wss://${window.location.hostname}${port ? `:${port}` : ''}/rhn/websocket/minion/virt-notifications`;
+      const url = `wss://${window.location.hostname}${port ? `:${port}` : ""}/rhn/websocket/minion/virt-notifications`;
       const ws = new WebSocket(url);
 
       ws.onopen = () => {
@@ -43,22 +43,22 @@ export function useVirtNotification(
       };
 
       ws.onclose = () => {
-        setErrors((errors || []).concat(
-            !pageUnloading && !webSocketErr
-              ? t('Websocket connection closed. Refresh the page to try again.')
-              : []
-        ));
+        setErrors(
+          (errors || []).concat(
+            !pageUnloading && !webSocketErr ? t("Websocket connection closed. Refresh the page to try again.") : []
+          )
+        );
       };
 
       ws.onerror = () => {
-        setErrors([t('Error connecting to server. Refresh the page to try again.')]);
+        setErrors([t("Error connecting to server. Refresh the page to try again.")]);
         setWebSocketErr(true);
       };
 
-      ws.onmessage = (e) => {
-        if (typeof e.data === 'string') {
+      ws.onmessage = e => {
+        if (typeof e.data === "string") {
           const newActions = JSON.parse(e.data);
-          const refreshKind = newActions['refresh'];
+          const refreshKind = newActions["refresh"];
           if (refreshKind != null) {
             refresh(refreshKind);
             return;
@@ -72,35 +72,35 @@ export function useVirtNotification(
            * the one we already got before, update the existing actions by matching
            * their IDs.
            */
-          const aidMap = Object.keys(actions)
-            .reduce((res, key) => Object.assign({}, res, {[actions[key].id]: key}), {});
+          const aidMap = Object.keys(actions).reduce(
+            (res, key) => Object.assign({}, res, { [actions[key].id]: key }),
+            {}
+          );
           const updatedActions = Object.keys(newActions)
             .filter(key => Object.keys(aidMap).includes(String(newActions[key].id)))
             .reduce((res, key) => {
               const newAction = newActions[key];
-              return Object.assign({},
-                res,
-                {[aidMap[newAction.id]]: newAction});
+              return Object.assign({}, res, { [aidMap[newAction.id]]: newAction });
             }, {});
           const addedActions = Object.keys(newActions)
             .filter(key => !Object.keys(aidMap).includes(String(newActions[key].id)))
-            .reduce((res, key) => Object.assign({}, res, {[key]: newActions[key]}), {});
+            .reduce((res, key) => Object.assign({}, res, { [key]: newActions[key] }), {});
           actions = Object.assign({}, actions, updatedActions, addedActions);
           setActionsResults(actions);
         }
       };
 
-      window.addEventListener('beforeunload', onBeforeUnload);
+      window.addEventListener("beforeunload", onBeforeUnload);
 
       websocket = ws;
     }
 
     return () => {
-      window.removeEventListener('beforeunload', onBeforeUnload);
+      window.removeEventListener("beforeunload", onBeforeUnload);
       if (websocket !== null) {
         websocket.close();
       }
-    }
+    };
   }, [serverId]);
   return [actionsResults, updateActions];
 }
