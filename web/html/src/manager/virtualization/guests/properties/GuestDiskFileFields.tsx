a6d7dda1f7ddab9c63b58600835a8058094f5bcd
Etheryte <karl.tarvas@suse.com>
2021-05-16 22:26:53 +0200

    Migrate properties

--- web/html/src/manager/virtualization/guests/properties/GuestDiskFileFields.tsx
+++ web/html/src/manager/virtualization/guests/properties/GuestDiskFileFields.tsx
@@ -1,23 +1,20 @@
-// @flow
-
-import * as React from 'react';
-import { Text } from 'components/input/Text';
-import { Select } from 'components/input/Select';
-import { FormContext } from 'components/input/Form';
+import * as React from "react";
+import { Text } from "components/input/Text";
+import { Select } from "components/input/Select";
+import { FormContext } from "components/input/Form";
 
 type Props = {
-  index: number,
-  domainCaps: Object,
-  poolCaps: Object,
-  pools: Array<Object>,
-  onlyHandledDisks: boolean,
-}
+  index: number;
+  domainCaps: any;
+  poolCaps: any;
+  pools: Array<any>;
+  onlyHandledDisks: boolean;
+};
 
-export function GuestDiskFileFields(props: Props) : React.Node {
+export function GuestDiskFileFields(props: Props) {
   const formContext = React.useContext(FormContext);
 
-  const pool_options = ((props.poolCaps.pool_types.find(item => item.name === "dir") || {})
-                                  .options || {}).volume;
+  const pool_options = ((props.poolCaps.pool_types.find(item => item.name === "dir") || {}).options || {}).volume;
   const isCdrom = formContext.model[`disk${props.index}_device`] === "cdrom";
   const format_values = pool_options.targetFormatType.filter(format => format === "raw" || !isCdrom) || [];
   const default_format = isCdrom ? "raw" : "qcow2";
@@ -25,23 +22,23 @@ export function GuestDiskFileFields(props: Props) : React.Node {
     <>
       <Text
         name={`disk${props.index}_source_file`}
-        label={t('File')}
+        label={t("File")}
         disabled={!Object.keys(formContext.model).includes(`disk${props.index}_editable`)}
         labelClass="col-md-3"
         divClass="col-md-6"
       />
-      {!isCdrom &&
+      {!isCdrom && (
         <Select
           key={`disk${props.index}_format`}
           name={`disk${props.index}_format`}
-          label={t('Format')}
+          label={t("Format")}
           labelClass="col-md-3"
           divClass="col-md-6"
           disabled={!props.onlyHandledDisks || !Object.keys(formContext.model).includes(`disk${props.index}_editable`)}
           defaultValue={default_format}
           options={format_values}
         />
-      }
+      )}
     </>
   );
 }
