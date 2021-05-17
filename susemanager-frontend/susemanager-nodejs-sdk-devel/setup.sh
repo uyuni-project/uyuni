7449066833a6a1b3fdedc26339875e1c3201a4a0
Etheryte <karl.tarvas@suse.com>
2021-05-17 13:10:04 +0200

    Initial tests with flock

--- susemanager-frontend/susemanager-nodejs-sdk-devel/setup.sh
+++ susemanager-frontend/susemanager-nodejs-sdk-devel/setup.sh
@@ -1,7 +1,17 @@
 set -euxo pipefail
-(cd susemanager-frontend/susemanager-nodejs-sdk-devel; rm -rf node_modules)
-yarn install --frozen-lockfile
-yarn autoclean --force
+# This lock is shared with web/setup.sh
+(
+    # Timeout on 600s
+    flock -x -w 600 200;
+    cd susemanager-frontend/susemanager-nodejs-sdk-devel;
+    # This is a very loaded use of yarn install, please be careful when changing it
+    # TODO: remove any previously installed optionalDependencies, was --force
+    # --ignore-scripts see https://github.com/vercel/next.js/pull/23056 and https://github.com/yarnpkg/yarn/issues/696
+    # --ignore-optional to ensure optionalDependencies are not installed
+    # --frozen-lockfile to ensure consistent state
+    # --prefer-offline to prefer cache when it's available
+    yarn install --ignore-optional --ignore-scripts --frozen-lockfile --prefer-offline;
+) 200>/tmp/setup_yarn.lock
 (cd susemanager-frontend/susemanager-nodejs-sdk-devel; yarn run remove-packages-before-obs-zip)
 (cd susemanager-frontend/susemanager-nodejs-sdk-devel; yarn zip)
 (cd susemanager-frontend/susemanager-nodejs-sdk-devel; yarn run restore-packages-after-obs-zip)
--- web/setup.sh
+++ web/setup.sh
@@ -1,4 +1,10 @@
 set -euxo pipefail
-(cd web/html/src; yarn install --frozen-lockfile)
+# This lock is shared with susemanager-frontend/susemanager-nodejs-sdk-devel/setup.sh
+(
+    # Timeout on 600s
+    flock -x -w 600 200;
+    cd web/html/src;
+    yarn install --ignore-optional --ignore-scripts --frozen-lockfile --prefer-offline;
+) 200>/tmp/setup_yarn.lock
 (cd web/html/src; yarn build:novalidate)
 echo ""
