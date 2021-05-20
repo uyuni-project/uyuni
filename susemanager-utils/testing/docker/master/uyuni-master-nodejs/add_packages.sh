41a5e42c39cfcaff1e871dc3784ed25b35777dfb
Etheryte <karl.tarvas@suse.com>
2021-05-20 18:59:39 +0200

    Remove Flow

--- susemanager-utils/testing/docker/master/uyuni-master-nodejs/add_packages.sh
+++ susemanager-utils/testing/docker/master/uyuni-master-nodejs/add_packages.sh
@@ -6,6 +6,3 @@ useradd --no-create-home --shell /dev/null -c '' nobody
 
 # Packages for Javascript tests
 zypper --non-interactive in nodejs npm
-
-# Install flow-bin globally
-npm_config_prefix=/usr npm install -g flow-bin@0.82.0 yarn
