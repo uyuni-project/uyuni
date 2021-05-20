71d93b337008ab18c2703eeeaed8816187f05a0f
Cédric Bosdonnat <cbosdonnat@suse.com>
2021-05-20 15:12:21 +0200

    Don't assume virt-install will create VM with proper VNC default
    For SUSE Manager to be able to show the graphical console of a VM the VM
    config needs to listen on all addresses. In some cases virt-install and
    libvirt default to 127.0.0.1 as listen value. To prevent this enforce
    listening on 0.0.0.0

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -1154,7 +1154,7 @@ When(/^I create "([^"]*)" virtual machine on "([^"]*)"$/) do |vm_name, host|
   # Actually define the VM, but don't start it
   raise 'not found: virt-install' unless file_exists?(node, '/usr/bin/virt-install')
   node.run("virt-install --name #{vm_name} --memory 512 --vcpus 1 --disk path=#{disk_path} "\
-           "--network network=test-net0 --graphics vnc "\
+           "--network network=test-net0 --graphics vnc,listen=0.0.0.0 "\
            "--serial file,path=/tmp/#{vm_name}.console.log "\
            "--import --hvm --noautoconsole --noreboot")
 end
