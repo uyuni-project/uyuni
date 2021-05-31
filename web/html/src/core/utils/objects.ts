c7ba56c68916e7f8d986bc0a0de4c4473124f0dc
Etheryte <karl.tarvas@suse.com>
2021-05-31 12:59:53 +0200

    Deprecate objectDefaultValueHandler

--- web/html/src/core/utils/objects.ts
+++
@@ -1,8 +0,0 @@
-// TODO: This is obsolete, see https://github.com/SUSE/spacewalk/issues/13648
-export function objectDefaultValueHandler(defaultValue: any) {
-  return {
-    get: function(target: any, name: string) {
-      return target.hasOwnProperty(name) ? target[name] : defaultValue;
-    },
-  };
-}
--- web/html/src/manager/content-management/shared/business/states.enum.ts
+++ web/html/src/manager/content-management/shared/business/states.enum.ts
@@ -1,9 +1,10 @@
-import { objectDefaultValueHandler } from "core/utils/objects";
 import _find from "lodash/find";
 
 type stateType = {
   key: string;
   description: string;
+  deletion: boolean;
+  edited: boolean;
   sign: string;
 };
 
@@ -11,20 +12,15 @@ type statesEnumType = {
   [key: string]: stateType;
 };
 
-const defaultState: any = {};
+const statesEnum: statesEnumType = {
+  ATTACHED: { key: "ATTACHED", description: "added", deletion: false, edited: true, sign: "+" },
+  DETACHED: { key: "DETACHED", description: "deleted", deletion: true, edited: true, sign: "-" },
+  EDITED: { key: "EDITED", description: "edited", deletion: false, edited: true, sign: " " },
+  BUILT: { key: "BUILT", description: "built", deletion: false, edited: false, sign: " " },
+};
 
-const statesEnum: statesEnumType = new Proxy(
-  {
-    ATTACHED: { key: "ATTACHED", description: "added", deletion: false, edited: true, sign: "+" },
-    DETACHED: { key: "DETACHED", description: "deleted", deletion: true, edited: true, sign: "-" },
-    EDITED: { key: "EDITED", description: "edited", deletion: false, edited: true, sign: " " },
-    BUILT: { key: "BUILT", description: "built", deletion: false, edited: false, sign: " " },
-  },
-  objectDefaultValueHandler(defaultState)
-);
-
-function findByKey(key: string) {
-  return _find(statesEnum, entry => entry.key === key) || defaultState;
+function findByKey(key: string): stateType | Partial<stateType> {
+  return _find(statesEnum, entry => entry.key === key) || {};
 }
 
 function isDeletion(key: string) {
@@ -40,9 +36,4 @@ export default {
   findByKey,
   isDeletion,
   isEdited,
-} as {
-  enum: statesEnumType;
-  findByKey: (arg0: string) => stateType;
-  isDeletion: (arg0: string) => boolean;
-  isEdited: (arg0: string) => boolean;
 };
