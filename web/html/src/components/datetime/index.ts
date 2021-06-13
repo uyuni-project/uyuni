0fa3bf5505181efe4b48c6f491f0cdc55e798597
Etheryte <karl.tarvas@suse.com>
2021-06-13 13:09:06 +0300

    Consistent naming

--- web/html/src/components/datetime/index.ts
+++ web/html/src/components/datetime/index.ts
@@ -1,2 +1,2 @@
-export * from './DatetimePicker';
+export * from './DateTimePicker';
 export * from './FromNow';
--- web/html/src/manager/legacy/index.ts
+++ web/html/src/manager/legacy/index.ts
@@ -1,4 +1,4 @@
 /**
  * This is the entrypoint for legacy code to integrate with the new React codebase
  */
-import './DatetimePicker';
+import './DateTimePicker';
