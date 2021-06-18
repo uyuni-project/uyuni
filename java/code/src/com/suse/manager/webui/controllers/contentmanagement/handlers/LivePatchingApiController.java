be5601973cb191d852a340dbb973b69515029cde
Can Bulut Bayburt <cbbayburt@suse.com>
2021-06-18 12:20:59 +0200

    Add 'live patching based on a system' filter template

--- java/code/src/com/suse/manager/webui/controllers/contentmanagement/handlers/LivePatchingApiController.java
+++ java/code/src/com/suse/manager/webui/controllers/contentmanagement/handlers/LivePatchingApiController.java
@@ -14,18 +14,25 @@
  */
 package com.suse.manager.webui.controllers.contentmanagement.handlers;
 
+import com.redhat.rhn.common.hibernate.LookupException;
 import com.redhat.rhn.domain.product.SUSEProduct;
 import com.redhat.rhn.domain.product.SUSEProductFactory;
 import com.redhat.rhn.domain.rhnpackage.PackageEvr;
+import com.redhat.rhn.domain.server.Server;
+import com.redhat.rhn.domain.server.ServerFactory;
 import com.redhat.rhn.domain.user.User;
+import com.redhat.rhn.manager.system.SystemManager;
 import com.suse.manager.model.products.Product;
 import com.suse.manager.webui.utils.gson.ResultJson;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpStatus;
 import spark.Request;
 import spark.Response;
 import spark.Spark;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.suse.manager.webui.utils.SparkApplicationHelper.json;
@@ -49,6 +56,7 @@ public class LivePatchingApiController {
 
         /**
          * Initialize an object from an EVR
+         *
          * @param evr the kernel EVR
          */
         public KernelVersion(PackageEvr evr) {
@@ -57,12 +65,36 @@ public class LivePatchingApiController {
         }
     }
 
+    /**
+     * JSON object for basic system ID and it's running kernel version
+     */
+    public static class System {
+        private Long id;
+        private String name;
+        private String kernel;
+
+        /**
+         * Initialize an object from a server
+         *
+         * @param server the server
+         */
+        public System(Server server) {
+            this.id = server.getId();
+            this.name = server.getName();
+            this.kernel = server.getRunningKernel();
+        }
+    }
+
     /** Init routes for ContentManagement Live Patching Api.*/
     public static void initRoutes() {
         get("/manager/api/contentmanagement/livepatching/products",
                 withUser(LivePatchingApiController::getSuseProducts));
-        get("/manager/api/contentmanagement/livepatching/kernels/:productId",
-                withUser(LivePatchingApiController::getKernelVersions));
+        get("/manager/api/contentmanagement/livepatching/systems",
+                withUser(LivePatchingApiController::getSlesSystems));
+        get("/manager/api/contentmanagement/livepatching/kernels/product/:productId",
+                withUser(LivePatchingApiController::getProductKernels));
+        get("/manager/api/contentmanagement/livepatching/kernels/system/:systemId",
+                withUser(LivePatchingApiController::getSystemKernels));
     }
 
     /**
@@ -81,6 +113,23 @@ public class LivePatchingApiController {
     }
 
     /**
+     * Get all SLES systems available to user
+     *
+     * @param req the http request
+     * @param res the http response
+     * @param user the current user
+     * @return the JSON data
+     */
+    public static String getSlesSystems(Request req, Response res, User user) {
+        String query = StringUtils.defaultString(req.queryParams("q"));
+        List<System> slesSystems = ServerFactory.querySlesSystems(query, 20, user)
+                .map(System::new)
+                .collect(Collectors.toList());
+
+        return json(res, ResultJson.success(slesSystems));
+    }
+
+    /**
      * Get all kernel versions contained in a SUSE root product
      *
      * @param req the http request
@@ -88,11 +137,14 @@ public class LivePatchingApiController {
      * @param user the current user
      * @return the JSON data
      */
-    public static String getKernelVersions(Request req, Response res, User user) {
+    public static String getProductKernels(Request req, Response res, User user) {
         SUSEProduct product;
         try {
             long productId = Long.parseLong(req.params("productId"));
             product = SUSEProductFactory.getProductById(productId);
+            if (product == null) {
+                throw Spark.halt(HttpStatus.SC_NOT_FOUND);
+            }
         }
         catch (NumberFormatException e) {
             throw Spark.halt(HttpStatus.SC_BAD_REQUEST);
@@ -105,5 +157,39 @@ public class LivePatchingApiController {
                         .map(KernelVersion::new);
 
         return json(res, ResultJson.success(kernelsInProductTree.collect(toList())));
+    }
+
+    /**
+     * Get all kernel versions installed in a system
+     *
+     * @param req the http request
+     * @param res the http response
+     * @param user the current user
+     * @return the JSON data
+     */
+    public static String getSystemKernels(Request req, Response res, User user) {
+        Server server;
+        try {
+            long systemId = Long.parseLong(req.params("systemId"));
+            server = ServerFactory.lookupByIdAndOrg(systemId, user.getOrg());
+            if (server == null) {
+                throw Spark.halt(HttpStatus.SC_NOT_FOUND);
+            }
+            SystemManager.ensureAvailableToUser(user, systemId);
+        }
+        catch (NumberFormatException e) {
+            throw Spark.halt(HttpStatus.SC_BAD_REQUEST);
+        }
+        catch (LookupException e) {
+            throw Spark.halt(HttpStatus.SC_BAD_REQUEST, e.getLocalizedTitle());
+        }
+
+        // Get kernel versions ordered from latest to earliest
+        Stream<KernelVersion> kernelsInSystem =
+                ServerFactory.getInstalledKernelVersions(server)
+                        .sorted(Comparator.reverseOrder())
+                        .map(KernelVersion::new);
+
+        return json(res, ResultJson.success(kernelsInSystem.collect(toList())));
     }
 }
