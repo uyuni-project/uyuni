35e0ae91d28c0b78c162725090685c4aa4223484
Frantisek Kobzik <hustodemon@users.noreply.github.com>
2021-07-07 12:11:03 +0200

    Backend: Initialize bootstrap optional params

--- java/code/src/com/suse/manager/webui/utils/gson/BootstrapParameters.java
+++ java/code/src/com/suse/manager/webui/utils/gson/BootstrapParameters.java
@@ -14,6 +14,7 @@
  */
 package com.suse.manager.webui.utils.gson;
 
+import static java.util.Optional.empty;
 import static java.util.Optional.of;
 import com.suse.manager.webui.utils.gson.BootstrapHostsJson.AuthMethod;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -30,10 +31,10 @@ public class BootstrapParameters {
     private String host;
     private Optional<Integer> port;
     private String user;
-    private Optional<String> password;
-    private Optional<String> privateKey;
-    private Optional<String> privateKeyPassphrase;
-    private Optional<Long> ansibleInventoryId;
+    private Optional<String> password = empty();
+    private Optional<String> privateKey = empty();
+    private Optional<String> privateKeyPassphrase = empty();
+    private Optional<Long> ansibleInventoryId = empty();
     private List<String> activationKeys;
     private Optional<String> reactivationKey;
     private boolean ignoreHostKeys;
@@ -58,8 +59,6 @@ public class BootstrapParameters {
         this.port = portIn;
         this.user = userIn;
         this.password = passwordIn;
-        this.privateKey = Optional.empty();
-        this.privateKeyPassphrase = Optional.empty();
         this.activationKeys = activationKeysIn;
         this.reactivationKey = reactivationKeyIn;
         this.ignoreHostKeys = ignoreHostKeysIn;
@@ -85,7 +84,6 @@ public class BootstrapParameters {
         this.host = hostIn;
         this.port = portIn;
         this.user = userIn;
-        this.password = Optional.empty();
         this.privateKey = of(privateKeyIn);
         this.privateKeyPassphrase = privateKeyPwdIn;
         this.activationKeys = activationKeysIn;
@@ -110,9 +108,6 @@ public class BootstrapParameters {
         this.host = hostIn;
         this.port = portIn;
         this.user = userIn;
-        this.password = Optional.empty();
-        this.privateKey = Optional.empty();
-        this.privateKeyPassphrase = Optional.empty();
         this.ansibleInventoryId = of(ansibleInventoryIdIn);
         this.activationKeys = activationKeysIn;
         this.ignoreHostKeys = ignoreHostKeysIn;
