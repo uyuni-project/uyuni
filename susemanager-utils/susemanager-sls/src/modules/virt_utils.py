5a47f07dc794da704e8890c83233bee6ddc4ad36
Cédric Bosdonnat <cbosdonnat@suse.com>
2021-08-02 09:27:42 +0200

    Handle more OCFS2 config in virt_utils
    The OCFS2 primitive may be nested at various levels with a group or not.
    Look for children of the <resources> element to get the proper name for
    the constraint configuration in VM config.

--- susemanager-utils/susemanager-sls/src/modules/virt_utils.py
+++ susemanager-utils/susemanager-sls/src/modules/virt_utils.py
@@ -46,17 +46,18 @@ def get_cluster_filesystem(path):
             ).communicate()[0]
         )
 
-        for resource in crm_conf.findall(".//primitive[@type='Filesystem']/.."):
-            directory = resource.find("./primitive//nvpair[@name='directory']")
-            if directory is None:
-                continue
-            directory_value = directory.get("value")
-            if directory_value:
-                if (
-                    os.path.commonpath([str(resolved), directory_value])
-                    == directory_value
-                ):
-                    return resource.get("id")
+        for resource in crm_conf.findall(".//resources/*"):
+            if resource.find(".//primitive[@type='Filesystem']/") is not None:
+                directory = resource.find(".//primitive//nvpair[@name='directory']")
+                if directory is None:
+                    continue
+                directory_value = directory.get("value")
+                if directory_value:
+                    if (
+                        os.path.commonpath([str(resolved), directory_value])
+                        == directory_value
+                    ):
+                        return resource.get("id")
     except FileNotFoundError as err:
         log.debug("Failed to get cluster resource name for path, %s: %s", path, err)
 
