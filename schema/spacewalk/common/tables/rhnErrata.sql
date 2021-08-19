34ba948bcd787c59445dd9e559a1d70a16dcfa21
Frantisek Kobzik <fkobzik@suse.de>
2021-08-19 10:59:18 +0200

    Support syncing patches with advisory status 'pending'
    (cherry picked from commit 7691083c85a360e0404e7f4b6a406a582ac316f3)

--- schema/spacewalk/common/tables/rhnErrata.sql
+++ schema/spacewalk/common/tables/rhnErrata.sql
@@ -30,7 +30,7 @@ CREATE TABLE rhnErrata
     advisory_status   VARCHAR(32) NOT NULL DEFAULT('final')
                           CONSTRAINT rhn_errata_adv_status_ck
                               CHECK (advisory_status in ('final', 'stable', 'testing',
-                                                         'retracted')),
+                                                         'pending', 'retracted')),
     product           VARCHAR(64) NOT NULL,
     description       VARCHAR(4000),
     synopsis          VARCHAR(4000) NOT NULL,
