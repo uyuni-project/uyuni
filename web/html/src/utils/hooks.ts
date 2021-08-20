9a21e5661c5aae2af064f6e33e4c7f96e4d833e1
Etheryte <karl.tarvas@suse.com>
2021-08-20 16:36:37 +0200

    Update commentary

--- web/html/src/utils/hooks.ts
+++ web/html/src/utils/hooks.ts
@@ -1,5 +1,9 @@
 import { useEffect, useRef } from "react";
 
+/**
+ * Get the value of a variable from the previous render of a functional component, initially returns undefined.
+ * Can be used for variables, props, state, or anything really.
+ */
 export function usePrevious<T>(value: T) {
   const ref = useRef<T>();
   useEffect(() => {
