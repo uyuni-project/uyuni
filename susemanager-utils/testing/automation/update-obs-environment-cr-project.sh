a27df3eb9fda41805fbc67110cb5b20db038892e
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2021-09-15 12:04:35 +0200

    [CI] Refactor: Extract project names to variables
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- susemanager-utils/testing/automation/update-obs-environment-cr-project.sh
+++ susemanager-utils/testing/automation/update-obs-environment-cr-project.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+parent_project="systemsmanagement:Uyuni:Master"
+
 if [ $# -ne 1 ];then
     echo "usage: $0 N"
     echo "where N is the environment"
-    echo "$0 will add new packages in systemsmanagement:Uyuni:Master to systemsmanagement:Uyuni:Master:TEST:N:CR"
+    echo "$0 will add new packages in ${parent_project} to ${parent_project}:N:CR"
     exit -1
 fi    
 
+test_parent_project="${parent_project}:TEST:${1}:CR"
 
-for i in $(diff <( osc ls systemsmanagement:Uyuni:Master:TEST:$1:CR ) <( osc ls systemsmanagement:Uyuni:Master ) | grep ">" | cut -d" " -f2);do
-    echo "Found new package $i in systemsmanagement:Uyuni:Master. Creating a link to systemsmanagement:Uyuni:Master:TEST:$1:CR"
-    osc linkpac systemsmanagement:Uyuni:Master $i systemsmanagement:Uyuni:Master:TEST:$1:CR
+for i in $(diff <( osc ls ${test_parent_project} ) <( osc ls ${parent_project} ) | grep ">" | cut -d" " -f2);do
+    echo "Found new package $i in ${parent_project}. Creating a link to ${test_parent_project}"
+    osc linkpac ${parent_project} $i ${test_parent_project}
 done
     
