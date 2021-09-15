f8751b2f19874a72a180e124e9cd594ca3815f8f
Etheryte <karl.tarvas@suse.com>
2021-09-15 18:58:32 +0200

    Normalize labels in tests to avoid regex

--- web/html/src/utils/test-utils/screen.ts
+++ web/html/src/utils/test-utils/screen.ts
@@ -1,4 +1,4 @@
-import { screen as rawScreen, Screen } from "@testing-library/react";
+import { screen as rawScreen, Screen, getDefaultNormalizer } from "@testing-library/react";
 
 // Utility type, if a function TargetFunction returns a Promise, return an intersection with Promise<T>, otherwise with T
 type ReturnFromWith<TargetFunction extends (...args: any[]) => any, T> = ReturnType<TargetFunction> extends Promise<unknown>
@@ -15,6 +15,28 @@ type GenericScreen = {
         ? <T extends unknown>(...args: Parameters<Screen[Key]>) => ReturnFromWith<Screen[Key], T>
         : Screen[Key];
 };
+
+/**
+ * Labels (as in `components/input/Label.tsx`) often have a required suffix "*" and a generic display suffix ":".
+ * Instead of cluttering the DOM there, we account for it in the tests by using a normalizer here.
+ * See: https://testing-library.com/docs/queries/about/#normalization
+ */
+const defaultNormalizer = getDefaultNormalizer();
+const labelNormalizer = (input: string) => {
+    return defaultNormalizer(input)
+    .replace(/:$/, "") // Remove trailing ":"
+    .replace(/ \*$/, ""); // Remove trailing " *"
+};
+
+const getByLabelText = rawScreen.getByLabelText;
+type GetByLabelTextArgs = Parameters<typeof rawScreen.getByLabelText>;
+Object.assign(rawScreen, {
+    getByLabelText: (...[text, options, waitForElementOptions]: GetByLabelTextArgs) => {
+        options ??= {};
+        options.normalizer ??= labelNormalizer;
+        return getByLabelText(text, options, waitForElementOptions);
+    },
+} as Partial<Screen>);
 
 const screen = rawScreen as GenericScreen;
 
