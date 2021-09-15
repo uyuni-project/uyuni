5b10a785b99a61b7802146cc878001facb3ac130
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2021-09-15 12:13:06 +0200

    [CI] Add optional parameter for updating a dev project
    This will help us test

--- susemanager-utils/testing/automation/update-obs-environment-cr-project.sh
+++ susemanager-utils/testing/automation/update-obs-environment-cr-project.sh
@@ -2,14 +2,26 @@
 
 parent_project="systemsmanagement:Uyuni:Master"
 
-if [ $# -ne 1 ];then
-    echo "usage: $0 N"
+usage_and_exit() {
+    echo "usage: $0 N [test_project]"
     echo "where N is the environment"
-    echo "$0 will add new packages in ${parent_project} to ${parent_project}:N:CR"
+    echo "and project is the test_project, which by default is ${parent_project}:N:CR" 
+    echo "$0 will add new packages from ${parent_project} to test_project"
     exit -1
-fi    
+}
+
+if [ $# -lt 1 ];then
+    usage_and_exit
+fi
+if [ $# -gt 2 ];then
+    usage_and_exit
+fi
 
 test_parent_project="${parent_project}:TEST:${1}:CR"
+
+if [ $# -eq 2 ];then
+    test_parent_project=${2}
+fi
 
 for i in $(diff <( osc ls ${test_parent_project} ) <( osc ls ${parent_project} ) | grep ">" | cut -d" " -f2);do
     echo "Found new package $i in ${parent_project}. Creating a link to ${test_parent_project}"
