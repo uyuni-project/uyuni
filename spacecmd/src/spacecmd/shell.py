0c3981f160059250a3d4a04bd69f1b144f80b521
sbluhm <stefan.bluhm@clacee.eu>
2021-09-29 07:45:14 +0200

    Used pylint name instead of id

--- spacecmd/src/spacecmd/shell.py
+++ spacecmd/src/spacecmd/shell.py
@@ -119,7 +119,7 @@ class SpacewalkShell(Cmd):
                                     self.history_file)
                 except IOError:
                     logging.error(_N('Could not read history file'))
-        # pylint: disable=W0703
+        # pylint: disable=broad-except
         except Exception as exc:
             logging.error(_N("Exception occurred: %s"), exc)
             sys.exit(1)
