644ee7a5b93f1e8bee9ef2877daa8da16ee68348
Cédric Bosdonnat <cbosdonnat@suse.com>
2021-10-04 09:18:26 +0200

    Remove unused and rotten rhnSQL unit tests

--- backend/test/docker-backend-server-tests.sh
+++ backend/test/docker-backend-server-tests.sh
@@ -2,4 +2,4 @@
 
 touch /etc/rhn/rhn.conf
 mkdir -p /manager/backend/reports
-pytest --verbose --junit-xml /manager/backend/reports/server_tests.xml --ignore '/manager/backend/server/test/unit-test/rhnSQL/' -s /manager/backend/server/test/unit-test/
+pytest --verbose --junit-xml /manager/backend/reports/server_tests.xml  -s /manager/backend/server/test/unit-test/
