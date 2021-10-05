2c4f9147fecd7e246daa9b36ec3b7b174b28f471
Cédric Bosdonnat <cbosdonnat@suse.com>
2021-10-05 14:28:11 +0200

    Move spacewalk/server unit tests to a test folder

--- python/spacewalk/test/docker-backend-server-tests.sh
+++ python/spacewalk/test/docker-backend-server-tests.sh
@@ -2,4 +2,4 @@
 
 touch /etc/rhn/rhn.conf
 mkdir -p /manager/python/spacewalk/reports
-pytest --verbose --junit-xml /manager/python/spacewalk/reports/server_tests.xml -s /manager/python/spacewalk/server/test/unit-test/
+pytest --verbose --junit-xml /manager/python/spacewalk/reports/server_tests.xml -s /manager/python/test/unit/spacewalk/server
