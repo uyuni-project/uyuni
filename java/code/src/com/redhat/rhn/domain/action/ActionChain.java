49adeb5a6291c931b4b0fbd03f5fdf744b451c30
Kevin Walter <kwalter@suse.de>
2021-10-18 15:39:40 +0200

    don't consider empty action chains done (bsc#1191377)

--- java/code/src/com/redhat/rhn/domain/action/ActionChain.java
+++ java/code/src/com/redhat/rhn/domain/action/ActionChain.java
@@ -115,7 +115,8 @@ public class ActionChain extends BaseDomainHelper {
     public boolean isDone() {
         // check if all Actions has been executed (there must be an entry in ServerActions)
         // and all the ServerActions are Done
-        return getEntries().stream().allMatch(ace -> !ace.getAction().getServerActions().isEmpty()) &&
+        return !getEntries().isEmpty() &&
+                getEntries().stream().noneMatch(ace -> ace.getAction().getServerActions().isEmpty()) &&
                 getEntries().stream().flatMap(ace -> ace.getAction().getServerActions().stream())
                 .allMatch(sa -> sa.getStatus().isDone());
     }
