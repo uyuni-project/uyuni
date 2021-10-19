14d71c50c0a99a62e188aef85afbd68d7054780b
Witek Bedyk <wbedyk@suse.com>
2021-10-19 18:32:13 +0200

    Run Prometheus JMX exporter as Java agent (#4268)
    Change the self-monitoring management of Uyuni's Java components (Tomcat and Taskomatic).
    Prometheus JMX exporter is now run as Java agent which allows to avoid exposing network ports for remote JMX monitoring.
    Additionally Salt states have been refactored to consistently manage drop-in systemd configuration files instead of `sed` string manipulation of configuration files. This allows to run Salt state during `susemanager-sls` package upgrade to migrate the configuration to the current implementation without additional user interaction.

--- java/code/src/com/redhat/rhn/taskomatic/TaskoXmlRpcHandler.java
+++ java/code/src/com/redhat/rhn/taskomatic/TaskoXmlRpcHandler.java
@@ -21,7 +21,6 @@ import com.redhat.rhn.taskomatic.domain.TaskoBunch;
 import com.redhat.rhn.taskomatic.domain.TaskoRun;
 import com.redhat.rhn.taskomatic.domain.TaskoSchedule;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
@@ -641,10 +640,15 @@ public class TaskoXmlRpcHandler {
 
     /**
      * Check if JMX is enabled.
-     * @return true if JMX system props props
+     * @return true if JavaAgent class can be found
      */
-    public boolean isJmxEnabled() {
-        return StringUtils.isNotEmpty(System.getProperty("com.sun.management.jmxremote.port")) &&
-                StringUtils.isNotEmpty(System.getProperty("java.rmi.server.hostname"));
+    public static boolean isJmxEnabled() {
+        try {
+            Class.forName("io.prometheus.jmx.shaded.io.prometheus.jmx.JavaAgent");
+        }
+        catch (ClassNotFoundException ex) {
+            return false;
+        }
+        return true;
     }
 }
