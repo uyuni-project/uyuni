623e5420401d488c988ccce1990afa5dd619e976
Michael Calmer <mc@suse.de>
2021-10-27 13:25:06 +0200

    enforce an order for tokens from multiple mirror credentials

--- java/code/src/com/redhat/rhn/domain/scc/SCCRepository.java
+++ java/code/src/com/redhat/rhn/domain/scc/SCCRepository.java
@@ -275,7 +275,10 @@ public class SCCRepository extends BaseDomainHelper {
                     return Optional.of(a);
                 }
             }
-            result = Optional.of(a);
+            if (result.isEmpty() || a.getId().longValue() < result.get().getId().longValue()) {
+                // get always the same result and use the oldest alternative if there are multiple
+                result = Optional.of(a);
+            }
         }
         return result;
     }
