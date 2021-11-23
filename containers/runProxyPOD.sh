ddb0411aa344637d1b4ab231b4703c8016a7561c
Silvio Moioli <moio@suse.com>
2021-11-23 10:57:08 +0100

    Introduce registry in workflow
    Signed-off-by: Silvio Moioli <moio@suse.com>

--- containers/runProxyPOD.sh
+++ containers/runProxyPOD.sh
@@ -6,11 +6,17 @@ CURDIR=$(dirname $(realpath $0))
 CONFIG_DIR="$CURDIR/proxy-config"
 RUNNER="sudo podman"
 
+export REGISTRY=registry.tf.local
+
 $RUNNER pod create --name uyuni_proxy_pod \
         -p 80:80 \
         -p 443:443 \
         -p 4505:4505 \
         -p 4506:4506
+
+$RUNNER pull --tls-verify=false $REGISTRY/proxy-main
+$RUNNER pull --tls-verify=false $REGISTRY/proxy-salt-broker
+$RUNNER pull --tls-verify=false $REGISTRY/proxy-squid
 
 $RUNNER run --rm=true -dt --pod uyuni_proxy_pod \
 	--env-file=$CONFIG_DIR/environment \
@@ -20,18 +26,17 @@ $RUNNER run --rm=true -dt --pod uyuni_proxy_pod \
 	-v proxy_log:/var/log \
 	-v proxy_proxy:/var/spool/rhn-proxy \
 	--name uyuni_proxy_main \
-        localhost/proxy-main
+        $REGISTRY/proxy-main
 
 $RUNNER run --rm=true -dt --pod uyuni_proxy_pod \
 	--env-file=$CONFIG_DIR/environment \
 	-v proxy_log:/var/log \
         --name uyuni_proxy_salt_broker \
-	localhost/proxy-salt-broker
+	$REGISTRY/proxy-salt-broker
 
 $RUNNER run --rm=true -dt --pod uyuni_proxy_pod \
 	--env-file=$CONFIG_DIR/environment \
 	-v proxy_squid:/var/cache/squid \
 	-v proxy_log:/var/log \
         --name uyuni_proxy_squid \
-        localhost/proxy-squid
-
+        $REGISTRY/proxy-squid
