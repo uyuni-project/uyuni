bd0b6033908a1d4814cd627019ee66fbd5360990
Johannes Renner <jrenner@suse.com>
2021-11-24 11:34:02 +0100

    Fixup! Use uyuni roster except for bootstrap and async calls

--- java/code/src/com/suse/manager/webui/services/impl/SaltSSHService.java
+++ java/code/src/com/suse/manager/webui/services/impl/SaltSSHService.java
@@ -646,7 +646,6 @@ public class SaltSSHService {
         return callSyncSSHInternal(call, target, roster, ignoreHostKeys, sudo, Optional.empty());
     }
 
-
     private <T> Map<String, Result<SSHResult<T>>> callSyncSSHInternal(LocalCall<T> call,
             SSHTarget target, Optional<SaltRoster> roster, boolean ignoreHostKeys, boolean sudo,
             Optional<String> extraFilerefs) throws SaltException {
