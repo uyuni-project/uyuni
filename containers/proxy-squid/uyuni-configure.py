e912205c80f7b075db21511d07d73e7606f94d65
dleidi <dleidi@suse.com>
2021-12-01 15:27:48 +0100

    Rename variables and parameters

--- containers/proxy-squid/uyuni-configure.py
+++ containers/proxy-squid/uyuni-configure.py
@@ -10,15 +10,15 @@ with open("/etc/uyuni/config.yaml") as source:
 
     # read to existing config file
     with open("/etc/squid/squid.conf", "r") as dest:
-        fileContent = dest.read()
+        file_content = dest.read()
 
         # adapt the config file
-        fileContent = re.sub(r"cache_dir ufs .*", f"cache_dir ufs /var/cache/squid {str(config['squid_size'])} 16 256", fileContent)
-        fileContent = re.sub(r"access_log .*", "access_log stdio:/proc/self/fd/1 squid", fileContent)
+        file_content = re.sub(r"cache_dir ufs .*", f"cache_dir ufs /var/cache/squid {str(config['max_cache_size_mb'])} 16 256", file_content)
+        file_content = re.sub(r"access_log .*", "access_log stdio:/proc/self/fd/1 squid", file_content)
 
         # write to file
         with open("/etc/squid/squid.conf", "w") as dest:
-            dest.write(fileContent)
+            dest.write(file_content)
 
 # make sure "squid" is the user and group owner of the cache squid path
 os.system('chown -R squid:squid /var/cache/squid')
