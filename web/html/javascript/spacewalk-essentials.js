83d26c798be8d020912f82af5b0fc2e7321a3930
Frantisek Kobzik <hustodemon@users.noreply.github.com>
2021-12-02 10:43:32 +0100

    Respect the preferred locale when doing the momentjs magic on "humanizing" dates
    This only affects <time /> with class 'human-from' and 'human-calendar'.

--- web/html/javascript/spacewalk-essentials.js
+++ web/html/javascript/spacewalk-essentials.js
@@ -263,6 +263,18 @@ function formFocus(form, name) {
 
 // Humanizes all the time elements with the human class
 function humanizeDates() {
+  // should be consistent with UserPreferencesUtils.java
+  moment.lang(window.preferredLocale, {
+    longDateFormat : {
+      LT: "HH:mm",
+      LTS: "HH:mm:ss",
+      L: "YYYY-MM-DD",
+      LL: "YYYY-MM-DD",
+      LLL: "YYYY-MM-DD",
+      LLLL: "YYYY-MM-DD"
+    },
+  });
+
   jQuery("time.human-from, time.human-calendar").each(function (index) {
     var datetime = jQuery(this).attr('datetime');
     if (datetime == undefined) {
