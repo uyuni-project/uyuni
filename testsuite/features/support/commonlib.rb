2f96ed1e340ff227a4d8859561262c5ec25d10f3
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2021-12-02 12:25:19 +0000

    Adjust POS image name computation for Uyuni

--- testsuite/features/support/commonlib.rb
+++ testsuite/features/support/commonlib.rb
@@ -60,6 +60,7 @@ def compute_image_name
   when 'sles15sp3', 'sles15sp3o'
     # 'POS_Image_JeOS7_42' for 4.2 branch
     'POS_Image_JeOS7_head'
+    $product == 'Uyuni' ? 'POS_Image_JeOS7_uyuni' : 'POS_Image_JeOS7_head'
   when 'sles15sp2', 'sles15sp2o'
     'POS_Image_JeOS7_41'
   when 'sles15sp1', 'sles15sp1o'
