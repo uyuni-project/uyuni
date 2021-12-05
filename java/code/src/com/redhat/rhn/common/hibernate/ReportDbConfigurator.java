15548207c66516da228bf35bd7a986d83fa4c6ac
Michael Calmer <mc@suse.de>
2021-12-05 11:00:36 +0100

    initial

---
+++ java/code/src/com/redhat/rhn/common/hibernate/ReportDbConfigurator.java
@@ -0,0 +1,13 @@
+package com.redhat.rhn.common.hibernate;
+
+import org.hibernate.cfg.Configuration;
+
+public class ReportDbConfigurator implements Configurator {
+
+    @Override
+    public void addConfig(Configuration c) {
+        // TODO Auto-generated method stub
+        c.addProperties(null);
+    }
+
+}
