c507033baca561444a966035b05d8d61278309cb
Thomas Florio <thomas.florio@suse.com>
2021-12-06 11:15:10 +0100

    Code cleanup

--- java/code/src/com/suse/manager/webui/services/impl/test/SaltSSHServiceTest.java
+++ java/code/src/com/suse/manager/webui/services/impl/test/SaltSSHServiceTest.java
@@ -1,6 +1,21 @@
+/**
+ * Copyright (c) 2017--2021 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package com.suse.manager.webui.services.impl.test;
 
 import com.redhat.rhn.common.conf.Config;
+import com.redhat.rhn.domain.server.ServerPath;
 import com.redhat.rhn.testing.JMockBaseTestCaseWithUser;
 import com.suse.manager.webui.services.impl.SaltSSHService;
 
@@ -8,7 +23,9 @@ import org.jmock.imposters.ByteBuddyClassImposteriser;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 /**
  * Tests for SaltSSHService.
@@ -29,7 +46,7 @@ public class SaltSSHServiceTest extends JMockBaseTestCaseWithUser {
     }
 
     public void testProxyCommandSSHPush1Proxy() {
-        Optional<String> res = SaltSSHService.sshProxyCommandOption(Arrays.asList("proxy1"), "ssh-push", "minion");
+        Optional<String> res = SaltSSHService.sshProxyCommandOption(List.of("proxy1"), "ssh-push", "minion");
         assertTrue(res.isPresent());
         assertEquals(
                 "ProxyCommand='" +
@@ -38,7 +55,8 @@ public class SaltSSHServiceTest extends JMockBaseTestCaseWithUser {
     }
 
     public void testProxyCommandSSHPushTunnel1Proxy() {
-        Optional<String> res = SaltSSHService.sshProxyCommandOption(Arrays.asList("proxy1"), "ssh-push-tunnel", "minion");
+        Optional<String> res = SaltSSHService.sshProxyCommandOption(
+                List.of("proxy1"), "ssh-push-tunnel", "minion");
         assertTrue(res.isPresent());
         assertEquals(
                 "ProxyCommand='" +
@@ -70,4 +88,23 @@ public class SaltSSHServiceTest extends JMockBaseTestCaseWithUser {
                 res.get());
     }
 
+    public void testProxyPathToHostnames() {
+        final Set<ServerPath> serverPaths = Set.of(
+                new ServerPath(1L, "rhn"),
+                new ServerPath(0L, "junit"),
+                new ServerPath(2L, "test")
+        );
+
+        assertEquals(List.of("test", "rhn", "junit", "unitTest"),
+                SaltSSHService.proxyPathToHostnames(serverPaths, Optional.of("unitTest")));
+
+        assertEquals(List.of("unitTest"),
+                SaltSSHService.proxyPathToHostnames(Collections.emptySet(), Optional.of("unitTest")));
+
+        assertEquals(List.of("test", "rhn", "junit"),
+                SaltSSHService.proxyPathToHostnames(serverPaths, Optional.empty()));
+
+        assertEquals(Collections.emptyList(),
+                SaltSSHService.proxyPathToHostnames(Collections.emptySet(), Optional.empty()));
+    }
 }
