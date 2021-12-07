40911fd961f57d5b656d894c77361f9190fc862f
Michael Calmer <mc@suse.de>
2021-12-07 16:35:32 +0100

    configure new task in DB

--- schema/spacewalk/common/data/rhnTaskoSchedule.sql
+++ schema/spacewalk/common/data/rhnTaskoSchedule.sql
@@ -156,4 +156,9 @@ INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
         (SELECT id FROM rhnTaskoBunch WHERE name='notifications-cleanup-bunch'),
         current_timestamp, '0 0 0 ? * *');
 
+INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
+    VALUES (sequence_nextval('rhn_tasko_schedule_id_seq'), 'update-reporting-default',
+        (SELECT id FROM rhnTaskoBunch WHERE name='mgr-update-reporting-bunch'),
+        current_timestamp, '0 0 0 ? * *');
+
 commit;
