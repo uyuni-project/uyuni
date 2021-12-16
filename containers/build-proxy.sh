dd6ffa08f7bc6c66ce0bff1159d37ca8a9915fc6
Silvio Moioli <silvio@moioli.net>
2021-12-16 12:49:20 +0100

    Improve hack to be more explicit with base image names
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- containers/build-proxy.sh
+++ containers/build-proxy.sh
@@ -6,13 +6,10 @@ REGISTRY=registry.tf.local
 
 IMAGES=(proxy-httpd proxy-salt-broker proxy-squid proxy-tftpd)
 
-echo "**** PULLING BASE IMAGE ****"
-podman pull registry.suse.com/suse/sle15:15.3
-
 for image in "${IMAGES[@]}"
 do
     echo "**** BUILDING $image ****"
-    podman build -f $image/Dockerfile -t $image -t $REGISTRY/$image ..
+    podman build -f $image/Dockerfile --build-arg BASE_IMAGE_PREFIX=registry.suse.com/ -t $image -t $REGISTRY/$image ..
 done
 
 for image in "${IMAGES[@]}"
