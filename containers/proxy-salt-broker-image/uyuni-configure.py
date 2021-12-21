34aae5ccd4164cca4fe812bc923cc8ea76031541
dleidi <dleidi@suse.com>
2021-12-21 17:04:18 +0100

    Make sure salt-broker container is configured to send logs to stdout/stderr

--- containers/proxy-salt-broker-image/uyuni-configure.py
+++ containers/proxy-salt-broker-image/uyuni-configure.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
- 
+
+import re
 import yaml
 
 # read from file
@@ -9,3 +10,13 @@ with open("/etc/uyuni/config.yaml") as source:
     # write to file
     with open("/etc/rhn/rhn.conf", "w") as dest:
        dest.write(f"rhn_parent={config['server']}")
+
+    # read to existing config file
+    with open("/etc/salt/broker", "r+") as config_file:
+        file_content = config_file.read()
+        # make sure to send logs to stdout/stderr instead to file
+        file_content = re.sub(r"log_to_file: .*", "log_to_file: 0", file_content)
+        # writing back the content
+        config_file.seek(0,0)
+        config_file.write(file_content)
+        config_file.truncate()
