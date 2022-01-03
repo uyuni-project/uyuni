0f31f664ae21e9f4fd7f9efdc402dcf93371743c
Karl Tarvas <Etheryte@users.noreply.github.com>
2022-01-03 10:38:30 +0100

    Drop spacewalk-login.js (#4627)

--- web/html/javascript/spacewalk-login.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-  jQuery("aside").remove();
-  var me = jQuery("section");
-  var newMe = jQuery("<div class='login-page'>");
-  newMe.html(me.html());
-  me.replaceWith(newMe);
-  formFocus('loginForm', 'username');
-});
-
