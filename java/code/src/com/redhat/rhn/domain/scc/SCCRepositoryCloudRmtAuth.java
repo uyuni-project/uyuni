695e6e4612cae9d9587448403d143fabe9e0f564
Ricardo Mateus <rmateus@suse.com>
2022-01-03 13:38:57 +0000

    JAVA: Database transformation
    Signed-off-by: Ricardo Mateus <rmateus@suse.com>
    
    PAYG: update get best authentication for repo
    
    Signed-off-by: Ricardo Mateus <rmateus@suse.com>

--- java/code/src/com/redhat/rhn/domain/scc/SCCRepositoryCloudRmtAuth.java
+++ java/code/src/com/redhat/rhn/domain/scc/SCCRepositoryCloudRmtAuth.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 SUSE LLC
+ * Copyright (c) 2021 SUSE LLC
  *
  * This software is licensed to you under the GNU General Public License,
  * version 2 (GPLv2). There is NO WARRANTY for this software, express or
@@ -12,58 +12,40 @@
  * granted to use or replicate Red Hat trademarks that are incorporated
  * in this software or its documentation.
  */
+
 package com.redhat.rhn.domain.scc;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 
-import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Transient;
 
 /**
- * This is a SUSE repository as parsed from JSON coming in from SCC.
+ * This is a SUSE repository loaded from a PAYG instance and linked to a cloud rmt server
  */
-@Entity(name = "TokenAuth")
-@DiscriminatorValue("tokenauth")
-public class SCCRepositoryTokenAuth extends SCCRepositoryAuth {
+@Entity(name = "CloudRmt")
+@DiscriminatorValue("cloudrmt")
+public class SCCRepositoryCloudRmtAuth extends SCCRepositoryAuth {
 
     // Logger instance
-    private static Logger log = Logger.getLogger(SCCRepositoryTokenAuth.class);
+    private static Logger log = Logger.getLogger(SCCRepositoryCloudRmtAuth.class);
 
-    private String auth;
+    private static final String MIRRCRED_QUERY = "credentials=mirrcred_";
 
     /**
      * Default Constructor
      */
-    public SCCRepositoryTokenAuth() { }
-
-    /**
-     * Constructor
-     * @param authIn e.g. token
-     */
-    public SCCRepositoryTokenAuth(String authIn) {
-        auth = authIn;
-    }
-
-    /**
-     * @return the auth string
-     */
-    @Column
-    public String getAuth() {
-        return auth;
-    }
-
-    /**
-     * @param authIn the auth string
-     */
-    public void setAuth(String authIn) {
-        auth = authIn;
-    }
+    public SCCRepositoryCloudRmtAuth() { }
 
     /**
      * @return the URL including authentication info
@@ -72,8 +54,15 @@ public class SCCRepositoryTokenAuth extends SCCRepositoryAuth {
     public String getUrl() {
         try {
             URI url = new URI(getRepo().getUrl());
-            URI newURI = new URI(url.getScheme(), url.getUserInfo(), url.getHost(), url.getPort(),
-                    url.getPath(), getAuth(), url.getFragment());
+            URI credUrl = new URI(getCredentials().getUrl());
+
+            List<String> sourceParams = new ArrayList<String>(Arrays.asList(
+                    StringUtils.split(Optional.ofNullable(url.getQuery()).orElse(""), '&')));
+            sourceParams.add(MIRRCRED_QUERY + getCredentials().getId());
+            String newQuery = StringUtils.join(sourceParams, "&");
+
+            URI newURI = new URI(credUrl.getScheme(), url.getUserInfo(), credUrl.getHost(), credUrl.getPort(),
+                    credUrl.getPath() + url.getPath(), newQuery, credUrl.getFragment());
             return newURI.toString();
         }
         catch (URISyntaxException ex) {
@@ -88,7 +77,6 @@ public class SCCRepositoryTokenAuth extends SCCRepositoryAuth {
             Function<SCCRepositoryNoAuth, ? extends T> noAuth,
             Function<SCCRepositoryTokenAuth, ? extends T> tokenAuth,
             Function<SCCRepositoryCloudRmtAuth, ? extends T> cloudRmtAuth) {
-        return tokenAuth.apply(this);
+        return cloudRmtAuth.apply(this);
     }
-
 }
