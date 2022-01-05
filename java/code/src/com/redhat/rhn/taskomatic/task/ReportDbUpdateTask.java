0fa6e600e4701fff48d9953086298c5aa53a3cb8
Michael Calmer <mc@suse.de>
2022-01-05 13:29:32 +0100

    query report db

--- java/code/src/com/redhat/rhn/taskomatic/task/ReportDbUpdateTask.java
+++ java/code/src/com/redhat/rhn/taskomatic/task/ReportDbUpdateTask.java
@@ -14,15 +14,42 @@
  */
 package com.redhat.rhn.taskomatic.task;
 
+import com.redhat.rhn.common.db.datasource.DataResult;
+import com.redhat.rhn.common.db.datasource.ModeFactory;
+import com.redhat.rhn.common.db.datasource.SelectMode;
+import com.redhat.rhn.common.db.datasource.WriteMode;
+import com.redhat.rhn.common.hibernate.ReportDbConnectionManager;
+import com.redhat.rhn.common.hibernate.ReportDbHibernateFactory;
+
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ReportDbUpdateTask extends RhnJavaJob {
 
     @Override
     public void execute(JobExecutionContext arg0) throws JobExecutionException {
-        // TODO Auto-generated method stub
+        ReportDbConnectionManager rcm = new ReportDbConnectionManager();
+        ReportDbHibernateFactory rh = new ReportDbHibernateFactory(rcm);
+        SelectMode m = ModeFactory.getMode("SystemReport_queries", "system", Map.class);
+        DataResult<Map<String, Object>> dataResult = m.execute();
+
+        WriteMode rd = ModeFactory.getWriteMode(rh.getSession(), "ReportDb_queries", "delete_system");
+        Map<String, Object> params = new HashMap<>();
+        params.put("mgm_id", 1); // 1 == "localhost"
+        rd.executeUpdate(params);
+
+        WriteMode rm = ModeFactory.getWriteMode(rh.getSession(), "ReportDb_queries", "insert_system");
+        dataResult.forEach(set -> {
+            set.putAll(params);
+            rm.executeUpdate(set);
+        });
+        rh.commitTransaction();
+        rh.closeSession();
+        rh.closeSessionFactory();
     }
 
     @Override
