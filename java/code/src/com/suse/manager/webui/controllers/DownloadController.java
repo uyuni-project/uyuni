9692e9b760140c31603ef18d54ad85b527bcbe8f
mbussolotto <michele.bussolotto@suse.com>
2022-02-07 09:26:24 +0100

    Update DownloadController.java

--- java/code/src/com/suse/manager/webui/controllers/DownloadController.java
+++ java/code/src/com/suse/manager/webui/controllers/DownloadController.java
@@ -521,8 +521,8 @@ public class DownloadController {
                 log.info(String.format("Token %s does provide access to any channel", token));
             }, channels -> {
                 if (!channels.contains(channel)) {
-                    log.info(String.format("Forbidden: Token does not provide access to channel %s", channel));
-                    halt(HttpStatus.SC_FORBIDDEN, "Token does not provide access to channel " + channel);
+                    log.info(String.format("Forbidden: Token %s does not provide access to channel %s", token, channel));
+                    halt(HttpStatus.SC_FORBIDDEN, "Token " + token + " does not provide access to channel " + channel);
                 }
             });
 
