230761a9c6c345a885f5445fce3ebd6b8aed63d5
Thomas Florio <thomas.florio@suse.com>
2022-02-10 00:40:09 +0100

    Added additional fields

--- schema/reportdb/common/tables/Errata.sql
+++ schema/reportdb/common/tables/Errata.sql
@@ -11,19 +11,25 @@
 
 CREATE TABLE Errata
 (
-    mgm_id              NUMERIC NOT NULL,
-    errata_id           NUMERIC NOT NULL,
-    advisory_name       VARCHAR(100),
-    advisory_type       VARCHAR(32),
-    issue_date          TIMESTAMPTZ,
-    update_date         TIMESTAMPTZ,
-    product             VARCHAR(64),
-    cve                 VARCHAR(20),
-    synopsis            VARCHAR(4000),
-    channel_label       VARCHAR(128),
-    organization        VARCHAR(128),
-    synced_date         TIMESTAMPTZ DEFAULT (current_timestamp)
+    mgm_id                        NUMERIC NOT NULL,
+    errata_id                     NUMERIC NOT NULL,
+    advisory_name                 VARCHAR(100),
+    advisory_type                 VARCHAR(32),
+    issue_date                    TIMESTAMPTZ,
+    update_date                   TIMESTAMPTZ,
+    severity                      VARCHAR(64),
+    reboot_required               CHAR(1) NOT NULL DEFAULT ('N'),
+    affects_package_manager       CHAR(1) NOT NULL DEFAULT ('N'),
+    retracted                     CHAR(1) NOT NULL DEFAULT ('N'),
+    cve                           VARCHAR(4000),
+    synopsis                      VARCHAR(4000),
+    channel_label                 VARCHAR(128),
+    organization                  VARCHAR(128),
+    synced_date                   TIMESTAMPTZ DEFAULT (current_timestamp)
 );
 
 ALTER TABLE Errata
-  ADD CONSTRAINT Errata_pk PRIMARY KEY (mgm_id, errata_id);
+  ADD CONSTRAINT Errata_pk PRIMARY KEY (mgm_id, errata_id),
+  ADD CONSTRAINT Errata_rr_ck CHECK (reboot_required in ('Y', 'N')),
+  ADD CONSTRAINT Errata_apm_ck CHECK (affects_package_manager in ('Y', 'N')),
+  ADD CONSTRAINT Errata_rd_ck CHECK (retracted in ('Y', 'N'));
