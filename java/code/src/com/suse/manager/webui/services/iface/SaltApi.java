c7c30863738ee12096c9588aa7d927a4155b2ab6
Cédric Bosdonnat <cbosdonnat@suse.com>
2022-02-14 14:15:40 +0100

    Add SaltService function to test SSL certificates
    Calt the Salt runner to ensure Salt certificates are valid.

--- java/code/src/com/suse/manager/webui/services/iface/SaltApi.java
+++ java/code/src/com/suse/manager/webui/services/iface/SaltApi.java
@@ -16,6 +16,7 @@ package com.suse.manager.webui.services.iface;
 
 import com.redhat.rhn.domain.server.MinionServer;
 
+import com.suse.manager.ssl.SSLCertPair;
 import com.suse.manager.webui.services.impl.SaltSSHService;
 import com.suse.manager.webui.services.impl.SaltService;
 import com.suse.manager.webui.services.impl.runner.MgrK8sRunner;
@@ -448,4 +449,17 @@ public interface SaltApi {
      * @param minionList minion list
      */
     void refreshPillar(MinionList minionList);
+
+    /**
+     * Check SSL certificates before deploying them.
+     *
+     * @param rootCA root CA used to sign the SSL certificate in PEM format
+     * @param intermediateCAs intermediate CAs used to sign the SSL certificate in PEM format
+     * @param serverCertKey server CRT an Key pair
+     * @return the certificate to deploy
+     *
+     * @throws IllegalArgumentException if the cert check fails due to erroneous certificates
+     */
+     String checkSSLCert(String rootCA, SSLCertPair serverCertKey, List<String> intermediateCAs)
+             throws IllegalArgumentException;
 }
