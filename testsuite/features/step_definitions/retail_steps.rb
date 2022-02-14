1615dd5e3c5f1bd669429d99fca6d3f3e457cc20
Oscar Barrios <obarrios@suse.com>
2022-02-14 12:41:47 +0100

    QA: Refactor to don't require pxeboot_minion deployed to have a pxeboot_image defined (#16978)

--- testsuite/features/step_definitions/retail_steps.rb
+++ testsuite/features/step_definitions/retail_steps.rb
@@ -28,7 +28,10 @@ end
 
 # determine image for PXE boot tests
 def compute_image_filename
-  case ENV['PXEBOOT_IMAGE']
+  case $pxeboot_image
+  when 'sles15sp3', 'sles15sp3o'
+    # 'Kiwi/POS_Image-JeOS7_42' for 4.2 branch
+    $product == 'Uyuni' ? 'Kiwi/POS_Image-JeOS7_uyuni' : 'Kiwi/POS_Image-JeOS7_head'
   when 'sles15sp2', 'sles15sp2o'
     'Kiwi/POS_Image-JeOS7_41'
   when 'sles15sp1', 'sles15sp1o'
@@ -43,7 +46,10 @@ def compute_image_filename
 end
 
 def compute_image_name
-  case ENV['PXEBOOT_IMAGE']
+  case $pxeboot_image
+  when 'sles15sp3', 'sles15sp3o'
+    # 'POS_Image_JeOS7_42' for 4.2 branch
+    $product == 'Uyuni' ? 'POS_Image_JeOS7_uyuni' : 'POS_Image_JeOS7_head'
   when 'sles15sp2', 'sles15sp2o'
     'POS_Image_JeOS7_41'
   when 'sles15sp1', 'sles15sp1o'
