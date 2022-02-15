545cabb9c875ebb4b7b5a7f69b429c9d7adad104
mbussolotto <michele.bussolotto@suse.com>
2022-02-15 15:41:21 +0100

    show link only after audit has been triggered

--- web/html/src/manager/audit/cveaudit/cveaudit.tsx
+++ web/html/src/manager/audit/cveaudit/cveaudit.tsx
@@ -95,6 +95,7 @@ class CVEAudit extends React.Component<Props, State> {
       results: [],
       messages: [],
       selectedItems: [],
+      auditExecuted: false
     };
   }
 
@@ -164,12 +165,14 @@ class CVEAudit extends React.Component<Props, State> {
           selectedItems: data.data.filter((i) => i.selected).map((i) => i.id),
           resultType: target,
           messages: [],
+          auditExecuted: true,
         });
       } else {
         this.setState({
           results: [],
           selectedItems: [],
           messages: data.messages,
+          auditExecuted: true,
         });
       }
     });
@@ -252,33 +255,35 @@ class CVEAudit extends React.Component<Props, State> {
               />
             </div>
           </p>
-          <div>
-            <p>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={
-                  "https://cve.mitre.org/cgi-bin/cvename.cgi?name=" + this.state.cveYear + "-" + this.state.cveNumber
-                }
-                data-senna-off="true"
-              >
-                <IconTag type="external-link" />
-                {t("MITRE CVE link")}
-              </a>
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={
-                  "https://www.suse.com/security/cve/CVE-" + this.state.cveYear + "-" + this.state.cveNumber + ".html"
-                }
-                data-senna-off="true"
-              >
-                <IconTag type="external-link" />
-                {t("SUSE Security CVE link")}
-              </a>
-            </p>
-          </div>
+          {this.state.auditExecuted === true &&
+            <div>
+              <p>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={
+                    "https://cve.mitre.org/cgi-bin/cvename.cgi?name=" + this.state.cveYear + "-" + this.state.cveNumber
+                  }
+                  data-senna-off="true"
+                >
+                  <IconTag type="external-link" />
+                  {t("MITRE CVE link")}
+                </a>
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={
+                    "https://www.suse.com/security/cve/CVE-" + this.state.cveYear + "-" + this.state.cveNumber + ".html"
+                  }
+                  data-senna-off="true"
+                >
+                  <IconTag type="external-link" />
+                  {t("SUSE Security CVE link")}
+                </a>
+              </p>
+            </div>
+          }
           <Table
             data={this.state.results}
             identifier={(row) => row.id}
