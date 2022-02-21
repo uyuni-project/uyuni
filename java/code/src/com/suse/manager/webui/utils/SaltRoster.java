ce6cf345867c5420b4fe2becf807b0c302fbd1e6
Victor Zhestkov <vzhestkov@suse.com>
2022-02-21 17:23:06 +0300

    Remove unused java.util.Arrays from SaltRoster

--- java/code/src/com/suse/manager/webui/utils/SaltRoster.java
+++ java/code/src/com/suse/manager/webui/utils/SaltRoster.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
