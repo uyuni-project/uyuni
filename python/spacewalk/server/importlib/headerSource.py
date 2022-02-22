ff729e3574d0b5a804c4cfe35f98f2d487870dc2
sbluhm <stefan.bluhm@clacee.eu>
2022-02-22 18:05:05 +0100

    Always filter out invalid UTF-8 characters

--- python/spacewalk/server/importlib/headerSource.py
+++ python/spacewalk/server/importlib/headerSource.py
@@ -498,11 +498,7 @@ class rpmChangeLog(ChangeLog):
                 except:
                     self[i] = self[i].encode("iso-8859-1")
             # Filter out invalid UTF-8 from string.
-            if type(self[i]) == str:
-                try:
-                    self[i].encode("utf-8")
-                except:
-                    self[i] = self[i].encode('utf-8', errors='replace').decode('utf-8')
+            self[i] = self[i].encode('utf-8', errors='replace').decode('utf-8')
 
 
 def sanitizeList(l):
