02a7f92d63be6cc066009334f2afe61d91009cf2
Michael Calmer <mc@suse.de>
2022-02-25 14:59:16 +0100

    create workflow to test schema migration on PRs

--- susemanager-utils/testing/automation/schema-migration-reportdb-test-pgsql.sh
+++ susemanager-utils/testing/automation/schema-migration-reportdb-test-pgsql.sh
@@ -5,10 +5,7 @@ HERE=`dirname $0`
 GITROOT=`readlink -f $HERE/../../../`
 
 # File created by Gitarro with info about a PR, it only exists when we are testing a PR
-GITARRO_JSON="${GITROOT}/../.gitarro_pr.json"
-
-# Make it available for the container
-cp ${GITARRO_JSON} ${GITROOT}
+GITARRO_JSON="${GITROOT}/.gitarro_pr.json"
 GITARRO_JSON_CONTAINER="/manager/.gitarro_pr.json"
 
 # we need a special (old) baseimage to migrate to current schema
