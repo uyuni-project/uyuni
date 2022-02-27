0f42c21a44b5d194b21ad232da7216031f26884b
sbluhm <stefan.bluhm@clacee.eu>
2022-02-27 07:56:52 +0100

    Create rhn.conf with httpd users

--- susemanager-utils/testing/automation/susemanager-unittest.sh
+++ susemanager-utils/testing/automation/susemanager-unittest.sh
@@ -33,8 +33,8 @@ CMD="cd /manager/susemanager; make -f Makefile.susemanager unittest_inside_docke
 CHOWN_CMD="/manager/susemanager-utils/testing/automation/chown-objects.sh $(id -u) $(id -g)"
 
 docker pull $REGISTRY/$PGSQL_CONTAINER
-echo docker run --rm=true -e $DOCKER_RUN_EXPORT -v "$GITROOT:/manager" $REGISTRY/$PGSQL_CONTAINER /bin/bash -c "${INITIAL_CMD}; ${CMD}; RET=\${?}; ${CHOWN_CMD} && exit \${RET}"
-docker run --rm=true -e $DOCKER_RUN_EXPORT -v "$GITROOT:/manager" $REGISTRY/$PGSQL_CONTAINER /bin/bash -c "${INITIAL_CMD}; ${CMD}; RET=\${?}; ${CHOWN_CMD} && exit \${RET}"
+echo docker run --rm=true -e $DOCKER_RUN_EXPORT -v "$GITROOT:/manager" $REGISTRY/$PGSQL_CONTAINER /bin/bash -c "$echo 'httpd_user = wwwrun' >> /etc/rhn/rhn.conf; echo 'httpd_group = www' >> /etc/rhn/rhn.conf; {INITIAL_CMD}; ${CMD}; RET=\${?}; ${CHOWN_CMD} && exit \${RET}"
+docker run --rm=true -e $DOCKER_RUN_EXPORT -v "$GITROOT:/manager" $REGISTRY/$PGSQL_CONTAINER /bin/bash -c "echo 'httpd_user = wwwrun' >> /etc/rhn/rhn.conf; echo 'httpd_group = www' >> /etc/rhn/rhn.conf; ${INITIAL_CMD}; ${CMD}; RET=\${?}; ${CHOWN_CMD} && exit \${RET}"
 if [ $? -ne 0 ]; then
    EXIT=1
 fi
