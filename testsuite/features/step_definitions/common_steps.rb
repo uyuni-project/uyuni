cf4348330afd53663f5979ba92f32b4eab23e05f
Maxime Noel <maxime.noel@suse.com>
2022-03-04 16:28:56 +1300

    Update build enable / disable repositories depending on PROVIDER

--- testsuite/features/step_definitions/common_steps.rb
+++ testsuite/features/step_definitions/common_steps.rb
@@ -804,13 +804,13 @@ When(/^I enable repositories before installing Docker$/) do
   # Development and Desktop Applications (required)
   # (we do not install Python 2 repositories in this branch
   #  because they are not needed anymore starting with version 4.1)
-  if os_family =~ /^sles/ && os_version =~ /^15/
+  if os_family =~ /^sles/ && os_version =~ /^15/ && PROVIDER != 'aws'
     repos = "devel_pool_repo devel_updates_repo desktop_pool_repo desktop_updates_repo"
     log $build_host.run("zypper mr --enable #{repos}")
   end
 
   # Containers
-  unless os_family =~ /^opensuse/ || os_version =~ /^11/
+  unless os_family =~ /^opensuse/ || os_version =~ /^11/ && PROVIDER != 'aws'
     repos = "containers_pool_repo containers_updates_repo"
     log $build_host.run("zypper mr --enable #{repos}")
   end
@@ -831,13 +831,13 @@ When(/^I disable repositories after installing Docker$/) do
   # Development and Desktop Applications (required)
   # (we do not install Python 2 repositories in this branch
   #  because they are not needed anymore starting with version 4.1)
-  if os_family =~ /^sles/ && os_version =~ /^15/
+  if os_family =~ /^sles/ && os_version =~ /^15/ && PROVIDER != 'aws'
     repos = "devel_pool_repo devel_updates_repo desktop_pool_repo desktop_updates_repo"
     log $build_host.run("zypper mr --disable #{repos}")
   end
 
   # Containers
-  unless os_family =~ /^opensuse/ || os_version =~ /^11/
+  unless os_family =~ /^opensuse/ || os_version =~ /^11/ && PROVIDER != 'aws'
     repos = "containers_pool_repo containers_updates_repo"
     log $build_host.run("zypper mr --disable #{repos}")
   end
