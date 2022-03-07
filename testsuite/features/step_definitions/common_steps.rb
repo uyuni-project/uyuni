5bcf2e1b7acce435b08ed03bc5c1e0ca01283bec
Maxime Noel <maxime.noel@suse.com>
2022-03-07 13:20:50 +1300

    Fix condition issue

--- testsuite/features/step_definitions/common_steps.rb
+++ testsuite/features/step_definitions/common_steps.rb
@@ -831,7 +831,7 @@ When(/^I enable repositories before installing Docker$/) do
   end
 
   # Containers
-  unless ( os_family =~ /^opensuse/ || os_version =~ /^11/ ) && PROVIDER == 'aws'
+  unless os_family =~ /^opensuse/ || os_version =~ /^11/ || PROVIDER == 'aws'
     repos = "containers_pool_repo containers_updates_repo"
     log $build_host.run("zypper mr --enable #{repos}")
   end
@@ -861,7 +861,7 @@ When(/^I disable repositories after installing Docker$/) do
   end
 
   # Containers
-  unless ( os_family =~ /^opensuse/ || os_version =~ /^11/ ) && PROVIDER == 'aws'
+  unless os_family =~ /^opensuse/ || os_version =~ /^11/ || PROVIDER == 'aws'
     repos = "containers_pool_repo containers_updates_repo"
     log $build_host.run("zypper mr --disable #{repos}")
   end
