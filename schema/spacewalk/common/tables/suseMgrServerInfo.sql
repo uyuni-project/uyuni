27f2073c63d18bdca29bcdaa9f91c05a7efdd378
Michael Calmer <mc@suse.de>
2022-03-10 16:05:53 +0100

    schema: drop rhnSatelliteInfo and add suseMgrServerInfo

---
+++ schema/spacewalk/common/tables/suseMgrServerInfo.sql
@@ -0,0 +1,33 @@
+--
+-- Copyright (c) 2022 SUSE LLC
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+
+CREATE TABLE suseMgrServerInfo
+(
+    server_id          NUMERIC NOT NULL
+                           CONSTRAINT suse_mgr_info_sid_fk
+                               REFERENCES rhnServer (id),
+    mgr_evr_id         NUMERIC
+                           CONSTRAINT suse_mgr_info_peid_fk
+                               REFERENCES rhnPackageEVR (id),
+    report_db_cred_id  NUMERIC
+                           CONSTRAINT suse_mgr_info_creds_fk
+                               REFERENCES suseCredentials (id)
+                               ON DELETE SET NULL,
+    report_db_name     VARCHAR(128),
+    report_db_host     VARCHAR(256),
+    report_db_port     NUMERIC DEFAULT (5432) NOT NULL,
+    created            TIMESTAMPTZ
+                           DEFAULT (current_timestamp) NOT NULL,
+    modified           TIMESTAMPTZ
+                           DEFAULT (current_timestamp) NOT NULL,
+    CONSTRAINT suse_mgr_info_sid_uq UNIQUE (server_id)
+)
+;
