bf8d1b3c75f63077cc30601ed9006974606a3eca
Kevin Walter <kwalter@suse.de>
2022-03-11 10:37:03 +0100

    add hub reporting db taskomatic task

---
+++ java/code/src/com/redhat/rhn/common/db/datasource/GeneratedSelectMode.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2022 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.common.db.datasource;
+
+import org.hibernate.Session;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * A {@link WriteMode} that can execute a fully dynamic query generated at runtime.
+ */
+public class GeneratedSelectMode extends SelectMode {
+
+    /**
+     * Creates am instance
+     * @param name the name to be used to identify the dynamic query
+     * @param session hibernate database session to be used
+     * @param sqlStatement the sql statement to execute
+     */
+    public GeneratedSelectMode(String name, Session session, String sqlStatement) {
+        super(session, new DynamicParsedMode(name, sqlStatement, List.of()));
+    }
+
+    private static class DynamicParsedMode implements ParsedMode {
+
+        private final String name;
+
+        private final String sqlStatement;
+
+        private final Collection<String> parameters;
+
+        private DynamicParsedMode(String nameIn, String sqlStatementIn, Collection<String> parametersIn) {
+            this.name = nameIn;
+            this.sqlStatement = sqlStatementIn;
+            this.parameters = parametersIn;
+        }
+
+        @Override
+        public String getName() {
+            return "generated." + name;
+        }
+
+        @Override
+        public ModeType getType() {
+            return ModeType.WRITE;
+        }
+
+        @Override
+        public ParsedQuery getParsedQuery() {
+            return new DynamicQuery(name, sqlStatement, parameters);
+        }
+
+        @Override
+        public String getClassname() {
+            return null;
+        }
+
+        @Override
+        public List<ParsedQuery> getElaborators() {
+            return List.of();
+        }
+    }
+
+    private static class DynamicQuery implements ParsedQuery {
+
+        private final String name;
+
+        private final String sqlStatement;
+
+        private final Collection<String> parameters;
+
+        private DynamicQuery(String nameIn, String sqlStatementIn, Collection<String> parametersIn) {
+            this.name = nameIn;
+            this.sqlStatement = sqlStatementIn;
+            this.parameters = parametersIn;
+        }
+
+        @Override
+        public String getName() {
+            return "query.generated." + name;
+        }
+
+        @Override
+        public String getAlias() {
+            return "";
+        }
+
+        @Override
+        public String getSqlStatement() {
+            return sqlStatement;
+        }
+
+        @Override
+        public String getElaboratorJoinColumn() {
+            return "";
+        }
+
+        @Override
+        public List<String> getParameterList() {
+            return new ArrayList<>(parameters);
+        }
+
+        @Override
+        public boolean isMultiple() {
+            return false;
+        }
+    }
+}
---
+++ java/code/src/com/redhat/rhn/taskomatic/task/HubReportDbUpdateDriver.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2021 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.taskomatic.task;
+
+import com.redhat.rhn.common.conf.Config;
+import com.redhat.rhn.common.conf.ConfigDefaults;
+import com.redhat.rhn.common.hibernate.HibernateFactory;
+import com.redhat.rhn.domain.server.MgrServerInfo;
+import com.redhat.rhn.taskomatic.task.threaded.QueueDriver;
+import com.redhat.rhn.taskomatic.task.threaded.QueueWorker;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Hibernate;
+import org.hibernate.query.Query;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+
+/**
+ * Hub Reporting DB Task Driver
+ */
+public class HubReportDbUpdateDriver implements QueueDriver<MgrServerInfo> {
+
+    private Logger log;
+    private static AtomicBoolean hasCandidates = new AtomicBoolean(true);
+
+    @Override
+    public void setLogger(Logger loggerIn) {
+        this.log = loggerIn;
+    }
+
+    @Override
+    public Logger getLogger() {
+        return log;
+    }
+
+    @Override
+    public List<MgrServerInfo> getCandidates() {
+        CriteriaBuilder builder = HibernateFactory.getSession().getCriteriaBuilder();
+        CriteriaQuery<MgrServerInfo> criteria = builder.createQuery(MgrServerInfo.class);
+        Query<MgrServerInfo> query = HibernateFactory.getSession().createQuery(criteria);
+        List<MgrServerInfo> mgrServerInfos = (List<MgrServerInfo>)query.list();
+        return mgrServerInfos.stream().filter(info ->
+                Optional.ofNullable(info.getReportDbLastSynced())
+                        .map(time -> time.toInstant().plus(24, ChronoUnit.HOURS).isBefore(Instant.now()))
+                        .orElse(true)
+        ).map(e -> {
+            Hibernate.initialize(e.getReportDbCredentials());
+           return e;
+        }).collect(Collectors.toList());
+    }
+
+    @Override
+    public int getMaxWorkers() {
+        return Config.get()
+                .getInt(ConfigDefaults.REPORT_DB_HUB_WORKERS, 2);
+    }
+
+    @Override
+    public QueueWorker makeWorker(MgrServerInfo workItem) {
+        return new HubReportDbUpdateWorker(log, workItem);
+    }
+
+    @Override
+    public boolean canContinue() {
+        return true;
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+}
