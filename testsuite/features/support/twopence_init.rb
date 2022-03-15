618c0545acf626da97a78c4b2c4a1d244e736720
maximenoel8 <55169628+maximenoel8@users.noreply.github.com>
2022-03-16 11:56:38 +1300

    Update testsuite/features/support/twopence_init.rb
    Co-authored-by: Oscar Barrios <obarrios@suse.com>

--- testsuite/features/support/twopence_init.rb
+++ testsuite/features/support/twopence_init.rb
@@ -319,7 +319,7 @@ def client_public_ip(host)
     return 'NOT_IMPLEMENTED' if node == it
   end
   # Select eth0 interface for ubuntu when deploying on AWS
-  ubuntu_interface = ENV['PROVIDER'] == 'aws' ? 'eth0' : 'ens3'
+  ubuntu_interface = $is_cloud_provider ? 'eth0' : 'ens3'
   interface = case host
               when /^sle/, /^opensuse/, /^ssh/, /^ceos/, /^debian9/, /^debian10/, 'server', 'proxy', 'build_host'
                 'eth0'
