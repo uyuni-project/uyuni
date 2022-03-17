acf286c75847aed6b369f76ab394f0f8ecdfa189
Cédric Bosdonnat <cbosdonnat@suse.com>
2022-03-17 18:14:24 +0100

    Allow null ssh_salt_pre_flight_script value

--- java/code/src/com/suse/manager/webui/services/impl/SaltSSHService.java
+++ java/code/src/com/suse/manager/webui/services/impl/SaltSSHService.java
@@ -515,7 +515,7 @@ public class SaltSSHService {
                         parameters.getPort().orElse(SSH_PUSH_PORT)),
                     getSshPushTimeout(),
                     minionOpts(parameters.getHost(), contactMethod),
-                    Optional.of(getSaltSSHPreflightScriptPath()),
+                    Optional.ofNullable(getSaltSSHPreflightScriptPath()),
                     Optional.of(Arrays.asList(
                             bootstrapProxyPath.isEmpty() ?
                                     ConfigDefaults.get().getCobblerHost() :
