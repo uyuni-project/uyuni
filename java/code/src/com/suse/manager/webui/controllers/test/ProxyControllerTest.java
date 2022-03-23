29ef4c543641b9dc34290a7158bfcc88bdf05f5e
Cédric Bosdonnat <cbosdonnat@suse.com>
2022-03-23 14:56:17 +0100

    Add container config creation endpoint for the web UI

---
+++ java/code/src/com/suse/manager/webui/controllers/test/ProxyControllerTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2022 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.suse.manager.webui.controllers.test;
+
+import com.redhat.rhn.common.util.FileUtils;
+import com.redhat.rhn.manager.system.SystemManager;
+import com.redhat.rhn.testing.TestUtils;
+
+import com.suse.manager.ssl.SSLCertData;
+import com.suse.manager.ssl.SSLCertPair;
+import com.suse.manager.webui.controllers.ProxyController;
+import com.suse.manager.webui.utils.SparkTestUtils;
+
+import org.jmock.Expectations;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+import spark.Request;
+
+/**
+ * Test ProxyController class
+ */
+public class ProxyControllerTest extends BaseControllerTestCase {
+
+    private static final String TEST_DIR = "/com/suse/manager/webui/controllers/test/";
+
+    private ProxyController proxyController;
+    private SystemManager systemManager;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        systemManager = context().mock(SystemManager.class);
+        proxyController = new ProxyController(systemManager);
+    }
+
+    public void testGenerateContainerConfigExternal() throws Exception {
+        byte[] data = "config data".getBytes();
+
+        context().checking(new Expectations() {{
+            oneOf(systemManager).createProxyContainerConfig(
+                    with(equal(user)), with(equal("pxy.acme.lab")), with(equal(8022)), with(equal("srv.acme.lab")),
+                    with(equal(2048L)), with(equal("coyote@acme.lab")), with(equal("Root CA")),
+                    with(equal(List.of("CA1", "CA2"))), with(equal(new SSLCertPair("CERT", "KEY"))),
+                    with(aNull(SSLCertPair.class)), with(aNull(String.class)), with(aNull(SSLCertData.class)));
+            will(returnValue(data));
+        }});
+
+        String path = new File(TestUtils.findTestData(TEST_DIR + "proxy-container-data-external.json")
+                .getPath()).getAbsolutePath();
+        String content = FileUtils.readStringFromFile(path);
+        Request request = SparkTestUtils.createMockRequestWithBody("/manager/api/proxy/container-config",
+                Collections.emptyMap(), content);
+        assertEquals(data, proxyController.generateContainerConfig(request, response, user));
+    }
+
+    public void testGenerateContainerConfigGenerate() throws Exception {
+        byte[] data = "config data".getBytes();
+
+        context().checking(new Expectations() {{
+            oneOf(systemManager).createProxyContainerConfig(
+                    with(equal(user)), with(equal("pxy.acme.lab")), with(equal(22)),
+                    with(equal("srv.acme.lab")), with(equal(2048L)), with(equal("coyote@acme.lab")),
+                    with(aNull(String.class)), with(aNull(List.class)), with(aNull(SSLCertPair.class)),
+                    with(equal(new SSLCertPair("CA CERT", "CA KEY"))), with(equal("secret")),
+                    with(equal(new SSLCertData("pxy.acme.lab", List.of("cname1", "cname2"), "DE", "Bavaria", "Nurnberg",
+                            "SUSE", "SUSE Unit", "roadrunner@acme.lab"))));
+            will(returnValue(data));
+        }});
+
+        String path = new File(TestUtils.findTestData(TEST_DIR + "proxy-container-data-generate.json")
+                .getPath()).getAbsolutePath();
+        String content = FileUtils.readStringFromFile(path);
+        Request request = SparkTestUtils.createMockRequestWithBody("/manager/api/proxy/container-config",
+                Collections.emptyMap(), content);
+        assertEquals(data, proxyController.generateContainerConfig(request, response, user));
+    }
+}
---
+++ java/code/src/com/suse/manager/webui/utils/gson/ProxyContainerConfigJson.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) 2022 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.suse.manager.webui.utils.gson;
+
+import com.suse.manager.ssl.SSLCertData;
+import com.suse.manager.ssl.SSLCertPair;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Represents the data sent from the UI to request a new proxy container configuration
+ */
+public class ProxyContainerConfigJson {
+
+    private static final String CREATE_SSL = "create-ssl";
+    private static final String USE_SSL = "use-ssl";
+
+    @SerializedName("proxyFQDN")
+    private String proxyFqdn;
+    private Integer proxyPort;
+    @SerializedName("serverFQDN")
+    private String serverFqdn;
+    @SerializedName("maxSquidCacheSize")
+    private Long maxCache;
+    @SerializedName("proxyAdminEmail")
+    private String email;
+    private String rootCA;
+    private List<String> intermediateCAs;
+    @SerializedName("proxyCertificate")
+    private String proxyCert;
+    private String proxyKey;
+    @SerializedName("caCertificate")
+    private String caCert;
+    private String caKey;
+    private String caPassword;
+    private List<String> cnames;
+    private String country;
+    private String state;
+    private String city;
+    private String org;
+    private String orgUnit;
+    private String sslEmail;
+    @SerializedName("ssl-mode")
+    private String sslMode;
+
+    /**
+     * @return whether the data are consistent or not
+     */
+    public boolean isValid() {
+        boolean valid = false;
+        if (USE_SSL.equals(sslMode)) {
+            valid = rootCA != null && proxyCert != null && proxyKey != null;
+        }
+        else if (CREATE_SSL.equals(sslMode)) {
+            valid = caCert != null && caKey != null && caPassword != null;
+        }
+        return valid && proxyFqdn != null && serverFqdn != null && maxCache != null && email != null;
+    }
+
+    /**
+     * @return value of proxyFqdn
+     */
+    public String getProxyFqdn() {
+        return proxyFqdn;
+    }
+
+    /**
+     * @return value of proxyPort
+     */
+    public Integer getProxyPort() {
+        return Optional.ofNullable(proxyPort).orElse(22);
+    }
+
+    /**
+     * @return value of serverFqdn
+     */
+    public String getServerFqdn() {
+        return serverFqdn;
+    }
+
+    /**
+     * @return value of maxCache
+     */
+    public Long getMaxCache() {
+        return maxCache;
+    }
+
+    /**
+     * @return value of email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @return the proxy cert and key
+     */
+    public SSLCertPair getProxyCertPair() {
+        if (USE_SSL.equals(sslMode)) {
+            return new SSLCertPair(proxyCert, proxyKey);
+        }
+        return null;
+    }
+
+    /**
+     * @return value of rootCA
+     */
+    public String getRootCA() {
+        return rootCA;
+    }
+
+    /**
+     * @return value of intermediateCAs
+     */
+    public List<String> getIntermediateCAs() {
+        return intermediateCAs;
+    }
+
+    /**
+     * @return value of caPassword
+     */
+    public String getCaPassword() {
+        return caPassword;
+    }
+
+    /**
+     * @return the CA cert and key
+     */
+    public SSLCertPair getCaPair() {
+        if (CREATE_SSL.equals(sslMode)) {
+            return new SSLCertPair(caCert, caKey);
+        }
+        return null;
+    }
+
+    /**
+     * @return the data to generate a new certificate
+     */
+    public SSLCertData getCertData() {
+        if (CREATE_SSL.equals(sslMode)) {
+            return new SSLCertData(proxyFqdn, cnames, country, state, city, org, orgUnit, sslEmail);
+        }
+        return null;
+    }
+}
