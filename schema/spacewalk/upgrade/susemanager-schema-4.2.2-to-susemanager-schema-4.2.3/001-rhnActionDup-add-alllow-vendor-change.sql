98d730bea98799abf0d541f71be78c953dbbca67
Michael Calmer <mc@suse.de>
2022-03-24 11:12:29 +0100

    add check constrains always back to not loose it on multiple executions

--- schema/spacewalk/upgrade/susemanager-schema-4.2.2-to-susemanager-schema-4.2.3/001-rhnActionDup-add-alllow-vendor-change.sql
+++ schema/spacewalk/upgrade/susemanager-schema-4.2.2-to-susemanager-schema-4.2.3/001-rhnActionDup-add-alllow-vendor-change.sql
@@ -1,6 +1,4 @@
 ALTER TABLE rhnActionDup DROP CONSTRAINT IF EXISTS rhn_actiondup_avc_ck;
 ALTER TABLE rhnActionDup
-  ADD COLUMN IF NOT EXISTS allow_vendor_change CHAR(1)
-  DEFAULT ('N') NOT NULL
-  CONSTRAINT rhn_actiondup_avc_ck
-  CHECK (allow_vendor_change in ('Y','N'));
+  ADD COLUMN IF NOT EXISTS allow_vendor_change CHAR(1) DEFAULT ('N') NOT NULL;
+ALTER TABLE rhnActionDup ADD CONSTRAINT rhn_actiondup_avc_ck CHECK (allow_vendor_change in ('Y', 'N'));
