9cc3bdd95cd553d08872a6f105d708d7766f8185
Pascal Arlt <parlt@suse.com>
2022-03-25 11:46:40 +0100

    Remove doc search from spacewalk-search
    Signed-off-by: Pascal Arlt <parlt@suse.com>

--- search-server/spacewalk-search/src/java/com/redhat/satellite/search/index/tests/IndexManagerTest.java
+++ search-server/spacewalk-search/src/java/com/redhat/satellite/search/index/tests/IndexManagerTest.java
@@ -2,18 +2,15 @@ package com.redhat.satellite.search.index.tests;
 
 import com.redhat.satellite.search.index.IndexManager;
 import com.redhat.satellite.search.index.IndexingException;
-import com.redhat.satellite.search.index.Result;
 import com.redhat.satellite.search.index.QueryParseException;
-import com.redhat.satellite.search.index.builder.BuilderFactory;
+import com.redhat.satellite.search.index.Result;
 import com.redhat.satellite.search.index.builder.DocumentBuilder;
 import com.redhat.satellite.search.index.builder.PackageDocumentBuilder;
 import com.redhat.satellite.search.tests.BaseTestCase;
 import com.redhat.satellite.search.tests.TestUtil;
-import com.redhat.satellite.search.config.Configuration;
 
 import org.apache.lucene.document.Document;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -60,28 +57,6 @@ public class IndexManagerTest extends BaseTestCase {
         assertTrue(results.size() >= 1);
     }
 
-
-    public void StillNeedsWork_testQueryDocs()
-        throws IOException, IndexingException, QueryParseException {
-
-        Configuration config = TestUtil.makeConfiguration();
-        config.setString("search.index_work_dir", "/var/lib/rhn/search/indexes/");
-        IndexManager indexMgr = new IndexManager(config);
-        //
-        // TODO: Revisit how docs data gets injected for testing.
-        // Currently relying on docs data to already exist for now.
-        //
-        System.out.println("We are expecting nutch to have been run previously, " +
-                        "and the index files to be copied to "+ config.getString("search.index_work_dir") +
-                        BuilderFactory.DOCS_TYPE);
-
-        String index = BuilderFactory.DOCS_TYPE;
-        String query = new String("redhat");
-        List<Result> results = indexMgr.search(index, query, "en");
-        System.out.println("Number of results returned is " + results.size());
-        assertTrue(results.size() >= 1);
-
-    }
 
     @SuppressWarnings("unchecked")
     @Override
