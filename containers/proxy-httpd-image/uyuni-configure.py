a721cf9805ddc4c6bce5f16405b5a83dbb3d6ba1
Ondrej Holecek <oholecek@suse.com>
2022-03-25 15:27:11 +0100

    Enable and configure tftp sync for http container
    - this adds requirement to have `proxy_fqdn` in config.yaml

--- containers/proxy-httpd-image/uyuni-configure.py
+++ containers/proxy-httpd-image/uyuni-configure.py
@@ -5,9 +5,37 @@ import subprocess
 import re
 import shutil
 import yaml
+import socket
 import sys
 
 config_path = "/etc/uyuni/"
+
+def getIPs(fqdn: str) -> [str, str]:
+    addrinfo = socket.getaddrinfo(fqdn, None)
+    ipv4s = set(map(lambda r: r[4][0], filter(lambda f: f[0] == socket.AF_INET, addrinfo)))
+    ipv6s = set(map(lambda r: r[4][0], filter(lambda f: f[0] == socket.AF_INET6, addrinfo)))
+    ipv4, ipv6 = "", ""
+
+    if len(ipv4s) == 0 and len(ipv6s) == 0:
+       print("FATAL: Cannot determine proxy IPv4 nor IPv6 from FQDN {}".format(fqdn))
+       sys.exit(1)
+
+    try:
+       ipv4 = ipv4s.pop()
+       if len(ipv4s) > 0:
+          print("WARNING: Cannot determine unique IPv4 address for the proxy. TFTP sync may not work. Using IPv4 {}".format(ipv4))
+    except KeyError:
+       print("WARNING: No IPv4 address detected for proxy. If this is single stack IPv6 setup this warning can be ignored")
+
+    try:
+       ipv6 = ipv6s.pop()
+       if len(ipv6s) > 0:
+          print("Multiple IPv6 addresses resolved, using IPv6 {}".format(ipv6))
+    except KeyError:
+       print("WARNING: No IPv6 address detected for proxy. If this is single stack IPv4 setup this warning can be ignored")
+
+    print(f"DEBUG: detected ips '{ipv4}', '{ipv6}' for fqdn {fqdn}")
+    return (ipv4, ipv6)
 
 # read from file
 with open(config_path + "config.yaml") as source:
@@ -47,9 +75,13 @@ with open(config_path + "config.yaml") as source:
         file.write(file_content)
         file.truncate()
 
+    # resolve needed IP addresses
+    serverIPv4, serverIPv6 = getIPs(config['server'])
+    proxyIPv4, proxyIPv6 = getIPs(config['proxy_fqdn'])
+
     # Create conf file
     with open("/etc/rhn/rhn.conf", "w") as file:
-        file.write(f'''# Automatically generated Spacewalk Proxy Server configuration file.
+        file.write(f'''# Automatically generated Uyuni Proxy Server configuration file.
         # -------------------------------------------------------------------------
 
         # SSL CA certificate location
@@ -67,11 +99,40 @@ with open(config_path + "config.yaml") as source:
         # Location of locally built, custom packages
         proxy.pkg_dir = /var/spool/rhn-proxy
         
-        # Hostname of RHN Classic Server or Red Hat Satellite
+        # Hostname of Uyuni, SUSE Manager Server or another proxy
         proxy.rhn_parent = {config['server']}
         
         # Destination of all tracebacks, etc.
-        traceback_mail = {config['email']}''')
+        traceback_mail = {config['email']}
+
+        # Tftp sync configuration
+        tftpsync.server_fqdn = {config['server']}
+        tftpsync.server_ip = {serverIPv4}
+        tftpsync.server_ip6 = {serverIPv6}
+        tftpsync.proxy_ip = {proxyIPv4}
+        tftpsync.proxy_ip6 = {proxyIPv6}
+        tftpsync.proxy_fqdn = {config['proxy_fqdn']}
+        tftpsync.tftpboot = /srv/tftpboot''')
+
+    with open("/etc/apache2/conf.d/susemanager-tftpsync-recv.conf", "w") as file:
+        requireIPv4 = ""
+        requireIPv6 = ""
+        if len(proxyIPv4) > 0:
+           requireIPv4 = "Require ip {}".format(proxyIPv4)
+        if len(proxyIPv6) > 0:
+           requireIPv6 = "Require ip {}".format(proxyIPv6)
+        file.write(f'''<Directory "/srv/www/tftpsync">
+    <IfVersion >= 2.4>
+        <RequireAny>
+            {requireIPv4}
+            {requireIPv6}
+        </RequireAny>
+    </IfVersion>
+</Directory>
+
+WSGIScriptAlias /tftpsync/add /srv/www/tftpsync/add
+WSGIScriptAlias /tftpsync/delete /srv/www/tftpsync/delete''')
+
     os.system('chown root:www /etc/rhn/rhn.conf')
     os.system('chmod 640 /etc/rhn/rhn.conf')
 
@@ -81,3 +142,5 @@ os.system('chmod -R 755 /srv/www/htdocs/pub')
 os.system('chown -R wwwrun:www /var/spool/rhn-proxy')
 os.system('chmod -R 750 /var/spool/rhn-proxy')
 os.system('chown -R wwwrun:root /var/cache/rhn/proxy-auth')
+os.system('chown -R wwwrun:root /srv/tftpboot')
+os.system('chmod 755 /srv/tftpboot')
