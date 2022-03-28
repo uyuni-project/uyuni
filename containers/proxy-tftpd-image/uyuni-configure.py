37b7c01d3d612c71c9ee4cf6d5801b859e69cb8f
Ondrej Holecek <oholecek@suse.com>
2022-03-28 11:12:40 +0200

    Remove unnecessary version check from tftp container

--- containers/proxy-tftpd-image/uyuni-configure.py
+++ containers/proxy-tftpd-image/uyuni-configure.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 import os
-import subprocess
-import re
 import yaml
 import sys
 
@@ -12,18 +10,6 @@ config_path = "/etc/uyuni/"
 with open(config_path + "config.yaml") as source:
     config = yaml.safe_load(source)
    
-    server_version = config.get("server_version")
-    # Only check version for SUSE Manager, not Uyuni
-    matcher = re.fullmatch(r"([0-9]+\.[0-9]+\.)[0-9]+", server_version)
-    if matcher:
-        major_version = matcher.group(1)
-        container_version = subprocess.run(["rpm", "-q", "--queryformat", "%{version}", "spacewalk-proxy-common"],
-                stdout=subprocess.PIPE, universal_newlines=True).stdout
-        if not container_version.startswith(major_version):
-            print("FATAL: Proxy container image version (%s) doesn't match server major version (%s)".format(
-                container_version, major_version), file=sys.stderr)
-            sys.exit(1)
-    
     tftp_config = "/etc/sysconfig/tftp"
     tftp_root = "/srv/tftpboot"
     with open(tftp_config, "w") as file:
@@ -40,5 +26,4 @@ if not os.access(tftp_root, os.R_OK | os.X_OK):
    print("FATAL: TFTP root directory does not have correct permissions.")
    sys.exit(1)
 
-print("DEBUG: configuration finished")
 sys.exit(0)
