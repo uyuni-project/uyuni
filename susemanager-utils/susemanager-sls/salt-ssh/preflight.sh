a1d5d0d370f799ff3de71d797eed46fb32070f1c
Victor Zhestkov <vzhestkov@suse.com>
2022-03-29 18:37:29 +0300

    Add extra checks to ensure the bundle was extracted

--- susemanager-utils/susemanager-sls/salt-ssh/preflight.sh
+++ susemanager-utils/susemanager-sls/salt-ssh/preflight.sh
@@ -219,7 +219,11 @@ if [ -n "${VENV_SOURCE}" ]; then
         VENV_HASH=$(dpkg -s venv-salt-minion | sha256sum | tr -d '\- ')
     fi
     if [ -f "${VENV_TMP_DIR}/${VENV_HASH_FILE}" ]; then
-        PRE_VENV_HASH=$(cat "${VENV_TMP_DIR}/${VENV_HASH_FILE}")
+        if [ -x "${VENV_TMP_DIR}/bin/python" ]; then
+            PRE_VENV_HASH=$(cat "${VENV_TMP_DIR}/${VENV_HASH_FILE}")
+        else
+            rm -f "${VENV_TMP_DIR}/${VENV_HASH_FILE}"
+        fi
     fi
     if [ "${VENV_HASH}" != "${PRE_VENV_HASH}" ]; then
         if [ "${VENV_SOURCE}" = "bootstrap" ]; then
@@ -246,6 +250,10 @@ if [ -n "${VENV_SOURCE}" ]; then
         else
             cp -r "${VENV_INST_DIR}" "${VENV_TMP_DIR}"
             pushd "${VENV_TMP_DIR}" > /dev/null
+        fi
+        if [ -x bin/python ]; then
+            rm -f "${VENV_TMP_DIR}/${VENV_HASH_FILE}"
+            exit_with_message_code "Error: Unable to extract the bundle from ${TEMP_DIR}/${VENV_PKG_FILE}!" 6
         fi
         grep -m1 -r "^#\!${VENV_INST_DIR}" bin/ | sed 's/:.*//' | sort | uniq | xargs -I '{}' sed -i "1s=^#!${VENV_INST_DIR}/bin/.*=#!${VENV_TMP_DIR}/bin/python=" {}
         sed -i "s#${VENV_INST_DIR}#${VENV_TMP_DIR}#g" bin/python
