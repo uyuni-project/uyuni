9f10671aaa3d70a62f840dbfb69bdd921cbe7bbc
Abid Mehmood <amehmood@suse.de>
2022-03-30 18:23:32 +0200

    rename state name

--- schema/spacewalk/upgrade/susemanager-schema-4.3.9-to-susemanager-schema-4.3.10/010-alllow-vendor-change-constrains.sql
+++ schema/spacewalk/upgrade/susemanager-schema-4.3.9-to-susemanager-schema-4.3.10/010-alllow-vendor-change-constrains.sql
@@ -1,3 +1,3 @@
 -- take care that the check exists. We might have lost it in the past
-ALTER TABLE rhnActionDup DROP CONSTRAINT IF EXISTS rhn_actiondup_avc_ck;
-ALTER TABLE rhnActionDup ADD CONSTRAINT rhn_actiondup_avc_ck CHECK (allow_vendor_change in ('Y', 'N'));
+--ALTER TABLE rhnActionDup DROP CONSTRAINT IF EXISTS rhn_actiondup_avc_ck;
+--ALTER TABLE rhnActionDup ADD CONSTRAINT rhn_actiondup_avc_ck CHECK (allow_vendor_change in ('Y', 'N'));
