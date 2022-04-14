b506d46f64a66b304e7310a0871b7b67da0410fc
Maxime Noel <maxime.noel@suse.com>
2022-04-14 13:31:24 +1200

    change building variable test

--- testsuite/features/support/env.rb
+++ testsuite/features/support/env.rb
@@ -414,12 +414,12 @@ end
 
 # skip tests if using a server build image
 Before('@skip_if_server_build_image') do
-  skip_this_scenario if $server_build_image
+  skip_this_scenario if $is_using_build_image
 end
 
 # execute tests if using a server build image
 Before('@server_build_image') do
-  skip_this_scenario unless $server_build_image
+  skip_this_scenario unless $is_using_build_image
 end
 
 # have more infos about the errors
