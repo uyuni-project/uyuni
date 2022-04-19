ae7f0a80a985a81e9ffa49278bb3737f213850d1
Thomas Florio <thomas.florio@suse.com>
2022-04-19 09:40:06 +0200

    Added scripts to generate the schema

---
+++ schema/reportdb/postgres/docs/end.sql
@@ -0,0 +1,9 @@
+-- Remove things that don't need documentation
+
+DROP TABLE IF EXISTS DUAL;
+
+DROP TABLE IF EXISTS VersionInfo;
+
+DROP FUNCTION IF EXISTS create_varnull_constriants;
+
+DROP EXTENSION IF EXISTS dblink;
