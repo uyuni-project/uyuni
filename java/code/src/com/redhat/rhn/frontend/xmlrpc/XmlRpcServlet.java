uyuni-project/uyuni

commit a98a76f64a32b6c0893af16b8fd85c3611be8023

Migrate XMLRPC serializers

java/code/src/com/redhat/rhn/frontend/xmlrpc/XmlRpcServlet.java
@@ -15,6 +15,8 @@
 
 package com.redhat.rhn.frontend.xmlrpc;
 
+import com.redhat.rhn.frontend.xmlrpc.serializer.BigDecimalSerializer;
+import com.redhat.rhn.frontend.xmlrpc.serializer.ObjectSerializer;
 import com.redhat.rhn.frontend.xmlrpc.serializer.SerializerFactory;
 
 import org.apache.logging.log4j.LogManager;
@@ -27,7 +29,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import redstone.xmlrpc.XmlRpcCustomSerializer;
+import redstone.xmlrpc.XmlRpcSerializer;
 
 /**
  * A basic servlet class that registers handlers for xmlrpc calls
@@ -43,8 +45,8 @@ public class XmlRpcServlet extends HttpServlet {
     private static Logger log = LogManager.getLogger(XmlRpcServlet.class);
 
     private RhnXmlRpcServer server;
-    private HandlerFactory handlers;
-    private SerializerFactory serializers;
+    private HandlerFactory handlerFactory;
+    private SerializerFactory serializerFactory;
 
     /**
      * Constructor which takes in HandlerFactory and SerializerFactory. The
@@ -55,8 +57,8 @@ public class XmlRpcServlet extends HttpServlet {
      * @param sf SerializerFactory to use.
      */
     public XmlRpcServlet(HandlerFactory hf, SerializerFactory sf) {
-        handlers = hf;
-        serializers = sf;
+        handlerFactory = hf;
+        serializerFactory = sf;
     }
 
     /**
@@ -88,30 +90,31 @@ public class XmlRpcServlet extends HttpServlet {
     }
 
     private void registerCustomSerializers(RhnXmlRpcServer srvr) {
-        if (serializers == null) {
-            serializers = new SerializerFactory();
+        if (serializerFactory == null) {
+            serializerFactory = new SerializerFactory();
         }
+        XmlRpcSerializer serializer = srvr.getSerializer();
+        serializer.addCustomSerializer(new ObjectSerializer());
+        serializer.addCustomSerializer(new BigDecimalSerializer());
+
         // find the configured serializers...
-        for (Object oIn : serializers.getSerializers()) {
-            srvr.getSerializer().addCustomSerializer(
-                    (XmlRpcCustomSerializer) oIn);
-        }
+        serializerFactory.getSerializers().forEach(serializer::addCustomSerializer);
     }
 
     private void registerInvocationHandlers(RhnXmlRpcServer srvr) {
-        if (handlers == null) {
-            handlers = new HandlerFactory();
+        if (handlerFactory == null) {
+            handlerFactory = new HandlerFactory();
         }
 
         // find the configured handlers...
-        for (String namespace : handlers.getKeys()) {
+        for (String namespace : handlerFactory.getKeys()) {
             if (log.isDebugEnabled()) {
                 log.debug("registerInvocationHandler: namespace [" + namespace +
-                        "] handler [" + handlers.getHandler(namespace).get() + "]");
+                        "] handler [" + handlerFactory.getHandler(namespace).get() + "]");
             }
             srvr.addInvocationHandler(
                     namespace,
-                    handlers.getHandler(namespace).get()
+                    handlerFactory.getHandler(namespace).get()
             );
         }
     }

commit 3882abeb8d39d410cd1ca43c207c20109312e8e6

Fix sonarcube errors in XmlRpcServlet
Caused by a98a76f64a32b6c0893af16b8fd85c3611be8023

java/code/src/com/redhat/rhn/frontend/xmlrpc/XmlRpcServlet.java
@@ -45,8 +45,8 @@ public class XmlRpcServlet extends HttpServlet {
     private static Logger log = LogManager.getLogger(XmlRpcServlet.class);
 
     private RhnXmlRpcServer server;
-    private HandlerFactory handlerFactory;
-    private SerializerFactory serializerFactory;
+    private final HandlerFactory handlerFactory;
+    private final SerializerFactory serializerFactory;
 
     /**
      * Constructor which takes in HandlerFactory and SerializerFactory. The
@@ -90,9 +90,6 @@ public class XmlRpcServlet extends HttpServlet {
     }
 
     private void registerCustomSerializers(RhnXmlRpcServer srvr) {
-        if (serializerFactory == null) {
-            serializerFactory = new SerializerFactory();
-        }
         XmlRpcSerializer serializer = srvr.getSerializer();
         serializer.addCustomSerializer(new ObjectSerializer());
         serializer.addCustomSerializer(new BigDecimalSerializer());
@@ -102,20 +99,14 @@ public class XmlRpcServlet extends HttpServlet {
     }
 
     private void registerInvocationHandlers(RhnXmlRpcServer srvr) {
-        if (handlerFactory == null) {
-            handlerFactory = new HandlerFactory();
-        }
-
         // find the configured handlers...
         for (String namespace : handlerFactory.getKeys()) {
-            if (log.isDebugEnabled()) {
-                log.debug("registerInvocationHandler: namespace [" + namespace +
-                        "] handler [" + handlerFactory.getHandler(namespace).get() + "]");
-            }
-            srvr.addInvocationHandler(
-                    namespace,
-                    handlerFactory.getHandler(namespace).get()
-            );
+            handlerFactory.getHandler(namespace).ifPresent((handler) -> {
+                if (log.isDebugEnabled()) {
+                    log.debug("registerInvocationHandler: namespace [" + namespace + "] handler [" + handler + "]");
+                }
+                srvr.addInvocationHandler(namespace, handler);
+            });
         }
     }
 

java/code/src/com/redhat/rhn/frontend/xmlrpc/test/XmlRpcServletTest.java
@@ -21,6 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import com.redhat.rhn.common.hibernate.HibernateFactory;
 import com.redhat.rhn.domain.user.User;
 import com.redhat.rhn.frontend.xmlrpc.XmlRpcServlet;
+import com.redhat.rhn.frontend.xmlrpc.serializer.SerializerFactory;
 import com.redhat.rhn.testing.MockObjectTestCase;
 import com.redhat.rhn.testing.UserTestUtils;
 
@@ -78,7 +79,7 @@ public class XmlRpcServletTest extends MockObjectTestCase {
         } });
 
         // ok run servlet
-        XmlRpcServlet xrs = new XmlRpcServlet(XmlRpcTestUtils.getTestHandlerFactory(), null);
+        XmlRpcServlet xrs = new XmlRpcServlet(XmlRpcTestUtils.getTestHandlerFactory(), new SerializerFactory());
         xrs.init();
         xrs.doPost(mockreq, mockresp);
 