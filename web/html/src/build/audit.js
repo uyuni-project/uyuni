767b5c850173c29bea9c1eed8cb2dcec3f6ff373
Etheryte <karl.tarvas@suse.com>
2022-04-28 09:58:34 +0200

    Add a way to ignore known issues in audits

---
+++ web/html/src/build/audit.js
@@ -0,0 +1,43 @@
+const { exec } = require("child_process");
+
+const ignore = require("../.auditignore.js");
+
+// Yarn 1.x doesn't currently support muting known issues, see https://github.com/yarnpkg/yarn/issues/6669
+exec(`yarn audit --json --groups "dependencies"`, (error, stdout, stderr) => {
+  try {
+    const lines = (stdout || "").split(/\r?\n/).filter((line) => line.trim() !== "");
+    const results = lines.map((line) => JSON.parse(line));
+
+    const advisories = results.filter((item) => item.type === "auditAdvisory");
+    if (!advisories.length) {
+      process.exitCode = 0;
+      return;
+    }
+
+    const remainingAdvisories = advisories.filter((item) => {
+      const { module_name: moduleName, id } = item.data.advisory;
+      if (ignore[moduleName]) {
+        console.info(`Ignoring advisory ${id} for module "${moduleName}": ${ignore[moduleName]}`);
+        return false;
+      }
+      return true;
+    });
+
+    if (remainingAdvisories.length) {
+      remainingAdvisories.forEach((item) => {
+        const { module_name: moduleName, id, overview, recommendation } = item.data.advisory;
+        console.error(
+          `Error: Found advisory ${id} for module "${moduleName}"\nOverview: ${overview}\nRecommendation: ${recommendation}\n`
+        );
+      });
+      process.exitCode = 1;
+      return;
+    }
+
+    process.exitCode = 0;
+    return;
+  } catch (error) {
+    process.exitCode = error.code || 1;
+    return;
+  }
+});
