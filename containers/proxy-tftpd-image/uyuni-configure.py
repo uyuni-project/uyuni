9597d03ef4f25f588a8d512aff2c93a95722bf9e
Cédric Bosdonnat <cbosdonnat@suse.com>
2022-04-28 18:37:13 +0200

    Fix tftpd container image

--- containers/proxy-tftpd-image/uyuni-configure.py
+++ containers/proxy-tftpd-image/uyuni-configure.py
@@ -11,6 +11,7 @@ with open(config_path + "config.yaml") as source:
     config = yaml.safe_load(source)
    
     # store SSL CA certificate
+    os.mkdir("/usr/share/uyuni/")
     with open("/usr/share/uyuni/ca.crt", "w") as file:
         file.write(config.get("ca_crt"))
 
@@ -22,7 +23,6 @@ TFTP_USER="tftp"
 TFTP_OPTIONS="{config.get('tftp_options', '')} "
 TFTP_DIRECTORY="{tftp_root}"''')
 
-    os.system("chown root:tftp {}".format(tftp_config))
     os.system("chmod 640 {}".format(tftp_config))
 
 # Make sure we can read 
