278dacf0716d39f1fa4d82ffe32ee8a360cbceff
Eric Bischoff <ebischoff@suse.com>
2022-05-01 12:21:21 +0200

    Use new parameter names

--- testsuite/features/support/namespaces/configchannel.rb
+++ testsuite/features/support/namespaces/configchannel.rb
@@ -8,47 +8,47 @@ class NamespaceConfigchannel
   end
 
   def channel_exists(channel)
-    @test.call('configchannel.channelExists', sessionKey: @test.token, channelLabel: channel)
+    @test.call('configchannel.channelExists', sessionKey: @test.token, label: channel)
   end
 
   def list_files(channel)
-    @test.call('configchannel.listFiles', sessionKey: @test.token, channelLabel: channel)
+    @test.call('configchannel.listFiles', sessionKey: @test.token, label: channel)
   end
 
   def list_subscribed_systems(channel)
-    @test.call('configchannel.listSubscribedSystems', sessionKey: @test.token, channelLabel: channel)
+    @test.call('configchannel.listSubscribedSystems', sessionKey: @test.token, label: channel)
   end
 
   def get_file_revision(channel, file_path, revision)
-    @test.call('configchannel.getFileRevision', sessionKey: @test.token, configChannelLabel: channel, filePath: file_path, revision: revision)
+    @test.call('configchannel.getFileRevision', sessionKey: @test.token, label: channel, filePath: file_path, revision: revision)
   end
 
   def create(label, name, description, type)
-    @test.call('configchannel.create', sessionKey: @test.token, label: label, name: name, description: description, channelType: type)
+    @test.call('configchannel.create', sessionKey: @test.token, label: label, name: name, description: description, type: type)
   end
 
-  def create_with_data(label, name, description, type, data)
-    @test.call('configchannel.create', sessionKey: @test.token, label: label, name: name, description: description, channelType: type, data: data)
+  def create_with_pathinfo(label, name, description, type, info)
+    @test.call('configchannel.create', sessionKey: @test.token, label: label, name: name, description: description, type: type, pathInfo: info)
   end
 
   def create_or_update_path(channel, file, contents)
     @test.call('configchannel.createOrUpdatePath',
                sessionKey: @test.token,
-               channelLabel: channel,
+               label: channel,
                path: file,
                isDir: false,
-               data: { contents: contents,
-                       owner: 'root',
-                       group: 'root',
-                       permissions: '644' }
+               pathInfo: { contents: contents,
+                           owner: 'root',
+                           group: 'root',
+                           permissions: '644' }
               )
   end
 
   def deploy_all_systems(channel)
-    @test.call('configchannel.deployAllSystems', sessionKey: @test.token, channelLabel: channel)
+    @test.call('configchannel.deployAllSystems', sessionKey: @test.token, label: channel)
   end
 
   def delete_channels(channels)
-    @test.call('configchannel.deleteChannels', sessionKey: @test.token, channelLabels: channels)
+    @test.call('configchannel.deleteChannels', sessionKey: @test.token, labels: channels)
   end
 end
