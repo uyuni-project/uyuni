f6ee55e6819ffdb127ce6631b3e773a9b53d4d86
Artem Shiliaev <artem.shiliaev@suse.com>
2022-05-05 14:48:52 +0200

    slim httpd

--- containers/proxy-httpd-image/prepare_target.sh
+++ containers/proxy-httpd-image/prepare_target.sh
@@ -3,19 +3,27 @@
 
 set -xe
 
+# rhn config
 mkdir -p /target/etc/rhn
-mv /etc/rhn/rhn*.conf /target/etc/rhn/rhn.conf
-
-mkdir -p /target/etc/salt
-mv /etc/salt/broker /target/etc/salt
-
 mkdir -p /target/usr/share/rhn/config-defaults
+mv /etc/rhn/rhn*.conf /target/etc/rhn/rhn.conf
 mv /usr/share/rhn/config-defaults/rhn*.conf /target/usr/share/rhn/config-defaults
 
+# apache2 and sysconfig
+mkdir -p /target/etc/apache2
+mkdir -p /target/etc/sysconfig
+mv /etc/apache2 /target/etc/
+mv /etc/sysconfig /target/etc/
+
+# rhn proxy
+mkdir -p /target/usr/share/rhn/proxy
+mv /usr/share/rhn/proxy /target/usr/share/rhn/
+
+# uyuni-configure.py
 mkdir -p /target/usr/bin
-mv /usr/bin/salt-broker /target/usr/bin/
 mv /usr/bin/uyuni-configure.py /target/usr/bin/
 
+# site packages
 SITE_PACKAGES=usr/lib/$(ls /usr/lib/ | grep python)/site-packages
 mkdir -p /target/$SITE_PACKAGES
 mv /$SITE_PACKAGES/spacewalk /target/$SITE_PACKAGES
