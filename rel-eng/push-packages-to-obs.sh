fca3a6d37ffff7529b5ab1d22496834966b11fb9
dleidi <dleidi@suse.com>
2022-05-10 16:44:39 +0200

    Revert "Tilde to split version value from additional info"
    This reverts commit 9ae9ed64a27f48074df02972c2a693f6dc501e38.

--- rel-eng/push-packages-to-obs.sh
+++ rel-eng/push-packages-to-obs.sh
@@ -269,8 +269,6 @@ while read PKG_NAME; do
       fi
       # to lowercase with ",," and replace spaces " " with "-"
       PRODUCT_VERSION=$(echo ${PRODUCT_VERSION,,} | sed -r 's/ /-/g')
-      # replace the first "-" with "~" for version comparison
-      PRODUCT_VERSION=$(echo ${PRODUCT_VERSION} | sed -r 's/-/~/')
       sed "s/%PKG_VERSION%/${PRODUCT_VERSION}/g" -i $SRPM_PKG_DIR/Dockerfile
   fi
 
