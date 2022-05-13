0cb44ddbf0e56b155073d222bbcc3f1fa32b6cd5
Michael Calmer <mc@suse.de>
2022-05-13 16:25:04 +0200

    fix bootstrap repo handling for SLE Micro

--- susemanager-utils/susemanager-sls/salt-ssh/preflight.sh
+++ susemanager-utils/susemanager-sls/salt-ssh/preflight.sh
@@ -138,6 +138,7 @@ function getZ_CLIENT_CODE_BASE() {
         if [ "$BASE" != "sle" ]; then
             grep -q 'openSUSE' /etc/os-release && BASE='opensuse'
         fi
+        grep -q 'Enterprise Micro' /etc/os-release && BASE='slemicro'
         VERSION="$(grep '^\(VERSION_ID\)' /etc/os-release | sed -n 's/.*"\([[:digit:]]\+\).*/\1/p')"
         PATCHLEVEL="$(grep '^\(VERSION_ID\)' /etc/os-release | sed -n 's/.*\.\([[:digit:]]*\).*/\1/p')"
     fi
