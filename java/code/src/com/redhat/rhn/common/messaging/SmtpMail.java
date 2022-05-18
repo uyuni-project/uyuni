010d7f6c34ac8fd5794f92ea57a64090dc37cbd7
Dominik Wombacher <dominik@wombacher.cc>
2022-05-19 00:28:55 +0200

    fixup! feat(smtp): Add support for port, auth, ssl/tls config

--- java/code/src/com/redhat/rhn/common/messaging/SmtpMail.java
+++ java/code/src/com/redhat/rhn/common/messaging/SmtpMail.java
@@ -29,6 +29,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
 
+import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.Message.RecipientType;
 import javax.mail.MessagingException;
