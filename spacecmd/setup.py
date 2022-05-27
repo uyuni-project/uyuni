b03bfe09df0ae7b26fbed141e9d1e568436fd2fd
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2022-05-27 11:56:23 +0200

    Automatic commit of package [spacecmd] release [4.1.18-1].

--- spacecmd/setup.py
+++ spacecmd/setup.py
@@ -11,7 +11,7 @@ def get_version_changelog():
     Get a version from the current changelog.
     """
     changelog = None
-    version = "4.1.17"
+    version = "4.1.18"
     for fname in os.listdir(os.path.dirname(os.path.abspath(__file__))):
         if fname.endswith(".changes"):
             changelog = fname
--- spacecmd/spacecmd.spec
+++ spacecmd/spacecmd.spec
@@ -44,7 +44,7 @@
 %endif
 
 Name:           spacecmd
-Version:        4.1.17
+Version:        4.1.18
 Release:        1%{?dist}
 Summary:        Command-line interface to Spacewalk and Red Hat Satellite servers
 %if %{_vendor} == "debbuild"
