723882182bcafe42e6499131b33757e594a423ce
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2022-06-23 16:37:56 +0100

    Fix issue with bootstrap repo definitions for RHEL/RES8 variants (bsc#1200863)

--- susemanager/src/mgr_bootstrap_data.py
+++ susemanager/src/mgr_bootstrap_data.py
@@ -382,7 +382,7 @@ RES8 = [
     "python3-pysocks",
     "python3-pytz",
     "python3-setuptools",
-    "python3-distro"
+    "python3-distro",
     "python3-immutables",
     "python3-contextvars",
 ]
