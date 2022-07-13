514a9086c7a603e7e34b873691ac1beb3d4accbe
Cédric Bosdonnat <cbosdonnat@suse.com>
2022-07-13 10:32:41 +0200

    Logging arguments should not require evaluation (java:S2629)

--- java/code/src/com/redhat/rhn/taskomatic/TaskoJob.java
+++ java/code/src/com/redhat/rhn/taskomatic/TaskoJob.java
@@ -171,8 +171,10 @@ public class TaskoJob implements Job {
                                     HibernateFactory.closeSession();
                                 }
 
-                                log.debug("{} ({}) ... {}", task.getName(), schedule.getJobLabel(),
-                                        taskRun.getStatus().toLowerCase());
+                                if (log.isDebugEnabled()) {
+                                    log.debug("{} ({}) ... {}", task.getName(), schedule.getJobLabel(),
+                                            taskRun.getStatus().toLowerCase());
+                                }
                                 if (((taskRun.getStatus() == TaskoRun.STATUS_FINISHED) ||
                                         (taskRun.getStatus() == TaskoRun.STATUS_FAILED)) &&
                                         (taskRun.getStatus() != lastStatus.get(task.getName()))) {
@@ -210,8 +212,10 @@ public class TaskoJob implements Job {
         }
         HibernateFactory.closeSession();
         log.info("{}: bunch {} FINISHED", schedule.getJobLabel(), schedule.getBunch().getName());
-        log.debug("{}: bunch {} START: {} END: {}", schedule.getJobLabel(), schedule.getBunch().getName(),
-                TIMESTAMP_FORMAT.format(start), TIMESTAMP_FORMAT.format(Instant.now()));
+        if (log.isDebugEnabled()) {
+            log.debug("{}: bunch {} START: {} END: {}", schedule.getJobLabel(), schedule.getBunch().getName(),
+                    TIMESTAMP_FORMAT.format(start), TIMESTAMP_FORMAT.format(Instant.now()));
+        }
     }
 
     /**
