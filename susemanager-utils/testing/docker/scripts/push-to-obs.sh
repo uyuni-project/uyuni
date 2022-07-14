24c36903837951d1bb4312b13d35654f9ce99292
Julio Gonzalez Gil <jgonzalez@suse.com>
2022-07-14 17:38:09 +0200

    Some extra checks for MFA

--- susemanager-utils/testing/docker/scripts/push-to-obs.sh
+++ susemanager-utils/testing/docker/scripts/push-to-obs.sh
@@ -16,7 +16,8 @@ help() {
   echo "      for example https://api.opensuse.org|systemsmanagement:Uyuni:Master"
   echo "  -p  Comma separated list of packages. If absent, all packages are submitted"
   echo "  -c  Path to the OSC credentials (usually ~/.osrc)"
-  echo "  -s  Path to the private key used for MFA"
+  echo "  -s  Path to the private key used for MFA, a file ending with .pub must also"
+  echo "      exist, containing the public key"
   echo "  -v  Verbose mode"
   echo "  -t  For tito, use current branch HEAD instead of latest package tag"
   echo "  -n  If used, update PROJECT instead of the projects specified with -d"
@@ -57,6 +58,17 @@ fi
 if [ ! -f ${OSCRC} ]; then
   echo "ERROR: File ${OSCRC} does not exist!"
   exit 1
+fi
+
+if [ "${SSHKEY}" != "" ]; then
+  if [ ! -f ${SSHKEY} ]; then
+    echo "ERROR: File ${SSHKEY} does not exist!"
+    exit 1
+  fi
+  if [ ! -f ${SSHKEY}.pub ]; then
+    echo "ERROR: File ${SSHKEY}.pub does not exist!"
+    exit 1
+  fi
 fi
 
 # declare /manager as "safe"
