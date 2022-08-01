96590559e58ec707ba3d105a70f9b9465ba3097c
Victor Zhestkov <Victor.Zhestkov@suse.com>
2022-08-01 15:26:47 +0300

    Add extra comment on find_proxy workaround

--- python/spacewalk/satellite_tools/download.py
+++ python/spacewalk/satellite_tools/download.py
@@ -112,6 +112,8 @@ class PyCurlFileObjectThread(PyCurlFileObject):
     def __init__(self, url, filename, opts, curl_cache, parent):
         self.curl_cache = curl_cache
         self.parent = parent
+        # Next 3 lines will not be required on having urlgrabber with proper fix
+        # https://github.com/rpm-software-management/urlgrabber/pull/35
         (url, parts) = opts.urlparser.parse(url, opts)
         (scheme, host, path, parm, query, frag) = parts
         opts.find_proxy(url, scheme)
