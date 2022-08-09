9d2570538efdf1176ff65e269a41a5575c7e3cc3
Artem Shiliaev <artem.shiliaev@suse.com>
2022-08-09 12:41:12 +0200

    fixed add_repos.sh and added comment

--- containers/proxy-tftpd-image/add_repos.sh
+++ containers/proxy-tftpd-image/add_repos.sh
@@ -4,8 +4,10 @@ set -xe
 
 # HACK: avoid adding repos if building inside of OBS (repos come from project configuration)
 if [ -n "$1" ]; then
-    zypper addrepo http://download.opensuse.org/distribution/leap/15.3/repo/oss/ main
-    zypper addrepo http://download.opensuse.org/update/leap/15.3/sle/ updates
+    echo "WARNING: using Leap 15.4 repositories! See the add_repos.sh for more detail."
+    zypper addrepo http://download.opensuse.org/distribution/leap/15.4/repo/oss/ main
+    zypper addrepo http://download.opensuse.org/update/leap/15.4/sle/ updates
+    zypper addrepo http://download.opensuse.org/update/leap/15.4/oss/ updates-oss
     zypper addrepo https://download.opensuse.org/repositories/systemsmanagement:/Uyuni:/Master:/Other/openSUSE_Leap_15.4/ uyuni-other
 
     zypper addrepo $1 product
