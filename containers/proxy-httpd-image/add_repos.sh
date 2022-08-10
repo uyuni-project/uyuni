d36dd52ac9695039b1e3e9938587fa937a8b484a
Artem Shiliaev <artem.shiliaev@suse.com>
2022-08-10 16:24:19 +0200

    Update containers/proxy-httpd-image/add_repos.sh
    Co-authored-by: Julio González Gil <juliogonzalez@users.noreply.github.com>

--- containers/proxy-httpd-image/add_repos.sh
+++ containers/proxy-httpd-image/add_repos.sh
@@ -4,7 +4,9 @@ set -xe
 
 # HACK: avoid adding repos if building inside of OBS (repos come from project configuration)
 if [ -n "$1" ]; then
-    echo "WARNING: using Leap 15.4 repositories! See the add_repos.sh for more detail."
+    echo "█████████████████████████████████████████████████████████████████████████████████"
+    echo "█ WARNING!!! using Leap 15.4 repositories! See the add_repos.sh for more detail █"
+    echo "█████████████████████████████████████████████████████████████████████████████████"
     zypper addrepo http://download.opensuse.org/distribution/leap/15.4/repo/oss/ main
     zypper addrepo http://download.opensuse.org/update/leap/15.4/sle/ updates
     zypper addrepo http://download.opensuse.org/update/leap/15.4/oss/ updates-oss
