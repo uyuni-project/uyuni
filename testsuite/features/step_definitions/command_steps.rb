a00f9e77dc55b55181a575975d357a55a5f3a650
Vanda Barata <vanda.barata@suse.com>
2022-09-01 22:50:35 +0100

    QE: Fix source package syncing (#18830)

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -382,6 +382,26 @@ When(/^I kill all running spacewalk\-repo\-sync, excepted the ones needed to boo
 end
 # rubocop:enable Metrics/BlockLength
 
+When(/^I ensure the channel "([^"]*)" has started syncing$/) do |channel_label|
+  reposync_not_running_streak = 0
+  # wait a maximum of 120s for reposync to start
+  while reposync_not_running_streak <= 120
+    command_output, _code = $server.run('ps axo pid,cmd | grep spacewalk-repo-sync | grep -v grep', check_errors: false)
+    if command_output.empty?
+      reposync_not_running_streak += 1
+      sleep 1
+      next
+    end
+    process = command_output.split("\n")[0]
+    channel = process.split[5]
+    break if channel == channel_label
+
+    log "Channel #{channel} is syncing"
+    reposync_not_running_streak += 1
+  end
+  raise StandardError "Channel #{channel_label} didn't start syncing in 2 minutes" if reposync_not_running_streak > 120
+end
+
 Then(/^the reposync logs should not report errors$/) do
   result, code = $server.run('grep -i "ERROR:" /var/log/rhn/reposync/*.log', check_errors: false)
   raise "Errors during reposync:\n#{result}" if code.zero?
