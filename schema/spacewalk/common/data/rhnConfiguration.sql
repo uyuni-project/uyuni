f6e62f028a95e95eef4555cf8f65fc4a9a3a78d9
Cédric Bosdonnat <cbosdonnat@suse.com>
2022-09-01 16:08:19 +0200

    Move web.system_checkin_threshold config to the database

--- schema/spacewalk/common/data/rhnConfiguration.sql
+++ schema/spacewalk/common/data/rhnConfiguration.sql
@@ -1,3 +1,5 @@
 INSERT INTO rhnConfiguration (key, description) VALUES ('extauth_default_orgid', 'Organization id, where externally authenticated users will be created.');
 INSERT INTO rhnConfiguration (key, description, default_value) VALUES ('extauth_use_orgunit', 'Use Org. Unit IPA setting as organization name to create externally authenticated users in.', 'false');
 INSERT INTO rhnConfiguration (key, description, default_value) VALUES ('extauth_keep_temproles', 'Keep temporary user roles granted due to the external authentication setup for subsequent logins using password.', 'false');
+INSERT INTO rhnConfiguration (key, description, value, default_value)
+VALUES ('system_checkin_threshold', 'Number of days before reporting a system as inactive', null, 1);
