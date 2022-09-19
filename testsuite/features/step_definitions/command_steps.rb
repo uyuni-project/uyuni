0d71802471358660ec67e70682e1246d9f6f63f3
elariekerboull <erwan.lariekerboull@suse.com>
2022-09-19 13:57:09 +0200

    Use 'unless'
    Co-authored-by: Oscar Barrios <obarrios@suse.com>

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -496,7 +496,7 @@ end
 
 Then(/^the messages log should not contain out of memory errors$/) do
   output, code = $server.run('grep -i "Out of memory: Killed process" /var/log/messages', check_errors: false)
-  raise "Out of memory errors in /var/log/messages:\n#{output}" if code.zero?
+  raise "Out of memory errors in /var/log/messages:\n#{output}" unless code.zero?
 end
 
 When(/^I restart cobbler on the server$/) do
