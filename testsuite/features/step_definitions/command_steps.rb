0123a8498ea882b42a2a493fec2ae6860b4aa832
elariekerboull <erwan.lariekerboull@suse.com>
2022-10-03 13:11:00 +0200

    Fixing OOM detection (#5973) (#19100)
    * Fixing OOM detection

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -448,9 +448,9 @@ Then(/^the taskomatic logs should not contain errors$/) do
   end
 end
 
-Then(/^the messages log should not contain out of memory errors$/) do
+Then(/^the log messages should not contain out of memory errors$/) do
   output, code = $server.run('grep -i "Out of memory: Killed process" /var/log/messages', check_errors: false)
-  raise "Out of memory errors in /var/log/messages:\n#{output}" unless code.zero?
+  raise "Out of memory errors in /var/log/messages:\n#{output}" if code.zero?
 end
 
 When(/^I restart cobbler on the server$/) do
