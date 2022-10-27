acbc8a801c0f06280bb63e9e7a251fb34db39589
elariekerboull <elariekerboull@suse.com>
2022-10-27 11:20:37 +0200

    Add docker channel to sync

--- testsuite/features/support/constants.rb
+++ testsuite/features/support/constants.rb
@@ -261,6 +261,8 @@ CHANNEL_TO_SYNCH_BY_OS_VERSION = {
     sle-module-desktop-applications15-sp4-updates-x86_64
     sle-module-devtools15-sp4-pool-x86_64
     sle-module-devtools15-sp4-updates-x86_64
+    sle-module-containers15-sp4-pool-x86_64
+    sle-module-containers15-sp4-updates-x86_64
   ],
   '12-SP4' =>
   %w[
@@ -349,6 +351,8 @@ CHANNEL_TO_SYNCH_BY_OS_VERSION = {
     sle-module-containers15-sp4-updates-x86_64
     sle-module-basesystem15-sp4-updates-x86_64
     sle-module-server-applications15-sp4-updates-x86_64
+    sle-module-containers15-sp4-pool-x86_64
+    sle-module-containers15-sp4-updates-x86_64
   ]
 }.freeze
 
