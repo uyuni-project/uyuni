d26b58c27964595e603ac606d69ac13afafa815f
maximenoel8 <55169628+maximenoel8@users.noreply.github.com>
2022-11-04 14:29:13 +1300

    Scc default repositories (#6097)
    * Change variable to select SCC repositories from cloud to scc repository

--- testsuite/features/support/env.rb
+++ testsuite/features/support/env.rb
@@ -32,6 +32,7 @@ Capybara.default_max_wait_time = ENV['CAPYBARA_TIMEOUT'] ? ENV['CAPYBARA_TIMEOUT
 DEFAULT_TIMEOUT = ENV['DEFAULT_TIMEOUT'] ? ENV['DEFAULT_TIMEOUT'].to_i : 250
 $is_cloud_provider = ENV["PROVIDER"].include? 'aws'
 $is_using_build_image = ENV.fetch('IS_USING_BUILD_IMAGE') { false }
+$is_using_scc_repositories = ENV.fetch('IS_USING_SCC_REPOSITORIES') { false }
 
 # QAM and Build Validation pipelines will provide a json file including all custom (MI) repositories
 custom_repos_path = File.dirname(__FILE__) + '/../upload_files/' + 'custom_repositories.json'
