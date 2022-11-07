f20da96f33f1214c1a47e2d7c6b8dc3510481184
mbussolotto <michele.bussolotto@suse.com>
2022-11-07 17:55:19 +0100

    fix tunnel ssh action chain

--- java/code/src/com/suse/manager/webui/services/SaltServerActionService.java
+++ java/code/src/com/suse/manager/webui/services/SaltServerActionService.java
@@ -970,29 +970,36 @@ public class SaltServerActionService {
             switch(fun) {
                 case "state.apply":
                     List<String> mods = (List<String>)kwargs.get("mods");
+                    if (CollectionUtils.isEmpty(mods)) {
+                        if (minion.isSshPush()) {
+                            // Apply highstate using a custom top.
+                            // The custom top is needed because salt-ssh invokes
+                            // "salt-call --local" and this needs a top file in order to apply the highstate
+                            // and salt-ssh doesn't pack one automatically in the state tarball
+                            return new SaltModuleRun(stateId,
+                                    "state.top",
+                                    serverAction.getParentAction().getId(),
+                                    singletonMap("topfn",
+                                            saltActionChainGeneratorService
+                                                    .getActionChainTopPath(actionChainId,
+                                                            serverAction.getParentAction().getId())),
+                                    emptyMap());
+                        }
+                        else {
+                            return new SaltModuleRun(stateId,
+                                    "state.apply",
+                                    serverAction.getParentAction().getId(),
+                                    emptyMap(),
+                                    createStateApplyKwargs(kwargs));
+                        }
 
-                    if (minion.isSshPush() && CollectionUtils.isEmpty(mods)) {
-                        // Apply highstate using a custom top.
-                        // The custom top is needed because salt-ssh invokes
-                        // "salt-call --local" and this needs a top file in order to apply the highstate
-                        // and salt-ssh doesn't pack one automatically in the state tarball
-                        return new SaltModuleRun(stateId,
-                                "state.top",
-                                serverAction.getParentAction().getId(),
-                                singletonMap("topfn",
-                                        saltActionChainGeneratorService
-                                        .getActionChainTopPath(actionChainId,
-                                                serverAction.getParentAction().getId())),
-                                emptyMap());
                     }
-                    else {
-                        return new SaltModuleRun(stateId,
-                                "state.apply",
-                                        serverAction.getParentAction().getId(),
-                                        !mods.isEmpty() ?
-                                                singletonMap("mods", mods) : emptyMap(),
-                                                createStateApplyKwargs(kwargs));
-                    }
+                    return new SaltModuleRun(stateId,
+                            "state.apply",
+                            serverAction.getParentAction().getId(),
+                            !mods.isEmpty() ?
+                                    singletonMap("mods", mods) : emptyMap(),
+                            createStateApplyKwargs(kwargs));
                 case "system.reboot":
                     Integer time = (Integer)kwargs.get("at_time");
                     return new SaltSystemReboot(stateId,
