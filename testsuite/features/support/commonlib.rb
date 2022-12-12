272ae99e3802287b96f4ce22f7cc975832a06095
Maxime Noel <maxime.noel@suse.com>
2022-12-12 16:05:06 +1300

    add regex support

--- testsuite/features/support/commonlib.rb
+++ testsuite/features/support/commonlib.rb
@@ -193,8 +193,8 @@ def generate_repository_name(repo_url)
   repo_name.delete_prefix! 'http://download.suse.de/ibs/SUSE:/Maintenance:/'
   repo_name.delete_prefix! 'http://download.suse.de/download/ibs/SUSE:/Maintenance:/'
   repo_name.delete_prefix! 'http://download.suse.de/download/ibs/SUSE:/'
-  repo_name.delete_prefix! '%r{http://.*compute.internal/SUSE:/}'
-  repo_name.delete_prefix! '%r{http://.*compute.internal/SUSE:/Maintenance:/}'
+  repo_name.sub!('%r{http://.*compute.internal/SUSE:/}','')
+  repo_name.sub!('%r{http://.*compute.internal/SUSE:/Maintenance:/}','')
   repo_name.gsub!('/', '_')
   repo_name.gsub!(':', '_')
   repo_name[0...64] # HACK: Due to the 64 characters size limit of a repository label
