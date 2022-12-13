f7f5c811d1c337d6a49d3b39603ac11acfe0f1a9
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2022-12-13 12:54:05 +0000

    Use debmirror package from Fedora 37 repo (as Fedora 35 is EOL)

--- susemanager-utils/testing/docker/scripts/cobbler/setup-reposync.sh
+++ susemanager-utils/testing/docker/scripts/cobbler/setup-reposync.sh
@@ -3,4 +3,4 @@
 # Dependencies for reposync tests
 zypper install --no-recommends -y dnf python3-librepo dnf dnf-plugins-core wget \
        perl-LockFile-Simple perl-Net-INET6Glue perl-LWP-Protocol-https ed
-dnf install -y  http://download.fedoraproject.org/pub/fedora/linux/releases/35/Everything/x86_64/os/Packages/d/debmirror-2.35-2.fc35.noarch.rpm
+dnf install -y  http://download.fedoraproject.org/pub/fedora/linux/releases/37/Everything/x86_64/os/Packages/d/debmirror-2.36-4.fc37.noarch.rpm
