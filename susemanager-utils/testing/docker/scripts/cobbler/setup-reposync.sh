c81d02318c0db6449a03c22be12569b0a5ec0e0f
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2022-12-22 12:14:00 +0000

    Consume cobbler-tests-containers instead of duplicating files

--- susemanager-utils/testing/docker/scripts/cobbler/setup-reposync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Dependencies for reposync tests
-zypper install --no-recommends -y dnf python3-librepo dnf dnf-plugins-core wget \
-       perl-LockFile-Simple perl-Net-INET6Glue perl-LWP-Protocol-https ed
-dnf install -y  http://download.fedoraproject.org/pub/fedora/linux/releases/37/Everything/x86_64/os/Packages/d/debmirror-2.36-4.fc37.noarch.rpm
