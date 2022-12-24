9f2c84a6fab867b57f20c855c5c4c82afb3bd9a3
elariekerboull <erwan.lariekerboull@suse.com>
2022-12-24 13:01:11 +0100

    Use salt bundle in key fingerprint feature (#19998)
    * Use salt bundle in key fingerprint feature

--- testsuite/features/step_definitions/salt_steps.rb
+++ testsuite/features/step_definitions/salt_steps.rb
@@ -56,12 +56,12 @@ When(/^I restart salt-minion on "(.*?)"$/) do |minion|
   if os_family =~ /^sles/ && os_version =~ /^11/
     node.run('rcsalt-minion restart', check_errors: false)
   else
-    node.run('systemctl restart salt-minion', check_errors: false)
+    node.run('systemctl restart venv-salt-minion', check_errors: false)
   end
 end
 
 When(/^I wait at most (\d+) seconds until Salt master sees "([^"]*)" as "([^"]*)"$/) do |key_timeout, minion, key_type|
-  cmd = "salt-key --list #{key_type}"
+  cmd = "venv-salt-key --list #{key_type}"
   repeat_until_timeout(timeout: key_timeout.to_i, message: "Minion '#{minion}' is not listed among #{key_type} keys on Salt master") do
     system_name = get_system_name(minion)
     unless system_name.empty?
@@ -338,7 +338,9 @@ end
 
 When(/^I see "([^"]*)" fingerprint$/) do |host|
   node = get_target(host)
-  salt_call = $use_salt_bundle ? "venv-salt-call" : "salt-call"
+  salt_call = "venv-salt-call"
+  # TODO: Replace previous line with following line
+  # salt_call = $use_salt_bundle ? "venv-salt-call" : "salt-call"
   output, _code = node.run("#{salt_call} --local key.finger")
   fing = output.split("\n")[1].strip!
   raise "Text: #{fing} not found" unless has_content?(fing)
