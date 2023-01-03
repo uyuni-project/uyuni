c549b8271b73626468b88d494969926a67eb732c
Cédric Bosdonnat <cbosdonnat@suse.com>
2023-01-03 11:20:51 +0100

    Clean up oracle exception translator

--- java/code/src/com/redhat/rhn/common/translation/SqlExceptionTranslator.java
+++ java/code/src/com/redhat/rhn/common/translation/SqlExceptionTranslator.java
@@ -15,7 +15,7 @@
 
 package com.redhat.rhn.common.translation;
 
-import com.redhat.rhn.common.db.ConstraintViolationException;
+import com.redhat.rhn.common.db.DatabaseException;
 import com.redhat.rhn.common.db.WrappedSQLException;
 
 import java.sql.SQLException;
@@ -31,50 +31,11 @@ public class SqlExceptionTranslator extends Translations {
     }
 
     /**
-     * Gets the appropriate runtime exception depending on whether the DB is oracle ornot
+     * Gets the appropriate runtime exception depending on whether the DB is oracle or not
      * @param e the exception
-     * @return the RuntimeExcetoion of the wrapped exception
+     * @return the RuntimeException of the wrapped exception
      */
-    public static RuntimeException sqlException(SQLException e) {
-        return postgreSqlException(e);
-    }
-
-    /**
-     * Convert from PSQLException to some RuntimeException sub-class.
-     *
-     * @param e Exception to translate.
-     * @return Translated RuntimeException with reference to the original.
-     */
-    private static RuntimeException postgreSqlException(SQLException e) {
+    public static DatabaseException sqlException(SQLException e) {
         return new WrappedSQLException(e.getMessage(), e);
-    }
-
-    // This will currently only work for SQLException's that come from
-    // Oracle.  To add more Databases, make this a private method, add a
-    // public method with a different name, but the same signature, in the
-    // new method, determine DB type, and call the correct translator.
-
-    /**
-     * Convert from SQLException to some other Exception type
-     * @param e The SQLException to translate
-     * @return The translated RuntimeException, which includes a reference
-     *         to the SQLException that was passed in.
-     */
-    private static RuntimeException oracleSQLException(SQLException e) {
-        int code = e.getErrorCode();
-        String msg = e.getMessage();
-        switch(code) {
-            case 1:
-                int ind = msg.indexOf('(') + 1;
-                String desc = msg.substring(ind, msg.indexOf(')', ind));
-                return new ConstraintViolationException(
-                             ExceptionConstants.VALUE_TOO_LARGE, desc, msg, e);
-            case 1401:
-            case 12899:
-                return new ConstraintViolationException(
-                             ExceptionConstants.VALUE_TOO_LARGE, null, msg, e);
-            default:
-                return new WrappedSQLException(e.getMessage(), e);
-        }
     }
 }
