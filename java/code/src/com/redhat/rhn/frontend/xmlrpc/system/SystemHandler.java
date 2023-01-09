fca690fa4428c4a6e99ab9aa1dfe5eeeab11d51a
Can Bulut Bayburt <1103552+cbbayburt@users.noreply.github.com>
2023-01-09 18:29:28 +0100

    Fix modular channel check during system update via XMLRPC (bsc#1206613) (#20098)
    (cherry picked from commit 934e707e4efd27ca33f473f614b63cce93806c1a)

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/system/SystemHandler.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/system/SystemHandler.java
@@ -3341,7 +3341,7 @@ public class SystemHandler extends BaseHandler {
             for (Long sid : serverIds) {
                 Server server = SystemManager.lookupByIdAndUser(sid, loggedInUser);
                 for (Channel channel : server.getChannels()) {
-                    if (channel.getModules() != null) {
+                    if (channel.isModular()) {
                         throw new ModulesNotAllowedException();
                     }
                 }
@@ -3646,7 +3646,7 @@ public class SystemHandler extends BaseHandler {
             for (Integer sid : sids) {
                 Server server = SystemManager.lookupByIdAndUser(sid.longValue(), loggedInUser);
                 for (Channel channel : server.getChannels()) {
-                    if (channel.getModules() != null) {
+                    if (channel.isModular()) {
                         hasModules = true;
                         break;
                     }
