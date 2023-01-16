0d1207c72da77cd7e1333db5bad022fa1ffb5ab8
elariekerboull <elariekerboull@suse.de>
2023-01-16 15:34:55 +0100

    Change quotes to resolve Rubocop errors

--- testsuite/features/step_definitions/navigation_steps.rb
+++ testsuite/features/step_definitions/navigation_steps.rb
@@ -749,7 +749,7 @@ Then(/^I should see a "([^"]*)" button in "([^"]*)" form$/) do |arg1, arg2|
 end
 
 Then(/^I should not see a warning sign$/) do
-  raise 'Errors detected in the Products Catalog' unless page.has_no_xpath?("//*[contains(@class, \"fa fa-li fa-exclamation-triangle text-warning\")]", wait: 5)
+  raise 'Errors detected in the Products Catalog' unless page.has_no_xpath?("//*[contains(@class, 'fa fa-li fa-exclamation-triangle text-warning')]", wait: 5)
 end
 
 Then(/^I select the "([^"]*)" repo$/) do |repo|
