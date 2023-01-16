4c379f9589780331ec2935ac99557c90a7b3ffcc
elariekerboull <elariekerboull@suse.de>
2023-01-16 15:12:07 +0100

    Change quotes

--- testsuite/features/step_definitions/navigation_steps.rb
+++ testsuite/features/step_definitions/navigation_steps.rb
@@ -749,8 +749,8 @@ Then(/^I should see a "([^"]*)" button in "([^"]*)" form$/) do |arg1, arg2|
 end
 
 Then(/^I should not see a warning sign$/) do
-  warning = "//*[contains(@class, \"fa fa-li fa-exclamation-triangle text-warning\")]"
-  raise "Errors detected in the Products Catalog" if page.has_xpath?(warning, wait: 5)
+  warning = '//*[contains(@class, \"fa fa-li fa-exclamation-triangle text-warning\")]'
+  raise 'Errors detected in the Products Catalog' if page.has_xpath?(warning, wait: 5)
 end
 
 Then(/^I select the "([^"]*)" repo$/) do |repo|
