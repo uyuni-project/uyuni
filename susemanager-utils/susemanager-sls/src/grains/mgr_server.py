8f519e5d0ecd05d00d730c9775a4c1421fd5e9fe
Witek Bedyk <witold.bedyk@suse.com>
2023-01-19 14:12:08 +0100

    Do not pass server related grains to minions

--- susemanager-utils/susemanager-sls/src/grains/mgr_server.py
+++ susemanager-utils/susemanager-sls/src/grains/mgr_server.py
@@ -13,6 +13,7 @@ RHNCONF = "/etc/rhn/rhn.conf"
 RHNCONFDEF = "/usr/share/rhn/config-defaults/rhn.conf"
 RHNWEBCONF = "/usr/share/rhn/config-defaults/rhn_web.conf"
 
+
 def _simple_parse_rhn_conf(cfile):
     result = {}
 
@@ -24,7 +25,7 @@ def _simple_parse_rhn_conf(cfile):
             line = line.strip()
             if not line or line[0] == '#':
                 continue
-            k,v = line.split("=", 1)
+            k, v = line.split("=", 1)
             result[k.strip()] = v.strip() or None
     return result
 
@@ -33,7 +34,7 @@ def server_grains():
     """ returns if this minion is a Uyuni/SUSE Manager Server and
         if it has a reporting database configured.
     """
-    grains = {'is_mgr_server': False, 'has_report_db': False, 'is_uyuni': True}
+    grains = {'is_mgr_server': False}
 
     config = _simple_parse_rhn_conf(RHNCONF)
 
@@ -44,9 +45,13 @@ def server_grains():
             grains['report_db_host'] = config.get('report_db_host')
             grains['report_db_name'] = config.get('report_db_name')
             grains['report_db_port'] = config.get('report_db_port', '5432')
+        else:
+            grains['has_report_db'] = False
         rhndef = _simple_parse_rhn_conf(RHNCONFDEF)
         if rhndef.get('product_name', 'uyuni') == 'SUSE Manager':
             grains['is_uyuni'] = False
+        else:
+            grains['is_uyuni'] = True
         webconfig = _simple_parse_rhn_conf(RHNWEBCONF)
         if grains['is_uyuni']:
             version = webconfig.get('web.version.uyuni')
