838e6c7fa0cc536a8d3644b9fe6e2c6a398018ab
elariekerboull <elariekerboull@suse.de>
2023-01-23 13:10:29 +0100

    Adjust conditions to raise salt error

--- testsuite/features/step_definitions/common_steps.rb
+++ testsuite/features/step_definitions/common_steps.rb
@@ -206,8 +206,8 @@ Then(/^the salt event log on server should contain no failures$/) do
   count_failures = output.to_s.scan(/false/).length
   output = output.join.to_s if output.respond_to?(:join)
   # Ignore the error if there is only the expected failure from min_salt_lock_packages.feature
-  ignore_error = (count_failures == 1) && output.include?('remove lock')
-  raise "\nFound #{count_failures} failures in salt event log:\n#{output}\n" if count_failures.nonzero?
+  ignore_error = (count_failures == 1) && output.include?('remove lock') && !$build_validation
+  raise "\nFound #{count_failures} failures in salt event log:\n#{output}\n" if count_failures.nonzero? && !ignore_error
 end
 
 # action chains
