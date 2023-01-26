f24862421ac24153486b65b78c8e18334a9bdb97
Michael Calmer <mc@suse.de>
2023-01-26 16:26:32 +0100

    update trigger for scc reg cache

--- schema/spacewalk/upgrade/susemanager-schema-4.3.16-to-susemanager-schema-4.3.17/003-update-sccregcache-trigger.sql
+++ schema/spacewalk/upgrade/susemanager-schema-4.3.16-to-susemanager-schema-4.3.17/003-update-sccregcache-trigger.sql
@@ -1,39 +1,29 @@
+drop trigger if exists rhn_cpu_up_trig on rhnCpu;
+create trigger
+rhn_cpu_up_trig
+after update on rhnCpu
+for each row
+when (OLD.nrcpu is distinct from NEW.nrcpu OR OLD.nrsocket is distinct from NEW.nrsocket OR OLD.nrcore is distinct from NEW.nrcore OR OLD.nrthread is distinct from NEW.nrthread)
+execute procedure rhn_cpu_up_trig_fun();
 
---
--- Copyright (c) 2008--2016 Red Hat, Inc.
---
--- This software is licensed to you under the GNU General Public License,
--- version 2 (GPLv2). There is NO WARRANTY for this software, express or
--- implied, including the implied warranties of MERCHANTABILITY or FITNESS
--- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
--- along with this software; if not, see
--- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
--- 
--- Red Hat trademarks are not licensed under GPLv2. No permission is
--- granted to use or replicate Red Hat trademarks that are incorporated
--- in this software or its documentation. 
---
---
--- 
---
 
-create or replace function rhn_virtinst_mod_trig_fun() returns trigger
-as
+create or replace function rhn_ram_up_trig_fun() returns trigger as
 $$
 begin
-        new.modified := current_timestamp;
-
-	return new;
+        update suseSCCRegCache
+           set scc_reg_required = 'Y'
+         where server_id = new.server_id;
+        return new;
 end;
-$$
-language plpgsql;
+$$ language plpgsql;
 
-
+drop trigger if exists rhn_ram_up_trig on rhnRam;
 create trigger
-rhn_virtinst_mod_trig
-before insert or update on rhnVirtualInstance
+rhn_ram_up_trig
+after update on rhnRam
 for each row
-execute procedure rhn_virtinst_mod_trig_fun();
+when (OLD.ram is distinct from NEW.ram)
+execute procedure rhn_ram_up_trig_fun();
 
 create or replace function rhn_virtinst_iud_trig_fun() returns trigger
 as
@@ -58,7 +48,7 @@ begin
 end;
 $$ language plpgsql;
 
-
+drop trigger if exists rhn_virtinst_iud_trig on rhnVirtualInstance;
 create trigger
 rhn_virtinst_iud_trig
 after insert or update or delete on rhnVirtualInstance
