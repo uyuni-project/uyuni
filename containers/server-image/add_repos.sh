b032822ca4af7faaf723bbaecf2438e2371cb5e8
Artem Shiliaev <artem.shiliaev@suse.com>
2023-01-27 15:56:17 +0100

    initial version uyuni server image

---
+++ containers/server-image/add_repos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -xe
+
+if [ -n "$1" ]; then
+  # update
+  zypper ar -G http://download.opensuse.org/update/leap/15.4/sle/ sle_update_repo
+  zypper ar -G http://download.opensuse.org/update/leap/15.4/oss/ os_update_repo
+  zypper ar -G http://download.opensuse.org/update/leap/15.4/backports/ backports_update_repo
+
+  # distribution
+  zypper ar -G http://download.opensuse.org/distribution/leap/15.4/repo/oss/ os_pool_repo
+
+  # product
+  #TODO uncomment when changes are in master branch
+  #zypper ar -f -G http://download.opensuse.org/repositories/systemsmanagement:/Uyuni:/Master/images/repo/Uyuni-Server-POOL-x86_64-Media1/ server_pool_repo
+  zypper ar -G http://download.opensuse.org/repositories/systemsmanagement:/Uyuni:/Master/images/repo/Testing-Overlay-POOL-x86_64-Media1/ testing_overlay_devel_repo
+
+  zypper addrepo $1 product
+fi
