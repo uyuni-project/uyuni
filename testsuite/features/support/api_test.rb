5b7f9d5cc06a47885c7bd8f6231d4d96df385522
Oscar Barrios <obarrios@suse.com>
2023-02-08 16:18:47 +0100

    Add Doc headers in all the Class and Module methods (#6453) (#20390)
    Co-authored-by: Vanda Barata <vanda.barata@suse.com>
    Co-authored-by: Dominik Gedon <dgedon@suse.de>

--- testsuite/features/support/api_test.rb
+++ testsuite/features/support/api_test.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 SUSE LLC.
+# Copyright (c) 2022-2023 SUSE LLC.
 # Licensed under the terms of the MIT license.
 
 require_relative 'namespaces/actionchain'
@@ -17,6 +17,11 @@ require_relative 'http_client'
 
 # Abstract parent class describing an API test
 class ApiTest
+  ##
+  # Creates objects that are used to interact with the API.
+  #
+  # Args:
+  #   _host: The hostname of the Spacewalk server.
   def initialize(_host)
     @actionchain = NamespaceActionchain.new(self)
     @activationkey = NamespaceActivationkey.new(self)
@@ -48,6 +53,12 @@ class ApiTest
   attr_reader :token
   attr_writer :token
 
+  ##
+  # Calls a function with the given name and parameters, and returns its response.
+  #
+  # Args:
+  #   name: The name of the method you want to call.
+  #   *params: The parameters to pass to the API call.
   def call(name, *params)
     thread =
       Thread.new do
@@ -65,16 +76,24 @@ end
 
 # Derived class for an XML-RPC test
 class ApiTestXmlrpc < ApiTest
+  ##
+  # Creates a new instance of the XmlrpcClient class, and assigns it to the @connection instance variable.
+  #
+  # Args:
+  #   host: The hostname of the server.
   def initialize(host)
     super
     @connection = XmlrpcClient.new(host)
   end
 
-  # during XML-RPC tests, dates are XMLRPC::DateTime's
+  ##
+  # Returns a boolean on whether the given attribute is an XMLRPC::DateTime object or not
   def date?(attribute)
     attribute.class == XMLRPC::DateTime
   end
 
+  ##
+  # Returns the current date and time as an XMLRPC::DateTime object.
   def date_now
     now = Time.now
     XMLRPC::DateTime.new(now.year, now.month, now.day, now.hour, now.min, now.sec)
@@ -83,12 +102,22 @@ end
 
 # Derived class for an HTTP test
 class ApiTestHttp < ApiTest
+  ##
+  # It creates a new instance of the HttpClient class.
+  #
+  # Args:
+  #   host: The hostname of the server.
   def initialize(host)
     super
     @connection = HttpClient.new(host)
   end
 
-  # during HTTP tests, dates are strings
+  ##
+  # Attempts to parse a given string as a Date object, to validate it.
+  # Returns a boolean on whether it's a string containing a valid Date or not.
+  #
+  # Args:
+  #     attribute: The date object to be parsed.
   def date?(attribute)
     begin
       ok = true
@@ -99,6 +128,8 @@ class ApiTestHttp < ApiTest
     ok
   end
 
+  ##
+  # It returns a string with the current date and time in the format `YYYY-MM-DDTHH:MM:SS.LLL+HHMM`
   def date_now
     now = Time.now
     now.strftime('%Y-%m-%dT%H:%M:%S.%L%z')
