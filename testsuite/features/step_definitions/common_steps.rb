b0998b6daf3f7948b7177cf9f438bcb605c197b8
elariekerboull <erwan.lariekerboull@suse.com>
2023-02-15 10:58:21 +0100

    QE: Adjust server salt error detection (#6490)

--- testsuite/features/step_definitions/common_steps.rb
+++ testsuite/features/step_definitions/common_steps.rb
@@ -197,15 +197,18 @@ end
 Then(/^the salt event log on server should contain no failures$/) do
   # upload salt event parser log
   file = 'salt_event_parser.py'
-  source = File.dirname(__FILE__) + '/../upload_files/' + file
-  dest = "/tmp/" + file
+  source = "#{File.dirname(__FILE__)}/../upload_files/#{file}"
+  dest = "/tmp/#{file}"
   return_code = file_inject($server, source, dest)
   raise 'File injection failed' unless return_code.zero?
   # print failures from salt event log
   output, _code = $server.run("python3 /tmp/#{file}")
   count_failures = output.to_s.scan(/false/).length
   output = output.join.to_s if output.respond_to?(:join)
-  raise "\nFound #{count_failures} failures in salt event log:\n#{output}\n" if count_failures.nonzero?
+  # Ignore the error if there is only the expected failure from min_salt_lock_packages.feature
+  ignore_error = false
+  ignore_error = output.include?('remove lock') if count_failures == 1 && !$build_validation
+  raise "\nFound #{count_failures} failures in salt event log:\n#{output}\n" if count_failures.nonzero? and !ignore_error
 end
 
 # action chains
