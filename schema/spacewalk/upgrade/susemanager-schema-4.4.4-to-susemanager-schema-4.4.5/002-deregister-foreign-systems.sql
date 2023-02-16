70ca120e86ee7afabf01e7938d733c9147ffb060
Michael Calmer <mc@suse.de>
2023-02-16 11:01:23 +0100

    initial trigger sending virualization host data to SCC

--- schema/spacewalk/upgrade/susemanager-schema-4.4.4-to-susemanager-schema-4.4.5/002-deregister-foreign-systems.sql
+++ schema/spacewalk/upgrade/susemanager-schema-4.4.4-to-susemanager-schema-4.4.5/002-deregister-foreign-systems.sql
@@ -6,3 +6,11 @@ update suseSCCRegCache
                        join rhnservergroup sg on sgm.server_group_id = sg.id
                        join rhnservergrouptype sgt on sg.group_type = sgt.id
                       where sgt.label = 'foreign_entitled');
+
+-- trigger sending virualization host data to SCC for the first time
+update susesccregcache
+   set scc_reg_required = 'Y'
+ where server_id in (
+	select distinct host_system_id
+         from rhnvirtualinstance
+        where host_system_id is not null);
