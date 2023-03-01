4b1dee6b2f547deca4983064be384be9df253190
elariekerboull <erwan.lariekerboull@suse.com>
2023-03-01 11:16:17 +0100

    Implement adding buildhost option on activation key via api

--- testsuite/features/step_definitions/api_common.rb
+++ testsuite/features/step_definitions/api_common.rb
@@ -233,6 +233,9 @@ When(/^I create an activation key including custom channels for "([^"]*)" via AP
 
   is_ssh_minion = client.include? 'ssh_minion'
   $api_test.activationkey.set_details(key, description, base_channel, 100, is_ssh_minion ? 'ssh-push' : 'default')
+  entitlements = ''
+  entitlements = ['container_build_host', 'osimage_build_host'] if client.include? 'buildhost'
+  $api_test.activationkey.set_entitlement(key, entitlements) unless entitlements.empty?
 
   # Get the list of child channels for this base channel
   child_channels = $api_test.channel.software.list_child_channels(base_channel)
