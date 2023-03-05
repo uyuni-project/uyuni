bd2c216c5d96e76da8a11fd596e4e9ae17b223fe
Bhavya031 <98141026+Bhavya031@users.noreply.github.com>
2023-03-05 23:38:21 +0530

    Elaborator.java now wraps a List with an object

--- java/code/src/com/redhat/rhn/common/db/datasource/Elaborator.java
+++ java/code/src/com/redhat/rhn/common/db/datasource/Elaborator.java
@@ -28,12 +28,12 @@ public interface Elaborator {
      * Returns an elaborated list for the given List of objects
      * @param objectsToElaborate the list of objects to elaborate
      */
-    void elaborate(List<T> objectsToElaborate);
+    void elaborate(List<object> objectsToElaborate);
 
     /**
      * Returns an elaborated list for the given List of objects with an updated session
      * @param objectsToElaborate the list of objects to elaborate
      * @param session session to use
      */
-    void elaborate(List<T> objectsToElaborate, Session session);
+    void elaborate(List<object> objectsToElaborate, Session session);
 }
