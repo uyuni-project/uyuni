a862253e649524c3dbabb05d1e09f9dc848c4421
Michael Calmer <mc@suse.de>
2023-03-05 14:07:23 +0100

    remove traditional stack package spacewalk-oscap

--- java/code/src/com/redhat/rhn/frontend/action/systems/audit/ScapSetupAction.java
+++ java/code/src/com/redhat/rhn/frontend/action/systems/audit/ScapSetupAction.java
@@ -31,7 +31,6 @@ import com.redhat.rhn.manager.audit.ScapManager;
 public abstract class ScapSetupAction extends RhnAction {
     private static final String SCAP_ENABLED = "scapEnabled";
     private static final String REQUIRED_PKG = "requiredPackage";
-    private static final String SPACEWALK_OSCAP = "spacewalk-oscap";
     private static final String OPENSCAP_SUSE_PKG = "openscap-utils";
     private static final String OPENSCAP_REDHAT_PKG = "openscap-scanner";
     private static final String OPENSCAP_DEBIAN_PKG = "libopenscap8";
@@ -40,7 +39,7 @@ public abstract class ScapSetupAction extends RhnAction {
         Server server = context.lookupAndBindServer();
         User user = context.getCurrentUser();
         boolean enabled = false;
-        String requiredPkg = SPACEWALK_OSCAP;
+        String requiredPkg = "";
         if (server.asMinionServer().isPresent()) {
             MinionServer minion = server.asMinionServer().get();
             switch (minion.getOsFamily()) {
