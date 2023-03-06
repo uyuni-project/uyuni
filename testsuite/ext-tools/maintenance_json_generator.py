def89b21d4b6e385fdc27588ad1c8f9d5b498d3e
Konstantinos Tsamis <ktsamis@suse.de>
2023-03-06 11:52:45 +0100

    Refactor variable name

--- testsuite/ext-tools/maintenance_json_generator.py
+++ testsuite/ext-tools/maintenance_json_generator.py
@@ -8,50 +8,50 @@ import requests
 # Default dictionary for client tools, essentially anything that is common between SUMA versions
 defaultdict = {
     "sle12sp4_client": "/SUSE_Updates_SLE-Manager-Tools_12_x86_64/",
-    "sle12sp4_minion": "/SUSE_Updates_SLE-Manager-Tools_12_x86_64/",
+    "sle12sp4_mi_idnion": "/SUSE_Updates_SLE-Manager-Tools_12_x86_64/",
     "sle12sp5_client": "/SUSE_Updates_SLE-Manager-Tools_12_x86_64/",
-    "sle12sp5_minion": "/SUSE_Updates_SLE-Manager-Tools_12_x86_64/",
+    "sle12sp5_mi_idnion": "/SUSE_Updates_SLE-Manager-Tools_12_x86_64/",
     "sle15_client": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
                      "/SUSE_Updates_SLE-Product-SLES_15-LTSS_x86_64/"],
-    "sle15_minion": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
+    "sle15_mi_idnion": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
                      "/SUSE_Updates_SLE-Product-SLES_15-LTSS_x86_64/"],
     "sle15sp1_client": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
                         "/SUSE_Updates_SLE-Product-SLES_15-SP1-LTSS_x86_64/"],
-    "sle15sp1_minion": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
+    "sle15sp1_mi_idnion": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
                         "/SUSE_Updates_SLE-Product-SLES_15-SP1-LTSS_x86_64/"],
     "sle15sp2_client": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
                         "/SUSE_Updates_SLE-Product-SLES_15-SP2-LTSS_x86_64/"],
-    "sle15sp2_minion": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
+    "sle15sp2_mi_idnion": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
                         "/SUSE_Updates_SLE-Product-SLES_15-SP2-LTSS_x86_64/"],
     "sle15sp3_client": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
                         "/SUSE_Updates_SLE-Module-Basesystem_15-SP3_x86_64/",
                         "/SUSE_Updates_SLE-Module-Server-Applications_15-SP3_x86_64/"],
-    "sle15sp3_minion": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
+    "sle15sp3_mi_idnion": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
                         "/SUSE_Updates_SLE-Module-Basesystem_15-SP3_x86_64/",
                         "/SUSE_Updates_SLE-Module-Server-Applications_15-SP3_x86_64/"],
     "sle15sp4_client": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
                         "/SUSE_Updates_SLE-Module-Basesystem_15-SP4_x86_64/",
                         "/SUSE_Updates_SLE-Module-Server-Applications_15-SP4_x86_64/"],
-    "sle15sp4_minion": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
+    "sle15sp4_mi_idnion": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
                         "/SUSE_Updates_SLE-Module-Basesystem_15-SP4_x86_64/",
                         "/SUSE_Updates_SLE-Module-Server-Applications_15-SP4_x86_64/"],
     "ceos7_client": "/SUSE_Updates_RES_7-CLIENT-TOOLS_x86_64/",
-    "ceos7_minion": "/SUSE_Updates_RES_7-CLIENT-TOOLS_x86_64",
-    "rocky8_minion": "/SUSE_Updates_RES_8-CLIENT-TOOLS_x86_64/",
-    "ubuntu1804_minion": "/SUSE_Updates_Ubuntu_18.04-CLIENT-TOOLS_x86_64/",
-    "ubuntu2004_minion": "/SUSE_Updates_Ubuntu_20.04-CLIENT-TOOLS_x86_64/",
-    "ubuntu2204_minion": "/SUSE_Updates_Ubuntu_22.04-CLIENT-TOOLS_x86_64/",
-    "debian10_minion": "/SUSE_Updates_Debian_10-CLIENT-TOOLS_x86_64/",
-    "debian11_minion": "/SUSE_Updates_Debian_11-CLIENT-TOOLS_x86_64/",
-    "opensuse153arm_minion": "/SUSE_Updates_openSUSE-SLE_15.3/",
-    "opensuse154arm_minion": "/SUSE_Updates_openSUSE-SLE_15.4/",
-    "rhel9_minion": "/SUSE_Updates_EL_9-CLIENT-TOOLS_x86_64/",
-    "rocky9_minion": "/SUSE_Updates_EL_9-CLIENT-TOOLS_x86_64/",
-    "alma9_minion": "/SUSE_Updates_EL_9-CLIENT-TOOLS_x86_64/",
-    "oracle9_minion": "/SUSE_Updates_EL_9-CLIENT-TOOLS_x86_64/",
-    "slemicro52_minion": ["/SUSE_Updates_SLE-Manager-Tools-For-Micro_5_x86_64/",
+    "ceos7_mi_idnion": "/SUSE_Updates_RES_7-CLIENT-TOOLS_x86_64",
+    "rocky8_mi_idnion": "/SUSE_Updates_RES_8-CLIENT-TOOLS_x86_64/",
+    "ubuntu1804_mi_idnion": "/SUSE_Updates_Ubuntu_18.04-CLIENT-TOOLS_x86_64/",
+    "ubuntu2004_mi_idnion": "/SUSE_Updates_Ubuntu_20.04-CLIENT-TOOLS_x86_64/",
+    "ubuntu2204_mi_idnion": "/SUSE_Updates_Ubuntu_22.04-CLIENT-TOOLS_x86_64/",
+    "debian10_mi_idnion": "/SUSE_Updates_Debian_10-CLIENT-TOOLS_x86_64/",
+    "debian11_mi_idnion": "/SUSE_Updates_Debian_11-CLIENT-TOOLS_x86_64/",
+    "opensuse153arm_mi_idnion": "/SUSE_Updates_openSUSE-SLE_15.3/",
+    "opensuse154arm_mi_idnion": "/SUSE_Updates_openSUSE-SLE_15.4/",
+    "rhel9_mi_idnion": "/SUSE_Updates_EL_9-CLIENT-TOOLS_x86_64/",
+    "rocky9_mi_idnion": "/SUSE_Updates_EL_9-CLIENT-TOOLS_x86_64/",
+    "alma9_mi_idnion": "/SUSE_Updates_EL_9-CLIENT-TOOLS_x86_64/",
+    "oracle9_mi_idnion": "/SUSE_Updates_EL_9-CLIENT-TOOLS_x86_64/",
+    "slemi_idcro52_mi_idnion": ["/SUSE_Updates_SLE-Manager-Tools-For-Micro_5_x86_64/",
                           "/SUSE_Updates_SUSE-MicroOS_5.2_x86_64/"],
-    "slemicro53_minion": ["/SUSE_Updates_SLE-Manager-Tools-For-Micro_5_x86_64/",
+    "slemi_idcro53_mi_idnion": ["/SUSE_Updates_SLE-Manager-Tools-For-Micro_5_x86_64/",
                           "/SUSE_Updates_SUSE-MicroOS_5.3_x86_64/"],
     }
 
@@ -88,7 +88,7 @@ def parse_args():
     parser.add_argument("-v", "--version", dest="version",
                         help="Version of SUMA you want to run this script for, options are 42 for 4.2 or 43 for 4.3",
                         default="43", action='store')
-    parser.add_argument("-i", "--mis", dest="mis", help="MI IDs", default=None, action='store')
+    parser.add_argument("-i", "--mi_ids", dest="mi_ids", help="MI IDs", default=None, action='store')
 
     args = parser.parse_args()
     return args
@@ -107,17 +107,17 @@ def read_requests():
     output = result.stdout.decode('utf-8')
     lines = output.splitlines()
     # Create empty list to add the maintenance incidents from the output
-    mis = []
+    mi_ids = []
     for line in lines:
         if "ReviewRequest" in line:
             line1 = line.rstrip()
             line2 = line1.split(sep=":")
-            mi = line2[3]
-            mis.append(mi)
-    return mis
+            mi_id = line2[3]
+            mi_ids.append(mi_id)
+    return mi_ids
 
 
-def find_valid_repos(mis, version):
+def find_valid_repos(mi_ids, version):
     if version == '42':
         dict_version = nodesdict42
     elif version == '43':
@@ -128,42 +128,42 @@ def find_valid_repos(mis, version):
 
     finaldict = {}
     for node, suffixraw in dict_version.items():
-        for mi in mis:
+        for mi_id in mi_ids:
             if isinstance(suffixraw, str):
                 suffix = suffixraw
-                repo = create_url(mi, suffix)
+                repo = create_url(mi_id, suffix)
                 if repo is not None:
                     if node in finaldict:
-                        # This is needed for mis that have multiple repos for each node, e.g. basesystem and server
+                        # This is needed for mi_ids that have multiple repos for each node, e.g. basesystem and server
                         # apps for server
-                        if mi in finaldict[node]:
+                        if mi_id in finaldict[node]:
                             for i in range(1, 100):
-                                if str(mi) + '-' + str(i) not in finaldict[node]:
-                                    finaldict[node][str(mi) + '-' + str(i)] = repo
+                                if str(mi_id) + '-' + str(i) not in finaldict[node]:
+                                    finaldict[node][str(mi_id) + '-' + str(i)] = repo
                                     break
                         else:
-                            finaldict[node][mi] = repo
+                            finaldict[node][mi_id] = repo
                     else:
-                        # for each mi we have multiple repos sometimes for each node
-                        finaldict[node] = {mi: repo}
+                        # for each mi_id we have multiple repos sometimes for each node
+                        finaldict[node] = {mi_id: repo}
             elif isinstance(suffixraw, list):
                 for suffix in suffixraw:
-                    repo = create_url(mi, suffix)
+                    repo = create_url(mi_id, suffix)
                     if repo is not None:
                         if node in finaldict:
-                            # This is needed for mis that have multiple repos for each node, e.g. basesystem and
+                            # This is needed for mi_ids that have multiple repos for each node, e.g. basesystem and
                             # server apps for server
-                            if mi in finaldict[node]:
+                            if mi_id in finaldict[node]:
                                 for i in range(1, 100):
-                                    if str(mi) + '-' + str(i) not in finaldict[node]:
-                                        finaldict[node][str(mi) + '-' + str(i)] = repo
+                                    if str(mi_id) + '-' + str(i) not in finaldict[node]:
+                                        finaldict[node][str(mi_id) + '-' + str(i)] = repo
                                         break
                             else:
-                                # for each mi we have multiple repos sometimes for each node
-                                finaldict[node][mi] = repo
+                                # for each mi_id we have multiple repos sometimes for each node
+                                finaldict[node][mi_id] = repo
                         else:
-                            # for each mi we have multiple repos sometimes for each node
-                            finaldict[node] = {mi: repo}
+                            # for each mi_id we have multiple repos sometimes for each node
+                            finaldict[node] = {mi_id: repo}
 
     # Format into json and print
     # Check that it's not empty and save to file
@@ -175,10 +175,10 @@ def find_valid_repos(mis, version):
         sys.exit(1)
 
 
-def create_url(mi, suffix):
-    link = ["http://download.suse.de/ibs/SUSE:/Maintenance:/", str(mi)]
+def create_url(mi_id, suffix):
+    link = ["http://download.suse.de/ibs/SUSE:/Maintenance:/", str(mi_id)]
 
-    if link[:-1] == str(mi):
+    if link[:-1] == str(mi_id):
         link.append(suffix)
         url = ''.join(link)
     else:
@@ -191,11 +191,11 @@ def create_url(mi, suffix):
 
 def main():
     args = parse_args()
-    if args.mis is not None:
-        mis = args.mis.split(",")
+    if args.mi_ids is not None:
+        mi_ids = args.mi_ids.split(",")
     else:
-        mis = read_requests()
-    find_valid_repos(mis, args.version)
+        mi_ids = read_requests()
+    find_valid_repos(mi_ids, args.version)
 
 
 if __name__ == '__main__':
