e557e2d6eb905ece5746bb387fd1682732a14b58
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2023-03-08 14:02:46 +0000

    Do not trigger extra cobbler sync when changing kickstart data (bsc#1208536)

--- java/code/src/com/redhat/rhn/manager/kickstart/BaseKickstartCommand.java
+++ java/code/src/com/redhat/rhn/manager/kickstart/BaseKickstartCommand.java
@@ -90,7 +90,7 @@ public abstract class BaseKickstartCommand implements PersistOperation {
 
         KickstartFactory.saveKickstartData(ksData);
 
-        CobblerProfileEditCommand cmd = new CobblerProfileEditCommand(ksdata, user);
+        CobblerProfileEditCommand cmd = new CobblerProfileEditCommand(ksdata, user, false);
         ValidatorError err = cmd.store();
         logger.debug("Did we get an error storing to cobbler: {}", err);
         return err;
