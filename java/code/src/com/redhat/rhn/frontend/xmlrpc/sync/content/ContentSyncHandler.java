1e05660fb3f7c3122609cabcc1bbb1aff03d30da
Michael Calmer <mc@suse.de>
2023-03-17 13:32:04 +0100

    rename lookupSCCCredentials in listSCCCredentials

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/sync/content/ContentSyncHandler.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/sync/content/ContentSyncHandler.java
@@ -315,7 +315,7 @@ public class ContentSyncHandler extends BaseHandler {
     public Integer deleteCredentials(User loggedInUser, String username)
             throws ContentSyncException {
         ensureSatAdmin(loggedInUser);
-        for (Credentials c : CredentialsFactory.lookupSCCCredentials()) {
+        for (Credentials c : CredentialsFactory.listSCCCredentials()) {
             if (c.getUsername().equals(username)) {
                 new MirrorCredentialsManager().deleteMirrorCredentials(c.getId(), null);
                 break;
