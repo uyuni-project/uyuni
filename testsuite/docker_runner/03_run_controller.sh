59f725c741ff9498627200c3a0c776c90c5dc14f
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2023-03-21 17:10:43 +0100

    CI: Acceptance test at PR v2: rename containers
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- testsuite/docker_runner/03_run_controller.sh
+++ testsuite/docker_runner/03_run_controller.sh
@@ -2,7 +2,7 @@
 set -ex
 src_dir=$(cd $(dirname "$0")/../.. && pwd -P)
 
-docker run --rm -d --network uyuni-network-1 -v /tmp/test-all-in-one:/tmp --name controller-test-1 -h controller-test-1 -v ${src_dir}/testsuite:/testsuite ghcr.io/$UYUNI_PROJECT/uyuni/controller-dev:$UYUNI_VERSION
+docker run --rm -d --network uyuni-network-1 -v /tmp/test-all-in-one:/tmp --name controller-test-1 -h controller-test-1 -v ${src_dir}/testsuite:/testsuite ghcr.io/$UYUNI_PROJECT/uyuni/ci-test-controller-dev:$UYUNI_VERSION
 
 # sleep 10
 docker ps
--- testsuite/docker_runner/09_run_sshminion.sh
+++ testsuite/docker_runner/09_run_sshminion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
-docker run --rm -d --network uyuni-network-1 -v /tmp/test-all-in-one:/tmp --name opensusessh -h opensusessh ghcr.io/$UYUNI_PROJECT/uyuni/opensuse-minion:$UYUNI_VERSION
+docker run --rm -d --network uyuni-network-1 -v /tmp/test-all-in-one:/tmp --name opensusessh -h opensusessh ghcr.io/$UYUNI_PROJECT/uyuni/ci-test-opensuse-minion:$UYUNI_VERSION
 # sleep 10
 
