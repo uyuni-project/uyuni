d72c681cb14a7e5e6fe8ab41cf05963db0356829
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2023-03-22 12:28:26 +0100

    CI: Acceptance tests at PR: rename poc to container
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- testsuite/docker_runner/11_run_core_tests.sh
+++ testsuite/docker_runner/11_run_core_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
-docker exec controller-test-1 bash -c "export CUCUMBER_PUBLISH_TOKEN=${CUCUMBER_PUBLISH_TOKEN} && export PROVIDER=docker && export SERVER=uyuni-server-all-in-one-test-1 && export HOSTNAME=controller-test-1 && cd /testsuite && rake cucumber:poc_core"
+docker exec controller-test-1 bash -c "export CUCUMBER_PUBLISH_TOKEN=${CUCUMBER_PUBLISH_TOKEN} && export PROVIDER=docker && export SERVER=uyuni-server-all-in-one-test-1 && export HOSTNAME=controller-test-1 && cd /testsuite && rake cucumber:container_core"
 
--- testsuite/docker_runner/16_run_init_clients_tests.sh
+++ testsuite/docker_runner/16_run_init_clients_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -xe
-docker exec controller-test-1 bash -c "export CUCUMBER_PUBLISH_TOKEN=${CUCUMBER_PUBLISH_TOKEN} && export PROVIDER=docker && export SERVER=uyuni-server-all-in-one-test-1 && export HOSTNAME=controller-test-1 && export SSH_MINION=opensusessh && export MINION=sle_minion && export RHLIKE_MINION=rhlike_minion && export DEBLIKE_MINION=deblike_minion && cd /testsuite && rake cucumber:poc_init_clients"
+docker exec controller-test-1 bash -c "export CUCUMBER_PUBLISH_TOKEN=${CUCUMBER_PUBLISH_TOKEN} && export PROVIDER=docker && export SERVER=uyuni-server-all-in-one-test-1 && export HOSTNAME=controller-test-1 && export SSH_MINION=opensusessh && export MINION=sle_minion && export RHLIKE_MINION=rhlike_minion && export DEBLIKE_MINION=deblike_minion && cd /testsuite && rake cucumber:container_init_clients"
