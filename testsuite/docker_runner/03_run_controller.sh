c1f71e53e5022d6eaff2d756da0c9cf56f6e7e2a
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2023-03-23 16:17:38 +0100

    CI: Acceptance tests at PR v2: remove commented code
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- testsuite/docker_runner/03_run_controller.sh
+++ testsuite/docker_runner/03_run_controller.sh
@@ -4,5 +4,4 @@ src_dir=$(cd $(dirname "$0")/../.. && pwd -P)
 
 docker run --rm -d --network uyuni-network-1 -v /tmp/test-all-in-one:/tmp --name controller-test-1 -h controller-test-1 -v ${src_dir}/testsuite:/testsuite ghcr.io/$UYUNI_PROJECT/uyuni/ci-test-controller-dev:$UYUNI_VERSION
 
-# sleep 10
 docker ps
--- testsuite/docker_runner/06_start_server.sh
+++ testsuite/docker_runner/06_start_server.sh
@@ -3,5 +3,4 @@ set -xe
 src_dir=$(cd $(dirname "$0")/../.. && pwd -P)
 
 docker run --rm --tmpfs /run -v ${src_dir}/schema/spacewalk/upgrade/:/etc/sysconfig/rhn/schema-upgrade/ -v ${src_dir}/schema/reportdb/upgrade/:/etc/sysconfig/rhn/reportdb-schema-upgrade/ -v ${src_dir}/web:/web -v ${src_dir}/branding:/branding -v ${src_dir}/java:/java -v /sys/fs/cgroup:/sys/fs/cgroup:rw -v /tmp/test-all-in-one:/tmp --cgroupns=host --add-host=download.opensuse.org:195.135.221.134 -h uyuni-server-all-in-one-test-1 -p 8443:443 -p 8080:80 -p 4505:4505 -p 4506:4506 -d --name=uyuni-server-all-in-one-test-1 --network uyuni-network-1 ghcr.io/$UYUNI_PROJECT/uyuni/ci-test-server-all-in-one-dev:$UYUNI_VERSION
-# sleep 10
 
