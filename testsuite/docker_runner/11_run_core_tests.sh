f70db9d4c777b590ef6e718740ceafe8af6cd16b
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2023-03-28 17:09:20 +0200

    CI: Acceptante  test at PR v2: replace docker by podman
    Fixes https://github.com/SUSE/spacewalk/issues/20909
    
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- testsuite/docker_runner/11_run_core_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -xe
-docker exec controller-test bash -c "export CUCUMBER_PUBLISH_TOKEN=${CUCUMBER_PUBLISH_TOKEN} && export PROVIDER=docker && export SERVER=uyuni-server-all-in-one-test && export HOSTNAME=controller-test && cd /testsuite && rake cucumber:container_core"
-
