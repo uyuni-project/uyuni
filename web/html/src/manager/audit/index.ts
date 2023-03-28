e1a30ce525f0caeefb8d6f7b85c43d012896b099
Abid Mehmood <amehmood@suse.de>
2023-03-28 14:28:03 +0200

    Add route for Tailoring file list

--- web/html/src/manager/audit/index.ts
+++ web/html/src/manager/audit/index.ts
@@ -2,4 +2,5 @@ export default {
   "audit/coco": () => import("./coco/coco-global-scans-list.renderer"),
   "audit/cveaudit": () => import("./cveaudit/cveaudit"),
   "audit/subscription-matching": () => import("./subscription-matching/subscription-matching"),
+  "audit/list-tailoring-files":() => import("./scap/list-tailoring-files"),
 };
