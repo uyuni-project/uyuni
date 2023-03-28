7c061e220e90b24191034e739db0c2c53321b978
Pascal Arlt <parlt@suse.com>
2023-03-28 16:51:13 +0200

    Update schema migration script
    Signed-off-by: Pascal Arlt <parlt@suse.com>

--- schema/spacewalk/upgrade/susemanager-schema-4.3.17-to-susemanager-schema-4.3.18/400-update-recurring-action.sql
+++ schema/spacewalk/upgrade/susemanager-schema-4.3.17-to-susemanager-schema-4.3.18/400-update-recurring-action.sql
@@ -8,5 +8,96 @@
 -- along with this software; if not, see
 -- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 --
---
--- TODO
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS suseInternalState
+(
+  id                NUMERIC NOT NULL
+                    CONSTRAINT suse_internal_state_id_pk PRIMARY KEY,
+  name              VARCHAR(128) NOT NULL,
+  label             VARCHAR(128) NOT NULL
+);
+
+INSERT INTO suseInternalState (id, name, label)
+         SELECT 1, 'certs', 'Update certificates'
+         WHERE NOT EXISTS (SELECT 1 FROM suseInternalState WHERE name='certs');
+
+INSERT INTO suseInternalState (id, name, label)
+         SELECT 2, 'channels', 'Update Software Channels'
+         WHERE NOT EXISTS (SELECT 1 FROM suseInternalState WHERE name='channels');
+
+CREATE TABLE IF NOT EXISTS suseRecurringHighstate
+(
+  rec_id            NUMERIC NOT NULL
+                    CONSTRAINT suse_recurring_highstate_id_pk PRIMARY KEY
+                    CONSTRAINT suse_recurring_action_highstate_id_fk
+                      REFERENCES suseRecurringAction(id)
+                      ON DELETE CASCADE,
+  test_mode         CHAR(1) NOT NULL
+                    DEFAULT 'N'
+);
+
+ALTER TABLE suseRecurringAction ADD COLUMN IF NOT EXISTS test_mode CHAR(1) DEFAULT 'N' NOT NULL;
+INSERT INTO suseRecurringHighstate (rec_id, test_mode)
+         SELECT id, test_mode FROM suseRecurringAction
+         WHERE NOT EXISTS (SELECT id FROM suseRecurringHighstate);
+ALTER TABLE suseRecurringAction DROP COLUMN IF EXISTS test_mode;
+
+CREATE TABLE IF NOT EXISTS suseRecurringState
+(
+  rec_id            NUMERIC NOT NULL
+                    CONSTRAINT suse_recurring_state_id_pk PRIMARY KEY
+                    CONSTRAINT suse_recurring_action_state_id_fk
+                      REFERENCES suseRecurringAction(id)
+                      ON DELETE CASCADE,
+  test_mode         CHAR(1) NOT NULL
+                    DEFAULT 'N'
+);
+
+CREATE TABLE IF NOT EXISTS suseRecurringStateConfig
+(
+  id                NUMERIC NOT NULL
+                    CONSTRAINT suse_recurring_state_config_id_pk PRIMARY KEY,
+  rec_id            NUMERIC NOT NULL
+                    CONSTRAINT suse_recurring_state_config_state_id_fk
+                      REFERENCES suseRecurringState(rec_id)
+                      ON DELETE CASCADE,
+  state_id          NUMERIC
+                    CONSTRAINT suse_recurring_state_intstate_id_fk
+                      REFERENCES suseInternalState(id)
+                      ON DELETE CASCADE,
+  confchan_id       NUMERIC
+                    CONSTRAINT suse_recurring_state_confchan_id_fk
+                      REFERENCES rhnConfigChannel(id)
+                      ON DELETE CASCADE,
+  position          NUMERIC NOT NULL
+);
+
+ALTER TABLE suseRecurringStateConfig 
+  DROP CONSTRAINT IF EXISTS suse_rec_state_conf_rec_id_pos_uq;
+
+ALTER TABLE suseRecurringStateConfig
+  ADD CONSTRAINT suse_rec_state_conf_rec_id_pos_uq UNIQUE (rec_id, position);
+
+CREATE SEQUENCE IF NOT EXISTS suse_recurring_state_config_id_seq;
+
+CREATE UNIQUE INDEX IF NOT EXISTS suse_rec_state_conf_rec_id_state_id_uq
+  ON suseRecurringStateConfig(rec_id, state_id)
+  WHERE confchan_id IS NULL AND state_id IS NOT NULL;
+
+CREATE UNIQUE INDEX IF NOT EXISTS suse_rec_state_conf_rec_id_confchan_id_uq
+  ON suseRecurringStateConfig(rec_id, confchan_id)
+  WHERE state_id IS NULL AND confchan_id IS NOT NULL;
+
+UPDATE rhnTaskoBunch SET description='Schedules actions for minion/group/org' 
+  WHERE name='recurring-state-apply-bunch';
+UPDATE rhnTaskoBunch SET name='recurring-action-executor-bunch'
+  WHERE name='recurring-state-apply-bunch';
+
+UPDATE rhnTaskoTask SET name='recurring-action-executor'
+  WHERE name='recurring-state-apply';
+UPDATE rhnTaskoTask SET class='com.redhat.rhn.taskomatic.task.RecurringActionJob'
+  WHERE class='com.redhat.rhn.taskomatic.task.RecurringStateApplyJob';
+
+COMMIT;
+
