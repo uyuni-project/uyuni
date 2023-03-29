493c9342583b35cce0ffc422af9344bf1b415ee0
Michael Calmer <mc@suse.de>
2023-03-29 16:50:33 +0200

    billing data service in python using Flask

---
+++ python/billingdataservice/billingdataservice.py
@@ -0,0 +1,40 @@
+#
+# Copyright (c) 2023 SUSE LLC
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+import json
+from flask import Flask
+from spacewalk.server import rhnSQL
+from spacewalk.common.rhnConfig import initCFG
+
+# disable the development warning banner for newer Flask versions
+# where maybe the environment variable WERKZEUG_RUN_MAIN does not
+# work anymore
+#
+#from flask import cli
+#cli.show_server_banner = lambda *_: None
+
+initCFG('server.susemanager')
+rhnSQL.initDB()
+
+app = Flask(__name__)
+
+@app.route("/metering")
+def metering():
+    st = rhnSQL.Statement("""
+    SELECT r.dimension, r.count
+      FROM susePaygDimensionResult r
+     WHERE r.computation_id = (SELECT c.id
+                                 FROM susePaygDimensionComputation c
+                                WHERE c.success = true
+                             ORDER BY c.timestamp DESC
+                                LIMIT 1)
+    """)
+    result = rhnSQL.fetchall_dict(st) or []
+    return json.dumps({ "dimensions" : result})
