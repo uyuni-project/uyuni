edbfbfc38dceea2d49eeab3ef403e45618adb7c1
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2023-03-30 11:25:13 +0200

    CI: Acceptance tests at PR v2: add scc_credentails to tests
    that need SCC to be configured.
    
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- testsuite/podman_runner/18_run_secondary_parallelizable_tests.sh
+++ testsuite/podman_runner/18_run_secondary_parallelizable_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -xe
+podman exec controller-test bash -c "zypper ref && zypper -n install expect"
 podman exec controller-test bash -c "export CUCUMBER_PUBLISH_TOKEN=${CUCUMBER_PUBLISH_TOKEN} && export PROVIDER=podman && export SERVER=uyuni-server-all-in-one-test && export HOSTNAME=controller-test && export SSH_MINION=opensusessh && export MINION=sle_minion && export RHLIKE_MINION=rhlike_minion && export DEBLIKE_MINION=deblike_minion && cd /testsuite && rake cucumber:secondary_parallelizable"
