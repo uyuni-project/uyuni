f2bd28054d77f8d5e4703fe49ef130514e151cbe
Cédric Bosdonnat <cbosdonnat@suse.com>
2023-04-13 16:32:48 +0200

    Don't log generated SSL certificate and key.

--- java/code/src/com/redhat/rhn/common/util/FileUtils.java
+++ java/code/src/com/redhat/rhn/common/util/FileUtils.java
@@ -114,6 +114,20 @@ public class FileUtils {
      * @return String containing file.
      */
     public static String readStringFromFile(String path) {
+        return readStringFromFile(path, false);
+    }
+
+
+    /**
+     * Read a file off disk into a String and return it.
+     *
+     * Expect weird stuff if the file is not textual.
+     *
+     * @param path of file to read in
+     * @param noLog don't log the content of the file
+     * @return String containing file.
+     */
+    public static String readStringFromFile(String path, boolean noLog) {
         if (log.isDebugEnabled()) {
             log.debug("readStringFromFile: {}", StringUtil.sanitizeLogInput(path));
         }
@@ -125,7 +139,7 @@ public class FileUtils {
             StringWriter writer = new StringWriter();
             IOUtils.getInstance().copyWriter(input, writer);
             String contents = writer.toString();
-            if (log.isDebugEnabled()) {
+            if (noLog && log.isDebugEnabled()) {
                 log.debug("contents: {}", contents);
             }
             return contents;
