784d2407f373d553b08e7fcf58c18cad9670df35
Michael Calmer <Michael.Calmer@suse.de>
2023-04-17 16:08:26 +0200

    fix typo
    Co-authored-by: Julio González Gil <juliogonzalez@users.noreply.github.com>

--- schema/spacewalk/upgrade/susemanager-schema-4.4.6-to-susemanager-schema-4.4.7/000-change-leap-gpgkeyurl.sql
+++ schema/spacewalk/upgrade/susemanager-schema-4.4.6-to-susemanager-schema-4.4.7/000-change-leap-gpgkeyurl.sql
@@ -2,4 +2,4 @@ UPDATE rhnChannel
    SET gpg_key_url = 'file:///usr/lib/rpm/gnupg/keys/gpg-pubkey-3dbdc284-53674dd4.asc'
  WHERE gpg_key_id = '3DBDC284'
    AND label NOT LIKE '%micro%';
--- opensuse micro may not habe openSUSE-build-key package installed
+-- opensuse micro may not have openSUSE-build-key package installed
