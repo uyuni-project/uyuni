a969d044393192b20c7395fca06a752c5fe5f404
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2023-04-17 18:48:00 +0200

    CI: Acceptance tests at PR v2: add unique product_uuid
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- testsuite/podman_runner/14_run_salt_deblike_minion.sh
+++ testsuite/podman_runner/14_run_salt_deblike_minion.sh
@@ -2,6 +2,8 @@
 set -xe
 src_dir=$(cd $(dirname "$0")/../.. && pwd -P)
 
-podman run --rm -d --network uyuni-network-1 -v /tmp/test-all-in-one:/tmp -v ${src_dir}/testsuite/podman_runner/salt-minion-entry-point.sh:/salt-minion-entry-point.sh --name deblike_minion -h deblike_minion ghcr.io/$UYUNI_PROJECT/uyuni/ci-test-ubuntu-minion:$UYUNI_VERSION bash -c "/salt-minion-entry-point.sh uyuni-server-all-in-one-test 1-DEBLIKE-KEY"
+echo ubuntuminionproductuuid > /tmp/ubuntu_product_uuid
+
+podman run --rm -d --network uyuni-network-1 -v /tmp/ubuntu_product_uuid:/sys/class/dmi/id/product_uuid -v /tmp/test-all-in-one:/tmp -v ${src_dir}/testsuite/podman_runner/salt-minion-entry-point.sh:/salt-minion-entry-point.sh --name deblike_minion -h deblike_minion ghcr.io/$UYUNI_PROJECT/uyuni/ci-test-ubuntu-minion:$UYUNI_VERSION bash -c "/salt-minion-entry-point.sh uyuni-server-all-in-one-test 1-DEBLIKE-KEY"
 podman exec deblike_minion bash -c "ssh-keygen -A && /usr/sbin/sshd -e"
 podman exec deblike_minion bash -c "if [ ! -d /root/.ssh ];then mkdir /root/.ssh/;chmod 700 /root/.ssh;fi;cp /tmp/authorized_keys /root/.ssh/"
