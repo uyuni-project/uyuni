2d4994bba9b741b442724ce8aa71a1e73e01f758
Ondrej Holecek <oholecek@suse.com>
2023-04-18 16:33:49 +0200

    Remove server keys to allow reregistering to different master

--- spacewalk/certs-tools/rhn_bootstrap_strings.py
+++ spacewalk/certs-tools/rhn_bootstrap_strings.py
@@ -896,12 +896,14 @@ if [ -n "$SNAPSHOT_ID" ]; then
 fi
 
 MINION_ID_FILE="${{SNAPSHOT_PREFIX}}/etc/salt/minion_id"
+MINION_PKI_CONF=${{SNAPSHOT_PREFIX}}/etc/salt/pki"
 MINION_CONFIG_DIR="${{SNAPSHOT_PREFIX}}/etc/salt/minion.d"
 SUSEMANAGER_MASTER_FILE="${{MINION_CONFIG_DIR}}/susemanager.conf"
 MINION_SERVICE="salt-minion"
 
 if [ $VENV_ENABLED -eq 1 ]; then
     MINION_ID_FILE="${{SNAPSHOT_PREFIX}}/etc/venv-salt-minion/minion_id"
+    MINION_PKI_CONF="${{SNAPSHOT_PREFIX}}/etc/venv-salt-minion/pki"
     MINION_CONFIG_DIR="${{SNAPSHOT_PREFIX}}/etc/venv-salt-minion/minion.d"
     SUSEMANAGER_MASTER_FILE="${{MINION_CONFIG_DIR}}/susemanager.conf"
     MINION_SERVICE="venv-salt-minion"
@@ -950,6 +952,11 @@ system-environment:
       _:
         SALT_RUNNING: 1
 EOF
+
+# Remove old minion keys so reregistration do different master works
+if [ -d "$MINION_PKI_CONF" ]; then
+    rm -r "$MINION_PKI_CONF"
+fi
 
 if [ -n "$SNAPSHOT_ID" ]; then
     cat <<EOF >> "${{MINION_CONFIG_DIR}}/transactional_update.conf"
