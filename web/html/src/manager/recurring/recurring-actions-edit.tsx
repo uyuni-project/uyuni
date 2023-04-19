30fb94cb7438815ff11bfbf92289bf82ea4fd667
Can Bulut Bayburt <cbbayburt@suse.com>
2023-04-19 13:31:57 +0200

    Minor frontend code improvements for recurring states

--- web/html/src/manager/recurring/recurring-actions-edit.tsx
+++ web/html/src/manager/recurring/recurring-actions-edit.tsx
@@ -75,7 +75,7 @@ class RecurringActionsEdit extends React.Component<Props, State> {
   updateSchedule = (schedule) => {
     return Network.post("/rhn/manager/api/recurringactions/save", schedule)
       .then((_) => {
-        const successMsg = <span>{t("Schedule successfully" + (this.isEdit() ? " updated." : " created."))}</span>;
+        const successMsg = <span>{this.isEdit() ? t("Schedule successfully updated.") : t("Schedule successfully created.")}</span>;
         this.props.onSetMessages(MessagesUtils.info(successMsg));
         this.props.onActionChanged("back");
       })
