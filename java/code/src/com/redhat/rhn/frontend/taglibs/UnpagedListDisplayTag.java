b09f694a60a88a5d237d0947fbd60cf6ff05d469
Can Bulut Bayburt <cbbayburt@suse.com>
2023-04-25 22:22:01 +0200

    Remove unused CSV export logic in JSP ListDisplay tag

--- java/code/src/com/redhat/rhn/frontend/taglibs/UnpagedListDisplayTag.java
+++ java/code/src/com/redhat/rhn/frontend/taglibs/UnpagedListDisplayTag.java
@@ -17,20 +17,14 @@ package com.redhat.rhn.frontend.taglibs;
 
 import com.redhat.rhn.common.localization.LocalizationService;
 import com.redhat.rhn.common.util.DynamicComparator;
-import com.redhat.rhn.common.util.ExportWriter;
-import com.redhat.rhn.common.util.ServletExportHandler;
 import com.redhat.rhn.frontend.dto.BaseListDto;
 import com.redhat.rhn.frontend.dto.UserOverview;
 import com.redhat.rhn.frontend.struts.RequestContext;
 
-import org.apache.commons.lang3.StringUtils;
-
 import java.io.IOException;
 import java.io.Writer;
-import java.util.Arrays;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.JspWriter;
@@ -232,16 +226,6 @@ public class UnpagedListDisplayTag extends ListDisplayTagBase {
         return (s != null && s.startsWith("c"));
     }
 
-    /**
-     * If the User requested an Export or not.
-     * @return boolean if export or not
-     */
-    public boolean isExport() {
-        RequestContext ctx = new RequestContext((HttpServletRequest)
-                pageContext.getRequest());
-        return (ctx.isRequestedExport() && getExportColumns() != null);
-    }
-
     //////////////////////////////////////////////////////////////////////////
     // JSP Tag lifecycle methods
     //////////////////////////////////////////////////////////////////////////
@@ -255,12 +239,6 @@ public class UnpagedListDisplayTag extends ListDisplayTagBase {
         try {
             out = pageContext.getOut();
             setupPageList();
-
-            // Now that we have setup the proper tag state we
-            // need to return if this is an export render.
-            if (isExport()) {
-                return SKIP_PAGE;
-            }
 
             String sortedColumn = getSortedColumn();
             if (sortedColumn != null) {
@@ -314,21 +292,6 @@ public class UnpagedListDisplayTag extends ListDisplayTagBase {
         try {
             if (getPageList().isEmpty()) {
                 return EVAL_PAGE;
-            }
-
-            if (isExport()) {
-                ExportWriter eh = createExportWriter();
-                String[] columns = StringUtils.split(this.getExportColumns(),
-                        ',');
-                eh.setColumns(Arrays.asList(columns));
-                ServletExportHandler seh = new ServletExportHandler(eh);
-                pageContext.getOut().clear();
-                pageContext.getOut().clearBuffer();
-                pageContext.getResponse().reset();
-                seh.writeExporterToOutput(
-                        (HttpServletResponse) pageContext.getResponse(),
-                        getPageList());
-                return SKIP_PAGE;
             }
 
             // Get the JSPWriter that the body used, then pop the
