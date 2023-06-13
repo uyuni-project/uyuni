7f4ece0133ced792d425f467af031cc58dac0682
Vanda Barata <vanda.barata@suse.com>
2023-06-13 09:35:18 +0100

    QE: Add cname to pod proxy configuration for retail (#21741)

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -1714,7 +1714,7 @@ When(/^I generate the configuration "([^"]*)" of Containerized Proxy on the serv
   # Doc: https://www.uyuni-project.org/uyuni-docs/en/uyuni/reference/spacecmd/proxy_container.html
   command = "echo spacewalk > cert_pass && spacecmd -u admin -p admin proxy_container_config_generate_cert" \
             " -- -o #{file_path} -p 8022 #{$proxy.full_hostname.sub('pxy', 'pod-pxy')} #{$server.full_hostname}" \
-            " 2048 galaxy-noise@suse.de --ca-pass cert_pass" \
+            " 2048 galaxy-noise@suse.de --ca-pass cert_pass --ssl-cname proxy.example.org" \
             " && rm cert_pass"
   $server.run(command)
 end
