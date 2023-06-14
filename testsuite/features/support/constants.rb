3c6d2bfca463d19062a3c9e24b60b390bbd58cd0
Vanda Barata <vanda.barata@suse.com>
2023-06-14 11:15:37 +0100

    QE: Add SLES15SP5 Base channel to testsuite (#21759)

--- testsuite/features/support/constants.rb
+++ testsuite/features/support/constants.rb
@@ -183,6 +183,7 @@ LABEL_BY_BASE_CHANNEL = { 'SLE-Product-SUSE-Manager-Proxy-4.2-Pool' => 'sle-prod
                           'SLES15-SP2-Pool' => 'sle-product-sles15-sp2-pool-x86_64',
                           'SLES15-SP3-Pool' => 'sle-product-sles15-sp3-pool-x86_64',
                           'SLES15-SP4-Pool' => 'sle-product-sles15-sp4-pool-x86_64',
+                          'SLES15-SP5-Pool' => 'sle-product-sles15-sp5-pool-x86_64',
                           'RHEL x86_64 Server 7' => 'rhel-x86_64-server-7',
                           'RHEL8-Pool for x86_64' => 'no-appstream-result-rhel8-pool-x86_64',
                           'ubuntu-18.04-pool' => 'ubuntu-18.04-pool-amd64',
