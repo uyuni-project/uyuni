f879c193e2c0381d2c7cc7657b6bf1e51d886e42
HoussemNasri <houssemnasri2001@gmail.com>
2023-06-21 14:37:32 +0100

    Create OVAL Linux extension types

---
+++ java/code/src/com/suse/oval/ovaltypes/linux/RpminfoTest.java
@@ -0,0 +1,17 @@
+package com.suse.oval.ovaltypes.linux;
+
+import com.suse.oval.ovaltypes.TestType;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "rpminfo_test")
+public class RpminfoTest extends TestType {
+
+}
