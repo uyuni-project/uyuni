123bc8d7f7fe44b4f53a7e1ac0f716e7d47c5209
HoussemNasri <houssemnasri2001@gmail.com>
2023-06-21 14:27:02 +0100

    Create OVAL objects, states and tests managers
    - It's an optimization to enable faster lookups of OVAL resources

---
+++ java/code/src/com/suse/oval/manager/OvalTestManager.java
@@ -0,0 +1,34 @@
+package com.suse.oval.manager;
+
+
+import com.suse.oval.ovaltypes.TestType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OvalTestManager {
+    private final Map<String, TestType> testsMap = new HashMap<>();
+
+    public OvalTestManager(List<TestType> tests) {
+        for (TestType test : tests) {
+            testsMap.put(test.getId(), test);
+        }
+    }
+
+    public TestType get(String testId) {
+        TestType test = testsMap.get(testId);
+        if (test == null) {
+            throw new IllegalArgumentException("The test id is invalid: " + testId);
+        }
+        return test;
+    }
+
+    public boolean exists(String testId) {
+        return testsMap.containsKey(testId);
+    }
+
+    public void add(TestType testType) {
+        testsMap.put(testType.getId(), testType);
+    }
+}
