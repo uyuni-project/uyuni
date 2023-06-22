b151d4b0ba14a4a3c5dba7d15a4f2ef7bde6b28e
HoussemNasri <houssemnasri2001@gmail.com>
2023-06-22 23:36:18 +0100

    Fix typo

--- java/code/src/com/suse/oval/db/OVALPackageObject.java
+++ java/code/src/com/suse/oval/db/OVALPackageObject.java
@@ -8,7 +8,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "suseOVALPackageObject")
 public class OVALPackageObject {
-    @Id
+
     private String id;
     private String packageName;
     private String comment;
