93b1d9057f5555d30479640c8a8ce4e0bbf7467d
Miguel Sama <miguel.sama@suse.com>
2023-06-22 15:17:07 +0200

    Add unit tests for cloning channels in PAYG instances
    Signed-off-by: Miguel Sama <miguel.sama@suse.com>

--- java/code/src/com/redhat/rhn/manager/channel/test/CloneChannelCommandTest.java
+++ java/code/src/com/redhat/rhn/manager/channel/test/CloneChannelCommandTest.java
@@ -15,19 +15,23 @@
 
 package com.redhat.rhn.manager.channel.test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import com.redhat.rhn.GlobalInstanceHolder;
 import com.redhat.rhn.domain.channel.Channel;
 import com.redhat.rhn.domain.channel.ChannelFactory;
 import com.redhat.rhn.domain.channel.Modules;
+import com.redhat.rhn.domain.channel.ProductName;
+import com.redhat.rhn.domain.channel.test.ChannelFactoryTest;
+import com.redhat.rhn.domain.product.SUSEProductChannel;
 import com.redhat.rhn.manager.channel.CloneChannelCommand;
+import com.redhat.rhn.manager.channel.ForbiddenCloneChannelPAYGException;
 import com.redhat.rhn.testing.BaseTestCaseWithUser;
 import com.redhat.rhn.testing.ChannelTestUtils;
 
+import com.redhat.rhn.testing.TestUtils;
+import com.suse.cloud.CloudPaygManager;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CloneChannelCommandTest extends BaseTestCaseWithUser {
 
@@ -111,6 +115,75 @@ public class CloneChannelCommandTest extends BaseTestCaseWithUser {
         assertFalse(c.isModular());
     }
 
+    @Test
+    public void testCloneParentChannelPAYG () throws Exception {
+        GlobalInstanceHolder.PAYG_MANAGER.setPaygInstance(true); // Mock we are in a PAYG SUMA instance
+        Channel parentChannel = createBaseChannel();
+
+        CloneChannelCommand ccc = new CloneChannelCommand(CloneChannelCommand.CloneBehavior.ORIGINAL_STATE, parentChannel);
+        ccc.setUser(user);
+        Channel clonedParentChannel = ccc.create();
+
+        // Assert that the channel actually exists
+        Channel gotChannel = ChannelFactory.lookupByIdAndUser(clonedParentChannel.getId(), user);
+        assertNotNull(gotChannel);
+    }
+
+    @Test
+    public void testCloneChildChannelPAYG () throws Exception {
+        GlobalInstanceHolder.PAYG_MANAGER.setPaygInstance(true); // Mock we are in a PAYG SUMA instance
+
+        Channel parentChannelWithProductChannels = createBaseChannel();
+        Channel parentChannelWithoutProductChannels = createBaseChannel();
+
+        Channel childrenChannel = ChannelTestUtils.createChildChannel(user, parentChannelWithProductChannels);
+        childrenChannel.setChecksumType(ChannelFactory.findChecksumTypeByLabel("sha256"));
+        childrenChannel.setSummary("summary");
+
+        ProductName pn = ChannelFactoryTest.lookupOrCreateProductName("sap-ha");
+        childrenChannel.setProductName(pn);
+        TestUtils.saveAndFlush(pn);
+        ChannelFactory.save(childrenChannel);
+
+
+        // Test 1 - Check cloning channel under a base channel with the same channel works
+        CloneChannelCommand ccc1 = new CloneChannelCommand(CloneChannelCommand.CloneBehavior.ORIGINAL_STATE, childrenChannel);
+        ccc1.setUser(user);
+        ccc1.setParentId(parentChannelWithProductChannels.getId());
+        Channel clonedChildChannel1 = ccc1.create();
+
+        // Assert that the channel actually exists
+        Channel gotChannelTest1 = ChannelFactory.lookupByIdAndUser(clonedChildChannel1.getId(), user);
+        assertNotNull(gotChannelTest1);
+        ChannelFactory.remove(gotChannelTest1);
+
+        // Test 2 - Check cloning channel under a base channel without the same channel fails
+        CloneChannelCommand ccc2 = new CloneChannelCommand(CloneChannelCommand.CloneBehavior.ORIGINAL_STATE, childrenChannel);
+        ccc2.setUser(user);
+        ccc2.setParentId(parentChannelWithoutProductChannels.getId());
+        Channel clonedChildChannel2 = null;
+
+        try {
+            clonedChildChannel2 = ccc2.create();
+        } catch (ForbiddenCloneChannelPAYGException f) {} // Catch this specific exception to avoid aborting the test
+
+        // Assert that the channel is null, which means it wasn't created
+        assertNull(clonedChildChannel2);
+
+
+        // Test 3 - Clone a base channel that has children with product channels, and clone under it a channel
+        // with those product channels (Recursive check).
+        CloneChannelCommand ccc3 = new CloneChannelCommand(CloneChannelCommand.CloneBehavior.ORIGINAL_STATE, parentChannelWithProductChannels);
+        ccc3.setUser(user);
+        Channel clonedBaseChannel = ccc3.create();
+
+        ccc2.setParentId(clonedBaseChannel.getId());
+        Channel clonedChildChannel3 = ccc2.create();
+
+        // Assert that the channel actually exists
+        Channel gotChannelTest3 = ChannelFactory.lookupByIdAndUser(clonedChildChannel3.getId(), user);
+        assertNotNull(gotChannelTest3);
+    }
 
     private Channel createBaseChannel() throws Exception {
         Channel channel = ChannelTestUtils.createBaseChannel(user);
