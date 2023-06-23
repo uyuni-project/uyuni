22779fd54853d2f36f88c817f7e2aeab4d6a4446
Miguel Sama <miguel.sama@suse.com>
2023-06-23 12:34:05 +0200

    Broke down test into several ones
    Signed-off-by: Miguel Sama <miguel.sama@suse.com>

--- java/code/src/com/redhat/rhn/manager/channel/test/CloneChannelCommandTest.java
+++ java/code/src/com/redhat/rhn/manager/channel/test/CloneChannelCommandTest.java
@@ -19,9 +19,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import com.redhat.rhn.GlobalInstanceHolder;
 import com.redhat.rhn.domain.channel.Channel;
 import com.redhat.rhn.domain.channel.ChannelFactory;
 import com.redhat.rhn.domain.channel.Modules;
@@ -32,6 +32,8 @@ import com.redhat.rhn.manager.channel.ForbiddenCloneChannelPAYGException;
 import com.redhat.rhn.testing.BaseTestCaseWithUser;
 import com.redhat.rhn.testing.ChannelTestUtils;
 import com.redhat.rhn.testing.TestUtils;
+
+import com.suse.cloud.CloudPaygManager;
 
 import org.junit.jupiter.api.Test;
 
@@ -118,93 +120,98 @@ public class CloneChannelCommandTest extends BaseTestCaseWithUser {
     }
 
     /**
-     * testCloneParentChannelPAYG tests that it's possible to clone all base channels in a SUMA PAYG instace.
+     * Tests if it's possible to clone base channels in a SUMA PAYG instace.
      *
      * @throws Exception if something goes wrong
      */
     @Test
-    public void testCloneParentChannelPAYG() throws Exception {
-        GlobalInstanceHolder.PAYG_MANAGER.setPaygInstance(true); // Mock we are in a PAYG SUMA instance
+    public void canCloneParentChannelInPAYGInstance() throws Exception {
+        CloudPaygManager fakeCloudPaygManager = getFakeCloudPaygManager();
         Channel parentChannel = createBaseChannel();
 
         CloneChannelCommand ccc = new CloneChannelCommand(CloneChannelCommand.CloneBehavior.ORIGINAL_STATE,
-                                                        parentChannel);
+                                                        parentChannel, fakeCloudPaygManager);
         ccc.setUser(user);
         Channel clonedParentChannel = ccc.create();
 
-        // Assert that the channel actually exists
+        // Asserts that the channel actually exists after cloning
         Channel gotChannel = ChannelFactory.lookupByIdAndUser(clonedParentChannel.getId(), user);
         assertNotNull(gotChannel);
     }
 
     /**
-     * testCloneChildChannelPAYG test different scenarios when cloning channels under base channels.
-     * Test 1 - Tests that cloning a channel under the same base channel works.
-     * Test 2 - Test that cloning a channel under a different base channel that has not the channel product we are
-     *          cloning from, fails.
-     * Test 3 - Tests some recursiveness. It clones a base channel that has associated some product channel, and then
-     *          under that cloned base channel it clones a child channel with the same product channel as the first one.
-     *          This test should work.
+     * Tests that cloning a channel under the same base channel works in a SUMA PAYG instace.
      *
-     * @throws Exception if something goes wrong
+     * @throws Exception
      */
     @Test
-    public void testCloneChildChannelPAYG() throws Exception {
-        GlobalInstanceHolder.PAYG_MANAGER.setPaygInstance(true); // Mock we are in a PAYG SUMA instance
+    public void canCloneChildChannelUnderSameParentChannel() throws Exception {
+        CloudPaygManager fakeCloudPaygManager = getFakeCloudPaygManager();
+
+        Channel parentChannelWithProductChannels = createBaseChannel();
+        Channel childrenChannel = createChildrenWIthProductChannel(parentChannelWithProductChannels);
+
+        CloneChannelCommand ccc = new CloneChannelCommand(CloneChannelCommand.CloneBehavior.ORIGINAL_STATE,
+                childrenChannel, fakeCloudPaygManager);
+        ccc.setUser(user);
+        ccc.setParentId(parentChannelWithProductChannels.getId());
+        Channel clonedChildChannel1 = ccc.create();
+
+        // Asserts that the channel actually exists after cloning
+        Channel gotChannelTest1 = ChannelFactory.lookupByIdAndUser(clonedChildChannel1.getId(), user);
+        assertNotNull(gotChannelTest1);
+    }
+
+    @Test
+    /**
+     * Tests that cloning a channel under a different base channel that has not the channel product we are
+     * cloning from fails in a SUMA PAYG instace.
+     *
+     * @throws Exception
+     */
+    public void canCloneChildChannelUnderDifferentParentChannelWithoutProductChannel() throws Exception {
+        CloudPaygManager fakeCloudPaygManager = getFakeCloudPaygManager();
 
         Channel parentChannelWithProductChannels = createBaseChannel();
         Channel parentChannelWithoutProductChannels = createBaseChannel();
+        Channel childrenChannel = createChildrenWIthProductChannel(parentChannelWithProductChannels);
 
-        Channel childrenChannel = ChannelTestUtils.createChildChannel(user, parentChannelWithProductChannels);
-        childrenChannel.setChecksumType(ChannelFactory.findChecksumTypeByLabel("sha256"));
-        childrenChannel.setSummary("summary");
+        CloneChannelCommand ccc = new CloneChannelCommand(CloneChannelCommand.CloneBehavior.ORIGINAL_STATE,
+                childrenChannel, fakeCloudPaygManager);
+        ccc.setUser(user);
+        ccc.setParentId(parentChannelWithoutProductChannels.getId());
 
-        ProductName pn = ChannelFactoryTest.lookupOrCreateProductName("sap-ha");
-        childrenChannel.setProductName(pn);
-        TestUtils.saveAndFlush(pn);
-        ChannelFactory.save(childrenChannel);
+        // Assert that cloning operation throws ForbiddenCloneChannelPAYGException exception
+        assertThrows(ForbiddenCloneChannelPAYGException.class, () -> ccc.create());
+    }
 
-        // Test 1
-        CloneChannelCommand ccc1 = new CloneChannelCommand(CloneChannelCommand.CloneBehavior.ORIGINAL_STATE,
-                                                            childrenChannel);
-        ccc1.setUser(user);
-        ccc1.setParentId(parentChannelWithProductChannels.getId());
-        Channel clonedChildChannel1 = ccc1.create();
+    /**
+     * Tests some recursiveness. It clones a base channel that has associated some product channel, and then
+     * under that cloned base channel it clones a child channel with the same product channel as the first one.
+     * This test is for SUMA PAYG instaces.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void canClonedBasedChannelGetChildChannelWithProductChannel() throws Exception {
+        CloudPaygManager fakeCloudPaygManager = getFakeCloudPaygManager();
 
-        // Assert that the channel actually exists
-        Channel gotChannelTest1 = ChannelFactory.lookupByIdAndUser(clonedChildChannel1.getId(), user);
-        assertNotNull(gotChannelTest1);
-        ChannelFactory.remove(gotChannelTest1);
+        Channel parentChannelWithProductChannels = createBaseChannel();
+        Channel childrenChannel = createChildrenWIthProductChannel(parentChannelWithProductChannels);
 
-        // Test 2
-        CloneChannelCommand ccc2 = new CloneChannelCommand(CloneChannelCommand.CloneBehavior.ORIGINAL_STATE,
-                                                            childrenChannel);
-        ccc2.setUser(user);
-        ccc2.setParentId(parentChannelWithoutProductChannels.getId());
-        Channel clonedChildChannel2 = null;
+        CloneChannelCommand cccBase = new CloneChannelCommand(CloneChannelCommand.CloneBehavior.ORIGINAL_STATE,
+                parentChannelWithProductChannels, fakeCloudPaygManager);
+        cccBase.setUser(user);
+        Channel clonedBaseChannel = cccBase.create();
 
-        try {
-            clonedChildChannel2 = ccc2.create();
-        }
-        catch (ForbiddenCloneChannelPAYGException f) {
-            // Catch this specific exception to avoid aborting the test
-        }
-
-        // Assert that the channel is null, which means it wasn't created
-        assertNull(clonedChildChannel2);
-
-
-        // Test 3
-        CloneChannelCommand ccc3 = new CloneChannelCommand(CloneChannelCommand.CloneBehavior.ORIGINAL_STATE,
-                                                            parentChannelWithProductChannels);
-        ccc3.setUser(user);
-        Channel clonedBaseChannel = ccc3.create();
-
-        ccc2.setParentId(clonedBaseChannel.getId());
-        Channel clonedChildChannel3 = ccc2.create();
+        CloneChannelCommand cccChildren = new CloneChannelCommand(CloneChannelCommand.CloneBehavior.ORIGINAL_STATE,
+                childrenChannel, fakeCloudPaygManager);
+        cccChildren.setUser(user);
+        cccChildren.setParentId(clonedBaseChannel.getId());
+        Channel clonedChildChannel = cccChildren.create();
 
         // Assert that the channel actually exists
-        Channel gotChannelTest3 = ChannelFactory.lookupByIdAndUser(clonedChildChannel3.getId(), user);
+        Channel gotChannelTest3 = ChannelFactory.lookupByIdAndUser(clonedChildChannel.getId(), user);
         assertNotNull(gotChannelTest3);
     }
 
@@ -213,5 +220,24 @@ public class CloneChannelCommandTest extends BaseTestCaseWithUser {
         channel.setChecksumType(ChannelFactory.findChecksumTypeByLabel("sha256"));
         channel.setSummary("summary");
         return channel;
+    }
+
+    private CloudPaygManager getFakeCloudPaygManager() {
+        CloudPaygManager cloudPaygManager = new CloudPaygManager();
+        cloudPaygManager.setPaygInstance(true);
+        return cloudPaygManager;
+    }
+
+    private Channel createChildrenWIthProductChannel(Channel parentChannel) throws Exception {
+        Channel childrenChannel = ChannelTestUtils.createChildChannel(user, parentChannel);
+        childrenChannel.setChecksumType(ChannelFactory.findChecksumTypeByLabel("sha256"));
+        childrenChannel.setSummary("summary");
+
+        ProductName pn = ChannelFactoryTest.lookupOrCreateProductName("sap-ha");
+        childrenChannel.setProductName(pn);
+        TestUtils.saveAndFlush(pn);
+        ChannelFactory.save(childrenChannel);
+
+        return childrenChannel;
     }
 }
