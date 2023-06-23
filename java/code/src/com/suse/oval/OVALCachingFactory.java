5985980159d739d384768084dbb2f59e8e253836
HoussemNasri <houssemnasri2001@gmail.com>
2023-06-23 01:33:24 +0100

    Define OVAL database access methods
    - Without implementation

---
+++ java/code/src/com/suse/oval/OVALCachingFactory.java
@@ -0,0 +1,64 @@
+package com.suse.oval;
+
+import com.redhat.rhn.common.hibernate.HibernateFactory;
+import com.suse.oval.db.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OVALCachingFactory extends HibernateFactory {
+    private static final Logger LOG = LogManager.getLogger(OVALCachingFactory.class);
+    private static OVALCachingFactory instance = new OVALCachingFactory();
+
+    private OVALCachingFactory() {
+        // Left empty on purpose
+    }
+
+    /**
+     * Insert the passed definition object into the database.
+     * <p>
+     * Also insert the affected platforms information (if not already inserted) into the relevant tables
+     */
+    public static void saveDefinition(OVALDefinition definition) {
+        instance.saveObject(definition);
+    }
+
+    public static void savePackageTest(OVALPackageTest pkgTest) {
+
+    }
+
+    public static void savePackageState(OVALPackageState pkgState) {
+
+    }
+
+    public static void savePackageObject(OVALPackageObject pkgObject) {
+
+    }
+
+    public static OVALPackageTest getPackageTestById(String id) {
+        return null;
+    }
+
+    public static OVALPackageState getPackageStateById(String id) {
+        return null;
+    }
+
+    public static OVALPackageObject getPackageObjectById(String id) {
+        return null;
+    }
+
+    public static List<OVALPlatform> getPlatformsAffectedByCve(String cve) {
+        return null;
+    }
+
+    public static Optional<OVALDefinition> getVulnerabilityDefinitionByCve(String cve) {
+        return Optional.empty();
+    }
+
+    @Override
+    protected Logger getLogger() {
+        return LOG;
+    }
+}
