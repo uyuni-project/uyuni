bde825c71ef6d2f31cc1cb9f1ceb9ac6bf56fe1b
HoussemNasri <houssemnasri2001@gmail.com>
2023-07-02 15:18:35 +0100

    Optimize the insertion of definitions further

--- java/code/src/com/suse/oval/ovaltypes/BaseCriteria.java
+++ java/code/src/com/suse/oval/ovaltypes/BaseCriteria.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.suse.oval.TestEvaluator;
 
+import java.io.Serializable;
+
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
         property = "type")
@@ -11,6 +13,6 @@ import com.suse.oval.TestEvaluator;
         @JsonSubTypes.Type(value = CriteriaType.class, name = "criteriaType"),
         @JsonSubTypes.Type(value = CriterionType.class, name = "criterionType")}
 )
-public interface BaseCriteria {
+public interface BaseCriteria extends Serializable {
     boolean evaluate(TestEvaluator testEvaluator);
 }
