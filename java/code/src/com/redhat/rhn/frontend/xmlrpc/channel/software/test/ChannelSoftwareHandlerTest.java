d20202d55edfcaa6207f5455498a95e6b01fd556
Miguel Sama <miguel.sama@suse.com>
2023-07-05 15:22:43 +0200

    Use new SystemHandler signature
    Signed-off-by: Miguel Sama <miguel.sama@suse.com>

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/channel/software/test/ChannelSoftwareHandlerTest.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/channel/software/test/ChannelSoftwareHandlerTest.java
@@ -132,7 +132,7 @@ public class ChannelSoftwareHandlerTest extends BaseHandlerTestCase {
     private SystemManager systemManager =
             new SystemManager(ServerFactory.SINGLETON, ServerGroupFactory.SINGLETON, saltApi);
     private SystemHandler systemHandler = new SystemHandler(taskomaticApi, xmlRpcSystemHelper, systemEntitlementManager,
-            systemManager, serverGroupManager);
+            systemManager, serverGroupManager, null);
     private ChannelSoftwareHandler handler = new ChannelSoftwareHandler(taskomaticApi, xmlRpcSystemHelper);
     private ErrataHandler errataHandler = new ErrataHandler();
 
@@ -325,7 +325,7 @@ public class ChannelSoftwareHandlerTest extends BaseHandlerTestCase {
         assertFalse(child2.isBaseChannel());
 
         SystemHandler sh = new SystemHandler(taskomaticApi, xmlRpcSystemHelper, systemEntitlementManager, systemManager,
-                serverGroupManager);
+                serverGroupManager, null);
 
         int sid = server.getId().intValue();
         int rc1 = sh.setBaseChannel(admin, sid, base.getLabel());
@@ -354,7 +354,7 @@ public class ChannelSoftwareHandlerTest extends BaseHandlerTestCase {
     @Test
     public void testSetBaseChannel() throws Exception {
         SystemHandler sh = new SystemHandler(taskomaticApi, xmlRpcSystemHelper, systemEntitlementManager, systemManager,
-                serverGroupManager);
+                serverGroupManager, null);
 
         Channel c1 = ChannelFactoryTest.createTestChannel(admin);
         Server server = ServerFactoryTest.createTestServer(admin, true);
@@ -875,7 +875,7 @@ public class ChannelSoftwareHandlerTest extends BaseHandlerTestCase {
         childChan.setParentChannel(baseChan);
 
         SystemHandler sh = new SystemHandler(taskomaticApi, xmlRpcSystemHelper, systemEntitlementManager, systemManager,
-                serverGroupManager);
+                serverGroupManager, null);
 
         int return1 = sh.setBaseChannel(admin, server.getId().intValue(), baseChan.getLabel());
         int return2 = sh.setChildChannels(admin, server.getId().intValue(), List.of(childChan.getLabel()));
