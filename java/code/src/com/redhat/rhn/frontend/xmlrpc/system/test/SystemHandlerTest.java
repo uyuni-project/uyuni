534ed88ca9093ad82fdd9d33fa92cafc6b222127
Miguel Sama <miguel.sama@suse.com>
2023-07-05 15:22:43 +0200

    Use new SystemHandler signature
    Signed-off-by: Miguel Sama <miguel.sama@suse.com>

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/system/test/SystemHandlerTest.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/system/test/SystemHandlerTest.java
@@ -229,7 +229,7 @@ public class SystemHandlerTest extends BaseHandlerTestCase {
             new SystemManager(ServerFactory.SINGLETON, ServerGroupFactory.SINGLETON, saltApi);
     private SystemHandler handler =
             new SystemHandler(taskomaticApi, xmlRpcSystemHelper, systemEntitlementManager, systemManager,
-                    serverGroupManager);
+                    serverGroupManager, null);
 
     @RegisterExtension
     protected final Mockery mockContext = new JUnit5Mockery() {{
@@ -3598,7 +3598,7 @@ public class SystemHandlerTest extends BaseHandlerTestCase {
     private SystemHandler getMockedHandler() throws Exception {
         TaskomaticApi taskomaticMock = mockContext.mock(TaskomaticApi.class);
         SystemHandler systemHandler = new SystemHandler(taskomaticMock, xmlRpcSystemHelper, systemEntitlementManager,
-                systemManager, serverGroupManager);
+                systemManager, serverGroupManager, null);
 
         mockContext.checking(new Expectations() {{
             allowing(taskomaticMock).scheduleActionExecution(with(any(Action.class)));
