eaef08efb695b22ae8e4eb3426945be8be6f4344
HoussemNasri <houssemnasri2001@gmail.com>
2023-07-14 13:36:08 +0100

    Fix query returning VRE instead of EVR

--- java/code/src/com/suse/oval/SystemPackage.java
+++ java/code/src/com/suse/oval/SystemPackage.java
@@ -1,5 +1,6 @@
 package com.suse.oval;
 
+import com.redhat.rhn.domain.rhnpackage.PackageEvr;
 import com.redhat.rhn.frontend.dto.IdComboDto;
 
 public class SystemPackage extends IdComboDto {
@@ -8,6 +9,10 @@ public class SystemPackage extends IdComboDto {
     private String name;
     private String evr;
     private String arch;
+    private String epoch;
+    private String version;
+    private String release;
+    private String type;
 
     public SystemPackage() {
     }
@@ -63,5 +68,41 @@ public class SystemPackage extends IdComboDto {
 
     public void setArch(String arch) {
         this.arch = arch;
+    }
+
+    public String getEpoch() {
+        return epoch;
+    }
+
+    public void setEpoch(String epoch) {
+        this.epoch = epoch;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getRelease() {
+        return release;
+    }
+
+    public void setRelease(String release) {
+        this.release = release;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public PackageEvr getPackageEVR() {
+        return new PackageEvr(epoch, version, release, type);
     }
 }
