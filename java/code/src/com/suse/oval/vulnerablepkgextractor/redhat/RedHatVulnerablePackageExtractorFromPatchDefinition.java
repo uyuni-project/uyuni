4703d930425bf35ff52dbc4e1e7a87340aad1aee
HoussemNasri <houssemnasri2001@gmail.com>
2023-08-08 18:37:10 +0100

    Implement redhat vulnerable packages extractor for patch definitions

--- java/code/src/com/suse/oval/vulnerablepkgextractor/redhat/RedHatVulnerablePackageExtractorFromPatchDefinition.java
+++ java/code/src/com/suse/oval/vulnerablepkgextractor/redhat/RedHatVulnerablePackageExtractorFromPatchDefinition.java
@@ -1,29 +1,77 @@
 package com.suse.oval.vulnerablepkgextractor.redhat;
 
 import com.suse.oval.OsFamily;
+import com.suse.oval.cpe.Cpe;
+import com.suse.oval.cpe.CpeBuilder;
 import com.suse.oval.ovaltypes.BaseCriteria;
+import com.suse.oval.ovaltypes.CriterionType;
 import com.suse.oval.ovaltypes.DefinitionClassEnum;
 import com.suse.oval.ovaltypes.DefinitionType;
 import com.suse.oval.vulnerablepkgextractor.CriteriaTreeBasedExtractor;
 import com.suse.oval.vulnerablepkgextractor.ProductVulnerablePackages;
+import com.suse.oval.vulnerablepkgextractor.VulnerablePackage;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RedHatVulnerablePackageExtractorFromPatchDefinition extends CriteriaTreeBasedExtractor {
+    private static final Pattern REDHAT_PACKAGE_REGEX = Pattern
+            .compile("(?<packageName>\\S+) is earlier than (?<evr>.*)");
+
     public RedHatVulnerablePackageExtractorFromPatchDefinition(DefinitionType patchDefinition) {
         super(patchDefinition);
-
         assertDefinitionIsValid();
     }
 
     @Override
-    protected List<ProductVulnerablePackages> extractItem(BaseCriteria criteriaType) {
-        return null;
+    protected List<ProductVulnerablePackages> extractItem(BaseCriteria criteria) {
+        CriterionType criterionType = (CriterionType) criteria;
+
+        Matcher matcher = REDHAT_PACKAGE_REGEX.matcher(criterionType.getComment());
+
+        // Although we know the comment matches the regex (see test()), we need to call matches() otherwise Matcher#group
+        // throws an exception
+        matcher.matches();
+
+        if (matcher.groupCount() != 2) {
+            return Collections.emptyList();
+        }
+
+        String packageName = matcher.group("packageName");
+        String evr = matcher.group("evr");
+
+        VulnerablePackage vulnerablePackage = new VulnerablePackage();
+        vulnerablePackage.setName(packageName);
+        vulnerablePackage.setFixVersion(evr);
+
+        ProductVulnerablePackages productVulnerablePackages = new ProductVulnerablePackages();
+        productVulnerablePackages.setProduct(deriveCpe().asString());
+        productVulnerablePackages.setVulnerablePackages(List.of(vulnerablePackage));
+        productVulnerablePackages.setCve(definition.getCve());
+
+        return List.of(productVulnerablePackages);
     }
 
     @Override
     protected boolean test(BaseCriteria criteria) {
-        return false;
+        if (!(criteria instanceof CriterionType)) {
+            return false;
+        }
+
+        CriterionType criterionType = (CriterionType) criteria;
+        return REDHAT_PACKAGE_REGEX.asMatchPredicate().test(criterionType.getComment());
+    }
+
+    public Cpe deriveCpe() {
+        String osVersion = definition.getOsVersion();
+
+        return new CpeBuilder()
+                .withVendor("redhat")
+                .withProduct("enterprise_linux")
+                .withVersion(osVersion)
+                .build();
     }
 
     private void assertDefinitionIsValid() {
