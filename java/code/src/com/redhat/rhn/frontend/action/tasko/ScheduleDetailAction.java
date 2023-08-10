27c2d84cedff2d6cdd0df7a2d01c8ddc43c1b829
Michael Calmer <mc@suse.de>
2023-08-10 11:23:11 +0200

    hide payg-dimension-computation-bunch from Task Schedule list

--- java/code/src/com/redhat/rhn/frontend/action/tasko/ScheduleDetailAction.java
+++ java/code/src/com/redhat/rhn/frontend/action/tasko/ScheduleDetailAction.java
@@ -19,6 +19,7 @@ import com.redhat.rhn.domain.user.User;
 import com.redhat.rhn.frontend.struts.RequestContext;
 import com.redhat.rhn.frontend.struts.RhnAction;
 import com.redhat.rhn.frontend.struts.RhnHelper;
+import com.redhat.rhn.taskomatic.TaskoFactory;
 import com.redhat.rhn.taskomatic.TaskomaticApi;
 import com.redhat.rhn.taskomatic.TaskomaticApiException;
 
@@ -211,9 +212,7 @@ public class ScheduleDetailAction extends RhnAction {
         List<Map<String, String>> dropDown = new ArrayList<>();
         try {
             List<Map<String, Object>> bunches = new TaskomaticApi().listSatBunchSchedules(loggedInUser);
-            // Since recurring states have their own place in the webUI we don't
-            // want them to show up in the Task Schedules UI
-            bunches.removeIf(bunch -> bunch.get("name").equals("recurring-action-executor-bunch"));
+            bunches.removeIf(bunch -> TaskoFactory.HIDDEN_BUNCHES.contains(bunch.get("name")));
 
             for (Map<String, Object> b : bunches) {
                 addOption(dropDown, (String)b.get("name"), (String)b.get("name"));
