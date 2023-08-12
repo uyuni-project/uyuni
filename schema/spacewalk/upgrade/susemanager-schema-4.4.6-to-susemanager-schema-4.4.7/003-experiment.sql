5e0dccfcbfb370eba934d0ecb740da2c9a8dd15e
HoussemNasri <houssemnasri2001@gmail.com>
2023-08-12 19:58:58 +0100

    Fix bug where vulnerable packages with a null fix version not inserted

--- schema/spacewalk/upgrade/susemanager-schema-4.4.6-to-susemanager-schema-4.4.7/003-experiment.sql
+++ schema/spacewalk/upgrade/susemanager-schema-4.4.6-to-susemanager-schema-4.4.7/003-experiment.sql
@@ -62,6 +62,9 @@ begin
 
     INSERT INTO suseOVALPlatformVulnerablePackage(platform_id, cve_id, vulnerable_pkg_id)
     VALUES (product_cpe_id_val, cve_id_val, vulnerable_pkg_id_val)
-    ON CONFLICT(platform_id, cve_id, vulnerable_pkg_id) DO NOTHING;
+    ON CONFLICT(platform_id, cve_id, vulnerable_pkg_id) DO UPDATE
+        SET platform_id       = EXCLUDED.platform_id,
+            cve_id            = EXCLUDED.cve_id,
+            vulnerable_pkg_id = EXCLUDED.vulnerable_pkg_id;
 end;
 $$;
