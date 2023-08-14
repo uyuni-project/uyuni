e009de07e9cc0729e6ae6f4ddf07c159d2e99c0c
HoussemNasri <houssemnasri2001@gmail.com>
2023-08-14 21:13:45 +0100

    Remove unused SQL

--- schema/spacewalk/upgrade/susemanager-schema-4.4.6-to-susemanager-schema-4.4.7/001-oval-add-tables.sql
+++ schema/spacewalk/upgrade/susemanager-schema-4.4.6-to-susemanager-schema-4.4.7/001-oval-add-tables.sql
@@ -1,99 +1,3 @@
-CREATE TABLE IF NOT EXISTS suseOVALPackageEvrState
-(
-    id           NUMERIC NOT NULL
-        CONSTRAINT suse_oval_pkg_evr_state_id_pk PRIMARY KEY,
-    evr          VARCHAR,
-    datatype     VARCHAR,
-    operation    VARCHAR
-);
-
-CREATE UNIQUE INDEX IF NOT EXISTS suse_oval_pkg_evr_state_uq
-    ON suseOVALPackageEvrState (evr, datatype, operation)
-;
-
-CREATE SEQUENCE IF NOT EXISTS suse_oval_pkg_evr_state_id_seq START WITH 101;
-
-CREATE TABLE IF NOT EXISTS suseOVALPackageArchState
-(
-    id           NUMERIC NOT NULL
-        CONSTRAINT suse_oval_pkg_arch_state_id_pk PRIMARY KEY,
-    value        VARCHAR,
-    operation    VARCHAR
-);
-
-CREATE UNIQUE INDEX IF NOT EXISTS suse_oval_pkg_arch_state_uq
-    ON suseOVALPackageArchState (value, operation)
-;
-
-CREATE SEQUENCE IF NOT EXISTS suse_oval_pkg_arch_state_id_seq START WITH 1;
-
-
-CREATE TABLE IF NOT EXISTS suseOVALPackageVersionState
-(
-    id           NUMERIC NOT NULL
-        CONSTRAINT suse_oval_pkg_version_state_id_pk PRIMARY KEY,
-    value        VARCHAR,
-    operation    VARCHAR
-);
-
-
-CREATE UNIQUE INDEX IF NOT EXISTS suse_oval_pkg_version_state_uq
-    ON suseOVALPackageVersionState (value, operation)
-;
-
-CREATE SEQUENCE IF NOT EXISTS suse_oval_pkg_version_state_id_seq START WITH 201;
-
-CREATE TABLE IF NOT EXISTS suseOVALDefinition
-(
-    id              VARCHAR NOT NULL
-        CONSTRAINT suse_oval_definition_id_pk PRIMARY KEY,
-    class           VARCHAR NOT NULL,
-    title           VARCHAR,
-    description     VARCHAR(10000),
-    cve_id          NUMERIC
-        REFERENCES rhnCve(id),
-    os_family       VARCHAR,
-    os_version      VARCHAR,
-    criteria_tree   JSON
-);
-
-CREATE TABLE IF NOT EXISTS suseOVALPackageState
-(
-    id                  VARCHAR NOT NULL
-        CONSTRAINT suse_oval_pkg_state_id_pk PRIMARY KEY,
-    operator            VARCHAR,
-    arch_state_id       NUMERIC
-        REFERENCES suseOVALPackageArchState(id),
-    version_state_id    NUMERIC
-        REFERENCES suseOVALPackageVersionState(id),
-    evr_state_id        NUMERIC
-        REFERENCES suseOVALPackageEvrState(id),
-    isRpm               BOOLEAN
-);
-
-CREATE TABLE IF NOT EXISTS suseOVALPackageObject
-(
-    id         VARCHAR NOT NULL
-        CONSTRAINT suse_oval_pkg_object_id_pk PRIMARY KEY,
-    name       VARCHAR NOT NULL,
-    isRpm      BOOLEAN NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS suseOVALPackageTest
-(
-    id                VARCHAR NOT NULL
-        CONSTRAINT suse_oval_pkg_test_id_pk PRIMARY KEY,
-    comment           VARCHAR,
-    check_exist       VARCHAR,
-    test_check        VARCHAR,
-    state_operator    VARCHAR,
-    isRpm             BOOLEAN,
-    pkg_object_id     VARCHAR NOT NULL
-        REFERENCES suseOVALPackageObject(id),
-    pkg_state_id      VARCHAR
-        REFERENCES suseOVALPackageState(id)
-);
-
 CREATE TABLE IF NOT EXISTS suseOVALPlatform
 (
     id        NUMERIC NOT NULL
@@ -101,30 +5,10 @@ CREATE TABLE IF NOT EXISTS suseOVALPlatform
     cpe      VARCHAR
 );
 
-
 CREATE SEQUENCE IF NOT EXISTS suse_oval_platform_id_seq START WITH 101;
 
-CREATE TABLE IF NOT EXISTS suseOVALReference
-(
-    ref_id    VARCHAR NOT NULL,
-    definition_id    VARCHAR NOT NULL
-        REFERENCES suseOVALDefinition(id),
-    source    VARCHAR,
-    url    VARCHAR,
-    CONSTRAINT suse_oval_reference_id_pk PRIMARY KEY (ref_id, definition_id)
-);
-
-CREATE TABLE IF NOT EXISTS suseOVALDefinitionAffectedPlatform
-(
-    definition_id    VARCHAR NOT NULL
-        REFERENCES suseOVALDefinition(id),
-    platform_id      NUMERIC
-        REFERENCES suseOVALPlatform(id)
-);
-
-/*CREATE UNIQUE INDEX IF NOT EXISTS suse_oval_def_affected_plat_uq
-    ON suseOVALDefinitionAffectedPlatform(definition_id, platform_id)
-;*/
+CREATE UNIQUE INDEX IF NOT EXISTS suse_oval_aff_platform_cpe_uq
+    ON suseovalplatform(cpe);
 
 CREATE TABLE IF NOT EXISTS suseOVALVulnerablePackage
 (
