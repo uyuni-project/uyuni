8ca6e1e14d35eea52e6c4512b590448fc3632423
Michael Calmer <mc@suse.de>
2023-08-17 13:01:50 +0200

    rename "PAYG Connection" to "PAYG Connections"

--- web/html/src/manager/admin/setup/products/products.tsx
+++ web/html/src/manager/admin/setup/products/products.tsx
@@ -71,7 +71,7 @@ const _SETUP_WIZARD_STEPS = [
   },
   {
     id: "wizard-step-suse-payg",
-    label: "PAYG Connection",
+    label: "PAYG Connections",
     url: "/rhn/manager/admin/setup/payg",
     active: false,
   },
