86df57655365b62135d531ee77c041462cebbe07
HoussemNasri <houssemnasri2001@gmail.com>
2023-08-22 19:37:37 +0100

    Indicate in UI when vulnerability scan was based on OVAL or Channels data

--- java/code/src/com/redhat/rhn/manager/audit/CVEAuditSystemBuilder.java
+++ java/code/src/com/redhat/rhn/manager/audit/CVEAuditSystemBuilder.java
@@ -30,6 +30,9 @@ public class CVEAuditSystemBuilder {
     private long systemID;
     private String systemName;
     private PatchStatus patchStatus;
+    // If system was audited wth CVEAuditManager#doAuditSystem instead of CVEAuditManagerOVAL#doAuditSystem then
+    // it's possible to get false negatives.
+    private boolean scannedWithOVAL;
 
     // LinkedHashSet is used to preserve insertion order when iterating
     private Set<AuditChannelInfo> channels =
@@ -180,5 +183,20 @@ public class CVEAuditSystemBuilder {
     /** {@inheritDoc} */
     public Long getId() {
         return systemID;
+    }
+
+    /**
+     * Returns {@code True} if server was scanned with OVAL and {@code False} otherwise
+     * */
+    public boolean isScannedWithOVAL() {
+        return scannedWithOVAL;
+    }
+
+    /**
+     * Sets scannedWithOVAL
+     * @param scannedWithOVALIn the value to set
+     * */
+    public void setScannedWithOVAL(boolean scannedWithOVALIn) {
+        this.scannedWithOVAL = scannedWithOVALIn;
     }
 }
