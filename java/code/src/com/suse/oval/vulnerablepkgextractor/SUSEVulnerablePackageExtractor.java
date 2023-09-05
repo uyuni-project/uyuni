a180ead16e83c2ffc852f3fbe721a39a644a6c0b
HoussemNasri <houssemnasri2001@gmail.com>
2023-09-05 19:42:46 +0100

    Add support for openSUSE Micro OVAL-based CVE auditing

--- java/code/src/com/suse/oval/vulnerablepkgextractor/SUSEVulnerablePackageExtractor.java
+++ java/code/src/com/suse/oval/vulnerablepkgextractor/SUSEVulnerablePackageExtractor.java
@@ -173,12 +173,23 @@ public class SUSEVulnerablePackageExtractor extends CriteriaTreeBasedExtractor {
         if (osProduct == OsFamily.LEAP) {
             return deriveOpenSUSELeapCpe();
         }
+        else if (osProduct == OsFamily.openSUSE_LEAP_MICRO) {
+            return deriveOpenSUSELeapMicroCpe();
+        }
         else if (osProduct == OsFamily.SUSE_LINUX_ENTERPRISE_MICRO) {
             return deriveSUSEMicroCpe();
         }
         else {
             return deriveFromProductOVALTest(productTest);
         }
+    }
+
+    private Cpe deriveOpenSUSELeapMicroCpe() {
+        return new CpeBuilder()
+                .withVendor("opensuse")
+                .withProduct("leap-micro")
+                .withVersion(definition.getOsVersion())
+                .build();
     }
 
     private Cpe deriveSUSEMicroCpe() {
@@ -239,6 +250,7 @@ public class SUSEVulnerablePackageExtractor extends CriteriaTreeBasedExtractor {
     public boolean isValidDefinition(DefinitionType definitionTypeIn) {
         OsFamily osFamily = definitionTypeIn.getOsFamily();
         boolean definitionFromASupportedFamily = osFamily == OsFamily.LEAP ||
+                osFamily == OsFamily.openSUSE_LEAP_MICRO ||
                 osFamily == OsFamily.SUSE_LINUX_ENTERPRISE_SERVER ||
                 osFamily == OsFamily.SUSE_LINUX_ENTERPRISE_DESKTOP ||
                 osFamily == OsFamily.SUSE_LINUX_ENTERPRISE_MICRO;
