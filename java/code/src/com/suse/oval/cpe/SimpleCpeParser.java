948ec8552f9d0ec1345bea914c2715a6e8822600
HoussemNasri <houssemnasri2001@gmail.com>
2023-09-11 20:04:16 +0100

    Implement a CPE parser
    - Used to parse CPEs found in OVAL files.

---
+++ java/code/src/com/suse/oval/cpe/SimpleCpeParser.java
@@ -0,0 +1,52 @@
+package com.suse.oval.cpe;
+
+/**
+ * A simple and minimal CPE parser to parse CPE URI bindings included in OVAL files.
+ */
+public class SimpleCpeParser {
+
+
+    /**
+     * CPE Examples:
+     * <br><br>
+     * CPE: <b>cpe:/o:opensuse:leap:15.4</b>
+     * <br>
+     * PART: <b>o</b> <i>(it's an operating system's CPE)</i><br>
+     * VENDOR: <b>opensuse</b> <i>(CPE describes a product manufactured by openSUSE)<i/><br>
+     * PRODUCT: <b>leap</b> <i>(The product name is leap)</i><br>
+     * VERSION: <b>15.4</b> <i>(The product version is 15.4)</i><br>
+     * <br><br>
+     * CPE: <b>cpe:/o:suse:sle_hpc:15:sp4</b>
+     * <br>
+     * PART: <b>o</b> <i>(it's an operating system's CPE)</i><br>
+     * VENDOR: <b>suse</b> <i>(CPE describes a product manufactured by SUSE)<i/><br>
+     * PRODUCT: <b>sle_hpc</b> <i>(The product name is sle_hpc)</i><br>
+     * VERSION: <b>15</b> <i>(The product version is 15)</i><br>
+     * UPDATE: <b>sp4</b> <i>(The product update is sp4)</i><br>
+     *
+     */
+    public Cpe parse(String cpeURI) {
+        if (!cpeURI.startsWith("cpe:/o:")) {
+            throw new IllegalArgumentException("CPE is expected to be in URI format and for operating systems");
+        }
+
+        cpeURI = cpeURI.replace("cpe:/o:", "");
+
+        String[] parts = cpeURI.split(":");
+
+        if (parts.length < 3) {
+            throw new IllegalArgumentException("CPE is expected to at least have vendor, product and version");
+        }
+
+        Cpe cpe = new Cpe();
+        cpe.setVendor(parts[0]);
+        cpe.setProduct(parts[1]);
+        cpe.setVersion(parts[2]);
+
+        if (parts.length > 3) {
+            cpe.setUpdate(parts[3]);
+        }
+
+        return cpe;
+    }
+}
