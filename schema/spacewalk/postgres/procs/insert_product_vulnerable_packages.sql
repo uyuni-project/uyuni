608782bad3fbb34cdf86f5f26f38a903dacbabf3
HoussemNasri <houssemnasri2001@gmail.com>
2023-09-29 00:46:56 +0100

    Define the insert_product_vulnerable_packages procedure

--- schema/spacewalk/postgres/procs/insert_product_vulnerable_packages.sql
+++ schema/spacewalk/postgres/procs/insert_product_vulnerable_packages.sql
@@ -1,29 +1,21 @@
-CREATE TABLE IF NOT EXISTS suseOVALVulnerablePackage
-(
-    id             NUMERIC NOT NULL
-        CONSTRAINT suse_oval_vulnerable_pkg_id_pk PRIMARY KEY,
-    name           VARCHAR NOT NULL,
-    fix_version    VARCHAR
-);
-
-CREATE SEQUENCE IF NOT EXISTS suse_oval_vulnerable_pkg_id_seq START WITH 301;
-
-CREATE UNIQUE INDEX IF NOT EXISTS suse_oval_vulnerable_pkg_name_fix_version ON suseOVALVulnerablePackage(name, fix_version);
-
-CREATE TABLE IF NOT EXISTS suseOVALPlatformVulnerablePackage
-(
-    platform_id          NUMERIC NOT NULL
-        REFERENCES suseOVALPlatform (id),
-    cve_id               NUMERIC NOT NULL
-        REFERENCES rhnCve (id),
-    vulnerable_pkg_id    NUMERIC
-        REFERENCES suseOVALVulnerablePackage (id),
-    CONSTRAINT suse_oval_platform_vulnerable_pkg_id_pk PRIMARY KEY (platform_id, cve_id, vulnerable_pkg_id)
-);
+--
+-- Copyright (c) 2023 SUSE LLC
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+-- Red Hat trademarks are not licensed under GPLv2. No permission is
+-- granted to use or replicate Red Hat trademarks that are incorporated
+-- in this software or its documentation.
+--
 
 CREATE OR REPLACE PROCEDURE
-    insert_product_vulnerable_packages(package_name_in varchar,fix_version_in varchar,product_cpe_in varchar,cve_name_in varchar)
-    LANGUAGE plpgsql
+insert_product_vulnerable_packages(package_name_in varchar,fix_version_in varchar,product_cpe_in varchar,cve_name_in varchar)
+LANGUAGE plpgsql
 AS
 $$
 DECLARE
@@ -66,4 +58,4 @@ begin
             cve_id            = EXCLUDED.cve_id,
             vulnerable_pkg_id = EXCLUDED.vulnerable_pkg_id;
 end;
-$$;
+$$ language plpgsql;
