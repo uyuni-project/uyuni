2160b60f987ed4de7253e71a91d8f2605bc75254
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2023-09-29 10:29:27 +0200

    CI: we do not need the server logs
    they are already part of the support config logs.
    
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.com>

--- testsuite/podman_runner/19_get_server_logs.sh
+++ testsuite/podman_runner/19_get_server_logs.sh
@@ -12,6 +12,5 @@ src_dir=$(cd $(dirname "$0")/../.. && pwd -P)
 server_id=${1}
 rm -rfv /tmp/test-all-in-one/server-logs/${server_id}
 mkdir -p /tmp/test-all-in-one/server-logs/${server_id}
-sudo -i podman exec uyuni-server-all-in-one-test bash -c "tar jcvf /tmp/server-logs/${server_id}/var.log.rhn.tar.bz2 /var/log/rhn/"
 sudo -i podman exec uyuni-server-all-in-one-test bash -c "supportconfig -R /tmp/server-logs/${server_id} && chmod 644 /tmp/server-logs/${server_id}/scc_uyuni-server-all-in-one-test*.txz*"
 
