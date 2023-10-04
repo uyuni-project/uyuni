9070e34909b39db1f3108bc1542c52e8c2fde952
Thomas Florio <thomas.florio@suse.com>
2023-10-04 15:04:13 +0200

    Add product definitions for Debian 12

--- susemanager/src/mgr_bootstrap_data.py
+++ susemanager/src/mgr_bootstrap_data.py
@@ -1782,7 +1782,12 @@ DATA = {
     },
     'debian11-amd64' : {
         'PDID' : [-32, 2410], 'BETAPDID' : [2411], 'PKGLIST' : PKGLISTDEBIAN11,
-        'DEST' : '/srv/www/htdocs/pub/repositories/debian/11/bootstrap/',
+        'DEST' : DOCUMENT_ROOT + '/pub/repositories/debian/11/bootstrap/',
+        'TYPE' : 'deb'
+    },
+    'debian12-amd64' : {
+        'PDID' : [-43, 2677], 'BETAPDID' : [2676], 'PKGLIST' : PKGLISTDEBIAN12,
+        'DEST' : DOCUMENT_ROOT + '/pub/repositories/debian/12/bootstrap/',
         'TYPE' : 'deb'
     },
     'debian9-amd64-uyuni' : {
