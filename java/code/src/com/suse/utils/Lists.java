db28023a31a1032039c2ffa6f6a7170336a03f79
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2023-10-23 15:30:28 +0200

    TEST
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.com>

--- java/code/src/com/suse/utils/Lists.java
+++ java/code/src/com/suse/utils/Lists.java
@@ -12,6 +12,7 @@
  * granted to use or replicate Red Hat trademarks that are incorporated
  * in this software or its documentation.
  */
+//TEST
 package com.suse.utils;
 
 import java.util.ArrayList;
