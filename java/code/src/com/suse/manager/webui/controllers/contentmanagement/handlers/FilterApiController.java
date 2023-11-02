f37e296fdd930d17b5d11f51117d7744bd0a0992
Can Bulut Bayburt <1103552+cbbayburt@users.noreply.github.com>
2023-11-02 16:16:32 +0100

    Fix filter ID comparison when attaching filters to a CLM project (bsc#1215949) (#22928)

--- java/code/src/com/suse/manager/webui/controllers/contentmanagement/handlers/FilterApiController.java
+++ java/code/src/com/suse/manager/webui/controllers/contentmanagement/handlers/FilterApiController.java
@@ -111,7 +111,7 @@ public class FilterApiController {
 
         List<Long> filterIdsToDetach = dbContentProject.getProjectFilters()
                 .stream()
-                .map(filter -> filter.getFilter().getId())
+                .map(pf -> pf.getFilter().getId())
                 .filter(filterId -> !filtersIdToUpdate.contains(filterId))
                 .collect(Collectors.toList());
         filterIdsToDetach.forEach(filterId -> CONTENT_MGR.detachFilter(
@@ -123,11 +123,9 @@ public class FilterApiController {
         List<Long> filterIdsToAttach = filtersIdToUpdate
                 .stream()
                 .filter(filterId ->
-                        !dbContentProject.getProjectFilters()
+                        dbContentProject.getProjectFilters()
                                 .stream()
-                                .filter(filter -> filter.getId() == filterId)
-                                .findFirst()
-                                .isPresent()
+                                .noneMatch(pf -> pf.getFilter().getId() == filterId)
                 )
                 .collect(Collectors.toList());
         filterIdsToAttach
