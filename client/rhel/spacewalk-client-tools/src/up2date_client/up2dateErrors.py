c9207fb4c4d021ee7d1d0987768f8a969899d988
Stefan Bluhm <stefan.bluhm@clacee.eu>
2023-11-07 11:52:48 +0100

    Removed unused and deprecated/removed platform.dist import

--- client/rhel/spacewalk-client-tools/src/up2date_client/up2dateErrors.py
+++ client/rhel/spacewalk-client-tools/src/up2date_client/up2dateErrors.py
@@ -27,7 +27,6 @@ if not hasattr(t, 'ugettext'):
     t.ugettext = t.gettext
 _ = t.ugettext
 import OpenSSL
-from platform import dist
 from rhn.stringutils import ustr
 from up2date_client import config
 from up2date_client import up2dateLog
