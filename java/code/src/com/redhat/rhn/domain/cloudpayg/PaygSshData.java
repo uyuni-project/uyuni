534c138690c16774a86b4c38cdbc381aff29b8e1
Michael Calmer <mc@suse.de>
2023-11-10 15:03:58 +0100

    encrypt secret PaygSshData in DB table

--- java/code/src/com/redhat/rhn/domain/cloudpayg/PaygSshData.java
+++ java/code/src/com/redhat/rhn/domain/cloudpayg/PaygSshData.java
@@ -15,11 +15,20 @@
 
 package com.redhat.rhn.domain.cloudpayg;
 
+import com.redhat.rhn.common.conf.Config;
+import com.redhat.rhn.common.util.AESCryptException;
+import com.redhat.rhn.common.util.CryptHelper;
+import com.redhat.rhn.common.util.FileUtils;
 import com.redhat.rhn.domain.BaseDomainHelper;
 import com.redhat.rhn.domain.credentials.Credentials;
+import com.redhat.rhn.domain.credentials.CredentialsFactory;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.io.IOException;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -45,16 +54,16 @@ public class PaygSshData extends BaseDomainHelper {
     private String host;
     private Integer port;
     private String username;
-    private String password;
-    private String key;
-    private String keyPassword;
+    private String passwordEncrypted;
+    private String keyEncrypted;
+    private String keyPasswordEncrypted;
 
     private String bastionHost;
     private Integer bastionPort;
     private String bastionUsername;
-    private String bastionPassword;
-    private String bastionKey;
-    private String bastionKeyPassword;
+    private String bastionPasswordEncrypted;
+    private String bastionKeyEncrypted;
+    private String bastionKeyPasswordEncrypted;
 
     private Status status;
     private String errorMessage;
@@ -114,21 +123,21 @@ public class PaygSshData extends BaseDomainHelper {
                        String keyIn, String keyPasswordIn,
                        String bastionHostIn, Integer bastionPortIn,
                        String bastionUsernameIn, String bastionPasswordIn,
-                       String bastionKeyIn, String bastionKeyPasswordIn) {
+                       String bastionKeyIn, String bastionKeyPasswordIn) throws AESCryptException, IOException {
         this();
         this.description = descriptionIn;
         this.host = hostIn;
         this.port = portIn;
         this.username = usernameIn;
-        this.password = passwordIn;
-        this.key = keyIn;
-        this.keyPassword = keyPasswordIn;
+        setPassword(passwordIn);
+        setKey(keyIn);
+        setKeyPassword(keyPasswordIn);
         this.bastionHost = bastionHostIn;
         this.bastionPort = bastionPortIn;
         this.bastionUsername = bastionUsernameIn;
-        this.bastionPassword = bastionPasswordIn;
-        this.bastionKey = bastionKeyIn;
-        this.bastionKeyPassword = bastionKeyPasswordIn;
+        setBastionPassword(bastionPasswordIn);
+        setBastionKey(bastionKeyIn);
+        setBastionKeyPassword(bastionKeyPasswordIn);
         this.status = Status.P;
     }
 
@@ -190,30 +199,121 @@ public class PaygSshData extends BaseDomainHelper {
     }
 
     @Column(name = "password")
-    public String getPassword() {
-        return password;
+    public String getPasswordEncrypted() {
+        return passwordEncrypted;
     }
 
-    public void setPassword(String passwordIn) {
-        this.password = passwordIn;
+    /**
+     * @return the Password in clear text
+     * @throws IOException when something went wrong
+     * @throws AESCryptException when something went wrong
+     */
+    @Transient
+    public String getPassword() throws IOException, AESCryptException {
+        if (passwordEncrypted != null && passwordEncrypted.startsWith(CredentialsFactory.SALTED_MAGIC_B64)) {
+            // password is encrypted
+            String mpw = FileUtils.readFirstLineFromFile(Config.getDefaultMasterPasswordFile());
+            return CryptHelper.aes256Decrypt(passwordEncrypted, mpw);
+        }
+        return passwordEncrypted;
+    }
+
+    /**
+     * Set the Password and encrypt it
+     * @param passwordIn the plain password
+     * @throws IOException when something went wrong
+     * @throws AESCryptException when something went wrong
+     */
+    public void setPassword(String passwordIn) throws IOException, AESCryptException {
+        if (StringUtils.isEmpty(passwordIn)) {
+            passwordEncrypted = passwordIn;
+            return;
+        }
+        String mpw = FileUtils.readFirstLineFromFile(Config.getDefaultMasterPasswordFile());
+        passwordEncrypted = CryptHelper.aes256Encrypt(passwordIn, mpw);
+
+    }
+
+    public void setPasswordEncrypted(String passwordEncryptedIn) {
+        passwordEncrypted = passwordEncryptedIn;
     }
 
     @Column(name = "key")
-    public String getKey() {
-        return key;
+    public String getKeyEncrypted() {
+        return keyEncrypted;
     }
 
-    public void setKey(String keyIn) {
-        this.key = keyIn;
+    /**
+     * @return the Key in clear text
+     * @throws IOException when something went wrong
+     * @throws AESCryptException when something went wrong
+     */
+    @Transient
+    public String getKey() throws IOException, AESCryptException {
+        if (keyEncrypted != null && keyEncrypted.startsWith(CredentialsFactory.SALTED_MAGIC_B64)) {
+            // password is encrypted
+            String mpw = FileUtils.readFirstLineFromFile(Config.getDefaultMasterPasswordFile());
+            return CryptHelper.aes256Decrypt(keyEncrypted, mpw);
+        }
+        return keyEncrypted;
+    }
+
+    /**
+     * Set the Key and encrypt it
+     * @param keyIn the plain password
+     * @throws IOException when something went wrong
+     * @throws AESCryptException when something went wrong
+     */
+    public void setKey(String keyIn) throws IOException, AESCryptException {
+        if (StringUtils.isEmpty(keyIn)) {
+            keyEncrypted = keyIn;
+            return;
+        }
+        String mpw = FileUtils.readFirstLineFromFile(Config.getDefaultMasterPasswordFile());
+        keyEncrypted = CryptHelper.aes256Encrypt(keyIn, mpw);
+    }
+
+    public void setKeyEncrypted(String keyEncryptedIn) {
+        keyEncrypted = keyEncryptedIn;
     }
 
     @Column(name = "key_password")
-    public String getKeyPassword() {
-        return keyPassword;
+    public String getKeyPasswordEncrypted() {
+        return keyPasswordEncrypted;
     }
 
-    public void setKeyPassword(String keyPasswordIn) {
-        this.keyPassword = keyPasswordIn;
+    /**
+     * @return the Key Password in clear text
+     * @throws IOException when something went wrong
+     * @throws AESCryptException when something went wrong
+     */
+    @Transient
+    public String getKeyPassword() throws IOException, AESCryptException {
+        if (keyPasswordEncrypted != null && keyPasswordEncrypted.startsWith(CredentialsFactory.SALTED_MAGIC_B64)) {
+            // password is encrypted
+            String mpw = FileUtils.readFirstLineFromFile(Config.getDefaultMasterPasswordFile());
+            return CryptHelper.aes256Decrypt(keyPasswordEncrypted, mpw);
+        }
+        return keyPasswordEncrypted;
+    }
+
+    /**
+     * Set the Bastion Key Password and encrypt it
+     * @param keyPasswordIn the plain password
+     * @throws IOException when something went wrong
+     * @throws AESCryptException when something went wrong
+     */
+    public void setKeyPassword(String keyPasswordIn) throws IOException, AESCryptException {
+        if (StringUtils.isEmpty(keyPasswordIn)) {
+            keyPasswordEncrypted = keyPasswordIn;
+            return;
+        }
+        String mpw = FileUtils.readFirstLineFromFile(Config.getDefaultMasterPasswordFile());
+        keyPasswordEncrypted = CryptHelper.aes256Encrypt(keyPasswordIn, mpw);
+    }
+
+    public void setKeyPasswordEncrypted(String keyPasswordEncryptedIn) {
+        keyPasswordEncrypted = keyPasswordEncryptedIn;
     }
 
     @Column(name = "bastion_host")
@@ -244,30 +344,122 @@ public class PaygSshData extends BaseDomainHelper {
     }
 
     @Column(name = "bastion_password")
-    public String getBastionPassword() {
-        return bastionPassword;
+    public String getBastionPasswordEncrypted() {
+        return bastionPasswordEncrypted;
     }
 
-    public void setBastionPassword(String bastionPasswordIn) {
-        this.bastionPassword = bastionPasswordIn;
+    /**
+     * @return the Bastion Password in clear text
+     * @throws IOException when something went wrong
+     * @throws AESCryptException when something went wrong
+     */
+    @Transient
+    public String getBastionPassword() throws IOException, AESCryptException {
+        if (bastionPasswordEncrypted != null &&
+                bastionPasswordEncrypted.startsWith(CredentialsFactory.SALTED_MAGIC_B64)) {
+            // password is encrypted
+            String mpw = FileUtils.readFirstLineFromFile(Config.getDefaultMasterPasswordFile());
+            return CryptHelper.aes256Decrypt(bastionPasswordEncrypted, mpw);
+        }
+        return bastionPasswordEncrypted;
+    }
+
+    /**
+     * Set the Bastion Password and encrypt it
+     * @param bastionPasswordIn the plain password
+     * @throws IOException when something went wrong
+     * @throws AESCryptException when something went wrong
+     */
+    public void setBastionPassword(String bastionPasswordIn) throws IOException, AESCryptException {
+        if (StringUtils.isEmpty(bastionPasswordIn)) {
+            bastionPasswordEncrypted = bastionPasswordIn;
+            return;
+        }
+        String mpw = FileUtils.readFirstLineFromFile(Config.getDefaultMasterPasswordFile());
+        bastionPasswordEncrypted = CryptHelper.aes256Encrypt(bastionPasswordIn, mpw);
+    }
+
+    public void setBastionPasswordEncrypted(String bastionPasswordEncryptedIn) {
+        bastionPasswordEncrypted = bastionPasswordEncryptedIn;
     }
 
     @Column(name = "bastion_key")
-    public String getBastionKey() {
-        return bastionKey;
+    public String getBastionKeyEncrypted() {
+        return bastionKeyEncrypted;
     }
 
-    public void setBastionKey(String bastionKeyIn) {
-        this.bastionKey = bastionKeyIn;
+    /**
+     * @return the Bastion Key in clear text
+     * @throws IOException when something went wrong
+     * @throws AESCryptException when something went wrong
+     */
+    @Transient
+    public String getBastionKey() throws IOException, AESCryptException {
+        if (bastionKeyEncrypted != null && bastionKeyEncrypted.startsWith(CredentialsFactory.SALTED_MAGIC_B64)) {
+            // password is encrypted
+            String mpw = FileUtils.readFirstLineFromFile(Config.getDefaultMasterPasswordFile());
+            return CryptHelper.aes256Decrypt(bastionKeyEncrypted, mpw);
+        }
+        return bastionKeyEncrypted;
+    }
+
+    /**
+     * Set the Bastion Key Password and encrypt it
+     * @param bastionKeyIn the plain password
+     * @throws IOException when something went wrong
+     * @throws AESCryptException when something went wrong
+     */
+    public void setBastionKey(String bastionKeyIn) throws IOException, AESCryptException {
+        if (StringUtils.isEmpty(bastionKeyIn)) {
+            bastionKeyEncrypted = bastionKeyIn;
+            return;
+        }
+        String mpw = FileUtils.readFirstLineFromFile(Config.getDefaultMasterPasswordFile());
+        bastionKeyEncrypted = CryptHelper.aes256Encrypt(bastionKeyIn, mpw);
+    }
+
+    public void setBastionKeyEncrypted(String bastionKeyEncryptedIn) {
+        bastionKeyEncrypted = bastionKeyEncryptedIn;
     }
 
     @Column(name = "bastion_key_password")
-    public String getBastionKeyPassword() {
-        return bastionKeyPassword;
+    public String getBastionKeyPasswordEncrypted() {
+        return bastionKeyPasswordEncrypted;
     }
 
-    public void setBastionKeyPassword(String bastionKeyPasswordIn) {
-        this.bastionKeyPassword = bastionKeyPasswordIn;
+    /**
+     * @return the Bastion Key Password in clear text
+     * @throws IOException when something went wrong
+     * @throws AESCryptException when something went wrong
+     */
+    @Transient
+    public String getBastionKeyPassword() throws IOException, AESCryptException {
+        if (bastionKeyPasswordEncrypted != null &&
+                bastionKeyPasswordEncrypted.startsWith(CredentialsFactory.SALTED_MAGIC_B64)) {
+            // password is encrypted
+            String mpw = FileUtils.readFirstLineFromFile(Config.getDefaultMasterPasswordFile());
+            return CryptHelper.aes256Decrypt(bastionKeyPasswordEncrypted, mpw);
+        }
+        return bastionKeyPasswordEncrypted;
+    }
+
+    /**
+     * Set the Bastion Key Password and encrypt it
+     * @param bastionKeyPasswordIn the plain password
+     * @throws IOException when something went wrong
+     * @throws AESCryptException when something went wrong
+     */
+    public void setBastionKeyPassword(String bastionKeyPasswordIn) throws IOException, AESCryptException {
+        if (StringUtils.isEmpty(bastionKeyPasswordIn)) {
+            bastionKeyPasswordEncrypted = bastionKeyPasswordIn;
+            return;
+        }
+        String mpw = FileUtils.readFirstLineFromFile(Config.getDefaultMasterPasswordFile());
+        bastionKeyPasswordEncrypted = CryptHelper.aes256Encrypt(bastionKeyPasswordIn, mpw);
+    }
+
+    public void setBastionKeyPasswordEncrypted(String bastionKeyPasswordEncryptedIn) {
+        bastionKeyPasswordEncrypted = bastionKeyPasswordEncryptedIn;
     }
 
     @Column(name = "status")
@@ -316,6 +508,23 @@ public class PaygSshData extends BaseDomainHelper {
         return "localhost".equals(host);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        ToStringBuilder builder = new ToStringBuilder(this)
+                .append(host)
+                .append(port)
+                .append(username);
+         if (!StringUtils.isBlank(bastionHost)) {
+             builder.append(bastionHost)
+                     .append(bastionPort)
+                     .append(bastionUsername);
+         }
+         return builder.append(status).toString();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -331,15 +540,15 @@ public class PaygSshData extends BaseDomainHelper {
         return new EqualsBuilder().append(host, that.host)
                 .append(port, that.port)
                 .append(username, that.username)
-                .append(password, that.password)
-                .append(key, that.key)
-                .append(keyPassword, that.keyPassword)
+                .append(passwordEncrypted, that.passwordEncrypted)
+                .append(keyEncrypted, that.keyEncrypted)
+                .append(keyPasswordEncrypted, that.keyPasswordEncrypted)
                 .append(bastionHost, that.bastionHost)
                 .append(bastionPort, that.bastionPort)
                 .append(bastionUsername, that.bastionUsername)
-                .append(bastionPassword, that.bastionPassword)
-                .append(bastionKey, that.bastionKey)
-                .append(bastionKeyPassword, that.bastionKeyPassword)
+                .append(bastionPasswordEncrypted, that.bastionPasswordEncrypted)
+                .append(bastionKeyEncrypted, that.bastionKeyEncrypted)
+                .append(bastionKeyPasswordEncrypted, that.bastionKeyPasswordEncrypted)
                 .append(status, that.status)
                 .append(errorMessage, that.errorMessage)
                 .isEquals();
@@ -351,15 +560,15 @@ public class PaygSshData extends BaseDomainHelper {
                 .append(host)
                 .append(port)
                 .append(username)
-                .append(password)
-                .append(key)
-                .append(keyPassword)
+                .append(passwordEncrypted)
+                .append(keyEncrypted)
+                .append(keyPasswordEncrypted)
                 .append(bastionHost)
                 .append(bastionPort)
                 .append(bastionUsername)
-                .append(bastionPassword)
-                .append(bastionKey)
-                .append(bastionKeyPassword)
+                .append(bastionPasswordEncrypted)
+                .append(bastionKeyEncrypted)
+                .append(bastionKeyPasswordEncrypted)
                 .append(status)
                 .append(errorMessage)
                 .toHashCode();
