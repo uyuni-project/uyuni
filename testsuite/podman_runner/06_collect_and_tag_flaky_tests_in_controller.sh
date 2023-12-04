8d2ba03c919a7b901035d6ab6e81b7fec9d68037
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2023-12-04 16:48:44 +0100

    DEBUG
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.com>

--- testsuite/podman_runner/06_collect_and_tag_flaky_tests_in_controller.sh
+++ testsuite/podman_runner/06_collect_and_tag_flaky_tests_in_controller.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -xe
-sudo -i podman exec controller-test bash -c "export GITHUB_TOKEN=${GITHUB_TOKEN} && cd /testsuite && echo "DEBUG IN SHELL" && ruby ext-tools/collect_and_tag_flaky_tests.rb features && rake utils:collect_and_tag_flaky_tests"
+sudo -i podman exec controller-test bash -c "export GITHUB_TOKEN=${GITHUB_TOKEN} && cd /testsuite && echo \"DEBUG IN SHELL\" && ruby ext-tools/collect_and_tag_flaky_tests.rb features && rake utils:collect_and_tag_flaky_tests"
