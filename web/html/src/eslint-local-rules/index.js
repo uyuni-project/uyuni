7c913c0ff82741e6854e347e4e2b691201fcd2e4
Karl Tarvas <Etheryte@users.noreply.github.com>
2023-12-13 20:41:09 +0100

    Fix single apostrophe curly interpolation bug, add linting rule to detect it (#7895)

--- web/html/src/eslint-local-rules/index.js
+++ web/html/src/eslint-local-rules/index.js
@@ -1,5 +1,7 @@
 const noRawDate = require("./no-raw-date");
+const intlApostropheCurly = require("./intl-apostrophe-curly");
 
 module.exports = {
   "no-raw-date": noRawDate,
+  "intl-apostrophe-curly": intlApostropheCurly,
 };
