c75f5e3ae4c69099e32c59b3e2277f429b25027f
Miguel Sama <miguel.sama@suse.com>
2023-12-14 12:05:48 +0100

    Add dummy method to salt test api
    Signed-off-by: Miguel Sama <miguel.sama@suse.com>

--- java/code/src/com/suse/manager/webui/services/test/TestSaltApi.java
+++ java/code/src/com/suse/manager/webui/services/test/TestSaltApi.java
@@ -78,6 +78,11 @@ public class TestSaltApi implements SaltApi {
     }
 
     @Override
+    public Optional<Boolean> mkDir(Path path, String modeString) {
+        return Optional.empty();
+    }
+
+    @Override
     public Optional<Boolean> copyFile(Path src, Path dst) {
         throw new UnsupportedOperationException();
     }
