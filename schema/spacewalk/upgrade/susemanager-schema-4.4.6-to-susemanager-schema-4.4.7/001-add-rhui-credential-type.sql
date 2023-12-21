78df4b2e95865105d258637260c2146f743095c2
Thomas Florio <thomas.florio@suse.com>
2023-12-21 13:52:42 +0100

    Fix existing scripts to be idempotent

--- schema/spacewalk/upgrade/susemanager-schema-4.4.6-to-susemanager-schema-4.4.7/001-add-rhui-credential-type.sql
+++ schema/spacewalk/upgrade/susemanager-schema-4.4.6-to-susemanager-schema-4.4.7/001-add-rhui-credential-type.sql
@@ -1,4 +1,11 @@
-insert into suseCredentialsType (id, label, name)
-  select sequence_nextval('suse_credtype_id_seq'), 'rhui', 'Red Hat Update Infrastructure'
-  where not exists(select 1 from suseCredentialsType where label = 'rhui');
-
+DO $$
+  BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'susecredentialstype') THEN
+      insert into suseCredentialsType (id, label, name)
+        select sequence_nextval('suse_credtype_id_seq'), 'rhui', 'Red Hat Update Infrastructure'
+        where not exists(select 1 from suseCredentialsType where label = 'rhui');
+    ELSE
+      RAISE NOTICE 'suseCredentialsType does not exists';
+    END IF;
+  END;
+$$;
