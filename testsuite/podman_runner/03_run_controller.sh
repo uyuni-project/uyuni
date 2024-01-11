f3bf5e45afe0bffd45ca60b5e417539ce00967d1
Michael Calmer <mc@suse.de>
2024-01-11 16:58:10 +0100

    Fix DB in podman acceptance test run

--- testsuite/podman_runner/03_run_controller.sh
+++ testsuite/podman_runner/03_run_controller.sh
@@ -4,4 +4,4 @@ src_dir=$(cd $(dirname "$0")/../.. && pwd -P)
 
 sudo -i podman run --rm -d --network uyuni-network-1 -v /tmp/test-all-in-one:/tmp --name controller-test -h controller-test -v ${src_dir}/testsuite:/testsuite ghcr.io/$UYUNI_PROJECT/uyuni/ci-test-controller-dev:$UYUNI_VERSION
 
-podman ps
+sudo podman ps
