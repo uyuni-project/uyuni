1ceb17471fb2e50dedb799dd6e0ce96d0db6c2e8
Michael Calmer <mc@suse.de>
2024-01-11 10:47:38 +0100

    change test scripts to reflect move of the schema dir to /usr/share/susemanager/db
    Co-authored-by: Wiktor Maj <wiktor.maj@suse.com>

--- susemanager-utils/testing/docker/scripts/init-pgsql-db4eclipse.sh
+++ susemanager-utils/testing/docker/scripts/init-pgsql-db4eclipse.sh
@@ -31,7 +31,7 @@ if [ -z "$NEXTVERSION" ]; then
     RPMVERSION=`rpm -q --qf "%{version}\n" --specfile /manager/schema/spacewalk/susemanager-schema.spec | head -n 1`
     NEXTVERSION=`echo $RPMVERSION | awk '{ pre=post=$0; gsub("[0-9]+$","",pre); gsub(".*\\\\.","",post); print pre post+1; }'`
 
-    if [ -d /etc/sysconfig/rhn/schema-upgrade/susemanager-schema-$RPMVERSION-to-susemanager-schema-$NEXTVERSION ]; then
+    if [ -d /usr/share/susemanager/db/schema-upgrade/susemanager-schema-$RPMVERSION-to-susemanager-schema-$NEXTVERSION ]; then
         export SUMA_TEST_SCHEMA_VERSION=$NEXTVERSION
 
     else
@@ -60,7 +60,7 @@ if [ -z "$REPORTNEXTVERSION" ]; then
     RPMVERSION=`rpm -q --qf "%{version}\n" --specfile /manager/schema/reportdb/uyuni-reportdb-schema.spec | head -n 1`
     NEXTVERSION=`echo $RPMVERSION | awk '{ pre=post=$0; gsub("[0-9]+$","",pre); gsub(".*\\\\.","",post); print pre post+1; }'`
 
-    if [ -d /etc/sysconfig/rhn/reportdb-schema-upgrade/uyuni-reportdb-schema-$RPMVERSION-to-uyuni-reportdb-schema-$NEXTVERSION ]; then
+    if [ -d /usr/share/susemanager/db/reportdb-schema-upgrade/uyuni-reportdb-schema-$RPMVERSION-to-uyuni-reportdb-schema-$NEXTVERSION ]; then
         export SUMA_TEST_SCHEMA_VERSION=$NEXTVERSION
 
     else
