c75036fbf70c601b5e6cedc28d49c935bfabc8ac
Michael Calmer <mc@suse.de>
2024-01-11 10:47:38 +0100

    change test scripts to reflect move of the schema dir to /usr/share/susemanager/db
    Co-authored-by: Wiktor Maj <wiktor.maj@suse.com>

--- susemanager-utils/testing/docker/scripts/build-schema.sh
+++ susemanager-utils/testing/docker/scripts/build-schema.sh
@@ -6,21 +6,21 @@ cp -r /manager/schema /tmp
 cd /tmp/schema/spacewalk
 
 # Build the schema
-make -f Makefile.schema SCHEMA=susemanager-schema VERSION=4.3 RELEASE=testing
+make -f Makefile.schema SCHEMA=susemanager-schema VERSION=5.0 RELEASE=testing
 
-if [ -d /etc/sysconfig/rhn/schema-upgrade ]; then
+if [ -d /usr/share/susemanager/db/schema-upgrade ]; then
     # remove old migration directories before we install the new
-    rm -r /etc/sysconfig/rhn/schema-upgrade
+    rm -r /usr/share/susemanager/db/schema-upgrade
 fi
 
 # Install directories
-install -m 0755 -d /etc/sysconfig/rhn
-install -m 0755 -d /etc/sysconfig/rhn/postgres
-install -m 0755 -d /etc/sysconfig/rhn/schema-upgrade
+install -m 0755 -d /usr/share/susemanager/db
+install -m 0755 -d /usr/share/susemanager/db/postgres
+install -m 0755 -d /usr/share/susemanager/db/schema-upgrade
 
 # Install sql files
-install -m 0644 postgres/main.sql /etc/sysconfig/rhn/postgres
-install -m 0644 postgres/end.sql /etc/sysconfig/rhn/postgres/upgrade-end.sql
+install -m 0644 postgres/main.sql /usr/share/susemanager/db/postgres
+install -m 0644 postgres/end.sql /usr/share/susemanager/db/postgres/upgrade-end.sql
 
-( cd upgrade && tar cf - --exclude='*.sql' . | ( cd /etc/sysconfig/rhn/schema-upgrade && tar xf - ) )
+( cd upgrade && tar cf - --exclude='*.sql' . | ( cd /usr/share/susemanager/db/schema-upgrade && tar xf - ) )
 
