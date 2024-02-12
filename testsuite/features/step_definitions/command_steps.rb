4d5d77a18d5b646201add29c9ce4efe95bfd1448
Wiktor Maj <wiktor.maj@suse.com>
2024-02-12 14:45:59 +0100

    Opt to iterate through the list items for the 'leap15.5-x86_64' Uyuni product, avoiding manual input (#23662)
    * CHANNEL_TO_SYNC iterate over leap15.5-x86_64 items of Uyuni product
    
    * Opt to iterate through the list items for the 'leap15.5-x86_64' Uyuni product, avoiding manual input (#8281)
    
    * CHANNEL_TO_SYNC iterate over leap15.5-x86_64 items of Uyuni product
    
    * CHANNEL_TO_SYNC iterate over leap15.5-x86_64 items of Uyuni product
    
    * refactoring
    
    * channels_to_synchronize variable assignment
    
    * check if channels_to_synchronize are strictly null
    
    * check if channels_to_synchronize are strictly null rubocop
    
    * timeouts and double checking
    
    * Opt to iterate through the list items for the 'leap15.5-x86_64' Uyuni product, avoiding manual input (#8281)
    
    * CHANNEL_TO_SYNC iterate over leap15.5-x86_64 items of Uyuni product
    
    * CHANNEL_TO_SYNC iterate over leap15.5-x86_64 items of Uyuni product
    
    * refactoring
    
    * channels_to_synchronize variable assignment
    
    * check if channels_to_synchronize are strictly null
    
    * check if channels_to_synchronize are strictly null rubocop
    
    * timeouts and double checking

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -164,6 +164,17 @@ When(/^I use spacewalk-common-channel to add channel "([^"]*)" with arch "([^"]*
   $command_output, _code = get_target('server').run(command)
 end
 
+When(/^I use spacewalk-common-channel to add all "([^"]*)" channels with arch "([^"]*)"$/) do |channel, architecture|
+  channels_to_synchronize = CHANNEL_TO_SYNC_BY_OS_PRODUCT_VERSION.dig(product, "#{channel}-#{architecture}")
+  raise ScriptError, "Synchronization error, version type #{channel}-#{architecture} in #{product} product not found" if channels_to_synchronize.nil?
+
+  channels_to_synchronize.each do |os_product_version_channel|
+    log "Adding channel: #{os_product_version_channel}"
+    command = "spacewalk-common-channels -u admin -p admin -a #{architecture} #{os_product_version_channel.gsub("-#{architecture}", '')}"
+    get_target('server').run(command)
+  end
+end
+
 When(/^I use spacewalk-repo-sync to sync channel "([^"]*)"$/) do |channel|
   $command_output, _code = get_target('server').run_until_ok("spacewalk-repo-sync -c #{channel}")
 end
@@ -318,7 +329,8 @@ end
 # This function kills spacewalk-repo-sync processes for a particular OS product version.
 # It waits for all the reposyncs in the allow-list to complete, and kills all others.
 When(/^I kill running spacewalk-repo-sync for "([^"]*)"$/) do |os_product_version|
-  next if CHANNEL_TO_SYNC_BY_OS_PRODUCT_VERSION[product][os_product_version].nil?
+  next if CHANNEL_TO_SYNC_BY_OS_PRODUCT_VERSION.dig(product, os_product_version).nil?
+
   channels_to_kill = CHANNEL_TO_SYNC_BY_OS_PRODUCT_VERSION[product][os_product_version]
   log "Killing channels:\n#{channels_to_kill}"
   time_spent = 0
@@ -399,7 +411,9 @@ When(/^I wait until the channel "([^"]*)" has been synced$/) do |channel|
 end
 
 When(/^I wait until all synchronized channels for "([^"]*)" have finished$/) do |os_product_version|
-  channels_to_wait = CHANNEL_TO_SYNC_BY_OS_PRODUCT_VERSION[product][os_product_version]
+  channels_to_wait = CHANNEL_TO_SYNC_BY_OS_PRODUCT_VERSION.dig(product, os_product_version)
+  raise ScriptError, "Synchronization error, version type #{os_product_version} in #{product} product not found" if channels_to_wait.nil?
+
   time_spent = 0
   checking_rate = 10
   timeout = 0
