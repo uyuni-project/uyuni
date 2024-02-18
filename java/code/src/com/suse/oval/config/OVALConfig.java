d9dd3739a35659961b31b58cbf662f19e72a6be2
HoussemNasri <houssemnasri2001@gmail.com>
2024-02-18 20:16:58 +0100

    Refactor and fix checkstyle

--- java/code/src/com/suse/oval/config/OVALConfig.java
+++ java/code/src/com/suse/oval/config/OVALConfig.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright (c) 2023 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
 package com.suse.oval.config;
 
-import com.google.gson.annotations.SerializedName;
 import com.suse.oval.OsFamily;
+
+import com.google.gson.annotations.SerializedName;
 
 import java.util.Map;
 import java.util.Optional;
@@ -10,17 +26,22 @@ public class OVALConfig {
     @SerializedName("sources")
     private Map<OsFamily, OVALDistributionSourceInfo> sources;
 
-    public OVALConfig() {
-    }
-
     public Map<OsFamily, OVALDistributionSourceInfo> getSources() {
         return sources;
     }
 
-    public void setSources(Map<OsFamily, OVALDistributionSourceInfo> sources) {
-        this.sources = sources;
+    private void setSources(Map<OsFamily, OVALDistributionSourceInfo> sourcesIn) {
+        this.sources = sourcesIn;
     }
 
+    /**
+     * Finds the location or URL of the OVAL vulnerability and/or patch files for the given OVAL product
+     * identified by the given os family and version.
+     *
+     * @param osFamily the os family of the OS to find OVAL data for
+     * @param version the version of the OS to find OVAL data for
+     * @return the locations of OVAL vulnerability and/or patch files for the given OS
+     * */
     public Optional<OVALSourceInfo> lookupSourceInfo(OsFamily osFamily, String version) {
         OVALDistributionSourceInfo distributionSources = sources.get(osFamily);
         if (distributionSources != null) {
