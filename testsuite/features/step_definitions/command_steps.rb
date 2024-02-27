d3db6da145df77c5a80fba0583da9d4a08e5efc7
Wiktor Maj <wiktor.maj@suse.com>
2024-02-27 13:52:40 +0000

    indent

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -185,15 +185,13 @@ When(/^I use spacewalk-common-channel to add all "([^"]*)" channels with arch "(
   raise ScriptError, "Synchronization error, channel #{channel} or #{channel}-#{architecture} in #{product} product not found" if channels_to_synchronize.nil? || channels_to_synchronize.empty?
 
   channels_to_synchronize.each do |os_product_version_channel|
-    begin
-      command = "spacewalk-common-channels -u admin -p admin -a #{architecture} #{os_product_version_channel.gsub("-#{architecture}", '')}"
-      get_target('server').run(command)
-      log "Channel #{os_product_version_channel.gsub("-#{architecture}", '')} added"
-    rescue ScriptError
-      command = "spacewalk-common-channels -u admin -p admin -a #{architecture} #{os_product_version_channel}"
-      get_target('server').run(command)
-      log "Channel #{os_product_version_channel} added"
-    end
+    command = "spacewalk-common-channels -u admin -p admin -a #{architecture} #{os_product_version_channel.gsub("-#{architecture}", '')}"
+    get_target('server').run(command)
+    log "Channel #{os_product_version_channel.gsub("-#{architecture}", '')} added"
+  rescue ScriptError
+    command = "spacewalk-common-channels -u admin -p admin -a #{architecture} #{os_product_version_channel}"
+    get_target('server').run(command)
+    log "Channel #{os_product_version_channel} added"
   end
 end
 
