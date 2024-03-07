1eb7bc9dd37eef009e458af817ac5a5c3d2211c3
Etheryte <karl.tarvas@suse.com>
2024-03-07 20:02:44 +0100

    Fix Java checkstyle

--- java/code/src/com/suse/manager/webui/utils/ViewHelper.java
+++ java/code/src/com/suse/manager/webui/utils/ViewHelper.java
@@ -55,7 +55,7 @@ public enum ViewHelper {
     /**
      * List of pages that are updated to newer Bootstrap.
      * @see ViewHelper#isBootstrapReady
-     * 
+     *
      * NB! This must be in sync with web/html/src/core/spa/view-helper.ts
      */
     private static final List<String> BOOTSTRAP_READY_PAGES = List.of("/rhn/YourRhn.do");
