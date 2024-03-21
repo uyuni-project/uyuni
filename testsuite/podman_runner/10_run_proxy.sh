cd22352f3810f3e0e10df7fccd4bb04352a28eb4
Wiktor Maj <wiktor.maj@suse.com>
2024-03-21 16:25:31 +0100

    pipeline

--- testsuite/podman_runner/10_run_proxy.sh
+++ testsuite/podman_runner/10_run_proxy.sh
@@ -11,7 +11,7 @@ create_proxy_configuration() {
   cat <<EOF > $HOME/config.yaml
 server: uyuni-server-all-in-one-test
 ca_crt: |
-$(sed 's/^/  /' /tmp/RHN-ORG-TRUSTED-SSL-CERT)
+$(sed 's/^/  /' /tmp/test-all-in-one/RHN-ORG-TRUSTED-SSL-CERT)
 proxy_fqdn: proxy-httpd
 max_cache_size_mb: 2048
 server_version: 5.0.0 Beta1
@@ -22,20 +22,20 @@ EOF
 httpd:
   system_id: <?xml version="1.0"?><params><param><value><struct><member><name>username</name><value><string>admin</string></value></member><member><name>os_release</name><value><string>(unknown)</string></value></member><member><name>operating_system</name><value><string>(unknown)</string></value></member><member><name>architecture</name><value><string>x86_64-redhat-linux</string></value></member><member><name>system_id</name><value><string>ID-1000010003</string></value></member><member><name>type</name><value><string>REAL</string></value></member><member><name>fields</name><value><array><data><value><string>system_id</string></value><value><string>os_release</string></value><value><string>operating_system</string></value><value><string>architecture</string></value><value><string>username</string></value><value><string>type</string></value></data></array></value></member><member><name>checksum</name><value><string>1d835605853e545ae07265a1b2317e14ce44162ef70581b0e4f0e044d2f17d25</string></value></member></struct></value></param></params>
   server_crt: |
-$(sed 's/^/      /' /tmp/server.crt)
+$(sed 's/^/      /' /tmp/test-all-in-one/server.crt)
   server_key: |
-$(sed 's/^/      /' /tmp/server.key)
+$(sed 's/^/      /' /tmp/test-all-in-one/server.key)
 EOF
 
-  ssh-keygen -t rsa -b 4096 -C "salt@uyuni-server-all-in-one-test" -f /tmp/id_openssh_rsa -N ""
+  ssh-keygen -t rsa -b 4096 -C "salt@uyuni-server-all-in-one-test" -f /tmp/test-all-in-one/id_openssh_rsa -N ""
   cat <<EOF > $HOME/ssh.yaml
 ssh:
   server_ssh_key_pub: |
     ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDBNYMZEWSmHVsuK77xotneNSiZ7kvs4jGIvhyMsHIYapk4ECQ3CHdekJjK7+KKO6fLswGNst+7f/xSK9D0A3XWvcvEU3FnIWNkYdVOO3P7F4sjiveFa+kj10ZODNfi9JpWQqD7ulu8KQsf9vuR5mGJHjx4Pqn8WcKXjzkfe+LbmKih5ZXoTBNHwruMMO3/P46tWeT9Xu9+2h7JpZzaFMY3u69OFchhH1XYjWxhVs0hBio0a4blNsItppm7IodKqwcoIy3mczZemQSvMt35Y0b22HpXAfnqdgv7Ms9gVxsp1say1P886yesFkHR39fDhEvDSvb0wF6DqB+Q08QJC5Z6cw2XhELcfvM2mcXVkW6/EXu1v03uv+RPnS+6m9Ok+lyUXZNe/xtOiLOQN8HjU4IfJvoj3YQGpGhBEz68oJ7IKxoxUfVJZ+1ccisceGQG8jjTOZxQFwEhaOYclT1+Czh7/2aOLXyDzB5gLfkyxnQEVfA5fEe5/dld21gmrt0bBPk= salt@uyuni-server-all-in-one-test
   server_ssh_push: |
-$(sed 's/^/    /' /tmp/id_openssh_rsa)
+$(sed 's/^/    /' /tmp/test-all-in-one/id_openssh_rsa)
   server_ssh_push_pub: |
-$(sed 's/^/    /' /tmp/id_openssh_rsa.pub)
+$(sed 's/^/    /' /tmp/test-all-in-one/id_openssh_rsa.pub)
 EOF
 }
 
@@ -100,7 +100,7 @@ run_proxy_containers() {
 }
 
 cleanup () {
-  rm /tmp/id_openssh_rsa /tmp/id_openssh_rsa.pub /tmp/RHN-ORG-TRUSTED-SSL-CERT /tmp/server.crt /tmp/server.key
+  rm /tmp/test-all-in-one/id_openssh_rsa /tmp/test-all-in-one/id_openssh_rsa.pub /tmp/test-all-in-one/RHN-ORG-TRUSTED-SSL-CERT /tmp/test-all-in-one/server.crt /tmp/test-all-in-one/server.key
 }
 
 get_server_certificates
