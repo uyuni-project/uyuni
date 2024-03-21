70276da621c38379745b319d5b12b93adb108865
Paolo Invernizzi <paolo.invernizzi@suse.com>
2024-03-21 19:03:40 +0100

    revert test

--- testsuite/podman_runner/19_run_secondary_parallelizable_tests.sh
+++ testsuite/podman_runner/19_run_secondary_parallelizable_tests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -xe
-echo "test"
 sudo -i podman exec controller-test bash -c "zypper ref && zypper -n install expect"
 sudo -i podman exec controller-test bash -c "export CUCUMBER_PUBLISH_TOKEN=${CUCUMBER_PUBLISH_TOKEN} && export PROVIDER=podman && export SERVER=uyuni-server-all-in-one-test && export HOSTNAME=controller-test && export SSH_MINION=opensusessh && export MINION=sle_minion && export RHLIKE_MINION=rhlike_minion && export TAGS=\"\\\"not @flaky\\\"\" && cd /testsuite && rake cucumber:secondary_parallelizable"
