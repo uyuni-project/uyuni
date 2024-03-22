2ab729d19e7eb72a60a46a0bb5fab4d03505b53e
Thomas Florio <thomas.florio@suse.com>
2024-03-22 16:33:10 +0100

    Added trigger to send notification on pending results

--- schema/spacewalk/postgres/triggers/suseCoCoAttestationResult.sql
+++ schema/spacewalk/postgres/triggers/suseCoCoAttestationResult.sql
@@ -22,3 +22,18 @@ AFTER UPDATE ON suseCoCoAttestationResult
 FOR EACH ROW
 WHEN (OLD.status IS DISTINCT FROM NEW.status)
 EXECUTE PROCEDURE suse_cocoatt_res_up_trig_fun();
+
+CREATE OR REPLACE FUNCTION suse_cocoatt_notify_pending_trig_fun() RETURNS TRIGGER AS
+$$
+BEGIN
+    NOTIFY pendingAttestationResult;
+    RETURN NEW;
+END;
+$$ language plpgsql;
+
+CREATE TRIGGER
+suse_cocoatt_notify_pending_trig
+AFTER INSERT OR UPDATE OF status ON suseCoCoAttestationResult
+FOR EACH ROW
+WHEN (NEW.status = 'PENDING')
+EXECUTE PROCEDURE suse_cocoatt_notify_pending_trig_fun();
