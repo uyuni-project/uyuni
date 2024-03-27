b65fc9864fd66e31d57eb2eb687d1cd3554fb4eb
Wiktor Maj <wiktor.maj@suse.com>
2024-03-27 13:31:19 +0100

    $HOME

--- testsuite/podman_runner/11_setup_sshd.sh
+++ testsuite/podman_runner/11_setup_sshd.sh
@@ -6,16 +6,16 @@ sudo -i podman exec uyuni-server-all-in-one-test bash -c "if [ ! -d /root/.ssh ]
 sudo -i podman exec opensusessh bash -c "echo 'root:linux' | chpasswd && echo 123456789 > /etc/machine-id"
 sudo -i podman exec opensusessh bash -c "if [ ! -d /root/.ssh ];then mkdir /root/.ssh/;chmod 700 /root/.ssh;fi;cp /tmp/authorized_keys /root/.ssh/"
 
-if [ ! -d $USER/.ssh ]; then
-    mkdir $USER/.ssh/
-    chmod 700 $USER/.ssh
+if [ ! -d $HOME/.ssh ]; then
+    mkdir $HOME/.ssh/
+    chmod 700 $HOME/.ssh
 fi
 
-cp /tmp/authorized_keys $USER/.ssh/
+cp /tmp/authorized_keys $HOME/.ssh/
 cat <<EOF > /tmp/test-all-in-one/config
 Host $(hostname -I | cut -d " " -f1)
     User $USER
 EOF
 
-cat * $USER/.ssh/
+cat * $HOME/.ssh/
 sudo -i podman exec controller-test bash -c "if [ ! -d /root/.ssh ];then mkdir /root/.ssh/;chmod 700 /root/.ssh;fi;cp /tmp/authorized_keys /root/.ssh/;cp /tmp/config /root/.ssh/"
