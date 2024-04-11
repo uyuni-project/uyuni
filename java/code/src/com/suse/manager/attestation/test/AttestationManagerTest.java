998be8c1042dbd3f540f39e87ea775dc3d6491a8
Michael Calmer <mc@suse.de>
2024-04-11 17:55:36 +0200

    unit test for coco attestation list API

--- java/code/src/com/suse/manager/attestation/test/AttestationManagerTest.java
+++ java/code/src/com/suse/manager/attestation/test/AttestationManagerTest.java
@@ -19,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.redhat.rhn.common.hibernate.HibernateFactory;
 import com.redhat.rhn.common.hibernate.LookupException;
 import com.redhat.rhn.common.security.PermissionException;
 import com.redhat.rhn.domain.action.Action;
@@ -26,9 +27,12 @@ import com.redhat.rhn.domain.action.CoCoAttestationAction;
 import com.redhat.rhn.domain.server.MinionServer;
 import com.redhat.rhn.domain.server.Pillar;
 import com.redhat.rhn.domain.server.Server;
+import com.redhat.rhn.domain.server.ServerFactory;
+import com.redhat.rhn.domain.server.ServerGroupFactory;
 import com.redhat.rhn.domain.server.test.MinionServerFactoryTest;
 import com.redhat.rhn.domain.server.test.ServerFactoryTest;
 import com.redhat.rhn.domain.user.User;
+import com.redhat.rhn.manager.system.SystemManager;
 import com.redhat.rhn.taskomatic.TaskomaticApi;
 import com.redhat.rhn.taskomatic.TaskomaticApiException;
 import com.redhat.rhn.testing.JMockBaseTestCaseWithUser;
@@ -37,6 +41,7 @@ import com.redhat.rhn.testing.UserTestUtils;
 import com.suse.manager.attestation.AttestationManager;
 import com.suse.manager.model.attestation.AttestationFactory;
 import com.suse.manager.model.attestation.CoCoAttestationResult;
+import com.suse.manager.model.attestation.CoCoAttestationStatus;
 import com.suse.manager.model.attestation.CoCoEnvironmentType;
 import com.suse.manager.model.attestation.ServerCoCoAttestationConfig;
 import com.suse.manager.model.attestation.ServerCoCoAttestationReport;
@@ -53,6 +58,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 public class AttestationManagerTest extends JMockBaseTestCaseWithUser {
 
@@ -60,6 +66,8 @@ public class AttestationManagerTest extends JMockBaseTestCaseWithUser {
     private Server server;
     private AttestationManager mgr;
     private static TaskomaticApi taskomaticApi;
+    private final SystemManager systemManager = new SystemManager(ServerFactory.SINGLETON,
+            ServerGroupFactory.SINGLETON, null);
 
     @Override
     @BeforeEach
@@ -127,7 +135,84 @@ public class AttestationManagerTest extends JMockBaseTestCaseWithUser {
                 attestationData.getOrDefault("environment_type", "environment_type not found"));
     }
 
-     private TaskomaticApi getTaskomaticApi() throws TaskomaticApiException {
+    @Test
+    public void testListReports() {
+        mgr.createConfig(user, server, CoCoEnvironmentType.KVM_AMD_EPYC_GENOA, true);
+
+        ServerCoCoAttestationReport report = mgr.initializeReport(user, server);
+        mgr.initializeResults(user, report);
+        fakeSuccessfullAttestation(report);
+
+        ServerCoCoAttestationReport report2 = mgr.initializeReport(user, server);
+        mgr.initializeResults(user, report2);
+        fakeSuccessfullAttestation(report2);
+        HibernateFactory.getSession().flush();
+        HibernateFactory.commitTransaction();
+        HibernateFactory.getSession().clear();
+
+        try {
+            List<ServerCoCoAttestationReport> reports = mgr.listCoCoAttestationReports(user, server, new Date(0), 0,
+                    Integer.MAX_VALUE);
+            assertEquals(2, reports.size());
+
+            ServerCoCoAttestationReport latestReport = mgr.lookupLatestCoCoAttestationReport(user, server);
+            assertEquals(CoCoAttestationStatus.SUCCEEDED, latestReport.getStatus());
+            assertEquals("Some details", latestReport.getResults().get(0).getDetailsOpt().orElse(""));
+        }
+        finally {
+            // cleanup
+            systemManager.deleteServer(user, server.getId());
+            HibernateFactory.commitTransaction();
+        }
+    }
+
+    @Test
+    public void testFilterListReports() throws InterruptedException {
+        mgr.createConfig(user, server, CoCoEnvironmentType.KVM_AMD_EPYC_GENOA, true);
+
+        long epochStart = (new Date().getTime() / 1000);
+        for (int i = 10; i > 0; i--) {
+            ServerCoCoAttestationReport report = mgr.initializeReport(user, server);
+            mgr.initializeResults(user, report);
+            fakeSuccessfullAttestation(report);
+            HibernateFactory.getSession().flush();
+            HibernateFactory.commitTransaction();
+            TimeUnit.SECONDS.sleep(2);
+        }
+        HibernateFactory.getSession().clear();
+        try {
+            List<ServerCoCoAttestationReport> reports = mgr.listCoCoAttestationReports(user, server, new Date(0), 0,
+                    Integer.MAX_VALUE);
+            assertEquals(10, reports.size());
+
+            List<ServerCoCoAttestationReport> reports2 = mgr.listCoCoAttestationReports(user, server,
+                    new Date((epochStart + 10) * 1000L), 0, Integer.MAX_VALUE);
+            assertTrue(reports2.get(0).getModified().compareTo(new Date((epochStart + 10) * 1000L)) >= 0);
+            assertEquals(5, reports2.size());
+
+            reports2 = mgr.listCoCoAttestationReports(user, server, new Date(0), 5, 2);
+            assertEquals(2, reports2.size());
+            assertEquals(reports.get(6), reports2.get(0));
+            assertEquals(reports.get(7), reports2.get(1));
+            assertTrue(reports2.get(0).getCreated().after(reports2.get(1).getCreated()),
+                    "Report 0 is not created after Report 1");
+        }
+        finally {
+            // cleanup
+            systemManager.deleteServer(user, server.getId());
+            HibernateFactory.commitTransaction();
+        }
+    }
+
+    private void fakeSuccessfullAttestation(ServerCoCoAttestationReport reportIn) {
+        reportIn.getResults().forEach(res -> {
+            res.setStatus(CoCoAttestationStatus.SUCCEEDED);
+            res.setDetails("Some details");
+            res.setAttested(new Date());
+        });
+    }
+
+    private TaskomaticApi getTaskomaticApi() throws TaskomaticApiException {
         if (taskomaticApi == null) {
             taskomaticApi = context.mock(TaskomaticApi.class);
             context.checking(new Expectations() {
