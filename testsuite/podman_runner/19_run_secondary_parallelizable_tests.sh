d65d0e25322de37c21fb42f28de35db575b42263
Wiktor Maj <wiktor.maj@suse.com>
2024-04-15 14:26:26 +0200

    ls -la

--- testsuite/podman_runner/19_run_secondary_parallelizable_tests.sh
+++ testsuite/podman_runner/19_run_secondary_parallelizable_tests.sh
@@ -2,3 +2,5 @@
 set -xe
 sudo -i podman exec controller bash -c "zypper ref && zypper -n install expect"
 sudo -i podman exec controller bash -c "export CUCUMBER_PUBLISH_TOKEN=${CUCUMBER_PUBLISH_TOKEN} && export PROVIDER=podman && export SERVER=server && export HOSTNAME=controller && export SSH_MINION=opensusessh && export MINION=sle_minion && export RHLIKE_MINION=rhlike_minion && export TAGS=\"\\\"not @flaky\\\"\" && cd /testsuite && rake cucumber:secondary_parallelizable"
+
+ls -la /tmp
