6beae4f54140059842e66c3215043b567254e424
Michael Calmer <mc@suse.de>
2024-04-17 13:19:27 +0200

    add tables and report views for confidential compute attestation

---
+++ schema/reportdb/common/tables/CoCoAttestation.sql
@@ -0,0 +1,28 @@
+--
+-- Copyright (c) 2024 SUSE LLC
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+
+CREATE TABLE CoCoAttestation
+(
+    mgm_id              NUMERIC NOT NULL,
+    report_id           NUMERIC NOT NULL,
+    system_id           NUMERIC NOT NULL,
+    action_id           NUMERIC NOT NULL,
+    environment_type    VARCHAR(120),
+    status              VARCHAR(32),
+    create_time         TIMESTAMPTZ,
+    pass                NUMERIC,
+    fail                NUMERIC,
+    synced_date         TIMESTAMPTZ DEFAULT (current_timestamp)
+);
+
+ALTER TABLE CoCoAttestation
+  ADD CONSTRAINT CoCoAttestation_pk PRIMARY KEY (mgm_id, report_id);
+
