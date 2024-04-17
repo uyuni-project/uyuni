1600ade72d949612ea5815ad76d04ad4fc881970
Thomas Florio <thomas.florio@suse.com>
2024-04-17 16:54:28 +0200

    Added global attestation listing under Audit

--- web/html/src/manager/audit/index.ts
+++ web/html/src/manager/audit/index.ts
@@ -1,4 +1,5 @@
 export default {
+  "audit/coco": () => import("./coco/coco-global-scans-list.renderer"),
   "audit/cveaudit": () => import("./cveaudit/cveaudit"),
   "audit/subscription-matching": () => import("./subscription-matching/subscription-matching"),
 };
