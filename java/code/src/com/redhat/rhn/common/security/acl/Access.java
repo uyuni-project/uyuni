b0b91fa7b6f41be6afb0465c869c743a63f479da
Thomas Florio <thomas.florio@suse.com>
2024-04-23 00:14:19 +0200

    Hide the list when server does not support co. co.

--- java/code/src/com/redhat/rhn/common/security/acl/Access.java
+++ java/code/src/com/redhat/rhn/common/security/acl/Access.java
@@ -625,6 +625,25 @@ public class Access extends BaseHandler {
         return server.isPayg();
     }
 
+
+    /**
+     * Checks if a system supports confidential computing and has a configuration
+     * @param ctx
+     * @param params
+     * @return true if the system supports confidential computing
+     */
+    public boolean aclSystemHasCoCoConfig(Map<String, Object> ctx, String[] params) {
+        Long sid = getAsLong(ctx.get("sid"));
+        User user = (User) ctx.get("user");
+
+        Server server = SystemManager.lookupByIdAndUser(sid, user);
+        if (server == null) {
+            return false;
+        }
+
+        return server.doesOsSupportCoCoAttestation();
+    }
+
     /**
      * Checks if a system has any AppStream channels assigned
      * @param ctx the acl context
