8a826891676977b0a3d42e904861fe21580d8a0b
Thomas Florio <thomas.florio@suse.com>
2024-04-23 00:11:50 +0200

    Improve attestation report list

--- web/html/src/components/coco-attestation/Utils.tsx
+++ web/html/src/components/coco-attestation/Utils.tsx
@@ -29,6 +29,9 @@ export type AttestationReport = {
   id: number;
   systemId: number;
   systemName: string;
+  environmentType: string;
+  environmentTypeLabel: string;
+  environmentTypeDescription: string;
   status: string;
   statusDescription: string;
   creationTime: Date;
