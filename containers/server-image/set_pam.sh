75b6266338c679c0bf2dfd1d8c43df8d96d9bcfc
mbussolotto <michele.bussolotto@suse.com>
2024-04-23 13:17:51 +0200

    fixup! fixup! set pam authentication using script and add dependencies in Build Service

--- containers/server-image/set_pam.sh
+++ containers/server-image/set_pam.sh
@@ -3,10 +3,13 @@
 pam-config -a --sss
 pam-config -a --ldap
 
-if command -v authselect ; then
+if command -v authselect ; then #requires nss-pam-ldapd and authselect
     authselect select sssd --force
     authselect apply-changes
-else
+elif command -v authconfig; then  #requires pam_ldap and authconfig
     authconfig --enablesssdauth --update
+else
+    echo "cannot use authselect or authconfig"
+    exit 1
 fi
 
