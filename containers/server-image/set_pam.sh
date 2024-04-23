aba5039b12ebd308a235a03562214896a90353da
mbussolotto <michele.bussolotto@suse.com>
2024-04-23 13:14:50 +0200

    fixup! set pam authentication using script and add dependencies in Build Service

--- containers/server-image/set_pam.sh
+++ containers/server-image/set_pam.sh
@@ -2,5 +2,11 @@
 
 pam-config -a --sss
 pam-config -a --ldap
-authselect select sssd --force
-authselect apply-changes
+
+if command -v authselect ; then
+    authselect select sssd --force
+    authselect apply-changes
+else
+    authconfig --enablesssdauth --update
+fi
+
