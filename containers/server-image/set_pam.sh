2ede50d02caf8199576f2435d03c38c403c43822
mbussolotto <michele.bussolotto@suse.com>
2024-04-23 18:14:34 +0200

    fixup! Update set_pam.sh

--- containers/server-image/set_pam.sh
+++ containers/server-image/set_pam.sh
@@ -2,14 +2,3 @@
 
 pam-config -a --sss
 pam-config -a --ldap
-
-if command -v authselect ; then #requires nss-pam-ldapd and authselect
-    authselect select sssd --force
-    authselect apply-changes
-elif command -v authconfig; then  #requires pam_ldap and authconfig
-    authconfig --enablesssdauth --update
-else
-    echo "cannot use authselect or authconfig"
-    exit 1
-fi
-
