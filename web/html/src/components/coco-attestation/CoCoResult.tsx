96c2c960a33a5b6e854465aeeec136fcee80a679
Thomas Florio <thomas.florio@suse.com>
2024-04-25 00:30:30 +0200

    Replaced table with responsive grid-based list

--- web/html/src/components/coco-attestation/CoCoResult.tsx
+++ web/html/src/components/coco-attestation/CoCoResult.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 
-import { BootstrapPanel } from "components/panels/BootstrapPanel";
-
 import { AttestationResult, renderTime } from "./Utils";
 
 type Props = {
@@ -13,52 +11,68 @@ class CoCoResult extends React.Component<Props> {
   render = () => {
     const result = this.props.result;
     return (
-      <BootstrapPanel key={result.id}>
-        <div className="table-responsive">
-          <table className="table">
-            <tbody>
-              <tr>
-                <td>{t("Result Type:")}</td>
-                <td>{result.resultTypeLabel}</td>
-              </tr>
-              <tr>
-                <td>{t("Description:")}</td>
-                <td>{result.description}</td>
-              </tr>
-              <tr>
-                <td>{t("Status:")}</td>
-                <td>{result.statusDescription}</td>
-              </tr>
-              {result.status === "SUCCEEDED" && (
-                <tr>
-                  <td>{t("Attested on")}:</td>
-                  <td>{renderTime(result.attestationTime)}</td>
-                </tr>
-              )}
-              {result.details && (
-                <tr>
-                  <td>{t("Details")}:</td>
-                  <td>
-                    <div className="row">
-                      <pre>{result.details}</pre>
-                    </div>
-                  </td>
-                </tr>
-              )}
-              {result.processOutput && (
-                <tr>
-                  <td>{t("Process Output")}:</td>
-                  <td>
-                    <div className="row">
-                      <pre>{result.processOutput}</pre>
-                    </div>
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </div>
-      </BootstrapPanel>
+      <div className="panel panel-default" key={result.id}>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <div className="row">
+              <div className="col-md-2">
+                <strong>{t("Result Type:")}</strong>
+              </div>
+              <div className="col-md-10">{result.resultTypeLabel}</div>
+            </div>
+          </li>
+          <li className="list-group-item">
+            <div className="row">
+              <div className="col-md-2">
+                <strong>{t("Description:")}</strong>
+              </div>
+              <div className="col-md-10">{result.description}</div>
+            </div>
+          </li>
+          <li className="list-group-item">
+            <div className="row">
+              <div className="col-md-2">
+                <strong>{t("Status:")}</strong>
+              </div>
+              <div className="col-md-10">{result.statusDescription}</div>
+            </div>
+          </li>
+          {result.status === "SUCCEEDED" && (
+            <li className="list-group-item">
+              <div className="row">
+                <div className="col-md-2">
+                  <strong>{t("Attested on:")}</strong>
+                </div>
+                <div className="col-md-10">{renderTime(result.attestationTime)}</div>
+              </div>
+            </li>
+          )}
+          {result.details && (
+            <li className="list-group-item">
+              <div className="row">
+                <div className="col-md-2">
+                  <strong>{t("Details")}:</strong>
+                </div>
+                <div className="col-md-10">
+                  <pre>{result.details}</pre>
+                </div>
+              </div>
+            </li>
+          )}
+          {result.processOutput && (
+            <li className="list-group-item">
+              <div className="row">
+                <div className="col-md-2">
+                  <strong>{t("Process Output")}:</strong>
+                </div>
+                <div className="col-md-10">
+                  <pre>{result.processOutput}</pre>
+                </div>
+              </div>
+            </li>
+          )}
+        </ul>
+      </div>
     );
   };
 }
