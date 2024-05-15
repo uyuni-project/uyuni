1c110f8343a0fc6f2d200139f80c8be05a366433
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2024-05-15 16:07:07 +0200

    Update 02_setup_network.sh

--- testsuite/podman_runner/02_setup_network.sh
+++ testsuite/podman_runner/02_setup_network.sh
@@ -25,6 +25,6 @@ echo "DEBUG"
 sudo -i find /etc/cni/
 echo "END DEBUG"
 # workaround for https://bugs.launchpad.net/ubuntu/+source/libpod/+bug/2024394
-sudo sed -ie "s/\"cniVersion\": \"1.0.0\"/\"cniVersion\": \"0.4.0\"/g" /etc/cni/net.d/uyuni-network-1.conflist
+# sudo sed -ie "s/\"cniVersion\": \"1.0.0\"/\"cniVersion\": \"0.4.0\"/g" /etc/cni/net.d/uyuni-network-1.conflist
 
 
