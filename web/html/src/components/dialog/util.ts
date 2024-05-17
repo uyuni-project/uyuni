62ebf28c80107bd5f62eae2139449203e1c766cd
Etheryte <karl.tarvas@suse.com>
2024-05-17 13:01:59 +0300

    Emulate Bootstrap 3 modals in Bootstrap 5

--- web/html/src/components/dialog/util.ts
+++ web/html/src/components/dialog/util.ts
@@ -1,7 +1,18 @@
 export function showDialog(dialogId: string) {
-  jQuery("#" + dialogId).modal("show");
+  jQuery("#" + dialogId)
+    .modal("show")
+    // Here and below, these manual handlers offer compatibility between Bootstrap 3 and 5, we can drop these after the migration is complete
+    .addClass("show");
+
+  jQuery(".modal-backdrop.in").addClass("show");
 }
 
 export function hideDialog(dialogId: string) {
-  jQuery("#" + dialogId).modal("hide");
+  jQuery("#" + dialogId)
+    .modal("hide")
+    .removeClass("show");
+
+  if (jQuery(".modal.in").length === 0) {
+    jQuery(".modal-backdrop.show").removeClass("show");
+  }
 }
