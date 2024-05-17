44bb1cc2f57e53565696aa5330616f97f78d5295
Marek Czernek <marek.czernek@suse.com>
2024-05-17 10:07:55 +0200

    Enable linting in the containers directory

--- containers/proxy-tftpd-image/uyuni-configure.py
+++ containers/proxy-tftpd-image/uyuni-configure.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# pylint: disable=invalid-name
+"""Configure script for proxy TFTP"""
 
 import logging
 import os
@@ -8,31 +10,33 @@ import sys
 config_path = "/etc/uyuni/"
 
 # read from file
-with open(config_path + "config.yaml") as source:
+with open(config_path + "config.yaml", encoding="utf-8") as source:
     config = yaml.safe_load(source)
-   
+
     # log_level is the value for rhn.conf and should be a positive integer
     log_level = logging.WARNING if config.get("log_level") == 1 else logging.DEBUG
     logging.getLogger().setLevel(log_level)
 
     # store SSL CA certificate
     os.mkdir("/usr/share/uyuni/")
-    with open("/usr/share/uyuni/ca.crt", "w") as file:
+    with open("/usr/share/uyuni/ca.crt", "w", encoding="utf-8") as file:
         file.write(config.get("ca_crt"))
 
     tftp_config = "/etc/sysconfig/tftp"
     tftp_root = "/srv/tftpboot"
-    with open(tftp_config, "w") as file:
-        file.write(f'''# Automatically generated Uyuni Proxy Server configuration file.
+    with open(tftp_config, "w", encoding="utf-8") as file:
+        file.write(
+            f'''# Automatically generated Uyuni Proxy Server configuration file.
 TFTP_USER="tftp"
 TFTP_OPTIONS="{config.get('tftp_options', '')} "
-TFTP_DIRECTORY="{tftp_root}"''')
+TFTP_DIRECTORY="{tftp_root}"'''
+        )
 
     os.system(f"chmod 640 {tftp_config}")
 
-# Make sure we can read 
+# Make sure we can read
 if not os.access(tftp_root, os.R_OK | os.X_OK):
-   logging.critical("TFTP root directory does not have correct permissions.")
-   sys.exit(1)
+    logging.critical("TFTP root directory does not have correct permissions.")
+    sys.exit(1)
 
 sys.exit(0)
