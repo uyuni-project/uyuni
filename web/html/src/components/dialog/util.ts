d6815e229deacacf8dc2c11c21cb9431368d7590
Etheryte <karl.tarvas@suse.com>
2024-05-21 13:11:18 +0300

    Fix modal backwards compatibility event handling

--- web/html/src/components/dialog/util.ts
+++ web/html/src/components/dialog/util.ts
@@ -1,18 +1,19 @@
 export function showDialog(dialogId: string) {
-  jQuery("#" + dialogId)
+  const dialog = jQuery("#" + dialogId)
     .modal("show")
     // Here and below, these manual handlers offer compatibility between Bootstrap 3 and 5, we can drop these after the migration is complete
-    .addClass("show");
+    .addClass("show")
+    .on("hide.bs.modal", () => {
+      dialog.removeClass("show");
+
+      if (jQuery(".modal.in").length === 0) {
+        jQuery(".modal-backdrop.show").removeClass("show");
+      }
+    });
 
   jQuery(".modal-backdrop.in").addClass("show");
 }
 
 export function hideDialog(dialogId: string) {
-  jQuery("#" + dialogId)
-    .modal("hide")
-    .removeClass("show");
-
-  if (jQuery(".modal.in").length === 0) {
-    jQuery(".modal-backdrop.show").removeClass("show");
-  }
+  jQuery("#" + dialogId).modal("hide");
 }
