e4f2817bdab1b0826a51fa5666a259bc31a1b573
Etheryte <karl.tarvas@suse.com>
2024-05-21 14:03:38 +0300

    Improve event handling, fix layout offset

--- web/html/src/components/dialog/util.ts
+++ web/html/src/components/dialog/util.ts
@@ -1,10 +1,14 @@
 export function showDialog(dialogId: string) {
+  // The base event is "hide.bs.modal", we only want to remove the listener we added so we add a namespace, see https://api.jquery.com/event.namespace/
+  const namespacedEventName = "hide.bs.modal.namespace-dialog-util";
+
   const dialog = jQuery("#" + dialogId)
     .modal("show")
     // Here and below, these manual handlers offer compatibility between Bootstrap 3 and 5, we can drop these after the migration is complete
     .addClass("show")
-    .on("hide.bs.modal", () => {
-      dialog.removeClass("show");
+    .off(namespacedEventName)
+    .on(namespacedEventName, () => {
+      dialog.removeClass("show").off(namespacedEventName);
 
       if (jQuery(".modal.in").length === 0) {
         jQuery(".modal-backdrop.show").removeClass("show");
