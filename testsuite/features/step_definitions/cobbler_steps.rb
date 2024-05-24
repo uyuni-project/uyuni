ad14869abfa0fb3103036d83d6a5b455287f7852
Dominik Gedon <dominik.gedon@suse.com>
2024-05-24 16:09:19 +0200

    QE: Refactor Cobbler distro and profile scenarios
    - Do not hide error in `^I trigger cobbler system record on the "([^"]*)"$/`
    anymore.
    - Merge scenarios that are logically linked together
    
    Signed-off-by: Dominik Gedon <dominik.gedon@suse.com>

--- testsuite/features/step_definitions/cobbler_steps.rb
+++ testsuite/features/step_definitions/cobbler_steps.rb
@@ -72,7 +72,7 @@ When(/^I trigger cobbler system record on the "([^"]*)"$/) do |host|
       And I am on the Systems overview page of this "#{host}"
       And I follow "Provisioning"
       And I click on "Create PXE installation configuration"
-      And I wait until file "/srv/tftpboot/pxelinux.cfg/01-*" contains "inst.ks=" on server
+      Then I should see a "System record created" text
     )
   end
 end
