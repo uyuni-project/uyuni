f903a50c95f2a359dd831e62efb2481935291aee
waterflow80 <thessalonikaathena@outlook.com>
2024-05-27 10:35:11 +0100

    update imports

--- python/lzreposync/src/lzreposync/parse.py
+++ python/lzreposync/src/lzreposync/parse.py
@@ -4,57 +4,21 @@ It downloads the target repository's metadata file(s) from the
 given url and parses it(them)
 """
 
-# TODO Check for the signature
-# TODO Use SAX parser
-# TODO Define the best chunk size (consider calculating the current os's free memory and the file size and extract a formula)
-# TODO Evaluate the two versions of the download/ benchmark them/ write some tests about them
-# TODO After finishing, complete for all other files, normalize the sql code
-# TODO Should we parse the files at the time of download, or in a second step ?
-
-import argparse
+import logging
+import pickle
+import tempfile
+import time
+import urllib.request
+import urllib.error
 import xml.sax
+from xml.sax.xmlreader import InputSource
 
-import requests
 import os
-import shutil
-from urllib.parse import urljoin
-import zipfile
-import tarfile
 import gzip
 
 from memory_profiler import profile
 
-from lzreposync.metadata_parser import PrimaryXmlHandler
-
-CACHE_DIR = "./cache/metadata/"  # TODO change it into /var/cache/...
-PRIMARY_XML = "00c99a7e67dac325f1cbeeedddea3e4001a8f803c9bf43d9f50b5f1c756b0887-primary.xml.gz"  # TODO can the name of this file change ?
-
-EMPTY_URL_ERROR = "ERROR: URL should not be empty."
-FILENAME_ERROR = "ERROR: Filename should not be empty."
-UNKNOWN_FORMAT = "ERROR: Unknown file format. Can't extract."
-
-# ARCHIVE EXTENSIONS
-ZIP_EXTENSION = ".zip"
-TAR_EXTENSION = ".tar"
-TAR_GZ_EXTENSION = ".tar.gz"
-TGZ_EXTENSION = ".tgz"
-GZ_EXTENSION = ".gz"
-
-
-def download_primary_xml_v1(url: str, primary_xml: str):
-    """Download the primary-xml file from the given repository url"""
-    try:
-        primary_xml_url = urljoin(url, primary_xml)
-        with requests.get(primary_xml_url, stream=True) as response:
-            response.raise_for_status()  # check for status code
-            with open(os.path.join(CACHE_DIR, primary_xml), "wb") as f:
-                for chunk in response.iter_content(chunk_size=8192):
-                    # chunk_size to be efficiently determined
-                    f.write(chunk)
-                print(f"file {primary_xml} was downloaded successfully!")
-    except requests.exceptions.RequestException as e:
-        print(f"Error downloading {primary_xml} file:", e)
-
+from lzreposync.primary_handler import Handler
 
 @profile
 def download_primary_xml_v2(url: str, primary_xml: str):
