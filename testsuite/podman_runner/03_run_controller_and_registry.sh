01c5240db8290f085afe306a5bd9647e1154bc42
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2024-05-28 17:10:07 +0200

    CI: add auth registry
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.com>

---
+++ testsuite/podman_runner/03_run_controller_and_registry.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+src_dir=$(cd $(dirname "$0")/../.. && pwd -P)
+
+AUTH_REGISTRY_USER=$(echo "$AUTH_REGISTRY_CREDENTIALS"| cut -d\| -f1)
+AUTH_REGISTRY_PASSWD=$(echo "$AUTH_REGISTRY_CREDENTIALS" | cut -d\| -f2)
+sudo -i podman run --rm -d --network network -v /tmp/testing:/tmp --name controller -h controller -v ${src_dir}/testsuite:/testsuite ghcr.io/$UYUNI_PROJECT/uyuni/ci-test-controller-dev:$UYUNI_VERSION
+sudo -i podman run --rm -d --network network --name $AUTH_REGISTRY -h $AUTH_REGISTRY -e AUTH_REGISTRY=${AUTH_REGISTRY} -e AUTH_REGISTRY_USER=${AUTH_REGISTRY_USER} -e AUTH_REGISTRY_PASSWD={$AUTH_REGISTRY_USER} ghcr.io/$UYUNI_PROJECT/uyuni/ci-container-registry-auth:$UYUNI_VERSION
+
+sudo podman ps
