c0e7c4c91ac9dd33f1aa24539bc6729dc928c729
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2024-06-25 11:23:32 +0200

    CI: buildhost as minion
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.com>

--- testsuite/podman_runner/03_run_controller_and_registry_and_buildhost.sh
+++ testsuite/podman_runner/03_run_controller_and_registry_and_buildhost.sh
@@ -2,10 +2,15 @@
 set -ex
 src_dir=$(cd $(dirname "$0")/../.. && pwd -P)
 
+echo buildhostproductuuid > /tmp/buildhost_product_uuid
+
 AUTH_REGISTRY_USER=$(echo "$AUTH_REGISTRY_CREDENTIALS"| cut -d\| -f1)
 AUTH_REGISTRY_PASSWD=$(echo "$AUTH_REGISTRY_CREDENTIALS" | cut -d\| -f2)
 sudo -i podman run --rm -d --network network -v /tmp/testing:/tmp --name controller -h controller -v ${src_dir}/testsuite:/testsuite ghcr.io/$UYUNI_PROJECT/uyuni/ci-test-controller-dev:$UYUNI_VERSION
 sudo -i podman run --rm -d --network network --name $AUTH_REGISTRY -h $AUTH_REGISTRY -e AUTH_REGISTRY=${AUTH_REGISTRY} -e AUTH_REGISTRY_USER=${AUTH_REGISTRY_USER} -e AUTH_REGISTRY_PASSWD={$AUTH_REGISTRY_USER} ghcr.io/$UYUNI_PROJECT/uyuni/ci-container-registry-auth:$UYUNI_VERSION
-sudo -i podman run --rm -d --network network --name buildhost -h buildhost --privileged -ti --rm ghcr.io/$UYUNI_PROJECT/uyuni/ci-buildhost:$UYUNI_VERSION
+sudo -i podman run --privileged --rm -d --network network -v /tmp/buildhost_product_uuid:/sys/class/dmi/id/product_uuid -v /tmp/testing:/tmp -v ${src_dir}/testsuite/podman_runner/salt-minion-entry-point.sh:/salt-minion-entry-point.sh --volume /run/dbus/system_bus_socket:/run/dbus/system_bus_socket:ro --name buildhost -h buildhost ghcr.io/$UYUNI_PROJECT/uyuni/ci-buildhost:$UYUNI_VERSION bash -c "/salt-minion-entry-point.sh server 1-SUSE-KEY-x86_64"
+sudo -i podman exec buildhost bash -c "ssh-keygen -A && /usr/sbin/sshd -e"
+sudo -i podman exec buildhost bash -c "if [ ! -d /root/.ssh ];then mkdir /root/.ssh/;chmod 700 /root/.ssh;fi;cp /tmp/authorized_keys /root/.ssh/"
+sudo -i podman exec -d buildhost dockerd
 
 sudo podman ps
