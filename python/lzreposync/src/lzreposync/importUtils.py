060c0d64bef67d54107e10a96b06ec8c3ee6b7ad
waterflow80 <thessalonikaathena@outlook.com>
2024-07-01 17:47:21 +0100

    removed unused exception

--- python/lzreposync/src/lzreposync/importUtils.py
+++ python/lzreposync/src/lzreposync/importUtils.py
@@ -1,7 +1,6 @@
 import logging
 import sys
 
-from lzreposync.exceptions import FormatError
 from spacewalk.satellite_tools.syncLib import log2, log
 from spacewalk.server import rhnSQL
 from spacewalk.server.importlib import importLib, mpmSource, packageImport
@@ -95,9 +94,6 @@ def import_package_batch(to_process, batch_index=-1, batch_count=-1):
                 #  The problem with this code, is that if one package from a batch fails, all the batch (the rest of it)
                 #  also fails
                 continue
-            except FormatError as e:
-                e_message = f"Exception: {e}"
-                raise e
             except Exception as e:
                 failed_packages += 1
                 e_message = f"Exception: {e}"
