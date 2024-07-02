c3d5629a8e90ed13510f919fea6ec73c04c89fc9
Michael Calmer <michael.calmer@suse.com>
2024-07-02 13:54:38 +0200

    include proxy products for aarch64 and beta products

--- java/code/src/com/redhat/rhn/domain/cloudpayg/PaygProductFactory.java
+++ java/code/src/com/redhat/rhn/domain/cloudpayg/PaygProductFactory.java
@@ -185,7 +185,7 @@ public class PaygProductFactory extends HibernateFactory {
         ChannelFamily family = product.getChannelFamily();
         String version = product.getVersion();
 
-        return ChannelFamilyFactory.PROXY_CHANNEL_FAMILY_LABEL.equals(family.getLabel()) &&
+        return family.getLabel().startsWith(ChannelFamilyFactory.PROXY_CHANNEL_FAMILY_LABEL) &&
             RPM_VERSION_COMPARATOR.compare(version, "4.3") >= 0;
     }
 
