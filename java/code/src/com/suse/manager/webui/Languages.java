7334f87f42705d044a16fa4ac2b79b413a5a68ad
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2024-07-02 12:21:10 +0200

    TEST
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.com>

--- java/code/src/com/suse/manager/webui/Languages.java
+++ java/code/src/com/suse/manager/webui/Languages.java
@@ -12,6 +12,9 @@
  * granted to use or replicate Red Hat trademarks that are incorporated
  * in this software or its documentation.
  */
+
+// TEST
+
 package com.suse.manager.webui;
 
 import java.text.MessageFormat;
