b15410b56016890de9e17de468545a09c3273f0d
Paolo Invernizzi <paolo.invernizzi@suse.com>
2024-07-05 19:12:29 +0200

    QE: correctly reset the API client

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -1642,7 +1642,7 @@ When(/^I run spacewalk-hostname-rename command on the server$/) do
 
   # Reset the API client to take the new CA into account
   log 'Resetting the API client'
-  new_api_client
+  $api_test = new_api_client
 
   raise SystemCallError, 'Error while running spacewalk-hostname-rename command - see logs above' unless result_code.zero?
   raise ScriptError, 'Error in the output logs - see logs above' if out_spacewalk.include? 'No such file or directory'
