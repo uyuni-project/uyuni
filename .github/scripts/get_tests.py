a2b2a8f90ae6c7d89598f1d8ae28a6d6a5d38a4f
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2024-07-08 17:36:45 +0200

    CI: Acceptance tests at PR: Merge with suggested tests
    Create a "Fast workflow" that runs only the suggested tests to give fast
    feedback to the user.
    
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.com>

---
+++ .github/scripts/get_tests.py
@@ -0,0 +1,25 @@
+import os.path
+import re
+import sys
+import yaml
+
+if len(sys.argv) != 2:
+    print("Expecting the xml file as argument")
+    exit(-1)
+if not os.path.isfile(sys.argv[1]):
+    print("Expecting the xml file as argument")
+    exit(-1)
+
+file_path = sys.argv[1]
+
+with open(file_path, 'r') as file:
+    result = file.read()
+pattern = re.compile(r'<li>[a-z|_]*</li>')
+match  = pattern.findall(result)
+tests = []
+for i in match:
+    i = i.replace("<li>","")
+    i = i.replace("</li>","")
+    tests.append(i)
+
+print(yaml.dump(tests))
