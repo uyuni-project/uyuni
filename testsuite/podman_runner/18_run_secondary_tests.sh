502d6b4fb89819e75eb17cf8460967c7275b0d71
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2024-07-10 10:37:07 +0200

    TTT
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.com>

--- testsuite/podman_runner/18_run_secondary_tests.sh
+++ testsuite/podman_runner/18_run_secondary_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
-TESTS=$1
+TESTS="$@"
 sudo -i podman exec controller bash -c "export CUCUMBER_PUBLISH_TOKEN=${CUCUMBER_PUBLISH_TOKEN} && export PROVIDER=podman && export SERVER=server && export HOSTNAME=controller && export SSH_MINION=opensusessh && export MINION=sle_minion && export RHLIKE_MINION=rhlike_minion && export TAGS=\"\\\"not @flaky\\\"\" && cd /testsuite && rake cucumber:secondary ${TESTS}"
