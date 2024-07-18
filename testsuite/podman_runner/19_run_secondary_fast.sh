bebd5d5d072c0e97c2eb2eaa91a71c829531ea17
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2024-07-18 13:07:04 +0200

    TEST
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.com>

--- testsuite/podman_runner/19_run_secondary_fast.sh
+++ testsuite/podman_runner/19_run_secondary_fast.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 
-sudo -i podman exec controller bash -c "export CUCUMBER_PUBLISH_TOKEN=${CUCUMBER_PUBLISH_TOKEN} && export PROVIDER=podman && export SERVER=server && export HOSTNAME=controller && export SSH_MINION=opensusessh && export MINION=sle_minion && export RHLIKE_MINION=rhlike_minion && cd /testsuite && export TAGS=\"\\\"not @flaky\\\"\" && rake cucumber:secondary_f"
+sudo -i podman exec controller bash -c "export CUCUMBER_PUBLISH_TOKEN=${CUCUMBER_PUBLISH_TOKEN} && export PROVIDER=podman && export SERVER=server && export HOSTNAME=controller && export SSH_MINION=opensusessh && export MINION=sle_minion && export RHLIKE_MINION=rhlike_minion && cd /testsuite && export TAGS=\"\\\"not @flaky\\\"\" && rake cucumber:secondary_fast"
