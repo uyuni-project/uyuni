7ba1367291534d7e8f5dcc41ac315b2ba554317f
maximenoel8 <55169628+maximenoel8@users.noreply.github.com>
2024-07-26 14:10:55 +1200

    43 backport missing references rocky8 (#24929)

--- testsuite/features/step_definitions/api_common.rb
+++ testsuite/features/step_definitions/api_common.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2015-2023 SUSE LLC
+# Copyright (c) 2015-2024 SUSE LLC
 # Licensed under the terms of the MIT license.
 
 ### This file contains the definitions for all steps concerning the API.
