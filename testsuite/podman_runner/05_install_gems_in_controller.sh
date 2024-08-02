b19b9216435fef656d329280c0200d0ad8e9f649
Dominik Gedon <dominik.gedon@suse.com>
2024-08-02 11:27:51 +0200

    QE: Update Ruby to version 3.3
    Fixes https://github.com/SUSE/spacewalk/issues/17431
    
    Signed-off-by: Dominik Gedon <dominik.gedon@suse.com>

--- testsuite/podman_runner/05_install_gems_in_controller.sh
+++ testsuite/podman_runner/05_install_gems_in_controller.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -xe
-sudo -i podman exec controller bash -c "cd /testsuite && bundle.ruby2.5 install --gemfile Gemfile"
+sudo -i podman exec controller bash -c "cd /testsuite && bundle.ruby3.3 install --gemfile Gemfile --verbose"
