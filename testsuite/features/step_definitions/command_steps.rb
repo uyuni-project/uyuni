9deefc7512eba908586f3b9b654fdf5954dc49c0
Dominik Gedon <dominik.gedon@suse.com>
2024-08-19 09:32:15 +0200

    QE: Add debug output to bootstrap creation step
    This makes ours lives easier in the BV and we directly see, if we are
    missing entries in `constants.rb`.
    Signed-off-by: Dominik Gedon <dominik.gedon@suse.com>

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -1054,6 +1054,10 @@ When(/^I create the bootstrap repository for "([^"]*)" on the server((?: without
   parent_channel = PARENT_CHANNEL_LABEL_TO_SYNC_BY_BASE_CHANNEL[product][base_channel]
   get_target('server').wait_while_process_running('mgr-create-bootstrap-repo')
 
+  $stdout.puts "base_channel: #{base_channel}"
+  $stdout.puts "channel: #{channel}"
+  $stdout.puts "parent_channel: #{parent_channel}"
+
   cmd =
     if parent_channel.nil?
       "mgr-create-bootstrap-repo --create #{channel} --with-custom-channels"
