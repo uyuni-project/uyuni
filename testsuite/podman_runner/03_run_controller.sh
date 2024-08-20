7af301d373686423c4d5e90db0d4413a6f980b19
Dominik Gedon <dominik.gedon@suse.com>
2024-08-20 15:30:04 +0200

    TMP: Add temporary new controller container
    Signed-off-by: Dominik Gedon <dominik.gedon@suse.com>

--- testsuite/podman_runner/03_run_controller.sh
+++ testsuite/podman_runner/03_run_controller.sh
@@ -2,6 +2,6 @@
 set -ex
 src_dir=$(cd $(dirname "$0")/../.. && pwd -P)
 
-sudo -i podman run --rm -d --network network -v /tmp/testing:/tmp --name controller -h controller -v ${src_dir}/testsuite:/testsuite ghcr.io/$UYUNI_PROJECT/uyuni/ci-test-controller-dev:$UYUNI_VERSION
+sudo -i podman run --rm -d --network network -v /tmp/testing:/tmp --name controller -h controller -v ${src_dir}/testsuite:/testsuite ghcr.io/$UYUNI_PROJECT/uyuni/ci-test-controller-dev:qe-develop
 
 sudo podman ps
