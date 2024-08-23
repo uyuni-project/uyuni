2086239e67989a801a381a5be74218101b4cb0f1
Dominik Gedon <dominik.gedon@suse.com>
2024-08-23 13:45:53 +0200

    Use Ruby 3.1 due to `net-ssh` dependency
    `net-ssh` does not support Ruby > 3.1, yet.
    
    Signed-off-by: Dominik Gedon <dominik.gedon@suse.com>

--- testsuite/podman_runner/05_install_gems_in_controller.sh
+++ testsuite/podman_runner/05_install_gems_in_controller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xe
-sudo -i podman exec controller bash -c 'export GEM_PATH="/usr/lib64/ruby/gems/3.3.0"'
-sudo -i podman exec controller bash -c "cd /testsuite && bundle.ruby3.3 install --gemfile Gemfile --verbose"
+sudo -i podman exec controller bash -c 'export GEM_PATH="/usr/lib64/ruby/gems/3.1.0"'
+sudo -i podman exec controller bash -c "cd /testsuite && bundle.ruby3.1 install --gemfile Gemfile --verbose"
 sudo -i podman exec controller bash -c "gem env"
 sudo -i podman exec controller bash -c "gem list"
