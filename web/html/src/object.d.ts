3e4e298ba9a63576c6affd0499edd2ae75113551
Etheryte <karl.tarvas@suse.com>
2024-08-27 22:40:31 +0300

    Add missing types

---
+++ web/html/src/object.d.ts
@@ -0,0 +1,24 @@
+// See https://stackoverflow.com/a/77724124/1470607
+interface ObjectConstructor {
+  /**
+   * Groups members of an iterable according to the return value of the passed callback.
+   * @param items An iterable.
+   * @param keySelector A callback which will be invoked for each item in items.
+   */
+  groupBy<K extends PropertyKey, T>(
+      items: Iterable<T>,
+      keySelector: (item: T, index: number) => K,
+  ): Partial<Record<K, T[]>>;
+}
+
+interface MapConstructor {
+  /**
+   * Groups members of an iterable according to the return value of the passed callback.
+   * @param items An iterable.
+   * @param keySelector A callback which will be invoked for each item in items.
+   */
+  groupBy<K, T>(
+      items: Iterable<T>,
+      keySelector: (item: T, index: number) => K,
+  ): Map<K, T[]>;
+}
