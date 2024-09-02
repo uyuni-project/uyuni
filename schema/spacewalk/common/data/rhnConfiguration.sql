12a3ac5d16a72ef2d21708309f045b6c5b1a60bb
Manuel Salvi <manuel.salvi@suse.com>
2024-09-02 14:04:49 +0200

    initial configurations flags on rhnconfiguration table

--- schema/spacewalk/common/data/rhnConfiguration.sql
+++ schema/spacewalk/common/data/rhnConfiguration.sql
@@ -3,3 +3,23 @@ INSERT INTO rhnConfiguration (key, description, default_value) VALUES ('extauth_
 INSERT INTO rhnConfiguration (key, description, default_value) VALUES ('extauth_keep_temproles', 'Keep temporary user roles granted due to the external authentication setup for subsequent logins using password.', 'false');
 INSERT INTO rhnConfiguration (key, description, value, default_value)
 VALUES ('system_checkin_threshold', 'Number of days before reporting a system as inactive', null, 1);
+INSERT INTO rhnConfiguration (key, description, value, default_value)
+VALUES ('password_check_lenght_min', 'Minimum number of characters in local user passwords', null, 12);
+INSERT INTO rhnConfiguration (key, description, value, default_value)
+VALUES ('password_check_lenght_max', 'Maximum number of characters in local user passwords', null, 32);
+INSERT INTO rhnConfiguration (key, description, value, default_value)
+VALUES ('password_check_lower_char_flag', 'Password has to have at least one lower alpha character', null, 1);
+INSERT INTO rhnConfiguration (key, description, value, default_value)
+VALUES ('password_check_upper_char_flag', 'Password has to have at least one upper alpha character', null, 1);
+INSERT INTO rhnConfiguration (key, description, value, default_value)
+VALUES ('password_check_digit_flag', 'Password has to have at least one digit', null, 1);
+INSERT INTO rhnConfiguration (key, description, value, default_value)
+VALUES ('password_check_consecutive_char_flag', 'Password has to have no consecutive characters', null, 0);
+INSERT INTO rhnConfiguration (key, description, value, default_value)
+VALUES ('password_check_special_char_flag', 'Password has to have at least a special character', null, 0);
+INSERT INTO rhnConfiguration (key, description, value, default_value)
+VALUES ('password_check_restricted_occurrence_flag', 'Password has to have no repeating characters', null, 0);
+INSERT INTO rhnConfiguration (key, description, value, default_value)
+VALUES ('password_check_max_occurrence_flag', 'Maximum number of valid occurrence of a character', null, 1);
+INSERT INTO rhnConfiguration (key, description, value, default_value)
+VALUES ('password_check_special_characters', 'List of special characters to check in a password', null, '!$%&()*+,./:;<=>?[\\]^_{|}~');
