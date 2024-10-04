9ac0bcc759b5d1f2ac0ad5b4bdcf4898330fa6ee
Yeray Gutiérrez Cedrés <yeray.gutierrez@suse.com>
2024-10-04 09:18:05 +0100

    Initial Uyuni Health Check tool commit

---
+++ health-check/src/uyuni_health_check/exporters/supportconfig/exporter.py
@@ -0,0 +1,78 @@
+import os
+from jinja2 import Environment, FileSystemLoader
+from uyuni_health_check.utils import HealthException, console
+from uyuni_health_check.containers.manager import (
+    image_exists,
+    build_image,
+    podman,
+    container_is_running,
+)
+
+
+def prepare_exporter(config=None, verbose=False, supportconfig_path=None):
+    """
+    Build the prometheus exporter image and deploy it on the server
+
+    :param server: the Uyuni server to deploy the exporter on
+    """
+    exporter_name = "supportconfig-exporter"
+    exporter_dir = config.load_dockerfile_dir("exporter")
+    create_supportconfig_exporter_cfg(
+        config=config, supportconfig_path=supportconfig_path
+    )
+    exporter_config = config.get_config_file_path("exporter")
+    # config = get_supportconfig_exporter_cfg(supportconfig_path=supportconfig_path)
+
+    console.log(f"[bold]Building {exporter_name} image")
+    if image_exists(f"{exporter_name}"):
+        console.log(f"[yellow]Skipped as the {exporter_name} image is already present")
+    else:
+        # sources_dir = config.get_sources_path()
+        build_image(f"{exporter_name}", exporter_dir, verbose=verbose)
+        # build_image(f"{exporter_name}", os.path.dirname(__file__), build_args=[f"config={config}"],verbose=verbose)
+        console.log(f"[green]The {exporter_name} image was built successfully")
+
+    # Run the container
+    console.log(f"[bold]Deploying {exporter_name} container")
+    if container_is_running(f"{exporter_name}"):
+        console.log(
+            f"[yellow]Skipped as the {exporter_name} container is already running"
+        )
+        return
+
+    # Prepare arguments for Podman call
+    podman_args = [
+        "run",
+        "--replace",
+        "-d",
+        "--network=health-check-network",
+        "-p",
+        "9000:9000",
+        "-v",
+        f"{supportconfig_path}:{supportconfig_path}",
+        "-v",
+        f"{exporter_config}:/opt/config.yml",
+        "-p",
+        "9000:9000",
+    ]
+
+    podman_args.extend(
+        [
+            "--name",
+            f"uyuni_health_check_{exporter_name}",
+            f"{exporter_name}",
+        ]
+    )
+
+    # Run the container
+    podman(
+        podman_args,
+        quiet=not verbose,
+    )
+
+
+def create_supportconfig_exporter_cfg(config=None, supportconfig_path=None):
+    exporter_template = config.load_jinja_template("exporter/exporter.yaml.j2")
+    opts = {"supportconfig_path": supportconfig_path}
+    exporter_config_file_path = config.get_config_file_path("exporter")
+    config.write_config("exporter", exporter_template.render(**opts))
---
+++ health-check/src/uyuni_health_check/main.py
@@ -0,0 +1,146 @@
+import click
+from rich.markdown import Markdown
+
+from grafana.grafana_manager import prepare_grafana
+from prometheus.prometheus_manager import prepare_prometheus
+
+from uyuni_health_check.utils import console, HealthException, run_command
+from uyuni_health_check.loki.loki_manager import (
+    run_loki,
+    wait_loki_init,
+    download_component_build_image,
+)
+from uyuni_health_check.loki.logs_gatherer import show_full_error_logs
+from uyuni_health_check.config_loader import ConfigLoader
+from uyuni_health_check.exporters.supportconfig import exporter
+import uyuni_health_check.containers.manager
+import uyuni_health_check.metrics
+
+
+@click.group()
+@click.option(
+    "-s",
+    "--supportconfig_path",
+    default=None,
+    help="Path to supportconfig path as the data source",
+)
+@click.option(
+    "-v",
+    "--verbose",
+    is_flag=True,
+    help="Show more stdout, including image building",
+)
+@click.pass_context
+def cli(ctx, supportconfig_path, verbose):
+    # ensure that ctx.obj exists and is a dict (in case `cli()` is called
+    # by means other than the `if` block below)
+    ctx.ensure_object(dict)
+    ctx.obj["verbose"] = verbose
+    ctx.obj["supportconfig_path"] = supportconfig_path
+    ctx.obj["config"] = ConfigLoader()
+
+    try:
+        console.log("[bold]Checking connection with podman:")
+        run_command(cmd=["podman", "--version"], console=console, quiet=False)
+    except HealthException as err:
+        console.log("[red bold]" + str(err))
+        console.print(Markdown("# Execution Finished"))
+        exit(1)
+
+
+@cli.command()
+@click.option(
+    "--logs",
+    is_flag=True,
+    help="Show the error logs",
+)
+@click.option(
+    "--since",
+    default=7,
+    type=int,
+    help="Show logs from last X days. (Default: 7)",
+)
+@click.pass_context
+def run(ctx, logs, since):
+    """
+    Start execution of Uyuni Health Check
+
+    Build the necessary containers, deploy them, get the metrics and display them
+
+    """
+    config = ctx.obj["config"]
+    verbose = ctx.obj["verbose"]
+    supportconfig_path = ctx.obj["supportconfig_path"]
+
+    if not supportconfig_path:
+        console.log("[red bold]You must provide a path to a supportconfig!")
+        console.print(Markdown("# Execution Finished"))
+        exit(1)
+
+    try:
+        with console.status(status=None):
+
+            console.log(
+                "[bold]Creating health-check-network podman network for containers"
+            )
+            uyuni_health_check.containers.manager.create_podman_network(verbose=verbose)
+
+            console.log("[bold]Building logcli image")
+            download_component_build_image("logcli", config=config, verbose=verbose)
+
+            console.log("[bold]Deploying promtail and Loki")
+            run_loki(
+                supportconfig_path=supportconfig_path, config=config, verbose=verbose
+            )
+            wait_loki_init()
+
+            console.log("[bold]Building exporter")
+            exporter.prepare_exporter(
+                config=config, supportconfig_path=supportconfig_path
+            )
+
+            # Fetch metrics from supportconfig-exporter
+            console.log("[bold]Fetching metrics from supportconfig-exporter")
+            metrics = (
+                uyuni_health_check.metrics.fetch_metrics_from_supportconfig_exporter(
+                    console
+                )
+            )
+
+            console.log("[bold]Preparing Prometheus")
+            prepare_prometheus(verbose=verbose)
+
+            console.log("[bold]Preparing Grafana")
+            prepare_grafana(verbose=verbose)
+
+        console.print(Markdown("# Summary of metrics gatherered from Supportconfig"))
+        uyuni_health_check.metrics.show_supportconfig_metrics(metrics, console)
+
+        console.print(Markdown("## Relevant Errors"))
+        uyuni_health_check.metrics.show_error_logs_stats(since, console)
+
+        if logs:
+            with console.pager():
+                show_full_error_logs(since)
+
+        console.print(Markdown("# Execution Finished"))
+
+    except HealthException as err:
+        console.log("[red bold]" + str(err))
+
+
+@cli.command()
+@click.pass_context
+def clean(ctx):
+    verbose = ctx.obj["verbose"]
+    uyuni_health_check.containers.manager.clean_containers(verbose=verbose)
+    console.print(Markdown("# Execution Finished"))
+
+
+def main():
+    print(Markdown("# Uyuni Health Check"))
+    cli()
+
+
+if __name__ == "__main__":
+    main()
