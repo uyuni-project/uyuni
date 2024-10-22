297f81f0ac91235aaa1b840d61831bedc856f7a1
Maxime Noel <maxime.noel@suse.com>
2024-10-22 14:06:23 +1300

    Improve pkill for fark_ipmi and check service before restarting one

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -578,12 +578,17 @@ When(/^the server starts mocking an IPMI host$/) do
     raise ScriptError, 'File injection failed' unless success
   end
   server.run('chmod +x /etc/ipmi/fake_ipmi_host.sh', verbose: true, check_errors: true)
-  server.run('nohup ipmi_sim -n > /var/log/ipmi_sim.log 2>&1 &', verbose: true, check_errors: true)
+  # Check if ipmi_sim is already running
+  if server.run('pgrep -f ipmi_sim', verbose: false, check_errors: false).empty?
+    server.run('ipmi_sim -n < /dev/null > /dev/null &', verbose: true, check_errors: true)
+  else
+    puts 'ipmi_sim is already running; skipping startup.'
+  end
 end
 
 When(/^the server stops mocking an IPMI host$/) do
   get_target('server').run('pkill ipmi_sim')
-  get_target('server').run("ps aux | grep [f]ake_ipmi_host.sh | awk '{print $2}' | xargs kill")
+  get_target('server').run('pkill --full fake_ipmi_host.sh || :')
 end
 
 When(/^the controller starts mocking a Redfish host$/) do
