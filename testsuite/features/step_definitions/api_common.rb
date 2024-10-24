61c6cea2dda5955f9ba26d1135b456e69e27920b
Oscar Barrios <obarrios@suse.com>
2024-10-24 09:55:46 +0200

    QE: Use the same e-mail for all users created through the test framework (#9389)

--- testsuite/features/step_definitions/api_common.rb
+++ testsuite/features/step_definitions/api_common.rb
@@ -142,7 +142,7 @@ Given(/^I create a user with name "([^"]*)" and password "([^"]*)"/) do |user, p
   $current_password = password
   next if $api_test.user.list_users.to_s.include? user
 
-  $api_test.user.create(user, password, user, user, "#{user}@mail.com")
+  $api_test.user.create(user, password, user, user, 'galaxy-noise@suse.de')
   roles = %w[org_admin channel_admin config_admin system_group_admin activation_key_admin image_admin]
   roles.each do |role|
     $api_test.user.add_role(user, role)
