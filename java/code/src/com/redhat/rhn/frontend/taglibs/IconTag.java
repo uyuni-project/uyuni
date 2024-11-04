d2afe24a5fc04c7e5d40d5b6b2ac385677096347
Richa <richa.bisht@suse.com>
2024-11-04 14:20:10 +0100

    Added inline icon button in My Account page and configuration channel page, add new edit icon in IconTag file, label text fix for Join Groups button

--- java/code/src/com/redhat/rhn/frontend/taglibs/IconTag.java
+++ java/code/src/com/redhat/rhn/frontend/taglibs/IconTag.java
@@ -168,6 +168,7 @@ public class IconTag extends TagSupport {
         icons.put("system-virt-guest", "fa fa-1-5x spacewalk-icon-virtual-guest");
         icons.put("system-virt-host", "fa fa-1-5x spacewalk-icon-virtual-host");
         icons.put("system-warn", "fa fa-exclamation-triangle fa-1-5x text-warning");
+        icons.put("item-close", "fa fa-times");
     }
 
     /**
