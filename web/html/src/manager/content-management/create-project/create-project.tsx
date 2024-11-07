0b07722bb5c551bfb2f5e1076b22a4077ef2ebbb
Richa <richa.bisht@suse.com>
2024-11-07 17:00:23 +0100

    Simplify  buttons in DangerDialog, move buttons at the bottom of the forms

--- web/html/src/manager/content-management/create-project/create-project.tsx
+++ web/html/src/manager/content-management/create-project/create-project.tsx
@@ -36,28 +36,30 @@ const CreateProject = () => {
     <TopPanel
       title={t("Create a new Content Lifecycle Project")}
       icon="fa-plus"
-      button={
-        <TopPanelButtons
-          onCreate={() =>
-            onAction(project, "create")
-              .then(() => {
-                window.pageRenderers?.spaengine?.navigate?.(
-                  `/rhn/manager/contentmanagement/project/${project.properties.label || ""}`
-                );
-              })
-              .catch((error) => {
-                setProject({ ...project, errors: error.errors });
-                showErrorToastr(error.messages, { autoHide: false });
-              })
-          }
-        />
-      }
     >
       <PropertiesCreate
         properties={project.properties}
         errors={project.errors}
         onChange={(newProperties) => setProject({ ...project, properties: newProperties })}
       />
+       <div className="form-group">
+            <div className="col-md-offset-2 offset-md-2 col-md-10">
+              <TopPanelButtons
+                onCreate={() =>
+                  onAction(project, "create")
+                    .then(() => {
+                      window.pageRenderers?.spaengine?.navigate?.(
+                        `/rhn/manager/contentmanagement/project/${project.properties.label || ""}`
+                      );
+                    })
+                    .catch((error) => {
+                      setProject({ ...project, errors: error.errors });
+                      showErrorToastr(error.messages, { autoHide: false });
+                    })
+                }
+              />
+            </div>
+          </div>
     </TopPanel>
   );
 };
