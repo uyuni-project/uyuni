0cdeb68e644bd2bd1b524d8c35c8d776c53826f2
mbussolotto <michele.bussolotto@suse.com>
2024-11-08 14:22:10 +0100

    fix rebase

--- java/code/src/com/redhat/rhn/domain/product/SUSEProductFactory.java
+++ java/code/src/com/redhat/rhn/domain/product/SUSEProductFactory.java
@@ -138,6 +138,7 @@ public class SUSEProductFactory extends HibernateFactory {
                         RPM_VERSION_COMPARATOR.compare(b.getProduct().getVersion(), a.getProduct().getVersion()))
                 .toList();
     }
+==== BASE ====
 
     /**
      * Insert or update a {@link SUSEProductExtension}.
@@ -186,7 +187,7 @@ public class SUSEProductFactory extends HibernateFactory {
     public static List<SUSEProductChannel> lookupSyncedProductChannelsByLabel(String channelLabel) {
         return Optional.ofNullable(ChannelFactory.lookupByLabel(channelLabel))
                 .map(channel -> channel.getSuseProductChannels().stream())
-                .orElseGet(Stream::empty).toList();
+                .orElseGet(Stream::empty).collect(Collectors.toList());
     }
 
     /**
@@ -263,7 +264,7 @@ public class SUSEProductFactory extends HibernateFactory {
                     List<String> selectedParts = List.of(channel.getLabel().split("-"));
                     List<String> uniqueParts = selectedParts.stream()
                             .filter(s -> !originalParts.contains(s))
-                            .toList();
+                            .collect(Collectors.toList());
 
                     if (channel.isBaseChannel()) {
                         return Stream.<Channel>empty();
