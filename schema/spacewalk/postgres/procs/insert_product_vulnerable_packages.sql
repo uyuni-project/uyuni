3cf1fa9990460eab451ba6edc7990799c66e9d82
HoussemNasri <houssemnasri2001@gmail.com>
2024-11-10 19:43:19 +0100

    Specify the OS product to which the OVAL vulnerability entry belongs in the database

--- schema/spacewalk/postgres/procs/insert_product_vulnerable_packages.sql
+++ schema/spacewalk/postgres/procs/insert_product_vulnerable_packages.sql
@@ -14,18 +14,34 @@
 --
 
 CREATE OR REPLACE PROCEDURE
-insert_product_vulnerable_packages(package_name_in varchar,fix_version_in varchar,product_cpe_in varchar,cve_name_in varchar)
+insert_product_vulnerable_packages(product_os_family_in varchar, product_os_version_in varchar, package_name_in varchar,fix_version_in varchar,platform_cpe_in varchar,cve_name_in varchar)
 AS
 $$
 DECLARE
     cve_id_val numeric;
-    product_cpe_id_val numeric;
+    platform_cpe_id_val numeric;
     vulnerable_pkg_id_val numeric;
+    product_os_id_val numeric;
 begin
 
     cve_id_val := lookup_cve(cve_name_in);
 
-    product_cpe_id_val := lookup_oval_platform(product_cpe_in);
+    platform_cpe_id_val := lookup_oval_platform(platform_cpe_in);
+
+    -- Check if the suseOVALOsProduct exists, if not insert it.
+    IF NOT EXISTS (
+        SELECT 1
+        FROM suseOVALOsProduct
+        WHERE os_family = product_os_family_in
+          AND version = product_os_version_in
+    ) THEN
+        INSERT INTO suseOVALOsProduct(id, os_family, version)
+        VALUES (nextval('suse_oval_os_product_id_seq'), product_os_family_in, product_os_version_in);
+    END IF;
+
+    SELECT id INTO product_os_id_val
+    FROM suseOVALOsProduct
+    WHERE os_family = product_os_family_in AND version = product_os_version_in;
 
     IF NOT EXISTS(SELECT 1
                   FROM suseovalvulnerablepackage
@@ -43,11 +59,12 @@ begin
       AND ((fix_version IS NOT NULL AND fix_version = fix_version_in) OR
            (fix_version IS NULL AND fix_version_in IS NULL));
 
-    INSERT INTO suseOVALPlatformVulnerablePackage(platform_id, cve_id, vulnerable_pkg_id)
-    VALUES (product_cpe_id_val, cve_id_val, vulnerable_pkg_id_val)
-    ON CONFLICT(platform_id, cve_id, vulnerable_pkg_id) DO UPDATE
+    INSERT INTO suseOVALPlatformVulnerablePackage(product_os_id, platform_id, cve_id, vulnerable_pkg_id)
+    VALUES (product_os_id_val, platform_cpe_id_val, cve_id_val, vulnerable_pkg_id_val)
+    ON CONFLICT(product_os_id, platform_id, cve_id, vulnerable_pkg_id) DO UPDATE
         SET platform_id       = EXCLUDED.platform_id,
             cve_id            = EXCLUDED.cve_id,
-            vulnerable_pkg_id = EXCLUDED.vulnerable_pkg_id;
+            vulnerable_pkg_id = EXCLUDED.vulnerable_pkg_id,
+            product_os_id     = EXCLUDED.product_os_id;
 end;
 $$ language plpgsql;
