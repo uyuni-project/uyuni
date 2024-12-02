d3ba906043a9902abc8e7aa66c71c3b32bc7e9bc
Manuel Salvi <manuel.salvi@suse.com>
2024-12-02 16:00:06 +0100

    default settings migration

--- schema/spacewalk/upgrade/susemanager-schema-5.1.1-to-susemanager-schema-5.1.2/101-default-settings-updates.sql
+++ schema/spacewalk/upgrade/susemanager-schema-5.1.1-to-susemanager-schema-5.1.2/101-default-settings-updates.sql
@@ -2,14 +2,14 @@ UPDATE rhnConfiguration SET
     default_value = NULL
     WHERE key = 'extauth_default_orgid';
 UPDATE rhnConfiguration SET
-    default_value = FALSE
+    default_value = FALSE,
     value = COALESCE(value, default_value)
     WHERE key = 'extauth_use_orgunit';
 UPDATE rhnConfiguration SET
-    default_value = 1
+    default_value = 1,
     value = COALESCE(value, default_value)
     WHERE key = 'system_checkin_threshold';
 UPDATE rhnConfiguration SET
-    default_value = FALSE
+    default_value = FALSE,
     value = COALESCE(value, default_value)
     WHERE key = 'extauth_keep_temproles';
