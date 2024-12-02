9902dcc266e714f96ff6b174db450a4e01f4076f
Manuel Salvi <manuel.salvi@suse.com>
2024-12-02 17:07:18 +0100

    string values because table is varchar

--- schema/spacewalk/upgrade/susemanager-schema-5.1.1-to-susemanager-schema-5.1.2/101-default-settings-updates.sql
+++ schema/spacewalk/upgrade/susemanager-schema-5.1.1-to-susemanager-schema-5.1.2/101-default-settings-updates.sql
@@ -3,15 +3,15 @@ UPDATE rhnConfiguration SET
     default_value = NULL
     WHERE key = 'extauth_default_orgid';
 UPDATE rhnConfiguration SET
-    default_value = FALSE,
+    default_value = 'false',
     value = COALESCE(value, default_value)
     WHERE key = 'extauth_use_orgunit';
 UPDATE rhnConfiguration SET
-    default_value = 1,
+    default_value = '1',
     value = COALESCE(value, default_value)
     WHERE key = 'system_checkin_threshold';
 UPDATE rhnConfiguration SET
-    default_value = FALSE,
+    default_value = 'false',
     value = COALESCE(value, default_value)
     WHERE key = 'extauth_keep_temproles';
 alter table rhnConfiguration enable trigger rhnconfiguration_log_trig;
