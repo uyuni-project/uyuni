cb505c7c5061da0b1408584bad84f1875aeea501
Thomas Florio <thomas.florio@suse.com>
2024-12-06 10:44:46 +0100

    Added new /iss path for v3 APIs

---
+++ java/code/src/com/suse/manager/webui/utils/RouteWithToken.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2024 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ */
+package com.suse.manager.webui.utils;
+
+import com.suse.manager.webui.utils.token.Token;
+
+import spark.Request;
+import spark.Response;
+
+/**
+ * A route that gets the authentication token in addition to the request and response.
+ */
+public interface RouteWithToken {
+
+    /**
+     * Invoked when a request is made on this route's corresponding path.
+     *
+     * @param request the request object
+     * @param response the response object
+     * @param token the token with this request
+     * @return the content to be set in the response
+     */
+    Object handle(Request request, Response response, Token token);
+}
