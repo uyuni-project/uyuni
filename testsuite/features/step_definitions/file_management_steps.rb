2a234773ebad1aab4bd65b7f18b418cc35896d43
Cédric Bosdonnat <cbosdonnat@suse.com>
2024-12-16 10:04:25 +0100

    tests: remove expect uses from the server container
    In order to remove the repositories from the container image the
    testsuite needs to stop relying on sumaform to add any package like
    expect.

--- testsuite/features/step_definitions/file_management_steps.rb
+++ testsuite/features/step_definitions/file_management_steps.rb
@@ -80,7 +80,7 @@ When(/^I bootstrap "([^"]*)" using bootstrap script with activation key "([^"]*)
   raise ScriptError, 'File injection failed' unless success
 
   system_name = get_system_name(host)
-  output, = target.run("sed -i '/^set timeout /c\\set timeout #{DEFAULT_TIMEOUT}' /tmp/#{boostrap_script} && expect -f /tmp/#{boostrap_script} #{system_name}", verbose: true)
+  output, = target.run_local("sed -i '/^set timeout /c\\set timeout #{DEFAULT_TIMEOUT}' /tmp/#{boostrap_script} && expect -f /tmp/#{boostrap_script} #{system_name} #{target.has_mgrctl}", verbose: true)
   unless output.include? '-bootstrap complete-'
     log output.encode('utf-8', invalid: :replace, undef: :replace, replace: '_')
     raise ScriptError, 'Bootstrap didn\'t finish properly'
