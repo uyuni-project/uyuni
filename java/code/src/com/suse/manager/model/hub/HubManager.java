dbbed0af7b256b6934eba4184a172a28c8e440df
Thomas Florio <thomas.florio@suse.com>
2025-01-10 11:06:37 +0100

    Added API to register hub/peripherals

--- java/code/src/com/suse/manager/model/hub/HubManager.java
+++ java/code/src/com/suse/manager/model/hub/HubManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 SUSE LLC
+ * Copyright (c) 2024--2025 SUSE LLC
  *
  * This software is licensed to you under the GNU General Public License,
  * version 2 (GPLv2). There is NO WARRANTY for this software, express or
@@ -12,13 +12,20 @@
 package com.suse.manager.model.hub;
 
 import com.redhat.rhn.common.conf.ConfigDefaults;
+import com.redhat.rhn.domain.iss.IssRole;
 
+import com.suse.manager.iss.IssClientFactory;
+import com.suse.manager.iss.IssInternalClient;
 import com.suse.manager.webui.utils.token.IssTokenBuilder;
 import com.suse.manager.webui.utils.token.Token;
 import com.suse.manager.webui.utils.token.TokenBuildingException;
-import com.suse.manager.webui.utils.token.TokenException;
 import com.suse.manager.webui.utils.token.TokenParser;
 import com.suse.manager.webui.utils.token.TokenParsingException;
+import com.suse.utils.CertificateUtils;
+
+import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.util.Optional;
 
 /**
  * Business logic to manage ISSv3 Sync
@@ -27,19 +34,23 @@ public class HubManager {
 
     private final HubFactory hubFactory;
 
+    private final IssClientFactory clientFactory;
+
     /**
      * Default constructor
      */
     public HubManager() {
-        this(new HubFactory());
+        this(new HubFactory(), new IssClientFactory());
     }
 
     /**
      * Builds an instance with the given dependencies
      * @param hubFactoryIn the hub factory
+     * @param clientFactoryIn the ISS client factory
      */
-    public HubManager(HubFactory hubFactoryIn) {
+    public HubManager(HubFactory hubFactoryIn, IssClientFactory clientFactoryIn) {
         this.hubFactory = hubFactoryIn;
+        this.clientFactory = clientFactoryIn;
     }
 
     /**
@@ -49,13 +60,35 @@ public class HubManager {
      * @throws TokenBuildingException when an error occurs during generation
      * @throws TokenParsingException when the generated token cannot be parsed
      */
-    public String issueAccessToken(String fqdn) throws TokenException {
+    public String issueAccessToken(String fqdn) throws TokenBuildingException, TokenParsingException {
         Token token = new IssTokenBuilder(fqdn)
             .usingServerSecret()
             .build();
 
         hubFactory.saveToken(fqdn, token.getSerializedForm(), TokenType.ISSUED, token.getExpirationTime());
         return token.getSerializedForm();
+    }
+
+    /**
+     * Save the given remote server as hub or peripheral depending on the specified role
+     * @param role the role of the server
+     * @param serverFqdn the fqdn of the server
+     * @param rootCA the root certificate, if needed
+     * @return the persisted remote server
+     */
+    public IssServer saveNewServer(IssRole role, String serverFqdn, String rootCA) {
+        return switch (role) {
+            case HUB -> {
+                IssHub hub = new IssHub(serverFqdn, rootCA);
+                hubFactory.save(hub);
+                yield hub;
+            }
+            case PERIPHERAL -> {
+                IssPeripheral peripheral = new IssPeripheral(serverFqdn, rootCA);
+                hubFactory.save(peripheral);
+                yield peripheral;
+            }
+        };
     }
 
     /**
@@ -82,5 +115,90 @@ public class HubManager {
         }
 
         hubFactory.saveToken(fqdn, token, TokenType.CONSUMED, parsedToken.getExpirationTime());
+    }
+
+    /**
+     * Register a remote server with the specified role for ISS
+     *
+     * @param remoteServer the peripheral server FQDN
+     * @param role the ISS role, either hub or peripheral
+     * @param username the username of a {@link com.redhat.rhn.domain.role.RoleFactory#SAT_ADMIN} of the remote server
+     * @param password the password of the specified user
+     * @param rootCA the optional root CA of the remote server. can be null
+     *
+     * @throws CertificateException if the specified certificate is not parseable
+     * @throws TokenParsingException if the specified token is not parseable
+     * @throws TokenBuildingException if an error occurs while generating the token for the server
+     * @throws IOException when connecting to the server fails
+     */
+    public void register(String remoteServer, IssRole role, String username, String password, String rootCA)
+        throws CertificateException, TokenBuildingException, IOException, TokenParsingException {
+        // Verify this server is not already registered as hub or peripheral
+        ensureServerNotRegistered(remoteServer);
+
+        // Generate a token for this server on the remote
+        String remoteToken;
+        try (var externalClient = clientFactory.newExternalClient(remoteServer, username, password, rootCA)) {
+            remoteToken = externalClient.generateAccessToken(ConfigDefaults.get().getHostname());
+        }
+
+        registerWithToken(remoteServer, role, rootCA, remoteToken);
+    }
+
+    /**
+     * Register a remote server with the specified role for ISS
+     *
+     * @param remoteServer the peripheral server FQDN
+     * @param role the ISS role, either hub or peripheral
+     * @param remoteToken the token used to connect to the peripheral server
+     * @param rootCA the optional root CA of the peripheral server
+     *
+     * @throws CertificateException if the specified certificate is not parseable
+     * @throws TokenParsingException if the specified token is not parseable
+     * @throws TokenBuildingException if an error occurs while generating the token for the peripheral server
+     * @throws IOException when connecting to the peripheral server fails
+     */
+    public void register(String remoteServer, IssRole role, String remoteToken, String rootCA)
+        throws CertificateException, TokenBuildingException, IOException, TokenParsingException {
+        // Verify this server is not already registered as hub or peripheral
+        ensureServerNotRegistered(remoteServer);
+
+        registerWithToken(remoteServer, role, rootCA, remoteToken);
+    }
+
+    private void registerWithToken(String remoteServer, IssRole role, String rootCA, String remoteToken)
+        throws TokenParsingException, CertificateException, TokenBuildingException, IOException {
+        storeAccessToken(remoteServer, remoteToken);
+
+        IssServer registeredServer = saveNewServer(role, remoteServer, rootCA);
+        registerToRemote(registeredServer, remoteToken, rootCA);
+    }
+
+    private void registerToRemote(IssServer remoteServer, String remoteToken, String rootCA)
+        throws CertificateException, TokenParsingException, TokenBuildingException, IOException {
+
+        // Create a client to connect to the internal API of the remote server
+        IssInternalClient internalApi = clientFactory.newInternalClient(remoteServer.getFqdn(), remoteToken, rootCA);
+
+        // Register this server on the remote with the opposite role
+        IssRole localRoleForRemote = remoteServer.getRole() == IssRole.HUB ? IssRole.PERIPHERAL : IssRole.HUB;
+        // Issue a token for granting access to the remote server
+        String localAccessToken = issueAccessToken(remoteServer.getFqdn());
+        // Send the local trusted root, if we needed a different certificate to connect
+        String localRootCA = rootCA != null ? CertificateUtils.loadLocalTrustedRoot() : null;
+
+        internalApi.register(localRoleForRemote, localAccessToken, localRootCA);
+    }
+
+    private void ensureServerNotRegistered(String peripheralServer) {
+        Optional<IssHub> issHub = hubFactory.lookupIssHubByFqdn(peripheralServer);
+        if (issHub.isPresent()) {
+            throw new IllegalStateException("Server " + peripheralServer + " is already registered as hub");
+        }
+
+        Optional<IssPeripheral> issPeripheral = hubFactory.lookupIssPeripheralByFqdn(peripheralServer);
+        if (issPeripheral.isPresent()) {
+            throw new IllegalStateException("Server " + peripheralServer + " is already registered as peripheral");
+        }
     }
 }
--- java/code/src/com/suse/manager/xmlrpc/iss/IssHandler.java
+++ java/code/src/com/suse/manager/xmlrpc/iss/IssHandler.java
@@ -11,21 +11,29 @@
 
 package com.suse.manager.xmlrpc.iss;
 
+import com.redhat.rhn.domain.iss.IssRole;
 import com.redhat.rhn.domain.user.User;
 import com.redhat.rhn.frontend.xmlrpc.BaseHandler;
 import com.redhat.rhn.frontend.xmlrpc.InvalidParameterException;
 import com.redhat.rhn.frontend.xmlrpc.InvalidTokenException;
 import com.redhat.rhn.frontend.xmlrpc.TokenAlreadyExistsException;
 import com.redhat.rhn.frontend.xmlrpc.TokenCreationException;
+import com.redhat.rhn.frontend.xmlrpc.TokenExchangeFailedException;
 
 import com.suse.manager.model.hub.HubManager;
+import com.suse.manager.webui.utils.token.TokenBuildingException;
 import com.suse.manager.webui.utils.token.TokenException;
 import com.suse.manager.webui.utils.token.TokenParsingException;
+import com.suse.manager.xmlrpc.InvalidCertificateException;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.exception.ConstraintViolationException;
+
+import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.util.Arrays;
 
 /**
  * IssHandler
@@ -120,6 +128,162 @@ public class IssHandler extends BaseHandler {
         catch (ConstraintViolationException ex) {
             LOGGER.error("Unable to store token, it already exists for {}", fqdn, ex);
             throw new TokenAlreadyExistsException();
+        }
+
+        return 1;
+    }
+
+    /**
+     * Registers automatically a remote server with the specified ISS role.
+     * @param loggedInUser the user logged in. It must have the sat admin role.
+     * @param fqdn the FQDN of the remote server to register
+     * @param role the ISS role of the remote server. Can be either HUB or PERIPHERAL
+     * @param username the name of the user, needed to access the remote server. It must have the sat admin role.
+     * @param password the password of the user, needed to access the remote server.
+     * @return 1 on success, exception otherwise
+     * @apidoc.doc Registers automatically a remote server with the specified ISS role.
+     * @apidoc.param #session_key()
+     * @apidoc.param #param_desc("string", "fqdn", "the FQDN of the remote server to register ")
+     * @apidoc.param #param_desc("string", "role", "the ISS role of the remote server. Either HUB or PERIPHERAL")
+     * @apidoc.param #param_desc("string", "username", "the name of the user, needed to access the remote server
+     * It must have the sat admin role")
+     * @apidoc.param #param_desc("string", "password", "the password of the user, needed to access the remote
+     * server")
+     * @apidoc.returntype #return_int_success()
+     */
+    public int register(User loggedInUser, String fqdn, String role, String username, String password) {
+        return register(loggedInUser, fqdn, role, username, password, null);
+    }
+
+    /**
+     * Registers automatically a remote server with the specified ISS role.
+     * @param loggedInUser the user logged in. It must have the sat admin role.
+     * @param fqdn the FQDN of the remote server to register
+     * @param role the ISS role of the remote server. Can be either HUB or PERIPHERAL
+     * @param username the name of the user, needed to access the remote server. It must have the sat admin role.
+     * @param password the password of the user, needed to access the remote server.
+     * @param rootCA the root CA certificate, in case it's needed to establish a secure connection
+     * @return 1 on success, exception otherwise
+     * @apidoc.doc Registers automatically a remote server with the specified ISS role.
+     * @apidoc.param #session_key()
+     * @apidoc.param #param_desc("string", "fqdn", "the FQDN of the remote server to register ")
+     * @apidoc.param #param_desc("string", "role", "the ISS role of the remote server. Either HUB or PERIPHERAL")
+     * @apidoc.param #param_desc("string", "username", "the name of the user, needed to access the remote server
+     * It must have the sat admin role")
+     * @apidoc.param #param_desc("string", "password", "the password of the user, needed to access the remote
+     * server")
+     * @apidoc.param #param_desc("string", "rootCA", the root CA certificate, in case it's needed to establish a secure
+     * connection")
+     * @apidoc.returntype #return_int_success()
+     */
+    public int register(User loggedInUser, String fqdn, String role, String username, String password, String rootCA) {
+        ensureSatAdmin(loggedInUser);
+
+        IssRole remoteRole = Arrays.stream(IssRole.values())
+            .filter(r -> r.name().equalsIgnoreCase(role))
+            .findFirst()
+            .orElseThrow(() -> new InvalidParameterException("Invalid role specified"));
+
+        if (StringUtils.isEmpty(fqdn)) {
+            throw new InvalidParameterException("No FQDN specified");
+        }
+
+        if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
+            throw new InvalidParameterException("No credentials specified");
+        }
+
+        try {
+            hubManager.register(fqdn, remoteRole, username, password, rootCA);
+        }
+        catch (CertificateException ex) {
+            LOGGER.error("Unable to load the provided certificate", ex);
+            throw new InvalidCertificateException(ex);
+        }
+        catch (TokenParsingException ex) {
+            LOGGER.error("Unable to parse the specified token", ex);
+            throw new TokenExchangeFailedException(ex);
+        }
+        catch (TokenBuildingException ex) {
+            LOGGER.error("Unable to create a token for {}", fqdn, ex);
+            throw new TokenExchangeFailedException(ex);
+        }
+        catch (IOException ex) {
+            LOGGER.error("Unable to connect to remote server {}", fqdn, ex);
+            throw new TokenExchangeFailedException(ex);
+        }
+
+        return 1;
+    }
+
+    /**
+     * Registers a remote server with the specified ISS role using an existing specified access token.
+     * @param loggedInUser the user logged in. It must have the sat admin role.
+     * @param fqdn the FQDN of the remote server to register
+     * @param role the ISS role of the remote server. Can be either HUB or PERIPHERAL
+     * @param token the token used to authenticate on the remote server.
+     * @return 1 on success, exception otherwise
+     * @apidoc.doc Registers a remote server with the specified ISS role using an existing specified access token.
+     * @apidoc.param #session_key()
+     * @apidoc.param #param_desc("string", "fqdn", "the FQDN of the remote server to register ")
+     * @apidoc.param #param_desc("string", "role", "the ISS role of the remote server. Either HUB or PERIPHERAL")
+     * @apidoc.param #param_desc("string", "token", "the token used to authenticate on the remote server.")
+     * @apidoc.returntype #return_int_success()
+     */
+    public int registerWithToken(User loggedInUser, String fqdn, String role, String token) {
+        return registerWithToken(loggedInUser, fqdn, role, token, null);
+    }
+
+    /**
+     * Registers a remote server with the specified ISS role using an existing specified access token.
+     * @param loggedInUser the user logged in. It must have the sat admin role.
+     * @param fqdn the FQDN of the remote server to register
+     * @param role the ISS role of the remote server. Can be either HUB or PERIPHERAL
+     * @param token the token used to authenticate on the remote server.
+     * @param rootCA the root CA certificate, in case it's needed to establish a secure connection
+     * @return 1 on success, exception otherwise
+     * @apidoc.doc Registers a remote server with the specified ISS role using an existing specified access token.
+     * @apidoc.param #session_key()
+     * @apidoc.param #param_desc("string", "fqdn", "the FQDN of the remote server to register ")
+     * @apidoc.param #param_desc("string", "role", "the ISS role of the remote server. Either HUB or PERIPHERAL")
+     * @apidoc.param #param_desc("string", "token", "the token used to authenticate on the remote server.")
+     * @apidoc.param #param_desc("string", "rootCA", the root CA certificate, in case it's needed to establish a secure
+     * connection")
+     * @apidoc.returntype #return_int_success()
+     */
+    public int registerWithToken(User loggedInUser, String fqdn, String role, String token, String rootCA) {
+        ensureSatAdmin(loggedInUser);
+
+        IssRole remoteRole = Arrays.stream(IssRole.values())
+            .filter(r -> r.name().equalsIgnoreCase(role))
+            .findFirst()
+            .orElseThrow(() -> new InvalidParameterException("Invalid role specified"));
+
+        if (StringUtils.isEmpty(fqdn)) {
+            throw new InvalidParameterException("No FQDN specified");
+        }
+
+        if (StringUtils.isEmpty(token)) {
+            throw new InvalidParameterException("No token");
+        }
+
+        try {
+            hubManager.register(fqdn, remoteRole, token, rootCA);
+        }
+        catch (CertificateException ex) {
+            LOGGER.error("Unable to load the provided certificate", ex);
+            throw new InvalidCertificateException(ex);
+        }
+        catch (TokenParsingException ex) {
+            LOGGER.error("Unable to parse the specified token", ex);
+            throw new TokenExchangeFailedException(ex);
+        }
+        catch (TokenBuildingException ex) {
+            LOGGER.error("Unable to create a token for {}", fqdn, ex);
+            throw new TokenExchangeFailedException(ex);
+        }
+        catch (IOException ex) {
+            LOGGER.error("Unable to connect to remote server {}", fqdn, ex);
+            throw new TokenExchangeFailedException(ex);
         }
 
         return 1;
