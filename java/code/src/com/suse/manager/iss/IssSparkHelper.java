43d0d4aefa2adfe604a2dbdfd0b1a27ee65713d0
Thomas Florio <thomas.florio@suse.com>
2025-01-10 11:06:37 +0100

    Added API to register hub/peripherals

--- java/code/src/com/suse/manager/iss/IssSparkHelper.java
+++ java/code/src/com/suse/manager/iss/IssSparkHelper.java
@@ -13,10 +13,13 @@ package com.suse.manager.iss;
 
 import static com.suse.manager.webui.utils.SparkApplicationHelper.json;
 
+import com.redhat.rhn.domain.iss.IssRole;
 import com.redhat.rhn.frontend.security.AuthenticationServiceFactory;
 
 import com.suse.manager.model.hub.HubFactory;
 import com.suse.manager.model.hub.IssAccessToken;
+import com.suse.manager.model.hub.IssHub;
+import com.suse.manager.model.hub.IssPeripheral;
 import com.suse.manager.webui.utils.gson.ResultJson;
 import com.suse.manager.webui.utils.token.Token;
 import com.suse.manager.webui.utils.token.TokenParser;
@@ -26,6 +29,9 @@ import com.google.gson.reflect.TypeToken;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -77,6 +83,68 @@ public final class IssSparkHelper {
                 authenticationService.invalidate(request.raw(), response.raw());
             }
         };
+    }
+
+    /**
+     * Use in ISS routes only accessible from a hub
+     * @param route the route
+     * @return the route
+     */
+    public static RouteWithIssToken allowingOnlyHub(RouteWithIssToken route) {
+        return allowingOnly(List.of(IssRole.HUB), route);
+    }
+
+    /**
+     * Use in ISS routes only accessible from a peripheral
+     * @param route the route
+     * @return the route
+     */
+    public static RouteWithIssToken allowingOnlyPeripheral(RouteWithIssToken route) {
+        return allowingOnly(List.of(IssRole.PERIPHERAL), route);
+    }
+
+    /**
+     * Use in ISS routes only accessible from an unregistered server
+     * @param route the route
+     * @return the route
+     */
+    public static RouteWithIssToken allowingOnlyUnregistered(RouteWithIssToken route) {
+        return allowingOnly(List.of(), route);
+    }
+
+    private static RouteWithIssToken allowingOnly(List<IssRole> allowedRoles, RouteWithIssToken route) {
+        return (request, response, token, fqdn) -> {
+            Optional<IssHub> issHub = HUB_FACTORY.lookupIssHubByFqdn(fqdn);
+            Optional<IssPeripheral> issPeripheral = HUB_FACTORY.lookupIssPeripheralByFqdn(fqdn);
+
+            if (isRouteForbidden(allowedRoles, issHub.isPresent(), issPeripheral.isPresent())) {
+                response.status(HttpServletResponse.SC_FORBIDDEN);
+                return json(response, ResultJson.error("Token does not allow access to this resource"),
+                    new TypeToken<>() { });
+
+            }
+
+            return route.handle(request, response, token, fqdn);
+        };
+    }
+
+    private static boolean isRouteForbidden(List<IssRole> allowedRoles, boolean isHub, boolean isPeripheral) {
+        // No role allowed, only unregistered server can use this route
+        if (allowedRoles.isEmpty() && (isHub || isPeripheral)) {
+            return true;
+        }
+
+        // Only hub servers are allowed
+        if (allowedRoles.contains(IssRole.HUB) && !isHub) {
+            return true;
+        }
+
+        // Only peripheral servers are allowed
+        if (allowedRoles.contains(IssRole.PERIPHERAL) && !isPeripheral) {
+            return true;
+        }
+
+        return false;
     }
 
     private static Token parseToken(String serializedToken) {
