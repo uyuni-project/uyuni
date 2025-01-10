3cc86aeb9468c6cd6de8261da8cf1025cf2813b1
Thomas Florio <thomas.florio@suse.com>
2025-01-10 11:06:37 +0100

    Generate and store the SCC credentials on the peripheral

--- java/code/src/com/suse/manager/iss/SyncController.java
+++ java/code/src/com/suse/manager/iss/SyncController.java
@@ -11,29 +11,33 @@
 
 package com.suse.manager.iss;
 
+import static com.suse.manager.iss.IssSparkHelper.allowingOnlyHub;
+import static com.suse.manager.iss.IssSparkHelper.allowingOnlyPeripheral;
 import static com.suse.manager.iss.IssSparkHelper.allowingOnlyUnregistered;
 import static com.suse.manager.iss.IssSparkHelper.usingTokenAuthentication;
 import static com.suse.manager.webui.utils.SparkApplicationHelper.asJson;
-import static com.suse.manager.webui.utils.SparkApplicationHelper.json;
+import static com.suse.manager.webui.utils.SparkApplicationHelper.badRequest;
+import static com.suse.manager.webui.utils.SparkApplicationHelper.message;
+import static com.suse.manager.webui.utils.SparkApplicationHelper.success;
 import static spark.Spark.post;
 
+import com.redhat.rhn.domain.iss.IssRole;
+
 import com.suse.manager.model.hub.HubManager;
+import com.suse.manager.model.hub.IssHub;
+import com.suse.manager.model.hub.IssPeripheral;
 import com.suse.manager.webui.controllers.ECMAScriptDateAdapter;
-import com.suse.manager.webui.utils.gson.ResultJson;
 import com.suse.manager.webui.utils.token.Token;
 import com.suse.manager.webui.utils.token.TokenParsingException;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Date;
-
-import javax.servlet.http.HttpServletResponse;
 
 import spark.Request;
 import spark.Response;
@@ -70,12 +74,14 @@ public class SyncController {
     public void initRoutes() {
         post("/iss/sync/ping", asJson(usingTokenAuthentication(this::ping)));
         post("/iss/sync/register", asJson(usingTokenAuthentication(allowingOnlyUnregistered(this::register))));
+        post("/iss/sync/storeCredentials", asJson(usingTokenAuthentication(allowingOnlyHub(this::storeCredentials))));
+        post("/iss/sync/generateCredentials",
+            asJson(usingTokenAuthentication(allowingOnlyPeripheral(this::generateCredentials))));
     }
 
     // Basic ping to check if the system is up
     private String ping(Request request, Response response, Token token, String fqdn) {
-        ResultJson<Object> result = ResultJson.successMessage("Pinged from %s".formatted(fqdn));
-        return json(GSON, response, result, new TypeToken<>() { });
+        return message(response, "Pinged from %s".formatted(fqdn));
     }
 
     private String register(Request request, Response response, Token token, String fqdn) {
@@ -84,20 +90,43 @@ public class SyncController {
         String tokenToStore = registerRequest.getToken();
         if (StringUtils.isEmpty(tokenToStore)) {
             LOGGER.error("No token received in the request for server {}", fqdn);
-            response.status(HttpServletResponse.SC_BAD_REQUEST);
-            return json(response, ResultJson.error("Required token is missing"), new TypeToken<>() { });
+            return badRequest(response, "Required token is missing");
         }
 
         try {
             hubManager.storeAccessToken(fqdn, tokenToStore);
             hubManager.saveNewServer(registerRequest.getRole(), fqdn, registerRequest.getRootCA());
 
-            return json(GSON, response, ResultJson.success(), new TypeToken<>() { });
+            return success(response);
         }
         catch (TokenParsingException ex) {
             LOGGER.error("Unable to parse the received token for server {}", fqdn);
-            response.status(HttpServletResponse.SC_BAD_REQUEST);
-            return json(response, ResultJson.error("The specified token is not parseable"), new TypeToken<>() { });
+            return badRequest(response, "The specified token is not parseable");
         }
+    }
+
+    private String generateCredentials(Request request, Response response, Token token, String fqdn) {
+        IssPeripheral peripheral = (IssPeripheral) hubManager.findServer(IssRole.PERIPHERAL, fqdn);
+        if (peripheral == null) {
+            // This should never happen, fqdn guaranteed be a hub after calling allowingOnlyHub() on route init.
+            return badRequest(response, "Specified FQDN is not a known peripheral");
+        }
+
+        SCCCredentialsJson credentialsJson = hubManager.generateSCCCredentials(peripheral.getId());
+        return success(response, credentialsJson);
+    }
+
+    private String storeCredentials(Request request, Response response, Token token, String fqdn) {
+        SCCCredentialsJson storeRequest = GSON.fromJson(request.body(), SCCCredentialsJson.class);
+
+        IssHub hub = (IssHub) hubManager.findServer(IssRole.HUB, fqdn);
+        if (hub == null) {
+            // This should never happen, fqdn guaranteed be a hub after calling allowingOnlyHub() on route init.
+            return badRequest(response, "Specified FQDN is not a known hub");
+        }
+
+        hubManager.storeSCCCredentials(hub, storeRequest.getUsername(), storeRequest.getPassword());
+
+        return success(response);
     }
 }
