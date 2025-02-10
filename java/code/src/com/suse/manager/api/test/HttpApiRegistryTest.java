b023419ebd4039e67164668088b95441d8da9292
Can Bulut Bayburt <cbbayburt@suse.com>
2025-02-10 18:31:49 +0100

    Fix unauthorized routes in ApiHandler

--- java/code/src/com/suse/manager/api/test/HttpApiRegistryTest.java
+++ java/code/src/com/suse/manager/api/test/HttpApiRegistryTest.java
@@ -65,7 +65,7 @@ public class HttpApiRegistryTest extends RhnJmockBaseTestCase {
         context().checking(new Expectations() {{
             // Auth routes are added in all cases
             oneOf(helper).addPostRoute(with("/manager/api/auth/login"), with(any(Route.class)));
-            oneOf(helper).addPostRoute(with("/manager/api/auth/logout"), with(any(Route.class)));
+            oneOf(helper).addGetRoute(with("/manager/api/auth/logout"), with(any(Route.class)));
 
             // Test routes
             oneOf(helper).addGetRoute(with("/manager/api/test/path/myFirstEndpoint"), with(any(Route.class)));
