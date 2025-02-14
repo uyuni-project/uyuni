5998f4768e93d81d246be36bcbd60a27b57eede9
Thomas Florio <thomas.florio@suse.com>
2025-02-14 14:20:12 +0100

    Added toggle validity and delete in the token list page

--- web/html/src/components/hub/TokenTable.tsx
+++ web/html/src/components/hub/TokenTable.tsx
@@ -2,15 +2,22 @@ import * as React from "react";
 
 import { pageSize } from "core/user-preferences";
 
+import { Button } from "components/buttons";
 import { FromNow } from "components/datetime";
+import { DangerDialog } from "components/dialog/DangerDialog";
 import { Column } from "components/table/Column";
 import { Table, TableRef } from "components/table/Table";
+import { showInfoToastr } from "components/toastr";
 
 import { localizedMoment } from "utils/datetime";
+import Network from "utils/network";
 
-import { AccessToken } from "./types";
+import { AccessToken, TokenType, ValidityRequest } from "./types";
 
-type Props = {};
+type Props = {
+  allowToggleValidity: boolean;
+  allowDeletion: boolean;
+};
 
 type State = {
   selectedRow: AccessToken | undefined;
@@ -85,6 +92,77 @@ export class TokenTable extends React.Component<Props, State> {
           header={t("Last Modified")}
           cell={(row: AccessToken) => <FromNow value={row.modificationDate} />}
         />
+        {(this.props.allowDeletion || this.props.allowToggleValidity) && (
+          <Column
+            columnClass="text-right"
+            headerClass="text-right"
+            header={t("Actions")}
+            cell={(row: AccessToken) => (
+              <div className="btn-group">
+                {this.props.allowToggleValidity && (
+                  <>
+                    <Button
+                      icon={row.valid ? "fa-ban" : "fa-check"}
+                      className="btn-default"
+                      handler={() =>
+                        // If the token is used, ask for confirmation. Otherwise, directly toggle the validity
+                        row.hubId !== null || row.peripheralId !== null
+                          ? this.setState({ confirmValidityDialog: true, selectedRow: row })
+                          : this.onToggleValidity(row)
+                      }
+                    >
+                      {row.valid ? t("Invalidate") : t("Validate")}
+                    </Button>
+                    {this.state.selectedRow && (
+                      <DangerDialog
+                        id="confirm-validity-modal"
+                        isOpen={this.state.confirmValidityDialog}
+                        title={t("Confirm access token modification")}
+                        content={
+                          <div>
+                            <div>{this.getValidityConfirmationMessage(this.state.selectedRow)}</div>
+                            <div>{t("Confirm if you want to proceed.")}</div>
+                          </div>
+                        }
+                        onConfirm={() => this.state.selectedRow && this.onToggleValidity(this.state.selectedRow)}
+                        onClose={() => this.setState({ confirmValidityDialog: false, selectedRow: undefined })}
+                        submitText={this.state.selectedRow.valid ? t("Invalidate") : t("Validate")}
+                        submitIcon={this.state.selectedRow.valid ? "fa-ban" : "fa-check"}
+                      />
+                    )}
+                  </>
+                )}
+                {this.props.allowDeletion && (
+                  <>
+                    <Button
+                      disabled={row.hubId !== null || row.peripheralId !== null}
+                      title={this.getDeleteTitle(row)}
+                      className="btn-default"
+                      icon="fa-trash"
+                      handler={() => this.setState({ confirmDeleteDialog: true, selectedRow: row })}
+                    >
+                      {t("Delete")}
+                      {this.state.selectedRow && (
+                        <DangerDialog
+                          id="confirm-deletion-modal"
+                          isOpen={this.state.confirmDeleteDialog}
+                          title={t("Confirm access token deletion")}
+                          content={
+                            <div>{t("Are you sure you want to delete this token. It is currently not used.")}</div>
+                          }
+                          onConfirm={() => this.state.selectedRow && this.onDelete(this.state.selectedRow)}
+                          onClose={() => this.setState({ confirmDeleteDialog: false, selectedRow: undefined })}
+                          submitText={t("Delete")}
+                          submitIcon="fa-trash"
+                        />
+                      )}
+                    </Button>
+                  </>
+                )}
+              </div>
+            )}
+          />
+        )}
       </Table>
     );
   }
@@ -102,5 +180,87 @@ export class TokenTable extends React.Component<Props, State> {
         {isExpired ? t("Yes, expired <expiration></expiration>") : t("No, expires <expiration></expiration>", param)}
       </span>
     );
+  }
+
+  private onDelete(row: AccessToken): void {
+    Network.del(`/rhn/manager/api/admin/hub/access-tokens/${row.id}`)
+      .catch((xhr) => Network.showResponseErrorToastr(xhr))
+      .then((response) => {
+        this.tableRef.current?.refresh();
+        showInfoToastr("Access token successfully deleted");
+      });
+  }
+
+  private onToggleValidity(row: AccessToken): void {
+    const request: ValidityRequest = { valid: !row.valid };
+
+    Network.post(`/rhn/manager/api/admin/hub/access-tokens/${row.id}/validity`, request)
+      .catch((xhr) => Network.showResponseErrorToastr(xhr))
+      .then((response) => {
+        row.valid = request.valid;
+        row.expirationDate = response.data;
+        this.tableRef.current?.refresh();
+        showInfoToastr("Access token validity successfully chande");
+      });
+  }
+
+  private getDeleteTitle(row: AccessToken): string {
+    const params = { fqdn: row.serverFqdn };
+    if (row.hubId !== null) {
+      return t("This access token cannot be deleted. It is currently used by hub {fqdn}", params);
+    }
+
+    if (row.peripheralId !== null) {
+      return t("This access token cannot be deleted. It is currently used by peripheral {fqdn}", params);
+    }
+
+    // both the peripheral and the hub are null means the token is not used and we can delete it
+    return t("Delete");
+  }
+
+  private getValidityConfirmationMessage(row: AccessToken): string {
+    const params = { fqdn: row.serverFqdn };
+    // token issued for another server currently valid and linked to a hub
+    if (row.type === TokenType.ISSUED && row.valid && row.hubId !== null) {
+      return t("Invalidating this access token will prevent the hub {fqdn} from accesssing.", params);
+    }
+
+    // token issued for another server currently valid and linked to a peripheral
+    if (row.type === TokenType.ISSUED && row.valid && row.peripheralId !== null) {
+      return t("Invalidating this access token will prevent the peripheral {fqdn} from accesssing.", params);
+    }
+
+    // token issued for another server currently not valid and linked to a hub
+    if (row.type === TokenType.ISSUED && !row.valid && row.hubId !== null) {
+      return t("Enabling this access token will grant access to the hub {fqdn}.", params);
+    }
+
+    // token issued for another server currently not valid and linked to a peripheral
+    if (row.type === TokenType.ISSUED && !row.valid && row.peripheralId !== null) {
+      return t("Enabling this access token will grant access to the peripheral {fqdn}.", params);
+    }
+
+    // token received from another server currently valid and linked to a hub
+    if (row.type === TokenType.CONSUMED && row.valid && row.hubId !== null) {
+      return t("Invalidating this access token will prevent access to the hub {fqdn}.", params);
+    }
+
+    // token received from another server currently valid and linked to a peripheral
+    if (row.type === TokenType.CONSUMED && row.valid && row.peripheralId !== null) {
+      return t("Invalidating this access token will prevent access to the peripheral {fqdn}.", params);
+    }
+
+    // token received from another server currently not valid and linked to a hub
+    if (row.type === TokenType.CONSUMED && !row.valid && row.hubId !== null) {
+      return t("Enabling this access token will restore access to the hub {fqdn}.", params);
+    }
+
+    // token received from another server currently not valid and linked to a peripheral
+    if (row.type === TokenType.CONSUMED && !row.valid && row.peripheralId !== null) {
+      return t("Enabling this access token will restore access to the peripheral {fqdn}.", params);
+    }
+
+    // This line should be unreachable
+    return `invalid row state: ${row.type}, ${row.valid}, ${row.hubId}/${row.peripheralId}`;
   }
 }
