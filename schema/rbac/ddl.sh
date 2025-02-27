ac52e8878ff887a8134ba78aec07e49a16022841
Can Bulut Bayburt <cbbayburt@suse.com>
2025-02-27 18:20:30 +0100

    Update SQL generator scripts
    - Wrap DML in a transaction
    - Add '-d' option to drop the schema

--- schema/rbac/ddl.sh
+++ schema/rbac/ddl.sh
@@ -1,4 +1,16 @@
-# Copy rbac schema DDL and execute on container server
+#!/bin/bash
+#
+# Deploy 'access' schema for RBAC into a container server via mgrctl
+# or drop existing schema if '-d' option is provided
 
-mgrctl cp ddl.sql server:ddl.sql
-mgrctl exec -ti -- "spacewalk-sql -i < ddl.sql"
+if [[ "$@" == *"-d"* ]]; then
+  # Remove the schema if '-d' option is used
+  echo "DROP SCHEMA access CASCADE;" | mgrctl exec -i -- spacewalk-sql -i \
+      && echo "RBAC schema dropped."
+
+else
+  # Original operation
+  cat ../spacewalk/upgrade/susemanager-schema-5.1.3-to-susemanager-schema-5.1.4/200-rbac.sql ddl.sql \
+    | mgrctl exec -i -- spacewalk-sql -i \
+    && echo "RBAC schema created."
+fi
