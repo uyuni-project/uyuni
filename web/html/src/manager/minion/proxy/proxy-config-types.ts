11e75b36405c07fce20654226da28ab9612a5bf3
Ricardo Mestre <ricardo.mestre@suse.com>
2025-02-27 14:31:15 +0000

    simplify proxy config front end structure

---
+++ web/html/src/manager/minion/proxy/proxy-config-types.ts
@@ -0,0 +1,64 @@
+export enum UseCertsMode {
+  Replace = "replace",
+  Keep = "keep",
+}
+
+export enum SourceMode {
+  Registry = "registry",
+  RPM = "rpm",
+}
+
+export enum RegistryMode {
+  Simple = "simple",
+  Advanced = "advanced",
+}
+
+export type ProxyConfigModel = {
+  rootCA: string;
+  proxyCertificate: string;
+  proxyKey: string;
+  intermediateCAs?: string[];
+  proxyAdminEmail: string;
+  maxSquidCacheSize: string;
+  parentFQDN: string;
+  proxyPort: string;
+  useCertsMode: UseCertsMode;
+  sourceMode: SourceMode;
+  registryMode: RegistryMode;
+  registryBaseURL: string;
+  registryBaseTag: string;
+  registryHttpdURL: string;
+  registryHttpdTag: string;
+  registrySaltbrokerURL: string;
+  registrySaltbrokerTag: string;
+  registrySquidURL: string;
+  registrySquidTag: string;
+  registrySshURL: string;
+  registrySshTag: string;
+  registryTftpdURL: string;
+  registryTftpdTag: string;
+};
+
+export interface Parent {
+  id: number;
+  name: string;
+  selected: boolean;
+  disabled: boolean;
+}
+
+export interface ProxyConfigProps {
+  serverId: string;
+  isUyuni: boolean;
+  parents: Parent[];
+  currentConfig: ProxyConfigModel;
+  initFailMessage?: string;
+}
+
+export type TagOptions = {
+  registryBaseURL?: string[];
+  registryHttpdURL?: string[];
+  registrySaltbrokerURL?: string[];
+  registrySquidURL?: string[];
+  registrySshURL?: string[];
+  registryTftpdURL?: string[];
+};
