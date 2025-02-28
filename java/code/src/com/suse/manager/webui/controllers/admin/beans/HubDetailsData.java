f1ab0f27669e424657befbd7ef46e98e40a9a8d7
Thomas Florio <thomas.florio@suse.com>
2025-02-28 19:10:22 +0100

    Use record for HubDetailsData

--- java/code/src/com/suse/manager/webui/controllers/admin/beans/HubDetailsData.java
+++ java/code/src/com/suse/manager/webui/controllers/admin/beans/HubDetailsData.java
@@ -13,125 +13,34 @@ package com.suse.manager.webui.controllers.admin.beans;
 
 import com.suse.manager.model.hub.IssHub;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-
 import java.util.Date;
 
-public class HubDetailsData {
-
-    private long id;
-
-    private String fqdn;
-
-    private String rootCA;
-
-    private String gpgKey;
-
-    private String sccUsername;
-
-    private Date created;
-
-    private Date modified;
+/**
+ * The details of a hub
+ * @param id the id
+ * @param fqdn the fully qualified domain name
+ * @param rootCA the root certificate authority, if present
+ * @param gpgKey the gpg key used to sign channel metadata, if present
+ * @param sccUsername the username for syncing scc data
+ * @param created when this hub configuration was registered
+ * @param modified when this hub configuration was modified
+ */
+public record HubDetailsData(long id, String fqdn, String rootCA, String gpgKey, String sccUsername, Date created,
+                             Date modified) {
 
     /**
      * Create an instance from the hub entity.
      * @param hub the hub
      */
     public HubDetailsData(IssHub hub) {
-        this.id = hub.getId();
-        this.fqdn = hub.getFqdn();
-        this.rootCA = hub.getRootCa();
-        this.gpgKey = hub.getGpgKey();
-        this.sccUsername = hub.getMirrorCredentials().getUsername();
-        this.created = hub.getCreated();
-        this.modified = hub.getModified();
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long idIn) {
-        this.id = idIn;
-    }
-
-    public String getFqdn() {
-        return fqdn;
-    }
-
-    public void setFqdn(String fqdnIn) {
-        this.fqdn = fqdnIn;
-    }
-
-    public String getRootCA() {
-        return rootCA;
-    }
-
-    public void setRootCA(String rootCAIn) {
-        this.rootCA = rootCAIn;
-    }
-
-    public String getGpgKey() {
-        return gpgKey;
-    }
-
-    public void setGpgKey(String gpgKeyIn) {
-        this.gpgKey = gpgKeyIn;
-    }
-
-    public String getSccUsername() {
-        return sccUsername;
-    }
-
-    public void setSccUsername(String sccUsernameIn) {
-        this.sccUsername = sccUsernameIn;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public void setCreated(Date createdIn) {
-        this.created = createdIn;
-    }
-
-    public Date getModified() {
-        return modified;
-    }
-
-    public void setModified(Date modifiedIn) {
-        this.modified = modifiedIn;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-
-        if (!(o instanceof HubDetailsData that)) {
-            return false;
-        }
-
-        return new EqualsBuilder()
-            .append(getFqdn(), that.getFqdn())
-            .isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37)
-            .append(getFqdn())
-            .toHashCode();
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("HubDetailsData{");
-        sb.append("id=").append(id);
-        sb.append(", fqdn='").append(fqdn).append('\'');
-        sb.append('}');
-        return sb.toString();
+        this(
+            hub.getId(),
+            hub.getFqdn(),
+            hub.getRootCa(),
+            hub.getGpgKey(),
+            hub.getMirrorCredentials().getUsername(),
+            hub.getCreated(),
+            hub.getModified()
+        );
     }
 }
