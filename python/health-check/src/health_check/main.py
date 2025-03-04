8418c5061e0701f84d231127c46f3dc7c54a4d2a
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2025-03-04 10:38:59 +0000

    Allow passing relative paths for the supportconfig

--- python/health-check/src/health_check/main.py
+++ python/health-check/src/health_check/main.py
@@ -72,6 +72,10 @@ def start(ctx: click.Context, from_datetime: str, to_datetime: str, since: int):
     verbose: bool = ctx.obj["verbose"]
     supportconfig_path: str | None = ctx.obj["supportconfig_path"]
 
+    # Try to resolve an absolute path to the supportconfig
+    if supportconfig_path:
+        supportconfig_path = os.path.abspath(os.path.expanduser(supportconfig_path))
+
     if not supportconfig_path or not os.path.exists(supportconfig_path):
         console.log("[red bold] Supportconfig path not accessible, exitting")
         exit(1)
