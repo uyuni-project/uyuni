64f0798014dec777354d72f36bc5ad171f182991
Michael Calmer <michael.calmer@suse.com>
2025-03-07 13:45:48 +0100

    add hibernate mapping for new action type supportdata.get

---
+++ java/code/src/com/redhat/rhn/domain/action/SupportDataAction.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2025 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ */
+
+package com.redhat.rhn.domain.action;
+
+/**
+ * SupportDataAction - Class representing TYPE_SUPPORTDATA_GET
+ */
+public class SupportDataAction extends Action {
+
+}
