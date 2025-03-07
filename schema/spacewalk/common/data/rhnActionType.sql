eff0e15ed92d07af5fa4dade6054aaaeb44dabfd
Michael Calmer <michael.calmer@suse.com>
2025-03-07 13:32:59 +0100

    DB: add new action type supportdata.get

--- schema/spacewalk/common/data/rhnActionType.sql
+++ schema/spacewalk/common/data/rhnActionType.sql
@@ -71,5 +71,6 @@ insert into rhnActionType values (506, 'channels.subscribe', 'Subscribe to chann
 insert into rhnActionType values (521, 'ansible.playbook', 'Execute an Ansible playbook', 'N', 'N', 'N');
 insert into rhnActionType values (523, 'coco.attestation', 'Confidential Compute Attestation', 'N', 'N', 'N');
 insert into rhnActionType values (524, 'appstreams.configure', 'Configure AppStreams in a system', 'N', 'N', 'N');
-commit;
 insert into rhnActionType values (525, 'ansible.inventory', 'Refresh Ansible inventories', 'N', 'N', 'N');
+insert into rhnActionType values (526, 'supportdata.get', 'Get supportdata from a system', 'N', 'N', 'N');
+commit;
