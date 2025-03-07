4ca54c3dac58fe778ebe3491ed08eb26787faabe
Thomas Florio <thomas.florio@suse.com>
2025-03-07 11:25:37 +0100

    Refactored the notification frontend component to avoid repeating the types.

--- web/html/src/manager/notifications/notification-messages.renderer.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-
-import SpaRenderer from "core/spa/spa-renderer";
-
-import { NotificationMessages } from "./notification-messages";
-
-export const renderer = () =>
-  SpaRenderer.renderNavigationReact(<NotificationMessages />, document.getElementById("notification-messages"));
