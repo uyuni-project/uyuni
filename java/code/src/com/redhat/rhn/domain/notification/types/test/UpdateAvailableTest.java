9b78e9003939730cecb24459ad0c3f4f761dbcd0
Thomas Florio <thomas.florio@suse.com>
2025-03-07 11:25:36 +0100

    Renamed existing message types to uppercase

--- java/code/src/com/redhat/rhn/domain/notification/types/test/UpdateAvailableTest.java
+++ java/code/src/com/redhat/rhn/domain/notification/types/test/UpdateAvailableTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023--2024 SUSE LLC
+ * Copyright (c) 2023--2025 SUSE LLC
  *
  * This software is licensed to you under the GNU General Public License,
  * version 2 (GPLv2). There is NO WARRANTY for this software, express or
@@ -7,10 +7,6 @@
  * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
  * along with this software; if not, see
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
- *
- * Red Hat trademarks are not licensed under GPLv2. No permission is
- * granted to use or replicate Red Hat trademarks that are incorporated
- * in this software or its documentation.
  */
 package com.redhat.rhn.domain.notification.types.test;
 
@@ -79,7 +75,7 @@ public class UpdateAvailableTest {
         };
 
         assertTrue(updateAvailable.hasUpdateAvailable());
-        assertEquals(NotificationType.UpdateAvailable, updateAvailable.getType());
+        assertEquals(NotificationType.UPDATE_AVAILABLE, updateAvailable.getType());
         assertEquals(NotificationMessage.NotificationMessageSeverity.WARNING, updateAvailable.getSeverity());
         assertEquals("Updates are available.", updateAvailable.getSummary());
     }
@@ -129,7 +125,7 @@ public class UpdateAvailableTest {
         };
 
         assertTrue(updateAvailable.hasUpdateAvailable());
-        assertEquals(NotificationType.UpdateAvailable, updateAvailable.getType());
+        assertEquals(NotificationType.UPDATE_AVAILABLE, updateAvailable.getType());
         assertEquals(NotificationMessage.NotificationMessageSeverity.WARNING, updateAvailable.getSeverity());
         assertEquals("Updates are available.", updateAvailable.getSummary());
     }
