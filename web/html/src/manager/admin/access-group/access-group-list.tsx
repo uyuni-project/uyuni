c546fc07e7dc2e6649e7bd125863c63e4f68067d
Richa <richa.bisht@suse.com>
2025-03-13 10:33:37 +0100

    Added changelog and fix failing tests

--- web/html/src/manager/admin/access-group/access-group-list.tsx
+++ web/html/src/manager/admin/access-group/access-group-list.tsx
@@ -1,68 +1,69 @@
 import * as React from "react";
-import { useState } from "react";
-import { TopPanel } from "components/panels/TopPanel";
+
 import { Button } from "components/buttons";
+import { TopPanel } from "components/panels/TopPanel";
 import { Column } from "components/table/Column";
-import { Table } from "components/table/Table";
-import { Utils } from "utils/functions";
 import { SearchField } from "components/table/SearchField";
+import { Table } from "components/table/Table";
+
+import { Utils } from "utils/functions";
 
 const dataTest = {
-  "items": [
+  items: [
     {
-      "id": 1000010000,
-      "name": "Content Management",
-      "description": "View image details, patches, packages, build log and cluster information",
-      "type": "Built-in",
-      "users": 0,
-      "permissions": 15,
-      "selected": false,
-      "disabled": false
+      id: 1000010000,
+      name: "Content Management",
+      description: "View image details, patches, packages, build log and cluster information",
+      type: "Built-in",
+      users: 0,
+      permissions: 15,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000020000,
-      "name": "Activation Key Administrator",
-      "description": "View Activation Administrator, Configuration Administrator.",
-      "type": "Built-in",
-      "users": 2,
-      "permissions": 5,
-      "selected": false,
-      "disabled": false
+      id: 1000020000,
+      name: "Activation Key Administrator",
+      description: "View Activation Administrator, Configuration Administrator.",
+      type: "Built-in",
+      users: 2,
+      permissions: 5,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000030000,
-      "name": "Configuration Administrator",
-      "description": "View Configuration Administrator Administrator, Configuration Administrator.",
-      "type": "Built-in",
-      "users": 4,
-      "permissions": 15,
-      "selected": false,
-      "disabled": false
+      id: 1000030000,
+      name: "Configuration Administrator",
+      description: "View Configuration Administrator Administrator, Configuration Administrator.",
+      type: "Built-in",
+      users: 4,
+      permissions: 15,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000040000,
-      "name": "System Group Administrator",
-      "description": "View Activation Administrator, Configuration Administrator.",
-      "type": "Built-in",
-      "users": 2,
-      "permissions": 5,
-      "selected": false,
-      "disabled": false
+      id: 1000040000,
+      name: "System Group Administrator",
+      description: "View Activation Administrator, Configuration Administrator.",
+      type: "Built-in",
+      users: 2,
+      permissions: 5,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000050000,
-      "name": "Custom group1",
-      "description": "Custom group1 Activation Administrator, Configuration Administrator.",
-      "type": "Custome",
-      "users": 7,
-      "permissions": 25,
-      "selected": false,
-      "disabled": false
+      id: 1000050000,
+      name: "Custom group1",
+      description: "Custom group1 Activation Administrator, Configuration Administrator.",
+      type: "Custome",
+      users: 7,
+      permissions: 25,
+      selected: false,
+      disabled: false,
     },
   ],
-  "total": 19,
-  "selectedIds": []
-}
+  total: 19,
+  selectedIds: [],
+};
 
 const actionButtons = (type) => {
   if (type === "Built-in") {
@@ -70,17 +71,18 @@ const actionButtons = (type) => {
       <div className="btn-group">
         <Button className="btn-default btn-sm" icon="fa-user" />
       </div>
-    )
+    );
   } else {
-    return (<div className="btn-group">
-      <Button className="btn-default btn-sm" icon="fa-user" />
-      <Button className="btn-default btn-sm" icon="fa-pencil" />
-      <Button className="btn-default btn-sm" icon="fa-trash" />
-    </div>)
+    return (
+      <div className="btn-group">
+        <Button className="btn-default btn-sm" icon="fa-user" />
+        <Button className="btn-default btn-sm" icon="fa-pencil" />
+        <Button className="btn-default btn-sm" icon="fa-trash" />
+      </div>
+    );
   }
-}
+};
 export function AccessGroupList(props) {
-  const [systemsData, setSystemsData] = useState(dataTest);
   const addAccessGroup = () => {
     window.pageRenderers?.spaengine?.navigate?.(`/rhn/manager/admin/access-group/create`);
   };
@@ -99,37 +101,22 @@ export function AccessGroupList(props) {
       }
     >
       <Table
-        data={systemsData.items}
+        data={dataTest.items}
         identifier={(item) => item.id}
         initialSortColumnKey="group_name"
         emptyText={t("No Access Group found.")}
         searchField={<SearchField placeholder={t("Filter by name")} />}
       >
-        <Column
-          columnKey="group_name"
-          comparator={Utils.sortByText}
-          header={t("Name")}
-          cell={(item) => item.name}
-        />
+        <Column columnKey="group_name" comparator={Utils.sortByText} header={t("Name")} cell={(item) => item.name} />
         <Column
           columnKey="group_description"
           comparator={Utils.sortByText}
           header={t("Description")}
           cell={(item) => item.description}
         />
-        <Column
-          columnKey="group_type"
-          comparator={Utils.sortByText}
-          header={t("Type")}
-          cell={(item) => item.type}
-        />
+        <Column columnKey="group_type" comparator={Utils.sortByText} header={t("Type")} cell={(item) => item.type} />
 
-        <Column
-          columnKey="group_users"
-          comparator={Utils.sortByText}
-          header={t("Users")}
-          cell={(item) => item.users}
-        />
+        <Column columnKey="group_users" comparator={Utils.sortByText} header={t("Users")} cell={(item) => item.users} />
 
         <Column
           columnKey="group_permissions"
@@ -146,4 +133,4 @@ export function AccessGroupList(props) {
       </Table>
     </TopPanel>
   );
-};
+}
--- web/html/src/manager/admin/access-group/access-group-permissions.tsx
+++ web/html/src/manager/admin/access-group/access-group-permissions.tsx
@@ -1,176 +1,176 @@
 import * as React from "react";
-import { useState } from "react";
-import { Column } from "components/table/Column";
-import { Table } from "components/table/Table";
-import { Utils } from "utils/functions";
+
 import { Button } from "components/buttons";
+import { Column } from "components/table/Column";
 import { SearchField } from "components/table/SearchField";
+import { Table } from "components/table/Table";
+
+import { Utils } from "utils/functions";
 
 const dataTest = {
-  "items": [
+  items: [
     {
-      "id": 1000010000,
-      "name": "Content Management",
-      "description": "View image details, patches, packages, build log and cluster information",
-      "type": "Built-in",
-      "users": 0,
-      "permissions": 15,
-      "selected": false,
-      "disabled": false
+      id: 1000010000,
+      name: "Content Management",
+      description: "View image details, patches, packages, build log and cluster information",
+      type: "Built-in",
+      users: 0,
+      permissions: 15,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000020000,
-      "name": "Activation Key Administrator",
-      "description": "View Activation Administrator, Configuration Administrator.",
-      "type": "Built-in",
-      "users": 2,
-      "permissions": 5,
-      "selected": false,
-      "disabled": false
+      id: 1000020000,
+      name: "Activation Key Administrator",
+      description: "View Activation Administrator, Configuration Administrator.",
+      type: "Built-in",
+      users: 2,
+      permissions: 5,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000030000,
-      "name": "Configuration Administrator",
-      "description": "View Configuration Administrator Administrator, Configuration Administrator.",
-      "type": "Built-in",
-      "users": 4,
-      "permissions": 15,
-      "selected": false,
-      "disabled": false
+      id: 1000030000,
+      name: "Configuration Administrator",
+      description: "View Configuration Administrator Administrator, Configuration Administrator.",
+      type: "Built-in",
+      users: 4,
+      permissions: 15,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000040000,
-      "name": "System Group Administrator",
-      "description": "View Activation Administrator, Configuration Administrator.",
-      "type": "Built-in",
-      "users": 2,
-      "permissions": 5,
-      "selected": false,
-      "disabled": false
+      id: 1000040000,
+      name: "System Group Administrator",
+      description: "View Activation Administrator, Configuration Administrator.",
+      type: "Built-in",
+      users: 2,
+      permissions: 5,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000050000,
-      "name": "Custom group1",
-      "description": "Custom group1 Activation Administrator, Configuration Administrator.",
-      "type": "Custome",
-      "users": 7,
-      "permissions": 25,
-      "selected": false,
-      "disabled": false
+      id: 1000050000,
+      name: "Custom group1",
+      description: "Custom group1 Activation Administrator, Configuration Administrator.",
+      type: "Custome",
+      users: 7,
+      permissions: 25,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000060000,
-      "name": "Content Management",
-      "description": "View image details, patches, packages, build log and cluster information",
-      "type": "Built-in",
-      "users": 0,
-      "permissions": 15,
-      "selected": false,
-      "disabled": false
+      id: 1000060000,
+      name: "Content Management",
+      description: "View image details, patches, packages, build log and cluster information",
+      type: "Built-in",
+      users: 0,
+      permissions: 15,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000070000,
-      "name": "Activation Key Administrator",
-      "description": "View Activation Administrator, Configuration Administrator.",
-      "type": "Built-in",
-      "users": 2,
-      "permissions": 5,
-      "selected": false,
-      "disabled": false
+      id: 1000070000,
+      name: "Activation Key Administrator",
+      description: "View Activation Administrator, Configuration Administrator.",
+      type: "Built-in",
+      users: 2,
+      permissions: 5,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000080000,
-      "name": "Configuration Administrator",
-      "description": "View Configuration Administrator Administrator, Configuration Administrator.",
-      "type": "Built-in",
-      "users": 4,
-      "permissions": 15,
-      "selected": false,
-      "disabled": false
+      id: 1000080000,
+      name: "Configuration Administrator",
+      description: "View Configuration Administrator Administrator, Configuration Administrator.",
+      type: "Built-in",
+      users: 4,
+      permissions: 15,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000090000,
-      "name": "System Group Administrator",
-      "description": "View Activation Administrator, Configuration Administrator.",
-      "type": "Built-in",
-      "users": 2,
-      "permissions": 5,
-      "selected": false,
-      "disabled": false
+      id: 1000090000,
+      name: "System Group Administrator",
+      description: "View Activation Administrator, Configuration Administrator.",
+      type: "Built-in",
+      users: 2,
+      permissions: 5,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000010000,
-      "name": "Custom group1",
-      "description": "Custom group1 Activation Administrator, Configuration Administrator.",
-      "type": "Custome",
-      "users": 7,
-      "permissions": 25,
-      "selected": false,
-      "disabled": false
-    }, {
-      "id": 1000010000,
-      "name": "Content Management",
-      "description": "View image details, patches, packages, build log and cluster information",
-      "type": "Built-in",
-      "users": 0,
-      "permissions": 15,
-      "selected": false,
-      "disabled": false
+      id: 1000010000,
+      name: "Custom group1",
+      description: "Custom group1 Activation Administrator, Configuration Administrator.",
+      type: "Custome",
+      users: 7,
+      permissions: 25,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000011000,
-      "name": "Activation Key Administrator",
-      "description": "View Activation Administrator, Configuration Administrator.",
-      "type": "Built-in",
-      "users": 2,
-      "permissions": 5,
-      "selected": false,
-      "disabled": false
+      id: 1000010000,
+      name: "Content Management",
+      description: "View image details, patches, packages, build log and cluster information",
+      type: "Built-in",
+      users: 0,
+      permissions: 15,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000012000,
-      "name": "Configuration Administrator",
-      "description": "View Configuration Administrator Administrator, Configuration Administrator.",
-      "type": "Built-in",
-      "users": 4,
-      "permissions": 15,
-      "selected": false,
-      "disabled": false
+      id: 1000011000,
+      name: "Activation Key Administrator",
+      description: "View Activation Administrator, Configuration Administrator.",
+      type: "Built-in",
+      users: 2,
+      permissions: 5,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000013000,
-      "name": "System Group Administrator",
-      "description": "View Activation Administrator, Configuration Administrator.",
-      "type": "Built-in",
-      "users": 2,
-      "permissions": 5,
-      "selected": false,
-      "disabled": false
+      id: 1000012000,
+      name: "Configuration Administrator",
+      description: "View Configuration Administrator Administrator, Configuration Administrator.",
+      type: "Built-in",
+      users: 4,
+      permissions: 15,
+      selected: false,
+      disabled: false,
     },
     {
-      "id": 1000014000,
-      "name": "Custom group1",
-      "description": "Custom group1 Activation Administrator, Configuration Administrator.",
-      "type": "Custome",
-      "users": 7,
-      "permissions": 25,
-      "selected": false,
-      "disabled": false
+      id: 1000013000,
+      name: "System Group Administrator",
+      description: "View Activation Administrator, Configuration Administrator.",
+      type: "Built-in",
+      users: 2,
+      permissions: 5,
+      selected: false,
+      disabled: false,
+    },
+    {
+      id: 1000014000,
+      name: "Custom group1",
+      description: "Custom group1 Activation Administrator, Configuration Administrator.",
+      type: "Custome",
+      users: 7,
+      permissions: 25,
+      selected: false,
+      disabled: false,
     },
   ],
-  "total": 19,
-  "selectedIds": []
-}
+  total: 19,
+  selectedIds: [],
+};
 
 type Props = {
-  state: any,
-  onChange: Function,
-  errors: any
+  state: any;
+  onChange: Function;
+  errors: any;
 };
 
 const AccessGroupPermissions = (props: Props) => {
-  const [systemsData, setSystemsData] = useState(dataTest);
-
   return (
     <div>
       <div className="d-flex">
@@ -184,23 +184,20 @@ const AccessGroupPermissions = (props: Props) => {
         </div>
       </div>
       <hr></hr>
-      <p>Click <strong>Add Permissions</strong> to select the permissions you want to add to this custom Access group.</p>
+      <p>
+        Click <strong>Add Permissions</strong> to select the permissions you want to add to this custom Access group.
+      </p>
       <div className="d-block mb-3">
         <Button className="btn-primary pull-right" text="Add Permissions" />
       </div>
       <Table
-        data={systemsData.items}
+        data={dataTest.items}
         identifier={(item) => item.id}
         initialSortColumnKey="server_name"
         emptyText={t("No Permissions found.")}
         searchField={<SearchField placeholder={t("Filter by name")} />}
       >
-        <Column
-          columnKey="name"
-          comparator={Utils.sortByText}
-          header={t("Name")}
-          cell={(item) => item.name}
-        />
+        <Column columnKey="name" comparator={Utils.sortByText} header={t("Name")} cell={(item) => item.name} />
         <Column
           columnKey="description"
           comparator={Utils.sortByText}
@@ -211,29 +208,18 @@ const AccessGroupPermissions = (props: Props) => {
           columnKey="view"
           comparator={Utils.sortByText}
           header={t("View")}
-          cell={<input
-            name="view"
-            type="checkbox"
-          />}
+          cell={<input name="view" type="checkbox" />}
         />
 
         <Column
           columnKey="modify"
           comparator={Utils.sortByText}
           header={t("Modify")}
-          cell={<input
-            name="Modify"
-            type="checkbox"
-          />}
+          cell={<input name="Modify" type="checkbox" />}
         />
-        <Column
-          columnKey="count"
-          comparator={Utils.sortByText}
-          header={t("Count")}
-          cell={(item) => item.permissions}
-        />
+        <Column columnKey="count" comparator={Utils.sortByText} header={t("Count")} cell={(item) => item.permissions} />
       </Table>
-    </div >
+    </div>
   );
 };
 
