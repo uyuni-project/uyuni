bc347166bafc4d12044fd4158267ebb71385ca47
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2025-03-14 10:40:33 +0100

    Update add_packages.sh: do not refresh

--- testsuite/features/profiles/github_runner/Docker/add_packages.sh
+++ testsuite/features/profiles/github_runner/Docker/add_packages.sh
@@ -5,7 +5,7 @@ cp /root/avahi-daemon.conf /etc/avahi/avahi-daemon.conf
 /usr/sbin/avahi-daemon -D
 
 # do the real test
-zypper --non-interactive --gpg-auto-import-keys ref
+# zypper --non-interactive --gpg-auto-import-keys ref
 
 # zypper --non-interactive in hoag-dummy orion-dummy
 # zypper --non-interactive up milkyway-dummy
