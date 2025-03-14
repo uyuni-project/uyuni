f7d888088178626c71c36f75fd700a5944575afa
Richa <richa.bisht@suse.com>
2025-03-14 14:05:40 +0100

    Fix lint

--- web/html/src/components/steps-progress-bar.tsx
+++ web/html/src/components/steps-progress-bar.tsx
@@ -43,8 +43,9 @@ const StepsProgressBar = ({ steps, onCreate, onCancel }: StepsProgressBarProps) 
           {steps.map((step, index) => (
             <div
               key={step.title}
-              className={`steps ${index < currentStep ? "completed" : index === currentStep ? "active" : ""}`}>
-              <div className="step-circle">{index < currentStep ? (<i className="fa fa-check"> </i>) : null}</div>
+              className={`steps ${index < currentStep ? "completed" : index === currentStep ? "active" : ""}`}
+            >
+              <div className="step-circle">{index < currentStep ? <i className="fa fa-check"> </i> : null}</div>
               <div className="step-title">{step.title}</div>
               <div className="step-line">
                 <span></span>
@@ -54,24 +55,14 @@ const StepsProgressBar = ({ steps, onCreate, onCancel }: StepsProgressBarProps) 
         </div>
         <div className="main-content">
           <div className="content-section">
-            {steps.map((step, index) =>
-              currentStep === index ? (
-                <div key={step.title}>{step.content}</div>
-              ) : null
-            )}
+            {steps.map((step, index) => (currentStep === index ? <div key={step.title}>{step.content}</div> : null))}
           </div>
         </div>
       </div>
       <div className="progress-bar-footer">
-        <LinkButton className="btn-default btn-sm pull-left" text="Cancel" href={onCancel} />
+        <LinkButton className="btn-default btn-sm pull-left" text={t("Cancel")} href={onCancel} />
         <div className="pull-right">
-          {currentStep != 0 ? (
-            <Button
-              className={`btn-default btn-sm me-3`}
-              text="Back"
-              handler={prevStep}
-            />
-          ) : null}
+          {currentStep !== 0 ? <Button className={`btn-default btn-sm me-3`} text="Back" handler={prevStep} /> : null}
           <Button
             className="btn-primary btn-sm"
             text={`${currentStep === steps.length - 1 ? "Create" : "Continue"}`}
