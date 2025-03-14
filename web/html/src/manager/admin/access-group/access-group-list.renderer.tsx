8989bc45b77bc7dd73d347b3934661120dd27833
Richa <richa.bisht@suse.com>
2025-03-14 13:58:41 +0100

    Removed fixed offset for fulll height, fixed lints and other comments

--- web/html/src/manager/admin/access-group/access-group-list.renderer.tsx
+++ web/html/src/manager/admin/access-group/access-group-list.renderer.tsx
@@ -1,11 +1,9 @@
 import * as React from "react";
 
 import SpaRenderer from "core/spa/spa-renderer";
+
 import { AccessGroupList } from "./access-group-list";
 
 export const renderer = (id: string) => {
-  SpaRenderer.renderNavigationReact(
-    <AccessGroupList />,
-    document.getElementById(id)
-  );
+  SpaRenderer.renderNavigationReact(<AccessGroupList />, document.getElementById(id));
 };
