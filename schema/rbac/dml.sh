fefb657ba073965da5121309a66667c684b50e38
Can Bulut Bayburt <cbbayburt@suse.com>
2025-03-20 18:35:12 +0100

    fixup! Update SQL generator scripts

--- schema/rbac/dml.sh
+++ schema/rbac/dml.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #
 # Generate RBAC data DML and execute on container server
+#
+# $1: namespace to grant access recursively to the 'test'
+# user (id=34) after all endpoints and namespaces are inserted
+# If not provided, unlimited access is granted.
 
 cat <(python3 generate_dml.py data/endpoints/*) \
-    <(echo "call access.grant_access(34, '%');") \
+    <(echo "call access.grant_access(34, '$1%');") \
     | mgrctl exec -i -- spacewalk-sql -i \
     && echo "RBAC data inserted."
