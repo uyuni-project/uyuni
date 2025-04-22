0ce6413832ca7a268ce215fc1e9d6d5f44e28335
naibu3 <pablipoyo@gmail.com>
2025-04-22 20:06:00 +0200

    Dowloading module finished

--- python/spacewalk/server/vex/main.py
+++ python/spacewalk/server/vex/main.py
@@ -3,6 +3,7 @@ import logging
 
 from vex.parser.csaf_parser import CSAFParser
 from vex.parser.cyclonedx_parser import CycloneDXParser
+from vex.downloader.downloader import Downloader
 #from vex.parser.openvex_parser import OpenVEXParser
 
 from vex.utils import detect_vex_format
@@ -11,37 +12,43 @@ logging.basicConfig(level=logging.INFO)
 
 def main():
     parser = argparse.ArgumentParser(description="VEX Parser")
-    parser.add_argument("-f", "--file", required=True, help="Path to the VEX document")
+    parser.add_argument("-f", "--file", required=False, help="Path to the VEX document")
     parser.add_argument("-x", "--format", required=False, choices=["csaf", "cyclonedx", "openvex"], help="VEX format", default="")
     parser.add_argument("-p", "--persist", action="store_true", required=False, help="Persist to the database")
     parser.add_argument("-i", "--info", action="store_true", required=False, help="Display VEX info")
+    parser.add_argument("-d", "--downloader", action="store_true", required=False, help="Download vex files")
     args = parser.parse_args()
 
-    if args.format == "csaf":
-        parser = CSAFParser()
-        logging.info("CSAF format selected.")
-    elif args.format == "cyclonedx":
-        parser = CycloneDXParser()
-        logging.info("CycloneDX format selected.")
-    elif args.format == "openvex":
-        #parser = OpenVEXParser()
-        logging.info("OpenVEX format selected.")
-        pass
-    else:
-        logging.info("No format specified, trying to auto-detection.")
-        parser = detect_vex_format(args.file)
+    if args.file:
+        if args.format == "csaf":
+            parser = CSAFParser()
+            logging.info("CSAF format selected.")
+        elif args.format == "cyclonedx":
+            parser = CycloneDXParser()
+            logging.info("CycloneDX format selected.")
+        elif args.format == "openvex":
+            #parser = OpenVEXParser()
+            logging.info("OpenVEX format selected.")
+            pass
+        else:
+            logging.info("No format specified, trying to auto-detection.")
+            parser = detect_vex_format(args.file)
 
+        parser.parse(args.file)
+        vulns =  parser.get_vulnerabilities()
 
-    parser.parse(args.file)
-    vulns =  parser.get_vulnerabilities()
+        if args.info:
+            for vuln in vulns:
+                #print(vuln)
+                vuln.show_vulnerability()
 
-    if args.info:
-        for vuln in vulns:
-            #print(vuln)
-            vuln.show_vulnerability()
+        if args.persist:
+            parser.persist_data("no")
 
-    if args.persist:
-        parser.persist_data()
+    if args.downloader:
+        downloader = Downloader(CSAFParser())
+
+        downloader.download()
 
 if __name__ == "__main__":
     main()
--- transfer.sh
+++ transfer.sh
@@ -1,26 +1,26 @@
 # Script to ease the development of the VEX processing module
 
-podman --log-level=debug system connection add server ssh://root@uyuni-exile.test
+SERVER="uyuni.test"
+
+podman --log-level=debug system connection add server ssh://root@$SERVER
 podman system connection ls # Debug
 export CONTAINER_CONNECTION=server # NOTE: this will only apply to the current shell instance  
 echo $CONTAINER_CONNECTION # Debug
 
+scp ../uyuni-VEX/schema/spacewalk/common/tables/suseVEXAnnotations.sql root@$SERVER:/tmp/
+scp ../uyuni-VEX/schema/spacewalk/common/tables/suseVEXHash.sql root@$SERVER:/tmp/
+ssh root@$SERVER "mgrctl cp /tmp/suseVEXAnnotations.sql server:/tmp/"
+ssh root@$SERVER "mgrctl cp /tmp/suseVEXHash.sql server:/tmp/"
 #ssh root@uyuni.test "sudo psql -U spacewalk -d susemanager -f suseVEXAnnotations.sql"
+#ssh root@uyuni.test "sudo psql -U spacewalk -d susemanager -f suseVEXHash.sql"
 
-ssh root@uyuni-exile.test "rm /tmp/vex-processor/ -r"
+ssh root@$SERVER "rm /tmp/vex-processor/ -r"
+scp -r ../uyuni-VEX/python/spacewalk/server/vex root@$SERVER:/tmp/vex-processor
+ssh root@$SERVER "mgrctl cp /tmp/vex-processor server:/tmp/"
 
-scp ../uyuni-VEX/schema/spacewalk/common/tables/suseVEXAnnotations.sql root@uyuni-exile.test:/tmp/
-scp -r ../uyuni-VEX/python/spacewalk/server/vex root@uyuni-exile.test:/tmp/vex-processor
+ssh root@$SERVER "mgrctl exec 'python3 -m venv /tmp/vex-processor/venv --system-site-packages'"
 
-ssh root@uyuni-exile.test "mgrctl cp /tmp/suseVEXAnnotations.sql server:/tmp/"
-ssh root@uyuni-exile.test "mgrctl cp /tmp/vex-processor server:/tmp/"
-
-ssh root@uyuni-exile.test "mgrctl exec 'python3 -m venv /tmp/vex-processor/venv --system-site-packages'"
-ssh root@uyuni-exile.test "mgrctl exec  '. /tmp/vex-processor/venv/bin/activate'"
-
-ssh root@uyuni-exile.test "mgrctl exec  './tmp/vex-processor/venv/bin/pip install pytest'"
-ssh root@uyuni-exile.test "mgrctl exec  './tmp/vex-processor/venv/bin/pip install packageurl-python'"
-ssh root@uyuni-exile.test "mgrctl exec  './tmp/vex-processor/venv/bin/pip install psycopg2'"
+ssh root@$SERVER "mgrctl exec  '/tmp/vex-processor/venv/bin/pip install pytest packageurl-python psycopg2'"
 
 #psql -U spacewalk -d susemanager -c "SELECT * FROM rhncve;"
 #psql -U spacewalk -d susemanager -c "SELECT * FROM suseovalplatform;"
