b128c4fc2730079c426ed7ffdb73da2a64645814
vzhestkov <vzhestkov@suse.com>
2025-04-23 10:43:20 +0200

    Optimize listing sap instances
    Improve test_get_workloads
    
    Add changelog entry

--- susemanager-utils/susemanager-sls/src/tests/test_module_sap.py
+++ susemanager-utils/susemanager-sls/src/tests/test_module_sap.py
@@ -1,34 +1,64 @@
 """
 Unit tests for the sap module
 """
+
 from ..modules import sap
 
 from unittest.mock import patch, MagicMock
 import pytest
 
+
 @pytest.mark.parametrize(
-    "mock_walk_return, expected_result",
+    "dir_struct, expected_result",
     [
         (
             [
-                ("/usr/sap", ["F1B", "F2C"], []),
-                ("/usr/sap/F1B", ["ASCS00", "DVEBMGS00"], []),
-                ("/usr/sap/F2C", ["HDB00"], []),
+                ("F1B", ("ASCS00", "DVEBMGS00", "AZCC00")),
+                ("F3C", ("HBA00",)),
+                ("F2C", ("HDB00",)),
             ],
             [
                 {"system_id": "F1B", "instance_type": "ASCS"},
+                {"system_id": "F1B", "instance_type": "AZCC"},
                 {"system_id": "F1B", "instance_type": "DVEBMGS"},
+                {"system_id": "F2C", "instance_type": "HDB"},
+                {"system_id": "F3C", "instance_type": "HBA"},
+            ],
+        ),
+        (
+            [
+                ("F2C", ("HDB00",)),
+            ],
+            [
                 {"system_id": "F2C", "instance_type": "HDB"},
             ],
         ),
         ([], []),
     ],
 )
-def test_get_workloads(mock_walk_return, expected_result):
-    mock_os_path_exists = MagicMock(return_value=True)
-    mock_os_walk = MagicMock(return_value=mock_walk_return)
-    with patch("os.path.exists", mock_os_path_exists), patch("os.walk", mock_os_walk):
+def test_get_workloads(tmpdir, dir_struct, expected_result):
+    for sap_dir1, sap_dirs2 in dir_struct:
+        tmp_sap_dir1 = tmpdir.mkdir(sap_dir1)
+        for sap_dir2 in sap_dirs2:
+            tmp_sap_dir1.mkdir(sap_dir2)
+
+    orig_sap_regex = sap.SAP_REGEX
+
+    tmpdir_path = str(tmpdir)
+
+    def mock_match(s):
+        if s.startswith(tmpdir_path):
+            s = "/usr/sap" + s[len(tmpdir_path) :]
+        return orig_sap_regex.match(s)
+
+    mock_sap_regex = MagicMock()
+    mock_sap_regex.match = mock_match
+
+    with patch.object(sap, "SAP_BASE_PATH", tmpdir), patch.object(
+        sap, "SAP_REGEX", mock_sap_regex
+    ):
         assert sap.get_workloads() == expected_result
+
 
 def test_no_sap_directory():
     with patch("os.path.exists", return_value=False):
