1719a529ea5a872edba0f649a2f9d23b79c77b84
Michael Calmer <michael.calmer@suse.com>
2025-05-07 16:26:55 +0200

    calculate the proxy command for ssh to use proxies as jump hosts
    fixup! implement salt ssh support for supportdata action

--- java/code/src/com/suse/manager/utils/SaltUtils.java
+++ java/code/src/com/suse/manager/utils/SaltUtils.java
@@ -975,12 +975,23 @@ public class SaltUtils {
         if (minionServer.isSSHPush()) {
             try {
                 var actionPath = MinionActionUtils.getActionPath(minionServer, actionId);
-
-                //TODO: add additional options required for tunnel
                 var user = SaltSSHService.getSSHUser();
                 var port = Optional.ofNullable(minionServer.getSSHPushPort()).orElse(SaltSSHService.SSH_PUSH_PORT);
-                var rsync = "rsync -p --chown salt:susemanager --chmod=660 -e 'ssh -o ConnectTimeout=2 -p %d -i %s' %s@%s:%s/* %s/."
-                        .formatted(port, SaltSSHService.SSH_KEY_PATH, user, hostname, supportDataDir, actionPath);
+                List<String> proxyPath = SaltSSHService.proxyPathToHostnames(minionServer.getServerPaths(),
+                        Optional.empty());
+                Optional<List<String>> proxyCommand = SaltSSHService.sshProxyCommandOption(proxyPath,
+                        minionServer.getContactMethod().getLabel(),
+                        minionServer.getMinionId(),
+                        Optional.ofNullable(minionServer.getSSHPushPort()).orElse(SaltSSHService.SSH_PUSH_PORT));
+
+                String sshOptions = "-o ConnectTimeout=2 " + String.join(" ",
+                        proxyCommand
+                                .map(l -> l.stream().map("-o %s"::formatted).collect(Collectors.toList()))
+                                .orElse(new ArrayList<>()));
+                var rsync = "rsync -p --chown salt:susemanager --chmod=660 -e \"ssh %s -p %d -i %s\" %s@%s:%s/* %s/."
+                        .formatted(sshOptions, port, SaltSSHService.SSH_KEY_PATH, user, hostname,
+                                supportDataDir, actionPath);
+                LOG.info(rsync);
                 var copyResult = saltApi.execOnMaster(rsync);
                 String error = copyResult.orElse("Error copying supportdata");
                 if (!error.isBlank()) {
