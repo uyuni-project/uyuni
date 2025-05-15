196cdeab8d3438d57369429b19650ccb8f935200
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2025-05-15 10:45:59 +0200

    Update 20_run_recommended_tests.sh

--- testsuite/podman_runner/20_run_recommended_tests.sh
+++ testsuite/podman_runner/20_run_recommended_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 
-sudo -i podman exec controller bash -c "export CUCUMBER_PUBLISH_TOKEN=${CUCUMBER_PUBLISH_TOKEN} && export PROVIDER=podman && export SERVER=server && export HOSTNAME=controller && export SSH_MINION=opensusessh && export MINION=sleminion && export RHLIKE_MINION=rhlikeminion && cd /testsuite && export TAGS=\"\\\"not @flaky\\\"\" && cat run_sets/recommended_tests.yml &&  rake cucumber:recommended_tests"
+sudo -i podman exec controller bash -c "export CUCUMBER_PUBLISH_TOKEN=${CUCUMBER_PUBLISH_TOKEN} && export PROVIDER=podman && export SERVER=server.test.lan && export HOSTNAME=controller && export SSH_MINION=opensusessh && export MINION=sleminion && export RHLIKE_MINION=rhlikeminion && cd /testsuite && export TAGS=\"\\\"not @flaky\\\"\" && cat run_sets/recommended_tests.yml &&  rake cucumber:recommended_tests"
