040b044669332237586f589bc2e1c080caa596af
Carlo Della Giusta <carlo.dellagiusta@suse.com>
2025-05-22 13:15:38 +0200

    Fix missing scc_proxy_status_t type creation in table creation

--- schema/spacewalk/common/tables/suseSccProxy.sql
+++ schema/spacewalk/common/tables/suseSccProxy.sql
@@ -13,6 +13,12 @@
 -- in this software or its documentation.
 --
 
+CREATE TYPE scc_proxy_status_t AS ENUM (
+    'scc_creation_pending',
+    'scc_created',
+    'scc_removal_pending',
+    'scc_virthost_pending'
+);
 
 CREATE TABLE suseSccProxy
 (
