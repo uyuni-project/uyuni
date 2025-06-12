622d9f8924b98a69c78c0ef77ee2b64d9f6064d1
Richa <richa.bisht@suse.com>
2025-06-12 14:24:21 +0200

    Fix faling test for tooltip title

--- web/html/src/components/states-picker.tsx
+++ web/html/src/components/states-picker.tsx
@@ -281,7 +281,11 @@ class StatesPicker extends React.Component<StatesPickerProps, StatesPickerState>
           </td>
           <td>{currentChannel.label}</td>
           <td>
-            <i data-bs-toggle="tooltip" className="fa fa-info-circle fa-1-5x text-primary" title={currentChannel.description} />
+            <i
+              data-bs-toggle="tooltip"
+              className="fa fa-info-circle fa-1-5x text-primary"
+              title={currentChannel.description}
+            />
           </td>
           <td>
             <div className="form-group">
