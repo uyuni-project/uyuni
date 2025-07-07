eed84e7a6d01d1973c121bd1fb06ea00abcaa674
Carlo Della Giusta <carlo.dellagiusta@suse.com>
2025-07-07 16:42:30 +0200

    moving action handling to KickstartInitiateAction

--- java/code/src/com/suse/manager/webui/services/SaltServerActionService.java
+++ java/code/src/com/suse/manager/webui/services/SaltServerActionService.java
@@ -47,7 +47,6 @@ import com.redhat.rhn.domain.action.config.ConfigAction;
 import com.redhat.rhn.domain.action.dup.DistUpgradeAction;
 import com.redhat.rhn.domain.action.errata.ErrataAction;
 import com.redhat.rhn.domain.action.kickstart.KickstartAction;
-import com.redhat.rhn.domain.action.kickstart.KickstartActionDetails;
 import com.redhat.rhn.domain.action.kickstart.KickstartInitiateAction;
 import com.redhat.rhn.domain.action.rhnpackage.PackageLockAction;
 import com.redhat.rhn.domain.action.rhnpackage.PackageRefreshListAction;
@@ -61,9 +60,6 @@ import com.redhat.rhn.domain.action.script.ScriptAction;
 import com.redhat.rhn.domain.action.script.ScriptRunAction;
 import com.redhat.rhn.domain.action.server.ServerAction;
 import com.redhat.rhn.domain.errata.Errata;
-import com.redhat.rhn.domain.kickstart.KickstartFactory;
-import com.redhat.rhn.domain.kickstart.KickstartableTree;
-import com.redhat.rhn.domain.org.OrgFactory;
 import com.redhat.rhn.domain.product.Tuple2;
 import com.redhat.rhn.domain.server.ErrataInfo;
 import com.redhat.rhn.domain.server.MinionServer;
@@ -73,7 +69,6 @@ import com.redhat.rhn.domain.server.Server;
 import com.redhat.rhn.domain.server.ServerFactory;
 import com.redhat.rhn.domain.user.User;
 import com.redhat.rhn.manager.action.ActionManager;
-import com.redhat.rhn.manager.kickstart.cobbler.CobblerXMLRPCHelper;
 import com.redhat.rhn.manager.system.SystemManager;
 import com.redhat.rhn.taskomatic.TaskomaticApi;
 import com.redhat.rhn.taskomatic.TaskomaticApiException;
@@ -113,13 +108,8 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.cobbler.CobblerConnection;
-import org.cobbler.Distro;
-import org.cobbler.Profile;
-import org.cobbler.SystemRecord;
 
 import java.io.File;
-import java.nio.file.Paths;
 import java.time.DateTimeException;
 import java.time.Duration;
 import java.time.Instant;
@@ -161,7 +151,6 @@ public class SaltServerActionService {
     private static final String PARAM_PKGS = "param_pkgs";
     private static final String PARAM_PATCHES = "param_patches";
     private static final String SYSTEM_REBOOT = "system.reboot";
-    private static final String KICKSTART_INITIATE = "bootloader.autoinstall";
     private static final String ANSIBLE_RUNPLAYBOOK = "ansible.runplaybook";
     private static final String ANSIBLE_INVENTORIES = "ansible.targets";
     private static final String COCOATTEST_REQUESTDATA = "cocoattest.requestdata";
@@ -277,8 +266,7 @@ public class SaltServerActionService {
             return SubscribeChannelsAction.subscribeChannelsAction(minions, saltApi, (SubscribeChannelsAction)actionIn);
         }
         else if (ActionFactory.TYPE_KICKSTART_INITIATE.equals(actionType)) {
-            KickstartInitiateAction autoInitAction = (KickstartInitiateAction)actionIn;
-            return autoinstallInitAction(minions, autoInitAction);
+            return KickstartInitiateAction.autoinstallInitAction(minions, (KickstartInitiateAction)actionIn);
         }
         else if (ActionFactory.TYPE_PLAYBOOK.equals(actionType)) {
             return singletonMap(executePlaybookActionCall((PlaybookAction) actionIn), minions);
@@ -992,122 +980,6 @@ public class SaltServerActionService {
         ));
     }
 
-
-
-    private Map<String, String> prepareCobblerBoot(String kickstartHost,
-                                                   String cobblerSystem,
-                                                   boolean autoinstall) {
-        CobblerConnection con = CobblerXMLRPCHelper.getAutomatedConnection();
-        SystemRecord system = SystemRecord.lookupByName(con, cobblerSystem);
-        Profile profile = system.getProfile();
-        Distro dist = profile.getDistro();
-        String kernel = dist.getKernel();
-        String initrd = dist.getInitrd();
-
-        List<String> nameParts = Arrays.asList(dist.getName().split(":"));
-        String saltFSKernel = Paths.get(nameParts.get(1), nameParts.get(0), new File(kernel).getName()).toString();
-        String saltFSInitrd = Paths.get(nameParts.get(1), nameParts.get(0), new File(initrd).getName()).toString();
-        KickstartableTree tree = KickstartFactory.lookupKickstartTreeByLabel(nameParts.get(0),
-                OrgFactory.lookupById(Long.valueOf(nameParts.get(1))));
-        tree.createOrUpdateSaltFS();
-        String kOpts = buildKernelOptions(system, kickstartHost, autoinstall);
-
-
-        Map<String, String> pillar = new HashMap<>();
-        pillar.put("kernel", saltFSKernel);
-        pillar.put("initrd", saltFSInitrd);
-        pillar.put("kopts", kOpts);
-
-        return pillar;
-    }
-
-    private Map<LocalCall<?>, List<MinionSummary>> autoinstallInitAction(List<MinionSummary> minions,
-            KickstartInitiateAction autoInitAction) {
-
-        Map<LocalCall<?>, List<MinionSummary>> ret = new HashMap<>();
-        KickstartActionDetails ksActionDetails = autoInitAction.getKickstartActionDetails();
-        String cobblerSystem = ksActionDetails.getCobblerSystemName();
-        String host = ksActionDetails.getKickstartHost();
-        Map<String, String> bootParams = prepareCobblerBoot(host, cobblerSystem, true);
-        Map<String, Object> pillar = new HashMap<>(bootParams);
-        pillar.put("uyuni-reinstall-name", "reinstall-system");
-        String kOpts = bootParams.get("kopts");
-
-        if (kOpts.contains("autoupgrade=1") || kOpts.contains("uyuni_keep_saltkey=1")) {
-            ksActionDetails.setUpgrade(true);
-        }
-        ret.put(State.apply(List.of(KICKSTART_INITIATE), Optional.of(pillar)), minions);
-
-        return ret;
-    }
-
-    private String buildKernelOptions(SystemRecord sys, String host, boolean autoinstall) {
-        String breed = sys.getProfile().getDistro().getBreed();
-        Map<String, Object> kopts = sys.getResolvedKernelOptions();
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("Resolved kernel options for {}: {}", sys.getName(), convertOptionsMap(kopts));
-        }
-        if (breed.equals("suse")) {
-            //SUSE is not using 'text'. Instead 'textmode' is used as kernel option.
-            if (kopts.containsKey("textmode")) {
-                kopts.remove("text");
-            }
-            else if (kopts.containsKey("text")) {
-                kopts.remove("text");
-                kopts.put("textmode", "1");
-            }
-        }
-        // no additional initrd parameter allowed
-        kopts.remove("initrd");
-        String kernelOptions = convertOptionsMap(kopts);
-        String autoinst = "http://" + host + "/cblr/svc/op/autoinstall/system/" + sys.getName();
-
-        if (StringUtils.isBlank(breed) || breed.equals("redhat")) {
-            if (sys.getProfile().getDistro().getOsVersion().equals("rhel6")) {
-                kernelOptions += " kssendmac ks=" + autoinst;
-            }
-            else {
-                kernelOptions += " inst.ks.sendmac ks=" + autoinst;
-            }
-        }
-        else if (breed.equals("suse")) {
-            kernelOptions += "autoyast=" + autoinst;
-        }
-        else if (breed.equals("debian") || breed.equals("ubuntu")) {
-            kernelOptions += "auto-install/enable=true priority=critical netcfg/choose_interface=auto url=" + autoinst;
-        }
-
-        if (autoinstall) {
-            String infoUrl = "http://" + host + "/cblr/svc/op/nopxe/system/" + sys.getName();
-            kernelOptions += " info=" + infoUrl;
-        }
-        return kernelOptions;
-    }
-
-    private String convertOptionsMap(Map<String, Object> map) {
-        StringBuilder string = new StringBuilder();
-        for (Map.Entry<String, Object> entry : map.entrySet()) {
-            String key = entry.getKey();
-            List<String> keyList;
-            try {
-                 keyList = (List<String>)entry.getValue();
-            }
-            catch (ClassCastException e) {
-                keyList = new ArrayList<>();
-                keyList.add((String) entry.getValue());
-            }
-            string.append(key);
-            if (keyList.isEmpty()) {
-                string.append(" ");
-            }
-            else {
-                for (String value : keyList) {
-                    string.append("=").append(value).append(" ");
-                }
-            }
-        }
-        return string.toString();
-    }
 
     private LocalCall<?> executePlaybookActionCall(PlaybookAction action) {
         PlaybookActionDetails details = action.getDetails();
