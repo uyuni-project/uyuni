2a88bed5b6b79aa5d8d3c83bf3aa9ecfbcdb8333
Michael Calmer <michael.calmer@suse.com>
2025-07-08 11:19:20 +0200

    support podman as container executor for unit tests

--- susemanager-utils/testing/automation/java-checkstyle.sh
+++ susemanager-utils/testing/automation/java-checkstyle.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 SCRIPT=$(basename ${0})
+EXECUTOR="${EXECUTOR:=docker}"
 
 if [ -z ${PRODUCT+x} ];then
     VPRODUCT="VERSION.Uyuni"
@@ -10,12 +11,15 @@ fi
 help() {
   echo ""
   echo "Script to run a docker container to verify java code style"
-  echo "Usage: ${SCRIPT} [-P PROJECT]"
+  echo "Usage: ${SCRIPT} [-P PROJECT] [-p]"
+  echo "       -p use podman instead of docker"
+  echo "       -P PROJECT use the special PROJECT configuration"
 }
 
-while getopts "c:P:h" opts; do
+while getopts "c:P:ph" opts; do
   case "${opts}" in
     P) VPRODUCT="VERSION.${OPTARG}" ;;
+    p) EXECUTOR="podman" ;;
     h) help
        exit 0;;
     *) echo "Invalid syntax. Use ${SCRIPT} -h"
@@ -39,8 +43,8 @@ INITIAL_CMD="/manager/susemanager-utils/testing/automation/initial-objects.sh"
 CMD="/manager/java/scripts/docker-checkstyle.sh"
 CHOWN_CMD="/manager/susemanager-utils/testing/automation/chown-objects.sh $(id -u) $(id -g)"
 
-docker pull $REGISTRY/$PGSQL_CONTAINER
-docker run --privileged --rm=true -v "$GITROOT:/manager" \
+$EXECUTOR pull $REGISTRY/$PGSQL_CONTAINER
+$EXECUTOR run --privileged --rm=true -v "$GITROOT:/manager" \
     -v "${HOME}/.obs-to-maven-cache:/manager/java/.obs-to-maven-cache" \
     -v "${HOME}/.obs-to-maven-cache/repository:/manager/java/buildconf/ivy/repository" \
     $REGISTRY/$PGSQL_CONTAINER \
