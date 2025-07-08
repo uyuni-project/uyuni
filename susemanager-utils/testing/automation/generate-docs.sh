f2688af65028b90253e1df3073af7f24fb60fb60
Michael Calmer <michael.calmer@suse.com>
2025-07-08 11:19:20 +0200

    support podman as container executor for unit tests

--- susemanager-utils/testing/automation/generate-docs.sh
+++ susemanager-utils/testing/automation/generate-docs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 SCRIPT=$(basename ${0})
+EXECUTOR="${EXECUTOR:=docker}"
 
 if [ -z ${PRODUCT+x} ];then
     VPRODUCT="VERSION.Uyuni"
@@ -8,11 +9,12 @@ else
     VPRODUCT="VERSION.${PRODUCT}"
 fi
 
-while getopts 'P:h' option
+while getopts 'P:ph' option
 do
     case ${option} in
         P) VPRODUCT="VERSION.${OPTARG}" ;;
-        h) echo "Usage ${SCRIPT} [-P PRODUCT]";exit 2;;
+        p) EXECUTOR="podman" ;;
+        h) echo "Usage ${SCRIPT} [-P PRODUCT] [-p]";exit 2;;
     esac
 done
 
@@ -27,4 +29,4 @@ echo "Loading ${VPRODUCT}"
 . ${HERE}/${VPRODUCT}
 GITROOT=`readlink -f ${HERE}/../../../`
 
-docker run --interactive --tty --rm -v "$GITROOT:/manager" $REGISTRY/$REPORTDB_DOC_CONTAINER /manager/susemanager-utils/testing/docker/scripts/generate-reportdb-docs.sh "$BRAND_NAME"
+$EXECUTOR run --interactive --tty --rm -v "$GITROOT:/manager" $REGISTRY/$REPORTDB_DOC_CONTAINER /manager/susemanager-utils/testing/docker/scripts/generate-reportdb-docs.sh "$BRAND_NAME"
