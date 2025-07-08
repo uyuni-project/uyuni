5726c5850fa9ce0173319efee44a6606aa91c7b6
Carlo Della Giusta <carlo.dellagiusta@suse.com>
2025-07-08 15:02:39 +0200

    moving SaltUtils handling code to InventoryAction

--- java/code/src/com/redhat/rhn/domain/action/ansible/InventoryAction.java
+++ java/code/src/com/redhat/rhn/domain/action/ansible/InventoryAction.java
@@ -18,6 +18,7 @@ import static java.util.Collections.singletonMap;
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
 
+import com.redhat.rhn.common.hibernate.LookupException;
 import com.redhat.rhn.domain.action.Action;
 import com.redhat.rhn.domain.server.MinionSummary;
 
@@ -25,9 +26,28 @@ import com.suse.manager.webui.services.SaltParameters;
 import com.suse.salt.netapi.calls.LocalCall;
 
 import com.google.gson.reflect.TypeToken;
+import com.redhat.rhn.domain.action.ActionFactory;
+import com.redhat.rhn.domain.action.server.ServerAction;
+import com.redhat.rhn.domain.server.AnsibleFactory;
+import com.redhat.rhn.domain.server.Server;
+import com.redhat.rhn.domain.server.ServerFactory;
+import com.redhat.rhn.domain.server.ansible.InventoryPath;
+import com.redhat.rhn.manager.system.AnsibleManager;
+
+import com.suse.utils.Json;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSyntaxException;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import java.util.List;
 import java.util.Map;
@@ -36,6 +56,8 @@ import java.util.Map;
  * InventoryAction - Action class representing the execution of an Ansible inventory refresh
  */
 public class InventoryAction extends Action {
+    private static final Logger LOG = LogManager.getLogger(InventoryAction.class);
+
     private InventoryActionDetails details;
 
     /**
@@ -90,5 +112,53 @@ public class InventoryAction extends Action {
 
         return new LocalCall<>(SaltParameters.ANSIBLE_INVENTORIES, empty(), of(Map.of("inventory", inventoryPath)),
                 new TypeToken<>() { });
+    }
+
+
+    /**
+     * @param action
+     * @param serverAction
+     * @param jsonResult
+     */
+    public static void handleInventoryRefresh(Action action, ServerAction serverAction, JsonElement jsonResult) {
+        if (jsonResult == null) {
+            serverAction.setStatus(ActionFactory.STATUS_FAILED);
+            serverAction.setResultMsg(
+                    "Error while requesting inventory data from target system: Got no result from system");
+            return;
+        }
+        String inventoryPath = ((InventoryAction) action).getDetails().getInventoryPath();
+        if (serverAction.getStatus().equals(ActionFactory.STATUS_COMPLETED)) {
+            try {
+                Set<String> inventorySystems = AnsibleManager.parseInventoryAndGetHostnames(
+                        Json.GSON.fromJson(jsonResult, Map.class));
+
+                InventoryPath inventory = AnsibleFactory.lookupAnsibleInventoryPath(
+                                serverAction.getServerId(), inventoryPath)
+                        .orElseThrow(() -> new LookupException("Unable to find Ansible inventory: " +
+                                inventoryPath + " for system " + serverAction.getServerId()));
+
+                Set<Server> systemsToAdd = inventorySystems.stream().map(s -> ServerFactory.findByFqdn(s)
+                        .orElse(null)).filter(Objects::nonNull).collect(Collectors.toSet());
+
+                AnsibleManager.handleInventoryRefresh(inventory, systemsToAdd);
+                AnsibleFactory.saveAnsiblePath(inventory);
+
+                serverAction.setResultMsg("Refreshed Ansible managed systems of inventory: '" + inventoryPath + "'");
+            }
+            catch (JsonSyntaxException e) {
+                LOG.error("Unable to parse Ansible hostnames from json: {}", e.getMessage());
+                serverAction.setStatus(ActionFactory.STATUS_FAILED);
+                serverAction.setResultMsg("Unable to parse hostnames from inventory: " + inventoryPath);
+            }
+            catch (LookupException e) {
+                LOG.error(e.getMessage());
+                serverAction.setStatus(ActionFactory.STATUS_FAILED);
+                serverAction.setResultMsg(e.getMessage());
+            }
+        }
+        else {
+            serverAction.setResultMsg(jsonResult.getAsString());
+        }
     }
 }
