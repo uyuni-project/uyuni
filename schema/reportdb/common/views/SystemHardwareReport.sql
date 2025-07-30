16dad3ce6067316c34f3e151ef59d72d93d0aa90
Michael Calmer <michael.calmer@suse.com>
2025-07-30 15:03:50 +0200

    add ip6_addresses to SystemHardwareReport

--- schema/reportdb/common/views/SystemHardwareReport.sql
+++ schema/reportdb/common/views/SystemHardwareReport.sql
@@ -11,6 +11,11 @@
 
 CREATE OR REPLACE VIEW SystemHardwareReport AS
     -- CTEs to group all one to many relationship joining values with ; as separator
+    WITH V6Addresses AS (
+        SELECT mgm_id, system_id, interface_id, string_agg(address || ' (' || scope || ')', ';') AS ip6_addresses
+          FROM SystemNetAddressV6
+      GROUP BY mgm_id, system_id, interface_id
+    )
     SELECT System.mgm_id
               , System.system_id
               , System.profile_name
@@ -43,6 +48,7 @@ CREATE OR REPLACE VIEW SystemHardwareReport AS
               , SystemNetInterface.name  AS primary_interface
               , SystemNetInterface.hardware_address AS hardware_address
               , SystemNetAddressV4.address AS ip_address
+              , V6Addresses.ip6_addresses
               , SystemVirtualdata.virtual_system_id IS NOT NULL AS is_virtualized
               , SystemHardware.synced_date
       FROM System
@@ -50,5 +56,6 @@ CREATE OR REPLACE VIEW SystemHardwareReport AS
               LEFT JOIN SystemVirtualdata ON ( System.mgm_id = SystemVirtualdata.mgm_id AND System.system_id = SystemVirtualdata.virtual_system_id )
               LEFT JOIN SystemNetInterface ON (System.mgm_id = SystemNetInterface.mgm_id AND System.system_id = SystemNetInterface.system_id AND SystemNetInterface.primary_interface)
               LEFT JOIN SystemNetAddressV4 ON (System.mgm_id = SystemNetAddressV4.mgm_id AND System.system_id = SystemNetAddressV4.system_id AND SystemNetInterface.interface_id = SystemNetAddressV4.interface_id)
+              LEFT JOIN V6Addresses ON (System.mgm_id = V6Addresses.mgm_id AND System.system_id = V6Addresses.system_id AND SystemNetInterface.interface_id = V6Addresses.interface_id)
   ORDER BY System.mgm_id, System.system_id
 ;
