ba91c09df0aa2666f3f4d97d57bf1183d7dd641f
Welder Luz <welder.luz@suse.com>
2025-08-07 17:06:57 -0300

    Remove server side pagination and built-in groups from access groups list

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/admin/configuration/AdminConfigurationHandler.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/admin/configuration/AdminConfigurationHandler.java
@@ -50,7 +50,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
 import java.util.stream.Collectors;
 
 import javax.security.auth.login.LoginException;
