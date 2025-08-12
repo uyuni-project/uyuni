7fe73bd815953b0e3973f52888138ad86a24a969
Welder Luz <welder.luz@suse.com>
2025-08-12 09:59:06 -0300

    Don't load groups list when editing

--- web/html/src/manager/admin/access-control/access-group.tsx
+++ web/html/src/manager/admin/access-control/access-group.tsx
@@ -3,8 +3,6 @@ import { hot } from "react-hot-loader/root";
 import * as React from "react";
 import { useRef, useState } from "react";
 
-import { access } from "fs";
-
 import AccessGroupTabContainer from "manager/admin/access-control/access-group-tab-container";
 
 import withPageWrapper from "components/general/with-page-wrapper";
