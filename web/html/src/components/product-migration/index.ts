29afb952114020d1b7d6d3dfb34690415abf585e
Thomas Florio <thomas.florio@suse.com>
2025-08-19 09:08:55 +0200

    Added component to select the channels for migration

--- web/html/src/components/product-migration/index.ts
+++ web/html/src/components/product-migration/index.ts
@@ -1,3 +1,5 @@
 export * from "./types";
 export * from "./MigrationProductList";
 export * from "./MigrationTargetSelectorForm";
+export * from "./MigrationChannelsSelectorForm";
+export * from "./utils";
