5d987090006397b46197830c9be293dcdbc329a3
Thomas Florio <thomas.florio@suse.com>
2025-08-19 09:08:55 +0200

    Renamed ChannelProcessor source file to match the class name

--- web/html/src/manager/content-management/shared/components/panels/sources/channels/base-channel.tsx
+++ web/html/src/manager/content-management/shared/components/panels/sources/channels/base-channel.tsx
@@ -4,7 +4,7 @@ import { BaseChannelType, ChannelTreeType, ChildChannelType } from "core/channel
 
 import { Highlight } from "components/table/Highlight";
 
-import ChannelProcessor from "./channels-processor";
+import ChannelProcessor from "./channel-processor";
 import styles from "./channels-selection.module.scss";
 import ChildChannel from "./child-channel";
 import EmptyChild from "./empty-child";
