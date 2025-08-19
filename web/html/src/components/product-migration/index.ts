ffb416799f14841a33dc3162fdfd140cff626054
Thomas Florio <thomas.florio@suse.com>
2025-08-19 09:08:55 +0200

    Added component to review and schedule the migration

--- web/html/src/components/product-migration/index.ts
+++ web/html/src/components/product-migration/index.ts
@@ -2,4 +2,5 @@ export * from "./types";
 export * from "./MigrationProductList";
 export * from "./MigrationTargetSelectorForm";
 export * from "./MigrationChannelsSelectorForm";
+export * from "./MigrationConfirmScheduleForm";
 export * from "./utils";
