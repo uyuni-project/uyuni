1b7d81f8afd719b5f1668344debea0b18371c47d
Michael Calmer <michael.calmer@suse.com>
2025-08-27 14:59:39 +0200

    succeed liberate product migration also when reinstall packages is disabled

--- java/code/src/com/suse/manager/webui/utils/salt/custom/DistUpgradeSlsResult.java
+++ java/code/src/com/suse/manager/webui/utils/salt/custom/DistUpgradeSlsResult.java
@@ -37,6 +37,9 @@ public class DistUpgradeSlsResult {
    @SerializedName("cmd_|-spmigration_|-/usr/bin/cat /var/log/dnf_sll_migration.log_|-run")
    private StateApplyResult<CmdResult> liberateDnfSll;
 
+   @SerializedName("cmd_|-spmigration_liberated_|-/usr/bin/cat /etc/sysconfig/liberated_|-run")
+   private StateApplyResult<CmdResult> liberated;
+
    /**
     * get spmigration state apply result
     *
@@ -52,6 +55,17 @@ public class DistUpgradeSlsResult {
     * @return spmigration state apply result for liberate
     */
    public StateApplyResult<CmdResult> getLiberate() {
-      return Optional.ofNullable(liberateYumEs).orElse(Optional.ofNullable(liberateDnfEs).orElse(liberateDnfSll));
+       return Optional.ofNullable(liberateYumEs)
+               .or(() -> Optional.ofNullable(liberateDnfEs))
+               .orElse(liberateDnfSll);
+
+   }
+
+    /**
+     * get spmiration_liberated state apply result
+     * @return spmiration_liberated state apply result
+     */
+   public StateApplyResult<CmdResult> getLiberatedResult() {
+       return liberated;
    }
 }
