611be9c3d66707dad08f502e966dcadfa8cde069
Richa <richa.bisht@suse.com>
2025-08-28 11:21:51 +0200

    Fix dropdown and tooltip

--- web/html/src/components/tooltips.tsx
+++ web/html/src/components/tooltips.tsx
@@ -1,19 +1,17 @@
-import { Tooltip } from "bootstrap";
-
 let isListening = false;
 export function initializeTooltips() {
   // Initialize tooltips on existing elements
 
   const initTooltips = () => {
-    const elements = document.querySelectorAll('[data-bs-toggle="tooltip"]:not([data-tooltip-initialized])');
-
-    elements.forEach((el) => {
-      const tooltip = Tooltip.getOrCreateInstance(el, {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    tooltipTriggerList.forEach((el) => {
+      // @ts-ignore
+      const tooltip = new bootstrap.Tooltip(el, {
         trigger: "hover",
       });
 
-      el.setAttribute("data-tooltip-initialized", "true");
       el.addEventListener("click", () => tooltip.hide());
+      el.addEventListener("mouseleave", () => tooltip.hide());
     });
   };
 
@@ -25,15 +23,4 @@ export function initializeTooltips() {
     subtree: true,
   });
   isListening = true;
-}
-
-export function disposeTooltips() {
-  const elements = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-  elements.forEach((el) => {
-    const tooltip = Tooltip.getInstance(el);
-    if (tooltip) {
-      tooltip.dispose();
-      el.removeAttribute("data-tooltip-initialized");
-    }
-  });
 }
--- web/html/src/core/spa/spa-engine.tsx
+++ web/html/src/core/spa/spa-engine.tsx
@@ -9,7 +9,6 @@ import SpaRenderer from "core/spa/spa-renderer";
 
 import { Button } from "components/buttons";
 import { showErrorToastr } from "components/toastr";
-import { disposeTooltips } from "components/tooltips";
 function isLoginPage(pathName) {
   const allLoginPossiblePaths = ["/", "/rhn/manager/login"];
   return allLoginPossiblePaths.some((loginPath) => loginPath === pathName);
@@ -72,8 +71,6 @@ window.pageRenderers.spaengine.init = function init(timeout?: number) {
     }
 
     appInstance.on("beforeNavigate", function (navigation) {
-      // Dispose tooltips
-      disposeTooltips();
       // Integration with bootstrap 3. We need to make sure all the existing modals get fully removed
       // but we have to do it after the navigation ends unless all form inputs contained in the modal will be dropped
       // before the form serialization happens and they will not submitted because they will not exist anymore
