1e304f2cc773cd7f9bbd9cb19ff13c0183e52086
Richa <richa.bisht@suse.com>
2025-09-03 13:39:28 +0200

    Fixed lint and review comments

--- web/html/src/manager/systems/subscribe-channels/subscribe-channels.tsx
+++ web/html/src/manager/systems/subscribe-channels/subscribe-channels.tsx
@@ -547,12 +547,13 @@ class SystemChannels extends Component<SystemChannelsProps, SystemChannelsState>
           {this.dependenciesTooltip(c.id) ? (
             // eslint-disable-next-line jsx-a11y/anchor-is-valid
             <a href="#">
-              <i 
-                className="fa fa-info-circle spacewalk-help-link" 
+              <i
+                className="fa fa-info-circle spacewalk-help-link"
                 data-bs-html="true"
                 data-bs-toggle="tooltip"
-                data-bs-custom-class="wide-tooltip" 
-                title={this.dependenciesTooltip(c.id)}></i>
+                data-bs-custom-class="wide-tooltip"
+                title={this.dependenciesTooltip(c.id)}
+              ></i>
             </a>
           ) : null}
           &nbsp;
