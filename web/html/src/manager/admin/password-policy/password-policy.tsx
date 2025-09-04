62727ad64928757f75088bcf52d2cdfda93e3757
Richa <richa.bisht@suse.com>
2025-09-04 10:54:51 +0200

    Adjusted spacing and alignment in Password Policy page

--- web/html/src/manager/admin/password-policy/password-policy.tsx
+++ web/html/src/manager/admin/password-policy/password-policy.tsx
@@ -26,131 +26,133 @@ const PasswordPolicy = (prop: PasswordPolicyProps) => {
       </div>
       <Form model={policy}>
         <Panel headingLevel="h2" title={t("Password Policy Settings")}>
-          <div className="col-md-8">
-            {/* Minimum Length */}
-            <div className="row">
-              <div className="col-md-4 text-left">
-                <label htmlFor="minLength">{t("Min Password Length")}</label>
-              </div>
-              <Text required name="minLength" divClass="col-md-2" type="number" />
+          {/* Minimum Length */}
+          <Text
+            label={t("Min Password Length")}
+            required name="minLength" 
+            labelClass="col-md-4 text-left"
+            divClass="col-md-2"
+            type="number"
+          />
+          {/* Maximum Length */}
+          <Text
+            label={t("Max Password Length")}
+            required
+            name="maxLength"
+            labelClass="col-md-4 text-left"
+            divClass="col-md-2"
+            type="number"
+          />
+          <div className="row">
+            <div className="col-md-4 text-right">
+              <label className="control-label">Password Complexity:</label>
             </div>
-            {/* Maximum Length */}
-            <div className="row">
-              <div className="col-md-4 text-left">
-                <label htmlFor="maxLength">{t("Max Password Length")}</label>
-              </div>
-              <Text required name="maxLength" divClass="col-md-2" type="number" />
-            </div>
-            {/* Require Digits */}
-            <div className="row">
-              <div className="col-md-4 text-left">
-                <label htmlFor="digitFlag">{t("Require Digits")}</label>
-              </div>
-              <DEPRECATED_Check name="digitFlag" key="digitFlag" divClass="col-md-2" />
-            </div>
-            {/* Require Lowercase Characters */}
-            <div className="row">
-              <div className="col-md-4 text-left">
-                <label htmlFor="lowerCharFlag">{t("Require Lowercase Characters")}</label>
-              </div>
-              <DEPRECATED_Check name="lowerCharFlag" key="lowerCharFlag" divClass="col-md-2" />
-            </div>
-            {/* Require Uppercase Characters */}
-            <div className="row">
-              <div className="col-md-4 text-left">
-                <label htmlFor="upperCharFlag">{t("Require Uppercase Characters")}</label>
-              </div>
-              <DEPRECATED_Check name="upperCharFlag" key="upperCharFlag" divClass="col-md-2" />
-            </div>
-            {/* Restrict Consecutive Characters */}
-            <div className="row">
-              <div className="col-md-4 text-left">
-                <label htmlFor="consecutiveCharsFlag">{t("Restrict Consecutive Characters")}</label>
-              </div>
-              <DEPRECATED_Check name="consecutiveCharsFlag" key="consecutiveCharsFlag" divClass="col-md-2" />
-            </div>
-            {/* Require Special Characters */}
-            <div className="row">
-              <div className="col-md-4 text-left">
-                <label htmlFor="specialCharFlag">{t("Require Special Characters")}</label>
-              </div>
-              <DEPRECATED_Check name="specialCharFlag" key="specialCharFlag" divClass="col-md-2" />
-            </div>
-            {/* Allowed Special Characters */}
-            <div className="row form-group">
-              <div className="col-md-4 text-left">
-                <label htmlFor="specialChars">{t("Allowed Special Characters")}</label>
-              </div>
-              <Text
-                disabled={!policy.specialCharFlag}
-                name="specialChars"
-                divClass="col-md-4"
-                defaultValue={defaults.specialChars?.toLocaleString()}
+            <div className="col-md-8">
+              {/* Require Digits */}
+              <DEPRECATED_Check
+                label={t("Require Digits")}
+                name="digitFlag"
+                key="digitFlag"
+                divClass="col-md-6"
+              />
+              {/* Require Lowercase Characters */}
+              <DEPRECATED_Check
+                label={t("Require Lowercase Characters")}
+                name="lowerCharFlag"
+                key="lowerCharFlag"
+                divClass="col-md-6"
+              />
+              {/* Require Uppercase Characters */}
+              <DEPRECATED_Check
+                label={t("Require Uppercase Characters")}
+                name="upperCharFlag"
+                key="upperCharFlag"
+                divClass="col-md-6"
+              />
+              {/* Restrict Consecutive Characters */}
+              <DEPRECATED_Check
+                label={t("Restrict Consecutive Characters")}
+                name="consecutiveCharsFlag"
+                key="consecutiveCharsFlag"
+                divClass="col-md-6"
+              />
+              {/* Require Special Characters */}
+              <DEPRECATED_Check
+                label={t("Require Special Characters")}
+                name="specialCharFlag"
+                key="specialCharFlag"
+                divClass="col-md-6"
               />
             </div>
-            {/* Restrict Character Occurrence */}
-            <div className="row">
-              <div className="col-md-4 text-left">
-                <label htmlFor="restrictedOccurrenceFlag">{t("Restrict Characters Occurrences")}</label>
-              </div>
-              <DEPRECATED_Check key="restrictedOccurrenceFlag" name="restrictedOccurrenceFlag" divClass="col-md-2" />
-            </div>
-            {/* Maximum Character Occurrence */}
-            <div className="row form-group">
-              <div className="col-md-4 text-left">
-                <label htmlFor="maxCharacterOccurrence">{t("Max Characters Occurrences")}</label>
-              </div>
-              <Text
-                disabled={!policy.restrictedOccurrenceFlag}
-                name="maxCharacterOccurrence"
-                divClass="col-md-2"
-                type="number"
-                defaultValue={defaults.maxCharacterOccurrence.toLocaleString()}
-              />
-            </div>
-            <div className="row">
-              <div className="col-md-4 text-left"></div>
-              <div className="col-md-4 text-left">
-                <div className="btn-group">
-                  <AsyncButton
-                    id="saveButton"
-                    className="btn-primary btn-large"
-                    title={t("Save Password Policy")}
-                    text={t("Save")}
-                    icon="fa-save"
-                    action={() => {
-                      Network.post(policy_endpoint, policy)
-                        .then(() => {
-                          showSuccessToastr(t("Password Policy Changed"));
-                        })
-                        .catch((error) => {
-                          showErrorToastr(error);
-                        });
-                    }}
-                  />
-                  <AsyncButton
-                    id="resetButton"
-                    className="btn-secondary btn-large"
-                    title={t("Reset")}
-                    text={t("Reset")}
-                    icon="fa-refresh"
-                    action={() => {
-                      const default_policy_endpoint = "/rhn/manager/api/admin/config/password-policy/default";
-                      Network.get(default_policy_endpoint)
-                        .then((resp) => {
-                          const defaults: PasswordPolicyData = JSON.parse(resp.data);
-                          Network.post(policy_endpoint, defaults)
-                            .then(() => showSuccessToastr(t("Password Policy Reset to Default")))
-                            .catch((error) => showErrorToastr(error));
-                          setDefaults(defaults);
-                          setPolicy(defaults);
-                        })
-                        .catch((error) => {
-                          showErrorToastr(error);
-                        });
-                    }}
-                  />
-                </div>
+          </div>
+
+          {/* Allowed Special Characters */}
+          <Text
+            label={t("Allowed Special Characters")}
+            disabled={!policy.specialCharFlag}
+            name="specialChars"
+            labelClass="col-md-4 text-left"
+            divClass="col-md-4"
+            defaultValue={defaults.specialChars?.toLocaleString()}
+          />
+          {/* Restrict Character Occurrence */}
+          <DEPRECATED_Check
+            label={t("Restrict Characters Occurrences")}
+            key="restrictedOccurrenceFlag"
+            name="restrictedOccurrenceFlag"
+            divClass="col-md-6 col-md-offset-4 offset-md-4"
+          />
+          {/* Maximum Character Occurrence */}
+          <Text
+            label={t("Max Characters Occurrences")}
+            disabled={!policy.restrictedOccurrenceFlag}
+            name="maxCharacterOccurrence"
+            labelClass="col-md-4 text-left"
+            divClass="col-md-2"
+            type="number"
+            defaultValue={defaults.maxCharacterOccurrence.toLocaleString()}
+          />
+          <div className="row">
+            <div className="col-md-4 text-left col-md-offset-4 offset-md-4">
+              <div className="btn-group">
+                <AsyncButton
+                  id="saveButton"
+                  className="btn-primary btn-large"
+                  title={t("Save Password Policy")}
+                  text={t("Save")}
+                  icon="fa-save"
+                  action={() => {
+                    Network.post(policy_endpoint, policy)
+                      .then(() => {
+                        showSuccessToastr(t("Password Policy Changed"));
+                      })
+                      .catch((error) => {
+                        showErrorToastr(error);
+                      });
+                  }}
+                />
+                <AsyncButton
+                  id="resetButton"
+                  className="btn-secondary btn-large"
+                  title={t("Reset")}
+                  text={t("Reset")}
+                  icon="fa-refresh"
+                  action={() => {
+                    const default_policy_endpoint = "/rhn/manager/api/admin/config/password-policy/default";
+                    Network.get(default_policy_endpoint)
+                      .then((resp) => {
+                        const defaults: PasswordPolicyData = JSON.parse(resp.data);
+                        Network.post(policy_endpoint, defaults)
+                          .then(() => showSuccessToastr(t("Password Policy Reset to Default")))
+                          .catch((error) => showErrorToastr(error));
+                        setDefaults(defaults);
+                        setPolicy(defaults);
+                      })
+                      .catch((error) => {
+                        showErrorToastr(error);
+                      });
+                  }}
+                />
               </div>
             </div>
           </div>
