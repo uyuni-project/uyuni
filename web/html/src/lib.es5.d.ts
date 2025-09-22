b878dd3c55112cc4d57e4776cc75e4b81ed24d97
Etheryte <karl.tarvas@suse.com>
2025-09-22 17:19:19 +0200

    Fix more interfaces

--- web/html/src/lib.es5.d.ts
+++ web/html/src/lib.es5.d.ts
@@ -15,11 +15,11 @@
 /** See https://stackoverflow.com/a/51390763/1470607  */
 type Falsy = false | 0 | "" | null | undefined;
 
-type Array<T> = {
+interface Array<T> {
   /**
    * Returns the elements of an array that meet the condition specified in a callback function.
    * @param predicate A function that accepts up to three arguments. The filter method calls the predicate function one time for each element in the array.
    * @param thisArg An object to which the this keyword can refer in the predicate function. If thisArg is omitted, undefined is used as the this value.
    */
   filter<S extends T>(predicate: BooleanConstructor, thisArg?: any): Exclude<S, Falsy>[];
-};
+}
