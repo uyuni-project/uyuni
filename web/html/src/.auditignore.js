3b594452e3f7711ef7b718bf745cfa0a4b2fee14
Etheryte <karl.tarvas@suse.com>
2025-10-01 14:38:09 +0200

    Update audit, improve gettext sanitization

--- web/html/src/.auditignore.js
+++ web/html/src/.auditignore.js
@@ -2,6 +2,5 @@
  * This file outlines known issues or grandfathered modules for audits
  */
 module.exports = {
-  // We don't have the bandwidth to upgrade Bootstrap right now
-  bootstrap: "No bandwidth to upgrade",
+  "node-gettext": "Patched in-tree, see web/html/src/core/intl/node-gettext.ts",
 };
