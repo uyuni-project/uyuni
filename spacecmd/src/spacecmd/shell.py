uyuni-project/uyuni

commit 4bf8ea8684fc718991e87ae23c3c3aee1ea31d26

Do not use single-char alternations in regexp
Found by Sonarqube's python:S6035
https://sonarcloud.io/organizations/uyuni-project/rules?open=python%3AS6035&rule_key=python%3AS6035

spacecmd/src/spacecmd/shell.py
@@ -130,7 +130,7 @@ class SpacewalkShell(Cmd):
         try:
             # don't split on hyphens or colons during tab completion
             newdelims = readline.get_completer_delims()
-            newdelims = re.sub(":|-|/", "", newdelims)
+            newdelims = re.sub("[:-/]", "", newdelims)
             readline.set_completer_delims(newdelims)
 
             if not options.nohistory:

python/spacewalk/satellite_tools/exporter/xmlWriter.py
@@ -29,8 +29,7 @@ class XMLWriter:
     # We escape &<>'" and chars UTF-8 does not properly escape (everything
     # other than tab (\x09), newline and carriage return (\x0a and \x0d),
     # stuff above ASCII 32 and UTF-8 alphanumeric chars in any language)
-    # pylint: disable-next=anomalous-backslash-in-string
-    _re = re.compile("(&|<|>|'|\"|[^\x09\x0a\x0d\x20-\xff\w])")
+    _re = re.compile(r"([&<>'\"]|[^\x09\x0a\x0d\x20-\xff\w])")
     _escaped_chars = {
         "&": "&amp;",
         "<": "&lt;",
@@ -139,7 +138,7 @@ if __name__ == "__main__":
         # pylint: disable-next=invalid-character-esc
         "String with \"quotes\", 'apostroph', Trond Eivind Glomsrød\n  and Bernhard Rosenkr)Bänzer"
     )
-    r = re.compile("(&|<|>|'|\"|[^\x09\x0a\x0d\x20-\xff])")
+    r = re.compile(r"([&<>'\"]|[^\x09\x0a\x0d\x20-\xff])")
     writer.close_tag("text")
     writer.close_tag("message")
     writer.empty_tag("yahoo", attributes={"abc": 1})

spacecmd/src/spacecmd/utils.py
@@ -390,7 +390,7 @@ def parse_time_input(userinput=""):
 
     # handle time differences (e.g., +1m, +2h)
     if not timestamp:
-        match = re.search(r"^(\+|-)?(\d+)(s|m|h|d)$", userinput, re.I)
+        match = re.search(r"^([+-])?(\d+)([smhd])$", userinput, re.I)
 
         if match and len(match.groups()) >= 2:
             sign = match.group(1)
@@ -400,14 +400,16 @@ def parse_time_input(userinput=""):
             if sign == "-":
                 number = -number
 
-            if re.match("s", unit, re.I):
+            if unit.lower() == "s":
                 delta = timedelta(seconds=number)
-            elif re.match("m", unit, re.I):
+            elif unit.lower() == "m":
                 delta = timedelta(minutes=number)
-            elif re.match("h", unit, re.I):
+            elif unit.lower() == "h":
                 delta = timedelta(hours=number)
-            elif re.match("d", unit, re.I):
+            elif unit.lower() == "d":
                 delta = timedelta(days=number)
+            else:
+                delta = timedelta()
 
             # pylint: disable-next=possibly-used-before-assignment
             timestamp = datetime.now() + delta

commit 74e6c0c59fdaaeb9a7deeea25e4f95f13650c28f

Fix the faulty regex
Caused by https://github.com/uyuni-project/uyuni/commit/4bf8ea8684fc718991e87ae23c3c3aee1ea31d26
from https://github.com/uyuni-project/uyuni/pull/10330

spacecmd/src/spacecmd/shell.py
@@ -130,7 +130,7 @@ class SpacewalkShell(Cmd):
         try:
             # don't split on hyphens or colons during tab completion
             newdelims = readline.get_completer_delims()
-            newdelims = re.sub("[:-/]", "", newdelims)
+            newdelims = re.sub(r"[:\-/]", "", newdelims)
             readline.set_completer_delims(newdelims)
 
             if not options.nohistory: