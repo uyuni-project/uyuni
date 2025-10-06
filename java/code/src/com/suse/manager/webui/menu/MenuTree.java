2bb02637dcf9564f89265edb3099b1b5d051fe28
Richa <richa.bisht@suse.com>
2025-10-06 14:41:25 +0200

    Updated the Documentation link

--- java/code/src/com/suse/manager/webui/menu/MenuTree.java
+++ java/code/src/com/suse/manager/webui/menu/MenuTree.java
@@ -606,7 +606,7 @@ public class MenuTree {
             .addChild(new MenuItem("header.jsp.documentation")
                     .withPrimaryUrl(ConfigDefaults.get().isUyuni() ?
                             "https://www.uyuni-project.org/uyuni-docs/" + docsLocale + "/uyuni/index.html" :
-                            "https://documentation.suse.com/suma/" + getMajorMinorProductVersion() + "/")
+                            "https://documentation.suse.com/multi-linux-manager/" + getMajorMinorProductVersion() + "/")
                     .withTarget("_blank"));
     }
 
