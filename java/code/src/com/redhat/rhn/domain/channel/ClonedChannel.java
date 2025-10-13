7894b04a5851d781083b65f6c13b9a9508f2d646
Michael Calmer <michael.calmer@suse.com>
2025-10-13 11:16:22 +0200

    migrate Channel to annotations

--- java/code/src/com/redhat/rhn/domain/channel/ClonedChannel.java
+++ java/code/src/com/redhat/rhn/domain/channel/ClonedChannel.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2025 SUSE LLC
  * Copyright (c) 2009--2012 Red Hat, Inc.
  *
  * This software is licensed to you under the GNU General Public License,
@@ -16,13 +17,31 @@ package com.redhat.rhn.domain.channel;
 
 import com.redhat.rhn.domain.common.ChecksumType;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import java.util.Optional;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
 
 /**
  * ClonedChannel
  */
+@Entity
+@Table(name = "rhnChannelCloned")
+@Inheritance(strategy = InheritanceType.JOINED)
+@PrimaryKeyJoinColumn(name = "id")
 public class ClonedChannel extends Channel {
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "original_id")
     private Channel original;
 
     /**
@@ -71,5 +90,31 @@ public class ClonedChannel extends Channel {
             setChecksumType(getOriginal().getChecksumType());
         }
         return super.getChecksumType();
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(final Object other) {
+        if (other instanceof SelectableChannel castOther) {
+            return this.equals(castOther.getChannel());
+        }
+        if (!(other instanceof ClonedChannel castOther)) {
+            return false;
+        }
+        return new EqualsBuilder()
+                .appendSuper(super.equals(other))
+                .append(isCloned(), castOther.isCloned())
+                .isEquals();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().appendSuper(super.hashCode()).append(isCloned()).toHashCode();
     }
 }
