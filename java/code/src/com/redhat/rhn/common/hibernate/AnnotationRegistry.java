d4d98f8449dd12f8ab0ac90eb8991fc4f812afab
Carlo Della Giusta <carlo.dellagiusta@suse.com>
2025-10-15 09:45:35 +0200

    migrated KickstartPreserveFileList class

--- java/code/src/com/redhat/rhn/common/hibernate/AnnotationRegistry.java
+++ java/code/src/com/redhat/rhn/common/hibernate/AnnotationRegistry.java
@@ -131,6 +131,7 @@ import com.redhat.rhn.domain.kickstart.KickstartDefaultRegToken;
 import com.redhat.rhn.domain.kickstart.KickstartInstallType;
 import com.redhat.rhn.domain.kickstart.KickstartIpRange;
 import com.redhat.rhn.domain.kickstart.KickstartPackage;
+import com.redhat.rhn.domain.kickstart.KickstartPreserveFileList;
 import com.redhat.rhn.domain.kickstart.KickstartSessionState;
 import com.redhat.rhn.domain.kickstart.KickstartTreeType;
 import com.redhat.rhn.domain.kickstart.KickstartVirtualizationType;
@@ -402,6 +403,7 @@ public class AnnotationRegistry {
             KickstartInstallType.class,
             KickstartIpRange.class,
             KickstartPackage.class,
+            KickstartPreserveFileList.class,
             KickstartSessionState.class,
             KickstartTreeType.class,
             KickstartVirtualizationType.class,
---
+++ java/code/src/com/redhat/rhn/domain/kickstart/KickstartPreserveFileListId.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2025 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ */
+
+package com.redhat.rhn.domain.kickstart;
+
+import com.redhat.rhn.domain.common.FileList;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class KickstartPreserveFileListId implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 8426626801295726357L;
+
+    private KickstartData ksdata;
+    private FileList fileList;
+
+    /**
+     * Constructor
+     */
+    public KickstartPreserveFileListId() {
+    }
+
+    /**
+     * Constructor
+     *
+     * @param ksdataIn   the input ksdata
+     * @param fileListIn the input fileList
+     */
+    public KickstartPreserveFileListId(KickstartData ksdataIn, FileList fileListIn) {
+        ksdata = ksdataIn;
+        fileList = fileListIn;
+    }
+
+    public KickstartData getKsdata() {
+        return ksdata;
+    }
+
+    public void setKsdata(KickstartData ksdataIn) {
+        ksdata = ksdataIn;
+    }
+
+    public FileList getFileList() {
+        return fileList;
+    }
+
+    public void setFileList(FileList fileListIn) {
+        fileList = fileListIn;
+    }
+
+    @Override
+    public boolean equals(Object oIn) {
+        if (!(oIn instanceof KickstartPreserveFileListId that)) {
+            return false;
+        }
+
+        return new EqualsBuilder()
+                .append(ksdata, that.ksdata)
+                .append(fileList, that.fileList)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .append(ksdata)
+                .append(fileList)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "KickstartPreserveFileListId{" +
+                "ksdata=" + ksdata +
+                ", fileList=" + fileList +
+                '}';
+    }
+}
