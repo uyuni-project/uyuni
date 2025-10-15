8dcbfb0f4ac9f8fb662d9d66353ecc4e1f8f77c0
Carlo Della Giusta <carlo.dellagiusta@suse.com>
2025-10-15 08:49:22 +0200

    migrated PrivateChannelFamily class

--- java/code/src/com/redhat/rhn/common/hibernate/AnnotationRegistry.java
+++ java/code/src/com/redhat/rhn/common/hibernate/AnnotationRegistry.java
@@ -65,6 +65,7 @@ import com.redhat.rhn.domain.channel.ChannelSyncFlag;
 import com.redhat.rhn.domain.channel.ClonedChannel;
 import com.redhat.rhn.domain.channel.ContentSourceType;
 import com.redhat.rhn.domain.channel.DistChannelMap;
+import com.redhat.rhn.domain.channel.PrivateChannelFamily;
 import com.redhat.rhn.domain.channel.ProductName;
 import com.redhat.rhn.domain.cloudpayg.CloudRmtHost;
 import com.redhat.rhn.domain.cloudpayg.PaygCredentialsProduct;
@@ -449,6 +450,7 @@ public class AnnotationRegistry {
             PinnedSubscription.class,
             PlaybookActionDetails.class,
             PlaybookPath.class,
+            PrivateChannelFamily.class,
             ProductName.class,
             ProfileCustomDataValue.class,
             ProfileType.class,
