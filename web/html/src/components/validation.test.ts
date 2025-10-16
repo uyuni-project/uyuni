084bf02f1aa7941cbbb659e04788175cfb45d17d
Etheryte <karl.tarvas@suse.com>
2025-10-16 13:44:30 +0200

    WIP validation updates

--- web/html/src/components/validation.test.ts
+++ web/html/src/components/validation.test.ts
@@ -5,10 +5,10 @@ describe("validation", () => {
 
   describe("isInt", () => {
     test("empty inputs", () => {
-      expect(Validation.isInt()()).toEqual(false);
-      expect(Validation.isInt()("")).toEqual(false);
-      expect(Validation.isInt()(null)).toEqual(false);
-      expect(Validation.isInt()(undefined)).toEqual(false);
+      expect(Validation.isInt("")).toEqual(false);
+      expect(Validation.isInt(null)).toEqual(false);
+      expect(Validation.isInt(undefined)).toEqual(false);
+      expect(Validation.isInt(NaN as any)).toEqual(false);
 
       expect(Validation.isInt({ gt: 0 })()).toEqual(false);
       expect(Validation.isInt({ gt: 0 })("")).toEqual(false);
@@ -27,20 +27,21 @@ describe("validation", () => {
     });
 
     test("basic inputs", () => {
-      expect(Validation.isInt()("0")).toEqual(true);
-      expect(Validation.isInt()("-0")).toEqual(true);
-      expect(Validation.isInt()("1")).toEqual(true);
-      expect(Validation.isInt()("-1")).toEqual(true);
+      expect(Validation.isInt("0")).toEqual(true);
+      expect(Validation.isInt("-0")).toEqual(true);
+      expect(Validation.isInt("1")).toEqual(true);
+      expect(Validation.isInt("-1")).toEqual(true);
 
-      expect(Validation.isInt()("0.5")).toEqual(false);
-      expect(Validation.isInt()("0.9999999999")).toEqual(false);
-      expect(Validation.isInt()("-0.5")).toEqual(false);
+      expect(Validation.isInt("0.5")).toEqual(false);
+      expect(Validation.isInt("0.9999999999")).toEqual(false);
+      expect(Validation.isInt("-0.5")).toEqual(false);
+      expect(Validation.isInt("abc")).toEqual(false);
 
-      expect(Validation.isInt()(Number.EPSILON.toString())).toEqual(false);
-      expect(Validation.isInt()((1 - Number.EPSILON).toString())).toEqual(false);
+      expect(Validation.isInt(Number.EPSILON.toString())).toEqual(false);
+      expect(Validation.isInt((1 - Number.EPSILON).toString())).toEqual(false);
 
-      expect(Validation.isInt()(Number.MAX_SAFE_INTEGER.toString())).toEqual(true);
-      expect(Validation.isInt()((-1 * Number.MAX_SAFE_INTEGER).toString())).toEqual(true);
+      expect(Validation.isInt(Number.MAX_SAFE_INTEGER.toString())).toEqual(true);
+      expect(Validation.isInt((-1 * Number.MAX_SAFE_INTEGER).toString())).toEqual(true);
     });
 
     test("gt ranges", () => {
@@ -64,6 +65,49 @@ describe("validation", () => {
       expect(Validation.isInt({ gt: 0, lt: 3 })("1")).toEqual(true);
       expect(Validation.isInt({ gt: 0, lt: 3 })("2")).toEqual(true);
       expect(Validation.isInt({ gt: 0, lt: 3 })("3")).toEqual(false);
+    });
+  });
+
+  describe("isFloat", () => {
+    test("empty inputs", () => {
+      expect(Validation.isFloat("")).toEqual(false);
+      expect(Validation.isFloat(null)).toEqual(false);
+      expect(Validation.isFloat(undefined)).toEqual(false);
+      expect(Validation.isFloat(NaN as any)).toEqual(false);
+    });
+
+    test("basic inputs", () => {
+      expect(Validation.isFloat("0")).toEqual(true);
+      expect(Validation.isFloat("-0")).toEqual(true);
+      expect(Validation.isFloat("0.5")).toEqual(true);
+      expect(Validation.isFloat("-0.5")).toEqual(true);
+      expect(Validation.isFloat("1")).toEqual(true);
+      expect(Validation.isFloat("-1")).toEqual(true);
+      expect(Validation.isFloat("abc")).toEqual(false);
+
+      expect(Validation.isFloat(Number.EPSILON.toString())).toEqual(true);
+      expect(Validation.isFloat((1 - Number.EPSILON).toString())).toEqual(true);
+
+      expect(Validation.isFloat(Number.MAX_VALUE.toString())).toEqual(true);
+      expect(Validation.isFloat((-1 * Number.MAX_VALUE).toString())).toEqual(true);
+    });
+  });
+
+  describe("isUrl", () => {
+    test("empty inputs", () => {
+      expect(Validation.isURL("")).toEqual(false);
+      expect(Validation.isURL(null)).toEqual(false);
+      expect(Validation.isURL(undefined)).toEqual(false);
+      expect(Validation.isURL(NaN as any)).toEqual(false);
+    });
+
+    test("basic inputs", () => {
+      expect(Validation.isURL("http://")).toEqual(false);
+      expect(Validation.isURL("https://")).toEqual(false);
+      expect(Validation.isURL("ftp://example.com")).toEqual(false);
+
+      expect(Validation.isURL("http://example.com")).toEqual(true);
+      expect(Validation.isURL("https://example.com")).toEqual(true);
     });
   });
 });
--- web/html/src/components/validation.ts
+++ web/html/src/components/validation.ts
@@ -1,32 +1,84 @@
-type Validator = (input?: any) => boolean;
+import _isNil from "lodash/isNil";
 
-function matches(regex: RegExp): Validator {
-  return (input?: any) => {
+// We can't infer NaN at a type level here since typeof NaN is number
+const isNoValue = (input: any): input is null | undefined => {
+  return _isNil(input) || (typeof input === "number" && isNaN(input));
+};
+
+type MaybeInput = string | null | undefined;
+type Validator = (input: MaybeInput) => boolean;
+
+const matches = (regex: RegExp): Validator => {
+  return (input: MaybeInput) => {
+    if (isNoValue(input)) {
+      return false;
+    }
+
     const stringified = String(input);
     return regex.test(stringified);
   };
-}
+};
 
-function isInt(range?: { gt: number } | { gt: number; lt: number } | { lt: number }): Validator {
-  return (input?: any) => {
+const isInt = (inputOrRange: MaybeInput | { gt: number } | { gt: number; lt: number } | { lt: number }) => {
+  if (isNoValue(inputOrRange)) {
+    return false;
+  }
+
+  if (typeof inputOrRange === "string") {
+    const parsed = parseFloat(inputOrRange);
+    if (isNaN(parsed) || !Number.isInteger(parsed)) {
+      return false;
+    }
+    return true;
+  }
+
+  return (input: MaybeInput) => {
+    if (isNoValue(input)) {
+      return false;
+    }
+
     const parsed = parseFloat(input);
     if (isNaN(parsed) || !Number.isInteger(parsed)) {
       return false;
     }
 
-    if (range) {
-      if (range) {
-        if ("gt" in range && parsed <= range.gt) {
-          return false;
-        }
-        if ("lt" in range && parsed >= range.lt) {
-          return false;
-        }
+    if (inputOrRange) {
+      if ("gt" in inputOrRange && parsed <= inputOrRange.gt) {
+        return false;
+      }
+      if ("lt" in inputOrRange && parsed >= inputOrRange.lt) {
+        return false;
       }
     }
-
     return true;
   };
-}
+};
 
-export default { matches, isInt };
+const isFloat: Validator = (input: MaybeInput) => {
+  if (isNoValue(input)) {
+    return false;
+  }
+
+  const parsed = parseFloat(input);
+  if (isNaN(parsed)) {
+    return false;
+  }
+  return true;
+};
+
+const isURL: Validator = (input: MaybeInput) => {
+  if (isNoValue(input)) {
+    return false;
+  }
+
+  let url: URL;
+  try {
+    url = new URL(input);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+};
+
+export default { matches, isInt, isFloat, isURL };
