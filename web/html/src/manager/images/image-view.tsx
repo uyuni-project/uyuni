c8e537174d6bc34b6dc85e34a6cc8c8f2debc97d
Etheryte <karl.tarvas@suse.com>
2025-10-23 19:25:01 +0200

    More lint fixes

--- web/html/src/manager/images/image-view.tsx
+++ web/html/src/manager/images/image-view.tsx
@@ -115,7 +115,11 @@ class ImageView extends React.Component<ImageViewProps, ImageViewState> {
   }
 
   updateView(id, tab) {
-    id ? this.getImageInfoDetails(id, tab) : this.getImageInfoList();
+    if (id) {
+      this.getImageInfoDetails(id, tab);
+    } else {
+      this.getImageInfoList();
+    }
     this.clearMessages();
   }
 
