b7d9701196aae862d3914ea32331ae450128f573
Michael Calmer <michael.calmer@suse.com>
2025-10-24 15:57:32 +0200

    fixes

--- java/code/src/com/redhat/rhn/domain/action/server/ServerAction.java
+++ java/code/src/com/redhat/rhn/domain/action/server/ServerAction.java
@@ -27,6 +27,7 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
@@ -52,6 +53,7 @@ import javax.persistence.Table;
 @IdClass(ServerActionId.class)
 public class ServerAction extends BaseDomainHelper implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
     private static final Logger LOG = LogManager.getLogger(ServerAction.class);
 
@@ -68,7 +70,6 @@ public class ServerAction extends BaseDomainHelper implements Serializable {
     @ManyToOne(targetEntity = Action.class, cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.DETACH})
     @JoinColumn(name = "action_id")
     private Action parentAction;
-
 
     @Column(name = "result_code")
     private Long resultCode;
