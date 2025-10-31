65b536aab07429f5f4c4c90a2afc23ebf1af2eae
Carlo Della Giusta <carlo.dellagiusta@suse.com>
2025-10-31 14:16:50 +0100

    moved old syntax generators to the new one

--- java/code/src/com/redhat/rhn/domain/server/ServerGroup.java
+++ java/code/src/com/redhat/rhn/domain/server/ServerGroup.java
@@ -25,6 +25,8 @@ import com.redhat.rhn.manager.configuration.SaltConfigurable;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import java.util.HashSet;
 import java.util.List;
@@ -37,14 +39,12 @@ import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 /**
@@ -58,8 +58,14 @@ import javax.persistence.Table;
 public class ServerGroup extends BaseDomainHelper implements SaltConfigurable  {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "server_group_seq")
-    @SequenceGenerator(name = "server_group_seq", sequenceName = "rhn_server_group_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "server_group_seq")
+    @GenericGenerator(
+            name = "server_group_seq",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "rhn_server_group_id_seq"),
+                    @Parameter(name = "increment_size", value = "1")
+            })
     private Long id;
 
     @Column
