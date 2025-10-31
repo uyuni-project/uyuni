329e1c90a7cc3aa74b1234aff4b897ae190f3d09
Thomas Florio <thomas.florio@suse.com>
2025-10-31 09:16:14 +0000

    Simplify type declarations

--- web/html/src/manager/content-management/shared/components/panels/sources/channels/channels-api-transforms.ts
+++ web/html/src/manager/content-management/shared/components/panels/sources/channels/channels-api-transforms.ts
@@ -1,24 +1,26 @@
-import { BaseChannelType, ChildChannelType } from "core/channels/type/channels.type";
+import { ChannelType } from "core/channels/type/channels.type";
 
 export const toCanonicalRequires = (
   rawRequiresMap: Record<string, number[] | undefined>,
-  channelsMap: Map<number, BaseChannelType | ChildChannelType>
+  channelsMap: Map<number, ChannelType>
 ) => {
   // Create a two-way mapping of what channels require what channels, and what channels are required by what channels
-  const requiresMap = new Map<number, Set<BaseChannelType | ChildChannelType> | undefined>();
-  const requiredByMap = new Map<number, Set<BaseChannelType | ChildChannelType> | undefined>();
+  const requiresMap = new Map<number, Set<ChannelType> | undefined>();
+  const requiredByMap = new Map<number, Set<ChannelType> | undefined>();
   for (const channelIdString in rawRequiresMap) {
     const channelId = parseInt(channelIdString, 10);
     const channel = channelsMap.get(channelId);
     if (isNaN(channelId) || !channel) {
       throw new RangeError("Invalid channel id");
     }
+
     const requiredChannels = rawRequiresMap[channelIdString]
-      ?.map((id) => channelsMap.get(id))
-      .filter((requiredChannel) => {
+      ?.filter((requiredChannelId) => {
         // The original data includes the channel's own id, don't include it in the set
-        return requiredChannel && requiredChannel.id !== channel.id;
-      }) as (BaseChannelType | ChildChannelType)[];
+        return requiredChannelId && requiredChannelId !== channel.id && channelsMap.has(requiredChannelId);
+      })
+      ?.map((requiredChannelId) => channelsMap.get(requiredChannelId)!);
+
     if (requiredChannels?.length) {
       requiresMap.set(channelId, new Set(requiredChannels));
       requiredChannels.forEach((requiredChannel) => {
