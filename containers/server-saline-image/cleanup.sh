8435d21858e151cc92fc864c0067e89962dda509
Cédric Bosdonnat <cbosdonnat@suse.com>
2025-11-06 15:08:30 +0100

    More container images cleanup

--- containers/server-saline-image/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find /etc -maxdepth 1 -type f -name '*-' -delete
-rm -rf /etc/salt /run/zypp.pid /srv/spm /usr/lib/rpm/macros.d/macros.python3 /usr/lib/sysimage/rpm/* \
-       /usr/share/doc /usr/share/licenses /usr/share/man
-find /var/log -maxdepth 1 -type f -name '*.log' | xargs -I /bin/bash -c "echo '' > {}"
