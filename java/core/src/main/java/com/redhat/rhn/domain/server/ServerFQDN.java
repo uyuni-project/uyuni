349b5086de9de34a78bc3e9aa546afc494be6041
Carlo Della Giusta <carlo.dellagiusta@suse.com>
2025-11-12 12:09:03 +0100

    substituted @Type(type = binary)

--- java/core/src/main/java/com/redhat/rhn/domain/server/ServerFQDN.java
+++ java/core/src/main/java/com/redhat/rhn/domain/server/ServerFQDN.java
@@ -16,8 +16,10 @@ import com.redhat.rhn.domain.Identifiable;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.hibernate.annotations.Type;
+import org.hibernate.type.YesNoConverter;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
