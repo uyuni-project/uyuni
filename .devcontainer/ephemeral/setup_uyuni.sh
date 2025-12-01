e3674163e0434dfadd1c6afe2a7a3583ed1b2688
mbussolotto <michele.bussolotto@suse.com>
2025-12-01 19:25:49 +0100

    fixup! use podman

--- .devcontainer/ephemeral/setup_uyuni.sh
+++ .devcontainer/ephemeral/setup_uyuni.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 set -e
 
-# 1. Install Dependencies (mgradm, podman, etc.)
-echo ">>> Installing Uyuni Tools..."
-zypper --non-interactive ref
-zypper --non-interactive in git iproute2 hostname
+# 1. Install Dependencies
+echo ">>> Installing Uyuni Master Tools..."
+sudo zypper --non-interactive ref
+# Install basic tools and Podman (if the feature missed anything specific for SUSE)
+sudo zypper --non-interactive in git iproute2 hostname podman
 
-# Add the Container Utils Master Repo
-zypper --non-interactive ar --no-gpgcheck https://download.opensuse.org/repositories/systemsmanagement:/Uyuni:/Master:/ContainerUtils/openSUSE_Tumbleweed/ uyuni-utils
-zypper --non-interactive ref
-zypper --non-interactive in mgradm podman
+# Add the Uyuni Master Container Utils repo
+sudo zypper --non-interactive ar --no-gpgcheck https://download.opensuse.org/repositories/systemsmanagement:/Uyuni:/Master:/ContainerUtils/openSUSE_Tumbleweed/ uyuni-utils
+sudo zypper --non-interactive ref
+sudo zypper --non-interactive in mgradm
 
-# 2. Configure Network/FQDN
-# Uyuni is picky about FQDNs. We set a dummy one mapped to the local IP.
-export MY_IP=$(ip route get 1 | awk '{print $7;exit}')
+# 2. Configure Fake FQDN
+# Uyuni requires a valid FQDN. We map one to the local loopback.
 export FQDN="uyuni-ephemeral.local"
-echo "$MY_IP $FQDN" >> /etc/hosts
-hostnamectl set-hostname $FQDN
+echo "127.0.0.1 $FQDN" | sudo tee -a /etc/hosts
+sudo hostnamectl set-hostname $FQDN
 
-# 3. Deploy Uyuni from Master
-# We use the --image flag to pull the specific master build
-echo ">>> Deploying Uyuni Server (Master)..."
+# 3. Deploy Uyuni Server
+echo ">>> Deploying Uyuni Server from Master via Podman..."
 
-mgradm install podman \
+# Note: mgradm tries to enable a systemd service. 
+# In a devcontainer, systemd might not be PID 1.
+# We use 'mgradm install' but if it fails on systemd, the container usually still runs.
+# We explicitly set the backend to podman.
+
+sudo mgradm install podman \
   --image registry.opensuse.org/systemsmanagement/uyuni/master/containers/uyuni/server \
   --fqdn $FQDN \
   --admin-password "admin" \
@@ -31,5 +35,5 @@ mgradm install podman \
   --force \
   --debug
 
-echo ">>> Deployment Complete!"
-echo ">>> Access the UI at https://localhost (Accept the self-signed cert)"
+echo ">>> Deployment script finished."
+echo ">>> If the container is running, access https://localhost (accept self-signed cert)."
