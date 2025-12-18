28ae3987e67ecbe990d30955fe161fec3ca3152d
Thomas Florio <thomas.florio@suse.com>
2025-12-18 16:49:47 +0000

    Moved SSM migration endpoints to the proper namespace

--- schema/spacewalk/common/data/endpointNamespace.sql
+++ schema/spacewalk/common/data/endpointNamespace.sql
@@ -5203,22 +5203,22 @@ INSERT INTO access.endpointNamespace (namespace_id, endpoint_id)
     ON CONFLICT DO NOTHING;
 INSERT INTO access.endpointNamespace (namespace_id, endpoint_id)
     SELECT ns.id, ep.id FROM access.namespace ns, access.endpoint ep
-    WHERE ns.namespace = 'systems.migration' AND ns.access_mode = 'W'
+    WHERE ns.namespace = 'systems.software.migration' AND ns.access_mode = 'W'
     AND ep.endpoint = '/manager/systems/ssm/product-migration' AND ep.http_method = 'GET'
     ON CONFLICT DO NOTHING;
 INSERT INTO access.endpointNamespace (namespace_id, endpoint_id)
     SELECT ns.id, ep.id FROM access.namespace ns, access.endpoint ep
-    WHERE ns.namespace = 'systems.migration' AND ns.access_mode = 'W'
+    WHERE ns.namespace = 'systems.software.migration' AND ns.access_mode = 'W'
     AND ep.endpoint = '/manager/systems/ssm/product-migration/dry-run/:actionId' AND ep.http_method = 'GET'
     ON CONFLICT DO NOTHING;
 INSERT INTO access.endpointNamespace (namespace_id, endpoint_id)
     SELECT ns.id, ep.id FROM access.namespace ns, access.endpoint ep
-    WHERE ns.namespace = 'systems.migration' AND ns.access_mode = 'W'
+    WHERE ns.namespace = 'systems.software.migration' AND ns.access_mode = 'W'
     AND ep.endpoint = '/manager/api/systems/migration/computeChannels' AND http_method = 'POST'
     ON CONFLICT DO NOTHING;
 INSERT INTO access.endpointNamespace (namespace_id, endpoint_id)
     SELECT ns.id, ep.id FROM access.namespace ns, access.endpoint ep
-    WHERE ns.namespace = 'systems.migration' AND ns.access_mode = 'W'
+    WHERE ns.namespace = 'systems.software.migration' AND ns.access_mode = 'W'
     AND ep.endpoint = '/manager/api/systems/migration/schedule' AND http_method = 'POST'
     ON CONFLICT DO NOTHING;
 INSERT INTO access.endpointNamespace (namespace_id, endpoint_id)
