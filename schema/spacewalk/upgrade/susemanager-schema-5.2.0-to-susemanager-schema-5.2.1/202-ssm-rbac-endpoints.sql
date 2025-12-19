d3673b87dc8cc0c74f59adef5c0ea2f7d046a3d8
Thomas Florio <thomas.florio@suse.com>
2025-12-19 15:56:21 +0000

    Fix sql idempotency for RBAC endpoints

--- schema/spacewalk/upgrade/susemanager-schema-5.2.0-to-susemanager-schema-5.2.1/202-ssm-rbac-endpoints.sql
+++ schema/spacewalk/upgrade/susemanager-schema-5.2.0-to-susemanager-schema-5.2.1/202-ssm-rbac-endpoints.sql
@@ -15,30 +15,26 @@ INSERT INTO access.endpoint (class_method, endpoint, http_method, scope, auth_re
     SELECT '', '/manager/api/systems/migration/schedule', 'POST', 'W', True
     WHERE NOT EXISTS (SELECT 1 FROM access.endpoint WHERE endpoint = '/manager/api/systems/migration/schedule' AND http_method = 'POST');
 
-INSERT INTO access.namespace (namespace, access_mode, description)
-    SELECT 'systems.migration', 'W', 'Perform product migration on systems'
-    WHERE NOT EXISTS (SELECT 1 FROM access.namespace WHERE namespace = 'systems.migration' AND access_mode = 'W');
-
 INSERT INTO access.endpointNamespace (namespace_id, endpoint_id)
     SELECT ns.id, ep.id FROM access.namespace ns, access.endpoint ep
-    WHERE ns.namespace = 'systems.migration' AND ns.access_mode = 'W'
+    WHERE ns.namespace = 'systems.software.migration' AND ns.access_mode = 'W'
     AND ep.endpoint = '/manager/systems/ssm/product-migration' AND ep.http_method = 'GET'
     ON CONFLICT DO NOTHING;
 
 INSERT INTO access.endpointNamespace (namespace_id, endpoint_id)
     SELECT ns.id, ep.id FROM access.namespace ns, access.endpoint ep
-    WHERE ns.namespace = 'systems.migration' AND ns.access_mode = 'W'
+    WHERE ns.namespace = 'systems.software.migration' AND ns.access_mode = 'W'
     AND ep.endpoint = '/manager/systems/ssm/product-migration/dry-run/:actionId' AND ep.http_method = 'GET'
     ON CONFLICT DO NOTHING;
 
 INSERT INTO access.endpointNamespace (namespace_id, endpoint_id)
     SELECT ns.id, ep.id FROM access.namespace ns, access.endpoint ep
-    WHERE ns.namespace = 'systems.migration' AND ns.access_mode = 'W'
+    WHERE ns.namespace = 'systems.software.migration' AND ns.access_mode = 'W'
     AND ep.endpoint = '/manager/api/systems/migration/computeChannels' AND http_method = 'POST'
     ON CONFLICT DO NOTHING;
 
 INSERT INTO access.endpointNamespace (namespace_id, endpoint_id)
     SELECT ns.id, ep.id FROM access.namespace ns, access.endpoint ep
-    WHERE ns.namespace = 'systems.migration' AND ns.access_mode = 'W'
+    WHERE ns.namespace = 'systems.software.migration' AND ns.access_mode = 'W'
     AND ep.endpoint = '/manager/api/systems/migration/schedule' AND http_method = 'POST'
     ON CONFLICT DO NOTHING;
