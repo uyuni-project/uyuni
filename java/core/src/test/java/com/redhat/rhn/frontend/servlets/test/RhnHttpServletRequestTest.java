d9f08d3b0d62323f1fca9d596649b5221097bbec
Ricardo Mestre <ricardo.mestre@suse.com>
2025-12-22 14:55:31 +0000

    update 2 on Hibernate Factory tests

--- java/core/src/test/java/com/redhat/rhn/frontend/servlets/test/RhnHttpServletRequestTest.java
+++ java/core/src/test/java/com/redhat/rhn/frontend/servlets/test/RhnHttpServletRequestTest.java
@@ -18,11 +18,11 @@ package com.redhat.rhn.frontend.servlets.test;
 import static com.redhat.rhn.frontend.servlets.RhnHttpServletRequest.ACTIVE_LANG_ATTR;
 import static com.redhat.rhn.testing.RhnMockHttpServletRequest.DEFAULT_SERVER_NAME;
 import static com.redhat.rhn.testing.RhnMockHttpServletRequest.LOCALHOST;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.redhat.rhn.frontend.servlets.RhnHttpServletRequest;
