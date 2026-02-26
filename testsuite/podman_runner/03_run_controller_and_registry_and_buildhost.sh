43c2e1a70e470f304b37dcf240c87840b888bac8
szachovy <wiktor.maj@suse.com>
2026-02-26 14:56:50 +0100

    no buildhost

--- testsuite/podman_runner/03_run_controller_and_registry_and_buildhost.sh
+++ testsuite/podman_runner/03_run_controller_and_registry_and_buildhost.sh
@@ -30,9 +30,3 @@ export TAGS="\"not @flaky\""
 EOF
 
 sudo -i podman exec controller bash --login -c 'source /etc/profile.local'
-sudo -i podman run --privileged --rm -d --pull newer --network network -v ${src_dir}/testsuite:/testsuite -v /tmp/buildhost_product_uuid:/sys/class/dmi/id/product_uuid -v /tmp/testing:/tmp -v ${src_dir}/testsuite/podman_runner/salt-minion-entry-point.sh:/salt-minion-entry-point.sh --volume /run/dbus/system_bus_socket:/run/dbus/system_bus_socket:ro -v /var/run/docker.sock:/var/run/docker.sock --name buildhost -h buildhost ghcr.io/$UYUNI_PROJECT/uyuni/ci-buildhost:$UYUNI_VERSION bash -c "/salt-minion-entry-point.sh server 1-SUSE-KEY-x86_64"
-sudo -i podman exec -d buildhost dockerd
-
-sudo -i podman exec buildhost bash -c "sed -e 's/http:\/\/download.opensuse.org/file:\/\/\/mirror\/download.opensuse.org/g' -i /etc/zypp/repos.d/*"
-sudo -i podman exec buildhost bash -c "sed -e 's/https:\/\/download.opensuse.org/file:\/\/\/mirror\/download.opensuse.org/g' -i /etc/zypp/repos.d/*"
-sudo podman ps
