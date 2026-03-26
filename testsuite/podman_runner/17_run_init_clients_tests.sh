45e729c7116e1bb4d8a826e1c6ec653685cd00ba
szachovy <wiktor.maj@suse.com>
2026-03-26 16:31:20 +0100

    optimize further

--- testsuite/podman_runner/17_run_init_clients_tests.sh
+++ testsuite/podman_runner/17_run_init_clients_tests.sh
@@ -12,7 +12,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 echo "Running SSH minion init client test..."
 if ! $PODMAN_CMD exec controller bash --login -c "cd /testsuite && rake cucumber:github_validation_init_clients_ssh_minion"; then
   echo "SSH minion bootstrap failed, cleaning up salt state and restarting sshd..."
-  $PODMAN_CMD exec opensusessh bash -c "rm -rf /var/tmp/venv-salt-minion /var/cache/salt /etc/salt /var/run/salt /tmp/salt-bundle-*; pkill sshd; ssh-keygen -A; /usr/sbin/sshd -e"
+  $PODMAN_CMD exec opensusessh bash -c "rm -rf /var/tmp/venv-salt-minion /var/cache/salt /etc/salt /var/run/salt /tmp/salt-bundle-*; pkill sshd; ssh-keygen -A; /usr/sbin/sshd -e" 
   $PODMAN_CMD exec controller bash --login -c "cd /testsuite && rake cucumber:github_validation_init_clients_ssh_minion"
 fi
 
