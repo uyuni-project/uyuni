d9bc183dd15f39a638b3511662689c7cbce5e2a7
szachovy <wiktor.maj@suse.com>
2026-03-30 19:47:53 +0200

    optimize further

--- testsuite/podman_runner/17_run_init_clients_tests.sh
+++ testsuite/podman_runner/17_run_init_clients_tests.sh
@@ -7,10 +7,12 @@ else
   PODMAN_CMD="sudo -i podman"
 fi
 
-if ! $PODMAN_CMD exec controller bash --login -c "cd /testsuite && cucumber features/github_validation/init_clients/sle_ssh_minion.feature"; then
-  echo "SSH minion bootstrap failed, restarting opensusessh container and retrying..."
+for attempt in 1 2 3 4; do
+  $PODMAN_CMD exec controller bash --login -c "cd /testsuite && cucumber features/github_validation/init_clients/sle_ssh_minion.feature" && break
+  [ "$attempt" -eq 4 ] && exit 1
+  echo "SSH minion bootstrap failed, retrying..."
   $PODMAN_CMD rm -f opensusessh || true
   bash "$(dirname "$0")/10_run_sshminion.sh"
-fi
+done
 
 $PODMAN_CMD exec controller bash --login -c "cd /testsuite && rake cucumber:github_validation_init_clients"
