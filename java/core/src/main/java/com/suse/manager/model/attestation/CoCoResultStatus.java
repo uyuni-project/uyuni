bcdafc43b28fee26e7b178310a9e6854fc06bd4e
Carlo Della Giusta <carlo.dellagiusta@suse.com>
2026-04-13 08:25:17 +0200

    make different states between report and result

---
+++ java/core/src/main/java/com/suse/manager/model/attestation/CoCoResultStatus.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2026 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.suse.manager.model.attestation;
+
+import com.redhat.rhn.common.localization.LocalizationService;
+
+public enum CoCoResultStatus {
+    PENDING,
+    SUCCEEDED,
+    FAILED;
+
+    private final String descriptionKey;
+
+    CoCoResultStatus() {
+        this.descriptionKey = "coco.status." + name().toLowerCase() + ".description";
+    }
+
+    public String getDescription() {
+        return LocalizationService.getInstance().getMessage(descriptionKey);
+    }
+}
