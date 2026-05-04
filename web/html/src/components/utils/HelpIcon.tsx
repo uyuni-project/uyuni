5671354c346686efb941d3f5344dad23d2ee00cd
Richa <richa.bisht@suse.com>
2026-05-04 15:18:34 +0200

    Added scss file

--- web/html/src/components/utils/HelpIcon.tsx
+++ web/html/src/components/utils/HelpIcon.tsx
@@ -5,7 +5,9 @@ type Props = {
 
 /** Display help icon with a title */
 const HelpIcon = ({ text }: Props) => {
-  return text ? <i className="fa fa-question-circle spacewalk-help-link" title={text}></i> : null;
+  return text ? (
+    <i className="fa fa-question-circle spacewalk-help-link" data-bs-toggle="tooltip" title={text}></i>
+  ) : null;
 };
 
 export default HelpIcon;
