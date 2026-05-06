a5fae279674d2d51b930d72b85f2011d0df4c9df
Carlo Della Giusta <carlo.dellagiusta@suse.com>
2026-05-06 16:11:28 +0200

    microservices: common: CertificateHelper refactoring

--- microservices/uyuni-java-common/src/test/java/com/suse/common/utilities/JsonUtilitiesTest.java
+++ microservices/uyuni-java-common/src/test/java/com/suse/common/utilities/JsonUtilitiesTest.java
@@ -15,6 +15,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.suse.common.security.CertificateHelper;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
