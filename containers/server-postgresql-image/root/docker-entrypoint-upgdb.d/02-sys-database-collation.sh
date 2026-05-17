c60dedb9448566a4fec96cb92699e32d97f4be5b
Michael Calmer <michael.calmer@suse.com>
2026-05-17 14:30:16 +0200

    Fix collation version differences in system tables (bsc#1264178)

---
+++ containers/server-postgresql-image/root/docker-entrypoint-upgdb.d/02-sys-database-collation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2026 SUSE LLC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+function run_sql() {
+    PGHOST='' PGHOSTADDR='' psql -v ON_ERROR_STOP=1 \
+        -p "${PGPORT:-5432}" \
+        -U "${POSTGRES_USER:-postgres}" \
+        --no-password --no-psqlrc --tuples-only --no-align "$@"
+}
+
+function query_collation_difference() {
+    cat << EOF | run_sql
+SELECT COALESCE(
+    (SELECT d.datcollversion IS DISTINCT FROM pg_database_collation_actual_version(d.oid)
+      FROM pg_database as d
+     WHERE d.datname = '$1'
+), false);
+EOF
+}
+
+# Check and fix collation differences in system tables
+for dbname in postgres template1; do
+    RES=$(query_collation_difference $dbname)
+
+    if [ "$RES" = "t" ]; then
+        echo "ALTER DATABASE $dbname REFRESH COLLATION VERSION;" | run_sql
+    fi
+done
