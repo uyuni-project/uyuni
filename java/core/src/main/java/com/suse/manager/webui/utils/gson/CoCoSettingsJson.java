0383afd41dc4502b39156aa580f5dd28fd46c1ba
Thomas Florio <thomas.florio@suse.com>
2026-05-29 14:26:16 +0000

    Use record classes for JSON serialization
    Replaced the CoCoSettings and SystemsCoCoSettings pojo with record
    classes to reduced boilerplate code and improve readability.

--- java/core/src/main/java/com/suse/manager/webui/utils/gson/CoCoSettingsJson.java
+++ java/core/src/main/java/com/suse/manager/webui/utils/gson/CoCoSettingsJson.java
@@ -18,92 +18,35 @@ package com.suse.manager.webui.utils.gson;
 import com.suse.manager.model.attestation.CoCoEnvironmentType;
 import com.suse.manager.model.attestation.ServerCoCoAttestationConfig;
 
-import java.util.Objects;
-import java.util.StringJoiner;
+import java.util.Map;
 
-public class CoCoSettingsJson {
-    private final boolean supported;
-
-    private final boolean enabled;
-
-    private final CoCoEnvironmentType environmentType;
-
-    private final boolean attestOnBoot;
+/**
+ * A JSON representation of the CoCo attestation configuration
+ *
+ * @param enabled if the configuration is enabled
+ * @param environmentType the environment type
+ * @param inputData the optional input data for the attestation
+ * @param attestOnBoot true if attestation is performed on boot
+*/
+public record CoCoSettingsJson(
+    boolean enabled,
+    CoCoEnvironmentType environmentType,
+    Map<String, Object> inputData,
+    boolean attestOnBoot) {
 
     /**
      * Builds a json configuration from an existing attestation config
      * @param attestationConfig the current attestation configuration
      */
     public CoCoSettingsJson(ServerCoCoAttestationConfig attestationConfig) {
-        this(true, attestationConfig.isEnabled(), attestationConfig.getEnvironmentType(),
-            attestationConfig.isAttestOnBoot());
+        this(attestationConfig.isEnabled(), attestationConfig.getEnvironmentType(),
+            attestationConfig.getInData(), attestationConfig.isAttestOnBoot());
     }
 
     /**
      * Creates an empty json configuration
-     * @param supportedIn if confidential computing is supported
      */
-    public CoCoSettingsJson(boolean supportedIn) {
-        this(supportedIn, false, CoCoEnvironmentType.getDefault(), false);
-    }
-
-    /**
-     * Default constructor
-     * @param supportedIn if confidential computing is supported
-     * @param enabledIn if the configuration is enabled
-     * @param environmentTypeIn the environment type
-     * @param attestOnBootIn true if attestation is performed on boot
-     */
-    public CoCoSettingsJson(boolean supportedIn, boolean enabledIn, CoCoEnvironmentType environmentTypeIn,
-                            boolean attestOnBootIn) {
-        this.supported = supportedIn;
-        this.enabled = enabledIn;
-        this.environmentType = environmentTypeIn;
-        this.attestOnBoot = attestOnBootIn;
-    }
-
-    public boolean isSupported() {
-        return supported;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public CoCoEnvironmentType getEnvironmentType() {
-        return environmentType;
-    }
-
-    public boolean isAttestOnBoot() {
-        return attestOnBoot;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof CoCoSettingsJson that)) {
-            return false;
-        }
-        return supported == that.supported &&
-            enabled == that.enabled &&
-            attestOnBoot == that.attestOnBoot &&
-            environmentType == that.environmentType;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(supported, enabled, environmentType, attestOnBoot);
-    }
-
-    @Override
-    public String toString() {
-        return new StringJoiner(", ", CoCoSettingsJson.class.getSimpleName() + "[", "]")
-            .add("supported=" + isSupported())
-            .add("enabled=" + isEnabled())
-            .add("environmentType=" + getEnvironmentType())
-            .add("attestOnBoot=" + isAttestOnBoot())
-            .toString();
+    public CoCoSettingsJson() {
+        this(false, CoCoEnvironmentType.getDefault(), Map.of(), false);
     }
 }
