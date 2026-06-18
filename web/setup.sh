b505e5356bbb3542bde8129a9bed9bbccaae8f88
EMaksy <emaksymenko@suse.de>
2026-06-18 09:12:28 +0200

    remove workaround from setup

--- web/setup.sh
+++ web/setup.sh
@@ -2,9 +2,5 @@ set -euxo pipefail
 
 npm --prefix web run clean;
 npm --prefix web ci --ignore-scripts --save=false --omit=dev;
-# leftovers and 0 byte tar files create problems in gitea with LFS
-# remove them
-rm -f web/node_modules/moment-timezone/blah.tar.gz
-rm -f web/node_modules/moment-timezone/curlxx.tar.gz
 npm --prefix web run zip;
 echo "node_modules.tar.gz"
