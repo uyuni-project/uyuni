5d345673c211463d6e1924280dd8f0c262de81c7
Carlo Della Giusta <carlo.dellagiusta@suse.com>
2026-06-29 10:31:17 +0200

    introducing SaltTestCaseExtension

---
+++ java/core/src/test/java/com/redhat/rhn/testing/SaltTestRootPath.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2026 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ */
+
+package com.redhat.rhn.testing;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SaltTestRootPath {
+}
