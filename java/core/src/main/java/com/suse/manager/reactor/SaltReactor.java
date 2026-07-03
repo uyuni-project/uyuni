4aa291e25edeec4ce78021f58f82f4caf019b2a3
Geetansh Goyal <goyalgeetansh@gmail.com>
2026-07-03 12:10:29 +0530

    feat(mqtt): implement Java MQTT event publisher
    Implements the core MQTT event publishing layer inside the Uyuni Java codebase.
    
    - Introduce MqttPublisherService as an async wrapper around Eclipse Paho
      MqttAsyncClient to publish messages to the Mosquitto broker.
    - Introduce MqttEventAction to intercept RegisterMinionEventMessage,
      ApplyStatesEventMessage, JobReturnEventMessage, ImageDeployedEventMessage,
      and BatchStartedEventMessage from the internal MessageQueue.
    - Manage MqttPublisherService lifecycle via RhnServletListener.
    - Add Eclipse Paho client dependency to pom.xml, core/pom.xml, and ivy-suse.xml.
    - Include unit tests in MqttEventActionTest.
    - Add temporary changes changelog file.

--- java/core/src/main/java/com/suse/manager/reactor/SaltReactor.java
+++ java/core/src/main/java/com/suse/manager/reactor/SaltReactor.java
@@ -46,6 +46,8 @@ import com.suse.manager.reactor.messaging.RunnableEventMessage;
 import com.suse.manager.reactor.messaging.RunnableEventMessageAction;
 import com.suse.manager.reactor.messaging.SystemIdGenerateEventMessage;
 import com.suse.manager.reactor.messaging.SystemIdGenerateEventMessageAction;
+import com.suse.manager.reactor.mqtt.MqttEventAction;
+import com.suse.manager.reactor.mqtt.MqttPublisherService;
 import com.suse.manager.saltboot.PXEEvent;
 import com.suse.manager.saltboot.PXEEventMessage;
 import com.suse.manager.saltboot.PXEEventMessageAction;
@@ -123,6 +125,21 @@ public class SaltReactor {
      * Start the salt reactor.
      */
     public void start() {
+        LOG.warn("SaltReactor started - MQTT publisher hook point ready [GSoC-2026]");
+
+        // Retrieve the global MQTT Event Publisher instance if active
+        MqttPublisherService mqttPublisherService = MqttPublisherService.getInstance();
+        if (mqttPublisherService != null) {
+            MqttEventAction mqttEventAction = new MqttEventAction(mqttPublisherService);
+
+            // Register MQTT event actions for the 5 target event classes
+            MessageQueue.registerAction(mqttEventAction, RegisterMinionEventMessage.class);
+            MessageQueue.registerAction(mqttEventAction, ApplyStatesEventMessage.class);
+            MessageQueue.registerAction(mqttEventAction, JobReturnEventMessage.class);
+            MessageQueue.registerAction(mqttEventAction, ImageDeployedEventMessage.class);
+            MessageQueue.registerAction(mqttEventAction, BatchStartedEventMessage.class);
+        }
+
         // Configure message queue to handle minion registrations
         MessageQueue.registerAction(new RegisterMinionEventMessageAction(systemQuery, saltApi, paygMgr, attestationMgr),
                 RegisterMinionEventMessage.class);
---
+++ java/core/src/main/java/com/suse/manager/reactor/mqtt/MqttEventAction.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright (c) 2026 SUSE LLC
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.suse.manager.reactor.mqtt;
+
+import com.redhat.rhn.common.messaging.EventMessage;
+import com.redhat.rhn.common.messaging.MessageAction;
+
+import com.suse.manager.reactor.messaging.ApplyStatesEventMessage;
+import com.suse.manager.reactor.messaging.BatchStartedEventMessage;
+import com.suse.manager.reactor.messaging.ImageDeployedEventMessage;
+import com.suse.manager.reactor.messaging.JobReturnEventMessage;
+import com.suse.manager.reactor.messaging.RegisterMinionEventMessage;
+import com.suse.manager.webui.utils.salt.custom.MinionStartupGrains;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Custom {@link MessageAction} for publishing Uyuni reactor events to an MQTT broker.
+ *
+ * <p>This action is registered alongside existing handlers in
+ * {@link com.suse.manager.reactor.SaltReactor} and intercepts five key event types,
+ * mapping each to a structured MQTT topic and JSON payload.</p>
+ */
+public class MqttEventAction implements MessageAction {
+
+    private static final Logger LOG = LogManager.getLogger(MqttEventAction.class);
+
+    private final MqttPublisherService mqttPublisherService;
+
+    /**
+     * Constructor taking the publisher service.
+     * @param mqttPublisherServiceIn the publisher service instance
+     */
+    public MqttEventAction(MqttPublisherService mqttPublisherServiceIn) {
+        this.mqttPublisherService = mqttPublisherServiceIn;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void execute(EventMessage msg) {
+        if (msg == null) {
+            return;
+        }
+
+        LOG.warn("MqttEventAction.execute called for message of type: {}", msg.getClass().getName());
+
+        try {
+            if (msg instanceof RegisterMinionEventMessage registerMsg) {
+                handleRegisterMinion(registerMsg);
+            }
+            else if (msg instanceof JobReturnEventMessage jobMsg) {
+                handleJobReturn(jobMsg);
+            }
+            else if (msg instanceof ApplyStatesEventMessage applyMsg) {
+                handleApplyStates(applyMsg);
+            }
+            else if (msg instanceof ImageDeployedEventMessage imageMsg) {
+                handleImageDeployed(imageMsg);
+            }
+            else if (msg instanceof BatchStartedEventMessage batchMsg) {
+                handleBatchStarted(batchMsg);
+            }
+            else {
+                LOG.debug("Unhandled event type in MqttEventAction: {}",
+                        msg.getClass().getName());
+            }
+        }
+        catch (Exception e) {
+            LOG.error("Failed to process event in MqttEventAction: {}",
+                    e.getMessage(), e);
+        }
+    }
+
+    private void handleRegisterMinion(RegisterMinionEventMessage registerMsg) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("minionId", registerMsg.getMinionId());
+        registerMsg.getMinionStartupGrains().ifPresent(grains -> {
+            grains.getMachineId().ifPresent(id -> data.put("machineId", id));
+            data.put("saltbootInitrd", grains.getSaltbootInitrd());
+            grains.getSuseManagerGrain()
+                    .flatMap(MinionStartupGrains.SuseManagerGrain::getManagementKey)
+                    .ifPresent(key -> data.put("managementKey", key));
+        });
+        mqttPublisherService.publish("uyuni/events/systems/registered", data);
+    }
+
+    private void handleJobReturn(JobReturnEventMessage jobMsg) {
+        var event = jobMsg.getJobReturnEvent();
+        if (event != null) {
+            Map<String, Object> data = new HashMap<>();
+            data.put("minionId", event.getMinionId());
+            data.put("jid", event.getJobId());
+            if (event.getData() != null) {
+                data.put("fun", event.getData().getFun());
+                data.put("success", event.getData().isSuccess());
+                data.put("retcode", event.getData().getRetcode());
+                data.put("timestamp", event.getData().getTimestamp());
+            }
+            mqttPublisherService.publish("uyuni/events/jobs/returned", data);
+        }
+    }
+
+    private void handleApplyStates(ApplyStatesEventMessage applyMsg) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("serverId", applyMsg.getServerId());
+        data.put("userId", applyMsg.getUserId());
+        data.put("stateNames", applyMsg.getStateNames());
+        data.put("forcePackageListRefresh",
+                applyMsg.isForcePackageListRefresh());
+        data.put("directCall", applyMsg.isDirectCall());
+        mqttPublisherService.publish("uyuni/events/states/applied", data);
+    }
+
+    private void handleImageDeployed(ImageDeployedEventMessage imageMsg) {
+        var event = imageMsg.getImageDeployedEvent();
+        if (event != null) {
+            Map<String, Object> data = new HashMap<>();
+            event.getMachineId().ifPresent(
+                    id -> data.put("machineId", id));
+            data.put("grains", event.getGrains());
+            mqttPublisherService.publish("uyuni/events/images/deployed", data);
+        }
+    }
+
+    private void handleBatchStarted(BatchStartedEventMessage batchMsg) {
+        var event = batchMsg.getBatchStartedEvent();
+        if (event != null) {
+            Map<String, Object> data = new HashMap<>();
+            data.put("jid", event.getJobId());
+            if (event.getData() != null) {
+                data.put("availableMinions",
+                        event.getData().getAvailableMinions());
+                data.put("downMinions",
+                        event.getData().getDownMinions());
+                data.put("timestamp",
+                        event.getData().getTimestamp());
+            }
+            mqttPublisherService.publish(
+                    "uyuni/events/batches/started", data);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean canRunConcurrently() {
+        return true;
+    }
+}
