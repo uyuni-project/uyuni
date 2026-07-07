9c34586b83b3a660f7d12c593c31da90c0fd8fec
EMaksy <emaksymenko@suse.de>
2026-07-07 16:22:16 +0200

    refactor test

--- web/html/src/components/dialog/util.test.ts
+++ web/html/src/components/dialog/util.test.ts
@@ -1,26 +1,47 @@
 import { hideDialog, showDialog } from "./util";
 
+function renderModalTarget(id: string) {
+  document.body.innerHTML = `<div id="${id}" class="modal"></div>`;
+}
+
+function renderNonModalTarget(id: string) {
+  document.body.innerHTML = `<div id="${id}"></div>`;
+}
+
+function mockBootstrapModalPlugin() {
+  const originalModal = (jQuery.fn as any).modal;
+  const modalMock = jest.fn(function (this: JQuery) {
+    return this;
+  });
+
+  (jQuery.fn as any).modal = modalMock;
+
+  return {
+    modalMock,
+    restore: () => {
+      if (originalModal === undefined) {
+        delete (jQuery.fn as any).modal;
+      } else {
+        (jQuery.fn as any).modal = originalModal;
+      }
+    },
+  };
+}
+
 describe("dialog util", () => {
-  let originalModal: unknown;
   let modalMock: jest.Mock;
+  let restoreModalPlugin: () => void;
 
   beforeEach(() => {
-    originalModal = (jQuery.fn as any).modal;
-    modalMock = jest.fn(function (this: JQuery) {
-      return this;
-    });
-    (jQuery.fn as any).modal = modalMock;
+    const bootstrapModalPlugin = mockBootstrapModalPlugin();
+    modalMock = bootstrapModalPlugin.modalMock;
+    restoreModalPlugin = bootstrapModalPlugin.restore;
     document.body.innerHTML = "";
   });
 
   afterEach(() => {
     document.body.innerHTML = "";
-
-    if (originalModal === undefined) {
-      delete (jQuery.fn as any).modal;
-    } else {
-      (jQuery.fn as any).modal = originalModal;
-    }
+    restoreModalPlugin();
   });
 
   test("does not invoke Bootstrap for missing targets", () => {
@@ -31,7 +52,7 @@ describe("dialog util", () => {
   });
 
   test("does not invoke Bootstrap for non-modal targets", () => {
-    document.body.innerHTML = '<div id="react-modal-dialog"></div>';
+    renderNonModalTarget("react-modal-dialog");
 
     showDialog("react-modal-dialog");
     hideDialog("react-modal-dialog");
@@ -40,7 +61,7 @@ describe("dialog util", () => {
   });
 
   test("invokes Bootstrap for modal targets", () => {
-    document.body.innerHTML = '<div id="legacy-dialog" class="modal"></div>';
+    renderModalTarget("legacy-dialog");
 
     showDialog("legacy-dialog");
     expect(modalMock).toBeCalledWith("show");
