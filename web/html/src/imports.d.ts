63945e0317ced77e46024c30d38062f3c381da29
EMaksy <emaksymenko@suse.de>
2026-07-08 17:43:53 +0200

    Add Storybook setup for web components

--- web/html/src/imports.d.ts
+++ web/html/src/imports.d.ts
@@ -1,5 +1,12 @@
 declare module "*.css";
 declare module "*.scss";
+declare module "*.scss?lazy" {
+  const stylesheet: {
+    use(): void;
+    unuse(): void;
+  };
+  export default stylesheet;
+}
 declare module "*.svg";
 declare module "*.png";
 declare module "*?raw";
