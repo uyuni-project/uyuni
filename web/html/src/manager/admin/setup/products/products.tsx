ccab5e7bb6ef9ab1be8318d0994233105680882f
EMaksy <emaksymenko@suse.de>
2026-07-27 15:34:53 +0200

    improve error handling

--- web/html/src/manager/admin/setup/products/products.tsx
+++ web/html/src/manager/admin/setup/products/products.tsx
@@ -160,7 +160,11 @@ class ProductsPageWrapper extends Component<ProductsPageWrapperProps, ProductsPa
           });
         }
       })
-      .catch(this.handleResponseError);
+      .catch((error) => {
+        if (this.metadataRequest === metadataRequest) {
+          this.handleResponseError(error);
+        }
+      });
 
     const productsRequest = reloadData();
     this.productsRequest = productsRequest;
@@ -177,7 +181,11 @@ class ProductsPageWrapper extends Component<ProductsPageWrapperProps, ProductsPa
           scheduledItems: [],
         });
       })
-      .catch(this.handleResponseError);
+      .catch((error) => {
+        if (this.productsRequest === productsRequest) {
+          this.handleResponseError(error);
+        }
+      });
   };
 
   handleSelectedItems = (items) => {
@@ -203,6 +211,10 @@ class ProductsPageWrapper extends Component<ProductsPageWrapperProps, ProductsPa
   };
 
   updateSccSyncRunning = (sccSyncStatus) => {
+    if (this.isUnmounted) {
+      return;
+    }
+
     // if it was running and now it's finished
     if (this.state.sccSyncRunning && !sccSyncStatus) {
       this.refreshServerData(); // reload data
