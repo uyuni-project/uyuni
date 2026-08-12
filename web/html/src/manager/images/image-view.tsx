52b903e55d9d8f149c4a7a465428c8f6f8a9bdd1
bisht-richa <richa.bisht@suse.com>
2026-08-12 10:10:46 +0200

    Removed broken Go to OS image directory listing link

--- web/html/src/manager/images/image-view.tsx
+++ web/html/src/manager/images/image-view.tsx
@@ -33,7 +33,6 @@ declare global {
     timezone?: any;
     localTime?: any;
     isRuntimeInfoEnabled?: any;
-    osImageStoreUrl?: string;
   }
 }
 
@@ -767,14 +766,6 @@ export class ImageViewList extends Component<ImageViewListProps, ImageViewListSt
             }}
           />
         </Table>
-        {window.osImageStoreUrl && (
-          <div>
-            <a href={window.osImageStoreUrl} target="_blank" rel="noopener noreferrer">
-              <i className="fa fa-folder-open" />
-              Go to OS image directory listing
-            </a>
-          </div>
-        )}
         <DeleteDialog
           id="delete-modal"
           title={t("Delete Image")}
