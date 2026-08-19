667f56fd4e82860dfc5d2e0b5e7ab2ba2b4bbb3a
Maxime Noel <maxime.noel@suse.com>
2026-08-19 12:16:17 +0200

    Add timeout minutes for all workflows and adjust timeout for some tests

--- testsuite/podman_runner/07_server_setup.sh
+++ testsuite/podman_runner/07_server_setup.sh
@@ -20,7 +20,7 @@ $PODMAN_CMD run --cap-add AUDIT_CONTROL \
     -v /tmp/testing/ssl:/ssl:z \
     --name=ssl-generator \
     --network network \
-    --pull newer \
+    --pull missing \
     ghcr.io/$UYUNI_PROJECT/uyuni/ci-test-server-all-in-one-dev:$UYUNI_VERSION \
     bash -xc "/testsuite/podman_runner/generate_certificates.sh"
 
@@ -47,7 +47,7 @@ $PODMAN_CMD run \
     --hostname uyuni-db.mgr.internal \
     --network-alias db \
     --network-alias reportdb \
-    --pull newer \
+    --pull missing \
     --secret uyuni-db-ca,type=mount,target=/etc/pki/trust/anchors/DB-RHN-ORG-TRUSTED-SSL-CERT \
     --secret uyuni-db-key,type=mount,uid=999,mode=0400,target=/etc/pki/tls/private/pg-spacewalk.key \
     --secret uyuni-db-cert,type=mount,target=/etc/pki/tls/certs/spacewalk.crt \
