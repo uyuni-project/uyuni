cea02c47bafd318d874ad1d8876ef8f86154e0f6
Michael Calmer <michael.calmer@suse.com>
2026-09-09 17:40:09 +0200

    allow testing when product is still in beta phase

--- java/core/src/main/java/com/suse/manager/model/products/migration/MigrationDataFactory.java
+++ java/core/src/main/java/com/suse/manager/model/products/migration/MigrationDataFactory.java
@@ -250,7 +250,10 @@ public class MigrationDataFactory {
         String sourceClass = Optional.ofNullable(sourceBase.getChannelFamily())
                 .map(ChannelFamily::getLabel).orElse("");
         String targetClass = Optional.ofNullable(targetBase.getChannelFamily())
-                .map(ChannelFamily::getLabel).orElse("");
+                .map(ChannelFamily::getLabel)
+                .map(l -> l.replace("-BETA", ""))
+                .map(l -> l.replace("-ALPHA", ""))
+                .orElse("");
         // 2. Products must be in the same family (e.g., SLES to SLES)
         if (!sourceClass.equals(targetClass)) {
             return false;
