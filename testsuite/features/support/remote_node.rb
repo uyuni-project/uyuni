04e8fdfabcda3c8e5d58346b84003345bd02397d
Maxime Noel <maxime.noel@suse.com>
2026-09-14 14:58:39 +0200

    Fix grctl detection

--- testsuite/features/support/remote_node.rb
+++ testsuite/features/support/remote_node.rb
@@ -34,10 +34,13 @@ class RemoteNode
 
     $named_nodes[host] = @hostname
     uyuni_not_installed = false
-    if %w[SERVER SERVER2 SERVER3 SERVER4].include?(ENV_VAR_BY_HOST[@host])
+    if @host == 'server'
       uyuni_not_installed = !ssh('which kubectl && kubectl get deployment uyuni -n ${SERVER_NAMESPACE:-uyuni}', host: @target).last.zero? && !ssh('podman container exists uyuni-server', host: @target).last.zero?
 
       @has_mgrctl = ssh('which mgrctl', host: @target).last.zero? && !uyuni_not_installed
+      @has_kubectl = ssh('which kubectl', host: @target).last.zero?
+    elsif %w[SERVER SERVER2 SERVER3 SERVER4].include?(ENV_VAR_BY_HOST[@host])
+      @has_mgrctl = ssh('which mgrctl', host: @target).last.zero?
       @has_kubectl = ssh('which kubectl', host: @target).last.zero?
     end
 
