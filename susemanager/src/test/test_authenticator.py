4d1957b59b4cc466927ad1d6acffc40042fb3dde
Cédric Bosdonnat <cbosdonnat@suse.com>
2026-09-16 18:18:29 +0200

    Add the SPDX headers in susemanager folder
    This commit has been generated with these fish shell commands:
    
        for f in (fd py susemanager/src/test/)
            sed -i '/^# Copyright/,/^$/c\
        # SPDX-FileCopyrightText: 2026 SUSE LLC\
        #\
        # SPDX-License-Identifier: LGPL-2.1-only' $f
        end
    
        reuse annotate --skip-unrecognised --no-replace --license "GPL-2.0-only" --copyright "SUSE LLC" --skip-existing -r susemanager/src

--- susemanager/src/test/test_authenticator.py
+++ susemanager/src/test/test_authenticator.py
@@ -1,20 +1,9 @@
 #  pylint: disable=missing-module-docstring
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2014 Novell, Inc.
-#   This library is free software; you can redistribute it and/or modify
-# it only under the terms of version 2.1 of the GNU Lesser General Public
-# License as published by the Free Software Foundation.
+# SPDX-FileCopyrightText: 2026 SUSE LLC
 #
-#   This library is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-# details.
-#
-#   You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-
+# SPDX-License-Identifier: LGPL-2.1-only
 try:
     import unittest2 as unittest
 except ImportError:
